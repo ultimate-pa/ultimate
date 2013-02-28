83fcb261d6c380c9845c2b06bfccb1770e608646
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-02-28 02:22:47 +0000

    example

---
+++ trunk/examples/programs/minitests/openBugs/ACSL-Paranthesis.c
@@ -0,0 +1,10 @@
+//#mUnsafe
+// Author: heizmann@informatik.uni-freiburg.de
+// Date: 27.2.2013
+
+//Bug in r8195.  ACSL Parser unable to handle paranthesis.
+
+
+int main(int n) {
+    //@ assert (1==1 || 1==0) && 0==1;
+}
