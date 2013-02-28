090dd046c1c0e509adb1d377b1e13119f4f73f26
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-02-28 20:27:54 +0000

    determinized automaton

--- trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operations/Det.java
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operations/Det.java
@@ -35,285 +35,274 @@ public class Det<LETTER, STATE> implements INestedWordAutomaton<LETTER, STATE> {
 		m_StateFactory = sf;
 		m_Cache = new NestedWordAutomaton<LETTER, STATE>(operand.getAlphabet(), 
 				operand.getCallAlphabet(), operand.getReturnAlphabet(), sf);
-		
+		constructInitialState();
+	}
+	
+	private void constructInitialState() {
+		DeterminizedState<LETTER, STATE> initialDet = 
+				m_StateDeterminizer.initialState();
+		STATE initialState = initialDet.getContent(m_StateFactory);
+		m_det2res.put(initialDet, initialState);
+		m_Cache.addState(true, initialDet.containsFinal(), initialState);
+	}
+	
+	private STATE getOrConstructState(DeterminizedState<LETTER, STATE> detState) {
+		STATE state = m_det2res.get(detState);
+		if (state == null) {
+			state = detState.getContent(m_StateFactory);
+			m_det2res.put(detState, state);
+			m_res2det.put(state, detState);
+			m_Cache.addState(false, detState.containsFinal(), state);
+		}
+		return state;
 	}
 	
 
 	@Override
 	public IRun<LETTER, STATE> acceptingRun() throws OperationCanceledException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean accepts(Word<LETTER> word) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<LETTER> getAlphabet() {
-		// TODO Auto-generated method stub
-		return null;
+		return m_Operand.getAlphabet();
 	}
 
 	@Override
 	public String sizeInformation() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<LETTER> getInternalAlphabet() {
-		// TODO Auto-generated method stub
-		return null;
+		return m_Operand.getInternalAlphabet();
 	}
 
 	@Override
 	public Collection<LETTER> getCallAlphabet() {
-		// TODO Auto-generated method stub
-		return null;
+		return m_Operand.getCallAlphabet();
 	}
 
 	@Override
 	public Collection<LETTER> getReturnAlphabet() {
-		// TODO Auto-generated method stub
-		return null;
+		return m_Operand.getReturnAlphabet();
 	}
 
 	@Override
 	public StateFactory<STATE> getStateFactory() {
-		// TODO Auto-generated method stub
-		return null;
+		return m_Operand.getStateFactory();
 	}
 
 	@Override
 	public Collection<STATE> getStates() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<STATE> getInitialStates() {
-		if (m_Cache.getInitialStates().iterator().hasNext()) {
-			
-		}
-		// TODO Auto-generated method stub
-		return null;
+		return m_Cache.getInitialStates();
 	}
 
 	@Override
 	public Collection<STATE> getFinalStates() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean isInitial(STATE state) {
-		// TODO Auto-generated method stub
-		return false;
+		return m_Cache.isInitial(state);
 	}
 
 	@Override
 	public boolean isFinal(STATE state) {
-		// TODO Auto-generated method stub
-		return false;
+		return m_Cache.isFinal(state);
 	}
 
 	@Override
 	public void addState(boolean isInitial, boolean isFinal, STATE state) {
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void removeState(STATE state) {
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public STATE getEmptyStackState() {
-		// TODO Auto-generated method stub
-		return null;
+		return m_Cache.getEmptyStackState();
 	}
 
 	@Override
 	public Collection<LETTER> lettersInternal(STATE state) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<LETTER> lettersCall(STATE state) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<LETTER> lettersReturn(STATE state) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<LETTER> lettersInternalIncoming(STATE state) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<LETTER> lettersCallIncoming(STATE state) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<LETTER> lettersReturnIncoming(STATE state) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<LETTER> lettersReturnSummary(STATE state) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<STATE> succInternal(STATE state, LETTER letter) {
 		Collection<STATE> succs = m_Cache.succInternal(state, letter);
 		if (succs == null) {
-			DeterminizedState<LETTER, STATE> det = m_res2det.get(state);
-			assert (det != null);
+			DeterminizedState<LETTER, STATE> detState = m_res2det.get(state);
+			assert (detState != null);
 			DeterminizedState<LETTER, STATE> detSucc = 
-					m_StateDeterminizer.internalSuccessor(det, letter);
-			STATE resSucc = m_det2res.get(detSucc);
-			if (resSucc == null) {
-				resSucc = detSucc.getContent(m_StateFactory);
-				m_det2res.put(detSucc, resSucc);
-			}
-			// FIXME: continue here !!!!!!!!!!!!!!!!!!111111;
-			
+					m_StateDeterminizer.internalSuccessor(detState, letter);
+			STATE succ = getOrConstructState(detSucc);
+			m_Cache.addInternalTransition(state, letter, succ);
 		}
-		// TODO Auto-generated method stub
-		return null;
+		return m_Cache.succInternal(state, letter);
 	}
 
 	@Override
 	public Collection<STATE> succCall(STATE state, LETTER letter) {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<STATE> succs = m_Cache.succCall(state, letter);
+		if (succs == null) {
+			DeterminizedState<LETTER, STATE> detState = m_res2det.get(state);
+			assert (detState != null);
+			DeterminizedState<LETTER, STATE> detSucc = 
+					m_StateDeterminizer.callSuccessor(detState, letter);
+			STATE succ = getOrConstructState(detSucc);
+			m_Cache.addCallTransition(state, letter, succ);
+		}
+		return m_Cache.succCall(state, letter);
 	}
 
 	@Override
 	public Collection<STATE> hierPred(STATE state, LETTER letter) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<STATE> succReturn(STATE state, STATE hier, LETTER letter) {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<STATE> succs = m_Cache.succReturn(state, hier, letter);
+		if (succs == null) {
+			DeterminizedState<LETTER, STATE> detState = m_res2det.get(state);
+			assert (detState != null);
+			DeterminizedState<LETTER, STATE> detHier = m_res2det.get(hier);
+			assert (detHier != null);
+			DeterminizedState<LETTER, STATE> detSucc = 
+					m_StateDeterminizer.returnSuccessor(detState, detHier, letter);
+			STATE succ = getOrConstructState(detSucc);
+			m_Cache.addReturnTransition(state, hier, letter, succ);
+		}
+		return m_Cache.succReturn(state, hier, letter);
 	}
 
 	@Override
 	public Collection<STATE> predInternal(STATE state, LETTER letter) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<STATE> predCall(STATE state, LETTER letter) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<STATE> predReturnLin(STATE state, LETTER letter,
 			STATE hier) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Collection<STATE> predReturnHier(STATE state, LETTER letter) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void addInternalTransition(STATE pred, LETTER letter, STATE succ) {
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void addCallTransition(STATE pred, LETTER letter, STATE succ) {
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void addReturnTransition(STATE pred, STATE hier, LETTER letter,
 			STATE succ) {
-		// TODO Auto-generated method stub
-		
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean finalIsTrap() {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean isDeterministic() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean isTotal() {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public NestedRun<LETTER, STATE> included(
 			INestedWordAutomaton<LETTER, STATE> nwa)
 			throws OperationCanceledException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public NestedLassoRun<LETTER, STATE> buchiIncluded(
 			INestedWordAutomaton<LETTER, STATE> nwa)
 			throws OperationCanceledException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Iterable<SummaryReturnTransition<LETTER, STATE>> getSummaryReturnTransitions(
 			LETTER letter, STATE hier) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Iterable<IncomingReturnTransition<LETTER, STATE>> getIncomingReturnTransitions(
 			LETTER letter, STATE hier) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 }
