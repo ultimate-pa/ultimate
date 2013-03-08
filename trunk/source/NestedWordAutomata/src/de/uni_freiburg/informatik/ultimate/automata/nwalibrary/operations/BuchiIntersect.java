eae6be43d31abcc8980e2484fb7aa9a103f8fc0c
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-03-08 14:49:36 +0000

    bugfix: missing class

---
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operations/BuchiIntersect.java
@@ -0,0 +1,31 @@
+package de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations;
+
+import org.apache.log4j.Logger;
+
+import de.uni_freiburg.informatik.ultimate.automata.Activator;
+import de.uni_freiburg.informatik.ultimate.automata.IOperation;
+import de.uni_freiburg.informatik.ultimate.automata.OperationCanceledException;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.INestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.core.api.UltimateServices;
+
+
+
+public class BuchiIntersect<LETTER,STATE> extends AbstractIntersect<LETTER,STATE>
+							implements IOperation {
+
+	private static Logger s_Logger = 
+		UltimateServices.getInstance().getLogger(Activator.PLUGIN_ID);
+	
+	public BuchiIntersect(boolean buchiIntersection, boolean minimizeResult,
+					 INestedWordAutomaton<LETTER,STATE> fstNwa,
+					 INestedWordAutomaton<LETTER,STATE> sndNwa) throws OperationCanceledException {
+		super(true,minimizeResult,fstNwa, sndNwa);
+	}
+	
+
+	@Override
+	public String operationName() {
+		return "buchiIntersect";
+	}
+
+}
