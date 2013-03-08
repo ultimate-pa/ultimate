bff0f81d3389ae5f23d4c74dad3033ddb6374412
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2013-03-08 10:43:54 +0000

    ModifiableLabeledEdgesMultigraph.java -- now finished

--- trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/model/structure/ModifiableLabeledEdgesMultigraph.java
+++ trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/model/structure/ModifiableLabeledEdgesMultigraph.java
@@ -6,51 +6,141 @@ public class ModifiableLabeledEdgesMultigraph<T extends ModifiableLabeledEdgesMu
 		BaseLabeledEdgesMultigraph<T,L> {
 
 	
+	/**
+	 * Adds an outgoing node to the corresponding list and updates the hashmap with the edge label accordingly.
+	 * @param node
+	 * @param label
+	 * @return true iff the list update was succesful, only then the hashmap is
+	 *  updated, too.
+	 */
 	public boolean addOutgoingNode(T node, L label) {
-		if (node != null) {
-			mOutgoingNodes.add(node);
+		if (mOutgoingNodes.add(node)) {
 			mOutgoingEdgeLabels.put(node, label);
 			return true;
 		}
 		return false;
 	}
-
-	public void removeOutgoingNode(T node) {
-		mOutgoingNodes.remove(node);
-		mOutgoingEdgeLabels.remove(node);
+	
+	/**
+	 * Removes an outgoing node from the corresponding list and updates the hashmap with the edge label accordingly.
+	 * @param node
+	 * @param label
+	 * @return true iff the list update was succesful, only then the hashmap is
+	 *  updated, too.
+	 */
+	public boolean removeOutgoingNode(T node) {
+		if (mOutgoingNodes.remove(node)) {
+			mOutgoingEdgeLabels.remove(node);
+			return true;
+		}
+		return false;
 	}
 	
+	/**
+	 * Adds an incoming node to the corresponding list.
+	 * @param node
+	 * @param label
+	 * @return true iff the list update was succesful.
+	 */
+	public boolean addIncomingNode(T node) {
+		return mIncomingNodes.add(node);
+	}
 	
+	/**
+	 * Remove an incoming node from the corresponding list.
+	 * @param node
+	 * @param label
+	 * @return true iff the list update was succesful.
+	 */
+	public boolean removeIncomingNode(T node) {
+		return mIncomingNodes.remove(node);
+	}
+	
+	/**
+	 * Create an (non-explicit) outgoing edge to the given node with the given label.
+	 * Updates the corresponding lists in the two nodes and updates the hashmap in
+	 * "this".
+	 * @param node
+	 * @param label
+	 * @return true iff the adding operations were successful, false otherwise. In
+	 *  the latter case, changes already made are undone.
+	 */
 	public boolean connectOutgoing(T node, L label) {
 		if(mOutgoingNodes.add(node)){
-			mOutgoingEdgeLabels.put(node, label);
 			if(node.mIncomingNodes.add((T) this)){
+				mOutgoingEdgeLabels.put(node, label);
 				return true;
 			} else {
-				mOutgoingEdgeLabels.remove(node);
 				mOutgoingNodes.remove(node);
 				return false;
 			}
 		}
 		return false;
 	}
-
-	public void disconnectOutgoing(T node) {
-		mOutgoingNodes.remove(node);
-		mOutgoingEdgeLabels.remove(node);
-		node.mIncomingNodes.remove(this);
+	
+	/**
+	 * Removes an (non-explicit) outgoing edge from the given node.
+	 * Updates the corresponding lists in the two nodes and updates the hashmap in
+	 * the node "this".
+	 * @param node
+	 * @param label
+	 * @return true iff the adding operations were successful, false otherwise. In
+	 *  the latter case, changes already made are undone.
+	 */
+	public boolean disconnectOutgoing(T node) {
+		if(mOutgoingNodes.remove(node)) {
+			if(node.mIncomingNodes.remove(this)) {
+				mOutgoingEdgeLabels.remove(node);
+				return true;
+			} else {
+				mOutgoingNodes.add(node);
+				return false;
+			}
+		}
+		return false;
 	}
 	
-	public void connectIncoming(T node, L label) {
-		this.mIncomingNodes.add(node);
-		node.mOutgoingEdgeLabels.put(node, label);
-		node.mOutgoingNodes.add(this);
-	}
-
-	public void disconnectIncoming(T node) {
-		mIncomingNodes.remove(node);
-		node.mOutgoingNodes.remove(this);
-		node.mOutgoingEdgeLabels.remove(this);
+	/**
+	 * Creates an (non-explicit) incoming edge from the given node with the given label.
+	 * Updates the corresponding lists in the two nodes and updates the hashmap in
+	 * the node given as the first argument.
+	 * @param node
+	 * @param label
+	 * @return true iff the adding operations were successful, false otherwise. In
+	 *  the latter case, changes already made are undone.
+	 */
+	public boolean connectIncoming(T node, L label) {
+		if(mIncomingNodes.add(node)) {
+			if(node.mOutgoingNodes.add((T) this)) {
+				node.mOutgoingEdgeLabels.put(node, label);
+				return true;
+			} else {
+				mIncomingNodes.remove(node);
+				return false;
+			}
+		}
+		return false;
 	}
 	
+	/**
+	 * Removes an (non-explicit) incoming edge from the given node.
+	 * Updates the corresponding lists in the two nodes and updates the hashmap in
+	 * the node given as an argument.
+	 * @param node
+	 * @param label
+	 * @return true iff the adding operations were successful, false otherwise. In
+	 *  the latter case, changes already made are undone.
+	 */
+	public boolean disconnectIncoming(T node) {
+		if (mIncomingNodes.remove(node)) {
+			if (node.mOutgoingNodes.remove(this)) {
+				node.mOutgoingEdgeLabels.remove(this);
+				return true;
+			} else {
+				mIncomingNodes.add(node);
+				return false;
+			}
+		}
+		return false;
+	}
 }
