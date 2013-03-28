80dddd5ffc211f1a1cc313d64d993d575969bfe3
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-03-28 10:36:16 +0000

    examples for variable initialization

---
+++ trunk/examples/programs/minitests/recursivePrograms/GlobalVarInitialization.c
@@ -0,0 +1,16 @@
+/* #iSafe
+ * 
+ * Test that global var g has been initialized.
+ * TODO: More detailed explantation what kind of variable g is according to
+ * C99 standard.
+ * 
+ * Author: heizmann@informatik.uni-freiburg.de
+ * Date: 28.3.2013
+ * 
+ */
+
+int g;
+
+int main() {
+    //@ assert g == 0;
+}
