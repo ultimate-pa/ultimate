3d8646f6e0295ccbb03fb320471f2403cf456926
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-03-31 16:47:02 +0000

    new operation

---
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operations/NumberOfStates.java
@@ -0,0 +1,42 @@
+package de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations;
+
+import de.uni_freiburg.informatik.ultimate.automata.IOperation;
+import de.uni_freiburg.informatik.ultimate.automata.OperationCanceledException;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.INestedWordAutomaton;
+
+/**
+ * Operation that returns the number of states of a nested word automaton.
+ * @author heizmann@informatik.uni-freiburg.de
+ *
+ * @param <LETTER>
+ * @param <STATE>
+ */
+public class NumberOfStates<LETTER, STATE> implements IOperation {
+	
+	INestedWordAutomaton<LETTER, STATE> m_Nwa;
+	
+	public NumberOfStates(INestedWordAutomaton<LETTER, STATE> nwa) {
+		m_Nwa = nwa;
+	}
+
+	@Override
+	public String operationName() {
+		return "numberOfStates";
+	}
+
+	@Override
+	public String startMessage() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public String exitMessage() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object getResult() throws OperationCanceledException {
+		return m_Nwa.size();
+	}
+
+}
