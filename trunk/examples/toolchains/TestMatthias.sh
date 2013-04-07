2eb6470c7605d285b2abc48a7c1d48f6607c756c
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-04-07 13:42:21 +0000

    testscript

--- trunk/examples/toolchains/TestMatthias.sh
+++ trunk/examples/toolchains/TestMatthias.sh
@@ -5,12 +5,22 @@
 #"TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-svcomp-StrongestMinimize" \
 #"TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-svcomp-LargeStrongest"
 
- trunk/examples/toolchains/TraceAbstractionTestDir.sh 20 trunk/examples/programs/minitests \
- "TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-LargeStatements-EagerPost-Hoare" \
- "TraceAbstractionWithBlockEncoding.xml;TraceAbstractionCWithBlockEncoding.xml;TraceAbstraction-BlockEncoding-EagerPost-Hoare" \
+#trunk/examples/toolchains/TraceAbstractionTestDir.sh 20 trunk/examples/programs \
+# "TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-LargeStatements-EagerPost-Hoare" \
+# "TraceAbstractionWithBlockEncoding.xml;TraceAbstractionCWithBlockEncoding.xml;TraceAbstraction-BlockEncoding-EagerPost-Hoare" \
 
 
-# trunk/examples/toolchains/TraceAbstractionTestDir.sh 1000 trunk/examples/svcomp13/trunk/ntdrivers-simplified/ \
+#trunk/examples/toolchains/TraceAbstractionTestDir.sh 1000 trunk/examples/svcomp13/systemc \
+#"TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-svcomp-StrongestMinimize" \
 # "TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-svcomp-EagerMinimize" \
 # "TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-svcomp-LazyMinimize" \
-# "TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-svcomp-StrongestMinimize"
+
+trunk/examples/toolchains/TraceAbstractionTestDir.sh 1000 trunk/examples/svcomp13/ssh-simplified/ \
+ "TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-svcomp-LargeStrongest" \
+ "TraceAbstractionWithBlockEncoding.xml;TraceAbstractionCWithBlockEncoding.xml;TraceAbstraction-svcomp-BlockEncodingStrongest" 
+trunk/examples/toolchains/TraceAbstractionTestDir.sh 1000 trunk/examples/svcomp13/ntdrivers-simplified \
+ "TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-svcomp-LargeStrongest" \
+ "TraceAbstractionWithBlockEncoding.xml;TraceAbstractionCWithBlockEncoding.xml;TraceAbstraction-svcomp-BlockEncodingStrongest" 
+trunk/examples/toolchains/TraceAbstractionTestDir.sh 1000 trunk/examples/svcomp13/systemc \
+ "TraceAbstraction.xml;TraceAbstractionC.xml;TraceAbstraction-svcomp-LargeStrongest" \
+ "TraceAbstractionWithBlockEncoding.xml;TraceAbstractionCWithBlockEncoding.xml;TraceAbstraction-svcomp-BlockEncodingStrongest" 
