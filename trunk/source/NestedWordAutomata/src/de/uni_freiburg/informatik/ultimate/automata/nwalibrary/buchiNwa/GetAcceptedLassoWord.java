8a20167be9aa7322faf3a0d4001d6ebe29f363c6
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-04-27 16:16:21 +0000

    getWordOperations

---
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/buchiNwa/GetAcceptedLassoWord.java
@@ -0,0 +1,55 @@
+package de.uni_freiburg.informatik.ultimate.automata.nwalibrary.buchiNwa;
+
+import org.apache.log4j.Logger;
+
+import de.uni_freiburg.informatik.ultimate.automata.Activator;
+import de.uni_freiburg.informatik.ultimate.automata.IOperation;
+import de.uni_freiburg.informatik.ultimate.automata.OperationCanceledException;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.INestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.core.api.UltimateServices;
+
+public class GetAcceptedLassoWord<LETTER, STATE> implements IOperation {
+
+	private static Logger s_Logger = UltimateServices.getInstance().getLogger(
+			Activator.PLUGIN_ID);
+
+	INestedWordAutomaton<LETTER, STATE> m_Operand;
+	NestedLassoWord<LETTER> m_AcceptedWord;
+
+	public GetAcceptedLassoWord(INestedWordAutomaton<LETTER, STATE> operand) throws OperationCanceledException {
+		m_Operand = operand;
+		s_Logger.info(startMessage());
+		BuchiIsEmpty<LETTER, STATE> isEmpty = new BuchiIsEmpty<LETTER, STATE>(operand);
+		if (isEmpty.getResult()) {
+			throw new IllegalArgumentException(
+					"unable to get word from emtpy language");
+		} else {
+			m_AcceptedWord = isEmpty.getAcceptingNestedLassoRun().getNestedLassoWord();
+		}
+		s_Logger.info(exitMessage());
+	}
+
+	@Override
+	public NestedLassoWord<LETTER> getResult() throws OperationCanceledException {
+		return m_AcceptedWord;
+	}
+
+	@Override
+	public String operationName() {
+		return "getAcceptedLassoWord";
+	}
+
+	@Override
+	public String startMessage() {
+		return "Start " + operationName() + ". Operand "
+				+ m_Operand.sizeInformation();
+	}
+
+	@Override
+	public String exitMessage() {
+		return "Finished " + operationName() + ". Length of stem: "
+				+ m_AcceptedWord.getStem().length() + " Length of loop:" 
+				+ m_AcceptedWord.getLoop().length();
+	}
+
+}
---
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operations/GetAcceptedWord.java
@@ -0,0 +1,55 @@
+package de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations;
+
+import org.apache.log4j.Logger;
+
+import de.uni_freiburg.informatik.ultimate.automata.Activator;
+import de.uni_freiburg.informatik.ultimate.automata.IOperation;
+import de.uni_freiburg.informatik.ultimate.automata.OperationCanceledException;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.INestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.NestedWord;
+import de.uni_freiburg.informatik.ultimate.core.api.UltimateServices;
+
+public class GetAcceptedWord<LETTER, STATE> implements IOperation {
+
+	private static Logger s_Logger = UltimateServices.getInstance().getLogger(
+			Activator.PLUGIN_ID);
+
+	INestedWordAutomaton<LETTER, STATE> m_Operand;
+	NestedWord<LETTER> m_AcceptedWord;
+
+	public GetAcceptedWord(INestedWordAutomaton<LETTER, STATE> operand) {
+		m_Operand = operand;
+		s_Logger.info(startMessage());
+		IsEmpty<LETTER, STATE> isEmpty = new IsEmpty<LETTER, STATE>(operand);
+		if (isEmpty.getResult()) {
+			throw new IllegalArgumentException(
+					"unable to get word from emtpy language");
+		} else {
+			m_AcceptedWord = isEmpty.getNestedRun().getWord();
+		}
+		s_Logger.info(exitMessage());
+	}
+
+	@Override
+	public Object getResult() throws OperationCanceledException {
+		return m_AcceptedWord;
+	}
+
+	@Override
+	public String operationName() {
+		return "bfsEmptiness";
+	}
+
+	@Override
+	public String startMessage() {
+		return "Start " + operationName() + ". Operand "
+				+ m_Operand.sizeInformation();
+	}
+
+	@Override
+	public String exitMessage() {
+		return "Finished " + operationName() + ". Found word of length "
+				+ m_AcceptedWord.length();
+	}
+
+}
