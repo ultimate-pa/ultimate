2df0de8e52b7158cf02d8ef2c746c58a0a18a7c0
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-05-09 11:53:11 +0000

    AutomataLibraryException

---
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/AutomataLibraryException.java
@@ -0,0 +1,10 @@
+package de.uni_freiburg.informatik.ultimate.automata;
+
+public class AutomataLibraryException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1643349110083991967L;
+
+}
--- trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/OperationCanceledException.java
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/OperationCanceledException.java
@@ -1,10 +1,16 @@
 package de.uni_freiburg.informatik.ultimate.automata;
 
-public class OperationCanceledException extends Exception {
+public class OperationCanceledException extends AutomataLibraryException {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1713238821191695165L;
 
+	@Override
+	public String getMessage() {
+		return "Timeout";
+	}
+
+	
 }
