78c47195d51360b17cc043a23796b612966d5b88
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-05-30 14:25:58 +0000

    hashCode, equals, toString

--- trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operationsOldApi/IOpWithDelayedDeadEndRemoval.java
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operationsOldApi/IOpWithDelayedDeadEndRemoval.java
@@ -1,5 +1,7 @@
 package de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operationsOldApi;
 
+import java.text.MessageFormat;
+
 import de.uni_freiburg.informatik.ultimate.automata.OperationCanceledException;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.INestedWordAutomatonOldApi;
 
@@ -32,6 +34,49 @@ public interface IOpWithDelayedDeadEndRemoval<LETTER, STATE> {
 		}
 		public STATE getEntry() {
 			return m_Entry;
+		}
+		
+		@Override
+		public String toString() {
+			return MessageFormat.format("up: {0} down: {1} entry {2}", m_Up, m_Down, m_Entry);
+		}
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result
+					+ ((m_Down == null) ? 0 : m_Down.hashCode());
+			result = prime * result
+					+ ((m_Entry == null) ? 0 : m_Entry.hashCode());
+			result = prime * result + ((m_Up == null) ? 0 : m_Up.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			UpDownEntry other = (UpDownEntry) obj;
+			if (m_Down == null) {
+				if (other.m_Down != null)
+					return false;
+			} else if (!m_Down.equals(other.m_Down))
+				return false;
+			if (m_Entry == null) {
+				if (other.m_Entry != null)
+					return false;
+			} else if (!m_Entry.equals(other.m_Entry))
+				return false;
+			if (m_Up == null) {
+				if (other.m_Up != null)
+					return false;
+			} else if (!m_Up.equals(other.m_Up))
+				return false;
+			return true;
 		}
 	}
 
