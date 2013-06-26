ee78c9bbdb89899fcdb64641d5d9b3553e473741
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-06-26 23:05:05 +0000

    examples

---
+++ trunk/examples/programs/toy/2006CAV-GopanReps.c
@@ -0,0 +1,30 @@
+//#iSafe
+/*
+ * Program depicted in Figure 1 of
+ * 2006CAV - Gopan,Reps - Lookahead Widening
+ * 
+ * Program is mentioned in 
+ * 2009PLDI - Gulwani,Jain,Koskinen - Control-flow refinement and progress invariants for bound analysis
+ * as "flagship example to motivate a new technique for proving non-trivial 
+ * safety assertions"
+ * 
+ * Date: 27.6.2013
+ * Author: heizmann@informatik.uni-freiburg.de
+ *
+ */
+
+int main() {
+    int x = 0, y = 0;
+    while (1) {
+        if (x <= 50) {
+            y++;
+        } else {
+            y--;
+        }
+        if (y<0) {
+            break;
+        }
+        x++;
+    }
+    //@assert (x == 102 );
+}
---
+++ trunk/examples/programs/toy/2007POPL-GulwaniJojic.c
@@ -0,0 +1,27 @@
+//#iSafe
+/*
+ * Program depicted in Figure 3 of
+ * 2007POPL - Gulwani,Jojic - Program Verification as Probabilistic Inference
+ * 
+ * Program is cited in 
+ * 2009PLDI - Gulwani,Jain,Koskinen - Control-flow refinement and progress invariants for bound analysis
+ * as "flagship example to motivate a new technique for proving non-trivial 
+ * safety assertions"
+ * 
+ * Date: 27.6.2013
+ * Author: heizmann@informatik.uni-freiburg.de
+ *
+ */
+
+int main() {
+    int x = 0, y = 50;
+    while (x < 100) {
+        if (x < 50) {
+            x++;
+        } else {
+            x++;
+            y++;
+        }
+    }
+    //@assert (y == 100 );
+}
