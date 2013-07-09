0c306bb79840a8ec0fba0d7d39a3bc5249f2cf47
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-07-09 13:31:43 +0000

    new plugin LassoRanker

---
+++ trunk/source/LassoRanker/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/lassoranker/LassoRankerObserver.java
@@ -0,0 +1,46 @@
+package de.uni_freiburg.informatik.ultimate.plugins.analysis.lassoranker;
+import de.uni_freiburg.informatik.ultimate.access.IUnmanagedObserver;
+import de.uni_freiburg.informatik.ultimate.access.WalkerOptions;
+import de.uni_freiburg.informatik.ultimate.core.api.UltimateServices;
+import de.uni_freiburg.informatik.ultimate.model.IElement;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Auto-Generated Stub for the plug-in's Observer
+ */
+public class LassoRankerObserver implements IUnmanagedObserver {
+
+	private static Logger s_Logger = UltimateServices.getInstance().getLogger(Activator.s_PLUGIN_ID);
+	
+	@Override
+	public boolean process(IElement root) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void finish() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public WalkerOptions getWalkerOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean performedChanges() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
