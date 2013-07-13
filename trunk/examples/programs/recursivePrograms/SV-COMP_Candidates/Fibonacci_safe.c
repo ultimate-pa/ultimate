62c0fbc2bb7040100f50414f658b59c9209bc230
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-07-13 17:17:05 +0000

    example

---
+++ trunk/examples/programs/recursivePrograms/SV-COMP_Candidates/Fibonacci_safe.c
@@ -0,0 +1,35 @@
+/*
+ * Recursive computation of fibonacci numbers.
+ * 
+ * Author: Matthias Heizmann
+ * Date: 2013-07-13
+ * Licence: GPLv3 
+ * 
+ */
+
+extern int __VERIFIER_nondet_int(void);
+
+
+int fibonacci(int n) {
+    if (n < 1) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+
+int main() {
+    int x = __VERIFIER_nondet_int();
+    int result = fibonacci(x);
+    if (result >= 0) {
+        return 0;
+    } else {
+        ERROR: 
+        goto ERROR;
+    }
+}
+    
+
---
+++ trunk/examples/programs/recursivePrograms/SV-COMP_Candidates/McCarthy91_unsafe.c
@@ -0,0 +1,32 @@
+/*
+ * Implementation the McCarthy 91 function.
+ * http://en.wikipedia.org/wiki/McCarthy_91_function
+ * 
+ * Author: Matthias Heizmann
+ * Date: 2013-07-13
+ * Licence: GPLv3 
+ * 
+ */
+
+extern int __VERIFIER_nondet_int(void);
+
+
+int f91(int x) {
+    if (x > 100)
+        return x -10;
+    else {
+        return f91(f91(x+11));
+    }
+}
+
+
+int main() {
+    int x = __VERIFIER_nondet_int();
+    int result = f91(x);
+    if (result == 91 || x > 102 && result == x - 10) {
+        return 0;
+    } else {
+        ERROR: 
+        goto ERROR;
+    }
+}
