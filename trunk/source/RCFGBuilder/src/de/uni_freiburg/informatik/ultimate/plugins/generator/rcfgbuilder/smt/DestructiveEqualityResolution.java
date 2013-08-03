1e805d692d2a86b29b0832823aa02f1542419714
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-08-03 17:04:07 +0000

    quantifier elimination via UPD

--- trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/smt/DestructiveEqualityResolution.java
+++ trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/smt/DestructiveEqualityResolution.java
@@ -3,9 +3,11 @@ package de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.smt;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -16,9 +18,13 @@ import de.uni_freiburg.informatik.ultimate.logic.FormulaUnLet;
 import de.uni_freiburg.informatik.ultimate.logic.FunctionSymbol;
 import de.uni_freiburg.informatik.ultimate.logic.QuantifiedFormula;
 import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
+import de.uni_freiburg.informatik.ultimate.logic.Util;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.Activator;
+import de.uni_freiburg.informatik.ultimate.util.DebugMessage;
+import de.uni_freiburg.informatik.ultimate.util.UnionFind;
 
 
 
@@ -28,6 +34,8 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.Activat
  * Resp. we use that the term ∀v.v!=c∨φ[v] is equivalent to term φ[c].
  */
 public class DestructiveEqualityResolution {
+	
+	static boolean USE_UPD = true;
 	
 	private static Logger s_Logger = 
 			UltimateServices.getInstance().getLogger(Activator.PLUGIN_ID);
@@ -41,20 +49,179 @@ public class DestructiveEqualityResolution {
 			TermVariable[] vars, Term body,	Term[]... patterns) {
 		Set<TermVariable> occuringVars = 
 				new HashSet<TermVariable>(Arrays.asList(body.getFreeVars()));
-		List<TermVariable> remaning = new ArrayList<TermVariable>(vars.length);
+		Set<TermVariable> remaning = new HashSet<TermVariable>(vars.length);
 		for (TermVariable tv : vars) {
 			if (occuringVars.contains(tv)) {
 				remaning.add(tv);
 			}
 		}
-		Term reduced = derSimple(script, quantifier, body, remaning);
+		Term reduced;
+		reduced = derSimple(script, quantifier, body, remaning);
 		if (remaning.isEmpty()) {
 			return reduced;
-		} else {
-			return script.quantifier(quantifier, 
+		}
+		if (USE_UPD) {
+			reduced = updSimple(script, quantifier, reduced, remaning);
+			if (remaning.isEmpty()) {
+				return reduced;
+			} 
+		}
+		return script.quantifier(quantifier, 
 					remaning.toArray(new TermVariable[0]), reduced, patterns);
+	}
+	
+	/**
+	 * Try to eliminate the variables vars = {x_1,...,x_n} in term φ_1∧...∧φ_m.
+	 * Therefore we use the following approach, which we call 
+	 * Unconnected Parameter Drop.
+	 * If X is a subset of {x_1,...,x_n} and Φ is a subset {φ_1,...,φ_m} such 
+	 * that
+	 * - variables in X occur only in term of Φ, and
+	 * - terms in Φ contain only variables of X, and
+	 * - the conjunction of all term in Φ is satisfiable.
+	 * Then we can remove the conjuncts Φ and the quantified variables X from
+	 * φ_1∧...∧φ_m and obtain an equivalent formula.
+	 * 
+	 * Is only sound if there are no uninterpreted function symbols in the term
+	 * TODO: extend this to uninterpreted function symbols
+	 */
+	public static Term updSimple(Script script, int quantifier, Term term, 
+			Set<TermVariable> vars) {
+		if (quantifier == QuantifiedFormula.FORALL) {
+			// not implemented yet.
+			return term;
+		} else {
+			Set<TermVariable> occuringVars = 
+					new HashSet<TermVariable>(Arrays.asList(term.getFreeVars()));
+			Term[] conjuncts = getConjuncts(term);
+			ConnectionPartition connection = new ConnectionPartition();
+			for (Term conjunct : conjuncts) {
+				connection.addTerm(conjunct);
+			}
+			List<TermVariable> removeableTvs = new ArrayList<TermVariable>();
+			List<TermVariable> unremoveableTvs = new ArrayList<TermVariable>();
+			List<Term> removeableTerms = new ArrayList<Term>();
+			List<Term> unremoveableTerms = new ArrayList<Term>();
+			for (Set<TermVariable> connectedVars : connection.getConnectedVariables()) {
+				Set<Term> terms = connection.getTermsOfConnectedVariables(connectedVars);
+				if (isSuperfluousComponent(script, terms, connectedVars, vars)) {
+					removeableTvs.addAll(connectedVars);
+					removeableTerms.addAll(terms);
+				} else {
+					unremoveableTvs.addAll(connectedVars);
+					unremoveableTerms.addAll(terms);
+				}
+			}
+			List<Term> termsWithoutTvs = connection.getTermsWithOutTvs();
+			assert occuringVars.size() == removeableTvs.size() + unremoveableTvs.size();
+			assert conjuncts.length == removeableTerms.size() + unremoveableTerms.size() + termsWithoutTvs.size();
+			for (Term termWithoutTvs : termsWithoutTvs) {
+				LBool sat = Util.checkSat(script, termWithoutTvs);
+				if (sat == LBool.UNSAT) {
+					vars.clear();
+					return script.term("false");
+				} else if (sat == LBool.SAT) {
+					// we drop this term its equivalent to true
+				} else {
+					throw new AssertionError("expecting sat or unsat");
+				}
+			}
+			if (removeableTerms.isEmpty()) {
+				s_Logger.debug(new DebugMessage("not eliminated quantifier via UPD for {0}", occuringVars));
+				return term;
+			} else {
+				vars.removeAll(removeableTvs);
+				s_Logger.debug(new DebugMessage("eliminated quantifier via UPD for {0}", removeableTvs));
+				return Util.and(script, unremoveableTerms.toArray(new Term[0]));
+			}
 		}
 	}
+	
+	/**
+	 * Return true if connectedVars is a subset of quantifiedVars and the
+	 * conjunction of terms is satisfiable.
+	 */
+	public static boolean isSuperfluousComponent(Script script, Set<Term> terms,  
+			Set<TermVariable> connectedVars, Set<TermVariable> quantifiedVars) {
+		if (quantifiedVars.containsAll(connectedVars)) {
+			Term conjunction = Util.and(script, terms.toArray(new Term[0]));
+			if (Util.checkSat(script, conjunction) == LBool.SAT) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * If term is a conjunction return all conjuncts, otherwise return term.
+	 */
+	public static Term[] getConjuncts(Term term) {
+		if (term instanceof ApplicationTerm) {
+			ApplicationTerm appTerm = (ApplicationTerm) term;
+			if (appTerm.getFunction().getName().equals("and")) {
+				return appTerm.getParameters();
+			}
+		}
+		Term[] result = { term };
+		return result;
+	}
+	
+	/**
+	 * Partition set of terms into equivalence classes. 
+	 * We call two terms connected if both share a common free variable.
+	 * we define two terms to be equivalent (resp. this partition) if they
+	 * are in the transitive closure of the connection relation.
+	 */
+	public static class ConnectionPartition {
+		Map<TermVariable, Set<Term>> m_Tv2Terms = new HashMap<TermVariable, Set<Term>>();
+		UnionFind<TermVariable> unionFind = new UnionFind<TermVariable>();
+		List<Term> m_TermWithoutTvs = new ArrayList<Term>();
+		
+		void addTerm(Term term) {
+			TermVariable[] tvs = term.getFreeVars();
+			if (tvs.length == 0) {
+				m_TermWithoutTvs.add(term);
+				return;
+			}
+			TermVariable firstTv = tvs[0];
+			add(term, firstTv);
+			if (unionFind.find(firstTv) == null) {
+				unionFind.makeEquivalenceClass(firstTv);
+			}
+			for (int i=1; i < tvs.length; i++) {
+				add(term, tvs[i]);
+				unionFind.add(tvs[i], firstTv);
+			}
+		}
+		
+		private void add(Term term, TermVariable tv) {
+			Set<Term> terms = m_Tv2Terms.get(tv);
+			if (terms == null) {
+				terms = new HashSet<Term>();
+				m_Tv2Terms.put(tv, terms);
+			}
+			terms.add(term);
+		}
+		
+		Set<Set<TermVariable>> getConnectedVariables() {
+			return unionFind.getEquivalenceClass();
+		}
+		
+		Set<Term> getTermsOfConnectedVariables(Set<TermVariable> connectedVars) {
+			Set<Term> result = new HashSet<Term>();
+			for (TermVariable tv : connectedVars) {
+				result.addAll(m_Tv2Terms.get(tv));
+			}
+			return result;
+		}
+		
+		List<Term> getTermsWithOutTvs() {
+			return m_TermWithoutTvs;
+		}
+		
+	}
+	
+	
 	
 	/**
 	 * Try to eliminate the variables vars in term.
@@ -78,7 +245,7 @@ public class DestructiveEqualityResolution {
 				throw new AssertionError("unknown quantifier");
 			}
 			if (replacementTerm != null) {
-				s_Logger.debug("eliminated quantifed variable " + tv);
+				s_Logger.debug(new DebugMessage("eliminated quantifier via DER for {0}", tv));
 				it.remove();
 				TermVariable[] varsAux = { tv };
 				Term[] valuesAux = { replacementTerm };
@@ -86,7 +253,7 @@ public class DestructiveEqualityResolution {
 				resFormula = new FormulaUnLet().unlet(resFormula);
 			}
 			else {
-				s_Logger.debug("unable to eliminated quantifed variable " + tv);
+				s_Logger.debug(new DebugMessage("not eliminated quantifier via DER for {0}", tv));
 			}
 		}
 		return resFormula;
