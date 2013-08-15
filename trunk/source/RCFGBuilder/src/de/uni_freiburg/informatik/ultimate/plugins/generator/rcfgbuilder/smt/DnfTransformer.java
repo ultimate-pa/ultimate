98bcd1af85450c1471bc5f88d3972add42c6306d
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-08-15 15:27:04 +0000

    dnf

--- trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/smt/DnfTransformer.java
+++ trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/smt/DnfTransformer.java
@@ -1,6 +1,7 @@
 package de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.smt;
 
 import de.uni_freiburg.informatik.ultimate.logic.ApplicationTerm;
+import de.uni_freiburg.informatik.ultimate.logic.QuantifiedFormula;
 import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermTransformer;
@@ -26,31 +27,102 @@ public class DnfTransformer extends TermTransformer {
 			String functionName = appTerm.getFunction().getName();
 			if (functionName.equals("and")) {
 				super.convert(term);
+				return;
 			} else if (functionName.equals("or")) {
 				super.convert(term);
+				return;
 			} else if (functionName.equals("not")) {
-				assert false;
+				assert appTerm.getParameters().length == 1;
+				Term notParam = appTerm.getParameters()[0];
+				convertNot(notParam, term);
+				return;
 			} else if (functionName.equals("=>")) {
 				Term[] params = appTerm.getParameters();
-				Term[] newParams = new Term[params.length];
-				newParams[0] = Util.not(m_Script, params[0]); 
-				for (int i=0; i<params.length; i++) {
-					newParams[i] = params[i];
-				}
-				super.convert(Util.or(m_Script, newParams));
+				super.convert(Util.or(m_Script, negateLast(params)));
+				return;
 			} else {
-				
+				//consider term as atom
+				setResult(term);
+				return;
 			}
-			
+		} else if (term instanceof QuantifiedFormula) {
+			throw new UnsupportedOperationException("quantifer not supported");
+		} else {
+			//consider term as atom
+			setResult(term);
+			return;
 		}
-		// TODO Auto-generated method stub
-		super.convert(term);
+	}
+	
+	private void convertNot(Term notParam, Term originalTerm) {
+		assert notParam.getSort().getName().equals("Bool");
+		if (notParam instanceof ApplicationTerm) {
+			ApplicationTerm appTerm = (ApplicationTerm) notParam; 
+			String functionName = appTerm.getFunction().getName();
+			Term[] params = appTerm.getParameters();
+			if (functionName.equals("and")) {
+				super.convert(Util.or(m_Script, negateTerms(params)));
+				return;
+			} else if (functionName.equals("or")) {
+				super.convert(Util.and(m_Script, negateTerms(params)));
+				return;
+			} else if (functionName.equals("not")) {
+				assert appTerm.getParameters().length == 1;
+				Term notnotParam = appTerm.getParameters()[0];
+				super.convert(notnotParam);
+				return;
+			} else if (functionName.equals("=>")) {
+				super.convert(Util.and(m_Script, negateAllButLast(params)));
+				return;
+			} else {
+				//consider original term as atom
+				setResult(originalTerm);
+				return;
+			}
+		} else if (notParam instanceof QuantifiedFormula) {
+			throw new UnsupportedOperationException("quantifer not supported");
+		} else {
+			//consider original term as atom
+			setResult(originalTerm);
+			return;
+		}
+	}
+	
+	private Term[] negateTerms(Term[] terms) {
+		Term[] newTerms = new Term[terms.length];
+		for (int i=0; i<terms.length; i++) {
+			newTerms[i] = Util.not(m_Script, terms[i]);
+		}
+		return newTerms;
+	}
+	
+	private Term[] negateLast(Term[] terms) {
+		Term[] newTerms = new Term[terms.length];
+		for (int i=0; i<terms.length-1; i++) {
+			newTerms[i] = terms[i];
+		}
+		newTerms[terms.length-1] = Util.not(m_Script, terms[terms.length-1]);
+		return newTerms;
+	}
+	
+	private Term[] negateAllButLast(Term[] terms) {
+		Term[] newTerms = new Term[terms.length];
+		for (int i=0; i<terms.length-1; i++) {
+			newTerms[i] = Util.not(m_Script, terms[i]);
+		}
+		newTerms[terms.length-1] = terms[terms.length-1];
+		return newTerms;
 	}
 
 	@Override
 	public void convertApplicationTerm(ApplicationTerm appTerm, Term[] newArgs) {
-		// TODO Auto-generated method stub
-		super.convertApplicationTerm(appTerm, newArgs);
+		String functionSymbolName = appTerm.getFunction().getName();
+		if (functionSymbolName.equals("and")) {
+			//TODO:
+			
+		} else {
+			super.convertApplicationTerm(appTerm, newArgs);
+		}
 	}
 
 }
