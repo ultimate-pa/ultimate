83340cdf6fee2aa13979a0f7e1c0fac96ca5c0d5
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-08-19 11:16:05 +0000

    safe substitution

---
+++ trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/smt/SafeSubstitution.java
@@ -0,0 +1,98 @@
+package de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.smt;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import de.uni_freiburg.informatik.ultimate.logic.LetTerm;
+import de.uni_freiburg.informatik.ultimate.logic.QuantifiedFormula;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.TermTransformer;
+import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
+import de.uni_freiburg.informatik.ultimate.util.ScopedHashMap;
+import de.uni_freiburg.informatik.ultimate.util.ScopedHashSet;
+
+/**
+ * Substitution that replaces subterms by terms. Takes care the quantified
+ * variables are renamed to fresh variables such that 
+ * - no variable in substituted term is "captured" by an existing quantifier
+ * - no subterm that contains a bound variable is substituted.
+ * @author Matthias Heizmann
+ */
+public class SafeSubstitution extends TermTransformer {
+	
+	private final Script m_Script;
+	private final ScopedHashMap<Term,Term> m_ScopedSubstitutionMapping;
+	private final ScopedHashSet<Term> m_VarsInSubstitutionMapping;
+	
+	public SafeSubstitution(Script script,
+			Map<Term, Term> substitutionMapping) {
+		super();
+		m_Script = script;
+		m_ScopedSubstitutionMapping = new ScopedHashMap<Term, Term>();
+		m_ScopedSubstitutionMapping.putAll(substitutionMapping);
+		m_VarsInSubstitutionMapping = new ScopedHashSet<Term>();
+		for (Entry<Term, Term> entry  : m_ScopedSubstitutionMapping.entrySet()) {
+			m_VarsInSubstitutionMapping.addAll(Arrays.asList(entry.getKey().getFreeVars()));
+			m_VarsInSubstitutionMapping.addAll(Arrays.asList(entry.getValue().getFreeVars()));
+		}
+	}
+
+	@Override
+	protected void convert(Term term) {
+		if (m_ScopedSubstitutionMapping.containsKey(term)) {
+			setResult(m_ScopedSubstitutionMapping.get(term));
+		} else {
+			if (term instanceof QuantifiedFormula) {
+				m_ScopedSubstitutionMapping.beginScope();
+				m_VarsInSubstitutionMapping.beginScope();
+				QuantifiedFormula qFormula = (QuantifiedFormula) term;
+				for (TermVariable var : qFormula.getVariables()) {
+					if (m_VarsInSubstitutionMapping.contains(var)) {
+						TermVariable freshVariable = var.getTheory().
+								createFreshTermVariable("subst", var.getSort());
+						//throw new UnsupportedOperationException();
+						m_ScopedSubstitutionMapping.put(var, freshVariable);
+						m_VarsInSubstitutionMapping.add(var);
+						m_VarsInSubstitutionMapping.add(freshVariable);
+					}
+				}
+				
+			} else if (term instanceof LetTerm) {
+				throw new UnsupportedOperationException("LetTerm not supported");
+			}
+			super.convert(term);
+		}
+	}
+	
+
+	@Override
+	public void postConvertQuantifier(QuantifiedFormula old, Term newBody) {
+		Term result;
+		TermVariable[] newQuantifiedVars = 
+									new TermVariable[old.getVariables().length];
+		boolean quantifiedVariablesChanged = false;
+		for (int i=0; i<old.getVariables().length; i++) {
+			if (m_ScopedSubstitutionMapping.containsKey(old.getVariables()[i])) {
+				newQuantifiedVars[i] = old.getVariables()[i];
+				quantifiedVariablesChanged = true;
+			} else {
+				newQuantifiedVars[i] = old.getVariables()[i];
+			}
+		}
+		if (old.getSubformula() == newBody) {
+			assert !quantifiedVariablesChanged;
+			result = old;
+		} else {
+			if (!quantifiedVariablesChanged) {
+				newQuantifiedVars = old.getVariables();
+			}
+			result = m_Script.quantifier(old.getQuantifier(), newQuantifiedVars, newBody);
+		}
+		setResult(result);
+	}
+
+
+
+}
