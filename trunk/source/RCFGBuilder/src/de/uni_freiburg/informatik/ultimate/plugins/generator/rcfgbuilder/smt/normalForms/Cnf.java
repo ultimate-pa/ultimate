9980270edbcf1092bc74f539b5e813144eb135c3
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-08-26 11:48:23 +0000

    cnf

---
+++ trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/smt/normalForms/Cnf.java
@@ -0,0 +1,45 @@
+package de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.smt.normalForms;
+
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.Util;
+
+/**
+ * Transform Boolean Term into conjunctive normal form.
+ * @author heizmann@informatik.uni-freiburg.de
+ */
+
+public class Cnf extends Xnf {
+	
+	public Cnf(Script script) {
+		super(script);
+	}
+	
+	@Override
+	public String innerConnectiveSymbol() {
+		return "or";
+	}
+
+	@Override
+	public String outerConnectiveSymbol() {
+		return "and";
+	}
+
+	@Override
+	public String innerConnectiveNeutralElement() {
+		return "false";
+	}
+
+	@Override
+	public Term innerConnective(Script script, Term... params) {
+		Term result = Util.or(m_Script, params);
+		return result;
+	}
+
+	@Override
+	public Term outerConnective(Script script, Term... params) {
+		Term result = Util.and(m_Script, params);
+		return result;
+	}
+
+}
