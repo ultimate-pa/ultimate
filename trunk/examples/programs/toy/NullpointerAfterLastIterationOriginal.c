770483042ef680931c8e713c5d1dc2adc9fb8509
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-08-29 09:14:54 +0000

    example

---
+++ trunk/examples/programs/toy/NullpointerAfterLastIterationOriginal.c
@@ -0,0 +1,28 @@
+//#mSafe
+/*
+ * Pointer free abstraction of example that Andreas discussed during his stay
+ * at NICTA in Australia.
+ * 
+ * Original version of the program as it occurs in
+ * Ansgar Fehnker, Ralf Huuck
+ * Model Checking Driven Static Analysis for the Real World
+ * Journal of Innovations in Systems and Software Engineering Springer-Verlag, doi:10.1007/s11334-012-0192-5, pages 1-12, August 2012. 
+ * 
+ * Date: August 2012
+ * Author: heizmann@informtik.uni-freiburg.de
+ * 
+ */
+
+int main() {
+    int x, *a;
+    int *p = malloc(sizeof(int));
+    x = 5;
+    while ( x > 0 ) {
+        a = p;
+        if (x == 1) {
+            free(p);
+        }
+        x--;
+    }
+    return 0;
+}
