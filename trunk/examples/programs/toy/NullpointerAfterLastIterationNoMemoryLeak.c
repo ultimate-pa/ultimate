3d2b16c8079c009281745e87ec1cc31924081d20
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-09-07 01:22:19 +0000

    example

---
+++ trunk/examples/programs/toy/NullpointerAfterLastIterationNoMemoryLeak.c
@@ -0,0 +1,26 @@
+//#mSafe
+/*
+ * Variant of Goanna example where x is not initialized, but there is no
+ * memory leak.
+ * 
+ * Date: September 2013
+ * Author: heizmann@informtik.uni-freiburg.de
+ * 
+ */
+
+int main() {
+    int x, *a;
+    int *p = malloc(sizeof(int));
+    if (x > 0) {
+    	while ( x > 0 ) {
+    		a = p;
+    		if (x == 1) {
+    			free(p);
+    		}
+    		x--;
+    	}
+    } else {
+    	free(p);
+    }
+    return 0;
+}
