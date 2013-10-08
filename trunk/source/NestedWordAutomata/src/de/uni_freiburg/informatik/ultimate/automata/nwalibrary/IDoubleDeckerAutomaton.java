a687ad0d9e91662ecbdeabc537c41249e8a9d0b8
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-10-08 02:07:14 +0000

    get Down States in Interface

--- trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/IDoubleDeckerAutomaton.java
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/IDoubleDeckerAutomaton.java
@@ -1,6 +1,10 @@
 package de.uni_freiburg.informatik.ultimate.automata.nwalibrary;
 
+import java.util.Set;
+
 public interface IDoubleDeckerAutomaton<LETTER, STATE> extends INestedWordAutomatonSimple<LETTER,STATE> {
 
 	public boolean isDoubleDecker(STATE up, STATE down);
+	
+	public Set<STATE> getDownStates(STATE up);
 }
