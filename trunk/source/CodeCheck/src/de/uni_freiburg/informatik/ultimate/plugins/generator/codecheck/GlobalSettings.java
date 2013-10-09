ceb0c1a6a3d020c7e0d6ad9da41eb32f88396548
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2013-10-09 20:59:43 +0000

    fta (forgot to add)

--- trunk/source/CodeCheck/src/de/uni_freiburg/informatik/ultimate/plugins/generator/codecheck/GlobalSettings.java
+++ trunk/source/CodeCheck/src/de/uni_freiburg/informatik/ultimate/plugins/generator/codecheck/GlobalSettings.java
@@ -2,25 +2,30 @@ package de.uni_freiburg.informatik.ultimate.plugins.generator.codecheck;
 
 import de.uni_freiburg.informatik.ultimate.plugins.generator.codecheck.preferences.PreferenceValues.EdgeCheckOptimization;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.codecheck.preferences.PreferenceValues.PredicateUnification;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.codecheck.preferences.PreferenceValues.SolverAndInterpolator;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.preferences.PreferenceValues.INTERPOLATION;
 
 public class GlobalSettings {
-
-	public static de.uni_freiburg.informatik.ultimate.plugins.generator.codecheck.preferences.PreferenceValues.SolverAndInterpolator _solverAndInterpolator = null;
-	public static GlobalSettings _instance;
-	public boolean _memoizeNormalEdgeChecks;
-	public PredicateUnification _predicateUnification;
-	public boolean _memoizeReturnEdgeChecks;
-	public EdgeCheckOptimization _edgeCheckOptimization;
-	public boolean _checkOnlyMain;
-	public INTERPOLATION _interpolationMode;
-	public String _dotGraphPath;
-	public Checker checker;
 	
-	public enum SolverAndInterpolator { SMTINTERPOL, Z3SPWP };
+	static GlobalSettings _instance;
+	
+		//default configuration
+//	String m_dotGraphPath = "C:/temp/codeCheckGraphs";
+	String _dotGraphPath = "";
+	SolverAndInterpolator _solverAndInterpolator = SolverAndInterpolator.SMTINTERPOL;
+	INTERPOLATION _interpolationMode = INTERPOLATION.Craig_TreeInterpolation;
+	PredicateUnification _predicateUnification = PredicateUnification.PER_VERIFICATION;
+	EdgeCheckOptimization _edgeCheckOptimization = EdgeCheckOptimization.NONE;
+	Checker checker = Checker.ULTIMATE;
+	boolean _checkOnlyMain = false;
+	boolean _memoizeNormalEdgeChecks = true;
+	boolean _memoizeReturnEdgeChecks = true;
+
 	public static void init() {
-		// TODO Auto-generated method stub
-		
+		_instance = new GlobalSettings();
+	}
+
+	public GlobalSettings() {
 	}
 
 }
