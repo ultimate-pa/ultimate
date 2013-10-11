1717cd8cd7b595d1497e9d439d1c899556505dde
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-10-11 14:42:09 +0000

    new kind of predicate

---
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/predicates/BasicPredicateExplicitQuantifier.java
@@ -0,0 +1,32 @@
+package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates;
+
+import java.util.Set;
+
+import de.uni_freiburg.informatik.ultimate.logic.QuantifiedFormula;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
+import de.uni_freiburg.informatik.ultimate.model.boogie.BoogieVar;
+
+public class BasicPredicateExplicitQuantifier extends BasicPredicate {
+	
+	private final int m_Quantifier;
+	private final Set<TermVariable> m_QuantifiedVariables;
+
+	protected BasicPredicateExplicitQuantifier(int serialNumber,
+			String[] procedures, Term term, Set<BoogieVar> vars,
+			Term closedFormula, int quantifier, Set<TermVariable> quantifiedVariables) {
+		super(serialNumber, procedures, term, vars, closedFormula);
+		assert quantifier == QuantifiedFormula.EXISTS || quantifier == QuantifiedFormula.FORALL;
+		m_Quantifier = quantifier;
+		m_QuantifiedVariables = quantifiedVariables;
+	}
+
+	public int getQuantifier() {
+		return m_Quantifier;
+	}
+
+	public Set<TermVariable> getQuantifiedVariables() {
+		return m_QuantifiedVariables;
+	}
+	
+}
