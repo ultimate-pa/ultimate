c13f175610f01ee923b99dce7bf2a8ad652c6315
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-10-13 13:42:03 +0000

    example

---
+++ trunk/examples/programs/minitests/quantifier/GlobalPointerInitialization.c
@@ -0,0 +1,14 @@
+//#iSafe
+/*
+ * Global variable (without further modifiers - TODO check details in C99 
+ * standard) is initialized with 0.
+ * Date: October 2013
+ * Author: heizmann@informtik.uni-freiburg.de
+ * 
+ */
+
+int *p;
+
+int main() {
+	// insert some check that tests that p is indeed 0
+}
