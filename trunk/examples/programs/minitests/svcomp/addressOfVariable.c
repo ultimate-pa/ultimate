e8879174ae379ecb08b7cd3eede184dc47dd4db2
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2013-10-13 00:18:41 +0000

    corrected/refined example

--- trunk/examples/programs/minitests/svcomp/addressOfVariable.c
+++ trunk/examples/programs/minitests/svcomp/addressOfVariable.c
@@ -2,18 +2,21 @@
 // program has to be stored on the heap by the translation
 // (the translation result should be the same as for the statements in the 
 // comments)
+// note that this does intentionally not include the possibility of writing
+// addresses to ints -- we consider that an implicit cast and (may) treat
+// it elsewhere
+//author: nutz
 
 int main() {
 	int v; 		// int* v;
 	int* addrV;	// int* addrV;
 	int* addr2;	// int* addr2;
-	int w;		// int* w;
+	int w;		// int w;
 	int x;
 
-	addrV = &v; 	// *addrV =  &*v;   --> write ptr -> addrV 
-	addr2 = addrV;	// *addr2 = *addrV;  --> write int -> addrV
-	w = v;		// *w = *v
-	x = 5;
+	addrV = &v; 	// addrV =  &*v;
+	addr2 = addrV;	// addr2 = addrV;
+	w = v;		// w = *v
 	
 	return 0;
 }
