1dfcc44fd2709cb82a9e3b7e8d56e8fe832fd37d
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-10-13 01:35:56 +0000

    example

---
+++ trunk/examples/programs/minitests/quantifier/AssignmentOfNullPointer.c
@@ -0,0 +1,15 @@
+//#iSafe
+/*
+ * Date: October 2013
+ * Author: heizmann@informtik.uni-freiburg.de
+ * 
+ */
+
+int nonMain() {
+    int* a; // some auxiliary statement to obtain memory model
+    int b = *a; // some auxiliary statement to obtain memory model
+	
+    int *p = 0;
+	
+    return 0;
+}
