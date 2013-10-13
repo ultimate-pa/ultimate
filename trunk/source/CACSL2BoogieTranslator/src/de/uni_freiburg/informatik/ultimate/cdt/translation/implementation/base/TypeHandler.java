b8d4dd9b2ac1bb7034f3e36e55d4dbff8a6de401
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-10-13 21:24:33 +0000

    bugfix: typedef applied to pointer type

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/TypeHandler.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/TypeHandler.java
@@ -187,7 +187,9 @@ public class TypeHandler implements ITypeHandler {
             for (IASTDeclarator cDecl : ((IASTSimpleDeclaration) node
                     .getParent()).getDeclarators()) {
                 String typedefId = cDecl.getName().getRawSignature();
-                typedef.put(typedefId, result);
+                ResultTypes checkedResult = main.cHandler.checkForPointer(main, cDecl.getPointerOperators(), result);
+                typedef.put(typedefId, checkedResult);
+                
             }
             // TODO : add an axiom for sizeOf(typedefId) == sizeOf(result.t);?
             // I think, the axiom is not required, as we resolve the type
