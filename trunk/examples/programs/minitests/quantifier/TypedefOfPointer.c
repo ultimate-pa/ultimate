69819e028be6be992f09fc044667f77918a7bbc6
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-10-14 01:41:11 +0000

    example

---
+++ trunk/examples/programs/minitests/quantifier/TypedefOfPointer.c
@@ -0,0 +1,16 @@
+//#iSafe
+/*
+ * Date: October 2013
+ * Author: heizmann@informtik.uni-freiburg.de
+ * 
+ */
+
+//int* intPointer;
+typedef int* intPointer;
+
+int main() {
+    int *p = malloc(sizeof(int));
+	int *q;
+	q = p;
+    return 0;
+}
