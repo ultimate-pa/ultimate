3a4045d63f683451501106cf939248021ad9c876
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-10-15 16:39:01 +0000

    example

--- trunk/examples/programs/minitests/quantifier/AssignConstantToPointer.c
+++ trunk/examples/programs/minitests/quantifier/AssignConstantToPointer.c
@@ -5,7 +5,13 @@
  * 
  */
 
-int nonMain() {
+typedef struct fraction {
+	int num;
+	int denom;
+} *fractionPtr;
+
+
+int main() {
     int* a; // some auxiliary statement to obtain memory model
     int b = *a; // some auxiliary statement to obtain memory model
 	
@@ -17,6 +23,19 @@ int nonMain() {
     q = 0;
     
     //@ assert p == q;
+    
+    // Case 3:
+    fractionPtr r = 0;
+    
+    // Case 4:
+    fractionPtr s;
+    s = 0;
+    
+    //@ assert r == s;
+    
+//    if (p != 0) {
+        // assert \false;
+//    }
 
     return 0;
 }
