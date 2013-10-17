becf2c1c891b4ba72ce081f630d81d88b40e70fe
Christian Schilling <schillic@informatik.uni-freiburg.de>
2013-10-17 08:21:28 +0000

    example

---
+++ trunk/examples/programs/minitests/ArrayDeclaration.c
@@ -0,0 +1,14 @@
+/*
+ * Date: October 2013
+ * Author: Christian Schilling
+ * 
+ * Mixed declarations of arrays and base types.
+ */
+
+int main() {
+    int i, arr1[5];
+    int arr2[6], j;
+    int arr3[7], arr4[8];
+    
+    return 0;
+}
