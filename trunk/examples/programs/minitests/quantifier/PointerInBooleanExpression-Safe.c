6a7f1e686e57eda5c5371969461c39a5373494d3
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-10-30 21:01:26 +0000

    example

--- trunk/examples/programs/minitests/quantifier/PointerInBooleanExpression-Safe.c
+++ trunk/examples/programs/minitests/quantifier/PointerInBooleanExpression-Safe.c
@@ -10,6 +10,13 @@ int main() {
 	int a;
 	a = !p;
 	//@ assert a == 0;
+	int *q = 0;
+//	q = 0;
+	int b = !q;
+	//@ assert b != 0;
+	if (!p || q) {
+	  //@ assert \false;
+	}
     free(p);
     return 0;
 }
