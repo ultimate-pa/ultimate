e612cac930e5464cc5f83614b7c513c18afe412b
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-11-01 11:25:09 +0000

    new class ModelUtils

---
+++ trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/model/ModelUtils.java
@@ -0,0 +1,15 @@
+package de.uni_freiburg.informatik.ultimate.model;
+
+/**
+ * Helper methods for Ultimate models.
+ */
+public final class ModelUtils {
+	
+	public static void mergeAnnotations(IElement oldE, IElement newE) {
+		
+	}
+	
+	
+	
+
+}
