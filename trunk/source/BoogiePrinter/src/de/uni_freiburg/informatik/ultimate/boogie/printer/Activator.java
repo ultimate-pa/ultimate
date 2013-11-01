05af42bd28c49d6d6b08f09609319bb8bd700c6b
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2013-11-01 20:02:58 +0000

    - success: UltimateCLI looses GUI dependency (and 50% of binary size)

--- trunk/source/BoogiePrinter/src/de/uni_freiburg/informatik/ultimate/boogie/printer/Activator.java
+++ trunk/source/BoogiePrinter/src/de/uni_freiburg/informatik/ultimate/boogie/printer/Activator.java
@@ -4,7 +4,6 @@
 package de.uni_freiburg.informatik.ultimate.boogie.printer;
 
 import org.eclipse.core.runtime.Plugin;
-import org.osgi.framework.BundleContext;
 
 /**
  * @author hoenicke
@@ -14,51 +13,11 @@ public class Activator extends Plugin {
 	 * The plug-in ID
 	 */
 	public static final String s_PLUGIN_ID = "BoogiePrinter";
-	/**
-	 * The shared instance
-	 */
-	private static Activator plugin;
 
 	/**
 	 * The constructor
 	 */
 	public Activator() {
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-	 * )
-	 */
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-	 * )
-	 */
-	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 * 
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
 
 }
