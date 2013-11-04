4743fcae762ee903affb245aab70c55aa8de14b6
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-11-04 21:39:43 +0000

    example

---
+++ trunk/examples/programs/minitests/NestedDeclarators.c
@@ -0,0 +1,21 @@
+//#iSafe
+// Author: heizmann@informatik.uni-freiburg.de
+// Date: November 2013
+
+int g;
+
+int (((increase)))(int a);
+
+
+int main() {
+    int ((x)) = g;
+    g = increase(x);
+    //@ assert x == g-1;
+}
+
+
+int increase(int a) {
+    int (b) = a;
+    b++;
+    return b;
+}
