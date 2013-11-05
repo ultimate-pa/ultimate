82a5cc7fc8e3bb3610d7748e94dc07c94dea5338
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2013-11-05 22:51:25 +0000

    update of python script

--- trunk/svcomp2014/alex/createZip.sh
+++ trunk/svcomp2014/alex/createZip.sh
@@ -3,6 +3,7 @@ mkdir kojak
 cp -a ../../source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* kojak/
 cp ../LICENSE* kojak/
 cp ./ToolchainKojakC.xml kojak/
-cp ./SettingsKojakSvComp kojak/
+cp ../../examples/settings/AlexSVCOMPstandard kojak/
+cp ../../examples/settings/AlexSVCOMPmemsafety kojak/
 cp KojakSvComp.py kojak/
 zip UltimateKojakCommandline.zip -r kojak/*
