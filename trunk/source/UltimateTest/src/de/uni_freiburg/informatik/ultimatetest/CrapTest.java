b159b13d23d3d9db427a44a357279a707932fa07
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2013-11-13 14:17:53 +0000

    - added really unnecessary classes to test maven build

---
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/CrapTest.java
@@ -0,0 +1,21 @@
+package de.uni_freiburg.informatik.ultimatetest;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class CrapTest {
+
+	@Test
+	public void permaWin() {
+		System.out.println("Success");
+	}
+
+	@Test
+	public void failBot() {
+
+		fail("HAHAH");
+
+	}
+
+}
