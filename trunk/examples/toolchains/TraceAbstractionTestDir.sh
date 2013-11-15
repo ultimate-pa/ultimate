252f23d3c71d5482edc89989336d157df0b4e8ab
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-11-15 17:57:03 +0000

    testscript

--- trunk/examples/toolchains/TraceAbstractionTestDir.sh
+++ trunk/examples/toolchains/TraceAbstractionTestDir.sh
@@ -213,7 +213,7 @@ do
 	if [ "$VCCPRELUDE" ]; then 
 	    Ultimate_OUTPUT=`bash -c "ulimit -t $timeout; $Ultimate_PATH/$UltimateEXE --console "$TOOLCHAIN" "$f" --prelude "$Ultimate_PATH/trunk/examples/programs/translated-vcc/Vcc2Prelude.bpl" --settings "$SETTINGS" 2>&1"`    
 	else
-	    # echo $Ultimate_PATH/$UltimateEXE --console "$TOOLCHAIN" "$examplesFolder/$f" --settings "$SETTINGS"
+	    echo $Ultimate_PATH/$UltimateEXE --console "$TOOLCHAIN" "$examplesFolder/$f" --settings "$SETTINGS"
 	    Ultimate_OUTPUT=`bash -c "ulimit -t $timeout; $Ultimate_PATH/$UltimateEXE --console "$TOOLCHAIN" "$f" --settings "$SETTINGS" 2>&1"`
 	fi
 
@@ -231,8 +231,8 @@ do
 	RESULT_UNKNOWN=`echo "$Ultimate_OUTPUT" | grep "Program might be incorrect, check conterexample."`
 	RESULT_INSUFFICIENT_ITERATIONS=`echo "$Ultimate_OUTPUT" | grep -c "RESULT: Ultimate could not prove your program: Insufficient iterations to proof correctness"`
 	RESULT_NORESULT=`echo "$Ultimate_OUTPUT" | grep -c "RESULT: Ultimate could not prove your program: Toolchain returned no Result."`
-	RESULT_PROVEN_TERMINATION=`echo "$Ultimate_OUTPUT" | grep "Program is terminating "`
-	RESULT_NOTPROVEN_TERMINATION=`echo "$Ultimate_OUTPUT" | grep "Program might not terminate."`
+	RESULT_PROVEN_TERMINATION=`echo "$Ultimate_OUTPUT" | grep "Buchi Automizer proved that your program is terminating"`
+	RESULT_NOTPROVEN_TERMINATION=`echo "$Ultimate_OUTPUT" | grep "Buchi Automizer was unable to prove termination"`
 	BUG_24=`echo "$Ultimate_OUTPUT" | grep "at de.uni_freiburg.informatik.ultimate.smtinterpol.convert.ConvertFormula.addClause(ConvertFormula.java:349)"`
 	BUG_14=`echo "$Ultimate_OUTPUT" | grep "at de.uni_freiburg.informatik.ultimate.smtinterpol.convert.ConvertFormula.convertFormula(ConvertFormula.java:"`
 	BUG_22=`echo "$Ultimate_OUTPUT" | grep "java.lang.AssertionError: Z3 says unsat, SmtInterpol says sat!"`
@@ -365,6 +365,7 @@ do
 
 	echo "$Ultimate_OUTPUT" | grep "Statistics:" | cut -c67-
 	RUNTIME=`echo "$Ultimate_OUTPUT" | grep "TraceAbstraction took" | cut -c73-`
+    TOTALRUNTIME=`echo "$Ultimate_OUTPUT" | grep "Finished toolchain execution after " | cut -c91-`
 
 	if [ "$RESULT_SYNTAX" ]; then
 	    if [ "$KEYWORD_SYNTAX" ]; then
@@ -394,7 +395,7 @@ do
 
 	if [ "$RESULT_TIMEOUT" ]; then
 	    printf "!Warning! "
-	    printf "TraceAbstraction terminated after $RUNTIME and says: Timeout.\n"
+	    printf "Ultimate terminated after $TOTALRUNTIME and says: Timeout.\n"
 	    continue;
 	fi
 
@@ -463,12 +464,12 @@ do
 	fi
 	
 	if [ "$RESULT_PROVEN_TERMINATION" ]; then
-        printf "Termination proven"
+        printf "Buchi Automizer terminated after $TOTALRUNTIME and says: Termination proven \n"
         printf "$RESULT_PROVEN_TERMINATION"
     fi
     
     if [ "$RESULT_NOTPROVEN_TERMINATION" ]; then
-        printf "Unable to prove termination"
+        printf "Buchi Automizer terminated after $TOTALRUNTIME and says: Unable to prove termination \n"
         printf "$RESULT_PROVEN_TERMINATION"
     fi
 
