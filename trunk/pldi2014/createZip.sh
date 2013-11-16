cb76cab9cfce71004c206659586eb0b20694c78b
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-11-16 03:38:57 +0000

    update timeout

--- trunk/pldi2014/createZip.sh
+++ trunk/pldi2014/createZip.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir buchiAutomizer
-cp -a ../../source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* buchiAutomizer/
-cp ../LICENSE* buchiAutomizer/
-cp ../../examples/toolchains/BuchiAutomizerCWithBlockEncoding.xml buchiAutomizer/
-cp ../../examples/settings/BuchiAutomizerDefaultSettings buchiAutomizer/
+cp -a ../source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* buchiAutomizer/
+cp LICENSE* buchiAutomizer/
+cp ../examples/toolchains/BuchiAutomizerCWithBlockEncoding.xml buchiAutomizer/
+cp BuchiAutomizerDefaultSettings buchiAutomizer/
 cp buchiAutomizer.sh buchiAutomizer/
 cp Ultimate.ini buchiAutomizer/
 cp README buchiAutomizer/
