0df5cb5a5aa63203a99156fef0e8a6810151ed65
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-12-18 16:11:56 +0000

    examples

---
+++ trunk/examples/programs/termination/termination-crafted-ultimate/Toulouse-MultiBranchesToLoop_true.c
@@ -0,0 +1,63 @@
+//#Termination
+/*
+ * Date: November 2013
+ * Author: heizmann@informatik.uni-freiburg.de
+ *
+ * 
+ */
+
+extern int __VERIFIER_nondet_int(void);
+
+int main() {
+    int x,y,z,m,n;
+    if (__VERIFIER_nondet_int()) {
+        x = 1;
+    } else {
+        x = -1;
+    }
+    if (x > 0) {
+        x++;
+    } else {
+        x--;
+    }
+    if (x > 0) {
+        x++;
+    } else {
+        x--;
+    }
+    if (x > 0) {
+        x++;
+    } else {
+        x--;
+    }
+    if (x > 0) {
+        x++;
+    } else {
+        x--;
+    }
+    if (x > 0) {
+        x++;
+    } else {
+        x--;
+    }
+    if (x > 0) {
+        x++;
+    } else {
+        x--;
+    }
+    if (x > 0) {
+        x++;
+    } else {
+        x--;
+    }
+    if (x > 0) {
+        x++;
+    } else {
+        x--;
+    }
+    while (y<100 && z<100) {
+        y = y+x;
+        z = z-x;
+    }
+    return 0;
+}
