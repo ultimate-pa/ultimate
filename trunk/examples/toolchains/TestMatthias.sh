9e54c44458a580edaab775623746df3585597bad
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-12-21 18:16:21 +0000

    testscript

--- trunk/examples/toolchains/TestMatthias.sh
+++ trunk/examples/toolchains/TestMatthias.sh
@@ -3,13 +3,13 @@
 if [ "$1" = "terminator-ea" ]; then
 echo "buchiAutomizer for folder $2"
 trunk/examples/toolchains/TraceAbstractionTestDir.sh -ea 120 "$2" \
- "BuchiAutomizerWithBlockEncoding.xml;BuchiAutomizerCWithBlockEncoding.xml;buchiAutomizer/defaultWithLBE"
+ "BuchiAutomizerWithBlockEncoding.xml;BuchiAutomizerCWithBlockEncoding.xml;buchiAutomizer/staged300"
 fi
 
 if [ "$1" = "terminator-da" ]; then
 echo "buchiAutomizer for folder $2"
-trunk/examples/toolchains/TraceAbstractionTestDir.sh -ea 200 "$2" \
- "BuchiAutomizerWithBlockEncoding.xml;BuchiAutomizerCWithBlockEncoding.xml;buchiAutomizer/def180"
+trunk/examples/toolchains/TraceAbstractionTestDir.sh -da 350 "$2" \
+ "BuchiAutomizerWithBlockEncoding.xml;BuchiAutomizerCWithBlockEncoding.xml;buchiAutomizer/staged300"
 fi
 
 
