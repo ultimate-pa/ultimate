1f631f0f21aafadb1f77b1b9973d0b914255cfd6
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2013-12-29 22:48:30 +0000

    classes for removal of ite terms

---
+++ trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/smt/IteRemover.java
@@ -0,0 +1,78 @@
+package de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.smt;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import de.uni_freiburg.informatik.ultimate.logic.ApplicationTerm;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.Util;
+import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
+
+/**
+ * Transform term into an equivalent term without ite terms. E.g.,
+ * a = b ? c : d becomes b && a = c || !b && a = d
+ * (= a (ite b c d)) becomes (or (and b (= a c)) (and (not b) (= a d)))
+ * 
+ * TODO: This could be much more efficient if could obtain the context in which
+ * the ite term occurs and check if the condition holds.(Use ideas from 
+ * SimplifyDDA). For small formulas we can achieve the same result by applying
+ * SimplifyDDA afterwards.
+ * @author Matthias Heizmann
+ *
+ */
+public class IteRemover extends NonCoreBooleanSubTermTransformer {
+	private final Script m_Script;
+
+	public IteRemover(Script script) {
+		super();
+		m_Script = script;
+	}
+
+	@Override
+	protected Term transformNonCoreBooleanSubterm(Term term) {
+		assert term.getSort().getName().equals("Bool");
+		Set<ApplicationTerm> iteSubterms = (new ApplicationTermFinder("ite")).findMatchingSubterms(term);
+		Term result = rec(term, iteSubterms.iterator());
+		assert (Util.checkSat(m_Script, m_Script.term("distinct", 
+				term, result)) != LBool.SAT);
+		return result;
+	}
+	
+	private Term rec(Term term, Iterator<ApplicationTerm> it) {
+		Term result;
+		if (it.hasNext()) {
+			ApplicationTerm ite = it.next();
+			assert ite.getFunction().getName().equals("ite");
+			assert ite.getParameters().length == 3;
+			Term condition = ite.getParameters()[0];
+			Term ifTerm = ite.getParameters()[1];
+			Term elseTerm = ite.getParameters()[2];
+			Term replacedWithIf;
+			{
+				Map<Term, Term> substitutionMapping = 
+						Collections.singletonMap((Term) ite, ifTerm);
+				replacedWithIf = (new SafeSubstitution(
+						m_Script, substitutionMapping)).transform(term);
+			}
+			Term replacedWithElse;
+			{
+				Map<Term, Term> substitutionMapping = 
+						Collections.singletonMap((Term) ite, elseTerm);
+				replacedWithElse = (new SafeSubstitution(
+						m_Script, substitutionMapping)).transform(term);
+			}
+			Term withoutThisIte = Util.or(m_Script, 
+					Util.and(m_Script, condition, replacedWithIf), 
+					Util.and(m_Script, Util.not(m_Script, condition), replacedWithElse)
+					);
+			result = rec(withoutThisIte, it); 
+		} else {
+			result = term;
+		}
+		return result;
+	}
+
+}
---
+++ trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/smt/NonCoreBooleanSubTermTransformer.java
@@ -0,0 +1,83 @@
+package de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.smt;
+
+import de.uni_freiburg.informatik.ultimate.logic.ApplicationTerm;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.TermTransformer;
+
+/**
+ * Transform non-CoreBoolean subterms of a term. Here, we call a term 
+ * non-CoreBoolean if it is not an ApplicationTerm whose connective is defined
+ * by the core theory and whose parameters dot not all have sort Bool.
+ * 
+ * @author Matthias Heizmann
+ *
+ */
+public abstract class NonCoreBooleanSubTermTransformer {
+	
+	private NonCoreBooleanSubtermTransformerHelper m_TransformerHelper;
+	
+	public Term transform(Term term) {
+		if (!term.getSort().getName().equals("Bool")) {
+			throw new IllegalArgumentException("Input term of sort Bool");
+		}
+		m_TransformerHelper = new NonCoreBooleanSubtermTransformerHelper();
+		Term result = m_TransformerHelper.transform(term);
+		return result;
+	}
+
+	private static boolean hasBooleanParams(Term[] parameters) {
+		for (Term term : parameters) {
+			if (!term.getSort().getName().equals("Bool")) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean isCoreBooleanConnective(String fun) {
+		if (fun.equals("=")) {
+			return true;
+		} else if (fun.equals("and")) {
+			return true;
+		} else if (fun.equals("or")) {
+			return true;
+		} else if (fun.equals("=>")) {
+			return true;
+		} else if (fun.equals("xor")) {
+			return true;
+		} else if (fun.equals("distinct")) {
+			return true;
+		} else if (fun.equals("ite")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+	private class NonCoreBooleanSubtermTransformerHelper extends TermTransformer {
+
+		@Override
+		protected void convert(Term term) {
+			assert term.getSort().getName().equals("Bool") : "not Bool";
+			if (term instanceof ApplicationTerm) {
+				ApplicationTerm appTerm = (ApplicationTerm) term;
+				String funName = appTerm.getFunction().getName();
+				if (isCoreBooleanConnective(funName) && 
+						hasBooleanParams(appTerm.getParameters())) {
+					super.convert(term);
+				} else {
+					Term transformed = transformNonCoreBooleanSubterm(term);
+					setResult(transformed);
+					return;
+				}
+			}
+			super.convert(term);
+		}
+		
+	}
+
+
+	protected abstract Term transformNonCoreBooleanSubterm(Term term);
+
+}
