9e23147c31d819253364fbd631fe210954dabc30
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-02-12 00:52:53 +0000

    require rank bound only for rank decrease

--- trunk/source/BuchiAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/buchiautomizer/BinaryStatePredicateManager.java
+++ trunk/source/BuchiAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/buchiautomizer/BinaryStatePredicateManager.java
@@ -338,7 +338,7 @@ public class BinaryStatePredicateManager {
 		m_LexEquality = new IPredicate[m_LexTerms.length];
 		for (int i=0; i<m_LexTerms.length; i++) {
 			m_LexEquality[i] = getRankInEquality(
-					m_LexTerms[i], "=", m_OldRankVariables[i], true);
+					m_LexTerms[i], "=", m_OldRankVariables[i], false);
 			if (s_Annotate) {
 				String name = "equality" + i;
 				Annotation annot = new Annotation(":named", name);
