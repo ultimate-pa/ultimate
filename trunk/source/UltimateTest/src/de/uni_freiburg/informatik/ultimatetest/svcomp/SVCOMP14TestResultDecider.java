c2bf4550c74eeef8b0502f93d79843ca799ee56f
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2014-03-03 14:44:36 +0000

    - uups, reverted

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/svcomp/SVCOMP14TestResultDecider.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/svcomp/SVCOMP14TestResultDecider.java
@@ -5,11 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.Map.Entry;
-
-import javax.sql.rowset.Predicate;
-
 import org.apache.log4j.Logger;
-
 import de.uni_freiburg.informatik.ultimate.core.api.UltimateServices;
 import de.uni_freiburg.informatik.ultimate.result.CounterExampleResult;
 import de.uni_freiburg.informatik.ultimate.result.IResult;
