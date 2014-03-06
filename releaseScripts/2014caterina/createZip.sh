181c86270ccac27320f9cc6df7e37118870d2fbc
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-03-06 13:26:04 +0000

    bugfix release script

--- releaseScripts/2014caterina/createZip.sh
+++ releaseScripts/2014caterina/createZip.sh
@@ -3,7 +3,7 @@ mkdir buchiAutomizer
 cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* buchiAutomizer/
 cp LICENSE* buchiAutomizer/
 cp ../../trunk/examples/toolchains/BuchiAutomizerCWithBlockEncoding.xml buchiAutomizer/
-cp BuchiAutomizerDefaultSettings buchiAutomizer/
+cp BuchiAutomizerDefaultSettings.epf buchiAutomizer/
 cp buchiAutomizer.sh buchiAutomizer/
 cp Ultimate.ini buchiAutomizer/
 cp README buchiAutomizer/
