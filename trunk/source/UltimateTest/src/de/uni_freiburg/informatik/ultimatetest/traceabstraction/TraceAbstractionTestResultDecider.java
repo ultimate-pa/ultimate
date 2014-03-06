0b2499b3c21a264edf6b1467a7206caa86b76a9d
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-03-06 18:40:27 +0000

    revised test result decider

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/TraceAbstractionTestResultDecider.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/TraceAbstractionTestResultDecider.java
@@ -6,39 +6,96 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.log4j.Logger;
 
 import de.uni_freiburg.informatik.ultimate.core.api.UltimateServices;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.TraceAbstractionBenchmarks;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.TraceAbstractionObserver;
 import de.uni_freiburg.informatik.ultimate.result.BenchmarkResult;
 import de.uni_freiburg.informatik.ultimate.result.CounterExampleResult;
-import de.uni_freiburg.informatik.ultimate.result.GenericResultAtElement;
+import de.uni_freiburg.informatik.ultimate.result.ExceptionOrErrorResult;
+import de.uni_freiburg.informatik.ultimate.result.GenericResult;
 import de.uni_freiburg.informatik.ultimate.result.IResult;
-import de.uni_freiburg.informatik.ultimate.result.NoResult;
 import de.uni_freiburg.informatik.ultimate.result.PositiveResult;
 import de.uni_freiburg.informatik.ultimate.result.SyntaxErrorResult;
-import de.uni_freiburg.informatik.ultimate.result.ExceptionOrErrorResult;
 import de.uni_freiburg.informatik.ultimate.result.TimeoutResult;
+import de.uni_freiburg.informatik.ultimate.result.TypeErrorResult;
+import de.uni_freiburg.informatik.ultimate.result.UnprovableResult;
+import de.uni_freiburg.informatik.ultimate.result.UnsupportedSyntaxResult;
 import de.uni_freiburg.informatik.ultimatetest.ITestResultDecider;
 import de.uni_freiburg.informatik.ultimatetest.Util;
 
+/**
+ * Decide if a Ultimate Automizer verified file correctly.
+ * 
+ * @author Betim Musa, Matthias Heizmann
+ *
+ */
 public class TraceAbstractionTestResultDecider implements ITestResultDecider {
 	private String m_InputFile;
 	private ExpectedResult m_ExpectedResult;
 	private TraceAbstractionTestSummary m_Summary;
 	private String m_UniqueString;
 	
+	private enum TestOutcome {
+		FAIL,
+		UNKNOWN,
+		SUCCESS
+	}
+	
+	/**
+	 * Result that we expect after checking filename and keywords of the input
+	 * file.
+	 */
 	private enum ExpectedResult {
 		SAFE,
 		UNSAFE,
-		NOSPECIFICATION,
 		SYNTAXERROR,
 		NOANNOTATION
 	}
-	public TraceAbstractionTestResultDecider(File inputFile, TraceAbstractionTestSummary testSummary,
+	
+	/**
+	 * Result that is returned by the Automizer toolchain.
+	 */
+	private enum AutomizerResultType {
+		SAFE,
+		UNSAFE,
+		UNKNOWN,
+		SYNTAX_ERROR,
+		TIMEOUT,
+		UNSUPPORTED_SYNTAX,
+		EXCEPTION_OR_ERROR,
+		NO_RESULT;
+	}
+	
+	private class AutomizerResult {
+		private final IResult m_IResult;
+		private final AutomizerResultType m_AutomizerResultType;
+		public AutomizerResult(AutomizerResultType automizerResultType,
+				IResult iResult) {
+			super();
+			m_IResult = iResult;
+			m_AutomizerResultType = automizerResultType;
+		}
+		public IResult getIResult() {
+			return m_IResult;
+		}
+		public AutomizerResultType getAutomizerResultType() {
+			return m_AutomizerResultType;
+		}
+		
+	}
+	
+
+	
+	public TraceAbstractionTestResultDecider(File inputFile, 
+			TraceAbstractionTestSummary testSummary,
 			String uniqueString) {
 		m_InputFile = inputFile.getAbsolutePath();
 		m_ExpectedResult = getExpectedResult(inputFile);
@@ -53,7 +110,7 @@ public class TraceAbstractionTestResultDecider implements ITestResultDecider {
 	 * Read the expected result from an input file
 	 * 
 	 * Expected results are expected to be specified in an input file's
-	 * first line and start with '//#Unsafe', '//#Safe' or '//#NoSpec'.
+	 * first line and start with '//#Unsafe', '//#Safe' or '//#SyntaxError'.
 	 * If this is not case, the expected result may be specified within the file name via the suffix
 	 * "-safe" or "-unsafe".
 	 */
@@ -72,8 +129,6 @@ public class TraceAbstractionTestResultDecider implements ITestResultDecider {
 				return ExpectedResult.SAFE;
 			} else if (line.contains("#Unsafe")) {
 				return ExpectedResult.UNSAFE;
-			} else if (line.contains("#NoSpec"))  {
-				return ExpectedResult.NOSPECIFICATION;
 			} else if (line.contains("#SyntaxError")) { 
 				return ExpectedResult.SYNTAXERROR;
 			}
@@ -88,11 +143,12 @@ public class TraceAbstractionTestResultDecider implements ITestResultDecider {
 		return ExpectedResult.NOANNOTATION;
 	}
 	
+
 	@Override
 	public boolean isResultFail() {
 		Logger log = Logger.getLogger(TraceAbstractionTestResultDecider.class);
 		Collection<String> customMessages = new LinkedList<String>();
-		final boolean fail;
+		final TestOutcome testoutcome;
 		List<IResult> traceAbstractionResults = new ArrayList<IResult>();
 		for (Entry<String, List<IResult>> entry : UltimateServices.getInstance().getResultMap().entrySet()) {
 			traceAbstractionResults.addAll(entry.getValue());
@@ -104,53 +160,94 @@ public class TraceAbstractionTestResultDecider implements ITestResultDecider {
 					"//#NoSpec if there is still no decision about the specification.");
 		}
 		String uniqueStringPrefixOfInputFile = m_UniqueString + "_" + m_InputFile;
-		if (traceAbstractionResults.size() == 0) {
-			fail = true;
-			customMessages
-					.add("There were no results. Therefore an exception has been thrown.");
-			m_Summary.addFail(null, uniqueStringPrefixOfInputFile, "Error: There were no results at all. "+
-																	"Tool didn't exit normally.");
-		} else {
-			IResult result = getTraceAbstractionResultOfSet(traceAbstractionResults);
-			switch (m_ExpectedResult) {
-			case SAFE:
-				fail = !(result instanceof PositiveResult);
-				break;
-			case UNSAFE:
-				fail = !(result instanceof CounterExampleResult);
-				break;
-			case NOSPECIFICATION:
-				fail = resultSetContainsGenericResultWithNoSpecification(traceAbstractionResults);
-				break;
-			case SYNTAXERROR:
-				fail = !(result instanceof SyntaxErrorResult);
-				break;
-			case NOANNOTATION:
-				fail = true;
-				break;
-			default:
-				throw new AssertionError("unexpected case");
-			}
-			
-			String resultCategory = convertResultToCategory(result);
-			
-			if (!fail) {
-				m_Summary.addSuccess(resultCategory, uniqueStringPrefixOfInputFile, "Annotation says: " + m_ExpectedResult + 
-						"\tModel checker says: " + m_ExpectedResult);
+		AutomizerResult automizerResult = getAutomizerResult(traceAbstractionResults);
+		assert (automizerResult != null) : "if there is no result you should use NO_RESULT"; 
+		switch (automizerResult.getAutomizerResultType()) {
+		case EXCEPTION_OR_ERROR:
+			testoutcome = TestOutcome.FAIL;
+			break;
+		case SAFE:
+			if (m_ExpectedResult == ExpectedResult.SAFE) {
+				testoutcome = TestOutcome.SUCCESS;
+			} else if (m_ExpectedResult == ExpectedResult.NOANNOTATION) {
+				testoutcome = TestOutcome.UNKNOWN;
 			} else {
-				if (result instanceof ExceptionOrErrorResult) {
-					m_Summary.addFail(resultCategory, uniqueStringPrefixOfInputFile, "Error: " + result.getLongDescription());
-				} else if (result instanceof TimeoutResult) {
-					m_Summary.addUnknown(resultCategory, uniqueStringPrefixOfInputFile, "Annotation says: " + m_ExpectedResult +
-							"\tModel checker says: Time out");
-				} else if (m_ExpectedResult == ExpectedResult.NOANNOTATION) {
-					m_Summary.addUnknown(resultCategory, uniqueStringPrefixOfInputFile, "File was neither annotated nor does the filename contain a specification.");
-				} else {
-					m_Summary.addFail(resultCategory, uniqueStringPrefixOfInputFile, "Annotation says: " + m_ExpectedResult + 
-							"\tModel checker says: " + (result.getShortDescription() != "" ? result.getShortDescription() : "NoResult"));
-				}
+				testoutcome = TestOutcome.FAIL;
 			}
+			break;
+		case UNSAFE:
+			if (m_ExpectedResult == ExpectedResult.UNSAFE) {
+				testoutcome = TestOutcome.SUCCESS;
+			} else if (m_ExpectedResult == ExpectedResult.NOANNOTATION) {
+				testoutcome = TestOutcome.UNKNOWN;
+			} else {
+				testoutcome = TestOutcome.FAIL;
+			}
+			break;
+		case UNKNOWN:
+			// syntax error should always have been found
+			if (m_ExpectedResult == ExpectedResult.SYNTAXERROR) {
+				testoutcome = TestOutcome.FAIL;
+			} else {
+				testoutcome = TestOutcome.UNKNOWN;
+			}
+			break;
+		case SYNTAX_ERROR:
+			if (m_ExpectedResult == ExpectedResult.SYNTAXERROR) {
+				testoutcome = TestOutcome.SUCCESS;
+			} else {
+				testoutcome = TestOutcome.FAIL;
+			}
+			break;
+		case TIMEOUT:
+			// syntax error should always have been found
+			if (m_ExpectedResult == ExpectedResult.SYNTAXERROR) {
+				testoutcome = TestOutcome.FAIL;
+			} else {
+				testoutcome = TestOutcome.UNKNOWN;
+			}
+			break;
+		case UNSUPPORTED_SYNTAX:
+			testoutcome = TestOutcome.FAIL;
+			break;
+		case NO_RESULT:
+			testoutcome = TestOutcome.FAIL;
+			break;
+		default:
+			throw new AssertionError("unknown case");
 		}
+
+		String message;
+		if (automizerResult.getAutomizerResultType() == AutomizerResultType.EXCEPTION_OR_ERROR) {
+			message = "Error: " + automizerResult.getIResult().getLongDescription();
+		} else if (m_ExpectedResult == ExpectedResult.NOANNOTATION) {
+			message = "File was neither annotated nor does the filename contain a specification." + 
+					"\tModel checker says: " + automizerResult.getAutomizerResultType().toString();
+		} else {
+			message = "Annotation says: " + m_ExpectedResult + 
+					"\tModel checker says: " + automizerResult.getAutomizerResultType().toString();
+		}
+
+		IResult iResult = automizerResult.getIResult();
+		if (iResult != null) {
+			message += "\t ShortDescription: " + iResult.getShortDescription();
+			message += "\t LongDescription: " + iResult.getLongDescription(); 
+		}
+
+		switch (testoutcome) {
+		case FAIL:
+			m_Summary.addFail(automizerResult.getAutomizerResultType().toString(), uniqueStringPrefixOfInputFile, message);
+			break;
+		case SUCCESS:
+			m_Summary.addSuccess(automizerResult.getAutomizerResultType().toString(), uniqueStringPrefixOfInputFile, message);
+			break;
+		case UNKNOWN:
+			m_Summary.addUnknown(automizerResult.getAutomizerResultType().toString(), uniqueStringPrefixOfInputFile, message);
+			break;
+		default:
+			throw new AssertionError("unknown testoutcome");
+		}
+
 		// Add benchmark results to TraceAbstraction summary
 		BenchmarkResult benchmarkResult = getBenchmarkResultOfResultSet(traceAbstractionResults);
 		if (benchmarkResult != null) {
@@ -158,59 +255,99 @@ public class TraceAbstractionTestResultDecider implements ITestResultDecider {
 		} else {
 			m_Summary.addTraceAbstractionBenchmarks(uniqueStringPrefixOfInputFile, "No benchmark results available.");
 		}
+		final boolean fail = (testoutcome == TestOutcome.FAIL);
 		Util.logResults(log, m_InputFile, fail, customMessages);
 		return fail;
 	}
 	
-	private String convertResultToCategory(IResult result) {
-		return result == null ? "NULL" : result.getClass().toString();
-	}
-
 	private BenchmarkResult getBenchmarkResultOfResultSet(List<IResult> results) {
 		for (IResult res : results) {
 			if (res instanceof BenchmarkResult) {
-				return ((BenchmarkResult) res); 
+				BenchmarkResult benchmarkResult = (BenchmarkResult) res;
+				Object benchmark = benchmarkResult.getBenchmark();
+				if (benchmark instanceof TraceAbstractionBenchmarks) {
+					return benchmarkResult;
+				}
 			}
 		}
 		return null;
 	}
 	
-	private boolean resultSetContainsGenericResultWithNoSpecification(List<IResult> results) {
-		for (IResult res : results) {
-			if (res instanceof GenericResultAtElement) {
-				if (((GenericResultAtElement<?>)res).getShortDescription() == "No specification checked" &&
-						((GenericResultAtElement<?>)res).getShortDescription() == "We were not able to verify any" +
-						" specifiation because the program does not contain any specification.") {
-					return true;
-				}
+	
+	private AutomizerResult getAutomizerResult(List<IResult> results) {
+		final AutomizerResult returnValue;
+		Map<AutomizerResultType, AutomizerResult> resultSet = 
+				new HashMap<AutomizerResultType, AutomizerResult>();
+		for (IResult result : results) {
+			AutomizerResult extracted = extractResult(result);
+			if (extracted != null) {
+				resultSet.put(extracted.getAutomizerResultType(), extracted);
 			}
 		}
-		return false;
+		if (resultSet.containsKey(AutomizerResultType.EXCEPTION_OR_ERROR)) {
+			returnValue = resultSet.get(AutomizerResultType.EXCEPTION_OR_ERROR);
+		} else if (resultSet.containsKey(AutomizerResultType.SYNTAX_ERROR)) {
+			if (resultSet.size() > 1) {
+				throw new AssertionError("no other result allowed");
+			} else {
+				returnValue = resultSet.get(AutomizerResultType.SYNTAX_ERROR);
+			}
+		} else if (resultSet.containsKey(AutomizerResultType.UNSUPPORTED_SYNTAX)) {
+			if (resultSet.size() > 1) {
+				throw new AssertionError("no other result allowed");
+			} else {
+				returnValue = resultSet.get(AutomizerResultType.UNSUPPORTED_SYNTAX);
+			}
+		} else if (resultSet.containsKey(AutomizerResultType.SAFE)) {
+			returnValue = resultSet.get(AutomizerResultType.SAFE);
+		} else if (resultSet.containsKey(AutomizerResultType.UNSAFE)) {
+			returnValue = resultSet.get(AutomizerResultType.UNSAFE);
+		} else if (resultSet.containsKey(AutomizerResultType.UNKNOWN)) {
+			returnValue = resultSet.get(AutomizerResultType.UNKNOWN);
+		} else if (resultSet.containsKey(AutomizerResultType.TIMEOUT)) {
+			returnValue = resultSet.get(AutomizerResultType.TIMEOUT);
+		} else {
+			returnValue = resultSet.get(AutomizerResultType.NO_RESULT);
+		}
+		return returnValue;
 	}
 	
 
-	private IResult getTraceAbstractionResultOfSet(List<IResult> results) {
-		for (IResult res : results) {
-			if (res instanceof PositiveResult) {
-				return res;
-			} else if (res instanceof CounterExampleResult) {
-				return res;
-			} else if (res instanceof TimeoutResult) {
-				return res;
-			} else if (res instanceof SyntaxErrorResult) {
-				return res;
-			} else if (res instanceof NoResult) {
-				return res;
-			} else if (res instanceof ExceptionOrErrorResult) {
-				return res;
+	private AutomizerResult extractResult(IResult result) {
+		if (result instanceof GenericResult) {
+			if (result.getShortDescription().equals(TraceAbstractionObserver.s_NoSpec)) {
+				return new AutomizerResult(AutomizerResultType.SAFE, null);
+			} else {
+				return null;
 			}
+		} else if (result instanceof PositiveResult) {
+			return new AutomizerResult(AutomizerResultType.SAFE, null);
+		} else if (result instanceof CounterExampleResult) {
+			return new AutomizerResult(AutomizerResultType.UNSAFE, null);
+		} else if (result instanceof UnprovableResult) {
+			return new AutomizerResult(AutomizerResultType.UNKNOWN, null);
+		} else if (result instanceof TypeErrorResult) {
+			return new AutomizerResult(AutomizerResultType.SYNTAX_ERROR, result);
+		} else if (result instanceof SyntaxErrorResult) {
+			return new AutomizerResult(AutomizerResultType.SYNTAX_ERROR, result);
+		} else if (result instanceof TimeoutResult) {
+			return new AutomizerResult(AutomizerResultType.TIMEOUT, null);
+		} else if (result instanceof UnsupportedSyntaxResult) {
+			return new AutomizerResult(AutomizerResultType.UNSUPPORTED_SYNTAX, result);
+		} else if (result instanceof ExceptionOrErrorResult) {
+			return new AutomizerResult(AutomizerResultType.EXCEPTION_OR_ERROR, result);
+		} else {
+			return null;
 		}
-		return new NoResult(null, null, null);
 	}
 	
+
+
 	public boolean isResultFail(Exception e) {
-		m_Summary.addFail(convertResultToCategory(new NoResult(null, null, null)), m_InputFile, "Exception of type " + e.getClass().getName() + " thrown.\t"+
-		                                       "Message: " + e.getMessage());
+		m_Summary.addFail(
+				AutomizerResultType.EXCEPTION_OR_ERROR.toString(), 
+				m_InputFile, 
+				"Exception of type " + e.getClass().getName() + " thrown.\t"+ "Message: " + e.getMessage());
 		m_Summary.addTraceAbstractionBenchmarks(m_InputFile, "No benchmark results available.");
 		Logger log = Logger.getLogger(TraceAbstractionTestResultDecider.class);
 		Util.logResults(log, m_InputFile, true, new LinkedList<String>());
