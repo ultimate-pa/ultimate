a9e5acdfcc1250b51f82e70d24be3821847be229
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-03-24 20:04:39 +0000

    new testfile

---
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/SvcompForwardBackward.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package de.uni_freiburg.informatik.ultimatetest.traceabstraction;
+
+import java.util.Collection;
+
+import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
+
+/**
+ * @author musab@informatik.uni-freiburg.de
+ *
+ */
+public class SvcompForwardBackward extends
+		AbstractTraceAbstractionTestSuite {
+	private static final String m_Path = "examples/svcomp/ssh-simplified/";
+	
+	// Time out for each test case in milliseconds
+	private static int m_Timeout = 60000;
+
+	private static final boolean m_TraceAbstractionWithForwardPredicates = true;
+	private static final boolean m_TraceAbstractionWithBackwardPredicates = true;
+	private static final boolean m_TraceAbstractionCWithForwardPredicates = true;
+	private static final boolean m_TraceAbstractionCWithBackwardPredicates = true;
+	
+	@Override
+	public Collection<UltimateTestCase> createTestCases() {
+		if (m_TraceAbstractionWithForwardPredicates) {
+			addTestCases(
+					"AutomizerBpl.xml",
+					"automizer/ForwardPredicates_Svcomp_300.epf",
+				    m_Path,
+				    new String[] {".bpl"},
+				    "Trace Abstraction via Forward Predicates (SP)",
+				    "BoogieFilesForwardPredicates",
+				    m_Timeout);
+		} 
+		if (m_TraceAbstractionWithBackwardPredicates) {
+			addTestCases(
+					"AutomizerBpl.xml",
+					"automizer/BackwardPredicates_Svcomp_300.epf",
+				    m_Path,
+				    new String[] {".bpl"},
+				    "Trace Abstraction via Backward Predicates (WP)",
+				    "BoogieFilesBackwardPredicates",
+				    m_Timeout);
+		}
+		if (m_TraceAbstractionCWithForwardPredicates) {
+			addTestCases(
+					"AutomizerC.xml",
+					"automizer/ForwardPredicates_Svcomp_300.epf",
+				    m_Path,
+				    new String[] {".c", ".i"},
+				    "Trace Abstraction via Forward Predicates (SP)",
+				    "CFilesForwardPredicates",
+				    m_Timeout);
+		}
+		if (m_TraceAbstractionCWithBackwardPredicates) {
+			addTestCases(
+					"AutomizerC.xml",
+					"automizer/BackwardPredicates_Svcomp_300.epf",
+				    m_Path,
+				    new String[] {".c", ".i"},
+				    "Trace Abstraction via Backward Predicates (WP)",
+				    "CFilesBackwardPredicates",
+				    m_Timeout);
+		}
+		return super.createTestCases();
+	}
+}
