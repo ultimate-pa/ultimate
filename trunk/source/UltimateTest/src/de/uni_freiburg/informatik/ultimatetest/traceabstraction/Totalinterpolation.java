d7f39af6960e165d5d950641eafd10f9d8b38198
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-03-29 15:45:45 +0000

    test totalinterpolation

---
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/Totalinterpolation.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package de.uni_freiburg.informatik.ultimatetest.traceabstraction;
+
+import java.util.Collection;
+
+import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
+
+/**
+ * @author heizmann@informatik.uni-freiburg.de
+ *
+ */
+public class Totalinterpolation extends
+		AbstractTraceAbstractionTestSuite {
+	private static final String m_Path = "examples/programs/regression";
+	
+	// Time out for each test case in milliseconds
+	private static int m_Timeout = 20000;
+
+	private static final boolean s_Boogie_TreeInterpolants = true;
+	private static final boolean s_C_TreeInterpolants = true;
+	private static final boolean s_Boogie_TreeInterpolantsWithTotalinterpolation = true;
+	private static final boolean s_C_TreeInterpolantsWithTotalinterpolation = true;
+	
+	@Override
+	public Collection<UltimateTestCase> createTestCases() {
+		if (s_Boogie_TreeInterpolants) {
+			addTestCases(
+					"AutomizerBpl.xml",
+					"automizer/TreeInterpolants.epf",
+				    m_Path,
+				    new String[] {".bpl"},
+				    "Treeinterpolants",
+				    "Boogie",
+				    m_Timeout);
+		} 
+		if (s_C_TreeInterpolants) {
+			addTestCases(
+					"AutomizerC.xml",
+					"automizer/TreeInterpolants.epf",
+				    m_Path,
+				    new String[] {".c", ".i"},
+				    "Treeinterpolants",
+				    "C",
+				    m_Timeout);
+		}
+		
+		if (s_Boogie_TreeInterpolantsWithTotalinterpolation) {
+			addTestCases(
+					"AutomizerBpl.xml",
+					"automizer/TreeInterpolants_TotalInterpolation.epf",
+				    m_Path,
+				    new String[] {".bpl"},
+				    "Treeinterpolants with totalinterpolation",
+				    "Boogie",
+				    m_Timeout);
+		} 
+		if (s_C_TreeInterpolantsWithTotalinterpolation) {
+			addTestCases(
+					"AutomizerC.xml",
+					"automizer/TreeInterpolants_TotalInterpolation.epf",
+				    m_Path,
+				    new String[] {".c", ".i"},
+				    "Treeinterpolants with totalinterpolation",
+				    "C",
+				    m_Timeout);
+		}
+		return super.createTestCases();
+	}
+}
