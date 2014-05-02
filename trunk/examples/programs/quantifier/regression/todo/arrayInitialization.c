c34be93f232fdf3abe4d83093bbca7d14f76c17e
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2014-05-02 11:33:29 +0000

    fixed bug in test

--- trunk/examples/programs/quantifier/regression/todo/arrayInitialization.c
+++ trunk/examples/programs/quantifier/regression/todo/arrayInitialization.c
@@ -6,8 +6,8 @@
  * Neither line works, but there is a different error.
  */
 int main(int argc, char **argv) {
-    int array[2] = { 1 };
-    static int array[2] = { 1 };
+    int array1[2] = { 1 };
+    static int array2[2] = { 1 };
     
     return 0;
 }
