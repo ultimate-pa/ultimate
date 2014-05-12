37a46e2d8fce569ccd465f4a8c7950f97f685c86
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-05-12 13:54:35 +0000

    also remove get-unsat-core

--- releaseScripts/smt/makeSmtCompCompatible.pl
+++ releaseScripts/smt/makeSmtCompCompatible.pl
@@ -12,6 +12,7 @@ while (<>) {
      next if $_ =~ /^\(set-option :produce.*\)/;
      next if $_ =~ /^\(get-value\s.*\)/;
      next if $_ =~ /^\(echo .*\)/;
+     next if $_ =~ /^\(get-unsat-core\)/;
 #following is only needed if we want to remove multiple occrences of check-sat
 #     if ($_ =~ /^\(check-sat\)/) {
 #        next if $checksatseen;
