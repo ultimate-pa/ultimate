a39ee30d0f585354d0f343e7b2649f1696a9f999
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-05-15 10:18:35 +0000

    reduce log level

--- trunk/source/BuchiAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/buchiautomizer/BuchiInterpolantAutomatonBouncer.java
+++ trunk/source/BuchiAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/buchiautomizer/BuchiInterpolantAutomatonBouncer.java
@@ -209,13 +209,13 @@ public class BuchiInterpolantAutomatonBouncer extends AbstractInterpolantAutomat
 		if (isPredHierLetterFalse(resPred, resHier, letter, sch)) {
 			if (!m_Result.contains(m_IaFalseState)) {
 				m_Result.addState(false, true, m_IaFalseState);
-				s_Logger.warn("BenchmarkResult: Transition to False Predicate");
+				s_Logger.debug("BenchmarkResult: Transition to False Predicate");
 			}
 			sch.addTransition(resPred, resHier, letter, m_IaFalseState);
 		} else if (isFalseSucc(resPred, resHier, letter, sch)) {
 			if (!m_Result.contains(m_IaFalseState)) {
 				m_Result.addState(false, true, m_IaFalseState);
-				s_Logger.warn("BenchmarkResult: Transition to False Predicate");
+				s_Logger.debug("BenchmarkResult: Transition to False Predicate");
 			}
 			sch.addTransition(resPred, resHier, letter, m_IaFalseState);
 		} else if (m_StemResPred2InputPreds.getDomain().contains(resPred)) {
