8ab496030a20489ced8ab28c112485ad2faa3d00
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-05-22 13:24:35 +0000

    generalize HashRelation and TreeRelation, documentation

---
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/AbstractRelation.java
@@ -0,0 +1,134 @@
+package de.uni_freiburg.informatik.ultimate.automata;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ * Object that represents a binary relation (i.e. a set of ordered pairs).
+ * Given an element of this relation (d,r)
+ * <ul>
+ * <li> we call the first entry d the domain element of relation, and
+ * <li> we call the second entry r the range element of the relation.
+ * </ul>
+ * We use a Map from domain elements to sets of range elements to store this
+ * relation. Therefore we can directly get
+ * <ul> 
+ * <li> the set of all domain elements and
+ * <li> the set of all range elements for a given domain element.
+ * </ul>
+ * This is an abstract class that does not define which Map data structure is
+ * used.
+ *  
+ * 
+ * @author Matthias Heizmann
+ *
+ * @param <D> Type of the elements that are in the domain of the relation.
+ * @param <R> Type of the elements that are in the range of the relation.
+ * @param <MAP> Type of Map that is used to store the relation.
+ */
+public abstract class AbstractRelation<D,R,MAP extends Map<D,Set<R>>> {
+	protected final MAP m_Map;
+	
+	public abstract MAP newMap();
+	
+	public abstract Set<R> newSet();
+
+	public AbstractRelation() {
+		super();
+		m_Map = newMap();
+	}
+	
+	/**
+	 * Add a pair (domainElem, rangeElem) to the relation.
+	 * @return if this relation did not already contain the specified pair. 
+	 */
+	public boolean addPair(D domainElem, R rangeElem) {
+		Set<R> rangeElems = m_Map.get(domainElem);
+		if (rangeElems == null) {
+			rangeElems = newSet();
+			m_Map.put(domainElem, rangeElems);
+		}
+		return rangeElems.add(rangeElem);
+	}
+
+	/**
+	 * Remove the pair (domainElem, rangeElem) from the relation.
+	 * @return true if the set contained the specified pair. 
+	 */
+	public boolean removePair(D domainElem, R rangeElem) {
+		final boolean result;
+		Set<R> rangeElems = m_Map.get(domainElem);
+		if (rangeElems == null) {
+			result = false;
+		} else {
+			result = rangeElems.remove(rangeElems);
+			if (rangeElems.isEmpty()) {
+				m_Map.remove(domainElem);
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Add all elements contained in relation rel to this relation.
+	 * Does not reuse sets of the relation rel but constructs new sets if 
+	 * necessary.
+	 */
+	public void addAll(AbstractRelation<D,R,?> rel) {
+		for (Entry<D, Set<R>> entry : rel.m_Map.entrySet()) {
+			Set<R> rangeElems = m_Map.get(entry.getKey());
+			if (rangeElems == null) {
+				rangeElems = newSet();
+				m_Map.put(entry.getKey(), rangeElems);
+			}
+			rangeElems.addAll(entry.getValue());
+		}
+	}
+	
+	/**
+	 * @return true if the pair (domainElem, rangeElem) is contained in the
+	 * relation.
+	 */
+	public boolean containsPair(D domainElem, R rangeElem) {
+		Set<R> rangeElems = m_Map.get(domainElem);
+		if (rangeElems == null) {
+			return false;
+		} else {
+			return rangeElems.contains(rangeElem);
+		}
+	}
+	
+	/**
+	 * Returns the set of all elements d such that for some r the pair
+	 * (d,r) is in the relation.
+	 */
+	public Set<D> getDomain() {
+		return m_Map.keySet();
+	}
+	
+	/**
+	 * Returns the set of all elements r such that for a the given element
+	 * domainElem, the pair (domainElem, r) is in the relation.
+	 */
+	public Set<R> getImage(D domainElem) {
+		return Collections.unmodifiableSet(m_Map.get(domainElem));
+	}
+	
+	/**
+	 * Returns the number of all pairs contained in this relation.
+	 */
+	public int size() {
+		int result = 0;
+		for (Set<R> rangeSet : m_Map.values()) {
+			result += rangeSet.size();
+		}
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+		return m_Map.toString();
+	}
+}
--- trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/HashRelation.java
+++ trunk/source/NestedWordAutomata/src/de/uni_freiburg/informatik/ultimate/automata/HashRelation.java
@@ -27,64 +27,23 @@ package de.uni_freiburg.informatik.ultimate.automata;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map.Entry;
 import java.util.Set;
 
-public class HashRelation<D,R> {
-	
-	private final HashMap<D,HashSet<R>> m_Map = new HashMap<D,HashSet<R>>();
-	
-	
+/**
+ * Implementation of the AbstractRelation that uses a HashMap.
+ * @author Matthias Heizmann
+ */
+public class HashRelation<D,R> extends AbstractRelation<D, R, HashMap<D,Set<R>>> {
 
-	public void addPair(D domainElem, R rangeElem) {
-		HashSet<R> rangeElems = m_Map.get(domainElem);
-		if (rangeElems == null) {
-			rangeElems = new HashSet<R>();
-			m_Map.put(domainElem, rangeElems);
-		}
-		rangeElems.add(rangeElem);
-	}
-	
-	public void addAll(HashRelation<D,R> hr) {
-		for (Entry<D, HashSet<R>> entry : hr.m_Map.entrySet()) {
-			HashSet<R> rangeElems = m_Map.get(entry.getKey());
-			if (rangeElems == null) {
-				rangeElems = new HashSet<R>();
-				m_Map.put(entry.getKey(), rangeElems);
-			}
-			rangeElems.addAll(entry.getValue());
-		}
-	}
-	
-	public boolean containsPair(D domainElem, R rangeElem) {
-		HashSet<R> rangeElems = m_Map.get(domainElem);
-		if (rangeElems == null) {
-			return false;
-		} else {
-			return rangeElems.contains(rangeElem);
-		}
-	}
-	
-	
-	public Set<D> getDomain() {
-		return m_Map.keySet();
-	}
-	
-	public Set<R> getImage(D domainElem) {
-		return m_Map.get(domainElem);
-	}
-	
-	public int size() {
-		int result = 0;
-		for (HashSet<R> rangeSet : m_Map.values()) {
-			result += rangeSet.size();
-		}
-		return result;
-	}
-	
 	@Override
-	public String toString() {
-		return m_Map.toString();
+	public HashMap<D, Set<R>> newMap() {
+		return new HashMap<D, Set<R>>();
 	}
+
+	@Override
+	public HashSet<R> newSet() {
+		return new HashSet<R>();
+	}
+
 
 }
