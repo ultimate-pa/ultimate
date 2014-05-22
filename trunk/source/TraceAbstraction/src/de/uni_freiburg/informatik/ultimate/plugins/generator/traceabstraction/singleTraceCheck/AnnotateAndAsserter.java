633dff7f9f0622211760028098362c21bf6dde5c
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-05-22 15:00:29 +0000

    computation of ProgramPoint sequence

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/singleTraceCheck/AnnotateAndAsserter.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/singleTraceCheck/AnnotateAndAsserter.java
@@ -20,6 +20,7 @@ import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.Call;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.CodeBlock;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.ParallelComposition;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.ProgramPoint;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.Return;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.SequentialComposition;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.StatementSequence;
@@ -330,6 +331,29 @@ public class AnnotateAndAsserter {
 
 		public NestedFormulas<Term, Term> getAnnotatedSsa() {
 			return m_AnnotSSA;
+		}
+		
+		
+		/**
+		 * Given a trace cb_0,...,cb_n returns the sequence of ProgramPoints 
+		 * that corresponds to this trace. This is the sequence
+		 * pp_0,...,pp_{n+1} such that
+		 * <ul>
+		 * <li> pp_i is the ProgramPoint before CodeBlock cb_i, and
+		 * <li> pp_{i+1} is the ProgramPoint after CodeBlock cb_i.
+		 * </ul>  
+		 */
+		public static List<ProgramPoint> getSequenceOfProgramPoints(
+												NestedWord<CodeBlock> trace) {
+			List<ProgramPoint> result = new ArrayList<ProgramPoint>();
+			for (CodeBlock cb : trace) {
+				ProgramPoint pp = (ProgramPoint) cb.getSource();
+				result.add(pp);
+			}
+			CodeBlock cb = trace.getSymbol(trace.length()-1);
+			ProgramPoint pp = (ProgramPoint) cb.getTarget();
+			result.add(pp);
+			return result;
 		}
 
 }
