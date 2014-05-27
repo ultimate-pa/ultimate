401c953d5a00ab686fca89fb4e07ef16cdf594b2
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-05-27 14:20:00 +0000

    removed examples that are already in the SV-COMP repository

--- trunk/examples/programs/recursivePrograms/SV-COMP_Candidates/RecursivePrograms/Readme.txt
+++
@@ -1,5 +0,0 @@
-This archive contains recursive C programs that hopefully become benchmarks in
-the SV-COMP http://sv-comp.sosy-lab.org/.
-
-Maintained by: heizmann@informatik.uni-freiburg.de
-Date: July 2013
--- trunk/examples/programs/recursivePrograms/SV-COMP_Candidates/createAndUpload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-zip -r RecursivePrograms.zip  RecursivePrograms
-chmod 755 RecursivePrograms.zip
-scp ./RecursivePrograms.zip heizmann@sotec.informatik.uni-freiburg.de:/export/server/httpd/ultimate/automizer/
