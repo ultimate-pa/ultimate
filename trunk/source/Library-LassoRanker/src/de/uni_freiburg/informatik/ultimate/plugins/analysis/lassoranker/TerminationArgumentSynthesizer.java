ff0fc64de88601ea567837778f1287f2625e9ed7
Jan Leike <leike@informatik.uni-freiburg.de>
2014-06-08 08:31:52 +0000

    fixed two warnings

--- trunk/source/Library-LassoRanker/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/lassoranker/TerminationArgumentSynthesizer.java
+++ trunk/source/Library-LassoRanker/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/lassoranker/TerminationArgumentSynthesizer.java
@@ -31,7 +31,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
