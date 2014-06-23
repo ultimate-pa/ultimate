fbbffbda14e5bd2e95dc2865ad1148f8b01d9722
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-06-23 00:54:48 +0000

    release scripts

--- releaseScripts/termcomp2014/createZip.sh
+++ releaseScripts/termcomp2014/createZip.sh
@@ -2,7 +2,7 @@
 mkdir automizer
 cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* automizer/
 cp ../LICENSE* automizer/
-cp README automizer/
+cp starexec_description.txt automizer/
 cp ../../trunk/examples/toolchains/BuchiAutomizerCWithBlockEncoding.xml automizer/
 cp BuchiAutomizer.sh automizer/
 cp Ultimate.ini automizer/
