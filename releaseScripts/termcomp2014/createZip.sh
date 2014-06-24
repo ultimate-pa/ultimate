577f5b8b2f29690d443ce4607603603bed252930
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-06-24 16:38:29 +0000

    starExecPackage

--- releaseScripts/termcomp2014/createZip.sh
+++ releaseScripts/termcomp2014/createZip.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-mkdir automizer
-cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* automizer/
-cp ../LICENSE* automizer/
-cp starexec_description.txt automizer/
-cp ../../trunk/examples/toolchains/BuchiAutomizerCWithBlockEncoding.xml automizer/
-cp BuchiAutomizer.sh automizer/
-cp Ultimate.ini automizer/
-cp settings.epf automizer/
-zip UltimateCommandline.zip -r automizer/*
+mkdir StarExecArchive
+mkdir StarExecArchive/bin
+mkdir StarExecArchive/Ultimate
+cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* StarExecArchive/Ultimate/
+cp LICENSE* StarExecArchive/Ultimate/
+cp starexec_description.txt StarExecArchive/
+cp ../../trunk/examples/toolchains/BuchiAutomizerCWithBlockEncoding.xml StarExecArchive/
+cp starexec_run_default StarExecArchive/bin
+cp Ultimate.ini StarExecArchive/Ultimate/
+cp settings.epf StarExecArchive/
+cp -R /opt/z3-4.3.2.ff265c6c6ccf-x64-ubuntu-12.04 StarExecArchive/
+cd StarExecArchive
+zip ../UltimateCommandline.zip -r *
+
--- releaseScripts/termcomp2014/zip2web.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-scp ./UltimateCommandline.zip heizmann@sotec.informatik.uni-freiburg.de:/export/server/httpd/ultimate/BuchiAutomizer
