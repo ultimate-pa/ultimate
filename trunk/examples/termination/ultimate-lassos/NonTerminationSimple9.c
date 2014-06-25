324c26be4276dc3cab042c3cb215042a161e6bc7
Jan Leike <leike@informatik.uni-freiburg.de>
2014-06-25 14:54:13 +0000

    some nontermination examples for c

---
+++ trunk/examples/termination/ultimate-lassos/NonTerminationSimple9.c
@@ -0,0 +1,17 @@
+//#rNonTerminationDerivable
+/*
+ * Date: 2014-06-26
+ * Author: leike@informatik.uni-freiburg.de
+ */
+
+extern int __VERIFIER_nondet_int();
+
+int main()
+{
+	int x = __VERIFIER_nondet_int();
+	while (x >= 0) {
+		x += __VERIFIER_nondet_int();
+	}
+	return 0;
+}
+
---
+++ trunk/examples/termination/ultimate-lassos/WhileTrue.c
@@ -0,0 +1,16 @@
+//#rNonTerminationDerivable
+/*
+ * Date: 2013-12-16
+ * Author: leike@informatik.uni-freiburg.de
+ *
+ * Very simple example for non-termination
+ */
+
+int main()
+{
+	while (1) {
+		// do nothing
+	}
+	return 0;
+}
+
