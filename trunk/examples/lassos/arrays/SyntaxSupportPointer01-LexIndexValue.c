e33e20dad1b3a6227f62cfb0309612d4c9508891
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-06-26 14:35:09 +0000

    example

---
+++ trunk/examples/lassos/arrays/SyntaxSupportPointer01-LexIndexValue.c
@@ -0,0 +1,23 @@
+//#rTerminationDerivable
+/*
+ * Date: 2012-06-03
+ * Author: heizmann@informatik.uni-freiburg.de
+ *
+ * 2-lex ranking function: f(k, *p) = (k, *p)
+ *
+ */
+
+extern int __VERIFIER_nondet_int();
+
+int main() {
+    int *p = malloc(133 * sizeof(int));
+    int k = 133;
+    while (*p >= 0 && k >= 0) {
+		if (__VERIFIER_nondet_int()) {
+			k--;
+		} else {
+			(*p)--;
+		}
+	}
+    return 0;
+}
