5ba4966c7c28b0f2fd4146d0734a40347eca46ae
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-06-29 11:29:00 +0000

    comment

--- trunk/examples/termination/newSVCOMP/LeikeHeizmann-TACAS2014-Ex7_true-termination.c
+++ trunk/examples/termination/newSVCOMP/LeikeHeizmann-TACAS2014-Ex7_true-termination.c
@@ -1,6 +1,13 @@
 /*
  * Program from Example 7 of
  * 2014TACAS - Leike, Heizmann - Ranking Templates for Linear Loops
+ * 
+ * In the conference version of this paper, the authors claimed that this
+ * lasso program does not have a multiphase ranking function. However, this
+ * lasso program has the following 2-phase ranking function.
+ *     f_0(x, y) = 2q + z
+ *     f_1(x, y) = q
+ * The authors thank Samir Genaim for pointing out this error in their paper.
  *
  * Date: 2014-06-29
  * Author: Jan Leike
