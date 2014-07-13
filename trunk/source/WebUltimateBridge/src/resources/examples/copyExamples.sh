79ba994d8bd9ff4f602af2df619343dd889cb1e8
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-07-13 01:45:54 +0000

    script for copying examples

---
+++ trunk/source/WebUltimateBridge/src/resources/examples/copyExamples.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Author: Matthias Heizmann
+# Date: 2014-07-13
+#
+# This bash script is in the folder
+#    trunk/source/WebUltimateBridge/src/resources/examples
+# The purpose of this script is to copy examples from
+#    trunk/examples
+# to this folder
+# Files that are in certain subfolders (e.g., rankBoogie, rankC) of this folder 
+# will be added to the web interface.
+# Before I build a new version of the web interface I (Matthias) will excute
+# this script.
+# 
+
+
+RANK_BOOGIE=(
+	"../../../../../examples/lassos/website/ATVA2013-yPositive-int.bpl" 
+	"../../../../../examples/lassos/arrays/SyntaxSupportArrays08-LexIndexValue.bpl"
+	)
+
+for i in "${RANK_BOOGIE[@]}"
+do
+	if [ ! -e "$i" ]; then
+		echo "cannot find $i"
+		exit 1
+	fi
+	cp $i rankBoogie/
+done
+
+
+RANK_C=(
+	"../../../../../examples/lassos/tigerbytes/ChenFlurMukhopadhyay-SAS2012-Ex1.01_true-termination.c"
+	"../../../../../examples/termination/TermCompOfficialBenchmarkSet/svcomp/LeikeHeizmann-TACAS2014-Ex1_true-termination.c"
+	"../../../../../examples/termination/TermCompOfficialBenchmarkSet/svcomp/LeikeHeizmann-TACAS2014-Ex7_true-termination.c"
+	"../../../../../examples/termination/TermCompOfficialBenchmarkSet/svcomp/LeikeHeizmann-TACAS2014-Ex8_true-termination.c"
+	"../../../../../examples/termination/TermCompOfficialBenchmarkSet/svcomp/LeikeHeizmann-TACAS2014-Ex9_true-termination.c"
+	"../../../../../examples/termination/TermCompOfficialBenchmarkSet/svcomp/LeikeHeizmann-TACAS2014-Fig1_true-termination.c"
+	"../../../../../examples/termination/TermCompOfficialBenchmarkSet/svcomp/LeikeHeizmann-WST2014-Ex5_false-termination.c"
+	"../../../../../examples/termination/TermCompOfficialBenchmarkSet/svcomp/LeikeHeizmann-WST2014-Ex6_false-termination.c"
+	"../../../../../examples/termination/TermCompOfficialBenchmarkSet/svcomp/LeikeHeizmann-WST2014-Ex9_true-termination.c"
+	)
+
+for i in "${RANK_C[@]}"
+do
+	if [ ! -e "$i" ]; then
+		echo "cannot find $i"
+		exit 1
+	fi
+	cp $i rankC/
+done
