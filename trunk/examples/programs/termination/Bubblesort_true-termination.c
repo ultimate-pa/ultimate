58a8a0b1a5b503cd98c62d8c27cee4b24ff5398b
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-07-16 00:48:23 +0000

    examples

--- trunk/examples/programs/termination/Bubblesort_true-termination.c
+++ trunk/examples/programs/termination/Bubblesort_true-termination.c
@@ -1,6 +1,5 @@
 /*
- * Implementation of bubble sort algorithm. All statements that are not relevant
- * for termination have been removed.
+ * Implementation of bubble sort algorithm.
  * Date: 2014-07-16
  * Author: heizmann@informatik.uni-freiburg.de
  */
@@ -8,10 +7,19 @@
 extern int __VERIFIER_nondet_int(void);
 
 
-void bubblesort(int i) {
+void swap(int a[], int i, int j) {
+    int tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+}
+
+void bubblesort(int a[], int i) {
     while (i>=0) {
         int j = 0;
         while (j < i) {
+            if (a[j] > a[i]) {
+                swap(a,i,j);
+            }
             j++;
         }
         i--;
@@ -23,7 +31,7 @@ int main() {
     if (n < 1) {
         n = 1;
     }
-    bubblesort(n-1);
+    int a[n];
+    bubblesort(a,n-1);
 }
-
 
--- trunk/examples/programs/termination/BugRewriteArraysNullpointerException.c
+++ trunk/examples/programs/termination/BugRewriteArraysNullpointerException.c
@@ -1,9 +1,4 @@
-/*
- * Implementation of bubble sort algorithm.
- * Date: 2014-07-16
- * Author: heizmann@informatik.uni-freiburg.de
- */
-
+ 
 extern int __VERIFIER_nondet_int(void);
 
 
@@ -17,9 +12,9 @@ void bubblesort(int a[], int i) {
     while (i>=0) {
         int j = 0;
         while (j < i) {
-            if (a[j] > a[i]) {
+//            if (a[j] > a[i]) {
                 swap(a,i,j);
-            }
+//            }
             j++;
         }
         i--;
