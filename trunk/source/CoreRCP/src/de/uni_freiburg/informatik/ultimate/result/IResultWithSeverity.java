39184aaff5710959a09a086a8fa5d6ad183d4886
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-08-11 10:52:30 +0000

    implement IResult

--- trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/result/IResultWithSeverity.java
+++ trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/result/IResultWithSeverity.java
@@ -1,7 +1,7 @@
 package de.uni_freiburg.informatik.ultimate.result;
 
 
-public interface IResultWithSeverity {
+public interface IResultWithSeverity extends IResult {
 
 	/**
 	 * Severity of a result determines how the result is visualized in a 
