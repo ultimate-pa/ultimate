dbd61739a3cc18c4967027134dac98e7e74a9bfb
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-08-27 22:56:05 +0000

    tests for BuchiAutomizer

---
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/buchiautomizer/AbstractBuchiAutomizerTestSuite.java
@@ -0,0 +1,30 @@
+package de.uni_freiburg.informatik.ultimatetest.buchiautomizer;
+
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.TraceAbstractionBenchmarks;
+import de.uni_freiburg.informatik.ultimatetest.AbstractModelCheckerTestSuite;
+import de.uni_freiburg.informatik.ultimatetest.TraceAbstractionTestSummary;
+import de.uni_freiburg.informatik.ultimatetest.UltimateRunDefinition;
+import de.uni_freiburg.informatik.ultimatetest.decider.ITestResultDecider;
+import de.uni_freiburg.informatik.ultimatetest.decider.TerminationAnalysisTestResultDecider;
+import de.uni_freiburg.informatik.ultimatetest.summary.CsvConcatenator;
+import de.uni_freiburg.informatik.ultimatetest.summary.ITestSummary;
+import de.uni_freiburg.informatik.ultimatetest.traceabstraction.TestSummaryWithBenchmarkResults;
+
+public abstract class AbstractBuchiAutomizerTestSuite extends AbstractModelCheckerTestSuite {
+
+	@Override
+	public ITestResultDecider constructITestResultDecider(
+			UltimateRunDefinition ultimateRunDefinition) {
+		return new TerminationAnalysisTestResultDecider(ultimateRunDefinition, true);
+	}
+
+	@Override
+	protected ITestSummary[] constructTestSummaries() {
+		return new ITestSummary[] {
+				new TestSummaryWithBenchmarkResults(this.getClass()),
+				new TraceAbstractionTestSummary(this.getClass()),
+				new CsvConcatenator(this.getClass(), TraceAbstractionBenchmarks.class)
+		};
+	}
+
+}
---
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/buchiautomizer/Lassos.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package de.uni_freiburg.informatik.ultimatetest.buchiautomizer;
+
+import java.util.Collection;
+
+import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
+
+/**
+ * @author heizmann@informatik.uni-freiburg.de
+ *
+ */
+public class Lassos extends
+		AbstractBuchiAutomizerTestSuite {
+	private static final String[] m_Directories = {
+		"examples/lassos",
+//		"examples/programs/quantifier",
+//		"examples/programs/recursivePrograms",
+//		"examples/programs/toy"
+//		"examples/termination/AProVE"
+	};
+	
+	// Time out for each test case in milliseconds
+	private static int m_Timeout = 10000;
+
+//	private static final boolean s_Boogie_TreeInterpolants = true;
+//	private static final boolean s_C_TreeInterpolants = true;
+	
+	
+	@Override
+	public Collection<UltimateTestCase> createTestCases() {
+		addTestCases(
+			"BuchiAutomizerBplWithBlockEncoding.xml",
+			"buchiAutomizerTestSuite/staged300-SMTInterpol.epf",
+		    m_Directories,
+		    new String[] {".bpl"},
+		    m_Timeout);
+		return super.createTestCases();
+	}
+}
