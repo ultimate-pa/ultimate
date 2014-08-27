1a5432ac6da36f53e934b02c85ad4ded1a3234ce
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-08-27 22:16:07 +0000

    new TerminationAnalysisResult

---
+++ trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/result/TerminationAnalysisResult.java
@@ -0,0 +1,73 @@
+package de.uni_freiburg.informatik.ultimate.result;
+
+/**
+ * Objects of this class are used to report the overall result of a termination
+ * analysis. Do not use this for synthesis of termination arguments.
+ * (I do not know if Severity.ERROR is a good choice for NONTERMINATION and
+ * UNKNOWN.)
+ * @author heizmann@informatik.uni-freiburg.de
+ *
+ */
+public class TerminationAnalysisResult extends AbstractResult implements
+		IResultWithSeverity {
+	
+	private final TERMINATION m_Termination;
+	private final String m_LongDescription;
+	
+	public TerminationAnalysisResult(String plugin, TERMINATION termination,
+			String longDescription) {
+		super(plugin);
+		m_Termination = termination;
+		m_LongDescription = longDescription;
+	}
+
+	public enum TERMINATION {
+		TERMINATING,
+		NONTERMINATING,
+		UNKNOWN
+	}
+
+	@Override
+	public String getShortDescription() {
+		final String shortDescription;
+		switch (m_Termination) {
+		case NONTERMINATING:
+			shortDescription = "Nontermination possible";
+			break;
+		case TERMINATING:
+			shortDescription = "Termination proven";
+			break;
+		case UNKNOWN:
+			shortDescription = "Unable to decide termination";
+			break;
+		default:
+			throw new AssertionError();
+		}
+		return shortDescription;
+	}
+
+	@Override
+	public String getLongDescription() {
+		return m_LongDescription;
+	}
+
+	@Override
+	public Severity getSeverity() {
+		Severity severity;
+		switch (m_Termination) {
+		case NONTERMINATING:
+			severity = Severity.ERROR;
+			break;
+		case TERMINATING:
+			severity = Severity.INFO;
+			break;
+		case UNKNOWN:
+			severity = Severity.ERROR;
+			break;
+		default:
+			throw new AssertionError();
+		}
+		return severity;
+	}
+
+}
