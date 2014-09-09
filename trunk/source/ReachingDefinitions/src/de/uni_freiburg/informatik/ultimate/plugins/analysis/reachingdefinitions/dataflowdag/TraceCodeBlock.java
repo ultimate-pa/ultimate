94525e959728638641c1d10c37426c2f6c0085c6
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2014-09-09 16:43:30 +0000

    ReachingDefinitions: Augmented TraceCodeBlock to also be able to hold an interpolant for the corresponding dag node

--- trunk/source/ReachingDefinitions/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/reachingdefinitions/dataflowdag/TraceCodeBlock.java
+++ trunk/source/ReachingDefinitions/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/reachingdefinitions/dataflowdag/TraceCodeBlock.java
@@ -3,12 +3,15 @@ package de.uni_freiburg.informatik.ultimate.plugins.analysis.reachingdefinitions
 import java.util.List;
 
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.CodeBlock;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
 
 public class TraceCodeBlock {
 
 	private final List<CodeBlock> mTrace;
 	private final CodeBlock mBlock;
 	private final int mIndex;
+	
+	private IPredicate mInterpolant;
 
 	public TraceCodeBlock(List<CodeBlock> trace, CodeBlock block, int index) {
 		mTrace = trace;
@@ -26,6 +29,14 @@ public class TraceCodeBlock {
 
 	public int getIndex() {
 		return mIndex;
+	}
+	
+	public void addInterpolant(IPredicate interpolant) {
+		this.mInterpolant = interpolant;
+	}
+	
+	public IPredicate getInterpolant() {
+		return mInterpolant;
 	}
 
 	@Override
