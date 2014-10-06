e7337b195198f9a7dce406ee70247a2c939e3333
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2014-10-06 09:51:45 +0000

    c translation:  - pragmas are ignored in svcomp mode now

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/svComp/SvComp14PreprocessorHandler.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/svComp/SvComp14PreprocessorHandler.java
@@ -1,6 +1,7 @@
 package de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.svComp;
 
 import org.eclipse.cdt.core.dom.ast.IASTPreprocessorIncludeStatement;
+import org.eclipse.cdt.core.dom.ast.IASTPreprocessorPragmaStatement;
 
 import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.PreprocessorHandler;
 import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.result.Result;
@@ -12,6 +13,13 @@ public class SvComp14PreprocessorHandler extends PreprocessorHandler {
 	@Override
 	public Result visit(Dispatcher main, IASTPreprocessorIncludeStatement node) {
 		// Ignore #include in our sv-comp mode
+		return new ResultSkip();
+	}
+
+	@Override
+	public Result visit(Dispatcher main, IASTPreprocessorPragmaStatement node) {
+		// TODO Auto-generated method stub
+		//return super.visit(main, node);
 		return new ResultSkip();
 	}
 	
