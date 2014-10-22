a36d759f475d2deca470cf91f591c13556c94843
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-10-22 16:17:39 +0000

    new release

--- releaseScripts/2014Chanh/createZip.sh
+++ releaseScripts/2014Chanh/createZip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir buchiAutomizer
-cp -a ../../source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* buchiAutomizer/
+cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* buchiAutomizer/
 cp LICENSE* buchiAutomizer/
-cp ../../examples/toolchains/BuchiAutomizerCWithBlockEncoding.xml buchiAutomizer/
+cp ../../trunk/examples/toolchains/BuchiAutomizerCWithBlockEncoding.xml buchiAutomizer/
 cp BuchiAutomizerDefaultSettings buchiAutomizer/
 cp buchiAutomizer.sh buchiAutomizer/
 cp Ultimate.ini buchiAutomizer/
