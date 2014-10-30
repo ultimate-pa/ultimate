14af789b9e08df9b8e02de7dc047c5a1be9e4331
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2014-10-30 04:04:47 +0000

    PQE USR more sound

--- trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/partialQuantifierElimination/XnfUsr.java
+++ trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/partialQuantifierElimination/XnfUsr.java
@@ -1,14 +1,17 @@
 package de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.partialQuantifierElimination;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.core.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.logic.ApplicationTerm;
 import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.ApplicationTermFinder;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.arrays.MultiDimensionalSelect;
 import de.uni_freiburg.informatik.ultimate.util.HashRelation;
 
@@ -35,24 +38,35 @@ public class XnfUsr extends XnfPartialQuantifierElimination {
 			Set<TermVariable> eliminatees) {
 		HashRelation<TermVariable, Term> var2arrays = new HashRelation<TermVariable, Term>();
 		HashRelation<TermVariable, Term> var2parameters = new HashRelation<TermVariable, Term>();
+		Set<TermVariable> blacklist = new HashSet<TermVariable>();
 		for (Term param : inputAtoms) {
-			List<MultiDimensionalSelect> slects = MultiDimensionalSelect.extractSelectDeep(param, false);
-			for (MultiDimensionalSelect mds : slects) {
-				Set<TermVariable> indexFreeVars = mds.getIndex().getFreeVars();
-				for (TermVariable tv : indexFreeVars) {
-					if (eliminatees.contains(tv)) {
-						var2arrays.addPair(tv, mds.getArray());
-						var2parameters.addPair(tv, param);
+			Set<ApplicationTerm> storeTerms = (new ApplicationTermFinder("store", true)).findMatchingSubterms(param);
+			if (storeTerms.isEmpty()) {
+				List<MultiDimensionalSelect> slects = MultiDimensionalSelect.extractSelectDeep(param, false);
+				for (MultiDimensionalSelect mds : slects) {
+					Set<TermVariable> indexFreeVars = mds.getIndex().getFreeVars();
+					for (TermVariable tv : indexFreeVars) {
+						if (eliminatees.contains(tv)) {
+							var2arrays.addPair(tv, mds.getArray());
+							var2parameters.addPair(tv, param);
+						}
 					}
 				}
+			} else {
+				//if there are store terms all occurring variables become
+				//blacklisted
+				blacklist.addAll(Arrays.asList(param.getFreeVars()));
 			}
+			
 		}
 		Set<Term> superfluousParams = new HashSet<Term>();
 		for (TermVariable eliminatee : var2arrays.getDomain()) {
-			if (var2arrays.getImage(eliminatee).size() == 1 &&
-					var2parameters.getImage(eliminatee).size() == 1) {
-				superfluousParams.addAll(var2parameters.getImage(eliminatee));
-				affectedEliminatees.add(eliminatee);
+			if (!blacklist.contains(eliminatee)) {
+				if (var2arrays.getImage(eliminatee).size() == 1 &&
+						var2parameters.getImage(eliminatee).size() == 1) {
+					superfluousParams.addAll(var2parameters.getImage(eliminatee));
+					affectedEliminatees.add(eliminatee);
+				}
 			}
 		}
 		ArrayList<Term> resultAtoms = new ArrayList<Term>();
