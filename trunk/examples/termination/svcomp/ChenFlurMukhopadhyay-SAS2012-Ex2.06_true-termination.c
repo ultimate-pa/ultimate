e90f6c5bae190c895f46cbe7785483210e988a21
Jan Leike <leike@informatik.uni-freiburg.de>
2014-10-31 02:45:52 +0000

    commited the wrong version of this file

--- trunk/examples/termination/svcomp/ChenFlurMukhopadhyay-SAS2012-Ex2.06_true-termination.c
+++ trunk/examples/termination/svcomp/ChenFlurMukhopadhyay-SAS2012-Ex2.06_true-termination.c
@@ -1,7 +1,7 @@
 /*
  * Date: 2014-06-08
  * Author: leike@informatik.uni-freiburg.de
- *
+ * Update: 2014-10-31
  *
  * This is Example 2.6 from the test suit used in
  *
@@ -9,8 +9,16 @@
  * Hong Yi Chen, Shaked Flur, and Supratik Mukhopadhyay.
  * SAS 2012.
  * 
- * The authors of the paper claim that this program is terminating, however
- * the program is nonterminating (e.g., initial state x=1 and y=1).
+ * The authors of the paper claim that this program is terminating,
+ * however the program only terminates over the integers and the rationals,
+ * not over the reals.
+ *
+ * There is an infinite execution staring with the eigenvector
+ * x = (sqrt(17) - 1) / 4
+ * y = 1
+ * with eigenvalue sqrt(17) - 1.
+ *
+ * The example was previously incorrectly classified.
  *
  * The test suite is available at the following URL.
  * https://tigerbytes2.lsu.edu/users/hchen11/lsl/LSL_benchmark.txt
