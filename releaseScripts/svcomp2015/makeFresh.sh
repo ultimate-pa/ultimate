6474b422ef4100008afef25b86c94ea2a19dfd0d
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2014-11-01 11:00:55 +0000

    - additional deployment script

---
+++ releaseScripts/svcomp2015/makeFresh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+	CURRENTUSER=$(whoami)
+else
+	CURRENTUSER="$1"
+fi
+
+svn up ../../.
+pushd ../../trunk/source/BA_MavenParentUltimate/
+mvn clean install -Pmaterialize
+popd
+
+./createZipAutomizer.sh
+./createZipCodeCheck.sh
+svn info ../../. > revision
+scp *.zip revision $CURRENTUSER@sotec.informatik.uni-freiburg.de:/export/server/httpd/ultimate/downloads/svcomp2015/.
+rm *.zip 
+rm revision
+
