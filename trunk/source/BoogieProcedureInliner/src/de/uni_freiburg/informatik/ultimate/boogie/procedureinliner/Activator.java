0b7164907d56ffd00c546fd6b2d845ec333f7c26
Claus Schaetzle <schaetzc@informatik.uni-freiburg.de>
2014-11-05 12:23:12 +0000

    Created base for procedure inliner

---
+++ trunk/source/BoogieProcedureInliner/src/de/uni_freiburg/informatik/ultimate/boogie/procedureinliner/Activator.java
@@ -0,0 +1,13 @@
+package de.uni_freiburg.informatik.ultimate.boogie.procedureinliner;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class Activator extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = BoogieProcedureInliner.class.getPackage().getName();
+	
+	// The plug-in name 
+	public static final String PLUGIN_NAME = "Boogie Procedure Inliner";
+
+}
---
+++ trunk/source/BoogieProcedureInliner/src/de/uni_freiburg/informatik/ultimate/boogie/procedureinliner/BoogieProcedureInliner.java
@@ -0,0 +1,85 @@
+package de.uni_freiburg.informatik.ultimate.boogie.procedureinliner;
+
+import java.util.List;
+
+import de.uni_freiburg.informatik.ultimate.access.IObserver;
+import de.uni_freiburg.informatik.ultimate.core.preferences.UltimatePreferenceInitializer;
+import de.uni_freiburg.informatik.ultimate.core.services.IToolchainStorage;
+import de.uni_freiburg.informatik.ultimate.core.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.ep.interfaces.IAnalysis;
+import de.uni_freiburg.informatik.ultimate.ep.interfaces.ITool.QueryKeyword;
+import de.uni_freiburg.informatik.ultimate.model.GraphType;
+import de.uni_freiburg.informatik.ultimate.result.GenericResult;
+
+/**
+ * 
+ * @author schaetzc@informatik.uni-freiburg.de
+ *
+ */
+public class BoogieProcedureInliner implements IAnalysis {
+
+	@Override
+	public GraphType getOutputDefinition() {
+		return null;
+	}
+
+	@Override
+	public boolean isGuiRequired() {
+		return false;
+	}
+
+	@Override
+	public QueryKeyword getQueryKeyword() {
+		return QueryKeyword.LAST;
+	}
+
+	@Override
+	public List<String> getDesiredToolID() {
+		return null;
+	}
+
+	@Override
+	public void setInputDefinition(GraphType graphType) {
+
+	}
+
+	@Override
+	public List<IObserver> getObservers() {
+		return null;
+	}
+
+	@Override
+	public void setToolchainStorage(IToolchainStorage storage) {
+		// TODO Auto-generated method stub
+		// #2
+	}
+
+	@Override
+	public void setServices(IUltimateServiceProvider services) {
+		// TODO Auto-generated method stub
+		// #1 (save it!)
+		//exceptions: services.getResultService().reportResult(getPluginID(), new GenericResult(getPluginID(), "", longDescription, severity));
+	}
+
+	@Override
+	public int init() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getPluginName() {
+		return Activator.PLUGIN_NAME;
+	}
+
+	@Override
+	public String getPluginID() {
+		return Activator.PLUGIN_ID;
+	}
+
+	@Override
+	public UltimatePreferenceInitializer getPreferences() {
+		return null;
+	}
+
+}
