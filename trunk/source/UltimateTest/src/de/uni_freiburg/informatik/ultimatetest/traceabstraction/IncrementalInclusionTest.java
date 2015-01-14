3cbe85bca271cf3ac80b668e414a04f27e21c53c
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-01-14 01:58:45 +0000

    documentation

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/IncrementalInclusionTest.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/IncrementalInclusionTest.java
@@ -8,13 +8,25 @@ import java.util.Collection;
 import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
 
 /**
- * Test for the different versions of incremental inclusion
+ * Test for the different versions of incremental inclusion.
+ * Run this as a "JUnit Plug-in Test".
+ * The results will be written to the following folder.
+ * trunk/source/UltimateTest/target/surefire-reports/de.uni_freiburg.informatik.ultimatetest.traceabstraction.IncrementalInclusionTest
+ * 
+ * Please remember to use "-ea" as VM argument for testing (because bugs will be
+ * found earlier).
+ * Please do not use "-ea" as VM argument for benchmarking (because settings with
+ * a lot of assert statements will have a disadvantage)
  * @author heizmanninformatik.uni-freiburg.de
  *
  */
 
 public class IncrementalInclusionTest extends
 		AbstractTraceAbstractionTestSuite {
+	/**
+	 * List of path. Ultimate will be run for each program that you find in
+	 * one of there paths.
+	 */
 	private static final String[] m_Programs = {
 //		"examples/programs/regression",
 //		"examples/programs/quantifier/",
@@ -25,6 +37,13 @@ public class IncrementalInclusionTest extends
 //		"examples/programs/real-life",
 	};
 	
+	/**
+	 * List of path to setting files. 
+	 * Ultimate will run on each program with each setting that is defined here.
+	 * The path are defined relative to the folder "trunk/examples/settings/",
+	 * because we assume that all settings files are in this folder.
+	 * 
+	 */
 	private static final String[] m_Settings = {
 		"automizer/incrementalInclusion/Difference.epf",
 		"automizer/incrementalInclusion/IncrementalInclusionViaDifference.epf",
@@ -34,10 +53,21 @@ public class IncrementalInclusionTest extends
 		"automizer/incrementalInclusion/IncrementalInclusion5.epf",
 	};
 	
+	/**
+	 * If set to true, files with the ending .bpl are taken into account,
+	 * otherwise files with this ending are ignored.
+	 */
 	private static final boolean m_Boogie = true;
+	/**
+	 * If set to true, files with the ending .c and .i are taken into account,
+	 * otherwise files with this ending are ignored.
+	 */
 	private static final boolean m_C = !true;
 
-	// Time out for each test case in milliseconds
+	/**
+	 * Time out for each test case in milliseconds.
+	 * This will override the timeout that is specified in the settings files.
+	 */
 	private final static int m_Timeout = 10 * 1000;
 	
 	@Override
