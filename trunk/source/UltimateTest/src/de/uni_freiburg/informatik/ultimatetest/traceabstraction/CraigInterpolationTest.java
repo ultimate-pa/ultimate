cc11c864eb65f34ecace891c0e540a3a13543be6
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-01-18 14:25:12 +0000

    test Craig interpolation

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/CraigInterpolationTest.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/CraigInterpolationTest.java
@@ -11,21 +11,24 @@ import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
  * @author heizmann@informatik.uni-freiburg.de
  *
  */
-public class All_TreeInterpolants extends
+public class CraigInterpolationTest extends
 		AbstractTraceAbstractionTestSuite {
 	private static final String[] m_Directories = {
-		"examples/programs/regression",
+//		"examples/programs/regression",
 //		"examples/programs/quantifier",
-//		"examples/programs/recursivePrograms",
+		"examples/programs/recursivePrograms",
 //		"examples/programs/toy"
 //		"examples/termination/AProVE"
+//		"examples/svcomp/recursive/",
 	};
 	
 	// Time out for each test case in milliseconds
-	private static int m_Timeout = 10000;
+	private static int m_Timeout = 100000;
 
 	private static final boolean s_Boogie_TreeInterpolants = true;
 	private static final boolean s_C_TreeInterpolants = true;
+	private static final boolean s_Boogie_NestedInterpolants = true;
+	private static final boolean s_C_NestedInterpolants = true;
 	
 	
 	@Override
@@ -36,8 +39,6 @@ public class All_TreeInterpolants extends
 					"automizer/TreeInterpolants.epf",
 				    m_Directories,
 				    new String[] {".bpl"},
-//				    "TraceAbstraction via tree interpolation",
-//				    "Boogie",
 				    m_Timeout);
 		} 
 		if (s_C_TreeInterpolants) {
@@ -46,8 +47,23 @@ public class All_TreeInterpolants extends
 					"automizer/TreeInterpolants.epf",
 				    m_Directories,
 				    new String[] {".c", ".i"},
-//				    "TraceAbstraction via tree interpolation",
-//				    "C",
+				    m_Timeout);
+		}
+		
+		if (s_Boogie_NestedInterpolants) {
+			addTestCases(
+					"AutomizerBpl.xml",
+					"automizer/NestedInterpolants.epf",
+				    m_Directories,
+				    new String[] {".bpl"},
+				    m_Timeout);
+		} 
+		if (s_C_NestedInterpolants) {
+			addTestCases(
+					"AutomizerC.xml",
+					"automizer/NestedInterpolants.epf",
+				    m_Directories,
+				    new String[] {".c", ".i"},
 				    m_Timeout);
 		}
 		return super.createTestCases();
