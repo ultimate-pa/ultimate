0d84a519d362d075f03d44bb39d4121f5ab5e3cd
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-01-20 12:19:47 +0000

    preferences new inclusion checks

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/IncrementalInclusionCegarLoop.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/IncrementalInclusionCegarLoop.java
@@ -14,8 +14,11 @@ import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.NestedWord;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.Accepts;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.IncrementalInclusionCheck2;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.IncrementalInclusionCheck3;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.IncrementalInclusionCheck3_2;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.IncrementalInclusionCheck4;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.IncrementalInclusionCheck4_2;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.IncrementalInclusionCheck5;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.IncrementalInclusionCheck5_2;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.RemoveUnreachable;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.incremental_inclusion.AbstractIncrementalInclusionCheck;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations.incremental_inclusion.InclusionViaDifference;
@@ -65,6 +68,13 @@ public class IncrementalInclusionCegarLoop extends BasicCegarLoop {
 		switch (m_LanguageOperation) {
 		case DIFFERENCE:
 			throw new AssertionError("wrong cegar loop for this");
+		case INCREMENTAL_INCLUSION_VIA_DIFFERENCE: {
+			m_InclusionCheck = new InclusionViaDifference(m_Services, 
+					m_StateFactoryForRefinement, 
+					m_PredicateFactoryInterpolantAutomata, 
+					(INestedWordAutomatonSimple) m_Abstraction);
+		}
+		break;
 		case INCREMENTAL_INCLUSION_2: {
 			List<INestedWordAutomatonSimple<CodeBlock, IPredicate>> empty = Collections.emptyList();
 			m_InclusionCheck = new IncrementalInclusionCheck2<CodeBlock, IPredicate>(
@@ -79,9 +89,23 @@ public class IncrementalInclusionCegarLoop extends BasicCegarLoop {
 					(INestedWordAutomatonSimple) m_Abstraction, empty);
 		}
 		break;
+		case INCREMENTAL_INCLUSION_3_2: {
+			List<INestedWordAutomatonSimple<CodeBlock, IPredicate>> empty = Collections.emptyList();
+			m_InclusionCheck = new IncrementalInclusionCheck3_2<CodeBlock, IPredicate>(
+					m_Services, m_StateFactoryForRefinement, 
+					(INestedWordAutomatonSimple) m_Abstraction, empty);
+		}
+		break;
 		case INCREMENTAL_INCLUSION_4: {
 			List<INestedWordAutomatonSimple<CodeBlock, IPredicate>> empty = Collections.emptyList();
 			m_InclusionCheck = new IncrementalInclusionCheck4<CodeBlock, IPredicate>(
+					m_Services, m_StateFactoryForRefinement, 
+					(INestedWordAutomatonSimple) m_Abstraction, empty);
+		}
+		break;
+		case INCREMENTAL_INCLUSION_4_2: {
+			List<INestedWordAutomatonSimple<CodeBlock, IPredicate>> empty = Collections.emptyList();
+			m_InclusionCheck = new IncrementalInclusionCheck4_2<CodeBlock, IPredicate>(
 					m_Services, m_StateFactoryForRefinement, 
 					(INestedWordAutomatonSimple) m_Abstraction, empty);
 		}
@@ -93,11 +117,11 @@ public class IncrementalInclusionCegarLoop extends BasicCegarLoop {
 					(INestedWordAutomatonSimple) m_Abstraction, empty);
 		}
 		break;
-		case INCREMENTAL_INCLUSION_VIA_DIFFERENCE: {
-			m_InclusionCheck = new InclusionViaDifference(m_Services, 
-					m_StateFactoryForRefinement, 
-					m_PredicateFactoryInterpolantAutomata, 
-					(INestedWordAutomatonSimple) m_Abstraction);
+		case INCREMENTAL_INCLUSION_5_2: {
+			List<INestedWordAutomatonSimple<CodeBlock, IPredicate>> empty = Collections.emptyList();
+			m_InclusionCheck = new IncrementalInclusionCheck5_2<CodeBlock, IPredicate>(
+					m_Services, m_StateFactoryForRefinement, 
+					(INestedWordAutomatonSimple) m_Abstraction, empty);
 		}
 		break;
 		default:
