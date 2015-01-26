121ed830f520e899cd750b587fad766f9b30f528
Vincent Langenfeld <langenfv@informatik.uni-freiburg.de>
2015-01-26 09:21:39 +0000

    ltl checker bug example

---
+++ trunk/examples/LTL/koskinen/ltlcmodelchecker-benchmarks/toylin1min_noLoopBugSample_unsafe_liveness.c
@@ -0,0 +1,30 @@
+// *************************************************************
+//
+//     Branching-time reasoning for infinite-state systems
+//
+//              Byron Cook * Eric Koskinen
+//                     July 2010
+//
+// *************************************************************
+
+// Benchmark: toylin1.c
+// Property: c > 5 => AF(resp > 5)
+
+//#Unsafe
+//@ ltl invariant positive: !<>AP(c > 5);
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern void __VERIFIER_assume() __attribute__ ((__noreturn__));
+extern int __VERIFIER_nondet_int() __attribute__ ((__noreturn__));
+
+int curr_serv;
+int c;
+curr_serv = 3;
+c = 0;
+
+int main() {
+  while(curr_serv > 0) {
+	  curr_serv--;
+	  c++; //can get > 3
+  }
+}
+
