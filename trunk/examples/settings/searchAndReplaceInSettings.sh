d62ebb20b7c81d59b57847cf2c088cb15959c36f
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-02-12 03:15:17 +0000

    script for updating setting files

---
+++ trunk/examples/settings/searchAndReplaceInSettings.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Script for replacing strings in all our settings files.
+# You might want so use this after you updated preferences.
+# This script might have some problems with special characters.
+# Author: Matthias Heizmann
+# Date: 2015-02-11
+
+
+#quote oldstring
+OLDSTRING=$(sed 's/\\/\\\\/g' <<< "$1")
+NEWSTRING="$2"
+echo "Replacing the OLDSTRING with NEWSTRING in each .epf file"
+echo "OLDSTRING: $1"
+echo "NEWSTRING: $2"
+#use comma as delimiter for sed because is hopefully does not occur in string
+find . -name "*.epf" -exec sed -i -e "s,${OLDSTRING},${NEWSTRING}," {} \;
+
+
