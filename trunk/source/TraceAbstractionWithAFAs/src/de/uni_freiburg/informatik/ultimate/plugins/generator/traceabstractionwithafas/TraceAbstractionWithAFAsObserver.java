da5560476536861b0d4e21afdd12cfeaa57a0c92
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2015-02-17 16:36:45 +0000

    TraceAbstractionWithAFAs:  added basic result reporting

--- trunk/source/TraceAbstractionWithAFAs/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstractionwithafas/TraceAbstractionWithAFAsObserver.java
+++ trunk/source/TraceAbstractionWithAFAs/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstractionwithafas/TraceAbstractionWithAFAsObserver.java
@@ -4,17 +4,36 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import de.uni_freiburg.informatik.ultimate.access.BaseObserver;
 import de.uni_freiburg.informatik.ultimate.automata.ExampleNWAFactory;
 import de.uni_freiburg.informatik.ultimate.core.services.IUltimateServiceProvider;
 import de.uni_freiburg.informatik.ultimate.model.IElement;
+import de.uni_freiburg.informatik.ultimate.model.boogie.ast.Expression;
+import de.uni_freiburg.informatik.ultimate.model.location.ILocation;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstractionwithafas.Activator;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.RcfgProgramExecution;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.CodeBlock;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.ProgramPoint;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.RcfgElement;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.RootAnnot;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.RootNode;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.AbstractCegarLoop.Result;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.TraceAbstractionBenchmarks;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.SmtManager;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.preferences.TAPreferences;
+import de.uni_freiburg.informatik.ultimate.result.AllSpecificationsHoldResult;
+import de.uni_freiburg.informatik.ultimate.result.BenchmarkResult;
+import de.uni_freiburg.informatik.ultimate.result.CounterExampleResult;
+import de.uni_freiburg.informatik.ultimate.result.GenericResult;
+import de.uni_freiburg.informatik.ultimate.result.IResult;
+import de.uni_freiburg.informatik.ultimate.result.PositiveResult;
+import de.uni_freiburg.informatik.ultimate.result.ResultUtil;
+import de.uni_freiburg.informatik.ultimate.result.TimeoutResultAtElement;
+import de.uni_freiburg.informatik.ultimate.result.UnprovableResult;
+import de.uni_freiburg.informatik.ultimate.result.IResultWithSeverity.Severity;
+import de.uni_freiburg.informatik.ultimate.util.csv.ICsvProviderProvider;
 
 /**
  * Auto-Generated Stub for the plug-in's Observer
@@ -30,8 +49,11 @@ public class TraceAbstractionWithAFAsObserver extends BaseObserver {
 
 	private final IUltimateServiceProvider mServices;
 
+	private final Logger mLogger;
+
 	public TraceAbstractionWithAFAsObserver(IUltimateServiceProvider services) {
 		mServices = services;
+		mLogger = mServices.getLoggingService().getLogger(Activator.s_PLUGIN_ID);
 	}
 
 	@Override
@@ -61,8 +83,89 @@ public class TraceAbstractionWithAFAsObserver extends BaseObserver {
 				errNodesOfAllProc, taPrefs.interpolation(), taPrefs.computeHoareAnnotation(), mServices);
 
 		Result result = cegarLoop.iterate();
-
+		
+		
+		
+		if (result == Result.SAFE) {
+			reportPositiveResults(errNodesOfAllProc);
+		} else if (result == Result.UNSAFE) {
+			reportCounterexampleResult(cegarLoop.getRcfgProgramExecution());
+		} else {
+			String shortDescription = "Unable to decide if program is safe!";
+			String longDescription = "Unable to decide if program is safe!";
+			GenericResult genResult = new GenericResult(Activator.s_PLUGIN_NAME, shortDescription, longDescription,
+					Severity.INFO);
+			mServices.getResultService().reportResult(Activator.s_PLUGIN_ID, genResult);
+		}
 		return false;
+	}
+	
+	
+	private <T> void reportBenchmark(ICsvProviderProvider<T> benchmark) {
+		String shortDescription = "Ultimate CodeCheck benchmark data";
+		BenchmarkResult<T> res = new BenchmarkResult<T>(Activator.s_PLUGIN_NAME, shortDescription, benchmark);
+		// s_Logger.warn(res.getLongDescription());
+
+		reportResult(res);
+	}
+
+	private void reportPositiveResults(Collection<ProgramPoint> errorLocs) {
+		final String longDescription;
+		if (errorLocs.isEmpty()) {
+			longDescription = "We were not able to verify any"
+					+ " specifiation because the program does not contain any specification.";
+		} else {
+			longDescription = errorLocs.size() + " specifications checked. All of them hold";
+			for (ProgramPoint errorLoc : errorLocs) {
+				PositiveResult<RcfgElement> pResult = new PositiveResult<RcfgElement>(Activator.s_PLUGIN_NAME,
+						errorLoc, mServices.getBacktranslationService());
+				reportResult(pResult);
+			}
+		}
+		AllSpecificationsHoldResult result = new AllSpecificationsHoldResult(Activator.s_PLUGIN_NAME, longDescription);
+		reportResult(result);
+		mLogger.info(result.getShortDescription() + " " + result.getLongDescription());
+	}
+
+	private void reportCounterexampleResult(RcfgProgramExecution pe) {
+		if (pe.isOverapproximation()) {
+			reportUnproveableResult(pe);
+			return;
+		}
+
+		reportResult(new CounterExampleResult<RcfgElement,CodeBlock, Expression>(getErrorPP(pe), Activator.s_PLUGIN_NAME,
+				mServices.getBacktranslationService(), pe));
+	}
+
+	private void reportUnproveableResult(RcfgProgramExecution pe) {
+		ProgramPoint errorPP = getErrorPP(pe);
+		UnprovableResult<RcfgElement, CodeBlock, Expression> uknRes = new UnprovableResult<RcfgElement, CodeBlock, Expression>(
+				Activator.s_PLUGIN_NAME, errorPP, mServices.getBacktranslationService(), pe);
+		reportResult(uknRes);
+	}
+
+	public ProgramPoint getErrorPP(RcfgProgramExecution rcfgProgramExecution) {
+		int lastPosition = rcfgProgramExecution.getLength() - 1;
+		CodeBlock last = rcfgProgramExecution.getTraceElement(lastPosition).getTraceElement();
+		ProgramPoint errorPP = (ProgramPoint) last.getTarget();
+		return errorPP;
+	}
+	
+	private void reportTimoutResult(Collection<ProgramPoint> errorLocs) {
+		for (ProgramPoint errorLoc : errorLocs) {
+			ILocation origin = errorLoc.getBoogieASTNode().getLocation().getOrigin();
+			String timeOutMessage = "Unable to prove that "
+					+ ResultUtil.getCheckedSpecification(errorLoc).getPositiveMessage();
+			timeOutMessage += " (line " + origin.getStartLine() + ")";
+			TimeoutResultAtElement<RcfgElement> timeOutRes = new TimeoutResultAtElement<RcfgElement>(errorLoc,
+					Activator.s_PLUGIN_NAME, mServices.getBacktranslationService(),
+					timeOutMessage);
+			reportResult(timeOutRes);
+		}
+	}
+
+	private void reportResult(IResult res) {
+		mServices.getResultService().reportResult(Activator.s_PLUGIN_ID, res);
 	}
 
 	/**
