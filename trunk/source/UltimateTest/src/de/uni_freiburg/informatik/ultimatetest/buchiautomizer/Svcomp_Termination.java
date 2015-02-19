fbfce6e7c70f0d15906403c2dfd28886b6fc272a
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-02-19 13:50:03 +0000

    svcomp tests

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/buchiautomizer/Svcomp_Termination.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/buchiautomizer/Svcomp_Termination.java
@@ -14,42 +14,15 @@ import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
 public class Svcomp_Termination extends AbstractBuchiAutomizerTestSuite {
 	private static final DirectoryFileEndingsPair[] m_DirectoryFileEndingsPairs = {
 
-//		/*** Category ??? ***/
+//		/*** Category 12. Termination ***/
+		new DirectoryFileEndingsPair("examples/svcomp/termination-crafted/", new String[]{ ".c" }) ,
+		new DirectoryFileEndingsPair("examples/svcomp/termination-crafted-lit/", new String[]{ ".c" }) ,
 		new DirectoryFileEndingsPair("examples/svcomp/termination-memory-alloca/", new String[]{ ".i" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/bitvector-regression/", new String[]{ ".i", ".c" }) ,
-//		
-//		/*** Category 4. Control Flow and Integer Variables ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/ntdrivers-simplified/", new String[]{".c" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/ssh-simplified/", new String[]{".c" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/locks/", new String[]{".c" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/locks/", new String[]{".c" }) ,
-//		
-//		new DirectoryFileEndingsPair("examples/svcomp/loops/", new String[]{".i"}) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/loop-acceleration/", new String[]{".c" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/loop-invgen/", new String[]{".i"}) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/loop-lit/", new String[]{ ".i", ".c" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/loop-new/", new String[]{".i"}) ,
-//		
-//		new DirectoryFileEndingsPair("examples/svcomp/eca/", new String[]{".c" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/product-lines/", new String[]{".c" }) ,
-//		
-//		/*** Category 6. Heap Manipulation / Dynamic Data Structures ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/heap-manipulation/", new String[]{ ".i" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/list-properties/", new String[]{ ".i" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/ldv-regression/", new String[]{ ".i" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/ddv-machzwd/", new String[]{ ".i" }) ,
-//
-//		/*** Category 8. Recursive ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/recursive/", new String[]{ ".c" }) ,
-//		
-//		/*** Category 9. Recursive ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/systemc/", new String[]{ ".c" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/seq-mthreaded/", new String[]{ ".c" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/seq-pthread/", new String[]{ ".i" }) ,
+		new DirectoryFileEndingsPair("examples/svcomp/termination-numeric/", new String[]{ ".c" }) ,
 	};
 
 	// Time out for each test case in milliseconds
-	private static long m_Timeout = 20 * 1000;
+	private static long m_Timeout = 60 * 1000;
 
 	private static final boolean s_UseTasimp = true;
 	private static final String s_TasimpSetting = "buchiAutomizer/staged300Forward-Z3-Tasimp.epf";
