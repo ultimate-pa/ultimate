6d715b774aba9fe3c403a84d88514908a8326fb0
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-03-06 16:22:30 +0000

    witness checking continued

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/witnesschecking/WitnessAutomatonLetter.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/witnesschecking/WitnessAutomatonLetter.java
@@ -1,5 +1,97 @@
 package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.witnesschecking;
 
+import de.uni_freiburg.informatik.ultimate.model.boogie.ast.Statement;
+import de.uni_freiburg.informatik.ultimate.model.location.ILocation;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.Call;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.CodeBlock;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.InterproceduralSequentialComposition;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.ParallelComposition;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.Return;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.SequentialComposition;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.StatementSequence;
+
 public class WitnessAutomatonLetter {
+	
+	public boolean isCompatible(CodeBlock cb) {
+		if (cb instanceof Call) {
+			Call call = (Call) cb;
+			return isCompatible(call);
+		} else if (cb instanceof InterproceduralSequentialComposition) {
+			InterproceduralSequentialComposition isc = (InterproceduralSequentialComposition) cb;
+			return isCompatible(isc);
+		} else if (cb instanceof ParallelComposition) {
+			ParallelComposition pc = (ParallelComposition) cb;
+			return isCompatible(pc);
+		} else if (cb instanceof Return) {
+			Return ret = (Return) cb;
+			return isCompatible(ret);
+		} else if (cb instanceof SequentialComposition) {
+			SequentialComposition sc = (SequentialComposition) cb;
+			return isCompatible(sc);
+		} else if (cb instanceof StatementSequence) {
+			StatementSequence ss = (StatementSequence) cb;
+			return isCompatible(ss);
+		} else {
+			throw new AssertionError("unknown type of CodeBlock");
+		}
+	}
+
+	
+	boolean isCompatible(Call call) {
+		return isCompatible(call.getCallStatement());
+	}
+	
+	boolean isCompatible(InterproceduralSequentialComposition isc) {
+		for (CodeBlock cb : isc.getCodeBlocks()) {
+			if (isCompatible(cb)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+
+
+	boolean isCompatible(ParallelComposition pc) {
+		for (CodeBlock cb : pc.getCodeBlocks()) {
+			if (isCompatible(cb)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	boolean isCompatible(Return ret) {
+		return isCompatible(ret.getPayload().getLocation());
+	}
+	
+	boolean isCompatible(SequentialComposition sc) {
+		for (CodeBlock cb : sc.getCodeBlocks()) {
+			if (isCompatible(cb)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	boolean isCompatible(StatementSequence ss) {
+		for (Statement st : ss.getStatements()) {
+			if (isCompatible(st)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	boolean isCompatible(Statement st) {
+		return isCompatible(st.getLocation());
+	}
+	
+	
+
+	private boolean isCompatible(ILocation location) {
+		//something with line numbers
+		return false;
+	}
 
 }
