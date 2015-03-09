e43e6cec8f5c87f2a2078d9fb181a30d6c61bd89
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-03-09 19:16:27 +0000

    rollback unintended commit

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/buchiautomizer/Svcomp_Termination.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/buchiautomizer/Svcomp_Termination.java
@@ -15,14 +15,14 @@ public class Svcomp_Termination extends AbstractBuchiAutomizerTestSuite {
 	private static final DirectoryFileEndingsPair[] m_DirectoryFileEndingsPairs = {
 
 //		/*** Category 12. Termination ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/termination-crafted/", new String[]{ ".c" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/termination-crafted-lit/", new String[]{ ".c" }) ,
+		new DirectoryFileEndingsPair("examples/svcomp/termination-crafted/", new String[]{ ".c" }) ,
+		new DirectoryFileEndingsPair("examples/svcomp/termination-crafted-lit/", new String[]{ ".c" }) ,
 		new DirectoryFileEndingsPair("examples/svcomp/termination-memory-alloca/", new String[]{ ".i" }) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/termination-numeric/", new String[]{ ".c" }) ,
+		new DirectoryFileEndingsPair("examples/svcomp/termination-numeric/", new String[]{ ".c" }) ,
 	};
 
 	// Time out for each test case in milliseconds
-	private static long m_Timeout = 10 * 1000;
+	private static long m_Timeout = 60 * 1000;
 
 	private static final boolean s_UseTasimp = true;
 	private static final String s_TasimpSetting = "buchiAutomizer/staged300Forward-Z3-Tasimp.epf";
