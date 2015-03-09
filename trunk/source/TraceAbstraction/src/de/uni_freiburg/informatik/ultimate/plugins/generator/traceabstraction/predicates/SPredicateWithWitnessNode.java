7187a2df53f7b0340c2c6b2fd42ce1d5710c1aa7
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-03-09 19:17:10 +0000

    predicate with witness

---
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/predicates/SPredicateWithWitnessNode.java
@@ -0,0 +1,38 @@
+package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates;
+
+import java.util.Set;
+
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.model.boogie.BoogieVar;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.ProgramPoint;
+import de.uni_freiburg.informatik.ultimate.witnessparser.graph.WitnessNode;
+
+public class SPredicateWithWitnessNode extends SPredicate {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3793267934783743767L;
+	private final WitnessNode m_WitnessNode;
+	
+	protected SPredicateWithWitnessNode(ProgramPoint programPoint,
+			int serialNumber, String[] procedures, Term term,
+			Set<BoogieVar> vars, Term closedFormula, WitnessNode witnessNode) {
+		super(programPoint, serialNumber, procedures, term, vars, closedFormula);
+		m_WitnessNode = witnessNode;
+	}
+	
+	@Override
+	public String toString() {
+		String result = super.m_SerialNumber + "#";
+		result += "(";
+//		if (m_ProgramPoint != null) {
+			result += m_ProgramPoint.getPosition();
+//		}
+		result += ",";
+		result += m_WitnessNode.getName();
+		result += ")";
+		return result;
+	}
+
+}
