8332c1cafe68b5d028f950b37e02900769a53a02
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-03-11 14:36:12 +0000

    release script

---
+++ releaseScripts/2015DavidMonniaux/createZip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir buchiAutomizer
+cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E3/linux/gtk/x86_64/* buchiAutomizer/
+cp LICENSE* buchiAutomizer/
+cp ../../trunk/examples/toolchains/BuchiAutomizerCWithBlockEncoding.xml buchiAutomizer/
+cp BuchiAutomizerDefaultSettings buchiAutomizer/
+cp buchiAutomizer.sh buchiAutomizer/
+cp Ultimate.ini buchiAutomizer/
+cp README buchiAutomizer/
+zip UltimateCommandline.zip -r buchiAutomizer/*
