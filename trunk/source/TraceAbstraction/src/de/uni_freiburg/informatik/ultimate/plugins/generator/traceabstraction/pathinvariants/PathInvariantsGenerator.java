a71100f62fe0465e39f866665542a69386801a93
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-03-12 19:27:50 +0000

    some notes for David and Dirk

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/pathinvariants/PathInvariantsGenerator.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/pathinvariants/PathInvariantsGenerator.java
@@ -8,8 +8,26 @@ import org.apache.log4j.Logger;
 import de.uni_freiburg.informatik.ultimate.automata.Word;
 import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.NestedRun;
 import de.uni_freiburg.informatik.ultimate.core.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.lassoranker.LinearTransition;
+import de.uni_freiburg.informatik.ultimate.lassoranker.exceptions.TermException;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.AddAxioms;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.DNF;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.MatchInVars;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.RemoveNegation;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.RewriteBooleans;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.RewriteDivision;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.RewriteEquality;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.RewriteIte;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.RewriteStrictInequalities;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.RewriteTrueFalse;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.SimplifyPreprocessor;
+import de.uni_freiburg.informatik.ultimate.lassoranker.preprocessors.TransitionPreProcessor;
+import de.uni_freiburg.informatik.ultimate.lassoranker.variables.TransFormulaLR;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.ModifiableGlobalVariableManager;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.TransFormula;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.TermVarsProc;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.CodeBlock;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.ProgramPoint;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.Activator;
@@ -108,6 +126,18 @@ public class PathInvariantsGenerator implements IInterpolantGenerator {
 		// 1. Project run into internal.ControlFlowGraph
 		// 2. Use CFGInvariantsGenerator on CFG with pre- and postcondition
 		// 3. Store result in m_Interpolants
+		
+		// Matthias: You obtain the transition formulas as follows. 
+		m_Run.getSymbol(0).getTransitionFormula(); 
+
+		
+		// Matthias: Given a term, that represents a set of program states, you
+		// obtain the corresponding IPredicate as follows.
+		// Each free variable in the term has to be bv.getTermVariable() for
+		// some BoogieVar bv.
+		Term term = null;
+		m_PredicateUnifier.getOrConstructPredicate(
+				TermVarsProc.computeTermVarsProc(term, m_SmtManager.getBoogie2Smt()));
 		throw new UnsupportedOperationException("Not implemented.");
 	}
 
