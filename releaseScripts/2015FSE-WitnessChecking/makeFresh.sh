3d711224e95ad5d6acf1f822b3cde606c0b247a2
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-03-15 21:34:39 +0000

    upload script

--- releaseScripts/2015FSE-WitnessChecking/makeFresh.sh
+++ releaseScripts/2015FSE-WitnessChecking/makeFresh.sh
@@ -15,10 +15,11 @@ popd
 svn info ../../. > revision
 REVISION=`svn info ../../. | grep '^Revision:' | sed -e 's/^Revision: //'`
 awk -v rev=$REVISION '/svnRevNumber = '"'"'.*'"'"'/ { print "svnRevNumber = '"'"'" rev "'"'"'"; next }1' Ultimate.py > Ultimate.py.tmp && mv Ultimate.py.tmp Ultimate.py
+awk -v rev=$REVISION '/svnRevNumber = '"'"'.*'"'"'/ { print "svnRevNumber = '"'"'" rev "'"'"'"; next }1' UltimateWitnessChecker.py > UltimateWitnessChecker.py.tmp && mv UltimateWitnessChecker.py.tmp UltimateWitnessChecker.py
 
 ./createZipAutomizer.sh
-./createZipCodeCheck.sh
-scp *.zip revision $CURRENTUSER@sotec.informatik.uni-freiburg.de:/export/server/httpd/ultimate/downloads/svcomp2015/.
+#./createZipCodeCheck.sh
+scp *.zip revision $CURRENTUSER@sotec.informatik.uni-freiburg.de:/export/server/httpd/ultimate/downloads/2015witness/.
 rm *.zip 
 rm revision
 
