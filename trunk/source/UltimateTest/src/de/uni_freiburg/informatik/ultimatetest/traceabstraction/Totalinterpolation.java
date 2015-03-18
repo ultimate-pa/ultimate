deb17f66ebbf7c235f405d7ce0e0da150aa33f69
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-03-18 19:09:41 +0000

    update test suite

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/Totalinterpolation.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/Totalinterpolation.java
@@ -13,17 +13,65 @@ import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
  */
 public class Totalinterpolation extends
 		AbstractTraceAbstractionTestSuite {
-	private static final String[] m_Programs = { 
-//		"examples/programs/regression",
-//		"examples/programs/toy",
-//		"examples/programs/recursivePrograms",
+	/**
+	 * Limit the number of files per directory.
+	 */
+	private static int m_FilesPerDirectoryLimit = Integer.MAX_VALUE;
+	
+	private static final DirectoryFileEndingsPair[] m_SVCOMP_Examples = {
+//		/*** Category 1. Arrays ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/array-examples/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		
+//		/*** Category 2. Bit Vectors ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/bitvector/", new String[]{ ".i", ".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/bitvector-regression/", new String[]{ ".i", ".c" }, m_FilesPerDirectoryLimit) ,
+//		
+//		/*** Category 4. Control Flow and Integer Variables ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/ntdrivers-simplified/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/ssh-simplified/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/locks/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+//		
+//		new DirectoryFileEndingsPair("examples/svcomp/loops/", new String[]{".i"}) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/loop-acceleration/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/loop-invgen/", new String[]{".i"}, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/loop-lit/", new String[]{ ".i", ".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/loop-new/", new String[]{".i"}, m_FilesPerDirectoryLimit) ,
+//		
+		new DirectoryFileEndingsPair("examples/svcomp/eca-rers2012/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/product-lines/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+//		
+//		/*** Category 6. Heap Manipulation / Dynamic Data Structures ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/heap-manipulation/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/list-properties/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/ldv-regression/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/ddv-machzwd/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		
+//		/*** Category 7. Memory Safety ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/memsafety/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/list-ext-properties/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/memory-alloca/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/memory-unsafe/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//
+//		/*** Category 8. Recursive ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/recursive/", new String[]{ ".c" }, m_FilesPerDirectoryLimit) ,
+//		
+//		/*** Category 9. Sequentialized Concurrent Programs ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/systemc/", new String[]{ ".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/seq-mthreaded/", new String[]{ ".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/seq-pthread/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+	};
+	
+	
+	private static final String[] m_UltimateRepository = {
+//		"examples/programs/nonlinearArithmetic",
+//		"examples/programs/quantifier",
 //		"examples/programs/random",
-//		"examples/programs/scaleable",
 //		"examples/programs/real-life",
-//		"examples/programs/reals"
-//		"examples/svcomp/ssh-simplified",
-//		"examples/svcomp/systemc"
-		"examples/svcomp/eca-rers2012",
+//		"examples/programs/reals",
+//		"examples/programs/recursivePrograms",
+//		"examples/programs/regression",
+//		"examples/programs/scalable",
+//		"examples/programs/toy",
 	};
 	
 	/**
@@ -51,29 +99,29 @@ public class Totalinterpolation extends
 		return 5 * 60 * 1000;
 	}
 
-	private static final boolean m_Boogie = true;
-	private static final boolean m_C = true;
 	
 	@Override
 	public Collection<UltimateTestCase> createTestCases() {
 		for (String setting : m_Settings) {
-			if (m_Boogie) {
-				addTestCases(
-						"AutomizerBpl.xml",
-						setting,
-						m_Programs,
-						new String[] {".bpl"});
-			}
-			if (m_C) {
-				addTestCases(
-						"AutomizerC.xml",
-						setting,
-						m_Programs,
-						new String[] {".c", ".i"});
-			}
+			addTestCases("AutomizerC.xml", 
+					setting, 
+					m_SVCOMP_Examples);
 		}
 		
-
+		for (String setting : m_Settings) {
+			addTestCases(
+					"AutomizerBpl.xml",
+					setting,
+					m_UltimateRepository,
+				    new String[] {".bpl"});
+		}
+		for (String setting : m_Settings) {
+			addTestCases(
+					"AutomizerC.xml",
+					setting,
+					m_UltimateRepository,
+				    new String[] {".c", ".i"});
+		}
 		return super.createTestCases();
 	}
 }
