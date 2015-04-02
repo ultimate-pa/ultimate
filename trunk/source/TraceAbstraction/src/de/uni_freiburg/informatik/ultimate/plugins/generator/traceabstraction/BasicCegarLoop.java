a04b0de4134bbc89722a64b568ad23e56ea68a03
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-04-02 09:25:47 +0000

    missing break

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/BasicCegarLoop.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/BasicCegarLoop.java
@@ -260,6 +260,7 @@ public class BasicCegarLoop extends AbstractCegarLoop {
 					new TreeMap<Integer, IPredicate>(), (NestedRun<CodeBlock, IPredicate>) m_Counterexample,
 					m_SmtManager, m_ModGlobVarManager, m_AssertCodeBlocksIncrementally, m_Services, m_ToolchainStorage, true,
 					predicateUnifier);
+			break;
 		default:
 			throw new UnsupportedOperationException("unsupported interpolation");
 		}
