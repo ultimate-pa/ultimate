3f189a89331b97b8a3a052cfa2fa1a51e832688f
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2015-04-09 13:22:57 +0000

    added two classe  - NestedMap4.java    --> straightforward extension of NestedMap3.java  - IsContained.java    --> a one-valued enum used for replacing NestedMap3<T1,T2,Set<T3>> through the more elegant NestedMap4<T1,T2,T3,IsContained>

---
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/relation/IsContained.java
@@ -0,0 +1,8 @@
+package de.uni_freiburg.informatik.ultimate.util.relation;
+
+/**
+ * One-valued enum (can be used for representing a Set<T> as a Map<T,IsContained>)
+ */
+public enum IsContained {
+ IsContained;
+}
