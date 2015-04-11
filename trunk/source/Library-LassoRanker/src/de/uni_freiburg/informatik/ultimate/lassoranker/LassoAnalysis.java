310ef6ba6d81f67667c91592ce1653d583fda007
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-04-11 13:27:55 +0000

    use LassoPartitioneer again after rewrite arrays

--- trunk/source/Library-LassoRanker/src/de/uni_freiburg/informatik/ultimate/lassoranker/LassoAnalysis.java
+++ trunk/source/Library-LassoRanker/src/de/uni_freiburg/informatik/ultimate/lassoranker/LassoAnalysis.java
@@ -312,7 +312,8 @@ public class LassoAnalysis {
 //						mServices
 //				),
 				new RewriteArrays2(true, m_stem_transition, m_loop_transition, mServices, m_ArrayIndexSupportingInvariants),
-//				new LassoPartitioneer(mServices),
+				new StemAndLoopPreProcessor(new MatchInVars(m_Boogie2SMT.getVariableManager())),
+				new LassoPartitioneer(mServices),
 				new StemAndLoopPreProcessor(new RewriteDivision(lassoBuilder.getReplacementVarFactory())),
 				new StemAndLoopPreProcessor(new RewriteBooleans(lassoBuilder.getReplacementVarFactory(), lassoBuilder.getScript())),
 				new StemAndLoopPreProcessor(new RewriteIte()),
