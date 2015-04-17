efa7a58a42c8e4846974430654081398311790e1
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-04-17 17:28:26 +0000

    incremental inclusion test with inlining and SV-COMP examples

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/IncrementalInclusionTest.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/traceabstraction/IncrementalInclusionTest.java
@@ -6,6 +6,7 @@ package de.uni_freiburg.informatik.ultimatetest.traceabstraction;
 import java.util.Collection;
 
 import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
+import de.uni_freiburg.informatik.ultimatetest.AbstractModelCheckerTestSuite.DirectoryFileEndingsPair;
 
 /**
  * Test for the different versions of incremental inclusion.
@@ -23,16 +24,58 @@ import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
 
 public class IncrementalInclusionTest extends
 		AbstractTraceAbstractionTestSuite {
+	
+	
+	/** Limit the number of files per directory that are used. */
+	private static int m_FilesPerDirectoryLimit = 1;
+	
+	private static final DirectoryFileEndingsPair[] s_SVCOMP_Programs = {
+//		/*** Category 1. Arrays ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/array-examples/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		
+//		/*** Category 2. Bit Vectors ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/bitvector/", new String[]{ ".i", ".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/bitvector-regression/", new String[]{ ".i", ".c" }, m_FilesPerDirectoryLimit) ,
+//		
+//		/*** Category 4. Control Flow and Integer Variables ***/
+		new DirectoryFileEndingsPair("examples/svcomp/ntdrivers-simplified/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/ssh-simplified/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/locks/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+		
+		new DirectoryFileEndingsPair("examples/svcomp/loops/", new String[]{".i"}, m_FilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/loop-acceleration/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/loop-invgen/", new String[]{".i"}, m_FilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/loop-lit/", new String[]{ ".i", ".c" }, m_FilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/loop-new/", new String[]{".i"}, m_FilesPerDirectoryLimit) ,
+//		
+//		new DirectoryFileEndingsPair("examples/svcomp/eca-rers2012/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/product-lines/", new String[]{".c" }, m_FilesPerDirectoryLimit) ,
+//		
+//		/*** Category 6. Heap Manipulation / Dynamic Data Structures ***/
+		new DirectoryFileEndingsPair("examples/svcomp/heap-manipulation/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/list-properties/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/ldv-regression/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/ddv-machzwd/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+//		
+//
+//		/*** Category 8. Recursive ***/
+//		new DirectoryFileEndingsPair("examples/svcomp/recursive/", new String[]{ ".c" }, m_FilesPerDirectoryLimit) ,
+//		
+//		/*** Category 9. Sequentialized Concurrent Programs ***/
+		new DirectoryFileEndingsPair("examples/svcomp/systemc/", new String[]{ ".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/seq-mthreaded/", new String[]{ ".c" }, m_FilesPerDirectoryLimit) ,
+//		new DirectoryFileEndingsPair("examples/svcomp/seq-pthread/", new String[]{ ".i" }, m_FilesPerDirectoryLimit) ,
+	};
 	/**
 	 * List of path. Ultimate will be run for each program that you find in
 	 * one of there paths.
 	 */
-	private static final String[] m_Programs = {
+	private static final String[] s_UltimateRepository_Programs = {
 //		"examples/programs/regression",
 //		"examples/programs/quantifier/",
 //		"examples/programs/quantifier/regression",
 //		"examples/programs/toy",
-		"examples/programs/random",
+//		"examples/programs/random",
 //		"examples/programs/scaleable",
 //		"examples/programs/real-life",
 	};
@@ -44,16 +87,16 @@ public class IncrementalInclusionTest extends
 	 * because we assume that all settings files are in this folder.
 	 * 
 	 */
-	private static final String[] m_Settings = {
+	private static final String[] s_Settings = {
 		"automizer/incrementalInclusion/Difference.epf",
-		"automizer/incrementalInclusion/IncrementalInclusionViaDifference.epf",
-		"automizer/incrementalInclusion/IncrementalInclusion2.epf",
-		"automizer/incrementalInclusion/IncrementalInclusion3.epf",
-		"automizer/incrementalInclusion/IncrementalInclusion3_2.epf",
-		"automizer/incrementalInclusion/IncrementalInclusion4.epf",
-		"automizer/incrementalInclusion/IncrementalInclusion4_2.epf",
-		"automizer/incrementalInclusion/IncrementalInclusion5.epf",
-		"automizer/incrementalInclusion/IncrementalInclusion5_2.epf",
+//		"automizer/incrementalInclusion/IncrementalInclusionViaDifference.epf",
+//		"automizer/incrementalInclusion/IncrementalInclusion2.epf",
+//		"automizer/incrementalInclusion/IncrementalInclusion3.epf",
+//		"automizer/incrementalInclusion/IncrementalInclusion3_2.epf",
+//		"automizer/incrementalInclusion/IncrementalInclusion4.epf",
+//		"automizer/incrementalInclusion/IncrementalInclusion4_2.epf",
+//		"automizer/incrementalInclusion/IncrementalInclusion5.epf",
+//		"automizer/incrementalInclusion/IncrementalInclusion5_2.epf",
 //		"automizer/incrementalInclusion/nonDeterministic/Difference.epf",
 //		"automizer/incrementalInclusion/nonDeterministic/IncrementalInclusionViaDifference.epf",
 //		"automizer/incrementalInclusion/nonDeterministic/IncrementalInclusion2.epf",
@@ -65,42 +108,36 @@ public class IncrementalInclusionTest extends
 //		"automizer/incrementalInclusion/nonDeterministic/IncrementalInclusion5_2.epf",
 	};
 	
-	/**
-	 * If set to true, files with the ending .bpl are taken into account,
-	 * otherwise files with this ending are ignored.
-	 */
-	private static final boolean m_Boogie = true;
-	/**
-	 * If set to true, files with the ending .c and .i are taken into account,
-	 * otherwise files with this ending are ignored.
-	 */
-	private static final boolean m_C = !true;
-
+	
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public long getTimeout() {
-		return 10 * 1000;
+		return 30 * 1000;
 	}
 	
 	@Override
 	public Collection<UltimateTestCase> createTestCases() {
-		for (String setting : m_Settings) {
-			if (m_Boogie) {
-				addTestCases(
-						"AutomizerBpl.xml",
-						setting,
-						m_Programs,
-						new String[] {".bpl"});
-			}
-			if (m_C) {
-				addTestCases(
-						"AutomizerC.xml",
-						setting,
-						m_Programs,
-						new String[] {".c", ".i"});
-			}
+		for (String setting : s_Settings) {
+			addTestCases("AutomizerCInline.xml", 
+					setting, 
+					s_SVCOMP_Programs);
+		}
+		
+		for (String setting : s_Settings) {
+			addTestCases(
+					"AutomizerBplInline.xml",
+					setting,
+					s_UltimateRepository_Programs,
+				    new String[] {".bpl"});
+		}
+		for (String setting : s_Settings) {
+			addTestCases(
+					"AutomizerCInline.xml",
+					setting,
+					s_UltimateRepository_Programs,
+				    new String[] {".c", ".i"});
 		}
 		return super.createTestCases();
 	}
