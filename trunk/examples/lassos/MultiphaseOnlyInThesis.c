d6d5c3173db55214dc85669d5b68c181557aa8cd
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-04-25 13:19:05 +0000

    example

--- trunk/examples/lassos/MultiphaseOnlyInThesis.c
+++ trunk/examples/lassos/MultiphaseOnlyInThesis.c
@@ -16,7 +16,12 @@
 // Author: Jan Leike, Matthias Heizmann
 // Date: 2015-02-9
 
+extern int __VERIFIER_nondet_int(void);
+
 int main() {
+	int x = __VERIFIER_nondet_int();
+	int y = __VERIFIER_nondet_int();
+	int z = __VERIFIER_nondet_int();
 	while (z >= 0) {
 		if (x < 0 && y < 0) {
 		z := z - 1;
