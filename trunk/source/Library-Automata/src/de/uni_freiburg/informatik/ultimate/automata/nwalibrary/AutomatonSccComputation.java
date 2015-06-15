8c1cb5f44ef3da0493253e695eba145938816924
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-06-15 19:18:00 +0000

    SCC computation for automata

---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/AutomatonSccComputation.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2009-2015 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
+ * containing parts covered by the terms of the Eclipse Public License, the 
+ * licensors of the ULTIMATE Automata Library grant you additional permission 
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.nwalibrary;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import de.uni_freiburg.informatik.ultimate.automata.LibraryIdentifiers;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.reachableStatesAutomaton.NestedWordAutomatonReachableStates;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.transitions.OutgoingCallTransition;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.transitions.OutgoingInternalTransition;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.transitions.OutgoingTransitionlet;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.transitions.StateBasedTransitionFilterPredicateProvider;
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.transitions.SummaryReturnTransition;
+import de.uni_freiburg.informatik.ultimate.core.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.util.FilteredIterable;
+import de.uni_freiburg.informatik.ultimate.util.IteratorConcatenation;
+import de.uni_freiburg.informatik.ultimate.util.scc.DefaultSccComputation;
+import de.uni_freiburg.informatik.ultimate.util.scc.SccComputation.ISuccessorProvider;
+import de.uni_freiburg.informatik.ultimate.util.scc.StronglyConnectedComponent;
+
+/**
+ * Compute SCCs of an automaton. Allows to restrict computation to a subgraph
+ * (subset of states with corresponding edges) of the automaton.
+ * 
+ * @author Matthias Heizmann
+ *
+ * @param <LETTER>
+ * @param <STATE>
+ */
+public class AutomatonSccComputation<LETTER, STATE> {
+	
+	
+	private final NestedWordAutomatonReachableStates<LETTER, STATE> m_NestedWordAutomatonReachableStates;
+	private final IUltimateServiceProvider m_Services;
+	private final Logger m_Logger;
+	private final DefaultSccComputation<STATE> m_SccComputation;
+	
+	
+	/**
+	 * Computes SCCs of an automaton for a given subset of states.
+	 * @param stateSubset subset of the automata's states
+	 * @param startNodes states at which the computation of SSCs starts
+	 * @return
+	 */
+	public AutomatonSccComputation(
+			NestedWordAutomatonReachableStates<LETTER, STATE> nestedWordAutomatonReachableStates,
+			IUltimateServiceProvider services, Set<STATE> stateSubset, Set<STATE> startNodes) {
+		super();
+		m_NestedWordAutomatonReachableStates = nestedWordAutomatonReachableStates;
+		m_Services = services;
+		m_Logger = m_Services.getLoggingService().getLogger(LibraryIdentifiers.s_LibraryID);
+		m_SccComputation = new DefaultSccComputation<STATE>(m_Logger, 
+				new InSumCaSuccessorProvider(m_NestedWordAutomatonReachableStates, stateSubset), 
+				stateSubset.size(), startNodes);
+	}
+
+
+	/**
+	 * Returns all balls of the computed SCC.
+	 * @return
+	 */
+	public Collection<StronglyConnectedComponent<STATE>> getBalls() {
+		return m_SccComputation.getBalls();
+	}
+
+
+
+	/**
+	 * Provides for a given STATE all STATEs that are 
+	 * <ul>
+	 * <li> successors of internal transitions, summaries and call transitions,
+	 * and
+	 * <li> contained in a given set of states.
+	 * </ul>
+	 * @author Matthias Heizmann
+	 *
+	 */
+	private class InSumCaSuccessorProvider implements ISuccessorProvider<STATE> {
+		
+		private final StateBasedTransitionFilterPredicateProvider<LETTER, STATE> m_TransitionFilter;
+
+		public InSumCaSuccessorProvider(
+				NestedWordAutomatonReachableStates<LETTER, STATE> nestedWordAutomatonReachableStates,
+				Set<STATE> stateSubset) {
+			super();
+			m_TransitionFilter = new StateBasedTransitionFilterPredicateProvider<>(stateSubset);
+		}
+		private <E extends OutgoingTransitionlet<LETTER, STATE>> Iterator<STATE> getStateContainerIterator(final Iterator<E> it) {
+			return new Iterator<STATE>() {
+
+				@Override
+				public boolean hasNext() {
+					return it.hasNext();
+				}
+
+				@Override
+				public STATE next() {
+					return it.next().getSucc();
+				}
+
+				@Override
+				public void remove() {
+					throw new UnsupportedOperationException("not modifiable");
+				}
+				
+			};
+			
+		}
+
+		@Override
+		public IteratorConcatenation<STATE> getSuccessors(final STATE state) {
+			
+			Iterator<STATE> internalTransitionsIterator = 
+					getStateContainerIterator(new FilteredIterable<OutgoingInternalTransition<LETTER, STATE>>(
+							m_NestedWordAutomatonReachableStates.internalSuccessors(state), m_TransitionFilter.getInternalSuccessorPredicate()).iterator());
+			
+			Iterator<STATE> returnSummaryTransitionsIterator = 
+					getStateContainerIterator(new FilteredIterable<SummaryReturnTransition<LETTER, STATE>>(
+							m_NestedWordAutomatonReachableStates.returnSummarySuccessor(state), m_TransitionFilter.getReturnSummaryPredicate()).iterator());
+			
+			
+			Iterator<STATE> callTransitionsIterator = 
+					getStateContainerIterator(new FilteredIterable<OutgoingCallTransition<LETTER, STATE>>(
+							m_NestedWordAutomatonReachableStates.callSuccessors(state), m_TransitionFilter.getCallSuccessorPredicate()).iterator());
+
+			
+			Iterator<STATE>[] iterators = (Iterator<STATE>[]) 
+					new Iterator<?>[] { internalTransitionsIterator, returnSummaryTransitionsIterator, callTransitionsIterator };
+			return new IteratorConcatenation<STATE>(Arrays.asList(iterators));
+		}
+
+	}
+
+}
