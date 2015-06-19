9869025937aa89681e3c6a75be3ad06608ad4f4d
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-06-19 10:40:37 +0000

    allow additional message in ToolchainCanceledException

--- trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/ToolchainCanceledException.java
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/ToolchainCanceledException.java
@@ -13,10 +13,18 @@ public class ToolchainCanceledException extends RuntimeException {
 	
 	public final static String s_Message = "Timeout or Toolchain cancelled by user";
 	private final Class<?> m_ClassOfThrower; 
+	private final String m_WhatWasGoingOnMessage;
 
 	public ToolchainCanceledException(Class<?> thrower) {
 		super(s_Message);
 		m_ClassOfThrower = thrower;
+		m_WhatWasGoingOnMessage = null;
+	}
+	
+	public ToolchainCanceledException(Class<?> thrower, String whatWasGoingOnMessage) {
+		super(s_Message);
+		m_ClassOfThrower = thrower;
+		m_WhatWasGoingOnMessage = whatWasGoingOnMessage;
 	}
 
 	@Override
@@ -31,4 +39,14 @@ public class ToolchainCanceledException extends RuntimeException {
 	public Class<?> getClassOfThrower() {
 		return m_ClassOfThrower;
 	}
+
+	/**
+	 * Return optional message that was added by the algorithm that has
+	 * thrown this Exception. Null if no optional Message was provided.
+	 */
+	public String getWhatWasGoingOnMessage() {
+		return m_WhatWasGoingOnMessage;
+	}
+	
+	
 }
