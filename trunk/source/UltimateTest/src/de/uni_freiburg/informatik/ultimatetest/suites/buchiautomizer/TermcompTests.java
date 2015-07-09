1a938152cbcda20e2d20250bbf4dc6bcd852a104
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-07-09 10:32:56 +0000

    termcomp tests

---
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/buchiautomizer/TermcompTests.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package de.uni_freiburg.informatik.ultimatetest.suites.buchiautomizer;
+
+import java.util.Collection;
+
+import de.uni_freiburg.informatik.ultimatetest.UltimateTestCase;
+
+/**
+ * @author heizmann@informatik.uni-freiburg.de
+ *
+ */
+public class TermcompTests extends
+		AbstractBuchiAutomizerTestSuite {
+	
+	
+	private static final String[] m_UltimateRepository = {
+		"examples/termination/termcomp2015/",
+	};
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public long getTimeout() {
+		return 300 * 1000;
+	}
+	
+	
+	/**
+	 * List of path to setting files. 
+	 * Ultimate will run on each program with each setting that is defined here.
+	 * The path are defined relative to the folder "trunk/examples/settings/",
+	 * because we assume that all settings files are in this folder.
+	 * 
+	 */
+	private static final String[] m_Settings = {
+		"buchiAutomizer/termcomp2015.epf",
+	};
+	
+	
+	private static final String[] m_CToolchains = {
+		"BuchiAutomizerCWithBlockEncoding.xml",
+//		"BuchiAutomizerCInlineWithBlockEncoding.xml",
+	};
+
+	
+	
+	
+
+	@Override
+	public Collection<UltimateTestCase> createTestCases() {
+		for (String setting : m_Settings) {
+			for (String toolchain : m_CToolchains) {
+				addTestCases(toolchain, setting, m_UltimateRepository, 
+						new String[] {".c", ".i"});
+			}
+		}
+		return super.createTestCases();
+	}
+	
+}
