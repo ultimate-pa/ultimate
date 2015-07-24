f106a00770f8d0117a5224e87b6431f4e11742bb
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-07-24 12:06:26 +0000

    tests

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/buchiautomizer/TermcompTests.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/buchiautomizer/TermcompTests.java
@@ -16,8 +16,8 @@ public class TermcompTests extends
 	
 	
 	private static final String[] m_UltimateRepository = {
-//		"examples/termination/termcomp2015/C/",
-//		"examples/termination/termcomp2015/C_Integer/Stroeder_15",
+		"examples/termination/termcomp2015/C/",
+		"examples/termination/termcomp2015/C_Integer/Stroeder_15",
 		"examples/termination/termcomp2015/C_Integer/Ton_Chanh_15",
 	};
 	
@@ -43,7 +43,7 @@ public class TermcompTests extends
 	
 	
 	private static final String[] m_CToolchains = {
-		"BuchiAutomizerCWithBlockEncoding.xml",
+		"AutomizerAndBuchiAutomizerCWithBlockEncoding.xml",
 //		"BuchiAutomizerCInlineWithBlockEncoding.xml",
 	};
 
