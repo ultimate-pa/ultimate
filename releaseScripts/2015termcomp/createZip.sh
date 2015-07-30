cff6a79086a5a9809cbcca1c57fd9fe5674f88b1
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-07-30 10:13:23 +0000

    release script

--- releaseScripts/2015termcomp/createZip.sh
+++ releaseScripts/2015termcomp/createZip.sh
@@ -6,11 +6,12 @@ cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E4/linux/gtk/x86_
 cp LICENSE* StarExecArchive/Ultimate/
 cp starexec_description.txt StarExecArchive/
 cp ../../trunk/examples/toolchains/AutomizerAndBuchiAutomizerCWithBlockEncoding.xml StarExecArchive/
-cp starexec_run_default StarExecArchive/bin
+cp starexec_run_default StarExecArchive/bin/
 cp Ultimate.ini StarExecArchive/Ultimate/
 cp ../../trunk/examples/settings/buchiAutomizer/termcomp2015.epf StarExecArchive/
 mkdir StarExecArchive/z3
-cp -R /usr/bin/z3 StarExecArchive/z3/
+cp -LR ./z3 StarExecArchive/z3/
+cp starexec_run_z3test StarExecArchive/bin/
 cd StarExecArchive
 zip ../UltimateCommandline.zip -r *
 
