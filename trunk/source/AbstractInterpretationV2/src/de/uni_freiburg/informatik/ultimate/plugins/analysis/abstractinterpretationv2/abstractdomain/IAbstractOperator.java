931b38f12c9230237b517d672060c5d238d352df
Jan Haettig <haettigj@informatik.uni-freiburg.de>
2015-08-03 13:24:08 +0000

    delete folder, to rename it

--- trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/abstractdomain/IAbstractOperator.java
+++
@@ -1,6 +0,0 @@
-package de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationV2.abstractdomain;
-
-public interface IAbstractOperator<T>
-{
-	IAbstractState<T> apply(IAbstractState<T> a, IAbstractState<T> b);
-}
