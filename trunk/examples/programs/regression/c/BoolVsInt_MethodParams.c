507e64ea7af4cc24081e62d88ec536c1fba29254
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-08-15 21:54:50 +0000

    bugfix example

--- trunk/examples/programs/regression/c/BoolVsInt_MethodParams.c
+++ trunk/examples/programs/regression/c/BoolVsInt_MethodParams.c
@@ -13,7 +13,7 @@ int test(int a, int b, _Bool c, _Bool d, int e, int f) {
 }
 
 int main() {
-	int result = test(1, 2, 0, 0, 5, 6);
+	int result = test(1, 2, 0, 23, 5, 6);
 	//@assert result == 0;
 	return 0;
 }
