cf719e56d60cbb433ff5d8f2146550f16ea9ad5b
Thomas Lang <langt@informatik.uni-freiburg.de>
2015-08-18 14:57:30 +0000

    Tests for bit vector conversion

---
+++ trunk/examples/programs/bitvector/ConversionTestsNoAssumptions.c
@@ -0,0 +1,52 @@
+//#Safe
+/* 
+ * Author: langt@informatik.uni-freiburg.de
+ * Date: 18.08.2015
+ */
+
+int main() {
+  /* signed int to unsigned int */
+  signed int a = -1;
+  unsigned int b = a;
+  unsigned int limit2 = 100U;
+
+  //assert(b > limit2);
+
+  /* unsigned int to signed int (fitting) */
+  unsigned int c = 100U;
+  signed int d = c;
+
+  //@assert(d == 100);
+
+  /* signed int to signed long long */
+  signed int e = -1;
+  signed long long f = e;
+
+  if (f != -1LL) {
+    //@assert(\false);
+  }
+
+  /* signed int to unsigned long long */
+  signed int g = -1;
+  unsigned long long h = g;
+  unsigned long long limit3 = 100ULL;
+
+  //assert(h > limit3);
+
+  /* unsigned int to signed long long */
+  unsigned int k = 100U;
+  signed long long l = k;
+
+  if (l != 100LL) {
+    //@assert(\false);
+  }
+
+  /* ARITHMOD ERROR */
+  /* unsigned int to unsigned long long */
+  //unsigned int m = 100U;
+  //unsigned long long n = m;
+
+  //if (n != 100ULL) {
+    //assert(\false);
+  //}
+}
