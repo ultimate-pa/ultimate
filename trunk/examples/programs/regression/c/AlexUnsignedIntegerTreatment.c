9a8db55feb5d6b97838136e7e62ca1bb9f973252
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-09-01 00:17:32 +0000

    example

--- trunk/examples/programs/regression/c/AlexUnsignedIntegerTreatment.c
+++ trunk/examples/programs/regression/c/AlexUnsignedIntegerTreatment.c
@@ -4,10 +4,14 @@
 // Author: heizmann@informatik.uni-freiburg.de
 // Date: 2015-08-31
 
+#include <stdio.h>
+
 int main() {
 	if (sizeof(long long) > 4 && sizeof(int) == 4) {
 		unsigned int x = 2147483648U;
+		printf("%u\n",x);
 		x += 2147483648U;
+		printf("%u\n",x);
 		// now, due to the wraparound the value of x is 0, however we
 		// store 2^32 and take this value modulo 2^32 in comparisons
 		// if the operands of the comparison are unsigned ints.
@@ -15,6 +19,7 @@ int main() {
 			//@ assert \false;
 		}
 		signed long long y = x;
+		printf("%lld\n",y);
 		// now the type of the expression is not unsigned any more
 		// it was important that we did the modulo 2^32 at the
 		// cast from unsigned to signed
