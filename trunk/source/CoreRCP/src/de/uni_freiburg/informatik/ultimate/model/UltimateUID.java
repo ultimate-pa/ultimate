0db8e173bf868d72ef3cbb43e39c389dad030d09
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2015-09-12 20:42:44 -0400

    removed old svn keywords

--- trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/model/UltimateUID.java
+++ trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/model/UltimateUID.java
@@ -32,80 +32,78 @@ import java.io.Serializable;
 import java.util.UUID;
 
 /**
- * This is an updated UltimateUID. It uses UUID instead of UID.
- * UUID provides its own compare methods so you only have to compare
- * Strings if you saved a UltimateUID as a String somewhere. Please note
- * that creating a random UUID takes three times the time of creating
- * a UID. That still should not matter because creating 100000 UUIDs
- * takes some 200ms.
- * <br><br>
- * @author dietsch
- * @since 486
- * <br><br>
- * $LastChangedBy$ <br>
- * $LastChangedDate$ <br>
- * $LastChangedRevision$
+ * This is an updated UltimateUID. It uses UUID instead of UID. UUID provides
+ * its own compare methods so you only have to compare Strings if you saved a
+ * UltimateUID as a String somewhere. Please note that creating a random UUID
+ * takes three times the time of creating a UID. That still should not matter
+ * because creating 100000 UUIDs takes some 200ms. <br>
+ * <br>
+ * 
+ * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  *
  */
 public class UltimateUID implements Serializable {
 
 	private static final long serialVersionUID = -5789249181482554415L;
-	//private UID m_UID;
+	// private UID m_UID;
 	private UUID m_UID;
 	private transient String m_UUIDString;
-	
+
 	/**
 	 * Creates a new UltimateUID
 	 */
 	public UltimateUID() {
-		//this.m_UID = new UID();
+		// this.m_UID = new UID();
 		this.m_UID = UUID.randomUUID();
 	}
-	
+
 	/**
 	 * Tests if another UltimateUID equals this one
-	 * @param uid the other UltimateUID
+	 * 
+	 * @param uid
+	 *            the other UltimateUID
 	 * @return true if they are the same
 	 */
-	public boolean equals(UltimateUID uid){
-		//return this.m_UID.toString().equals(uid.m_UID.toString());
+	public boolean equals(UltimateUID uid) {
+		// return this.m_UID.toString().equals(uid.m_UID.toString());
 		return this.m_UID.equals(uid.m_UID);
 	}
-	
+
 	/**
-	 * Tests if another UltimateUID represented by the parameter
-	 * equals this UltimateUID.
-	 * @param uid The other UltimateUID as String
+	 * Tests if another UltimateUID represented by the parameter equals this
+	 * UltimateUID.
+	 * 
+	 * @param uid
+	 *            The other UltimateUID as String
 	 * @return true if they are the same
 	 */
-	public boolean equals(String uid){
-		if(m_UUIDString == null || m_UUIDString.length() == 0)
-		{
+	public boolean equals(String uid) {
+		if (m_UUIDString == null || m_UUIDString.length() == 0) {
 			m_UUIDString = this.m_UID.toString();
 		}
 		return m_UUIDString.equals(uid);
 	}
-	
+
 	/**
 	 * Tests if another Object is the same
-	 * @param o the other possible UltimateUID
+	 * 
+	 * @param o
+	 *            the other possible UltimateUID
 	 * @return true if they are the same
 	 */
-	public boolean equals(Object o)
-	{
-		if(o instanceof UltimateUID)
-		{
-			UltimateUID uid = (UltimateUID)o;
+	public boolean equals(Object o) {
+		if (o instanceof UltimateUID) {
+			UltimateUID uid = (UltimateUID) o;
 			return equals(uid);
 		}
 		return false;
 	}
-	
-	public int hashCode(){
+
+	public int hashCode() {
 		return m_UID.hashCode();
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return m_UID.toString();
 	}
 }
