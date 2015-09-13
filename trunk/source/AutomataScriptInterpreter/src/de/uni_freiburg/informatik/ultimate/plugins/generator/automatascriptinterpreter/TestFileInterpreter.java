5a4798503a014b610d6b978b2bf1588c10d0374e
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2015-09-13 00:13:57 -0400

    fixed issues with dynamic IOperation loading in Java8 and/or Windows

--- trunk/source/AutomataScriptInterpreter/src/de/uni_freiburg/informatik/ultimate/plugins/generator/automatascriptinterpreter/TestFileInterpreter.java
+++ trunk/source/AutomataScriptInterpreter/src/de/uni_freiburg/informatik/ultimate/plugins/generator/automatascriptinterpreter/TestFileInterpreter.java
@@ -42,6 +42,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -638,7 +640,7 @@ public class TestFileInterpreter implements IMessagePrinter {
 	private enum Finished {
 		FINISHED, TIMEOUT, ERROR, OUTOFMEMORY
 	};
-	
+
 	public static final String s_AssertionHoldsMessage = "Assertion holds.";
 	public static final String s_AssertionViolatedMessage = "Assertion violated!";
 
@@ -1067,12 +1069,12 @@ public class TestFileInterpreter implements IMessagePrinter {
 				if ((Boolean) result) {
 
 					mResultOfAssertStatements.add(new GenericResultAtElement<AtsASTNode>(oe, Activator.s_PLUGIN_ID,
-							mServices.getBacktranslationService(), s_AssertionHoldsMessage , oe
-									.getAsString(), Severity.INFO));
+							mServices.getBacktranslationService(), s_AssertionHoldsMessage, oe.getAsString(),
+							Severity.INFO));
 				} else {
 					mResultOfAssertStatements.add(new GenericResultAtElement<AtsASTNode>(oe, Activator.s_PLUGIN_ID,
-							mServices.getBacktranslationService(), s_AssertionViolatedMessage, oe
-									.getAsString(), Severity.ERROR));
+							mServices.getBacktranslationService(), s_AssertionViolatedMessage, oe.getAsString(),
+							Severity.ERROR));
 				}
 			} else {
 				throw new AssertionError("assert expects boolean result, type checker should have found this");
@@ -1085,7 +1087,8 @@ public class TestFileInterpreter implements IMessagePrinter {
 				final String text;
 				if (o instanceof IAutomaton) {
 					mLastPrintedAutomaton = (IAutomaton<?, ?>) o;
-					text = (new AtsDefinitionPrinter<String, String>(mServices, "automaton", o)).getDefinitionAsString();
+					text = (new AtsDefinitionPrinter<String, String>(mServices, "automaton", o))
+							.getDefinitionAsString();
 				} else {
 					text = String.valueOf(o);
 				}
@@ -1102,8 +1105,8 @@ public class TestFileInterpreter implements IMessagePrinter {
 			IOperation<String, String> op = getAutomataOperation(oe, arguments);
 			if (op != null) {
 				try {
-					assert op.checkResult(new StringFactory()) : 
-						"Result of operation " + op.operationName() + " is wrong (according to its checkResult method)";
+					assert op.checkResult(new StringFactory()) : "Result of operation " + op.operationName()
+							+ " is wrong (according to its checkResult method)";
 					result = op.getResult();
 				} catch (AutomataLibraryException e) {
 					throw new InterpreterException(oe.getLocation(), e.getMessage());
@@ -1309,8 +1312,8 @@ public class TestFileInterpreter implements IMessagePrinter {
 		if (node == null) {
 			result = new GenericResult(Activator.s_PLUGIN_ID, shortDescr, longDescr, sev);
 		} else {
-			result = new GenericResultAtElement<AtsASTNode>(node, Activator.s_PLUGIN_ID, mServices
-					.getBacktranslationService(), shortDescr, longDescr, sev);
+			result = new GenericResultAtElement<AtsASTNode>(node, Activator.s_PLUGIN_ID,
+					mServices.getBacktranslationService(), shortDescr, longDescr, sev);
 		}
 		mServices.getResultService().reportResult(Activator.s_PLUGIN_ID, result);
 	}
@@ -1358,6 +1361,7 @@ public class TestFileInterpreter implements IMessagePrinter {
 	 *             if the operation does not exist
 	 */
 
+	@SuppressWarnings("unchecked")
 	private IOperation<String, String> getAutomataOperation(OperationInvocationExpressionAST oe,
 			ArrayList<Object> arguments) throws InterpreterException {
 		String operationName = oe.getOperationName().toLowerCase();
@@ -1367,7 +1371,8 @@ public class TestFileInterpreter implements IMessagePrinter {
 			for (Class<?> operationClass : operationClasses) {
 				Constructor<?>[] operationConstructors = operationClass.getConstructors();
 				if (operationConstructors.length == 0) {
-					String description = "Error in automata library: operation " + operationName + " does not have a constructor";
+					String description = "Error in automata library: operation " + operationName
+							+ " does not have a constructor";
 					throw new InterpreterException(oe.getLocation(), description, description);
 				}
 				// Find the constructor which expects the correct arguments
@@ -1432,13 +1437,11 @@ public class TestFileInterpreter implements IMessagePrinter {
 
 	/**
 	 * Prepend mServices to args if IUltimateServiceProvider is the first
-	 * parameter of the constructor.
-	 * FIXME: This is only a workaround! In the future IUltimateServiceProvider
-	 * will be the first argument of each IOperation and we will always
-	 * prepend mServices
+	 * parameter of the constructor. FIXME: This is only a workaround! In the
+	 * future IUltimateServiceProvider will be the first argument of each
+	 * IOperation and we will always prepend mServices
 	 */
-	private Object[] prependIUltimateServiceProviderIfNecessary(
-			Constructor<?> c, Object[] args) {
+	private Object[] prependIUltimateServiceProviderIfNecessary(Constructor<?> c, Object[] args) {
 		boolean firstParameterIsIUltimateServiceProvider;
 		Class<?> fstParam = c.getParameterTypes()[0];
 		if (IUltimateServiceProvider.class.isAssignableFrom(fstParam)) {
@@ -1457,7 +1460,7 @@ public class TestFileInterpreter implements IMessagePrinter {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Return args.toArray(), but prepend a new StringFactory if the first
 	 * parameter of the Constructor c is a StateFacotry.
@@ -1489,12 +1492,10 @@ public class TestFileInterpreter implements IMessagePrinter {
 	}
 
 	/**
-	 * TODO: get rid of this workaround 
-	 * Workaround that is necessary as long as not all operations use
-	 * Services as their first parameter.
+	 * TODO: get rid of this workaround Workaround that is necessary as long as
+	 * not all operations use Services as their first parameter.
 	 */
-	private boolean firstParameterIsServicesAndSecondParameterIsStateFactory(
-			Constructor<?> c, Class<?> fstParam) {
+	private boolean firstParameterIsServicesAndSecondParameterIsStateFactory(Constructor<?> c, Class<?> fstParam) {
 		boolean firstParameterIsServicesAndSecondParameterIsStateFactory;
 		if (c.getParameterTypes().length < 2) {
 			firstParameterIsServicesAndSecondParameterIsStateFactory = false;
@@ -1549,85 +1550,87 @@ public class TestFileInterpreter implements IMessagePrinter {
 	 *         in the directories.
 	 */
 	private Map<String, Set<Class<?>>> getOperationClasses() {
-		Map<String, Set<Class<?>>> result = new HashMap<String, Set<Class<?>>>();
+		final Map<String, Set<Class<?>>> result = new HashMap<String, Set<Class<?>>>();
 		/*
-		 * NOTE: The following directories are scanned recursively.
-		 * Hence, do not add directories where one directory is a subdirectory
-		 * of another in the list to avoid unnecessary work.
+		 * NOTE: The following directories are scanned recursively. Hence, do
+		 * not add directories where one directory is a subdirectory of another
+		 * in the list to avoid unnecessary work.
 		 */
-		String[] baseDirs = {
-			"/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operations",
-			"/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operationsOldApi",
-			"/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/alternating",
-			"/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/buchiNwa",
-			"/de/uni_freiburg/informatik/ultimate/automata/petrinet" };
-		for (String baseDir : baseDirs) {
-			ArrayDeque<String> dirs = new ArrayDeque<String>();
-			dirs.add("");
-			String baseDirInPackageFormat = baseDir.replaceAll(File.separator, ".");
-			if (baseDirInPackageFormat.charAt(0) == '.') {
-				baseDirInPackageFormat = baseDirInPackageFormat.substring(1);
-			}
-			while (!dirs.isEmpty()) {
-				String dir = dirs.removeFirst();
-				String[] files = filesInDirectory(baseDir + File.separator + dir);
-				for (String file : files) {
-					if (file.endsWith(".class")) {
-						String fileWithoutSuffix = file.substring(0, file.length() - 6);
-						String path;
-						if (dir.isEmpty()) {
-							path = baseDirInPackageFormat + "." +
-									fileWithoutSuffix;
-						} else {
-							path = baseDirInPackageFormat + "." +
-									dir.replaceAll(File.separator, ".") + "." +
-									fileWithoutSuffix;
-						}
-						Class<?> clazz = null;
-						try {
-							clazz = Class.forName(path);
-						} catch (ClassNotFoundException e) {
-							e.printStackTrace();
-							mLogger.error("Couldn't load/find class " + path);
-							break;
-						}
-						if ((clazz != null) &&
-								(classImplementsIOperationInterface(clazz))) {
-							String operationName = fileWithoutSuffix.toLowerCase();
-							if (result.containsKey(operationName)) {
-								Set<Class<?>> s = result.get(operationName);
-								s.add(clazz);
-							} else {
-								Set<Class<?>> s = new HashSet<Class<?>>();
-								s.add(clazz);
-								result.put(operationName, s);
-							}
+		final String[] packages = { "de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operations",
+				"de.uni_freiburg.informatik.ultimate.automata.nwalibrary.operationsOldApi",
+				"de.uni_freiburg.informatik.ultimate.automata.nwalibrary.alternating",
+				"de.uni_freiburg.informatik.ultimate.automata.nwalibrary.buchiNwa",
+				"de.uni_freiburg.informatik.ultimate.automata.petrinet" };
+		for (final String packageName : packages) {
+			final Collection<File> files = filesInDirectory(getPathFromPackageName(packageName));
 
-						}
+			for (final File file : files) {
+				final String filename = file.getName();
+				if (filename.endsWith(".class")) {
+					final Class<?> clazz = getClassFromFile(packageName, file);
+					if (clazz != null && classImplementsIOperationInterface(clazz)) {
+						tryAdd(result, clazz);
+						continue;
 					}
-					// if the file has no ending, it may be a directory
-					else if (!file.contains(".")) {
-						String suffix;
-						if (dir.isEmpty()) {
-							// first subfolder
-							suffix = file;
-						}
-						else {
-							// already in a subfolder, append it
-							suffix = dir + File.separator + file;
-						}
-						
-						if (isDirectory(baseDir + File.separator + suffix)) {
-							// add subfolder
-							dirs.addLast(suffix);
-						} else {
-							mLogger.error("Invalid file or directory: " + suffix);
-						}
-					}
+				}
+				if (mLogger.isDebugEnabled()) {
+					mLogger.debug("Not considering " + file.getAbsolutePath());
 				}
 			}
 		}
 		return result;
+	}
+
+	private Class<?> getClassFromFile(final String packageName, final File file) {
+		final String qualifiedName = getQualifiedNameFromFile(packageName, file);
+		final Class<?> clazz;
+		try {
+			clazz = Class.forName(qualifiedName);
+		} catch (ClassNotFoundException e) {
+			mLogger.error("Couldn't load/find class " + qualifiedName);
+			throw new RuntimeException(e);
+		}
+		return clazz;
+	}
+
+	private boolean tryAdd(final Map<String, Set<Class<?>>> result, final Class<?> clazz) {
+		final String opName = clazz.getSimpleName().toLowerCase();
+		Set<Class<?>> set = result.get(opName);
+		if (set == null) {
+			set = new HashSet<>();
+			result.put(opName, set);
+		}
+		set.add(clazz);
+		return true;
+	}
+
+	/**
+	 * Tries to resolve the fully qualified name from the package name and the
+	 * found file.
+	 * 
+	 * If the package is a.b.c.d and we found a class with the path
+	 * /foo/bar/a/b/c/d/huh/OurClass.class, then the fully qualified name is
+	 * a.b.c.d.huh.OurClass
+	 * 
+	 * @param packageName
+	 * @param file
+	 * @return
+	 */
+	private String getQualifiedNameFromFile(final String packageName, final File file) {
+		assert file != null;
+		assert file.getName().endsWith(".class");
+
+		final String fullname = file.getAbsolutePath();
+		final String filenameWithoutSuffix = fullname.substring(0, fullname.length() - 6);
+		final String knownPath = getPathFromPackageName(packageName);
+		int validAfter = filenameWithoutSuffix.indexOf(knownPath);
+		assert validAfter != -1;
+
+		return filenameWithoutSuffix.substring(validAfter).replace(File.separatorChar, '.');
+	}
+
+	private String getPathFromPackageName(final String packageName) {
+		return packageName.replace(".", File.separator);
 	}
 
 	/**
@@ -1639,21 +1642,13 @@ public class TestFileInterpreter implements IMessagePrinter {
 	 *         interface. Otherwise false.
 	 */
 	private static boolean classImplementsIOperationInterface(Class<?> c) {
-		Class<?>[] implementedInterfaces = c.getInterfaces();
-		for (Class<?> interFace : implementedInterfaces) {
+		final Class<?>[] implementedInterfaces = c.getInterfaces();
+		for (final Class<?> interFace : implementedInterfaces) {
 			if (interFace.equals(IOperation.class)) {
 				return true;
 			}
 		}
 		return false;
-	}
-
-	private static boolean isDirectory(String dir) {
-		URL dirURL = IOperation.class.getClassLoader().getResource(dir);
-		if (dirURL == null)
-			return false;
-		else
-			return dirURL.getProtocol().equals("bundleresource");
 	}
 
 	/**
@@ -1662,42 +1657,48 @@ public class TestFileInterpreter implements IMessagePrinter {
 	 * protocol <i>file</i> and <i>bundleresource</i>. At the moment these are
 	 * the only ones that occur in Website and WebsiteEclipseBridge.
 	 */
-	private String[] filesInDirectory(String dir) {
-		URL dirURL = IOperation.class.getClassLoader().getResource(dir);
+	private Collection<File> filesInDirectory(String dir) {
+		final URL dirURL = IOperation.class.getClassLoader().getResource(dir);
 		if (dirURL == null) {
-			// throw new UnsupportedOperationException("Directory \"" + dir +
-			// "\" does not exist");
 			mLogger.error("Directory \"" + dir + "\" does not exist");
-			return new String[0];
+			return Collections.emptyList();
 		}
-		String protocol = dirURL.getProtocol();
-		File dirFile = null;
+		final String protocol = dirURL.getProtocol();
+		final File dirFile;
 		if (protocol.equals("file")) {
 			try {
 				dirFile = new File(dirURL.toURI());
 			} catch (URISyntaxException e) {
-				e.printStackTrace();
-				// throw new UnsupportedOperationException("Directory \"" + dir
-				// + "\" does not exist");
 				mLogger.error("Directory \"" + dir + "\" does not exist");
-				return new String[0];
+				return Collections.emptyList();
 			}
 		} else if (protocol.equals("bundleresource")) {
 			try {
 				URL fileURL = FileLocator.toFileURL(dirURL);
 				dirFile = new File(fileURL.getFile());
 			} catch (Exception e) {
-				e.printStackTrace();
-				// throw new UnsupportedOperationException("Directory \"" + dir
-				// + "\" does not exist");
 				mLogger.error("Directory \"" + dir + "\" does not exist");
-				return new String[0];
+				return Collections.emptyList();
 			}
 		} else {
 			throw new UnsupportedOperationException("unknown protocol");
 		}
-		String[] files = dirFile.list();
-		return files;
+		return resolveDirectories(Arrays.asList(dirFile));
+	}
+
+	private Collection<File> resolveDirectories(final Collection<File> files) {
+		final ArrayDeque<File> worklist = new ArrayDeque<File>();
+		final ArrayList<File> rtr = new ArrayList<File>();
+		worklist.addAll(files);
+		while (!worklist.isEmpty()) {
+			final File file = worklist.removeFirst();
+			if (file.isFile()) {
+				rtr.add(file);
+				continue;
+			}
+			worklist.addAll(Arrays.asList(file.listFiles()));
+		}
+		return rtr;
 	}
 
 	/**
@@ -1706,38 +1707,37 @@ public class TestFileInterpreter implements IMessagePrinter {
 	 * is provided.
 	 * 
 	 */
-	private class InterpreterException extends Exception {
+	private static class InterpreterException extends Exception {
 		private static final long serialVersionUID = -7514869048479460179L;
-		private final ILocation m_Location;
-		private final String m_ShortDescription;
-		private final String m_LongDescription;
+		private final ILocation mLocation;
+		private final String mShortDescription;
+		private final String mLongDescription;
 
-		public InterpreterException(ILocation m_Location, String longDescription) {
+		public InterpreterException(final ILocation location, final String longDescription) {
 			super();
-			this.m_Location = m_Location;
-			this.m_LongDescription = longDescription;
-			this.m_ShortDescription = null;
+			this.mLocation = location;
+			this.mLongDescription = longDescription;
+			this.mShortDescription = null;
 		}
 
-		public InterpreterException(ILocation m_Location, String longDescription, String shortDescription) {
+		public InterpreterException(final ILocation location, final String longDescription,
+				final String shortDescription) {
 			super();
-			this.m_Location = m_Location;
-			this.m_LongDescription = longDescription;
-			this.m_ShortDescription = shortDescription;
+			this.mLocation = location;
+			this.mLongDescription = longDescription;
+			this.mShortDescription = shortDescription;
 		}
 
 		public ILocation getLocation() {
-			return m_Location;
+			return mLocation;
 		}
 
 		public String getLongDescription() {
-			return m_LongDescription;
+			return mLongDescription;
 		}
 
 		public String getShortDescription() {
-			return m_ShortDescription;
+			return mShortDescription;
 		}
-
 	}
-
 }
