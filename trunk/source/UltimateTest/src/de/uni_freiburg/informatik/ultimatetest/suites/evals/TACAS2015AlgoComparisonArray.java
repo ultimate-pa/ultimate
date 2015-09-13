8e8f12bf090415d43ca9d882b001d27dcee5cadf
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-09-13 16:45:26 +0200

    undo more changes that were done accidentally

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/evals/TACAS2015AlgoComparisonArray.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/evals/TACAS2015AlgoComparisonArray.java
@@ -35,17 +35,17 @@ public class TACAS2015AlgoComparisonArray extends TACAS2015 {
 	@Override
 	public Collection<UltimateTestCase> createTestCases() {
 
-		addTestCases("AutomizerCInline.xml", "TACASInterpolation2015/Automizer/Z3_Interpolation.epf", getPairs());
-		addTestCases("AutomizerCInline.xml", "TACASInterpolation2015/Automizer/Princess_Interpolation.epf", getPairs());
-//		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/SMTInterpol_Interpolation.epf", getPairs());
-//		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/SMTInterpol_SP-IC-LV.epf", getPairs());
-		addTestCases("AutomizerCInline.xml", "TACASInterpolation2015/Automizer/Z3_SP-IC-LV.epf", getPairs());
-//		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/CVC4_SP-IC-LV.epf", getPairs());
-//		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/SMTInterpol_SP-IC.epf", getPairs());
-//		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/Z3_SP-IC.epf", getPairs());
-//		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/CVC4_SP-IC.epf", getPairs());
-//		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/Z3_SP.epf", getPairs());
-//		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/Z3_SP-LV.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/Z3_Interpolation.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/Princess_Interpolation.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/SMTInterpol_Interpolation.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/SMTInterpol_SP-IC-LV.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/Z3_SP-IC-LV.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/CVC4_SP-IC-LV.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/SMTInterpol_SP-IC.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/Z3_SP-IC.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/CVC4_SP-IC.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/Z3_SP.epf", getPairs());
+		addTestCases("AutomizerC.xml", "TACASInterpolation2015/Automizer/Z3_SP-LV.epf", getPairs());
 
 		// addTestCases("CodeCheckNoBE-C.xml",
 		// "TACASInterpolation2015/Kojak-SP.epf", getPairs());
@@ -64,23 +64,22 @@ public class TACAS2015AlgoComparisonArray extends TACAS2015 {
 
 	@Override
 	protected long getTimeout() {
-		return 60 * 1000;
+		return 600 * 1000;
 	}
 
 	@Override
 	protected String[] getDirectories() {
 		// @formatter:off
 		String[] directories = {
-			"examples/programs/MapReduce/nondet/",
-//		    "examples/svcomp/heap-manipulation",
-//		    "examples/svcomp/list-properties",
-//		    "examples/svcomp/ldv-regression",
-//		    
-//		    "examples/svcomp/loops",
-//		    "examples/svcomp/loop-acceleration",
-//		    "examples/svcomp/loop-invgen",
-//		    "examples/svcomp/loop-lit",
-//		    "examples/svcomp/loop-new",
+		    "examples/svcomp/heap-manipulation",
+		    "examples/svcomp/list-properties",
+		    "examples/svcomp/ldv-regression",
+		    
+		    "examples/svcomp/loops",
+		    "examples/svcomp/loop-acceleration",
+		    "examples/svcomp/loop-invgen",
+		    "examples/svcomp/loop-lit",
+		    "examples/svcomp/loop-new",
 
 		};
 		return directories;
