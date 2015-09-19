d426b11400adeb5ac31f7f2b7cd2f045a1cba37d
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-09-19 02:33:07 +0200

    omit ACSL

--- trunk/examples/programs/quantifier/SimpleArrayTest-ArrayEntryMayHaveAnyValue.c
+++ trunk/examples/programs/quantifier/SimpleArrayTest-ArrayEntryMayHaveAnyValue.c
@@ -2,13 +2,13 @@
 // Author: heizmann@informatik.uni-freiburg.de
 // Date: 27.7.2012
 
-/*@ requires n > 1048;
-  @*/
 void procWithArray(int n);
 
 void procWithArray(int n) {
-	int a[n];
-	int x;
-	x = a[23];
-	//@assert x == 0;
+	if (n > 1048) {
+		int a[n];
+		int x;
+		x = a[23];
+		//@assert x == 0;
+	}
 }
