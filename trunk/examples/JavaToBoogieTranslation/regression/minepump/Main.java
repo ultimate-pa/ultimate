21d87a496c56a21a3ed6267dd53a9fd7ad173945
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2015-10-06 17:12:27 -0400

    added one svcomp example to the regression folder (executable jar is easier than folder; have to write script to convert all examples to jar)

---
+++ trunk/examples/JavaToBoogieTranslation/regression/minepump/Main.java
@@ -0,0 +1,54 @@
+
+import java.util.Random;
+import MinePumpSystem.Environment; 
+import MinePumpSystem.MinePump; 
+
+public  class  Main {
+
+	private static int cleanupTimeShifts = 2;
+
+    	public static void main(String[] args) {
+		   randomSequenceOfActions(3); 
+	    }
+
+	public static boolean getBoolean() {
+	     Random random = new Random();
+	     return random.nextBoolean();		
+	}
+
+
+    public static void randomSequenceOfActions(int maxLength) {
+		Actions a = new Actions();
+
+		int counter = 0;
+		while (counter < maxLength) {
+			counter++;
+		
+			boolean action1 = getBoolean();
+			boolean action2 = getBoolean();
+			boolean action3 = getBoolean();			
+			boolean action4 = false;
+			if (!action3) action4 = getBoolean();
+
+			if (action1) {
+				a.waterRise();
+			}
+
+			if (action2) {
+				a.methaneChange();
+			}
+
+			if (action3) {
+				a.startSystem();
+			} else if (action4) {
+				a.stopSystem();
+			}
+
+			a.timeShift();
+		}
+		
+		for (counter = 0; counter < cleanupTimeShifts; counter++) {
+			a.timeShift();
+		}
+	}
+}
