4f4f054e3fc545c2f4c108858fbcaf55add7448f
Marius Greitschus <greitsch@informatik.uni-freiburg.de>
2015-10-16 17:53:03 +0200

    Towards AIv2: - Added HelperFunctions for interval arithmetic testing. - Added tests for subtractions of intervals.

--- trunk/source/AbstractInterpretationV2Test/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/interval/IntervalDomainArithmeticAdditionTest.java
+++ trunk/source/AbstractInterpretationV2Test/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/interval/IntervalDomainArithmeticAdditionTest.java
@@ -28,142 +28,64 @@
 package de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationv2.domain.interval;
 
 import static org.junit.Assert.*;
-
-import java.math.BigDecimal;
-
 import org.junit.Test;
 
-import de.uni_freiburg.informatik.ultimate.model.boogie.ast.BinaryExpression.Operator;
-import de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationv2.domain.IEvaluationResult;
-
-public class IntervalDomainArithmeticTest {
-
-	private IntervalDomainValue createInterval(int lower, int upper) {
-		return new IntervalDomainValue(new IntervalValue(new BigDecimal(lower)),
-		        new IntervalValue(new BigDecimal(upper)));
-	}
-
-	private IntervalBinaryExpressionEvaluator createBinaryEvaluator(IntervalDomainValue first,
-	        IntervalDomainValue second, Operator operator) {
-		IntervalSingletonValueExpressionEvaluator value1Evaluator = new IntervalSingletonValueExpressionEvaluator(
-		        first);
-		IntervalSingletonValueExpressionEvaluator value2Evaluator = new IntervalSingletonValueExpressionEvaluator(
-		        second);
-
-		IntervalBinaryExpressionEvaluator binaryExpressionEvaluator = new IntervalBinaryExpressionEvaluator();
-
-		binaryExpressionEvaluator.setOperator(operator);
-		binaryExpressionEvaluator.addSubEvaluator(value1Evaluator);
-		binaryExpressionEvaluator.addSubEvaluator(value2Evaluator);
-
-		return binaryExpressionEvaluator;
-	}
-
-	private String getMethodName() {
-		final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
-
-		return ste[4].getMethodName();
-	}
-
-	private boolean computeResult(IntervalDomainValue interval1, IntervalDomainValue interval2,
-	        IntervalDomainValue expectedResult, IEvaluationResult<IntervalDomainValue> evaluatorResult) {
-
-		System.out.println(getMethodName());
-		System.out.println("Result  : " + evaluatorResult.toString());
-		System.out.println("Expected: " + expectedResult.toString());
-		System.out.println();
-
-		if (interval1.isBottom() || interval2.isBottom()) {
-			return evaluatorResult.equals(expectedResult);
-		}
-
-		final boolean lowerResult = evaluatorResult.getResult().getLower().equals(expectedResult.getLower());
-		final boolean upperResult = evaluatorResult.getResult().getUpper().equals(expectedResult.getUpper());
-
-		return lowerResult && upperResult;
-	}
-
-	private boolean computeAdditionResult(IntervalDomainValue interval1, IntervalDomainValue interval2,
-	        IntervalDomainValue expectedResult) {
-
-		final IEvaluationResult<IntervalDomainValue> result = createBinaryEvaluator(interval1, interval2,
-		        Operator.ARITHPLUS).evaluate(null);
-
-		return computeResult(interval1, interval2, expectedResult, result);
-	}
-
-	private boolean computeSubtractionResult(IntervalDomainValue interval1, IntervalDomainValue interval2,
-	        IntervalDomainValue expectedResult) {
-
-		final IEvaluationResult<IntervalDomainValue> result = createBinaryEvaluator(interval1, interval2,
-		        Operator.ARITHMINUS).evaluate(null);
-
-		return computeResult(interval1, interval2, expectedResult, result);
-	}
-
-	private boolean computeMultiplicationResult(IntervalDomainValue interval1, IntervalDomainValue interval2,
-	        IntervalDomainValue expectedResult) {
-
-		final IEvaluationResult<IntervalDomainValue> result = createBinaryEvaluator(interval1, interval2,
-		        Operator.ARITHMUL).evaluate(null);
-
-		return computeResult(interval1, interval2, expectedResult, result);
-	}
+public class IntervalDomainArithmeticAdditionTest {
 
 	@Test
 	public void testIntervalAddition() {
 
 		// Interval [1;10]
-		IntervalDomainValue interval1 = createInterval(1, 10);
+		IntervalDomainValue interval1 = HelperFunctions.createInterval(1, 10);
 
 		// Interval [15;20]
-		IntervalDomainValue interval2 = createInterval(15, 20);
+		IntervalDomainValue interval2 = HelperFunctions.createInterval(15, 20);
 
 		// Result should be [16;30]
-		IntervalDomainValue expectedResult = createInterval(16, 30);
+		IntervalDomainValue expectedResult = HelperFunctions.createInterval(16, 30);
 
-		assertTrue(computeAdditionResult(interval1, interval2, expectedResult));
+		assertTrue(HelperFunctions.computeAdditionResult(interval1, interval2, expectedResult));
 	}
 
 	@Test
 	public void testInfiniteIntervalAddition() {
 		// Interval [1, \infty]
-		IntervalDomainValue interval1 = createInterval(1, 1);
+		IntervalDomainValue interval1 = HelperFunctions.createInterval(1, 1);
 		interval1.getUpper().setToInfinity();
 
 		assertTrue(interval1.isUnbounded());
 		assertFalse(interval1.isInfinity());
 
 		// Interval [1,2]
-		IntervalDomainValue interval2 = createInterval(1, 2);
+		IntervalDomainValue interval2 = HelperFunctions.createInterval(1, 2);
 
 		// Result should be [2, \infty]
-		IntervalDomainValue expectedResult1 = createInterval(2, 2);
+		IntervalDomainValue expectedResult1 = HelperFunctions.createInterval(2, 2);
 		expectedResult1.getUpper().setToInfinity();
 
-		assertTrue(computeAdditionResult(interval1, interval2, expectedResult1));
+		assertTrue(HelperFunctions.computeAdditionResult(interval1, interval2, expectedResult1));
 
 		// Interval [1, \infty]
-		IntervalDomainValue interval3 = createInterval(-1, -1);
+		IntervalDomainValue interval3 = HelperFunctions.createInterval(-1, -1);
 		interval3.getUpper().setToInfinity();
 
 		// Result should be [0, \infty]
-		IntervalDomainValue expectedResult2 = createInterval(0, 0);
+		IntervalDomainValue expectedResult2 = HelperFunctions.createInterval(0, 0);
 		expectedResult2.getUpper().setToInfinity();
 
-		assertTrue(computeAdditionResult(interval3, interval2, expectedResult2));
+		assertTrue(HelperFunctions.computeAdditionResult(interval3, interval2, expectedResult2));
 
 		// Interval [-\infty, 0]
-		IntervalDomainValue interval4 = createInterval(0, 0);
+		IntervalDomainValue interval4 = HelperFunctions.createInterval(0, 0);
 		interval4.getLower().setToInfinity();
 		assertTrue(interval4.isUnbounded());
 		assertFalse(interval4.isInfinity());
 
 		// Result should be [-\infty, 2]
-		IntervalDomainValue expectedResult3 = createInterval(0, 2);
+		IntervalDomainValue expectedResult3 = HelperFunctions.createInterval(0, 2);
 		expectedResult3.getLower().setToInfinity();
 
-		assertTrue(computeAdditionResult(interval4, interval2, expectedResult3));
+		assertTrue(HelperFunctions.computeAdditionResult(interval4, interval2, expectedResult3));
 
 		// Interval [\-infty, \infty]
 		IntervalDomainValue infinite = new IntervalDomainValue();
@@ -173,44 +95,44 @@ public class IntervalDomainArithmeticTest {
 		assertTrue(infinite.getUpper().isInfinity());
 		assertTrue(infinite.isUnbounded());
 
-		assertTrue(computeAdditionResult(infinite, infinite, infinite));
+		assertTrue(HelperFunctions.computeAdditionResult(infinite, infinite, infinite));
 	}
 
 	@Test
 	public void testNegativeIntervalAddition() {
 
 		// Interval [-30;-20]
-		IntervalDomainValue interval1 = createInterval(-30, -20);
+		IntervalDomainValue interval1 = HelperFunctions.createInterval(-30, -20);
 
 		// Interval [-5;-1]
-		IntervalDomainValue interval2 = createInterval(-5, -1);
+		IntervalDomainValue interval2 = HelperFunctions.createInterval(-5, -1);
 
 		// Result should be [-35;-21]
-		IntervalDomainValue expectedResult = createInterval(-35, -21);
+		IntervalDomainValue expectedResult = HelperFunctions.createInterval(-35, -21);
 
-		assertTrue(computeAdditionResult(interval1, interval2, expectedResult));
+		assertTrue(HelperFunctions.computeAdditionResult(interval1, interval2, expectedResult));
 	}
 
 	@Test
 	public void testMixedNegativeIntervalAddition() {
 		// Interval [-30; -20]
-		IntervalDomainValue interval1 = createInterval(-30, -20);
+		IntervalDomainValue interval1 = HelperFunctions.createInterval(-30, -20);
 
 		// Interval [5; 10]
-		IntervalDomainValue interval2 = createInterval(5, 10);
+		IntervalDomainValue interval2 = HelperFunctions.createInterval(5, 10);
 
 		// Result should be [-25;-10]
-		IntervalDomainValue expectedResult = createInterval(-25, -10);
+		IntervalDomainValue expectedResult = HelperFunctions.createInterval(-25, -10);
 
-		assertTrue(computeAdditionResult(interval1, interval2, expectedResult));
+		assertTrue(HelperFunctions.computeAdditionResult(interval1, interval2, expectedResult));
 
 		// Interval [-10; 5]
-		IntervalDomainValue interval3 = createInterval(-10, 5);
+		IntervalDomainValue interval3 = HelperFunctions.createInterval(-10, 5);
 
 		// Result should be [-5; 15]
-		IntervalDomainValue expectedResult1 = createInterval(-5, 15);
+		IntervalDomainValue expectedResult1 = HelperFunctions.createInterval(-5, 15);
 
-		assertTrue(computeAdditionResult(interval3, interval2, expectedResult1));
+		assertTrue(HelperFunctions.computeAdditionResult(interval3, interval2, expectedResult1));
 	}
 
 	@Test
@@ -221,15 +143,15 @@ public class IntervalDomainArithmeticTest {
 		assertFalse(interval1.isInfinity());
 
 		// Interval [0, 1]
-		IntervalDomainValue interval2 = createInterval(0, 1);
+		IntervalDomainValue interval2 = HelperFunctions.createInterval(0, 1);
 
 		// Result should be \bot
 		IntervalDomainValue expectedResult = new IntervalDomainValue(true);
 
-		assertTrue(computeAdditionResult(interval1, interval2, expectedResult));
+		assertTrue(HelperFunctions.computeAdditionResult(interval1, interval2, expectedResult));
 
-		assertTrue(computeAdditionResult(interval2, interval1, expectedResult));
+		assertTrue(HelperFunctions.computeAdditionResult(interval2, interval1, expectedResult));
 
-		assertTrue(computeAdditionResult(interval1, interval1, expectedResult));
+		assertTrue(HelperFunctions.computeAdditionResult(interval1, interval1, expectedResult));
 	}
 }
