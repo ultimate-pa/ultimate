ac6fde98754b7123842dd2f9ba227a9225e4abda
Claus Schätzle <Claus_Schaetzle@web.de>
2015-11-17 23:24:36 +0100

    Upper and lower are stored in one array

--- trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/valuedomain/octagon/OctMatrix.java
+++ trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/valuedomain/octagon/OctMatrix.java
@@ -3,81 +3,109 @@ package de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretat
 import java.util.Arrays;
 import java.util.function.BiFunction;
 
-// Stores entries for variables +v1, -v1, ..., +vn, -vn
-// Matrix is always coherent
-// Elements on the main diagonal are not stored and have value 0 at all times
+/**
+ * Matrix for octagons (as presented by Antoine Mine).
+ * <p>
+ * 
+ * TODO More documentation
+ * TODO Stores entries for variables +v1, -v1, ..., +vn, -vn
+ * TODO Variables (names, type, ...) aren't stored
+ * TODO Matrix is divided into 2x2 blocks
+ * 
+ * This class ensures that all elements on the main diagonal are 0
+ * and each matrix is coherent (as seen in the following ASCII art).
+ * <pre>
+ *     \ .   D B   L J  
+ *     . \   C A   K I  
+ *                 
+ *     A B   \ .   W Y  
+ *     C D   . \   Z X  
+ *                 
+ *     I J   X Y   \ .  
+ *     K L   Z W   . \ 
+ * </pre>
+ * 
+ * @author schaetzc@informatik.uni-freiburg.de
+ */
 public class OctMatrix {
 
-	// Only some parts of the matrix are stored: upper (U) and lower (L).
-	//
-	// \ U . . . .   <= OctagonMatrix for 3 variables
-	// L \ . . . .      Each variable comes in two flavors: + and -
-	// L L \ U . .      Matrix is divided into 2x2 blocks
-	// L L L \ . .      Variables (names, type, ...) aren't stored in OctMatrix
-	// L L L L \ U
-	// L L L L L \
+	/** Size of this matrix (size = #rows = #columns). */
+	private final int mSize;
 
-	// part above the main diagonal, not coherent to any elements from mLower
-	private OctValue[] mUpper;
+	/** Offset of the strictly lower triangular matrix in {@link #mElements}. */
+	private final int mOffsetLower;
 	
-	// lower triangular part of the matrix, without the main diagonal
-	// stored row-wise from top to bottom, left to right
-	private OctValue[] mLower;
-	
-	private int mSize;
+	/**
+	 * Stores the elements of this matrix.
+	 * <p>
+	 * Some elements are neglected because they are constant or coherent to
+	 * other elements (see documentation of this class). The remaining
+	 * elements are either part of "lower" (L) or "upper" (U) as seen in the
+	 * following ASCII art.
+	 * 
+	 * <pre>
+	 *     \ U - - - -            \ 0 - - - - 
+	 *     L \ - - - -            3 \ - - - - 
+	 *     L L \ U - -            4 5 \ 1 - - 
+	 *     L L L \ - -            6 7 8 \ - - 
+	 *     L L L L \ U            9 . . . \ 2 
+	 *     L L L L L \            . . . . . \
+	 *     stored parts             indexing
+	 *    (only L and U)
+	 *
+	 * mElements: [ 0 1 2 3 4 5 6 7 8 9 ... ]
+	 *              `-U-´ `-------L-------´
+	 * </pre>
+	 */
+	private final OctValue[] mElements;
+
 
 	public OctMatrix(OctMatrix octMatrix) {
 		mSize = octMatrix.mSize;
-		mUpper = new OctValue[octMatrix.mUpper.length];
-		mLower = new OctValue[octMatrix.mLower.length];
-		Arrays.fill(mUpper, OctValue.INFINITY);
-		Arrays.fill(mLower, OctValue.INFINITY);
+		mOffsetLower = octMatrix.mOffsetLower;
+		mElements = new OctValue[octMatrix.mElements.length];
+		System.arraycopy(octMatrix.mElements, 0, mElements, 0, mElements.length);
 	}
 	
 	public OctMatrix(int size) {
+		assert (size & 1) == 0 : "Octagon matrices must have an even numbered size.";
 		mSize = size;
-		mUpper = new OctValue[size / 2];
-		mLower = new OctValue[size * (size - 1) / 2];
-		Arrays.fill(mUpper, OctValue.INFINITY);
-		Arrays.fill(mLower, OctValue.INFINITY);
+		mOffsetLower = size / 2;
+		mElements = new OctValue[mOffsetLower + size * (size - 1) / 2];
+		Arrays.fill(mElements, OctValue.INFINITY);
 	}
-	
+
 	public OctValue get(int row, int col) {
-		if (row > col)
-			return mLower[lowerIndex(row, col)];
 		if (row == col)
 			return OctValue.ZERO;
-		if ((row ^ col) == 1) // && row < col
-			return mUpper[col / 2];
-		return mLower[lowerIndex(col ^ 1, row ^ 1)];
+		return mElements[indexOf(row, col)];
 	}
 	
 	protected void set(int row, int col, OctValue value) {
 		assert value != null : "null is not a valid matrix element.";
-		if (row > col) {
-			mLower[lowerIndex(row, col)] = value;
-		} else if (row == col) {
-			throw new UnsupportedOperationException("Elements on main diagonal are constant and cannot be set.");
-		} else if ((row ^ col) == 1) { // && row < col
-			mUpper[col / 2] = value;
-		} else {
-			mLower[lowerIndex(col ^ 1, row ^ 1)] = value;
-		}
+		mElements[indexOf(row, col)] = value;
 	}
 
-	private int lowerIndex(int row, int col) {
-		//       pyramid above row + offset in row
-		return (row - 1) * row / 2 + col;
+	private int indexOf(int row, int col) {
+		if (row > col)
+			return indexOfLower(row, col);
+		if (row == col)
+			return -1;
+		if ((row ^ col) == 1) // && row < col
+			return col / 2;
+		return indexOfLower(col ^ 1, row ^ 1);
+	}
+
+	private int indexOfLower(int row, int col) {
+		//                      triangle above row   +  offset in row
+		return mOffsetLower  +  (row - 1) * row / 2  +  col;
 	}
 	
 	public OctMatrix elementwise(OctMatrix other, BiFunction<OctValue, OctValue, OctValue> operator) {
 		assert other.mSize == mSize : "Incompatible matrices";
 		OctMatrix result = new OctMatrix(mSize);
-		// loops are faster than streams
-		for (int i = 0; i < mUpper.length; ++i)
-			result.mUpper[i] = operator.apply(mUpper[i], other.mUpper[i]);
-		for (int i = 0; i < mLower.length; ++i)
-			result.mLower[i] = operator.apply(mLower[i], other.mLower[i]);
+		for (int i = 0; i < mElements.length; ++i) // a loop is faster than a stream
+			result.mElements[i] = operator.apply(mElements[i], other.mElements[i]);
 		return result;
 	}
 
