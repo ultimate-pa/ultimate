0a284b475d80f684d3d0138802a0e42507c20b91
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2015-11-18 22:41:53 +0100

    some commented code that we might use in the future

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/cHandler/TypeSizes.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/cHandler/TypeSizes.java
@@ -157,6 +157,9 @@ public class TypeSizes {
 	}
 	
 	public BigInteger getMaxValueOfPrimitiveType(CPrimitive cPrimitive) {
+//		if (cPrimitive.getType() == PRIMITIVE.BOOL) {
+//			return BigInteger.ONE;
+//		}
 		int byteSize = getSize(cPrimitive.getType());
 		BigInteger maxValue;
 		if (cPrimitive.isUnsigned()) {
