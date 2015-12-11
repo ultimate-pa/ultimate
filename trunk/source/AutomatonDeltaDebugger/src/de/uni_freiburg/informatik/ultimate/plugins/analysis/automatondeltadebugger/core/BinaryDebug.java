905036a9bcce9b546c4fcb930a95b5798f45a577
schillic <schillic@informatik.uni-freiburg.de>
2015-12-11 13:31:34 +0100

    outsourced automaton delta debugger from automata library to a new plugin Also fixed a bug in the NormalizeStateShrinker.

---
+++ trunk/source/AutomatonDeltaDebugger/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/automatondeltadebugger/core/BinaryDebug.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright (C) 2015 Christian Schilling <schillic@informatik.uni-freiburg.de>
+ * Copyright (C) 2009-2015 University of Freiburg
+ * 
+ * This file is part of the ULTIMATE Automaton Delta Debugger.
+ * 
+ * The ULTIMATE Automaton Delta Debugger is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * The ULTIMATE Automaton Delta Debugger is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automaton Delta Debugger. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7: If you modify the
+ * ULTIMATE Automaton Delta Debugger, or any covered work, by linking or
+ * combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automaton Delta Debugger grant you additional
+ * permission to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.plugins.analysis.automatondeltadebugger.core;
+
+import java.util.ArrayDeque;
+import java.util.List;
+
+import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.INestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatondeltadebugger.shrinkers.AShrinker;
+
+/**
+ * Reduces a list of objects in a binary search manner until a local minimum is
+ * found.
+ * 
+ * Note that the local minimum is only according to the current shrinker, i.e.,
+ * the respective shrinker cannot be applied to a subinterval of objects anymore
+ * while still producing the error. However, removing, say, objects 1 and 3
+ * might still work.
+ * 
+ * @author Christian Schilling <schillic@informatik.uni-freiburg.de>
+ */
+public class BinaryDebug<T, LETTER, STATE> {
+	/**
+	 * Left and right bound for the list of objects
+	 */
+	private class SublistBounds {
+		final int mLeft, mRight;
+		final boolean mIsLhs;
+
+		SublistBounds(final int left, final int right, final boolean isLhs) {
+			this.mLeft = left;
+			this.mRight = right;
+			this.mIsLhs = isLhs;
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder b = new StringBuilder();
+			b.append("(");
+			b.append(mLeft);
+			b.append(", ");
+			b.append(mRight);
+			b.append(")");
+			if (mIsLhs) {
+				b.append("*");
+			}
+			return b.toString();
+		}
+	}
+
+	private final ATester<LETTER, STATE> mTester;
+	private final AShrinker<T, LETTER, STATE> mShrinker;
+	private final ArrayDeque<SublistBounds> mStack;
+
+	/**
+	 * @param tester tester
+	 * @param shrinker shrinker
+	 */
+	public BinaryDebug(final ATester<LETTER, STATE> tester,
+			final AShrinker<T, LETTER, STATE> shrinker) {
+		this.mTester = tester;
+		this.mShrinker = shrinker;
+		this.mStack = new ArrayDeque<SublistBounds>();
+	}
+
+	/**
+	 * Splits a list into two sublists of equal size.
+	 * 
+	 * @param bounds bounds
+	 */
+	private void split(final SublistBounds bounds) {
+		final int left = bounds.mLeft;
+		final int right = bounds.mRight;
+
+		// do not split intervals of size <= 1 (useless and would run forever)
+		if (right - left <= 1) {
+			return;
+		}
+
+		final int mid = (left + right) / 2;
+		final boolean isLhs;
+		if (mid < right) {
+			mStack.push(new SublistBounds(mid, right, false));
+			isLhs = true;
+		} else {
+			isLhs = false;
+		}
+		if (left < mid) {
+			mStack.push(new SublistBounds(left, mid, isLhs));
+		}
+	}
+
+	/**
+	 * Runs the binary search for the current shrinker.
+	 * 
+	 * @return true iff the sublist could be shrunk
+	 */
+	public boolean run() {
+		boolean result = false;
+		List<T> list = mShrinker.extractList();
+		if (list.isEmpty()) {
+			return result;
+		}
+		mStack.add(new SublistBounds(0, list.size(), false));
+		while (!mStack.isEmpty()) {
+			final SublistBounds sb = mStack.poll();
+			final List<T> sublist = list.subList(sb.mLeft, sb.mRight);
+			if (sublist.isEmpty()) {
+				continue;
+			}
+
+			// initialize test for the sublist
+			final INestedWordAutomaton<LETTER, STATE> automaton =
+					mShrinker.createAutomaton(sublist);
+
+			// run test
+			final boolean isTestSuccessful = mTester.test(automaton);
+
+			if (isTestSuccessful) {
+				// error reproduced
+				mShrinker.error(automaton);
+				result = true;
+				if (sb.mIsLhs) {
+					/*
+					 * When the success happened on the first half part, we can
+					 * remove the corresponding second half part and directly
+					 * work on its children. This is because by removing the
+					 * second half part we would have removed the whole sublist
+					 * and this was, by construction, already unsuccessful in a
+					 * previous test.
+					 */
+					split(mStack.poll());
+				}
+			} else {
+				// error not reproduced => split list into two parts
+				mShrinker.noError(automaton);
+				split(sb);
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName();
+	}
+}
--- trunk/source/AutomatonDeltaDebugger/src/de/uni_freiburg/informatik/ultimate/plugins/source/automatondeltadebugger/shrinkers/NormalizeStateShrinker.java
+++ trunk/source/AutomatonDeltaDebugger/src/de/uni_freiburg/informatik/ultimate/plugins/source/automatondeltadebugger/shrinkers/NormalizeStateShrinker.java
@@ -2,29 +2,30 @@
  * Copyright (C) 2015 Christian Schilling <schillic@informatik.uni-freiburg.de>
  * Copyright (C) 2009-2015 University of Freiburg
  * 
- * This file is part of the ULTIMATE Automata Library.
+ * This file is part of the ULTIMATE Automaton Delta Debugger.
  * 
- * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * The ULTIMATE Automaton Delta Debugger is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  * 
- * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * The ULTIMATE Automaton Delta Debugger is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ * along with the ULTIMATE Automaton Delta Debugger. If not, see
+ * <http://www.gnu.org/licenses/>.
  * 
- * Additional permission under GNU GPL version 3 section 7:
- * If you modify the ULTIMATE Automata Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Automata Library grant you additional permission 
- * to convey the resulting work.
+ * Additional permission under GNU GPL version 3 section 7: If you modify the
+ * ULTIMATE Automaton Delta Debugger, or any covered work, by linking or
+ * combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automaton Delta Debugger grant you additional
+ * permission to convey the resulting work.
  */
-package de.uni_freiburg.informatik.ultimate.automata.nwalibrary.deltaDebug;
+package de.uni_freiburg.informatik.ultimate.plugins.source.automatondeltadebugger.shrinkers;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -43,10 +44,9 @@ import de.uni_freiburg.informatik.ultimate.automata.nwalibrary.transitions.Outgo
 /**
  * Normalizes state names.
  * 
- * This shrinker renames states in the following way:
- * - All initial states are named "q0_i" for some i.
- * - All other final states are named "qf_i" for some i.
- * - All other states are named "q_i" for some i.
+ * This shrinker renames states in the following way: - All initial states are
+ * named "q0_i" for some i. - All other final states are named "qf_i" for some
+ * i. - All other states are named "q_i" for some i.
  * 
  * For each of those three types of states the index is a consecutive number,
  * assigned in a depth-first manner starting from some initial state.
@@ -57,26 +57,25 @@ public class NormalizeStateShrinker<LETTER, STATE>
 		extends AShrinker<STATE, LETTER, STATE> {
 	@SuppressWarnings("unchecked")
 	@Override
-	public INestedWordAutomaton<LETTER, STATE> createAutomaton(
-			final List<STATE> list) {
+	public INestedWordAutomaton<LETTER, STATE>
+			createAutomaton(final List<STATE> list) {
 		// create fresh automaton
-		final INestedWordAutomaton<LETTER, STATE> automaton =
-				m_factory.create();
-		
+		final INestedWordAutomaton<LETTER, STATE> automaton = mFactory.create();
+
 		// remaining original states
 		final HashSet<STATE> remaining =
-				new HashSet<STATE>(m_automaton.getStates());
+				new HashSet<STATE>(mAutomaton.getStates());
 		final HashSet<STATE> toVisit = new HashSet<STATE>();
 		final ArrayDeque<STATE> stack = new ArrayDeque<STATE>();
 		for (final STATE state : list) {
 			remaining.remove(state);
-			if (m_automaton.isInitial(state)) {
+			if (mAutomaton.isInitial(state)) {
 				stack.add(state);
 			} else {
 				toVisit.add(state);
 			}
 		}
-		
+
 		// depth-first renaming of states
 		final HashMap<STATE, STATE> old2new = new HashMap<STATE, STATE>();
 		final HashSet<STATE> onStackOrVisited = new HashSet<STATE>(stack);
@@ -88,8 +87,11 @@ public class NormalizeStateShrinker<LETTER, STATE>
 			if (stack.isEmpty()) {
 				if (firstRun) {
 					firstRun = false;
-					stack.addAll(toVisit);
-					onStackOrVisited.addAll(toVisit);
+					for (final STATE state : toVisit) {
+						if (onStackOrVisited.add(state)) {
+							stack.push(state);
+						}
+					}
 					stack.addAll(remaining);
 					onStackOrVisited.addAll(remaining);
 					continue;
@@ -100,6 +102,9 @@ public class NormalizeStateShrinker<LETTER, STATE>
 			final STATE oldState = stack.pop();
 			final STATE newState;
 			
+			final boolean isInitial = mAutomaton.isInitial(oldState);
+			final boolean isFinal = mAutomaton.isFinal(oldState);
+			
 			if (remaining.remove(oldState)) {
 				// do not reassign this state name
 				newState = oldState;
@@ -108,76 +113,76 @@ public class NormalizeStateShrinker<LETTER, STATE>
 				assert (oldState instanceof String) :
 					"The state was a string during list creation.";
 				final String name;
-				if (m_automaton.isInitial(oldState)) {
+				if (isInitial) {
 					name = "q0_" + ++initials;
-				} else if (m_automaton.isFinal(oldState)) {
+				} else if (isFinal) {
 					name = "qf_" + ++finals;
 				} else {
 					name = "q_" + ++normals;
 				}
 				newState = (STATE) name;
 			}
-			
+
 			old2new.put(oldState, newState);
-			m_factory.addState(automaton, newState);
-			
+			mFactory.addState(automaton, newState, isInitial, isFinal);
+
 			// push successors which have not been visited
 			for (final OutgoingInternalTransition<LETTER, STATE> trans :
-					m_automaton.internalSuccessors(oldState)) {
+					mAutomaton.internalSuccessors(oldState)) {
 				final STATE succ = trans.getSucc();
-				if (! onStackOrVisited.contains(succ)) {
+				if (!onStackOrVisited.contains(succ)) {
 					stack.push(succ);
 					onStackOrVisited.add(succ);
 				}
 			}
 			for (final OutgoingCallTransition<LETTER, STATE> trans :
-					m_automaton.callSuccessors(oldState)) {
+					mAutomaton.callSuccessors(oldState)) {
 				final STATE succ = trans.getSucc();
-				if (! onStackOrVisited.contains(succ)) {
+				if (!onStackOrVisited.contains(succ)) {
 					stack.push(succ);
 					onStackOrVisited.add(succ);
 				}
 			}
 			for (final OutgoingReturnTransition<LETTER, STATE> trans :
-					m_automaton.returnSuccessors(oldState)) {
+					mAutomaton.returnSuccessors(oldState)) {
 				final STATE succ = trans.getSucc();
-				if (! onStackOrVisited.contains(succ)) {
+				if (!onStackOrVisited.contains(succ)) {
 					stack.push(succ);
 					onStackOrVisited.add(succ);
 				}
 			}
 		}
-		assert (automaton.size() == m_automaton.size()) :
-			"The number of states must be retained.";
-		
+		assert (automaton.size() == mAutomaton
+				.size()) : "The number of states must be retained.";
+
 		// add transitions
 		for (final Entry<STATE, STATE> entry : old2new.entrySet()) {
 			final STATE oldState = entry.getKey();
 			final STATE newState = entry.getValue();
 			for (final OutgoingInternalTransition<LETTER, STATE> trans :
-					m_automaton.internalSuccessors(oldState)) {
-				m_factory.addInternalTransition(automaton, newState,
+					mAutomaton.internalSuccessors(oldState)) {
+				mFactory.addInternalTransition(automaton, newState,
 						trans.getLetter(), old2new.get(trans.getSucc()));
 			}
 			for (final OutgoingCallTransition<LETTER, STATE> trans :
-					m_automaton.callSuccessors(oldState)) {
-				m_factory.addCallTransition(automaton, newState,
+					mAutomaton.callSuccessors(oldState)) {
+				mFactory.addCallTransition(automaton, newState,
 						trans.getLetter(), old2new.get(trans.getSucc()));
 			}
 			for (final OutgoingReturnTransition<LETTER, STATE> trans :
-					m_automaton.returnSuccessors(oldState)) {
-				m_factory.addReturnTransition(automaton, newState,
+					mAutomaton.returnSuccessors(oldState)) {
+				mFactory.addReturnTransition(automaton, newState,
 						old2new.get(trans.getHierPred()), trans.getLetter(),
 						old2new.get(trans.getSucc()));
 			}
 		}
-		
+
 		return automaton;
 	}
-	
+
 	@Override
 	public List<STATE> extractList() {
-		final Set<STATE> states = m_automaton.getStates();
+		final Set<STATE> states = mAutomaton.getStates();
 		final Iterator<STATE> iterator = states.iterator();
 		if (iterator.hasNext()) {
 			if (iterator.next() instanceof String) {
