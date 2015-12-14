756233c2c02618d5c1af3d4ff8578f5d04eae19a
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2015-12-14 10:02:37 +0100

    removed old AI projects (should all be merged in AIv2) and dependency in TA postpone renaming until merging with wip/oct fixed classpath of website

--- trunk/source/AbstractInterpretationMk2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationMk2/abstractdomain/IAbstractOperator.java
+++
@@ -1,5 +0,0 @@
-package de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationMk2.abstractdomain;
-
-public interface IAbstractOperator<T> {
-	IAbstractState<T> apply(IAbstractState<T> a, IAbstractState<T> b);
-}
--- trunk/source/AbstractInterpretationMk2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationMk2/valuedomain/IValueOperator.java
+++
@@ -1,6 +0,0 @@
-package de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationMk2.valuedomain;
-
-public interface IValueOperator<T> {
-	public IAbstractValue<T> apply(IAbstractValue<?> valueA,
-			IAbstractValue<?> valueB);
-}
