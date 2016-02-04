580627851d10d56690b52da249d57151bd89c0e4
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2016-02-04 10:41:03 +0100

    current bugs

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/evals/AbstractInterpretationDevelTestSuite.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/evals/AbstractInterpretationDevelTestSuite.java
@@ -73,56 +73,62 @@ public class AbstractInterpretationDevelTestSuite extends AbstractEvalTestSuite 
 	private static final String[] INPUT = new String[] {
 			/* ULTIMATE repo */
 //			 "examples/programs/abstractInterpretation/",
-			 "examples/programs/abstractInterpretation/regression",
+//			 "examples/programs/abstractInterpretation/regression",
 //			"examples/programs/abstractInterpretation/regression/whileProcedure.bpl",
 
-			// ########### Bugs ###########
-			// Here are representatives of current bugs 
+			// ################################# Bugs #########################
+			// ########### Here are representatives of current bugs ########### 
 
 			 
-			//no exact representable decimal result (106 total) 
-			 "examples/svcomp/eca-rers2012/Problem16_label02_true-unreach-call.c",
-			 "examples/svcomp/eca-rers2012/Problem16_label00_false-unreach-call.c",
+			//nullpointer exception (754 total)
+//			"examples/svcomp/eca-rers2012/Problem14_label24_true-unreach-call.c",
+//			"examples/svcomp/eca-rers2012/Problem10_label15_false-unreach-call.c",
+//			"examples/svcomp/eca-rers2012/Problem10_label00_true-unreach-call.c",
+//			"examples/svcomp/eca-rers2012/Problem16_label02_true-unreach-call.c",
+//			"examples/svcomp/eca-rers2012/Problem16_label00_false-unreach-call.c",
+			 
+//			//no exact representable decimal result (46 total) 
+//			"examples/svcomp/product-lines/elevator_spec13_product21_true-unreach-call.cil.c",
+//			"examples/svcomp/product-lines/elevator_spec13_product22_true-unreach-call.cil.c",
+//			"examples/svcomp/product-lines/elevator_spec13_product23_true-unreach-call.cil.c",
+//			"examples/svcomp/product-lines/elevator_spec13_product24_true-unreach-call.cil.c",
+//			"examples/svcomp/product-lines/elevator_spec13_product29_true-unreach-call.cil.c",
+//			"examples/svcomp/product-lines/elevator_spec13_product30_true-unreach-call.cil.c",
 //			 
-//			 //NoSuchElementException: No value present: java.util.Optional.get(Optional.java:135) (16 total)
-			 "examples/svcomp/recursive-simple/fibo_2calls_10_true-unreach-call.c",
-			 "examples/svcomp/recursive-simple/id2_b3_o5_true-unreach-call.c",
-
-//			 //ArrayIndexOutOfBoundsException (all)
-			 "examples/svcomp/systemc/transmitter.01_false-unreach-call_false-termination.cil.c",
-			 "examples/svcomp/systemc/transmitter.02_false-unreach-call_false-termination.cil.c",
-			 "examples/svcomp/systemc/transmitter.03_false-unreach-call_false-termination.cil.c",
-			 "examples/svcomp/systemc/transmitter.04_false-unreach-call_false-termination.cil.c",
-			 "examples/svcomp/systemc/transmitter.05_false-unreach-call_false-termination.cil.c",
-			 "examples/svcomp/systemc/transmitter.06_false-unreach-call_false-termination.cil.c",
-			 "examples/svcomp/systemc/transmitter.07_false-unreach-call_false-termination.cil.c",
-			 "examples/svcomp/systemc/transmitter.08_false-unreach-call_false-termination.cil.c",
+//			//NoSuchElementException: No value present: java.util.Optional.get(Optional.java:135) (16 total)
+//			"examples/svcomp/recursive-simple/fibo_2calls_10_true-unreach-call.c",
+//			"examples/svcomp/recursive-simple/id2_b3_o5_true-unreach-call.c",
 //
-//			 //unsoundness (all) 
-			 "examples/svcomp/recursive-simple/id_i10_o10_false-unreach-call.c",
-			 "examples/svcomp/recursive-simple/id_i15_o15_false-unreach-call.c",
-			 "examples/svcomp/recursive-simple/id_i20_o20_false-unreach-call.c",
-			 "examples/svcomp/recursive-simple/id_i25_o25_false-unreach-call.c",
-			 "examples/svcomp/recursive-simple/id_o1000_false-unreach-call.c",
-			 "examples/svcomp/recursive-simple/id_o100_false-unreach-call.c",
-			 "examples/svcomp/recursive-simple/id_o10_false-unreach-call.c",
-			 "examples/svcomp/recursive-simple/id_o200_false-unreach-call.c",
-			 "examples/svcomp/recursive-simple/id_o20_false-unreach-call.c",
+//			//ArrayIndexOutOfBoundsException (all)
+//			 "examples/svcomp/systemc/transmitter.01_false-unreach-call_false-termination.cil.c",
+//			 "examples/svcomp/systemc/transmitter.02_false-unreach-call_false-termination.cil.c",
+//			 "examples/svcomp/systemc/transmitter.03_false-unreach-call_false-termination.cil.c",
+//			 "examples/svcomp/systemc/transmitter.04_false-unreach-call_false-termination.cil.c",
+//			 "examples/svcomp/systemc/transmitter.05_false-unreach-call_false-termination.cil.c",
+//			 "examples/svcomp/systemc/transmitter.06_false-unreach-call_false-termination.cil.c",
+//			 "examples/svcomp/systemc/transmitter.07_false-unreach-call_false-termination.cil.c",
+//			 "examples/svcomp/systemc/transmitter.08_false-unreach-call_false-termination.cil.c",
+//
+//			//unsoundness (all) 
+//			 "examples/svcomp/loops/linear_search_false-unreach-call.i",
+//			 "examples/svcomp/loops/verisec_OpenSER__cases1_stripFullBoth_arr_false-unreach-call.i",
+//			 "examples/svcomp/recursive-simple/id_i10_o10_false-unreach-call.c",
+//			 "examples/svcomp/recursive-simple/id_i15_o15_false-unreach-call.c",
+//			 "examples/svcomp/recursive-simple/id_i20_o20_false-unreach-call.c",
+//			 "examples/svcomp/recursive-simple/id_i25_o25_false-unreach-call.c",
+//			 "examples/svcomp/recursive-simple/id_o1000_false-unreach-call.c",
+//			 "examples/svcomp/recursive-simple/id_o100_false-unreach-call.c",
+//			 "examples/svcomp/recursive-simple/id_o10_false-unreach-call.c",
+//			 "examples/svcomp/recursive-simple/id_o200_false-unreach-call.c",
+//			 "examples/svcomp/recursive-simple/id_o20_false-unreach-call.c",
 //			 "examples/programs/abstractInterpretation/regression/Collatz.bpl",
-//
-//			//AStar does not terminate (246 total) 
-			"examples/svcomp/locks/test_locks_12_true-unreach-call_false-termination.c",
-			"examples/svcomp/locks/test_locks_9_true-unreach-call.c",
-			"examples/svcomp/product-lines/email_spec0_product16_false-unreach-call.cil.c",
-			"examples/svcomp/product-lines/email_spec4_productSimulator_false-unreach-call.cil.c",
-			"examples/svcomp/seq-mthreaded/rekh_nxt_false-unreach-call.1.M1.c",
 	};
 
 	// @formatter:on
 
 	@Override
 	protected long getTimeout() {
-		return 15 * 1000 ;
+		return 15 * 1000;
 	}
 
 	@Override
