e8bedf458f1b5d9cc1ab3620892146fee155c3bf
Claus Schätzle <schaetzc@informatik.uni-freiburg.de>
2016-02-09 19:18:03 +0100

    removed debug output

--- trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/relational/octagon/OctPostOperator.java
+++ trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/relational/octagon/OctPostOperator.java
@@ -13,7 +13,6 @@ import org.apache.log4j.Logger;
 import de.uni_freiburg.informatik.ultimate.boogie.symboltable.BoogieSymbolTable;
 import de.uni_freiburg.informatik.ultimate.model.IType;
 import de.uni_freiburg.informatik.ultimate.model.boogie.IBoogieVar;
-import de.uni_freiburg.informatik.ultimate.model.boogie.ast.AssignmentStatement;
 import de.uni_freiburg.informatik.ultimate.model.boogie.ast.CallStatement;
 import de.uni_freiburg.informatik.ultimate.model.boogie.ast.Declaration;
 import de.uni_freiburg.informatik.ultimate.model.boogie.ast.Expression;
@@ -126,10 +125,6 @@ public class OctPostOperator implements IAbstractPostOperator<OctDomainState, Co
 		List<OctDomainState> currentState = deepCopy(Collections.singletonList(oldState));
 		List<Statement> statements = mHavocBundler.bundleHavocsCached(codeBlock);
 		for (Statement statement : statements) {
-//			if (statement instanceof AssignmentStatement) {
-//				String printedStatement = BoogiePrettyPrinter.print(statement); 
-//				mLogger.warn("Assign " + printedStatement);
-//			}
 			currentState = mStatementProcessor.processStatement(statement, currentState);
 //			mLogger.warn("after " + BoogiePrettyPrinter.print(statement));
 //			mLogger.warn(statement);
