68e723f4e91392db31671b954a9ea0d47ade4f00
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2016-02-24 04:30:29 +0100

    examples

---
+++ trunk/examples/programs/quantifier/regression/c/memset02.c
@@ -0,0 +1,18 @@
+//#Unsafe
+/*
+ * Check that invalid pointer base is detected.
+ * Date: 2016-02-23
+ * Author: heizmann@informtik.uni-freiburg.de
+ * 
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+	unsigned char *p = malloc(3*sizeof(char));
+	free(p);
+	char *s = memset(p, -1, 3*sizeof(char));
+	return 0;
+}
