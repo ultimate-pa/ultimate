97a96bf58598e8d18349cbb6675c9a039d23ada8
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2016-03-09 19:47:29 +0100

    hack to remove acsl from correctness witnesses

--- trunk/source/WitnessPrinter/src/de/uni_freiburg/informatik/ultimate/witnessprinter/graphml/GeneratedWitnessNode.java
+++ trunk/source/WitnessPrinter/src/de/uni_freiburg/informatik/ultimate/witnessprinter/graphml/GeneratedWitnessNode.java
@@ -63,13 +63,17 @@ public class GeneratedWitnessNode {
 	}
 
 	public String getInvariant() {
+		// TODO: Hack to remove invariants with \old or \result
+		if (mInvariant != null && (mInvariant.contains("\\old") || mInvariant.contains("\\result"))) {
+			return null;
+		}
 		return mInvariant;
 	}
 
 	public void setInvariant(final String invariant) {
 		mInvariant = invariant;
 	}
-	
+
 	@Override
 	public String toString() {
 		return mId;
