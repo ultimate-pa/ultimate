da8d3a3e84d6b6361b8e4b759f730c25a8e0b793
Daniel Dietsch <dietsch@informatik.uni-freiburg.de>
2016-03-15 13:03:19 +0100

    switching to IncrementalHoareTripleChecker (partly)

--- trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/algorithm/rcfg/RcfgDebugHelper.java
+++ trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/algorithm/rcfg/RcfgDebugHelper.java
@@ -3,24 +3,19 @@ package de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretat
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.log4j.Logger;
 
 import de.uni_freiburg.informatik.ultimate.core.services.model.IUltimateServiceProvider;
 import de.uni_freiburg.informatik.ultimate.logic.Script;
-import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.Util;
-import de.uni_freiburg.informatik.ultimate.model.boogie.BoogieVar;
 import de.uni_freiburg.informatik.ultimate.model.boogie.IBoogieVar;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.Boogie2SMT;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.ModifiableGlobalVariableManager;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.TransFormula;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicate;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.PredicateUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.TermVarsProc;
 import de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationv2.Activator;
 import de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationv2.algorithm.IDebugHelper;
@@ -29,6 +24,9 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.Cal
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.CodeBlock;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.Return;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.RootAnnot;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.hoaretriple.IHoareTripleChecker;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.hoaretriple.IHoareTripleChecker.Validity;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.hoaretriple.IncrementalHoareTripleChecker;
 
 /**
  * 
@@ -40,47 +38,49 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.Roo
 public class RcfgDebugHelper<STATE extends IAbstractState<STATE, CodeBlock, IBoogieVar>, LOCATION>
 		implements IDebugHelper<STATE, CodeBlock, IBoogieVar, LOCATION> {
 
-	private final Boogie2SMT mBoogie2Smt;
-	private final ModifiableGlobalVariableManager mGlobalVariableManager;
-	private final Script mScript;
 	private final Logger mLogger;
 	private final IUltimateServiceProvider mServices;
+	private final IHoareTripleChecker mHTC;
+	private final Script mScript;
+	private final Boogie2SMT mBoogie2Smt;
 
 	private static int sIllegalPredicates = Integer.MAX_VALUE;
 
 	public RcfgDebugHelper(final RootAnnot rootAnnot, final IUltimateServiceProvider services) {
-		mScript = rootAnnot.getScript();
-		mBoogie2Smt = rootAnnot.getBoogie2SMT();
-		mGlobalVariableManager = rootAnnot.getModGlobVarManager();
 		mServices = services;
+		mScript = rootAnnot.getScript();
 		mLogger = services.getLoggingService().getLogger(Activator.PLUGIN_ID);
+		mBoogie2Smt = rootAnnot.getBoogie2SMT();
+		mHTC = new IncrementalHoareTripleChecker(new ManagedScript(services, mScript), rootAnnot.getModGlobVarManager(),
+				mBoogie2Smt);
 	}
 
 	@Override
 	public boolean isPostSound(final STATE pre, final List<STATE> postStates, final CodeBlock transition) {
-		if (transition instanceof Call || transition instanceof Return) {
-			// TODO: The current hoare triple checker is not usable for call/return, so we just say its sound (but it
-			// should be anyways)
-			return true;
-		}
 		final IPredicate precond = createPredicateFromState(Collections.singletonList(pre));
 		final IPredicate postcond = createPredicateFromState(postStates);
-		final TransFormula tf = transition.getTransitionFormula();
-		final Set<BoogieVar> modifiableGlobalsBefore = mGlobalVariableManager
-				.getModifiedBoogieVars(transition.getPreceedingProcedure());
-		final Set<BoogieVar> modifiableGlobalsAfter = mGlobalVariableManager
-				.getModifiedBoogieVars(transition.getSucceedingProcedure());
+		final Validity result;
 
-		final LBool result = PredicateUtils.isInductiveHelper(mBoogie2Smt, precond, postcond, tf,
-				modifiableGlobalsBefore, modifiableGlobalsAfter);
-		
-		if (result != LBool.UNSAT) {
-			mLogger.fatal("Soundness check failed for the following triple:");
-			mLogger.fatal("Pre: {" + SmtUtils.simplify(mScript, precond.getFormula(), mServices).toStringDirect() + "}");
-			mLogger.fatal(tf.getFormula().toStringDirect());
-			mLogger.fatal("Post: {" + SmtUtils.simplify(mScript, postcond.getFormula(), mServices).toStringDirect() + "}");
+		if (transition instanceof Call) {
+			result = mHTC.checkCall(precond, transition, postcond);
+		} else if (transition instanceof Return) {
+//			result = mHTC.checkReturn(preLin, preHier, cb, succ)(precond, transition, postcond);
+			//TODO: How to check return? 
+			result = Validity.VALID;
+		} else {
+			result = mHTC.checkInternal(precond, transition, postcond);
 		}
-		return result == LBool.UNSAT;
+		mHTC.releaseLock();
+
+		if (result != Validity.VALID) {
+			mLogger.fatal("Soundness check failed for the following triple:");
+			mLogger.fatal(
+					"Pre: {" + SmtUtils.simplify(mScript, precond.getFormula(), mServices).toStringDirect() + "}");
+			mLogger.fatal(transition.getTransitionFormula().getFormula().toStringDirect());
+			mLogger.fatal(
+					"Post: {" + SmtUtils.simplify(mScript, postcond.getFormula(), mServices).toStringDirect() + "}");
+		}
+		return result == Validity.VALID;
 	}
 
 	private IPredicate createPredicateFromState(Collection<STATE> states) {
