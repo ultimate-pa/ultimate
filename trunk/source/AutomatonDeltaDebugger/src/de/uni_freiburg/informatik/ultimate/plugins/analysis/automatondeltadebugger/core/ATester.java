6472db872a4880875f8ce51c2cbdabb8bc4cee08
schillic <schillic@informatik.uni-freiburg.de>
2016-05-27 16:24:17 +0200

    ...apparently it was not so useless...

--- trunk/source/AutomatonDeltaDebugger/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/automatondeltadebugger/core/ATester.java
+++ trunk/source/AutomatonDeltaDebugger/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/automatondeltadebugger/core/ATester.java
@@ -79,7 +79,8 @@ public abstract class ATester<LETTER, STATE> {
 	 * @throws any
 	 *             type of throwable
 	 */
-	public abstract void execute(final INestedWordAutomaton<LETTER, STATE> automaton);
+	@SuppressWarnings("squid:S00112")
+	public abstract void execute(final INestedWordAutomaton<LETTER, STATE> automaton) throws Throwable;
 
 	@Override
 	public String toString() {
