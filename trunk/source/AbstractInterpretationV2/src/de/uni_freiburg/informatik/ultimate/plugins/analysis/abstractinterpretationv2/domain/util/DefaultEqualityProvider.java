fa2b10edecc7bbe74310574754d05407cd64cabd
Marius Greitschus <greitsch@informatik.uni-freiburg.de>
2016-06-02 17:27:33 +0200

    Documentation and Sonar cleanup

--- trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/util/DefaultEqualityProvider.java
+++ trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/util/DefaultEqualityProvider.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (C) 2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ * Copyright (C) 2015 Marius Greitschus (greitsch@informatik.uni-freiburg.de)
+ * Copyright (C) 2015 University of Freiburg
+ * 
+ * This file is part of the ULTIMATE AbstractInterpretationV2 plug-in.
+ * 
+ * The ULTIMATE AbstractInterpretationV2 plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The ULTIMATE AbstractInterpretationV2 plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE AbstractInterpretationV2 plug-in. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE AbstractInterpretationV2 plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
+ * containing parts covered by the terms of the Eclipse Public License, the 
+ * licensors of the ULTIMATE AbstractInterpretationV2 plug-in grant you additional permission 
+ * to convey the resulting work.
+ */
+
 package de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationv2.domain.util;
 
 import java.util.ArrayList;
@@ -31,6 +59,14 @@ public class DefaultEqualityProvider<STATE extends IAbstractState<STATE, CodeBlo
 	private final IAbstractPostOperator<STATE, CodeBlock, IBoogieVar> mPostOperator;
 	private final CodeBlockFactory mCodeBlockFactory;
 
+	/**
+	 * Creates an instance of a default Equality Provider for Boogie-based abstract domains.
+	 * 
+	 * @param postOperator
+	 *            The post operator of the abstract domain.
+	 * @param rootAnnotation
+	 *            The root annotation of the program.
+	 */
 	public DefaultEqualityProvider(final IAbstractPostOperator<STATE, CodeBlock, IBoogieVar> postOperator,
 	        final RootAnnot rootAnnotation) {
 		mPostOperator = postOperator;
