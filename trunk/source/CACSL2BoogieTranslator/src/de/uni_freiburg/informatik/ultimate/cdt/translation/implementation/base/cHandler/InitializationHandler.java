97756eff27fe35feb3f6e325b2403ec8c12a595e
Maximilian Rohland <max@rohlandm.de>
2016-06-10 13:48:59 +0200

    exceptions for not supported floating types
    in InitializationHandler

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/cHandler/InitializationHandler.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/cHandler/InitializationHandler.java
@@ -366,6 +366,8 @@ public class InitializationHandler {
 							rhs = mExpressionTranslation.translateFloatingLiteral(loc, "0.0").getValue();
 						} else if (((CPrimitive) lCType).getType().equals(PRIMITIVE.LONGDOUBLE)) {
 							rhs = mExpressionTranslation.translateFloatingLiteral(loc, "0.0l").getValue();
+						} else {
+							throw new UnsupportedOperationException("UNsopported Floating Type");
 						}
 					} else {
 						main.mCHandler.convert(loc, initializer, lCType);
