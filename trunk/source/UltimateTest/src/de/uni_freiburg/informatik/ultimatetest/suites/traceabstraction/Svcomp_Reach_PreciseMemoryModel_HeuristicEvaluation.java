cde5b29f1a87e167202a2cd3facb5cc0fa364690
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2016-06-16 22:40:30 +0200

    testsuite

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/traceabstraction/Svcomp_Reach_PreciseMemoryModel_HeuristicEvaluation.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/traceabstraction/Svcomp_Reach_PreciseMemoryModel_HeuristicEvaluation.java
@@ -48,46 +48,46 @@ public class Svcomp_Reach_PreciseMemoryModel_HeuristicEvaluation extends
 	private static int mFilesPerDirectoryLimit = 10;
 	
 	private static final DirectoryFileEndingsPair[] mSVCOMP_Examples = {
-//		/*** Category 1. Arrays ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/array-examples/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
-//		
-//		/*** Category 2. Bit Vectors ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/bitvector/", new String[]{ ".i", ".c" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/bitvector-regression/", new String[]{ ".i", ".c" }, mFilesPerDirectoryLimit) ,
-//		
-//		/*** Category 4. Control Flow and Integer Variables ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/ntdrivers-simplified/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/ssh-simplified/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/locks/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
-//		
-//		new DirectoryFileEndingsPair("examples/svcomp/loops/", new String[]{".i"}) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/loop-acceleration/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/loop-invgen/", new String[]{".i"}, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/loop-lit/", new String[]{ ".i", ".c" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/loop-new/", new String[]{".i"}, mFilesPerDirectoryLimit) ,
-//		
-//		new DirectoryFileEndingsPair("examples/svcomp/eca-rers2012/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/product-lines/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
-//		
-//		/*** Category 6. Heap Manipulation / Dynamic Data Structures ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/heap-manipulation/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/list-properties/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/ldv-regression/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/ddv-machzwd/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
-//		
-//		/*** Category 7. Memory Safety ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/memsafety/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/list-ext-properties/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/memory-alloca/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/memory-unsafe/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
-//
-//		/*** Category 8. Recursive ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/recursive/", new String[]{ ".c" }, mFilesPerDirectoryLimit) ,
-//		
-//		/*** Category 9. Sequentialized Concurrent Programs ***/
-//		new DirectoryFileEndingsPair("examples/svcomp/systemc/", new String[]{ ".c" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/seq-mthreaded/", new String[]{ ".c" }, mFilesPerDirectoryLimit) ,
-//		new DirectoryFileEndingsPair("examples/svcomp/seq-pthread/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+		/*** Category 1. Arrays ***/
+		new DirectoryFileEndingsPair("examples/svcomp/array-examples/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+		
+		/*** Category 2. Bit Vectors ***/
+		new DirectoryFileEndingsPair("examples/svcomp/bitvector/", new String[]{ ".i", ".c" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/bitvector-regression/", new String[]{ ".i", ".c" }, mFilesPerDirectoryLimit) ,
+		
+		/*** Category 4. Control Flow and Integer Variables ***/
+		new DirectoryFileEndingsPair("examples/svcomp/ntdrivers-simplified/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/ssh-simplified/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/locks/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
+		
+		new DirectoryFileEndingsPair("examples/svcomp/loops/", new String[]{".i"}) ,
+		new DirectoryFileEndingsPair("examples/svcomp/loop-acceleration/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/loop-invgen/", new String[]{".i"}, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/loop-lit/", new String[]{ ".i", ".c" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/loop-new/", new String[]{".i"}, mFilesPerDirectoryLimit) ,
+		
+		new DirectoryFileEndingsPair("examples/svcomp/eca-rers2012/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/product-lines/", new String[]{".c" }, mFilesPerDirectoryLimit) ,
+		
+		/*** Category 6. Heap Manipulation / Dynamic Data Structures ***/
+		new DirectoryFileEndingsPair("examples/svcomp/heap-manipulation/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/list-properties/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/ldv-regression/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/ddv-machzwd/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+		
+		/*** Category 7. Memory Safety ***/
+		new DirectoryFileEndingsPair("examples/svcomp/memsafety/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/list-ext-properties/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/memory-alloca/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/memory-unsafe/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
+
+		/*** Category 8. Recursive ***/
+		new DirectoryFileEndingsPair("examples/svcomp/recursive/", new String[]{ ".c" }, mFilesPerDirectoryLimit) ,
+		
+		/*** Category 9. Sequentialized Concurrent Programs ***/
+		new DirectoryFileEndingsPair("examples/svcomp/systemc/", new String[]{ ".c" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/seq-mthreaded/", new String[]{ ".c" }, mFilesPerDirectoryLimit) ,
+		new DirectoryFileEndingsPair("examples/svcomp/seq-pthread/", new String[]{ ".i" }, mFilesPerDirectoryLimit) ,
 	};
 	
 	private static final String[] mUltimateRepository = {
@@ -112,17 +112,17 @@ public class Svcomp_Reach_PreciseMemoryModel_HeuristicEvaluation extends
 	 */
 	private static final String[] mSettings = {
 		/*** No Heuristic ***/
-		"automizer/ForwardPredicates_SvcompReachPreciseMmNotIncrementally.epf",
+		"automizer/ForwardPredicates_SvcompReachPreciseMM_NotIncrementally.epf",
 		/*** Heuristic 1 (OUTSIDE_LOOP_FIRST1) ***/
-		"automizer/ForwardPredicates_SvcompReachPreciseMmHeuristic1.epf",
+		"automizer/ForwardPredicates_SvcompReachPreciseMM_Heuristic1.epf",
 		/*** Heuristic 2 (OUTSIDE_LOOP_FIRST2) ***/
-		"automizer/ForwardPredicates_SvcompReachPreciseMmHeuristic2.epf",
+		"automizer/ForwardPredicates_SvcompReachPreciseMM_Heuristic2.epf",
 		/*** Heuristic 3 (INSIDE_LOOP_FIRST1) ***/
-		"automizer/ForwardPredicates_SvcompReachPreciseMmHeuristic3.epf",
+		"automizer/ForwardPredicates_SvcompReachPreciseMM_Heuristic3.epf",
 		/*** Heuristic 4 (MIX_INSIDE_OUTSIDE) ***/
-		"automizer/ForwardPredicates_SvcompReachPreciseMmHeuristic4.epf",
+		"automizer/ForwardPredicates_SvcompReachPreciseMM_Heuristic4.epf",
 		/*** Heuristic 5 (TERMS_WITH_SMALL_CONSTANTS_FIRST) ***/
-		"automizer/ForwardPredicates_SvcompReachPreciseMmHeuristic5.epf",
+		"automizer/ForwardPredicates_SvcompReachPreciseMM_Heuristic5.epf",
 	};
 	
 
@@ -131,7 +131,7 @@ public class Svcomp_Reach_PreciseMemoryModel_HeuristicEvaluation extends
 	 */
 	@Override
 	public long getTimeout() {
-		return 60 * 1000;
+		return 30 * 1000;
 	}
 	
 
