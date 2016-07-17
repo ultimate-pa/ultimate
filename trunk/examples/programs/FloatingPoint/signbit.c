4ba5bbc02751692ec4967b277467202d5a1ec3ea
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2016-07-17 15:47:16 +0200

    float examples

--- trunk/examples/programs/FloatingPoint/signbit.c
+++ trunk/examples/programs/FloatingPoint/signbit.c
@@ -10,13 +10,11 @@
 #include <float.h>
 #include <math.h>
 
-extern int __finitef (float __value) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__const__));
-
 int main() {
 	float f = 0.1;
 	printf("%f", f); 
-	printf("%d", __finitef(f)); 
-	if (!__finitef(f)) {
+	printf("%d", signbit(f)); 
+	if (signbit(f) != 0) {
 		//@ assert \false;
 	}
 
