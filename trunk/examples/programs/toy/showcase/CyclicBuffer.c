0f1b0f7560e817ab430ce2e3693c230effcf818a
Matthias <heizmann@informatik.uni-freiburg.de>
2016-07-22 13:51:27 +0200

    example

--- trunk/examples/programs/toy/showcase/CyclicBuffer.c
+++ trunk/examples/programs/toy/showcase/CyclicBuffer.c
@@ -1,6 +1,7 @@
-//#Safe
+//#Unsafe
 /*
- * Assume that a is some cyclic buffer of size 256.
+ * This is an example for a non-obvious bug in a C program.
+ * Assume that array arr is some cyclic buffer of size 256.
  * In each iteration we write two new values in the buffer.
  * Since the current position pos is an unsigned char one might expect that
  * (pos + 1) is always between 0 and 255. However, because of the "usual
@@ -18,10 +19,10 @@ extern int getNextValue();
 
 int main() {
     unsigned char pos = getInitialPosition();
-    int a[256];
+    int arr[256];
     while(1) {
-        a[pos] = getNextValue();
-        a[pos + 1] = getNextValue();
+        arr[pos] = getNextValue();
+        arr[pos + 1] = getNextValue();
         pos += 2;
     }
     return 0;
