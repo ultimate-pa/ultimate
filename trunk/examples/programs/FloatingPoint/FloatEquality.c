fb562c275874ed6540964d9e252b80dae22248c7
Matthias <heizmann@informatik.uni-freiburg.de>
2016-07-27 11:53:57 +0200

    example

--- trunk/examples/programs/FloatingPoint/FloatEquality.c
+++ trunk/examples/programs/FloatingPoint/FloatEquality.c
@@ -1,18 +1,19 @@
 //#Unsafe
 /* 
+ * Assert statement can be violated if x is a NaN value.
+ * 
  * Author: heizmann@informatik.uni-freiburg.de
  * Date: 2016-07-26
- * 
  */
 
 
 float __VERIFIER_nondet_float();
 
 int main() {
-	float f = __VERIFIER_nondet_float();
-
-	if (f == f) {
-		//@ assert \false;
+	float x = __VERIFIER_nondet_float();
+	float y = __VERIFIER_nondet_float();
+	if (x == x && y != y) {
+		// do nothing
 	} else {
 		//@ assert \false;
 	}
