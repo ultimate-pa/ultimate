4f13989cc2d0fc1e52fa7523e148f9fdd45ab9e9
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2016-08-01 10:57:13 +0200

    example: documentation

--- trunk/examples/programs/toy/tooDifficultLoopInvariant/GasCake01_InvariantCheck.c
+++ trunk/examples/programs/toy/tooDifficultLoopInvariant/GasCake01_InvariantCheck.c
@@ -1,18 +1,13 @@
-
+//#Safe
+/* Encodes invariant inductivity check for some larger example program.
+ * 
+ * Use large block encoding and Mathsat.
+ * Verification took 350s on my computer.
+ * 
+ * Author: heizmann@informatik.uni-freiburg.de
+ * Date: 2016-07-26
+ */
 typedef float float32;
-
-
-// extern boolean tclRange_b(boolean _min_b, boolean _max_b);
-// extern char8 tclRange_c8(char8 _min_c8, char8 _max_c8);
-// extern sint8 tclRange_s8(sint8 _min_s8, sint8 _max_s8);
-// extern sint16 tclRange_s16(sint16 _min_s16, sint16 _max_s16);
-// extern sint32 tclRange_s32(sint32 _min_s32, sint32 _max_s32);
-// extern uint8 tclRange_u8(uint8 _min_u8, uint8 _max_u8);
-// extern uint16 tclRange_u16(uint16 _min_u16, uint16 _max_u16);
-// extern uint32 tclRange_u32(uint32 _min_u32, uint32 _max_u32);
-// extern float32 tclRange_f32(float32 _min_f32, float32 _max_f32);
-// extern float64 tclRange_f64(float64 _min_f64, float64 _max_f64);
-
 
 
 float32 tclRange_f32(float32 min, float32 max) {
@@ -25,6 +20,7 @@ float32 tclRange_f32(float32 min, float32 max) {
 }
 
 
+
 void holdsInitially(void) {
 	float32 input_f32;
 	float32 output_f32 = 0;
@@ -32,6 +28,8 @@ void holdsInitially(void) {
 	float32 outputOld_f32 = 0;
  	//@ assert ((internal_f32 * 0.9 + outputOld_f32 >= -1.0) && (internal_f32 * 0.9 + outputOld_f32 <= 101.0) && (outputOld_f32 >= -1.0) && (outputOld_f32 <= 101.0) && (internal_f32 >= -57.0));
 }
+
+
 
 void isInductive(void) {
 	float32 input_f32;
