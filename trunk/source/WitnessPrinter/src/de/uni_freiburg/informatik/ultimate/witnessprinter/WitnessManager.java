e8bfb92114758ac6979d2310b32a602379b1c34f
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-08-18 21:11:14 +0200

    fixed logging in monitored process

--- trunk/source/WitnessPrinter/src/de/uni_freiburg/informatik/ultimate/witnessprinter/WitnessManager.java
+++ trunk/source/WitnessPrinter/src/de/uni_freiburg/informatik/ultimate/witnessprinter/WitnessManager.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Core.
- * 
+ *
  * The ULTIMATE Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Core. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Core grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Core grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.witnessprinter;
@@ -54,33 +54,27 @@ import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Triple;
 import de.uni_freiburg.informatik.ultimate.witnessprinter.preferences.PreferenceInitializer;
 
 /**
- * The poorly named WitnessManager "handles" witnesses as specified by the Ultimate Core settings.
- * 
- * E.g. it may create witness files from counter example results and may call another verifier with this witness as
- * input.
- * 
- * The actual witness generation is done by the corresponding plugin (i.e. via
- * {@link IProgramExecution#getSVCOMPWitnessString()}).
- * 
+ * The poorly named WitnessManager "handles" witnesses as specified by the Ultimate Core settings. E.g. it may create
+ * witness files from counter example results and may call another verifier with this witness as input. The actual
+ * witness generation is done by the corresponding plugin (i.e. via {@link IProgramExecution#getSVCOMPWitnessString()}).
+ *
  * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * @see IProgramExecution
- * 
  */
 public class WitnessManager {
-
+	
 	private final ILogger mLogger;
 	private final IUltimateServiceProvider mServices;
 	private final IToolchainStorage mStorage;
-
+	
 	public WitnessManager(final ILogger logger, final IUltimateServiceProvider services,
 			final IToolchainStorage storage) {
 		mLogger = logger;
 		mServices = services;
 		mStorage = storage;
 	}
-
+	
 	/**
-	 * 
 	 * @param funsResultSupplier
 	 *            A collection of functions were each provides a triple (IResult, filename, string that represents a
 	 *            valid SVCOMP witness).
@@ -90,7 +84,7 @@ public class WitnessManager {
 	public void run(final Collection<Supplier<Triple<IResult, String, String>>> funsResultSupplier)
 			throws IOException, InterruptedException {
 		final IPreferenceProvider ups = mServices.getPreferenceProvider(Activator.PLUGIN_ID);
-
+		
 		int cexNo = 0;
 		String suffix = null;
 		for (final Supplier<Triple<IResult, String, String>> funResultSupplier : funsResultSupplier) {
@@ -98,11 +92,11 @@ public class WitnessManager {
 			final IResult cex = triple.getFirst();
 			final String originalFile = triple.getSecond();
 			final String svcompWitness = triple.getThird();
-
+			
 			final boolean writeInWorkingDir = ups.getBoolean(PreferenceInitializer.LABEL_WITNESS_WRITE_WORKINGDIR);
 			final boolean writeBesideInputFile = ups.getBoolean(PreferenceInitializer.LABEL_WITNESS_WRITE);
 			final List<String> filenamesToDelete = new ArrayList<>();
-
+			
 			if (!writeBesideInputFile && !writeInWorkingDir) {
 				continue;
 			}
@@ -111,12 +105,12 @@ public class WitnessManager {
 				filename = writeWitness(svcompWitness, null, suffix);
 				filenamesToDelete.add(filename);
 			}
-
+			
 			if (writeBesideInputFile && svcompWitness != null) {
 				filename = writeWitness(svcompWitness, originalFile, suffix);
 				filenamesToDelete.add(filename);
 			}
-
+			
 			if (ups.getBoolean(PreferenceInitializer.LABEL_WITNESS_VERIFY)) {
 				if (svcompWitness == null) {
 					reportWitnessResult(null, cex, WitnessVerificationStatus.INTERNAL_ERROR,
@@ -128,7 +122,7 @@ public class WitnessManager {
 				reportWitnessResult(svcompWitness, cex, WitnessVerificationStatus.UNVERIFIED,
 						WitnessVerificationStatus.UNVERIFIED);
 			}
-
+			
 			if (ups.getBoolean(PreferenceInitializer.LABEL_WITNESS_DELETE_GRAPHML)) {
 				for (final String fi : filenamesToDelete) {
 					deleteFile(fi);
@@ -138,8 +132,8 @@ public class WitnessManager {
 			suffix = String.valueOf(cexNo);
 		}
 	}
-
-	private void deleteFile(String filename) {
+	
+	private void deleteFile(final String filename) {
 		if (filename == null) {
 			return;
 		}
@@ -148,24 +142,24 @@ public class WitnessManager {
 			mLogger.warn("File " + filename + " could not be deleted");
 		}
 	}
-
-	private String writeWitness(String svcompWitness, String origInputFile, String additionalSuffix) {
+	
+	private String writeWitness(final String svcompWitness, final String origInputFile, final String additionalSuffix) {
 		final String suffix = "witness";
 		final String fileending = ".graphml";
 		final String separator = "-";
-
+		
 		final StringBuilder filename = new StringBuilder();
 		if (origInputFile != null) {
 			filename.append(origInputFile).append(separator).append(suffix);
 		} else {
 			filename.append(suffix);
 		}
-
+		
 		if (additionalSuffix != null) {
 			filename.append(separator).append(additionalSuffix);
 		}
 		filename.append(fileending);
-
+		
 		try {
 			final File file = CoreUtil.writeFile(filename.toString(), svcompWitness);
 			mLogger.info("Wrote witness to " + file.getAbsolutePath());
@@ -175,21 +169,21 @@ public class WitnessManager {
 		}
 		return filename.toString();
 	}
-
+	
 	private void reportWitnessResult(final String svcompWitness, final IResult cex,
 			final WitnessVerificationStatus verificationStatus, final WitnessVerificationStatus expectedStatus) {
 		mServices.getResultService().reportResult(cex.getPlugin(),
 				new WitnessResult(Activator.PLUGIN_ID, cex, svcompWitness, verificationStatus, expectedStatus));
 	}
-
-	private boolean checkWitness(String svcompWitnessFile, IResult cex, String originalFile, String svcompWitness)
-			throws IOException, InterruptedException {
+	
+	private boolean checkWitness(final String svcompWitnessFile, final IResult cex, final String originalFile,
+			final String svcompWitness) throws IOException, InterruptedException {
 		mLogger.info("Verifying witness for CEX: " + cex.getShortDescription());
 		final IPreferenceProvider ups = mServices.getPreferenceProvider(Activator.PLUGIN_ID);
 		final PreferenceInitializer.WitnessVerifierType type = ups.getEnum(PreferenceInitializer.LABEL_WITNESS_VERIFIER,
 				PreferenceInitializer.WitnessVerifierType.class);
 		final String command = ups.getString(PreferenceInitializer.LABEL_WITNESS_VERIFIER_COMMAND);
-
+		
 		switch (type) {
 		case CPACHECKER:
 			return checkWitnessWithCPAChecker(svcompWitnessFile, cex, originalFile, command, svcompWitness);
@@ -197,9 +191,10 @@ public class WitnessManager {
 			throw new UnsupportedOperationException("Witness verifier " + type + " unknown");
 		}
 	}
-
-	private boolean checkWitnessWithCPAChecker(String svcompWitnessFile, IResult cex, String originalFile,
-			String command, String svcompWitness) throws IOException, InterruptedException {
+	
+	private boolean checkWitnessWithCPAChecker(final String svcompWitnessFile, final IResult cex,
+			final String originalFile, final String command, final String svcompWitness)
+			throws IOException, InterruptedException {
 		final String cpaCheckerHome = System.getenv().get("CPACHECKER_HOME");
 		if (cpaCheckerHome == null) {
 			mLogger.error("CPACHECKER_HOME not set, cannot use CPACHECKER as witness verifier");
@@ -209,27 +204,28 @@ public class WitnessManager {
 		}
 		final IPreferenceProvider ups = mServices.getPreferenceProvider(Activator.PLUGIN_ID);
 		int timeoutInS = ups.getInt(PreferenceInitializer.LABEL_WITNESS_VERIFIER_TIMEOUT);
-
+		
 		final String[] cmdArray = makeCPACheckerCommand(command, svcompWitnessFile,
 				ups.getString(PreferenceInitializer.LABEL_WITNESS_CPACHECKER_PROPERTY), originalFile, cpaCheckerHome,
 				timeoutInS);
 		timeoutInS++;
 		mLogger.info(StringUtils.join(cmdArray, " "));
-		final MonitoredProcess cpaCheckerProcess = MonitoredProcess.exec(cmdArray, cpaCheckerHome, null, mServices, mStorage);
+		final MonitoredProcess cpaCheckerProcess =
+				MonitoredProcess.exec(cmdArray, cpaCheckerHome, null, mServices, mStorage, mLogger);
 		final BufferedInputStream errorStream = new BufferedInputStream(cpaCheckerProcess.getErrorStream());
 		final BufferedInputStream outputStream = new BufferedInputStream(cpaCheckerProcess.getInputStream());
-
+		
 		final String error = convertStreamToString(errorStream);
 		final String output = convertStreamToString(outputStream);
-
+		
 		// wait for timeoutInS seconds for the witness checker, then kill it
 		// forcefully
 		mLogger.info("Waiting for " + timeoutInS + "s for CPA Checker...");
 		final MonitoredProcessState cpaCheckerState = cpaCheckerProcess.impatientWaitUntilTime(timeoutInS * 1000L);
 		mLogger.info("Return code was " + cpaCheckerState.getReturnCode());
-
+		
 		// TODO: interpret error and output
-
+		
 		if (checkOutputForSuccess(output)) {
 			mLogger.info("Witness for CEX was verified successfully");
 			reportWitnessResult(svcompWitness, cex, WitnessVerificationStatus.VERIFIED,
@@ -250,8 +246,8 @@ public class WitnessManager {
 			return false;
 		}
 	}
-
-	private boolean checkOutputForSuccess(String output) {
+	
+	private boolean checkOutputForSuccess(final String output) {
 		for (final String line : output.split(CoreUtil.getPlatformLineSeparator())) {
 			if (line.startsWith("Verification result: FALSE.")) {
 				return true;
@@ -259,9 +255,9 @@ public class WitnessManager {
 		}
 		return false;
 	}
-
-	private String[] makeCPACheckerCommand(String command, String svcompWitnessFile, String cpaCheckerProp,
-			String originalFile, String workingDir, int timeoutInS) {
+	
+	private String[] makeCPACheckerCommand(final String command, final String svcompWitnessFile,
+			final String cpaCheckerProp, final String originalFile, final String workingDir, final int timeoutInS) {
 		final List<String> cmdArgs = new ArrayList<>();
 		final String[] args = command.split(" ");
 		final File file = new File(workingDir + File.separator + args[0]);
@@ -278,12 +274,12 @@ public class WitnessManager {
 		cmdArgs.add(escape(originalFile));
 		return cmdArgs.toArray(new String[cmdArgs.size()]);
 	}
-
-	private String escape(String str) {
+	
+	private String escape(final String str) {
 		return str;
 	}
-
-	private static String convertStreamToString(InputStream input) {
+	
+	private static String convertStreamToString(final InputStream input) {
 		final BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 		final StringBuilder out = new StringBuilder();
 		String line;
