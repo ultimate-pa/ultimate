5b83667b9d381821d763969ef8bfd53fe7d5f884
schillic <schillic@informatik.uni-freiburg.de>
2016-08-19 14:34:43 +0200

    fixed confusing operationName() method

--- trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operations/simulation/delayed/BuchiReduce.java
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/nwalibrary/operations/simulation/delayed/BuchiReduce.java
@@ -22,9 +22,9 @@
  * 
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Automata Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Automata Library grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
  * to convey the resulting work.
  */
 /**
@@ -191,7 +191,7 @@ public class BuchiReduce<LETTER, STATE> implements IOperation<LETTER, STATE> {
 	 */
 	@Override
 	public String operationName() {
-		return "reduceBuchi";
+		return "BuchiReduce";
 	}
 
 	/*
