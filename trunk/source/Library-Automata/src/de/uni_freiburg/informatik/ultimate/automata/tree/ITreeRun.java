e9f214932e9a6d37a4252ff0276c2a0705a1fdba
schillic <schillic@informatik.uni-freiburg.de>
2016-08-20 09:00:25 +0200

    moved ITreeRun to tree package

---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/tree/ITreeRun.java
@@ -0,0 +1,13 @@
+package de.uni_freiburg.informatik.ultimate.automata.tree;
+
+public interface ITreeRun<LETTER> {
+	
+	//public Word<LETTER> getWord();
+	
+	//public LETTER getSymbol(int i);
+	
+	//public int getLength();
+	
+	public LETTER getRoot();
+
+}
