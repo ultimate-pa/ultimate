40bd8804f656fbeda415a54e2e97ff74bc628ce6
schillic <schillic@informatik.uni-freiburg.de>
2016-08-22 16:24:36 +0200

    added helper methods to statistics generation

--- trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/AutomataOperationStatistics.java
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/AutomataOperationStatistics.java
@@ -45,6 +45,9 @@ public class AutomataOperationStatistics implements ICsvProviderProvider<Object>
 	
 	private final LinkedHashMap<StatisticsType, Object> mKeyValueMap = new LinkedHashMap<>();
 	
+	private static final String FIRST_INSERT_THE_VALUE_FOR_KEY = "First insert the value for key ";
+	private static final String MUST_BE_OF_NUMBER_TYPE = "must be of number type.";
+	
 	@Override
 	public ICsvProvider<Object> createCvsProvider() {
 		final List<String> columnTitles = new ArrayList<>(mKeyValueMap.size());
@@ -68,5 +71,64 @@ public class AutomataOperationStatistics implements ICsvProviderProvider<Object>
 	 */
 	public Object addKeyValuePair(final StatisticsType key, final Object value) {
 		return mKeyValueMap.put(key, value);
+	}
+	
+	/**
+	 * Adds another key-value pair with the content being the percentage of the first key <tt>sndBaseKey</tt> wrt. the
+	 * second key <tt>fstBaseKey</tt>.
+	 * 
+	 * @param fstBaseKey
+	 *            first key
+	 * @param sndBaseKey
+	 *            second key
+	 * @param resultKey
+	 *            result key
+	 * @param invert true iff percentage should be inverted, i.e., instead of <tt>x</tt> we add <tt>100 - x</tt>
+	 */
+	public void addPercentageData(final StatisticsType fstBaseKey, final StatisticsType sndBaseKey,
+			final StatisticsType resultKey, final boolean invert) {
+		final int fst = getInteger(fstBaseKey);
+		final int snd = getInteger(sndBaseKey);
+		final int percentageRaw = 100 * snd / fst;
+		final Integer percentage = invert ? 100 - percentageRaw : percentageRaw;
+		addKeyValuePair(resultKey, percentage);
+	}
+	
+	/**
+	 * Adds another key-value pair with the content being the difference between the first key <tt>sndBaseKey</tt> wrt.
+	 * the second key <tt>fstBaseKey</tt>.
+	 * 
+	 * @param fstBaseKey
+	 *            first key
+	 * @param sndBaseKey
+	 *            second key
+	 * @param resultKey
+	 *            result key
+	 */
+	public void addDifferenceData(final StatisticsType fstBaseKey, final StatisticsType sndBaseKey,
+			final StatisticsType resultKey) {
+		final int difference = getDifference(fstBaseKey, sndBaseKey);
+		addKeyValuePair(resultKey, difference);
+	}
+	
+	private int getDifference(final StatisticsType fstBaseKey, final StatisticsType sndBaseKey) {
+		final Integer fst = getInteger(fstBaseKey);
+		
+		final Integer snd = getInteger(sndBaseKey);
+		
+		final int difference = fst - snd;
+		return difference;
+	}
+	
+	private Integer getInteger(final StatisticsType key) {
+		final Object raw = mKeyValueMap.get(key);
+		if (raw == null) {
+			throw new IllegalArgumentException(FIRST_INSERT_THE_VALUE_FOR_KEY + key);
+		}
+		if (!(raw instanceof Integer)) {
+			throw new IllegalArgumentException(key + MUST_BE_OF_NUMBER_TYPE);
+		}
+		final Integer result = (Integer) raw;
+		return result;
 	}
 }
