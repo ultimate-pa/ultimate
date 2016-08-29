e090f46a97adb143acff0007a868e11e16852ed0
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-08-29 17:26:27 +0200

    some cc fixes

--- trunk/source/CDTPlugin/src/de/uni_freiburg/informatik/ultimate/cdt/test/TestValuation.java
+++ trunk/source/CDTPlugin/src/de/uni_freiburg/informatik/ultimate/cdt/test/TestValuation.java
@@ -1,31 +1,31 @@
 /*
  * Copyright (C) 2012-2015 Stefan Wissert
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE CDTPlugin plug-in.
- * 
+ *
  * The ULTIMATE CDTPlugin plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE CDTPlugin plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE CDTPlugin plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE CDTPlugin plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE CDTPlugin plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE CDTPlugin plug-in grant you additional permission
  * to convey the resulting work.
  */
 /**
- * 
+ *
  */
 package de.uni_freiburg.informatik.ultimate.cdt.test;
 
@@ -42,46 +42,35 @@ import de.uni_freiburg.informatik.ultimate.core.model.translation.IValuation;
 
 /**
  * Objects return Test Data.
- * 
+ *
  * @author Stefan Wissert
- * 
+ *
  */
 public class TestValuation implements IValuation {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see de.uni_freiburg.informatik.ultimate.result.IValuation#
-	 * getValuesForFailurePathIndex(int)
-	 */
 	@Override
-	public Map<String, SimpleEntry<IType, List<String>>> getValuesForFailurePathIndex(
-			int index) {
-		final HashMap<String, SimpleEntry<IType, List<String>>> map = new HashMap<String, SimpleEntry<IType, List<String>>>();
+	public Map<String, SimpleEntry<IType, List<String>>> getValuesForFailurePathIndex(final int index) {
+		final Map<String, SimpleEntry<IType, List<String>>> map =
+				new HashMap<String, SimpleEntry<IType, List<String>>>();
 		switch (index) {
 		case 0:
-			map.put("x",
-					new SimpleEntry<IType, List<String>>(BoogieType.TYPE_INT,
-							new ArrayList<String>(Arrays.asList("11"))));
+			map.put("x", new SimpleEntry<IType, List<String>>(BoogieType.TYPE_INT,
+					new ArrayList<String>(Arrays.asList("11"))));
 			return map;
 		case 1:
 			map.put("names",
-					new SimpleEntry<IType, List<String>>(BoogieType
-							.createArrayType(3,
-									new BoogieType[] { BoogieType.TYPE_INT },
-									BoogieType.TYPE_INT), new ArrayList<String>(
-							Arrays.asList("Stefan", "Alex", "Markus"))));
+					new SimpleEntry<IType, List<String>>(
+							BoogieType.createArrayType(3, new BoogieType[] { BoogieType.TYPE_INT },
+									BoogieType.TYPE_INT),
+							new ArrayList<String>(Arrays.asList("Stefan", "Alex", "Markus"))));
 			return map;
 		default:
-			map.put("x",
-					new SimpleEntry<IType, List<String>>(BoogieType.TYPE_INT,
-							new ArrayList<String>(Arrays.asList("11"))));
-			map.put("y",
-					new SimpleEntry<IType, List<String>>(BoogieType.TYPE_INT,
-							new ArrayList<String>(Arrays.asList("4711"))));
-			map.put("counter",
-					new SimpleEntry<IType, List<String>>(BoogieType.TYPE_INT,
-							new ArrayList<String>(Arrays.asList("133423421"))));
+			map.put("x", new SimpleEntry<IType, List<String>>(BoogieType.TYPE_INT,
+					new ArrayList<String>(Arrays.asList("11"))));
+			map.put("y", new SimpleEntry<IType, List<String>>(BoogieType.TYPE_INT,
+					new ArrayList<String>(Arrays.asList("4711"))));
+			map.put("counter", new SimpleEntry<IType, List<String>>(BoogieType.TYPE_INT,
+					new ArrayList<String>(Arrays.asList("133423421"))));
 			return map;
 		}
 	}
--- trunk/source/Library-UltimateModel/src/de/uni_freiburg/informatik/ultimate/core/model/translation/IValuation.java
+++ trunk/source/Library-UltimateModel/src/de/uni_freiburg/informatik/ultimate/core/model/translation/IValuation.java
@@ -4,27 +4,27 @@
  * Copyright (C) 2015 Oleksii Saukh (saukho@informatik.uni-freiburg.de)
  * Copyright (C) 2012-2015 Stefan Wissert
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Core.
- * 
+ *
  * The ULTIMATE Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Core. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Core grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Core grant you additional permission
  * to convey the resulting work.
  */
 /**
@@ -47,20 +47,17 @@ import de.uni_freiburg.informatik.ultimate.core.model.models.IType;
 public interface IValuation {
 
 	/**
-	 * This method returns the Variable Values for current position on the
-	 * failure path. How this is done is in the responsibility of each
-	 * ModelChecker, that have to implement this interface.
-	 * <br><br>
-	 * The method returns a map which maps from the variable name to the values.
-	 * The values are modeled as SimpleEntry<IType, List<String>>, where IType
-	 * signals the type of the Variable (so Integer, Double or Array<Integer>)
-	 * and the List represent the values of the single variable or the array.
-	 * 
+	 * This method returns the Variable Values for current position on the failure path. How this is done is in the
+	 * responsibility of each ModelChecker, that have to implement this interface. <br>
+	 * <br>
+	 * The method returns a map which maps from the variable name to the values. The values are modeled as
+	 * SimpleEntry<IType, List<String>>, where IType signals the type of the Variable (so Integer, Double or Array
+	 * <Integer>) and the List represent the values of the single variable or the array.
+	 *
 	 * @param index
 	 *            the current position on the failure path.
 	 * @return the values as Strings for the variables.
 	 */
-	public Map<String, SimpleEntry<IType, List<String>>> getValuesForFailurePathIndex(
-			int index);
+	Map<String, SimpleEntry<IType, List<String>>> getValuesForFailurePathIndex(int index);
 
 }
