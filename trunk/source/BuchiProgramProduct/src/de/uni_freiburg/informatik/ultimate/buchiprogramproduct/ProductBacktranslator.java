a13fe2018ebd269fce57940dd5b3313426d9ac20
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-08-29 17:10:40 +0200

    towards Neon compatibility (fixing generic parameterization in backtranslation)

--- trunk/source/BuchiProgramProduct/src/de/uni_freiburg/informatik/ultimate/buchiprogramproduct/ProductBacktranslator.java
+++ trunk/source/BuchiProgramProduct/src/de/uni_freiburg/informatik/ultimate/buchiprogramproduct/ProductBacktranslator.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE BuchiProgramProduct plug-in.
- * 
+ *
  * The ULTIMATE BuchiProgramProduct plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE BuchiProgramProduct plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE BuchiProgramProduct plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE BuchiProgramProduct plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE BuchiProgramProduct plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE BuchiProgramProduct plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.buchiprogramproduct;
@@ -42,24 +42,23 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.RCF
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.util.RcfgProgramExecution;
 
 /**
- * 
+ *
  * @author dietsch@informatik.uni-freiburg.de
- * 
+ *
  */
-public class ProductBacktranslator extends DefaultTranslator<RCFGEdge, RCFGEdge, Term, Term> {
+public class ProductBacktranslator extends DefaultTranslator<RCFGEdge, RCFGEdge, Term, Term, String, String> {
 
 	private final HashMap<RCFGEdge, RCFGEdge> mEdgeMapping;
 
 	public ProductBacktranslator(final Class<RCFGEdge> traceElementType, final Class<Term> expressionType) {
-		super(traceElementType, traceElementType, expressionType,
-				expressionType);
+		super(traceElementType, traceElementType, expressionType, expressionType);
 		mEdgeMapping = new HashMap<>();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public IProgramExecution<RCFGEdge, Term> translateProgramExecution(
-			final IProgramExecution<RCFGEdge, Term> programExecution) {
+	public IProgramExecution<RCFGEdge, Term>
+			translateProgramExecution(final IProgramExecution<RCFGEdge, Term> programExecution) {
 
 		Map<TermVariable, Boolean>[] oldBranchEncoders = null;
 		if (programExecution instanceof RcfgProgramExecution) {
--- trunk/source/Library-UltimateTest/src/de/uni_freiburg/informatik/ultimate/test/mocks/BacktranslationServiceMock.java
+++ trunk/source/Library-UltimateTest/src/de/uni_freiburg/informatik/ultimate/test/mocks/BacktranslationServiceMock.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2016 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2016 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE JUnit Helper Library.
- * 
+ *
  * The ULTIMATE JUnit Helper Library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE JUnit Helper Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE JUnit Helper Library. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE JUnit Helper Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE JUnit Helper Library grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE JUnit Helper Library grant you additional permission
  * to convey the resulting work.
  */
 
@@ -36,50 +36,50 @@ import de.uni_freiburg.informatik.ultimate.core.model.translation.IProgramExecut
 import de.uni_freiburg.informatik.ultimate.core.model.translation.ITranslator;
 
 /**
- * 
+ *
  * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  *
  */
 final class BacktranslationServiceMock implements IBacktranslationService {
 
-		@Override
-		public <STE, TTE, SE, TE> void addTranslator(ITranslator<STE, TTE, SE, TE> translator) {
-			// does nothing
-		}
-
-		@Override
-		public <SE, TE> TE translateExpression(SE expression, Class<SE> sourceExpressionClass) {
-			return null;
-		}
-
-		@Override
-		public <SE> String translateExpressionToString(SE expression, Class<SE> clazz) {
-			return "";
-		}
-
-		@Override
-		public <STE> List<?> translateTrace(List<STE> trace, Class<STE> clazz) {
-			return Collections.emptyList();
-		}
-
-		@Override
-		public <STE> List<String> translateTraceToHumanReadableString(List<STE> trace, Class<STE> clazz) {
-			return Collections.emptyList();
-		}
-
-		@Override
-		public <STE, SE> IProgramExecution<?, ?> translateProgramExecution(
-				IProgramExecution<STE, SE> programExecution) {
-			return new ProgramExecutionMock<>(null, null);
-		}
-
-		@Override
-		public <STE, SE> IBacktranslatedCFG<?, ?> translateCFG(IBacktranslatedCFG<?, STE> cfg) {
-			return null;
-		}
-
-		@Override
-		public IBacktranslationService getTranslationServiceCopy() {
-			return this;
-		}
+	@Override
+	public <STE, TTE, SE, TE, SVL, TVL> void addTranslator(final ITranslator<STE, TTE, SE, TE, SVL, TVL> translator) {
+		// does nothing
 	}
+
+	@Override
+	public <SE, TE> TE translateExpression(final SE expression, final Class<SE> sourceExpressionClass) {
+		return null;
+	}
+
+	@Override
+	public <SE> String translateExpressionToString(final SE expression, final Class<SE> clazz) {
+		return "";
+	}
+
+	@Override
+	public <STE> List<?> translateTrace(final List<STE> trace, final Class<STE> clazz) {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public <STE> List<String> translateTraceToHumanReadableString(final List<STE> trace, final Class<STE> clazz) {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public <STE, SE> IProgramExecution<?, ?>
+			translateProgramExecution(final IProgramExecution<STE, SE> programExecution) {
+		return new ProgramExecutionMock<>(null, null);
+	}
+
+	@Override
+	public <STE, SE> IBacktranslatedCFG<?, ?> translateCFG(final IBacktranslatedCFG<?, STE> cfg) {
+		return null;
+	}
+
+	@Override
+	public IBacktranslationService getTranslationServiceCopy() {
+		return this;
+	}
+}
