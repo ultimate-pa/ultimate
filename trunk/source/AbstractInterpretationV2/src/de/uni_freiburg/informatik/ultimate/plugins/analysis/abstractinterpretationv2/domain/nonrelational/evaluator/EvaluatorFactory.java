97fc0a8de07155626a0b41919e95d47c277e927f
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-08-29 11:48:05 +0200

    removing log pollution by using logger wrapper for evaluators

--- trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/nonrelational/evaluator/EvaluatorFactory.java
+++ trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/nonrelational/evaluator/EvaluatorFactory.java
@@ -2,27 +2,27 @@
  * Copyright (C) 2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 Marius Greitschus (greitsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE AbstractInterpretationV2 plug-in.
- * 
+ *
  * The ULTIMATE AbstractInterpretationV2 plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE AbstractInterpretationV2 plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE AbstractInterpretationV2 plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE AbstractInterpretationV2 plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE AbstractInterpretationV2 plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE AbstractInterpretationV2 plug-in grant you additional permission
  * to convey the resulting work.
  */
 
@@ -39,7 +39,7 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.Cod
 
 /**
  * Factory for evaluators in the nonrelational abstract domain.
- * 
+ *
  * @author Marius Greitschus (greitsch@informatik.uni-freiburg.de)
  *
  * @param <VALUE>
@@ -48,45 +48,46 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.Cod
  *            The type of states of the abstract domain.
  */
 public class EvaluatorFactory<VALUE extends INonrelationalValue<VALUE>, STATE extends NonrelationalState<STATE, VALUE>>
-        implements IEvaluatorFactory<VALUE, STATE, CodeBlock> {
+		implements IEvaluatorFactory<VALUE, STATE, CodeBlock> {
 
 	private static final int ARITY_MIN = 1;
 	private static final int ARITY_MAX = 2;
 	private static final int BUFFER_MAX = 100;
 
-	private final ILogger mLogger;
 	private final int mMaxParallelStates;
+	private final EvaluatorLogger mEvalLogger;
 
 	private final INonrelationalValueFactory<VALUE> mNonrelationalValueFactory;
 	private final Function<String, VALUE> mSingletonValueExpressionEvaluatorCreator;
 
 	public EvaluatorFactory(final ILogger logger, final int maxParallelStates,
-	        final INonrelationalValueFactory<VALUE> nonrelationalValueFactory,
-	        final Function<String, VALUE> singletonValueEvaluatorCreator) {
-		mLogger = logger;
+			final INonrelationalValueFactory<VALUE> nonrelationalValueFactory,
+			final Function<String, VALUE> singletonValueEvaluatorCreator) {
 		mMaxParallelStates = maxParallelStates;
 		mNonrelationalValueFactory = nonrelationalValueFactory;
 		mSingletonValueExpressionEvaluatorCreator = singletonValueEvaluatorCreator;
+		mEvalLogger = new EvaluatorLogger(logger);
 	}
 
 	@Override
 	public INAryEvaluator<VALUE, STATE, CodeBlock> createNAryExpressionEvaluator(final int arity,
-	        final EvaluatorType type) {
+			final EvaluatorType type) {
 		assert arity >= ARITY_MIN && arity <= ARITY_MAX;
 
 		switch (arity) {
 		case ARITY_MIN:
-			return new UnaryExpressionEvaluator<>(mLogger, mNonrelationalValueFactory);
+			return new UnaryExpressionEvaluator<>(mEvalLogger, mNonrelationalValueFactory);
 		case ARITY_MAX:
-			return new BinaryExpressionEvaluator<>(mLogger, type, mMaxParallelStates, mNonrelationalValueFactory);
+			return new BinaryExpressionEvaluator<>(mEvalLogger, type, mMaxParallelStates, mNonrelationalValueFactory);
 		default:
 			throw new UnsupportedOperationException(new StringBuilder(BUFFER_MAX).append("Arity of ").append(arity)
-			        .append(" is not implemented.").toString());
+					.append(" is not implemented.").toString());
 		}
 	}
 
 	@Override
-	public IEvaluator<VALUE, STATE, CodeBlock> createFunctionEvaluator(String functionName, int inputParamCount) {
+	public IEvaluator<VALUE, STATE, CodeBlock> createFunctionEvaluator(final String functionName,
+			final int inputParamCount) {
 		return new FunctionEvaluator<>(functionName, inputParamCount, mNonrelationalValueFactory);
 	}
 
@@ -102,21 +103,22 @@ public class EvaluatorFactory<VALUE extends INonrelationalValue<VALUE>, STATE ex
 
 	@Override
 	public IEvaluator<VALUE, STATE, CodeBlock> createSingletonValueExpressionEvaluator(final String value,
-	        final Class<?> valueType) {
+			final Class<?> valueType) {
 		assert value != null;
 		return new SingletonValueExpressionEvaluator<>(
-		        mSingletonValueExpressionEvaluatorCreator.apply(value, valueType));
+				mSingletonValueExpressionEvaluatorCreator.apply(value, valueType));
 	}
 
 	@Override
-	public IEvaluator<VALUE, STATE, CodeBlock> createSingletonVariableExpressionEvaluator(
-	        final IBoogieVar variableName) {
+	public IEvaluator<VALUE, STATE, CodeBlock>
+			createSingletonVariableExpressionEvaluator(final IBoogieVar variableName) {
 		assert variableName != null;
 		return new SingletonVariableExpressionEvaluator<>(variableName, mNonrelationalValueFactory);
 	}
 
 	@Override
-	public IEvaluator<VALUE, STATE, CodeBlock> createSingletonLogicalValueExpressionEvaluator(BooleanValue value) {
+	public IEvaluator<VALUE, STATE, CodeBlock>
+			createSingletonLogicalValueExpressionEvaluator(final BooleanValue value) {
 		return new SingletonBooleanExpressionEvaluator<>(value, mNonrelationalValueFactory);
 	}
 
