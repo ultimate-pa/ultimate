23bb9c5166556e3d159b1300f288538e46934e93
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-08-30 15:23:03 +0200

    reorder methods

--- trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/core/coreplugin/services/Log4JWrapper.java
+++ trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/core/coreplugin/services/Log4JWrapper.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2016 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2016 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Core.
- * 
+ *
  * The ULTIMATE Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Core. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Core grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Core grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.core.coreplugin.services;
@@ -32,7 +32,7 @@ import org.apache.log4j.Logger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 
 /**
- * 
+ *
  * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  *
  */
@@ -55,28 +55,8 @@ public final class Log4JWrapper implements ILogger {
 	}
 
 	@Override
-	public void fatal(final Object msg, final Throwable t) {
-		mLogger.log(FQCN, Level.FATAL, msg, t);
-	}
-
-	@Override
-	public void fatal(final Object msg) {
-		mLogger.log(FQCN, Level.FATAL, msg, null);
-	}
-
-	@Override
 	public boolean isErrorEnabled() {
 		return Level.ERROR.isGreaterOrEqual(mLogger.getLevel());
-	}
-
-	@Override
-	public void error(final Object msg, final Throwable t) {
-		mLogger.log(FQCN, Level.ERROR, msg, t);
-	}
-
-	@Override
-	public void error(final Object msg) {
-		mLogger.log(FQCN, Level.ERROR, msg, null);
 	}
 
 	@Override
@@ -85,23 +65,43 @@ public final class Log4JWrapper implements ILogger {
 	}
 
 	@Override
-	public void warn(final Object msg) {
-		mLogger.log(FQCN, Level.WARN, msg, null);
-	}
-
-	@Override
 	public boolean isInfoEnabled() {
 		return mLogger.isInfoEnabled();
 	}
 
 	@Override
-	public void info(final Object msg) {
-		mLogger.log(FQCN, Level.INFO, msg, null);
+	public boolean isDebugEnabled() {
+		return mLogger.isDebugEnabled();
 	}
 
 	@Override
-	public boolean isDebugEnabled() {
-		return mLogger.isDebugEnabled();
+	public void fatal(final Object msg, final Throwable t) {
+		mLogger.log(FQCN, Level.FATAL, msg, t);
+	}
+
+	@Override
+	public void error(final Object msg, final Throwable t) {
+		mLogger.log(FQCN, Level.ERROR, msg, t);
+	}
+
+	@Override
+	public void fatal(final Object msg) {
+		mLogger.log(FQCN, Level.FATAL, msg, null);
+	}
+
+	@Override
+	public void error(final Object msg) {
+		mLogger.log(FQCN, Level.ERROR, msg, null);
+	}
+
+	@Override
+	public void warn(final Object msg) {
+		mLogger.log(FQCN, Level.WARN, msg, null);
+	}
+
+	@Override
+	public void info(final Object msg) {
+		mLogger.log(FQCN, Level.INFO, msg, null);
 	}
 
 	@Override
