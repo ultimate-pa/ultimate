338d38d4e8fd0699167595e0aff0bb51a4417c04
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-08-30 14:40:40 +0200

    cc fixes

--- trunk/source/GuiRCP/src/de/uni_freiburg/informatik/ultimate/gui/GuiToolchainJob.java
+++ trunk/source/GuiRCP/src/de/uni_freiburg/informatik/ultimate/gui/GuiToolchainJob.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE DebugGUI plug-in.
- * 
+ *
  * The ULTIMATE DebugGUI plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE DebugGUI plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE DebugGUI plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE DebugGUI plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE DebugGUI plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE DebugGUI plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.gui;
@@ -38,18 +38,20 @@ import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 
 public class GuiToolchainJob extends DefaultToolchainJob {
 
-	public GuiToolchainJob(String name, ICore<ToolchainListType> core, GuiController controller, File[] inputFiles,
-			ILogger logger) {
+	public GuiToolchainJob(final String name, final ICore<ToolchainListType> core, final GuiController controller,
+			final File[] inputFiles, final ILogger logger) {
 		super(name, core, controller, logger, inputFiles);
 	}
 
-	public GuiToolchainJob(String name, ICore<ToolchainListType> core, IController<ToolchainListType> controller,
-			ILogger logger, IToolchain<ToolchainListType> toolchain) {
+	public GuiToolchainJob(final String name, final ICore<ToolchainListType> core,
+			final IController<ToolchainListType> controller, final ILogger logger,
+			final IToolchain<ToolchainListType> toolchain) {
 		super(name, core, controller, logger, toolchain);
 	}
 
-	public GuiToolchainJob(String name, ICore<ToolchainListType> core, IController<ToolchainListType> controller,
-			ILogger logger, IToolchainData<ToolchainListType> data, File[] inputFiles) {
+	public GuiToolchainJob(final String name, final ICore<ToolchainListType> core,
+			final IController<ToolchainListType> controller, final ILogger logger,
+			final IToolchainData<ToolchainListType> data, final File[] inputFiles) {
 		super(name, core, controller, logger, data, inputFiles);
 	}
 
--- trunk/source/UltimateCLI/src/de/uni_freiburg/informatik/ultimate/cli/CommandLineOptions.java
+++ trunk/source/UltimateCLI/src/de/uni_freiburg/informatik/ultimate/cli/CommandLineOptions.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2016 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2016 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE CLI plug-in.
- * 
+ *
  * The ULTIMATE CLI plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE CLI plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE CLI plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE CLI plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE CLI plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE CLI plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.cli;
@@ -33,7 +33,7 @@ import java.util.List;
 import org.apache.commons.cli.Option;
 
 /**
- * 
+ *
  * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  *
  */
@@ -56,5 +56,4 @@ public final class CommandLineOptions {
 		rtr.add(Option.builder(CommandLineOptions.OPTION_NAME_HELP).longOpt("help").type(Boolean.class).build());
 		return rtr;
 	}
-
 }
