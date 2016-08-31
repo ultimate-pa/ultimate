5159625c4078819463db9318889099dd28aaf092
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-08-31 18:00:37 +0200

    using the correct constructor

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/automatascript/AutomataScriptTestSuite.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/automatascript/AutomataScriptTestSuite.java
@@ -2,27 +2,27 @@
  * Copyright (C) 2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Test Library.
- * 
+ *
  * The ULTIMATE Test Library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Test Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Test Library. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Test Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Test Library grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Test Library grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimatetest.suites.automatascript;
@@ -55,43 +55,34 @@ public class AutomataScriptTestSuite extends UltimateTestSuite {
 	private static final String mToolchain = "examples/toolchains/AutomataScriptInterpreter.xml";
 	private static final File mToolchainFile = new File(TestUtil.getPathFromTrunk(mToolchain));
 	private static int mTimeout = 10 * 1000;
-	private static final String[] mDirectories = {
-		"examples/Automata/AlternatingAutomata",
-		"examples/Automata/atsTestFiles",
-		"examples/Automata/AUTOMATA_SCRIPT", 
-		"examples/Automata/BuchiAutomata", 
-		"examples/Automata/BuchiNwa",
-		"examples/Automata/finiteAutomata", 
-		"examples/Automata/nwa",
-		"examples/Automata/nwaOperations",
-//		"examples/Automata/BuchiNwa/reduceBuchiNwa/",
-//		"examples/Automata/nwaOperations/minimizeMaxSAT/",
-	// the following two have still bugs
-	// "examples/Automata/PetriNet",
-	// "examples/Automata/senwa",
-	// the following is not yet tested
-	// "examples/Automata/syntaxError",
+	private static final String[] mDirectories = { "examples/Automata/AlternatingAutomata",
+			"examples/Automata/atsTestFiles", "examples/Automata/AUTOMATA_SCRIPT", "examples/Automata/BuchiAutomata",
+			"examples/Automata/BuchiNwa", "examples/Automata/finiteAutomata", "examples/Automata/nwa",
+			"examples/Automata/nwaOperations",
+			// "examples/Automata/BuchiNwa/reduceBuchiNwa/",
+			// "examples/Automata/nwaOperations/minimizeMaxSAT/",
+			// the following two have still bugs
+			// "examples/Automata/PetriNet",
+			// "examples/Automata/senwa",
+			// the following is not yet tested
+			// "examples/Automata/syntaxError",
 	};
 	private static final String[] mFileEndings = { ".ats" };
 
 	@Override
 	protected ITestSummary[] constructTestSummaries() {
-		final ArrayList<Class<? extends ICsvProviderProvider<? extends Object>>> benchmarks = 
+		final ArrayList<Class<? extends ICsvProviderProvider<? extends Object>>> benchmarks =
 				new ArrayList<Class<? extends ICsvProviderProvider<? extends Object>>>();
-		
-		final ColumnDefinition[] columnDef = new ColumnDefinition[] { 
-				new ColumnDefinition(
-						CegarLoopStatisticsDefinitions.OverallTime.toString(), "Avg. runtime",
-						ConversionContext.Divide(1000000000, 2, " s"), Aggregate.Sum, Aggregate.Average),	
-			};
-		
-		return new ITestSummary[] { 
-				new AutomataScriptTestSummary(this.getClass()),
-				new CsvConcatenator(this.getClass(), AutomataOperationStatistics.class), 
-				new LatexOverviewSummary(getClass(), benchmarks, columnDef),		
-		};
+
+		final ColumnDefinition[] columnDef = new ColumnDefinition[] {
+				new ColumnDefinition(CegarLoopStatisticsDefinitions.OverallTime.toString(), "Avg. runtime",
+						ConversionContext.Divide(1000000000, 2, " s"), Aggregate.Sum, Aggregate.Average), };
+
+		return new ITestSummary[] { new AutomataScriptTestSummary(this.getClass()),
+				new CsvConcatenator(this.getClass(), AutomataOperationStatistics.class),
+				new LatexOverviewSummary(getClass(), benchmarks, columnDef), };
 	}
-	
+
 	@Override
 	protected IIncrementalLog[] constructIncrementalLog() {
 		return new IIncrementalLog[0];
@@ -109,7 +100,7 @@ public class AutomataScriptTestSuite extends UltimateTestSuite {
 		for (final File inputFile : inputFiles) {
 			final File settingsFile = null;
 			final UltimateRunDefinition urd = new UltimateRunDefinition(inputFile, settingsFile, mToolchainFile);
-			final UltimateStarter starter = new UltimateStarter(urd, mTimeout, null, null);
+			final UltimateStarter starter = new UltimateStarter(urd, mTimeout);
 			final UltimateTestCase utc = new UltimateTestCase(urd.generateShortStringRepresentation(),
 					new AutomataScriptTestResultDecider(), starter,
 					// mDescription + "_" + inputFile.getAbsolutePath(),
