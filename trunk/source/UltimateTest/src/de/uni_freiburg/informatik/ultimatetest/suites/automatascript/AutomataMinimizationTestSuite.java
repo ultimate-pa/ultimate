3828c90cb5ae5ab72ef3c9a631ddc8d20b4d1b38
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2016-08-31 02:58:38 +0200

    AutomataMinimizationTestSuite and settings

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/automatascript/AutomataMinimizationTestSuite.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/automatascript/AutomataMinimizationTestSuite.java
@@ -54,11 +54,17 @@ public class AutomataMinimizationTestSuite extends UltimateTestSuite {
 
 	private static final String mToolchain = "examples/toolchains/AutomataScriptInterpreter.xml";
 	private static final File mToolchainFile = new File(TestUtil.getPathFromTrunk(mToolchain));
-	private static int mTimeout = 60 * 60 * 1000;
+	private static int mTimeout = 60 * 1000;
 	private static final String[] mDirectories = {
 		"examples/Automata/nwaOperations/MinimizationBenchmarks",
 	};
 	private static final String[] mFileEndings = { ".ats" };
+	
+	private static final String[] SETTINGS = {
+			"AutomataScript/minimizeSevpa.epf",
+			"AutomataScript/shrinkNwa.epf",
+			"AutomataScript/minimizeNwaMaxSat2.epf",
+		};
 
 	@Override
 	protected ITestSummary[] constructTestSummaries() {
@@ -93,14 +99,16 @@ public class AutomataMinimizationTestSuite extends UltimateTestSuite {
 		}
 
 		for (final File inputFile : inputFiles) {
-			final File settingsFile = null;
-			final UltimateRunDefinition urd = new UltimateRunDefinition(inputFile, settingsFile, mToolchainFile);
-			final UltimateStarter starter = new UltimateStarter(urd, mTimeout, null, null);
-			final UltimateTestCase utc = new UltimateTestCase(urd.generateShortStringRepresentation(),
-					new AutomataScriptTestResultDecider(), starter,
-					// mDescription + "_" + inputFile.getAbsolutePath(),
-					urd, super.getSummaries(), null);
-			testCases.add(utc);
+			for (final String settingFileName : SETTINGS) {
+				final File settingsFile = new File(TestUtil.getPathFromTrunk("/examples/settings/" + settingFileName));
+				final UltimateRunDefinition urd = new UltimateRunDefinition(inputFile, settingsFile, mToolchainFile);
+				final UltimateStarter starter = new UltimateStarter(urd, mTimeout, null, null);
+				final UltimateTestCase utc = new UltimateTestCase(urd.generateShortStringRepresentation(),
+						new AutomataScriptTestResultDecider(), starter,
+						// mDescription + "_" + inputFile.getAbsolutePath(),
+						urd, super.getSummaries(), null);
+				testCases.add(utc);
+			}
 		}
 		testCases.sort(null);
 		return testCases;
