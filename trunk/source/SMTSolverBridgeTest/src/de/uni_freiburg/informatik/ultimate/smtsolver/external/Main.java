bfb31e9abb372d984a853d3a924e872aa4c82df6
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-09-04 11:59:07 +0200

    stitching for SMTInterpol 2.1-324-ga0525a0 (no review required) additional exports logger wrapper for new SMTInterpol logging interface moved Main and MainMultipleFiles of SMTSolverBridge to their own files s.t. inclusion of test lib makes more sense (they are for testing anyways) using new logging interface

--- trunk/source/SMTSolverBridgeTest/src/de/uni_freiburg/informatik/ultimate/smtsolver/external/Main.java
+++ trunk/source/SMTSolverBridgeTest/src/de/uni_freiburg/informatik/ultimate/smtsolver/external/Main.java
@@ -2,27 +2,27 @@
  * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2012-2015 Oday Jubran
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE SMTSolverBridge.
- * 
+ *
  * The ULTIMATE SMTSolverBridge is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE SMTSolverBridge is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE SMTSolverBridge. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE SMTSolverBridge, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE SMTSolverBridge grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE SMTSolverBridge grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.smtsolver.external;
@@ -30,32 +30,32 @@ package de.uni_freiburg.informatik.ultimate.smtsolver.external;
 import java.io.IOException;
 import java.math.BigInteger;
 
-import org.apache.log4j.Logger;
-
-import de.uni_freiburg.informatik.ultimate.core.coreplugin.services.Log4JWrapper;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.logic.SMTLIBException;
 import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.smtinterpol.option.OptionMap;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.smtlib2.ParseEnvironment;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.smtlib2.SMTInterpol;
+import de.uni_freiburg.informatik.ultimate.test.ConsoleLogger;
 
 public class Main {
-
+	
 	private static void usage() {
 		System.err.println("USAGE smtinterpol [-q] [-v] [-t <num>] [-r <num>] [file.smt2]");
 	}
 
-	public static void main(String[] param) throws IOException {
-
+	public static void main(final String[] param) throws IOException {
+		
 		/** Specify the solver command here. **/
 		final String command = "z3 -smt2 -in";
-
-		final Logger logger = Logger.getRootLogger();
+		final ILogger logger = new ConsoleLogger();
+		final SmtInterpolLogProxyWrapper logProxy = new SmtInterpolLogProxyWrapper(logger);
 		int paramctr = 0;
 		Script benchmark;
 		if (!command.equals("SMTInterpol")) {
-			benchmark = new Scriptor(command, new Log4JWrapper(logger), null, null, "external in solverbridge");
+			benchmark = new Scriptor(command, logger, null, null, "external in solverbridge");
 		} else {
-			benchmark = new SMTInterpol(logger, true);
+			benchmark = new SMTInterpol(logProxy);
 		}
 
 		while (paramctr < param.length && param[paramctr].startsWith("-")) {
@@ -119,7 +119,8 @@ public class Main {
 			usage();
 			return;
 		}
-		final ParseEnvironment parseEnv = new ParseEnvironment(benchmark);
+		final OptionMap opmap = new OptionMap(logProxy);
+		final ParseEnvironment parseEnv = new ParseEnvironment(benchmark, opmap);
 		try {
 			parseEnv.parseScript(filename);
 		} catch (final SMTLIBException exc) {
--- trunk/source/SmtParser/src/de/uni_freiburg/informatik/ultimate/source/smtparser/SmtParser.java
+++ trunk/source/SmtParser/src/de/uni_freiburg/informatik/ultimate/source/smtparser/SmtParser.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE SmtParser plug-in.
- * 
+ *
  * The ULTIMATE SmtParser plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE SmtParser plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE SmtParser plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE SmtParser plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE SmtParser plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE SmtParser plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.source.smtparser;
@@ -30,8 +30,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.log4j.Logger;
 
 import de.uni_freiburg.informatik.ultimate.boogie.ast.Unit;
 import de.uni_freiburg.informatik.ultimate.core.model.ISource;
@@ -47,14 +45,16 @@ import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.treeautomizer.script.HCGBuilderHelper;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.treeautomizer.script.HCGBuilderHelper.ConstructAndInitializeBackendSmtSolver;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.treeautomizer.script.HornClauseParserScript;
+import de.uni_freiburg.informatik.ultimate.smtinterpol.LogProxy;
+import de.uni_freiburg.informatik.ultimate.smtinterpol.option.OptionMap;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.smtlib2.ParseEnvironment;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.smtlib2.SMTInterpol;
 import de.uni_freiburg.informatik.ultimate.smtsolver.external.Scriptor;
+import de.uni_freiburg.informatik.ultimate.smtsolver.external.SmtInterpolLogProxyWrapper;
 
 /**
  * @author Daniel Dietsch
  * @author Matthias Heizmann
- * 
  */
 public class SmtParser implements ISource {
 	protected String[] mFileTypes;
@@ -90,12 +90,12 @@ public class SmtParser implements ISource {
 	}
 
 	@Override
-	public IElement parseAST(File[] files) throws IOException {
+	public IElement parseAST(final File[] files) throws IOException {
 		throw new UnsupportedOperationException("processing several files is not yet implemented");
 	}
 
 	@Override
-	public IElement parseAST(File file) throws IOException {
+	public IElement parseAST(final File file) throws IOException {
 		if (file.isDirectory()) {
 			return parseAST(file.listFiles());
 		} else {
@@ -105,7 +105,7 @@ public class SmtParser implements ISource {
 	}
 
 	@Override
-	public boolean parseable(File[] files) {
+	public boolean parseable(final File[] files) {
 		for (final File f : files) {
 			if (!parseable(f)) {
 				return false;
@@ -115,7 +115,7 @@ public class SmtParser implements ISource {
 	}
 
 	@Override
-	public boolean parseable(File file) {
+	public boolean parseable(final File file) {
 		for (final String s : getFileTypes()) {
 			if (file.getName().endsWith(s)) {
 				return true;
@@ -131,11 +131,11 @@ public class SmtParser implements ISource {
 
 	@Override
 	public ModelType getOutputDefinition() {
-		return new ModelType(Activator.PLUGIN_ID,ModelType.Type.OTHER, mFileNames);
+		return new ModelType(Activator.PLUGIN_ID, ModelType.Type.OTHER, mFileNames);
 	}
 
 	@Override
-	public void setPreludeFile(File prelude) {
+	public void setPreludeFile(final File prelude) {
 	}
 
 	@Override
@@ -144,61 +144,65 @@ public class SmtParser implements ISource {
 	}
 
 	@Override
-	public void setToolchainStorage(IToolchainStorage storage) {
+	public void setToolchainStorage(final IToolchainStorage storage) {
 		mStorage = storage;
 	}
 
 	@Override
-	public void setServices(IUltimateServiceProvider services) {
+	public void setServices(final IUltimateServiceProvider services) {
 		mServices = services;
 		mLogger = mServices.getLoggingService().getLogger(Activator.PLUGIN_ID);
 	}
 
 	@Override
 	public void finish() {
-
+		
 	}
 	
-	private void processFile(File file) throws IOException {
+	private void processFile(final File file) throws IOException {
 		
-		final boolean useExternalSolver = (mServices.getPreferenceProvider(Activator.PLUGIN_ID)).getBoolean(PreferenceInitializer.LABEL_UseExtSolver);
-		final String commandExternalSolver = (mServices.getPreferenceProvider(Activator.PLUGIN_ID)).getString(PreferenceInitializer.LABEL_ExtSolverCommand);
+		final boolean useExternalSolver = mServices.getPreferenceProvider(Activator.PLUGIN_ID)
+				.getBoolean(PreferenceInitializer.LABEL_UseExtSolver);
+		final String commandExternalSolver = mServices.getPreferenceProvider(Activator.PLUGIN_ID)
+				.getString(PreferenceInitializer.LABEL_ExtSolverCommand);
 		
-		final boolean writeCommandsToFile = (mServices.getPreferenceProvider(Activator.PLUGIN_ID)).getBoolean(PreferenceInitializer.LABEL_WriteToFile);
-		final String filename = (mServices.getPreferenceProvider(Activator.PLUGIN_ID)).getString(PreferenceInitializer.LABEL_Filename);
+		final boolean writeCommandsToFile = mServices.getPreferenceProvider(Activator.PLUGIN_ID)
+				.getBoolean(PreferenceInitializer.LABEL_WriteToFile);
+		final String filename =
+				mServices.getPreferenceProvider(Activator.PLUGIN_ID).getString(PreferenceInitializer.LABEL_Filename);
 
-		final boolean inHornSolverMode = (mServices.getPreferenceProvider(Activator.PLUGIN_ID)).getBoolean(PreferenceInitializer.LABEL_HornSolverMode);
+		final boolean inHornSolverMode = mServices.getPreferenceProvider(Activator.PLUGIN_ID)
+				.getBoolean(PreferenceInitializer.LABEL_HornSolverMode);
 		
+		final LogProxy logProxy = new SmtInterpolLogProxyWrapper(mLogger);
 		Script script;
-
 		if (inHornSolverMode) {
 			mLogger.info("Parsing .smt2 file as a set of Horn Clauses");
-			final ConstructAndInitializeBackendSmtSolver caibss = 
+			final ConstructAndInitializeBackendSmtSolver caibss =
 					new HCGBuilderHelper.ConstructAndInitializeBackendSmtSolver(mServices, mStorage, null);
-			script = new HornClauseParserScript(
-					caibss.getScript(), 
-					caibss.getLogicForExternalSolver(), 
-					caibss.getSolverSettings()); 
+			script = new HornClauseParserScript(caibss.getScript(), caibss.getLogicForExternalSolver(),
+					caibss.getSolverSettings());
 			output = ((HornClauseParserScript) script).getHornClauses();
 		} else {
 			if (useExternalSolver) {
 				mLogger.info("Starting external SMT solver with command " + commandExternalSolver);
-				script = new Scriptor(commandExternalSolver, mLogger, mServices, mStorage, 
+				script = new Scriptor(commandExternalSolver, mLogger, mServices, mStorage,
 						"external solver of SMT parser plugin");
 			} else {
 				mLogger.info("Starting SMTInterpol");
-				script = new SMTInterpol((Logger) mServices.getLoggingService().getBacking(mLogger, Logger.class), true);
+				script = new SMTInterpol(logProxy);
 			}
 
 			if (writeCommandsToFile) {
-				final String abs = (new File(filename)).getAbsolutePath();
+				final String abs = new File(filename).getAbsolutePath();
 				mLogger.info("Writing all SMT commands to " + abs);
-				script = new LoggingScript(script ,filename, true);
+				script = new LoggingScript(script, filename, true);
 			}
 		}
 
 		mLogger.info("Executing SMT file " + file.getAbsolutePath());
-		final ParseEnvironment parseEnv = new ParseEnvironment(script);
+		final OptionMap optionMap = new OptionMap(logProxy);
+		final ParseEnvironment parseEnv = new ParseEnvironment(script, optionMap);
 		try {
 			parseEnv.parseScript(file.getAbsolutePath());
 			mLogger.info("Succesfully executed SMT file " + file.getAbsolutePath());
