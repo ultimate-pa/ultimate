fcd80e6d14ad7a5655af902f81e7145c85f5880b
schillic <schillic@informatik.uni-freiburg.de>
2016-09-08 21:26:26 +0200

    use BigDecimal.TEN

--- trunk/source/AbstractInterpretationV2Test/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/util/lpsolver/LinearConstraintTest.java
+++ trunk/source/AbstractInterpretationV2Test/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/util/lpsolver/LinearConstraintTest.java
@@ -20,9 +20,9 @@
  * 
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE AbstractInterpretationV2 plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE AbstractInterpretationV2 plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE AbstractInterpretationV2 plug-in grant you additional permission
  * to convey the resulting work.
  */
 
@@ -49,7 +49,7 @@ public class LinearConstraintTest {
 		xconstr.addCoefficient("x", BigDecimal.ONE);
 		xconstr.addCoefficient("y", BigDecimal.ZERO);
 		xconstr.setLower(BigDecimal.ZERO);
-		xconstr.setUpper(new BigDecimal(10));
+		xconstr.setUpper(BigDecimal.TEN);
 
 		assertTrue(xconstr.getName() == "x-constraint");
 		assertTrue(xconstr.getVariableCount() == 2);
