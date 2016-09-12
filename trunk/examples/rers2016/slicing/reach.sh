244f48fe42b39c99d0da9ebff0daa1dda07e02d9
Sergio Feo-Arenis <arenis@monteverdi.informatik.uni-freiburg.de>
2016-09-12 17:51:28 +0200

    settings used for reachability checking with Frama-C value analysis

--- trunk/examples/rers2016/slicing/reach.sh
+++ trunk/examples/rers2016/slicing/reach.sh
@@ -7,6 +7,6 @@ FN="${FN%.*}"
 
 echo "Processing $FN..."
 awk '/.*__VERIFIER_error\([0-9].*/ {print "/*@ assert 0; */"} {print $0}' $INPUT > tmpfiler$FN.c
-frama-c -quiet -val -eva -slevel 10 tmpfiler$FN.c > result$FN.txt
+frama-c -quiet -val -eva -slevel 15 tmpfiler$FN.c > result$FN.txt
 grep assertion result$FN.txt | awk -F: '{ln=$2; cmd="sed -n " (ln+1) "p '"tmpfiler$FN.c"'"; cmd | getline line; print line " " $4}' -
 rm -f tmpfiler$FN.c
