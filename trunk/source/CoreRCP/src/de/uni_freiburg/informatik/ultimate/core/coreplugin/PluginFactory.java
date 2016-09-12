9b85ceae73be595efd5d6f89a2cff7bbf74c8eb6
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-09-12 16:17:32 +0200

    fixed wrong usage of Boolean.getBoolean

--- trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/core/coreplugin/PluginFactory.java
+++ trunk/source/CoreRCP/src/de/uni_freiburg/informatik/ultimate/core/coreplugin/PluginFactory.java
@@ -293,7 +293,7 @@ final class PluginFactory implements IServiceFactoryFactory {
 
 	private IController<RunDefinition> loadControllerPlugin(final IConfigurationElement controllerDescriptor) {
 		final IController<RunDefinition> controller = createInstance(controllerDescriptor);
-		mGuiMode = Boolean.getBoolean(controllerDescriptor.getAttribute("isGraphical"));
+		mGuiMode = Boolean.valueOf(controllerDescriptor.getAttribute("isGraphical"));
 		mSettingsManager.registerPlugin(controller);
 
 		mLogger.info("Loaded " + (mGuiMode ? "graphical " : "") + "controller " + controller.getPluginName());
