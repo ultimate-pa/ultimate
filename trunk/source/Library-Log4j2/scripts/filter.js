d6bf3a4f972f64a1218f4778bb8f0d7ccbf91bc4
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-09-13 15:08:37 +0200

    reworked log4j2 library support (see #42)  - readded log4j2 with only api and core  - added Libary-Disruptor to fix windows build

--- trunk/source/Library-Log4j2/scripts/filter.js
+++
@@ -1,7 +0,0 @@
-var result = false;
-if (logEvent.getMarker() != null && logEvent.getMarker().isInstanceOf("FLOW")) {
-	result = true;
-} else if (logEvent.getContextMap().containsKey("UserId")) {
-	result = true;
-}
-result;
