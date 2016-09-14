f2ede10e6071d8583c5d5a9404353ccfc2fb5a92
Langenfeld <langenfv@informatik.uni-freiburg.de>
2016-09-14 17:37:43 +0200

    Produkt construction for ltl checking for steps annotated in c code (currently breaks single statement ltl checker)

---
+++ trunk/examples/LTL/ltlStepwise/G_smallInCall2_unsafe.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+#include <assert.h>
+#include <math.h>
+
+//#Safe
+//@ ltl invariant positive: []( AP(a > b) );
+
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern void __VERIFIER_ltl_step() __attribute__ ((__noreturn__));
+
+int a = 5;
+int b = 3;
+int tempa = 33, tempb = 13; 
+	
+	
+void foo(int x, int y){
+	__VERIFIER_ltl_step();
+	b = x;
+	a = y;
+	return;
+}
+	
+int main()
+{
+	while(1){
+		a = 20;
+		foo(33, 1);
+	}
+}
+
