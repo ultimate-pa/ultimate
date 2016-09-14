5a1fefcd768522027972cf77b954ecd0acfc1274
schillic <schillic@informatik.uni-freiburg.de>
2016-09-14 18:42:39 +0200

    removed imports which are only used in JavaDoc

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/interpolantautomata/builders/IInterpolantAutomatonBuilder.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/interpolantautomata/builders/IInterpolantAutomatonBuilder.java
@@ -27,17 +27,14 @@
 package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.interpolantautomata.builders;
 
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
-import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.AbstractCegarLoop;
 
 /**
  * An {@link IInterpolantAutomatonBuilder} is used in trace abstraction's
- * {@link AbstractCegarLoop#constructInterpolantAutomaton} to construct an interpolant automaton.
+ * {@link de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.AbstractCegarLoop
+ * #constructInterpolantAutomaton} to construct an interpolant automaton.
  *
  * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
- *
  */
 public interface IInterpolantAutomatonBuilder<LETTER, STATE> {
-
 	NestedWordAutomaton<LETTER, STATE> getResult();
-
 }
