243b0e1bfd1c68ba8cf891c812f855a76e0ccf85
schillic <schillic@informatik.uni-freiburg.de>
2016-09-23 20:10:34 +0200

    added 'package-info's to automaton delta debugger

---
+++ trunk/source/AutomatonDeltaDebugger/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/automatondeltadebugger/utils/package-info.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016 Christian Schilling (schillic@informatik.uni-freiburg.de)
+ * Copyright (C) 2016 University of Freiburg
+ * 
+ * This file is part of the ULTIMATE Automaton Delta Debugger.
+ * 
+ * The ULTIMATE Automaton Delta Debugger is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * The ULTIMATE Automaton Delta Debugger is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automaton Delta Debugger. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7: If you modify the
+ * ULTIMATE Automaton Delta Debugger, or any covered work, by linking or
+ * combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automaton Delta Debugger grant you additional
+ * permission to convey the resulting work.
+ */
+/**
+ * Utilities for the automaton delta debugger.
+ * 
+ * @author Christian Schilling (schillic@informatik.uni-freiburg.de)
+ */
+package de.uni_freiburg.informatik.ultimate.plugins.analysis.automatondeltadebugger.utils;
