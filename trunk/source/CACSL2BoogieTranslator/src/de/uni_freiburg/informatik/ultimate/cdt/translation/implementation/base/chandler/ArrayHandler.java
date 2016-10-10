512e1523c19046f131555473de4e1e8009668cc6
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-10-10 14:09:53 +0200

    cc fixes

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/ArrayHandler.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/ArrayHandler.java
@@ -3,22 +3,22 @@
  * Copyright (C) 2012-2015 Markus Lindenmann (lindenmm@informatik.uni-freiburg.de)
  * Copyright (C) 2013-2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE CACSL2BoogieTranslator plug-in.
- * 
+ *
  * The ULTIMATE CACSL2BoogieTranslator plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE CACSL2BoogieTranslator plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE CACSL2BoogieTranslator plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE CACSL2BoogieTranslator plug-in, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
@@ -63,12 +63,12 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.cacsl2boogietransla
 
 /**
  * Class that handles translation of arrays.
- * 
+ *
  * @author Markus Lindenmann, Matthias Heizmann
  * @date 12.10.2012
  */
 public class ArrayHandler {
-	
+
 	private final PointerCheckMode mCheckArrayAccessOffHeap;
 
 	public ArrayHandler(final IPreferenceProvider prefs) {
@@ -77,23 +77,21 @@ public class ArrayHandler {
 	}
 
 	/**
-	 * Handle array subscript expression according to Sections 6.5.2.1 of C11.
-	 * For a[i] we will <b>not</b> return the object a[i] as an {@link RValue}
-	 * instead, we return the address of the object a[i] as a {@link HeapLValue}
-	 * or a {@link LocalLValue}.
+	 * Handle array subscript expression according to Sections 6.5.2.1 of C11. For a[i] we will <b>not</b> return the
+	 * object a[i] as an {@link RValue} instead, we return the address of the object a[i] as a {@link HeapLValue} or a
+	 * {@link LocalLValue}.
 	 */
-	public ExpressionResult handleArraySubscriptExpression(final Dispatcher main,
-			final MemoryHandler memoryHandler, final StructHandler structHandler,
-			final IASTArraySubscriptExpression node) {
+	public ExpressionResult handleArraySubscriptExpression(final Dispatcher main, final MemoryHandler memoryHandler,
+			final StructHandler structHandler, final IASTArraySubscriptExpression node) {
 		final ILocation loc = LocationFactory.createCLocation(node);
-		
+
 		ExpressionResult subscript = (ExpressionResult) main.dispatch(node.getArgument());
 		subscript = subscript.switchToRValueIfNecessary(main, memoryHandler, structHandler, loc);
 		subscript.rexBoolToIntIfNecessary(loc, ((CHandler) main.mCHandler).getExpressionTranslation());
 		assert subscript.lrVal.getCType().isIntegerType();
 
 		ExpressionResult leftExpRes = ((ExpressionResult) main.dispatch(node.getArrayExpression()));
-		
+
 		final ExpressionResult result;
 		final CType cTypeLeft = leftExpRes.lrVal.getCType();
 		if (cTypeLeft instanceof CPointer) {
@@ -122,8 +120,8 @@ public class ArrayHandler {
 				assert isOutermostSubscriptExpression(node) : "not outermost";
 				resultCType = cArray.getValueType();
 			} else {
-				final RValue[] newDimensions = Arrays.copyOfRange(
-						cArray.getDimensions(), 1, cArray.getDimensions().length);
+				final RValue[] newDimensions =
+						Arrays.copyOfRange(cArray.getDimensions(), 1, cArray.getDimensions().length);
 				assert newDimensions.length == cArray.getDimensions().length - 1;
 				resultCType = new CArray(newDimensions, cArray.getValueType());
 			}
@@ -137,14 +135,14 @@ public class ArrayHandler {
 				// declaration is a[3][5][7] and we are processing the innermost
 				// of a nested subscript expression a[2].
 				// Then the resulting address will be
-				//     addressOf(a) + 2 * 5 * 7 * sizeof(valueType)
+				// addressOf(a) + 2 * 5 * 7 * sizeof(valueType)
 				// We achieve this by doing pointer arithmetic where we use
 				// the "remaining" array as pointsToType, i.e., we compute
-				//     addressOf(a) + 2 * sizeof(resultCType)
+				// addressOf(a) + 2 * sizeof(resultCType)
 				final Expression oldAddress = ((HeapLValue) leftExpRes.lrVal).getAddress();
 				final RValue index = (RValue) subscript.lrVal;
 				final ExpressionResult newAddress_ER = ((CHandler) main.mCHandler).doPointerArithmeticWithConversion(
-						main, IASTBinaryExpression.op_plus, loc, oldAddress, index,	resultCType);
+						main, IASTBinaryExpression.op_plus, loc, oldAddress, index, resultCType);
 				final Expression newAddress = newAddress_ER.lrVal.getValue();
 				final HeapLValue lValue = new HeapLValue(newAddress, resultCType, false);
 				result = ExpressionResult.copyStmtDeclAuxvarOverapprox(leftExpRes, subscript);
@@ -167,9 +165,8 @@ public class ArrayHandler {
 					final Expression[] oldIndices = ((ArrayLHS) oldInnerArrayLHS).getIndices();
 					final Expression[] newIndices = new Expression[oldIndices.length + 1];
 					System.arraycopy(oldIndices, 0, newIndices, 0, oldIndices.length);
-					newIndices[newIndices.length-1] = index.getValue();
-					newInnerArrayLHS = new ArrayLHS(loc,
-							((ArrayLHS) oldInnerArrayLHS).getArray(), newIndices);
+					newIndices[newIndices.length - 1] = index.getValue();
+					newInnerArrayLHS = new ArrayLHS(loc, ((ArrayLHS) oldInnerArrayLHS).getArray(), newIndices);
 				} else {
 					assert isInnermostSubscriptExpression(node) : "not innermost";
 					newInnerArrayLHS = new ArrayLHS(loc, oldInnerArrayLHS, new Expression[] { index.getValue() });
@@ -182,31 +179,30 @@ public class ArrayHandler {
 				throw new AssertionError("result.lrVal has to be either HeapLValue or LocalLValue");
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
-	 * Add to exprResult a check that the index is within the bounds of an array.
-	 * Depending on the preferences of this plugin we
+	 * Add to exprResult a check that the index is within the bounds of an array. Depending on the preferences of this
+	 * plugin we
 	 * <ul>
-	 *  <li> assert that the index is in the range of the bounds,
-	 *  <li> assume that the index is in the range of the bounds, or
-	 *  <li> add nothing.
+	 * <li>assert that the index is in the range of the bounds,
+	 * <li>assume that the index is in the range of the bounds, or
+	 * <li>add nothing.
 	 * </ul>
-	 * For multidimensional arrays, this check has to be done separately for
-	 * each index.
-	 * This simple check ignores the typesize of the value, compares only
-	 * the index with the dimension and is hence only applicable if the
-	 * array is represented as a {@link LocalLValue}.
-	 * @param currentIndex {@link Expression} that represents the index
-	 * @param currentDimension {@link Expression} that represents the dimension
-	 * 		that corresponds to the index
+	 * For multidimensional arrays, this check has to be done separately for each index. This simple check ignores the
+	 * typesize of the value, compares only the index with the dimension and is hence only applicable if the array is
+	 * represented as a {@link LocalLValue}.
+	 * 
+	 * @param currentIndex
+	 *            {@link Expression} that represents the index
+	 * @param currentDimension
+	 *            {@link Expression} that represents the dimension that corresponds to the index
 	 */
-	private void addArrayBoundsCheckForCurrentIndex(final Dispatcher main,
-			final ILocation loc, final RValue currentIndex,
-			final RValue currentDimension, final ExpressionResult exprResult) {
-		if (mCheckArrayAccessOffHeap  == PointerCheckMode.IGNORE) {
+	private void addArrayBoundsCheckForCurrentIndex(final Dispatcher main, final ILocation loc,
+			final RValue currentIndex, final RValue currentDimension, final ExpressionResult exprResult) {
+		if (mCheckArrayAccessOffHeap == PointerCheckMode.IGNORE) {
 			// do not check anything
 			return;
 		}
@@ -221,14 +217,13 @@ public class ArrayHandler {
 		// of the dimension.
 		{
 			final CPrimitive indexType = (CPrimitive) currentIndex.getCType();
-			final Expression zero = cHandler.getExpressionTranslation().constructLiteralForIntegerType(
-					loc, indexType, BigInteger.ZERO);
-			final Expression nonNegative = cHandler.getExpressionTranslation().constructBinaryComparisonExpression(
-					loc, IASTBinaryExpression.op_lessEqual, zero, indexType,
-					currentIndex.getValue(), indexType);
-			final Expression notTooBig = cHandler.getExpressionTranslation().constructBinaryComparisonExpression(
-					loc, IASTBinaryExpression.op_lessThan, currentIndex.getValue(), indexType,
-					currentDimension.getValue(), (CPrimitive) currentDimension.getCType());
+			final Expression zero =
+					cHandler.getExpressionTranslation().constructLiteralForIntegerType(loc, indexType, BigInteger.ZERO);
+			final Expression nonNegative = cHandler.getExpressionTranslation().constructBinaryComparisonExpression(loc,
+					IASTBinaryExpression.op_lessEqual, zero, indexType, currentIndex.getValue(), indexType);
+			final Expression notTooBig = cHandler.getExpressionTranslation().constructBinaryComparisonExpression(loc,
+					IASTBinaryExpression.op_lessThan, currentIndex.getValue(), indexType, currentDimension.getValue(),
+					(CPrimitive) currentDimension.getCType());
 			inRange = ExpressionFactory.newBinaryExpression(loc, Operator.LOGICAND, nonNegative, notTooBig);
 		}
 		switch (mCheckArrayAccessOffHeap) {
@@ -247,15 +242,15 @@ public class ArrayHandler {
 		default:
 			throw new AssertionError("unknown value");
 		}
-		
+
 	}
 
-	private boolean isInnermostSubscriptExpression(final IASTArraySubscriptExpression node) {
+	private static boolean isInnermostSubscriptExpression(final IASTArraySubscriptExpression node) {
 		return !(node.getArrayExpression() instanceof IASTArraySubscriptExpression);
 	}
-	
-	private boolean isOutermostSubscriptExpression(final IASTArraySubscriptExpression node) {
+
+	private static boolean isOutermostSubscriptExpression(final IASTArraySubscriptExpression node) {
 		return !(node.getParent() instanceof IASTArraySubscriptExpression);
 	}
-	
+
 }
