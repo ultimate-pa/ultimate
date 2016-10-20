2cfd28bb2b479c5c2f8bf261d3dba50f4d7fe0b3
schillic <schillic@informatik.uni-freiburg.de>
2016-10-20 09:43:40 +0200

    benchmark: add relative minimization column

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/summaries/tacasminimization/PrepareOnlineCsv.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/summaries/tacasminimization/PrepareOnlineCsv.java
@@ -18,6 +18,7 @@ import de.uni_freiburg.informatik.ultimate.util.csv.CsvProviderRounding;
 import de.uni_freiburg.informatik.ultimate.util.csv.CsvProviderScale;
 import de.uni_freiburg.informatik.ultimate.util.csv.CsvProviderScale.ScaleMode;
 import de.uni_freiburg.informatik.ultimate.util.csv.ICsvProvider;
+import de.uni_freiburg.informatik.ultimate.util.csv.SimpleCsvProvider;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
 
 /**
@@ -29,6 +30,7 @@ import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
  * @author Christian Schilling (schillic@informatik.uni-freiburg.de)
  */
 public final class PrepareOnlineCsv {
+	private static final String MINIMIZATON_ATTEMPTS_RELATIVE = "MinimizatonAttemptsRelative";
 	private static final String MINIMIZATON_ATTEMPTS = "MinimizatonAttempts";
 	private static final String STATES_REMOVED_BY_MINIMIZATION = "StatesRemovedByMinimization";
 	private static final String OVERALL_ITERATIONS = "OverallIterations";
@@ -36,10 +38,13 @@ public final class PrepareOnlineCsv {
 	private static final String SETTING = "Settings";
 	private static final String FILE = "File";
 	
+	private static final double HUNDRED = 100.0;
+	
 	private static final String EXTENSION = ".csv";
 	private static final String INPUT_FILE_NAME = "AutomizerOnline";
 	private static final String OUTPUT_AGGREGATED_FILE_NAME = "AutomizerOnlineAggregated";
 	private static final String COUNT = "Count";
+	private static final Double SCALING = Double.valueOf(1_000_000.0);
 	private static final boolean VERBOSE = true;
 	
 	private PrepareOnlineCsv() {
@@ -76,16 +81,19 @@ public final class PrepareOnlineCsv {
 		for (final ICsvProvider<String> csv : csvs) {
 			final ICsvProvider<String> scaledCsv = csvProviderScale.transform(csv);
 			final ICsvProvider<String> roundedCsv = csvProviderRound.transform(scaledCsv);
+			final ICsvProvider<String> addedColumnCsv =
+					addRelativeColumn(roundedCsv, roundedCsv.getColumnTitles().indexOf(OVERALL_ITERATIONS),
+							roundedCsv.getColumnTitles().indexOf(MINIMIZATON_ATTEMPTS), MINIMIZATON_ATTEMPTS_RELATIVE);
 			
-			writeCsvToFile(roundedCsv, OUTPUT_AGGREGATED_FILE_NAME + i);
+			writeCsvToFile(addedColumnCsv, OUTPUT_AGGREGATED_FILE_NAME + i);
 			
-			aggregatedCsvs.add(csv);
+			aggregatedCsvs.add(addedColumnCsv);
 			
 			++i;
 		}
 		writeCsvToFile(new CsvProviderPartition<>(aggregatedCsvs).toCsvProvider(), OUTPUT_AGGREGATED_FILE_NAME);
 	}
-
+	
 	private static CsvProviderPartition<String> getExamplePartition(final ICsvProvider<String> csv) {
 		final CsvProviderPartition<String> partitionByExample;
 		final String statesColumn = FILE;
@@ -110,7 +118,7 @@ public final class PrepareOnlineCsv {
 	private static CsvProviderScale getScaling() {
 		final Map<String, Pair<Double, ScaleMode>> column2Scale = new HashMap<>();
 		
-		column2Scale.put(OVERALL_TIME, new Pair<>(1_000_000.0, CsvProviderScale.ScaleMode.DIV_INT));
+		column2Scale.put(OVERALL_TIME, new Pair<>(SCALING, CsvProviderScale.ScaleMode.DIV_INT));
 		
 		return new CsvProviderScale(column2Scale);
 	}
@@ -124,6 +132,38 @@ public final class PrepareOnlineCsv {
 		column2aggregation.put(MINIMIZATON_ATTEMPTS, Aggregation.AVERAGE);
 		
 		partitionBySetting.transform(new CsvProviderAggregator<>(column2aggregation, COUNT));
+	}
+	
+	@SuppressWarnings("squid:S1244")
+	private static ICsvProvider<String> addRelativeColumn(final ICsvProvider<String> csv, final int baseColumnIndex,
+			final int partColumnIndex, final String newColumnTitle) {
+		final List<String> columnTitles = new ArrayList<>(csv.getColumnTitles().size() + 1);
+		for (final String columnTitle : csv.getColumnTitles()) {
+			columnTitles.add(columnTitle);
+		}
+		columnTitles.add(newColumnTitle);
+		final int newColumnIndex = columnTitles.size() - 1;
+		final ICsvProvider<String> result = new SimpleCsvProvider<>(columnTitles);
+		
+		final int rows = csv.getRowHeaders().size();
+		for (int i = 0; i < rows; ++i) {
+			final List<String> row = csv.getRow(i);
+			final double base = Double.parseDouble(row.get(baseColumnIndex));
+			final double part = Double.parseDouble(row.get(partColumnIndex));
+			final String rel;
+			if (part == 0.0) {
+				rel = "-";
+			} else {
+				rel = Integer.toString((int) (HUNDRED * part / base));
+			}
+			final List<String> newRow = new ArrayList<>(row.size() + 1);
+			for (final String entry : row) {
+				newRow.add(entry);
+			}
+			newRow.add(newColumnIndex, rel);
+			result.addRow(csv.getRowHeaders().get(i), newRow);
+		}
+		return result;
 	}
 	
 	private static void writeCsvToFile(final ICsvProvider<String> csv, final String fileName) {
@@ -184,6 +224,10 @@ public final class PrepareOnlineCsv {
 	 * @author Christian Schilling (schillic@informatik.uni-freiburg.de)
 	 */
 	private static class OnlyOneFilter implements Predicate<ICsvProvider<String>> {
+		public OnlyOneFilter() {
+			// nothing to do
+		}
+		
 		@Override
 		public boolean test(final ICsvProvider<String> csv) {
 			return csv.getRowHeaders().size() == 2;
