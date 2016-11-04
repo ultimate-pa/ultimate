6a0899eebb252db6c2d3f2912aea74ef02c85bd8
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-11-04 19:27:43 +0100

    cc fixes

--- trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/cfg/BoogieIcfgLocation.java
+++ trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/cfg/BoogieIcfgLocation.java
@@ -2,27 +2,27 @@
  * Copyright (C) 2013-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2010-2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE RCFGBuilder plug-in.
- * 
+ *
  * The ULTIMATE RCFGBuilder plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE RCFGBuilder plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE RCFGBuilder plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE RCFGBuilder plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE RCFGBuilder plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE RCFGBuilder plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg;
@@ -41,8 +41,8 @@ import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgE
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgLocation;
 
 /**
- * {@link IcfgLocation} of an ICFG that was obtained directly from a 
- * Boogie program 
+ * {@link IcfgLocation} of an ICFG that was obtained directly from a Boogie program
+ * 
  * @author heizmann@informatik.uni-freiburg.
  */
 
@@ -59,7 +59,8 @@ public class BoogieIcfgLocation extends IcfgLocation {
 	@Visualizable
 	private final boolean mIsErrorLocation;
 
-	public BoogieIcfgLocation(final String debugIdentifier, final String procedure, final boolean isErrorLoc, final BoogieASTNode boogieASTNode) {
+	public BoogieIcfgLocation(final String debugIdentifier, final String procedure, final boolean isErrorLoc,
+			final BoogieASTNode boogieASTNode) {
 		super(debugIdentifier, procedure, new Payload(getLocationFromASTNode(boogieASTNode)));
 		mIsErrorLocation = isErrorLoc;
 		mBoogieASTNode = boogieASTNode;
@@ -81,7 +82,6 @@ public class BoogieIcfgLocation extends IcfgLocation {
 		return loc;
 	}
 
-
 	public boolean isErrorLocation() {
 		return mIsErrorLocation;
 	}
@@ -89,7 +89,6 @@ public class BoogieIcfgLocation extends IcfgLocation {
 	public BoogieASTNode getBoogieASTNode() {
 		return mBoogieASTNode;
 	}
-
 
 	public String BoogieASTNodeType() {
 		if (mBoogieASTNode instanceof AssertStatement) {
@@ -107,19 +106,16 @@ public class BoogieIcfgLocation extends IcfgLocation {
 	public boolean addIncoming(final IcfgEdge incoming) {
 		if (incoming instanceof CodeBlock || incoming instanceof RootEdge) {
 			return super.addIncoming(incoming);
-		} else {
-			throw new IllegalArgumentException("predecessor has to be CodeBlock or RootEdge");
 		}
+		throw new IllegalArgumentException("predecessor has to be CodeBlock or RootEdge");
 	}
 
 	@Override
 	public boolean addOutgoing(final IcfgEdge outgoing) {
 		if (outgoing instanceof CodeBlock || outgoing instanceof RootEdge) {
 			return super.addOutgoing(outgoing);
-		} else {
-			throw new IllegalArgumentException("successor has to be CodeBlock");
 		}
+		throw new IllegalArgumentException("successor has to be CodeBlock");
 	}
-
 
 }
