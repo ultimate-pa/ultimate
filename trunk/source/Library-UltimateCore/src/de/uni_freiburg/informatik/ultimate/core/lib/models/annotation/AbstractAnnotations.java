66897d0b2b852f188641a7c89c4a51b113a47b7b
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-11-08 14:33:49 +0100

    deprecate AbstractAnnotation

--- trunk/source/Library-UltimateCore/src/de/uni_freiburg/informatik/ultimate/core/lib/models/annotation/AbstractAnnotations.java
+++ trunk/source/Library-UltimateCore/src/de/uni_freiburg/informatik/ultimate/core/lib/models/annotation/AbstractAnnotations.java
@@ -2,27 +2,27 @@
  * Copyright (C) 2015 Björn Buchhold
  * Copyright (C) 2013-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Core.
- * 
+ *
  * The ULTIMATE Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Core. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Core grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Core grant you additional permission
  * to convey the resulting work.
  */
 /*
@@ -41,13 +41,16 @@ import java.util.Map;
 import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.core.model.models.annotation.IAnnotations;
+import de.uni_freiburg.informatik.ultimate.core.model.models.annotation.Visualizable;
 
 /**
  * DefaultAnnotations
- * 
+ *
  * @author Björn Buchhold
- * 
+ * @deprecated Use {@link ModernAnnotations} instead and mark fields or methods without parameters with the
+ *             {@link Visualizable} attribute.
  */
+@Deprecated
 public abstract class AbstractAnnotations implements IAnnotations {
 	/**
 	 * long serialVersionUID
@@ -60,14 +63,14 @@ public abstract class AbstractAnnotations implements IAnnotations {
 	private transient Map<String, Object> mBackingMap;
 
 	/**
-	 * Returns the array of keys of this annotation. Subclasses must override
-	 * this to specify which fields are available. May not return null!
+	 * Returns the array of keys of this annotation. Subclasses must override this to specify which fields are
+	 * available. May not return null!
 	 */
 	protected abstract String[] getFieldNames();
 
 	/**
-	 * Returns for a key in the annotation the corresponding object. Subclasses
-	 * must override this to return the value of the corresponding field.
+	 * Returns for a key in the annotation the corresponding object. Subclasses must override this to return the value
+	 * of the corresponding field.
 	 */
 	protected abstract Object getFieldValue(String field);
 
@@ -92,7 +95,7 @@ public abstract class AbstractAnnotations implements IAnnotations {
 							@Override
 							public Entry<String, Object> next() {
 								final String field = mAttribFields[fieldCount++];
-								return new AbstractMap.SimpleImmutableEntry<String, Object>(field, getFieldValue(field));
+								return new AbstractMap.SimpleImmutableEntry<>(field, getFieldValue(field));
 							}
 
 							@Override
@@ -118,9 +121,8 @@ public abstract class AbstractAnnotations implements IAnnotations {
 	}
 
 	/**
-	 * Return a string representation of this class. The default implementation
-	 * will print the class name of the annotation followed by colon and the
-	 * attribute map.
+	 * Return a string representation of this class. The default implementation will print the class name of the
+	 * annotation followed by colon and the attribute map.
 	 */
 	@Override
 	public String toString() {
