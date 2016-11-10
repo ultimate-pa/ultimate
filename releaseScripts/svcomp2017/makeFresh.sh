a5d610146c1fda46b9b218f1e935a13ad1c675e4
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-11-10 14:14:27 +0100

    improved makefresh

--- releaseScripts/svcomp2017/makeFresh.sh
+++ releaseScripts/svcomp2017/makeFresh.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+DEPLOY_SERVER=sotec.informatik.uni-freiburg.de
+DEPLOY_DIR=/export/server/httpd/ultimate/downloads/svcomp2017
+TESTFILE=caniwrite
+FILE="$DEPLOY_DIR"/"$TESTFILE"
+
 if [ -z "$1" ]
 then
 	CURRENTUSER=$(whoami)
 else
 	CURRENTUSER="$1"
+fi
+
+echo "Enter password for user ${CURRENTUSER} on ${DEPLOY_SERVER}:" 
+read -s -p "Password: " SSHPASS
+export SSHPASS
+sshpass -e ssh -oHostKeyAlgorithms=+ssh-dss $CURRENTUSER@$DEPLOY_SERVER "( test -e ${FILE} || touch ${FILE} ) && test -w ${FILE} && /usr/bin/rm ${FILE}"
+if [ ! $? -eq 0 ]; then 
+	echo "Something is wrong with your write permissions to ${DEPLOY_DIR}, fix it before continuing"
+	exit 1
 fi
 
 pushd ../../trunk/source/BA_MavenParentUltimate/
@@ -15,7 +29,6 @@ popd
 ./createZip.sh Automizer linux AutomizerC_WitnessPrinter.xml BuchiAutomizerCWithBlockEncoding.xml AutomizerC.xml
 ./createZip.sh Kojak linux KojakC_WitnessPrinter.xml
 
-scp -oHostKeyAlgorithms=+ssh-dss *.zip $CURRENTUSER@sotec.informatik.uni-freiburg.de:/export/server/httpd/ultimate/downloads/svcomp2017/.
+rsync -P --rsh="sshpass -e ssh -l me8 -oHostKeyAlgorithms=+ssh-dss" *.zip $CURRENTUSER@${DEPLOY_SERVER}:${DEPLOY_DIR}/. 
 rm *.zip 
-rm revision
 
