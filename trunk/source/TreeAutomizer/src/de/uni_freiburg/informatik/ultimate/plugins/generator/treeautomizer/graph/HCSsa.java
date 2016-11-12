ed06ceba1b7c5e15a74f3be1eb16a0a5619abead
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2016-11-12 04:04:57 +0100

    Updated the generation of the HornClause-SSA.

--- trunk/source/TreeAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/treeautomizer/graph/HCSsa.java
+++ trunk/source/TreeAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/treeautomizer/graph/HCSsa.java
@@ -1,17 +1,70 @@
 package de.uni_freiburg.informatik.ultimate.plugins.generator.treeautomizer.graph;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import de.uni_freiburg.informatik.ultimate.automata.tree.Tree;
+import de.uni_freiburg.informatik.ultimate.automata.tree.TreeRun;
+import de.uni_freiburg.informatik.ultimate.logic.Annotation;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 
 public class HCSsa {
-	private final Tree<Term> nestedFormulas;
+	private final TreeRun<Term, HCPredicate> nestedFormulas;
 	private final Term postCondition;
 	private final Term preCondition;
+	private final Map<Term, Integer> mCounters;
+	private final Map<Term, Term> mTermToAssertion;
 	
-	
-	public HCSsa(final Tree<Term> t, final Term pre, final Term post) {
+	public HCSsa(final TreeRun<Term, HCPredicate> t, final Term pre, final Term post, final Map<Term, Integer> counters) {
 		nestedFormulas = t;
 		postCondition = post;
 		preCondition = pre;
+		mCounters = counters;
+		mTermToAssertion = new HashMap<>();
+	}
+	
+	protected int getCounter(final Term t) {
+		if (!mCounters.containsKey(t)) {
+			int r = mCounters.size() + 1;
+			mCounters.put(t, r);
+		}
+		return mCounters.get(t);
+	}
+	
+	protected String getName(final Term t) {
+		return "HCsSATerm_" + getCounter(t);
+	}
+	
+	public List<Term> flatten() {
+		return flatten(nestedFormulas);
+	}
+	
+	private List<Term> flatten(final TreeRun<Term, HCPredicate> tree) {
+		ArrayList<Term> res = new ArrayList<>();
+		for (final TreeRun<Term, HCPredicate> child : tree.getChildren()) {
+			res.addAll(flatten(child));
+		}
+		if (tree.getRootSymbol() != null) {
+			final Annotation ann = new Annotation(":named", getName(tree.getRootSymbol()));
+			res.add(tree.getRootSymbol());
+		}
+		return res;
+	}
+	
+	public TreeRun<Term, HCPredicate> getFormulasTree() {
+		return nestedFormulas;
+	}
+
+	protected Term getPredicateVariable(final Term term, final Script script) {
+		if (!mTermToAssertion.containsKey(term)) {
+			final String name = getName(term);
+			mTermToAssertion.put(term, script.term(name));
+		}
+
+		return mTermToAssertion.get(term);
 	}
 }
+
--- trunk/source/TreeAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/treeautomizer/graph/SSABuilder.java
+++ trunk/source/TreeAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/treeautomizer/graph/SSABuilder.java
@@ -13,110 +13,113 @@ import de.uni_freiburg.informatik.ultimate.logic.Sort;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.boogie.MultiElementCounter;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.hornutil.HCTransFormula;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.hornutil.HCVar;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.Substitution;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.TermTransferrer;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.PredicateUtils;
-import de.uni_freiburg.informatik.ultimate.plugins.generator.treeautomizer.hornutil.HCTransFormula;
-import de.uni_freiburg.informatik.ultimate.plugins.generator.treeautomizer.hornutil.HCVar;
 
 public class SSABuilder {
-	private final ITreeRun<HCTransFormula, IPredicate> mTreeRun;
-	private final IPredicate mPostCondition;
-	private final IPredicate mPreCondition;
+	private final ITreeRun<HCTransFormula, HCPredicate> mTreeRun;
+	private final HCPredicate mPostCondition;
+	private final HCPredicate mPreCondition;
 	private final Script mScript;
-	
+
 	private final HCSsa mResult;
 
 	private final boolean mTransferToScriptNeeded;
-	
+
 	protected final Map<Term, HCVar> mConstants2HCVar = new HashMap<>();
 
 	private final TermTransferrer mTermTransferrer;
-	
-	private final MultiElementCounter<TermVariable> mConstForTvCounter = 
-			new MultiElementCounter<TermVariable>();
-	
-	
-	private Map<HCVar, Term> currentLocalAndOldVarVersion;
-	
+
+	private final MultiElementCounter<TermVariable> mConstForTvCounter = new MultiElementCounter<TermVariable>();
+
+	private final Map<HCVar, Term> currentLocalAndOldVarVersion;
 
 	private final Map<HCVar, TreeMap<Integer, Term>> mIndexedVarRepresentative = new HashMap<HCVar, TreeMap<Integer, Term>>();
 
+	private final Map<Term, Integer> mCounters;
 	private int currentTree = -1;
 	private final Map<String, Term> mIndexedConstants = new HashMap<String, Term>();
-	
-	public SSABuilder(final ITreeRun<HCTransFormula, IPredicate> trace, final Script script, final IPredicate preCondition, final IPredicate postCondition) {
+
+	public SSABuilder(final ITreeRun<HCTransFormula, HCPredicate> trace, final Script script,
+			final HCPredicate preCondition, final HCPredicate postCondition, 
+			final Map<Term, Integer> counters) {
 		mTreeRun = trace;
 		mScript = script;
 		mTermTransferrer = new TermTransferrer(mScript);
-		mTransferToScriptNeeded = false;
+		mTransferToScriptNeeded = true;
 		mPreCondition = preCondition;
 		mPostCondition = postCondition;
-		
+		mCounters = counters;
+
+		currentLocalAndOldVarVersion = new HashMap<>();
 		mResult = buildSSA();
 	}
-	
-	public SSABuilder(final ITreeRun<HCTransFormula, IPredicate> trace, final Script script) {
-		this(trace, script, null, null);
+
+	public SSABuilder(final ITreeRun<HCTransFormula, HCPredicate> trace, final Script script) {
+		this(trace, script, null, null, new HashMap<>());
 	}
-	
-	
+
 	public HCSsa getSSA() {
 		return mResult;
 	}
-	
-	private final Map<TreeRun<HCTransFormula, IPredicate>, Integer> idxMap = new HashMap<>();
+
+	private final Map<TreeRun<HCTransFormula, HCPredicate>, Integer> idxMap = new HashMap<>();
 	private int curIdx = 0;
-	
-	public int getIndex(final TreeRun<HCTransFormula, IPredicate> tree) {
+
+	public int getIndex(final TreeRun<HCTransFormula, HCPredicate> tree) {
 		if (!idxMap.containsKey(tree)) {
 			idxMap.put(tree, ++curIdx);
 		}
 		return idxMap.get(tree);
 	}
-	
-	private Tree<Term> buildNestedFormulaTree(final TreeRun<HCTransFormula, IPredicate> tree) {
-		final ArrayList<Tree<Term>> childTrees = new ArrayList<>();
+
+	private TreeRun<Term, HCPredicate> buildNestedFormulaTree(final TreeRun<HCTransFormula, HCPredicate> tree) {
+		final ArrayList<TreeRun<Term, HCPredicate>> childTrees = new ArrayList<>();
 		currentTree = getIndex(tree);
-		for (final TreeRun<HCTransFormula, IPredicate> child : tree.getChildren()) {
+		for (final TreeRun<HCTransFormula, HCPredicate> child : tree.getChildren()) {
 			childTrees.add(buildNestedFormulaTree(child));
 		}
-		currentTree = getIndex(tree);
-		
+
+		if (tree.getRootSymbol() == null) {
+			return new TreeRun<Term, HCPredicate>(tree.getRoot(), null, childTrees);
+		}
 		final VariableVersioneer vvRoot = new VariableVersioneer(tree.getRootSymbol());
 		vvRoot.versionInVars();
 		vvRoot.versionAssignedVars(getIndex(tree));
 		
-		return new Tree<Term>(vvRoot.getVersioneeredTerm(), childTrees);
+		currentTree = getIndex(tree);
+
+		return new TreeRun<Term, HCPredicate>(tree.getRoot(), vvRoot.getVersioneeredTerm(), childTrees);
 	}
-	
+
 	private HCSsa buildSSA() {
 		currentTree = 0;
 		final VariableVersioneer vvPre = new VariableVersioneer(mPreCondition);
 		vvPre.versionPredicate();
-		
-		
-		final Tree<Term> tree = buildNestedFormulaTree((TreeRun<HCTransFormula, IPredicate>) mTreeRun);
+
+		final TreeRun<Term, HCPredicate> tree = buildNestedFormulaTree((TreeRun<HCTransFormula, HCPredicate>) mTreeRun);
 		currentTree = 0;
 		final VariableVersioneer vvPost = new VariableVersioneer(mPostCondition);
 		vvPost.versionPredicate();
-		
-		return new HCSsa(tree, vvPre.getVersioneeredTerm(), vvPost.getVersioneeredTerm());
+
+		return new HCSsa(tree, vvPre.getVersioneeredTerm(), vvPost.getVersioneeredTerm(), mCounters);
 	}
-	
-	
+
 	public Map<Term, HCVar> getConstants2BoogieVar() {
 		return mConstants2HCVar;
 	}
+
 	private Term getCurrentVarVersion(final HCVar bv) {
 		Term result = currentLocalAndOldVarVersion.get(bv);
 		if (result == null) {
 			// variable was not yet assigned in the calling context
 			result = setCurrentVarVersion(bv, currentTree);
 		}
-		
+
 		return result;
 	}
 
@@ -126,9 +129,9 @@ public class SSABuilder {
 	 */
 	private Term setCurrentVarVersion(final HCVar bv, final int index) {
 		final Term var = buildVersion(bv, index);
-		
+
 		currentLocalAndOldVarVersion.put(bv, var);
-		
+
 		return var;
 	}
 
@@ -144,12 +147,11 @@ public class SSABuilder {
 		}
 		assert !index2constant.containsKey(index) : "version was already constructed";
 		final Sort sort = transferToCurrentScriptIfNecessary(bv.getTermVariable()).getSort();
-		final Term constant = PredicateUtils.getIndexedConstant(bv.getGloballyUniqueId(), 
-				sort, index, mIndexedConstants, mScript);
+		final Term constant = PredicateUtils.getIndexedConstant(bv.getGloballyUniqueId(), sort, index,
+				mIndexedConstants, mScript);
 		index2constant.put(index, constant);
 		return constant;
 	}
-	
 
 	// TODO
 	private TermVariable transferToCurrentScriptIfNecessary(final TermVariable tv) {
@@ -171,12 +173,12 @@ public class SSABuilder {
 		}
 		return result;
 	}
-	
+
 	class VariableVersioneer {
 		private final HCTransFormula mTF;
 		private final Map<Term, Term> mSubstitutionMapping = new HashMap<>();
 		private final Term mformula;
-		private final IPredicate mPred;
+		private final HCPredicate mPred;
 
 		public VariableVersioneer(final HCTransFormula tf) {
 			mTF = tf;
@@ -184,12 +186,12 @@ public class SSABuilder {
 			mPred = null;
 		}
 
-		public VariableVersioneer(final IPredicate p) {
+		public VariableVersioneer(final HCPredicate p) {
 			mTF = null;
 			mformula = transferToCurrentScriptIfNecessary(p.getFormula());
 			mPred = p;
 		}
-		
+
 		public void versionInVars() {
 			for (final HCVar bv : mTF.getInVars().keySet()) {
 				final TermVariable tv = transferToCurrentScriptIfNecessary(mTF.getInVars().get(bv));
@@ -207,7 +209,7 @@ public class SSABuilder {
 				mSubstitutionMapping.put(tv, versioneered);
 			}
 		}
-		
+
 		private Term constructFreshConstant(final TermVariable tv) {
 			final Integer newIndex = mConstForTvCounter.increase(tv);
 			final String name = SmtUtils.removeSmtQuoteCharacters(tv.getName()) + "_fresh_" + newIndex;
@@ -218,26 +220,27 @@ public class SSABuilder {
 
 		public void versionPredicate() {
 			/*
-			for (final IProgramVar bv : mPred.getVars()) {
-				final TermVariable tv = transferToCurrentScriptIfNecessary(bv.getTermVariable());
-				final Term versioneered = getCurrentVarVersion(bv);
-				mConstants2BoogieVar.put(versioneered, bv);
-				mSubstitutionMapping.put(tv, versioneered);
-			}
+			 * for (final IProgramVar bv : mPred.getVars()) { final TermVariable
+			 * tv = transferToCurrentScriptIfNecessary(bv.getTermVariable());
+			 * final Term versioneered = getCurrentVarVersion(bv);
+			 * mConstants2BoogieVar.put(versioneered, bv);
+			 * mSubstitutionMapping.put(tv, versioneered); }
 			 */
 		}
-		
+
 		public Term getVersioneeredTerm() {
 			if (mformula == null) {
-				// TODO(mostafa): A hack for precondition and post condition, should be removed.
+				// TODO(mostafa): A hack for precondition and post condition,
+				// should be removed.
 				return null;
 			}
 			final Substitution subst = new Substitution(mScript, mSubstitutionMapping);
 			final Term result = subst.transform(mformula);
-			assert result.getFreeVars().length == 0 : "free vars in versioneered term: " + String.valueOf(result.getFreeVars());
+			assert result.getFreeVars().length == 0 : "free vars in versioneered term: "
+					+ String.valueOf(result.getFreeVars());
 			return result;
 		}
-		
+
 		public Map<Term, Term> getSubstitutionMapping() {
 			return mSubstitutionMapping;
 		}
