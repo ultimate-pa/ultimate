d32e00e752f77c81b777a4787406330ddd66e76f
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-11-14 11:08:28 +0100

    Fix total calculation

--- releaseScripts/benchmark-processing/showIntermediate.sh
+++ releaseScripts/benchmark-processing/showIntermediate.sh
@@ -9,9 +9,9 @@ do
         for i in `grep -oh 'Settings:[^ ]*' ${FILE} | sort | uniq`
         do
                 SETTING=${i#Settings:}
-                SUCCESS=`ack SUCCESS "$FILE" | grep "$SETTING.*$TOOLCHAIN" | wc -l`
-                FAILURE=`ack FAIL "$FILE" | grep "$SETTING.*$TOOLCHAIN" | wc -l`
-                UNKNOWN=`ack UNKNOWN "$FILE" | grep "$SETTING.*$TOOLCHAIN" | wc -l`
+                SUCCESS=`ack "with SUCCESS" "$FILE" | grep "$SETTING.*$TOOLCHAIN" | wc -l`
+                FAILURE=`ack "with FAIL" "$FILE" | grep "$SETTING.*$TOOLCHAIN" | wc -l`
+                UNKNOWN=`ack "with UNKNOWN" "$FILE" | grep "$SETTING.*$TOOLCHAIN" | wc -l`
                 TOTAL=$((SUCCESS+FAILURE+UNKNOWN))
                 if [ $TOTAL -gt "0" ]; then
                         RTR="${RTR}S: $SUCCESS F: $FAILURE U: $UNKNOWN T: $TOTAL $SETTING\n"
