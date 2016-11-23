e99723cd0dbfd2a07e33d3156420072400e7243b
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-11-23 16:07:48 +0100

    cc fixes & new toolchain format for webinterface

--- trunk/source/WebUltimateBridge/src/de/uni_freiburg/informatik/ultimate/website/WebToolchain.java
+++ trunk/source/WebUltimateBridge/src/de/uni_freiburg/informatik/ultimate/website/WebToolchain.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -25,13 +26,20 @@ import de.uni_freiburg.informatik.ultimate.website.Setting.SettingType;
  */
 public abstract class WebToolchain {
 
+	private static final int MAX_LENGTH_NAME = 30;
+	private static final int MAX_LENGTH_ID = 30;
+	private static final int TIMEOUT_MS = 5 * 60 * 1000;
+	private static final int MAX_LENGTH_DESCRIPTION = 250;
+	private static final String EOL = System.getProperty("line.separator");
+	private static final Set<String> IDS = new HashSet<>();
+
 	/**
 	 * @author Markus Lindenmann
 	 * @author Oleksii Saukh
 	 * @author Stefan Wissert
 	 * @date 07.03.2012
 	 */
-	public static enum LoggingLevel {
+	public enum LoggingLevel {
 		/**
 		 * Debug logging level - all messages will be printed.
 		 */
@@ -49,10 +57,6 @@ public abstract class WebToolchain {
 		 */
 		ERROR
 	}
-
-	private static final String sEOL = System.getProperty("line.separator");
-
-	private static final Set<String> sIds = new HashSet<>();
 
 	private String mName;
 	private String mId;
@@ -89,7 +93,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the name of this toolchain.
-	 * 
+	 *
 	 * @return the name
 	 */
 	public final String getName() {
@@ -98,7 +102,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the task name.
-	 * 
+	 *
 	 * @return the names of the tasks, where this toolchain can be applied.
 	 */
 	public final Tasks.TaskNames[] getTaskName() {
@@ -107,7 +111,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the toolchain ID.
-	 * 
+	 *
 	 * @return the unique id of this toolchain.
 	 */
 	public final String getId() {
@@ -116,7 +120,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the toolchains tools.
-	 * 
+	 *
 	 * @return an ordered list of tools that this toolchain executes.
 	 */
 	public final List<Tool> getTools() {
@@ -126,7 +130,7 @@ public abstract class WebToolchain {
 	/**
 	 * Getter for the short description String.<br />
 	 * <i>Might return the empty String, as this field is optional</i>
-	 * 
+	 *
 	 * @return a String describing this toolchain
 	 */
 	public final String getDescription() {
@@ -135,47 +139,49 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the toolchain XML String, listing the tools to call.
-	 * 
+	 *
 	 * @return the toolchain XML String
 	 */
 	public final String getToolchainXML() {
-		final StringBuffer toolchainXML = new StringBuffer("<toolchain>");
-		toolchainXML.append(sEOL);
+		final StringBuilder toolchainXml = new StringBuilder();
+		toolchainXml.append("<rundefinition>").append(EOL);
+		toolchainXml.append("<name>").append(getClass().getSimpleName()).append("</name>").append(EOL);
+		toolchainXml.append("<toolchain>").append(EOL);
 		for (final Tool t : mTools) {
-			toolchainXML.append("\t<plugin id=\"");
-			toolchainXML.append(t.getId());
-			toolchainXML.append("\"/>").append(sEOL);
+			toolchainXml.append("\t<plugin id=\"");
+			toolchainXml.append(t.getId());
+			toolchainXml.append("\"/>").append(EOL);
 		}
-		toolchainXML.append("</toolchain>");
-		return toolchainXML.toString();
+		toolchainXml.append("</toolchain></rundefinition>");
+		return toolchainXml.toString();
 	}
 
 	/**
 	 * Getter for the setting file.<br />
-	 * This messages collects information about this toolchain and returns them
-	 * in the format of an Ultimate settings file as a <b>String</b>.
-	 * 
+	 * This messages collects information about this toolchain and returns them in the format of an Ultimate settings
+	 * file as a <b>String</b>.
+	 *
 	 * @return the content of a created settings file for this toolchain.
 	 */
 	public final String getSettingFileContent() {
 		final DateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
-		final StringBuffer settings = new StringBuffer("#");
-		settings.append(dateFormat.format(new Date())).append(sEOL);
-		settings.append("# Settings file for ").append(getId()).append(", ").append(getName()).append(sEOL);
-		settings.append(sEOL).append("#").append(dateFormat.format(new Date()));
-		settings.append(sEOL).append("file_export_version=3.0").append(sEOL);
+		final StringBuilder settings = new StringBuilder("#");
+		settings.append(dateFormat.format(new Date())).append(EOL);
+		settings.append("# Settings file for ").append(getId()).append(", ").append(getName()).append(EOL);
+		settings.append(EOL).append("#").append(dateFormat.format(new Date()));
+		settings.append(EOL).append("file_export_version=3.0").append(EOL);
 		for (final Setting set : mSettings) {
 			if (set.isUserModifiable()) {
-				settings.append("# User-modifiable").append(sEOL);
+				settings.append("# User-modifiable").append(EOL);
 			}
-			if (set.getSettingString().equals("")) {
+			if ("".equals(set.getSettingString())) {
 				settings.append("\\!");
 			}
 			settings.append("/instance/");
 			settings.append(set.getSettingString());
 			settings.append("=");
 			settings.append(set.getSetValues());
-			settings.append(sEOL);
+			settings.append(EOL);
 		}
 		return settings.toString();
 	}
@@ -183,50 +189,50 @@ public abstract class WebToolchain {
 	/**
 	 * Setter for the toolchains description string.<br />
 	 * <i>optional field: leave empty if not required.</i>
-	 * 
+	 *
 	 * @return the description
 	 */
 	protected abstract String defineDescription();
 
 	/**
 	 * Setter for the toolchains name.
-	 * 
+	 *
 	 * @return the name
 	 */
 	protected abstract String defineName();
 
 	/**
-	 * Setter for the toolchains id. ID must be unique and must not contain any
-	 * spaces or symbols not contained in <code>(a-Z0-9)*</code>
-	 * 
+	 * Setter for the toolchains id. ID must be unique and must not contain any spaces or symbols not contained in
+	 * <code>(a-Z0-9)*</code>
+	 *
 	 * @return the id
 	 */
 	protected abstract String defineId();
 
 	/**
 	 * Setter for the task name.
-	 * 
+	 *
 	 * @return the task names
 	 */
 	protected abstract Tasks.TaskNames[] defineTaskName();
 
 	/**
 	 * Setter for the tools.
-	 * 
+	 *
 	 * @return the tools
 	 */
 	protected abstract List<Tool> defineTools();
 
 	/**
 	 * Setter for the language used on the website.
-	 * 
+	 *
 	 * @return the language string
 	 */
 	protected abstract String defineLanguage();
 
 	/**
 	 * Setter for layout font size for the interface on the website.
-	 * 
+	 *
 	 * @return the fontsize string
 	 */
 	protected String defineInterfaceLayoutFontsize() {
@@ -235,7 +241,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for layout orientation for the interface on the website.
-	 * 
+	 *
 	 * @return the orientation string
 	 */
 	protected String defineInterfaceLayoutOrientation() {
@@ -244,7 +250,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for layout transitions preset for the interface on the website.
-	 * 
+	 *
 	 * @return the transitions string
 	 */
 	protected String defineInterfaceLayoutTransitions() {
@@ -253,7 +259,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for the user info for this specific toolchain used on the website.
-	 * 
+	 *
 	 * @return the user info string
 	 */
 	protected String defineUserInfo() {
@@ -261,39 +267,37 @@ public abstract class WebToolchain {
 	}
 
 	/**
-	 * @return the path to a settings file relative to resources.examples/ that
-	 *         should be loaded as default settings for this tool, i.e.
-	 *         "LTLAutomizerC.epf", or null if no file should be loaded
+	 * @return the path to a settings file relative to resources.examples/ that should be loaded as default settings for
+	 *         this tool, i.e. "LTLAutomizerC.epf", or null if no file should be loaded
 	 */
 	protected String defineToolchainSettingsFile() {
 		return null;
 	}
 
 	/**
-	 * @return a list of manually defined {@link Setting}s for this tool. They
-	 *         will override settings loaded by
-	 *         {@link #defineToolchainSettingsFile()}. If the property
-	 *         {@link Setting#isUserModifiable()} is true, the setting will be
-	 *         modifiable by the user from the settings menu of the website.
-	 * 
+	 * @return a list of manually defined {@link Setting}s for this tool. They will override settings loaded by
+	 *         {@link #defineToolchainSettingsFile()}. If the property {@link Setting#isUserModifiable()} is true, the
+	 *         setting will be modifiable by the user from the settings menu of the website.
+	 *
 	 *         Per default, this method returns null.
 	 */
 	protected List<Setting> defineAdditionalSettings() {
-		return null;
+		return Collections.emptyList();
 	}
 
 	/**
 	 * Setter for the toolchains description string.<br />
 	 * <i>optional field: leave empty if not required.</i>
-	 * 
+	 *
 	 * @param description
 	 *            the description to set
 	 */
-	protected final void setDescription(String description) {
+	protected final void setDescription(final String description) {
 		if (description == null) {
-			description = "";
+			mDescription = "";
+			return;
 		}
-		if (description.length() > 250) {
+		if (description.length() > MAX_LENGTH_DESCRIPTION) {
 			throw new IllegalArgumentException("String to long!");
 		}
 		mDescription = description;
@@ -301,47 +305,47 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for the toolchains name.
-	 * 
+	 *
 	 * @param name
 	 *            the name to set
 	 */
 	protected final void setName(final String name) {
-		if (name == null || name.equals("")) {
+		if (name == null || "".equals(name)) {
 			throw new IllegalArgumentException("Name cannot be empty!");
 		}
-		if (name.length() > 30) {
-			throw new IllegalArgumentException("Name cannot be longer than 30 characters!");
+		if (name.length() > MAX_LENGTH_NAME) {
+			throw new IllegalArgumentException("Name cannot be longer than " + MAX_LENGTH_NAME + " characters!");
 		}
 		mName = name;
 	}
 
 	/**
-	 * Setter for the toolchains id. ID must be unique and must not contain any
-	 * spaces or symbols not contained in <code>(a-Z0-9)*</code>
-	 * 
+	 * Setter for the toolchains id. ID must be unique and must not contain any spaces or symbols not contained in
+	 * <code>(a-Z0-9)*</code>
+	 *
 	 * @param id
 	 *            the id to set
 	 */
 	protected final void setId(final String id) {
-		if (id == null || id.equals("")) {
+		if (id == null || "".equals(id)) {
 			throw new IllegalArgumentException("ID cannot be empty!");
 		}
-		if (id.length() > 30) {
-			throw new IllegalArgumentException("ID cannot be longer than 30 characters!");
+		if (id.length() > MAX_LENGTH_ID) {
+			throw new IllegalArgumentException("ID cannot be longer than " + MAX_LENGTH_ID + " characters!");
 		}
 		if (!id.matches("[a-z][a-zA-Z0-9]*")) {
 			throw new IllegalArgumentException("ID must be element of (a-z)(a-Z0-9)*");
 		}
-		if (sIds.contains(id)) {
+		if (IDS.contains(id)) {
 			throw new IllegalArgumentException("ID must be unique!");
 		}
-		sIds.add(id);
+		IDS.add(id);
 		mId = id;
 	}
 
 	/**
 	 * Setter for the task name.
-	 * 
+	 *
 	 * @param taskName
 	 *            the taskname to set
 	 */
@@ -351,7 +355,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for the language.
-	 * 
+	 *
 	 * @param language
 	 *            the language string to set
 	 */
@@ -361,7 +365,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for the interface font size on the website.
-	 * 
+	 *
 	 * @param mLanguage
 	 *            the fontsize string to set
 	 */
@@ -371,7 +375,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for the interface orientation on the website.
-	 * 
+	 *
 	 * @param mLanguage
 	 *            the orientation string to set
 	 */
@@ -381,7 +385,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for the interface transitions on the website.
-	 * 
+	 *
 	 * @param mLanguage
 	 *            the transitions preset string to set
 	 */
@@ -391,7 +395,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for the user information shown on the website
-	 * 
+	 *
 	 * @return
 	 */
 	protected final void setUserInfo(final String userInfo) {
@@ -400,7 +404,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Setter for the tools list.
-	 * 
+	 *
 	 * @param tools
 	 *            the tools
 	 */
@@ -436,7 +440,7 @@ public abstract class WebToolchain {
 
 		final List<Setting> rtr = new ArrayList<>();
 
-		String line = "";
+		String line;
 		while (true) {
 			line = buff.readLine();
 			if (line == null) {
@@ -459,7 +463,7 @@ public abstract class WebToolchain {
 
 				final String[] valueName = splitAtEqualsSign(line).toArray(new String[0]);
 				if (valueName.length != 2) {
-					System.out.println("Ignoring line " + line);
+					SimpleLogger.log("Ignoring line " + line);
 					continue;
 				}
 				final String name = valueName[0].replaceFirst("/instance/", "");
@@ -473,7 +477,7 @@ public abstract class WebToolchain {
 				}
 				continue;
 			}
-			System.out.println("Ignoring line " + line);
+			SimpleLogger.log("Ignoring line " + line);
 		}
 		buff.close();
 		return rtr;
@@ -494,7 +498,7 @@ public abstract class WebToolchain {
 	}
 
 	private void setAdditionalSettings(final List<Setting> additionalSettings) {
-		if (additionalSettings == null) {
+		if (additionalSettings == null || additionalSettings.isEmpty()) {
 			return;
 		}
 		// remove all settings that are already present
@@ -511,7 +515,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the user information shown on the website
-	 * 
+	 *
 	 * @return the user information
 	 */
 	public String getUserInfo() {
@@ -520,7 +524,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the language used on the website
-	 * 
+	 *
 	 * @return the toolchains language
 	 */
 	public String getLanguage() {
@@ -529,7 +533,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the tools HTML layout fontsize.
-	 * 
+	 *
 	 * @return the fontsize.
 	 */
 	public String getInterfaceLayoutFontsize() {
@@ -538,7 +542,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the tools HTML layout orientation.
-	 * 
+	 *
 	 * @return the orientation.
 	 */
 	public String getInterfaceLayoutOrientation() {
@@ -547,7 +551,7 @@ public abstract class WebToolchain {
 
 	/**
 	 * Getter for the tools HTML layout transitions preset.
-	 * 
+	 *
 	 * @return the transitions preset.
 	 */
 	public String getInterfaceLayoutTransitions() {
@@ -555,12 +559,11 @@ public abstract class WebToolchain {
 	}
 
 	/**
-	 * 
-	 * @return A default timeout in milliseconds for this toolchain. 0 means no
-	 *         timeout.
+	 *
+	 * @return A default timeout in milliseconds for this toolchain. 0 means no timeout.
 	 */
 	public long getTimeout() {
-		return 5 * 60 * 1000;
+		return TIMEOUT_MS;
 	}
 
 	public List<Setting> getUserModifiableSettings() {
