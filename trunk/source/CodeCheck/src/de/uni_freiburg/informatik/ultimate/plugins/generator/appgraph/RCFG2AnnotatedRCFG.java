80f6db5d36c251eb53fec4ee732d3448e4d8d819
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-11-28 18:11:50 +0100

    cc fixes

--- trunk/source/CodeCheck/src/de/uni_freiburg/informatik/ultimate/plugins/generator/appgraph/RCFG2AnnotatedRCFG.java
+++ trunk/source/CodeCheck/src/de/uni_freiburg/informatik/ultimate/plugins/generator/appgraph/RCFG2AnnotatedRCFG.java
@@ -2,27 +2,27 @@
  * Copyright (C) 2014-2015 Alexander Nutz (nutz@informatik.uni-freiburg.de)
  * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE CodeCheck plug-in.
- * 
+ *
  * The ULTIMATE CodeCheck plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE CodeCheck plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE CodeCheck plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE CodeCheck plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE CodeCheck plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE CodeCheck plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.plugins.generator.appgraph;
@@ -47,22 +47,22 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.pr
 
 public class RCFG2AnnotatedRCFG {
 
-
-	HashMap<BoogieIcfgLocation, AnnotatedProgramPoint> moldPpTonew;
+	HashMap<BoogieIcfgLocation, AnnotatedProgramPoint> mOldPpTonew;
 	private final ILogger mLogger;
-	private final CfgSmtToolkit mcsToolkit;
+	private final CfgSmtToolkit mCfgSmtToolkit;
 	private final PredicateFactory mPredicateFactory;
-	private final IPredicate mtruePredicate;
-	private final Map<IcfgLocation, Term> minitialPredicates;
-	private final boolean museInitialPredicates;
+	private final IPredicate mTruePredicate;
+	private final Map<IcfgLocation, Term> mInitialPredicates;
+	private final boolean mUseInitialPredicates;
 
-	public RCFG2AnnotatedRCFG(final CfgSmtToolkit smtMan, final PredicateFactory predicateFactory, final ILogger logger, final IPredicate truePredicate, final Map<IcfgLocation, Term> initialPredicates) {
+	public RCFG2AnnotatedRCFG(final CfgSmtToolkit smtMan, final PredicateFactory predicateFactory, final ILogger logger,
+			final IPredicate truePredicate, final Map<IcfgLocation, Term> initialPredicates) {
 		mLogger = logger;
-		mcsToolkit = smtMan;
+		mCfgSmtToolkit = smtMan;
 		mPredicateFactory = predicateFactory;
-		mtruePredicate = truePredicate;
-		minitialPredicates = initialPredicates;
-		museInitialPredicates = initialPredicates != null;
+		mTruePredicate = truePredicate;
+		mInitialPredicates = initialPredicates;
+		mUseInitialPredicates = initialPredicates != null;
 	}
 
 	public ImpRootNode convert(final IUltimateServiceProvider mServices, final BoogieIcfgContainer oldRoot) {
@@ -71,8 +71,8 @@ public class RCFG2AnnotatedRCFG {
 
 		final ImpRootNode newRoot = new ImpRootNode(ra);
 
-		final ArrayDeque<BoogieIcfgLocation> openNodes = new ArrayDeque<BoogieIcfgLocation>();
-		moldPpTonew = new HashMap<BoogieIcfgLocation, AnnotatedProgramPoint>();
+		final ArrayDeque<BoogieIcfgLocation> openNodes = new ArrayDeque<>();
+		mOldPpTonew = new HashMap<>();
 
 		for (final Entry<String, BoogieIcfgLocation> entry : oldRoot.getProcedureEntryNodes().entrySet()) {
 			final BoogieIcfgLocation oldNode = entry.getValue();
@@ -80,7 +80,7 @@ public class RCFG2AnnotatedRCFG {
 
 			newRoot.connectOutgoing(new DummyCodeBlock(mLogger), newNode);
 			openNodes.add(oldNode);
-			moldPpTonew.put(oldNode, newNode);
+			mOldPpTonew.put(oldNode, newNode);
 		}
 
 		/*
@@ -91,17 +91,17 @@ public class RCFG2AnnotatedRCFG {
 
 			for (final IcfgEdge outEdge : currentNode.getOutgoingEdges()) {
 				final BoogieIcfgLocation newNode = (BoogieIcfgLocation) outEdge.getTarget();
-				if (moldPpTonew.containsKey(newNode)) {
+				if (mOldPpTonew.containsKey(newNode)) {
 					continue;
 				}
-				moldPpTonew.put(newNode, createAnnotatedProgramPoint(newNode));
+				mOldPpTonew.put(newNode, createAnnotatedProgramPoint(newNode));
 				openNodes.add(newNode);
 				if (outEdge instanceof Return) {
 					final BoogieIcfgLocation hier = ((Return) outEdge).getCallerProgramPoint();
-					if (moldPpTonew.containsKey(hier)) {
+					if (mOldPpTonew.containsKey(hier)) {
 						continue;
 					}
-					moldPpTonew.put(hier, createAnnotatedProgramPoint(hier));
+					mOldPpTonew.put(hier, createAnnotatedProgramPoint(hier));
 					openNodes.add(hier);
 				}
 			}
@@ -110,12 +110,12 @@ public class RCFG2AnnotatedRCFG {
 		/*
 		 * put edges into annotated program points
 		 */
-		for (final Entry<BoogieIcfgLocation, AnnotatedProgramPoint> entry : moldPpTonew.entrySet()) {
+		for (final Entry<BoogieIcfgLocation, AnnotatedProgramPoint> entry : mOldPpTonew.entrySet()) {
 			for (final IcfgEdge outEdge : entry.getKey().getOutgoingEdges()) {
-				final AnnotatedProgramPoint annotatedTarget = moldPpTonew.get(outEdge.getTarget());
+				final AnnotatedProgramPoint annotatedTarget = mOldPpTonew.get(outEdge.getTarget());
 
 				if (outEdge instanceof Return) {
-					final AnnotatedProgramPoint callPred = moldPpTonew.get(((Return) outEdge).getCallerProgramPoint());
+					final AnnotatedProgramPoint callPred = mOldPpTonew.get(((Return) outEdge).getCallerProgramPoint());
 					entry.getValue().connectOutgoingReturn(callPred, (Return) outEdge, annotatedTarget);
 				} else {
 					entry.getValue().connectOutgoing((CodeBlock) outEdge, annotatedTarget);
@@ -126,27 +126,25 @@ public class RCFG2AnnotatedRCFG {
 	}
 
 	/**
-	 * Creates an AnnotatedProgramPoint from a ProgramPoint.
-	 * The annotation is an IPredicate.
-	 * If we have a Term from AbstractInterpretation for that ProgramPoint, we annotate it
-	 * with the corresponding Predicate. Otherwise the annotation is "true".
+	 * Creates an AnnotatedProgramPoint from a ProgramPoint. The annotation is an IPredicate. If we have a Term from
+	 * AbstractInterpretation for that ProgramPoint, we annotate it with the corresponding Predicate. Otherwise the
+	 * annotation is "true".
 	 */
 	private AnnotatedProgramPoint createAnnotatedProgramPoint(final BoogieIcfgLocation pp) {
-		if (museInitialPredicates) {
-			final Term aiTerm = minitialPredicates.get(pp);
+		if (mUseInitialPredicates) {
+			final Term aiTerm = mInitialPredicates.get(pp);
 			IPredicate aiPredicate;
 			if (aiTerm != null) {
 				aiPredicate = mPredicateFactory.newPredicate(aiTerm);
 			} else {
-				aiPredicate = mtruePredicate;
+				aiPredicate = mTruePredicate;
 			}
 			return new AnnotatedProgramPoint(aiPredicate, pp);
-		} else {
-			return  new AnnotatedProgramPoint(mtruePredicate, pp);
 		}
+		return new AnnotatedProgramPoint(mTruePredicate, pp);
 	}
 
 	public HashMap<BoogieIcfgLocation, AnnotatedProgramPoint> getOldPpTonew() {
-		return moldPpTonew;
+		return mOldPpTonew;
 	}
 }
--- trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/cfg/InterproceduralSequentialComposition.java
+++ trunk/source/RCFGBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/rcfgbuilder/cfg/InterproceduralSequentialComposition.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2013-2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE RCFGBuilder plug-in.
- * 
+ *
  * The ULTIMATE RCFGBuilder plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE RCFGBuilder plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE RCFGBuilder plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE RCFGBuilder plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE RCFGBuilder plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE RCFGBuilder plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg;
@@ -42,23 +42,19 @@ public class InterproceduralSequentialComposition extends SequentialComposition 
 	private static final long serialVersionUID = -1637790156358220366L;
 
 	InterproceduralSequentialComposition(final int serialNumber, final BoogieIcfgLocation source,
-			final BoogieIcfgLocation target, final ManagedScript mgdScript, 
-			final CfgSmtToolkit modGlobVarManager, 
-			final boolean simplify, final boolean extPqe, final List<CodeBlock> codeBlocks, 
-			final ILogger logger, final IUltimateServiceProvider services,
-			final XnfConversionTechnique xnfConversionTechnique, final SimplificationTechnique simplificationTechnique, 
-			final ICfgSymbolTable symbolTable) {
-		super(serialNumber, source, target, modGlobVarManager, simplify, extPqe, services, codeBlocks, xnfConversionTechnique, 
-				simplificationTechnique);
+			final BoogieIcfgLocation target, final ManagedScript mgdScript, final CfgSmtToolkit modGlobVarManager,
+			final boolean simplify, final boolean extPqe, final List<CodeBlock> codeBlocks, final ILogger logger,
+			final IUltimateServiceProvider services, final XnfConversionTechnique xnfConversionTechnique,
+			final SimplificationTechnique simplificationTechnique, final ICfgSymbolTable symbolTable) {
+		super(serialNumber, source, target, modGlobVarManager, simplify, extPqe, services, codeBlocks,
+				xnfConversionTechnique, simplificationTechnique);
 	}
 
 	@Override
-	protected void checkNumberOfCallsAndReturns(final int numberCalls, final int  numberReturns) {
-		if(numberCalls <= numberReturns) {
+	protected void checkNumberOfCallsAndReturns(final int numberCalls, final int numberReturns) {
+		if (numberCalls <= numberReturns) {
 			throw new IllegalArgumentException("more calls and returns required");
 		}
 	}
-	
-	
-	
+
 }
