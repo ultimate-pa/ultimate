2ffaea4a5469341a3a94dea0c2b7f64cecdd088a
schillic <schillic@informatik.uni-freiburg.de>
2016-11-29 16:25:55 +0100

    software engineering's finest: hack to support TraceCheckerSpWp

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/tracehandling/TraceAbstractionRefinementEngine.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/tracehandling/TraceAbstractionRefinementEngine.java
@@ -54,8 +54,10 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.Ba
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.CachingHoareTripleChecker;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.PredicateFactory;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.preferences.TAPreferences;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.IInterpolantGenerator;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.InterpolantConsolidation;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.PredicateUnifier;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.TraceCheckerSpWp;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.TraceCheckerUtils.InterpolantsPreconditionPostcondition;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.tracehandling.IRefinementStrategy.RefinementStrategyAdvance;
 
@@ -179,21 +181,29 @@ public final class TraceAbstractionRefinementEngine
 	 * This method is the heart of the refinement engine.<br>
 	 * It first checks feasibility of the counterexample. If infeasible, the method tries to find a perfect interpolant
 	 * sequence. If unsuccessful, it collects all tested sequences. In the end an interpolant automaton is created.
+	 * <p>
+	 * There is a huge hack for supporting the special {@link TraceCheckerSpWp} architecture where
+	 * <ol>
+	 * <li>we need a different getter for the interpolant sequence and</li>
+	 * <li>there are two sequences of interpolants</li>
+	 * </ol>
 	 * 
 	 * @param strategy
 	 *            refinement strategy
 	 * @return counterexample feasibility
 	 */
+	@SuppressWarnings("squid:LabelsShouldNotBeUsedCheck")
 	private LBool executeStrategy(final IRefinementStrategy strategy) {
 		List<InterpolantsPreconditionPostcondition> interpolantSequences = new LinkedList<>();
-		do {
+		boolean perfectInterpolantsFound = false;
+		outer: do {
 			// check feasibility using the strategy
 			final LBool feasibility = strategy.getTraceChecker().isCorrect();
 			
 			if (feasibility == LBool.UNKNOWN && strategy.hasNext(RefinementStrategyAdvance.TRACE_CHECKER)) {
 				// feasibility check failed, try next combination in the strategy
 				strategy.next(RefinementStrategyAdvance.TRACE_CHECKER);
-				continue;
+				continue outer;
 			}
 			
 			switch (feasibility) {
@@ -205,20 +215,35 @@ public final class TraceAbstractionRefinementEngine
 					mRcfgProgramExecution = strategy.getTraceChecker().getRcfgProgramExecution();
 					break;
 				case UNSAT:
-					final InterpolantsPreconditionPostcondition interpolants =
-							strategy.getInterpolantGenerator().getIpp();
-					
-					if (interpolants != null && strategy.getInterpolantGenerator().isPerfectSequence()) {
-						// construct interpolant automaton using only this (perfect) sequence
-						interpolantSequences = Collections.singletonList(interpolants);
-						if (mLogger.isInfoEnabled()) {
-							mLogger.info("Found a perfect sequence of interpolants.");
-						}
+					final IInterpolantGenerator interpolantGenerator = strategy.getInterpolantGenerator();
+					InterpolantsPreconditionPostcondition interpolants;
+					int iterate;
+					if (interpolantGenerator instanceof TraceCheckerSpWp) {
+						interpolants = ((TraceCheckerSpWp) interpolantGenerator).getForwardIpp();
+						iterate = 2;
 					} else {
-						if (interpolants != null) {
-							interpolantSequences.add(interpolants);
+						interpolants = interpolantGenerator.getIpp();
+						iterate = 1;
+					}
+					
+					for (int i = 1; i < iterate; ++i) {
+						if (i == 2) {
+							interpolants = ((TraceCheckerSpWp) interpolantGenerator).getBackwardIpp();
 						}
-						
+						if (interpolants == null) {
+							continue;
+						} else if (strategy.getInterpolantGenerator().isPerfectSequence()) {
+							perfectInterpolantsFound = true;
+							// construct interpolant automaton using only this (perfect) sequence
+							interpolantSequences = Collections.singletonList(interpolants);
+							if (mLogger.isInfoEnabled()) {
+								mLogger.info("Found a perfect sequence of interpolants.");
+							}
+							break;
+						}
+						interpolantSequences.add(interpolants);
+					}
+					if (!perfectInterpolantsFound) {
 						if (strategy.hasNext(RefinementStrategyAdvance.INTERPOLANT_GENERATOR)) {
 							// construct the next sequence of interpolants
 							if (mLogger.isInfoEnabled()) {
@@ -226,7 +251,7 @@ public final class TraceAbstractionRefinementEngine
 										"The current sequence of interpolants is not perfect, trying the next one.");
 							}
 							strategy.next(RefinementStrategyAdvance.INTERPOLANT_GENERATOR);
-							continue;
+							continue outer;
 						} else if (mLogger.isInfoEnabled()) {
 							mLogger.info("No perfect sequence of interpolants found, combining those we have.");
 						}
