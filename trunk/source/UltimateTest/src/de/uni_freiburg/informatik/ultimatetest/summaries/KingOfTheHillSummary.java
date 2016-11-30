5ab56008d097eb9996aeb1adc79f827321820e82
schillic <schillic@informatik.uni-freiburg.de>
2016-11-30 13:03:48 +0100

    typo, cc fix

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/summaries/KingOfTheHillSummary.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/summaries/KingOfTheHillSummary.java
@@ -20,9 +20,9 @@
  * 
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Test Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Test Library grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Test Library grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimatetest.summaries;
@@ -35,78 +35,79 @@ import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.test.UltimateRunDefinition;
 import de.uni_freiburg.informatik.ultimate.test.UltimateTestSuite;
-import de.uni_freiburg.informatik.ultimate.test.reporting.ExtendedResult;
 import de.uni_freiburg.informatik.ultimate.test.reporting.BaseTestSummary;
+import de.uni_freiburg.informatik.ultimate.test.reporting.ExtendedResult;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
 
 /**
- * List which toolchain/setting pair combinations are the only ones that 
- * produced a certain result.
+ * List which toolchain/setting pair combinations are the only ones that produced a certain result.
  * 
  * @author Matthias Heizmann
- *
  */
 public class KingOfTheHillSummary extends BaseTestSummary {
-
-	public KingOfTheHillSummary(
-			Class<? extends UltimateTestSuite> ultimateTestSuite) {
+	/**
+	 * @param ultimateTestSuite
+	 *            Ultimate test suite.
+	 */
+	public KingOfTheHillSummary(final Class<? extends UltimateTestSuite> ultimateTestSuite) {
 		super(ultimateTestSuite);
 	}
 	
 	@Override
 	public String getSummaryLog() {
 		final PartitionedResults pr = getAllResultsPartitioned();
-		final StringBuilder sb = new StringBuilder();
-
-		{
-			final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory = pr.Success;
-			final String resultCategory = "SUCCESS";
-			printKingOfTheHillForResultCategory(allOfResultCategory, resultCategory, sb);
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-		}
+		final StringBuilder builder = new StringBuilder();
 		
-		{
-			final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory = pr.Timeout;
-			final String resultCategory = "TIMEOUT";
-			printKingOfTheHillForResultCategory(allOfResultCategory, resultCategory, sb);
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-		}
-
-		{
-			final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory = pr.Unknown;
-			final String resultCategory = "UNKNOWN";
-			printKingOfTheHillForResultCategory(allOfResultCategory, resultCategory, sb);
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-		}
-
-		{
-			final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory = pr.Failure;
-			final String resultCategory = "FAILURE";
-			printKingOfTheHillForResultCategory(allOfResultCategory, resultCategory, sb);
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-			sb.append(System.lineSeparator());
-		}
-
-		return sb.toString();
+		printSuccess(pr, builder);
+		
+		printTimeout(pr, builder);
+		
+		printUnknown(pr, builder);
+		
+		printFailure(pr, builder);
+		
+		return builder.toString();
 	}
-
 	
+	private void printSuccess(final PartitionedResults partRes, final StringBuilder builder) {
+		final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory = partRes.Success;
+		final String resultCategory = "SUCCESS";
+		printCategory(builder, allOfResultCategory, resultCategory);
+	}
+	
+	private void printTimeout(final PartitionedResults partRes, final StringBuilder builder) {
+		final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory = partRes.Timeout;
+		final String resultCategory = "TIMEOUT";
+		printCategory(builder, allOfResultCategory, resultCategory);
+	}
+	
+	private void printUnknown(final PartitionedResults partRes, final StringBuilder builder) {
+		final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory = partRes.Unknown;
+		final String resultCategory = "UNKNOWN";
+		printCategory(builder, allOfResultCategory, resultCategory);
+	}
+	
+	private void printFailure(final PartitionedResults partRes, final StringBuilder builder) {
+		final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory = partRes.Failure;
+		final String resultCategory = "FAILURE";
+		printCategory(builder, allOfResultCategory, resultCategory);
+	}
+	
+	private void printCategory(final StringBuilder builder,
+			final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory,
+			final String resultCategory) {
+		printKingOfTheHillForResultCategory(allOfResultCategory, resultCategory, builder);
+		builder.append(System.lineSeparator());
+		builder.append(System.lineSeparator());
+		builder.append(System.lineSeparator());
+		builder.append(System.lineSeparator());
+	}
 	
 	private void printKingOfTheHillForResultCategory(
-			Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory,
-			String resultCategory, StringBuilder sb) {
-		sb.append("======= King of the Hill for ").append(resultCategory).append(" =======").append(System.lineSeparator());
+			final Collection<Entry<UltimateRunDefinition, ExtendedResult>> allOfResultCategory,
+			final String resultCategory, final StringBuilder builder) {
+		builder.append("======= King of the Hill for ").append(resultCategory).append(" =======")
+				.append(System.lineSeparator());
 		
 		final HashRelation<String, TCS> input2tcses = new HashRelation<>();
 		for (final Entry<UltimateRunDefinition, ExtendedResult> result : allOfResultCategory) {
@@ -119,32 +120,26 @@ public class KingOfTheHillSummary extends BaseTestSummary {
 			final Set<TCS> tcses = input2tcses.getImage(input);
 			tcses2input.addPair(tcses, input);
 		}
-		final Set<TCS>[] tcsPowerset = (Set<TCS>[]) tcses2input.getDomain().toArray(new Set<?>[tcses2input.getDomain().size()]);
-		final Comparator<Set<TCS>> comp = new Comparator<Set<TCS>>() {
-
-			@Override
-			public int compare(Set<TCS> arg0, Set<TCS> arg1) {
-				return arg0.size() - arg1.size();
-			}
-		};
+		final Comparator<Set<TCS>> comp = (arg0, arg1) -> arg0.size() - arg1.size();
+		final Set<TCS>[] tcsPowerset =
+				(Set<TCS>[]) tcses2input.getDomain().toArray(new Set<?>[tcses2input.getDomain().size()]);
 		
 		Arrays.sort(tcsPowerset, comp);
 		for (final Set<TCS> tcses : tcsPowerset) {
-			sb.append("The " + tcses.size() + " toolchain/settings pairs").append(System.lineSeparator());
+			builder.append("The " + tcses.size() + " toolchain/settings pairs").append(System.lineSeparator());
 			for (final TCS tcs : tcses) {
-				sb.append("    ").append(tcs).append(System.lineSeparator());
+				builder.append("    ").append(tcs).append(System.lineSeparator());
 			}
 			final Set<String> inputFiles = tcses2input.getImage(tcses);
-			sb.append("are exactly the pairs were the result was "); 
-			sb.append(resultCategory);
-			sb.append(" on the following ");
-			sb.append(inputFiles.size());
-			sb.append(" files").append(System.lineSeparator());
+			builder.append("are exactly the pairs where the result was ");
+			builder.append(resultCategory);
+			builder.append(" on the following ");
+			builder.append(inputFiles.size());
+			builder.append(" files").append(System.lineSeparator());
 			for (final String file : inputFiles) {
-				sb.append("  ").append(file).append(System.lineSeparator());
+				builder.append("  ").append(file).append(System.lineSeparator());
 			}
-			sb.append(System.lineSeparator());
+			builder.append(System.lineSeparator());
 		}
 	}
-
 }
