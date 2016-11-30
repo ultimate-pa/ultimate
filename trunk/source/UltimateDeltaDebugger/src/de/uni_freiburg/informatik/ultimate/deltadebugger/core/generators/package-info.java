5c87fbd5186a97d1ddbf468592b0670623cabee5
schillic <schillic@informatik.uni-freiburg.de>
2016-11-30 21:07:23 +0100

    added package-info.java files for delta debugger

---
+++ trunk/source/UltimateDeltaDebugger/src/de/uni_freiburg/informatik/ultimate/deltadebugger/core/generators/package-info.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2016 Christian Schilling (schillic@informatik.uni-freiburg.de)
+ * Copyright (C) 2016 University of Freiburg
+ *
+ * This file is part of the Ultimate Delta Debugger plug-in.
+ *
+ * The Ultimate Delta Debugger plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Ultimate Delta Debugger plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Ultimate Delta Debugger plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the Ultimate Delta Debugger plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the Ultimate Delta Debugger plug-in grant you additional permission
+ * to convey the resulting work.
+ */
+
+/**
+ * This package contains generators used by the delta debugger.
+ * 
+ * @author Christian Schilling (schillic@informatik.uni-freiburg.de)
+ */
+package de.uni_freiburg.informatik.ultimate.deltadebugger.core.generators;
