4a6c0f25fffe8c64661df8110813e101b8be9215
schillic <schillic@informatik.uni-freiburg.de>
2016-12-01 22:39:35 +0100

    added license headers

--- trunk/source/UltimateDeltaDebugger/src/de/uni_freiburg/informatik/ultimate/deltadebugger/core/parser/util/RewriteUtils.java
+++ trunk/source/UltimateDeltaDebugger/src/de/uni_freiburg/informatik/ultimate/deltadebugger/core/parser/util/RewriteUtils.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2016 University of Freiburg
+ *
+ * This file is part of the Ultimate Delta Debugger plug-in.
+ *
+ * The Ultimate Delta Debugger plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Ultimate Delta Debugger plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Ultimate Delta Debugger plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the Ultimate Delta Debugger plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the Ultimate Delta Debugger plug-in grant you additional permission
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.deltadebugger.core.parser.util;
 
 import java.util.ArrayList;
@@ -29,7 +54,7 @@ import de.uni_freiburg.informatik.ultimate.deltadebugger.core.parser.pst.interfa
  *
  */
 public final class RewriteUtils {
-	private static final int MAX_PRETTY_PRINTED_LENGTH = 10_000; 
+	private static final int MAX_PRETTY_PRINTED_LENGTH = 10_000;
 
 	private RewriteUtils() {
 	}
@@ -116,7 +141,7 @@ public final class RewriteUtils {
 	 */
 	public static List<String> removeEquivalentReplacements(final IPSTNode node, final List<String> replacementStrings) {
 		final List<String> validReplacements = new ArrayList<>();
-		for (String replacement : replacementStrings) {
+		for (final String replacement : replacementStrings) {
 			if (RewriteUtils.skipEquivalentReplacement(node, replacement)) {
 				break;
 			}
@@ -125,7 +150,7 @@ public final class RewriteUtils {
 		return validReplacements;
 	}
 
-	public static List<String> getMinimalExpressionReplacements(IASTExpression expression) {
+	public static List<String> getMinimalExpressionReplacements(final IASTExpression expression) {
 		if (expression instanceof IASTLiteralExpression) {
 			final IASTLiteralExpression literalExpression = (IASTLiteralExpression) expression;
 			if (literalExpression.getKind() == IASTLiteralExpression.lk_float_constant) {
