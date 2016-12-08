a2f2f32535b2b4875dc7d28e3ec83b6fd32c57b7
schillic <schillic@informatik.uni-freiburg.de>
2016-12-08 16:27:45 +0100

    typo

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/preferences/TraceAbstractionPreferenceInitializer.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/preferences/TraceAbstractionPreferenceInitializer.java
@@ -393,7 +393,7 @@ public class TraceAbstractionPreferenceInitializer extends UltimatePreferenceIni
 		 */
 		FIXED_PREFERENCES,
 		/**
-		 * Tapian strategy.
+		 * Taipan strategy.
 		 */
 		TAIPAN,
 		/**
