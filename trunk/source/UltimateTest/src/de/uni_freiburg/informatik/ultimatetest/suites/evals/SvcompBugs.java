bf7d5f0c2cb467aac68b11ddc9dc39e38926f319
Daniel Dietsch <daniel.dietsch@gmail.com>
2016-12-12 13:05:06 +0100

    add svcomp bugs test suite, remove unused suites

---
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimatetest/suites/evals/SvcompBugs.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ * Copyright (C) 2015 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Test Library.
+ *
+ * The ULTIMATE Test Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Test Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Test Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Test Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Test Library grant you additional permission
+ * to convey the resulting work.
+ */
+
+package de.uni_freiburg.informatik.ultimatetest.suites.evals;
+
+import java.util.Collection;
+
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.CegarLoopStatisticsDefinitions;
+import de.uni_freiburg.informatik.ultimate.test.DirectoryFileEndingsPair;
+import de.uni_freiburg.informatik.ultimate.test.UltimateTestCase;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Triple;
+import de.uni_freiburg.informatik.ultimatetest.suites.AbstractEvalTestSuite;
+import de.uni_freiburg.informatik.ultimatetest.summaries.ColumnDefinition;
+import de.uni_freiburg.informatik.ultimatetest.summaries.ColumnDefinition.Aggregate;
+import de.uni_freiburg.informatik.ultimatetest.summaries.ConversionContext;
+
+/**
+ * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ */
+public class SvcompBugs extends AbstractEvalTestSuite {
+
+	private static final String[] C = new String[] { ".i", ".c" };
+
+	// @formatter:off
+	@SuppressWarnings("unchecked")
+	private static final Triple<String, String[], String>[] TOOLCHAINS = new Triple[] {
+			//### C
+			new Triple<>("AutomizerC.xml", C, "svcomp2017/automizer/svcomp-Reach-32bit-Automizer_Bitvector.epf"),
+	};
+
+	private static final String[] INPUT = new String[] {
+			"examples/svcomp/ldv-linux-4.0-rc1-mav/linux-4.0-rc1---drivers--net--ethernet--atheros--atl1e--atl1e.ko_true-unreach-call.cil.c"
+
+	};
+	// @formatter:on
+
+	@Override
+	protected long getTimeout() {
+		return 900 * 1000;
+	}
+
+	@Override
+	public Collection<UltimateTestCase> createTestCases() {
+		final int timeout = (int) getTimeout();
+		for (final Triple<String, String[], String> triple : TOOLCHAINS) {
+			final DirectoryFileEndingsPair[] pairs = new DirectoryFileEndingsPair[INPUT.length];
+			for (int i = 0; i < INPUT.length; ++i) {
+				pairs[i] = new DirectoryFileEndingsPair(INPUT[i], triple.getSecond(), timeout);
+			}
+			addTestCase(triple.getFirst(), triple.getThird(), pairs);
+		}
+		return super.createTestCases();
+	}
+
+	@Override
+	protected ColumnDefinition[] getColumnDefinitions() {
+		return new ColumnDefinition[] {
+				new ColumnDefinition("Runtime (ns)", "Avg. runtime", ConversionContext.Divide(1000000000, 2, " s"),
+						Aggregate.Sum, Aggregate.Average),
+				new ColumnDefinition("Allocated memory end (bytes)", "Memory",
+						ConversionContext.Divide(1048576, 2, " MB"), Aggregate.Max, Aggregate.Average),
+				new ColumnDefinition(CegarLoopStatisticsDefinitions.OverallIterations.toString(), "Iter{-}ations",
+						ConversionContext.BestFitNumber(), Aggregate.Ignore, Aggregate.Average),
+				new ColumnDefinition(CegarLoopStatisticsDefinitions.OverallTime.toString(), "Trace Abstraction Time",
+						ConversionContext.Divide(1000000000, 2, " s"), Aggregate.Sum, Aggregate.Average), };
+	}
+}
