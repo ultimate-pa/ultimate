0dd05622eb0711951995f117afd1e22edfde7a27
Matthias <heizmann@informatik.uni-freiburg.de>
2016-12-16 17:19:45 +0100

    transformation for concurrent programs

---
+++ trunk/source/TraceAbstractionConcurrent/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstractionconcurrent/Icfg2PetriNet.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2016 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * Copyright (C) 2016 University of Freiburg
+ *
+ * This file is part of the ULTIMATE TraceAbstractionConcurrent plug-in.
+ *
+ * The ULTIMATE TraceAbstractionConcurrent plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE TraceAbstractionConcurrent plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE TraceAbstractionConcurrent plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE TraceAbstractionConcurrent plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE TraceAbstractionConcurrent plug-in grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstractionconcurrent;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.Place;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.julian.PetriNetJulian;
+import de.uni_freiburg.informatik.ultimate.automata.statefactory.IStateFactory;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.CfgSmtToolkit;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgEdge;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgEdgeIterator;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils.SimplificationTechnique;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils.XnfConversionTechnique;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.BoogieIcfgContainer;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.BoogieIcfgLocation;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.PredicateFactory;
+import de.uni_freiburg.informatik.ultimate.util.ConstructionCache;
+import de.uni_freiburg.informatik.ultimate.util.ConstructionCache.IValueConstruction;
+
+public abstract class Icfg2PetriNet {
+	
+	protected final ILogger mLogger;
+	protected final IUltimateServiceProvider mServices;
+	private final SimplificationTechnique mSimplificationTechnique;
+	private final XnfConversionTechnique mXnfConversionTechnique;
+	
+	private final BoogieIcfgContainer mIcfg;
+	private final CfgSmtToolkit mCsToolkit;
+	private final PredicateFactory mPredicateFactory;
+	private final IStateFactory<IPredicate> mContentFactory;
+
+	
+
+	
+	public Icfg2PetriNet(final BoogieIcfgContainer icfg, final IStateFactory<IPredicate> contentFactory,
+			final CfgSmtToolkit csToolkit, final PredicateFactory predicateFactory,
+			final IUltimateServiceProvider services, final SimplificationTechnique simplificationTechnique,
+			final XnfConversionTechnique xnfConversionTechnique) {
+		mServices = services;
+		mLogger = mServices.getLoggingService().getLogger(Activator.PLUGIN_ID);
+		mSimplificationTechnique = simplificationTechnique;
+		mXnfConversionTechnique = xnfConversionTechnique;
+		mIcfg = icfg;
+		mContentFactory = contentFactory;
+		mCsToolkit = csToolkit;
+		mPredicateFactory = predicateFactory;
+		
+		final Set<IcfgEdge> alphabet = new HashSet<>();
+		
+		final PetriNetJulian<IcfgEdge, IPredicate> net = new PetriNetJulian<>(
+				new AutomataLibraryServices(services), alphabet, contentFactory, false);
+		
+		final Set<BoogieIcfgLocation> init = icfg.getInitialNodes();
+		final Map<String, Set<BoogieIcfgLocation>> procErrorNodes = icfg.getProcedureErrorNodes();
+		
+		final IValueConstruction<BoogieIcfgLocation, Place<IcfgEdge, IPredicate>> valueConstruction = 
+				new IValueConstruction<BoogieIcfgLocation, Place<IcfgEdge, IPredicate>>() {
+
+			@Override
+			public Place<IcfgEdge, IPredicate> constructValue(final BoogieIcfgLocation key) {
+				final IPredicate pred = predicateFactory.newDontCarePredicate(key);
+				final boolean isInitial = init.contains(key);
+				final String proc = key.getProcedure();
+				final boolean isFinal = procErrorNodes.get(proc).contains(key);
+				final Place<IcfgEdge, IPredicate> place = net.addPlace(pred, isInitial, isFinal);
+				return place;
+			}
+			
+		};
+		final ConstructionCache<BoogieIcfgLocation, Place<IcfgEdge, IPredicate>> cc = new ConstructionCache<>(valueConstruction);
+
+		for (final BoogieIcfgLocation loc : init) {
+			cc.getOrConstruct(loc);
+		}
+		
+		final IcfgEdgeIterator it = new IcfgEdgeIterator(icfg);
+		while(it.hasNext()) {
+			final IcfgEdge edge = it.next();
+			final Place<IcfgEdge, IPredicate> pred = cc.getOrConstruct((BoogieIcfgLocation) edge.getSource());
+			final Place<IcfgEdge, IPredicate> succ = cc.getOrConstruct((BoogieIcfgLocation) edge.getTarget());
+			net.addTransition(edge, Collections.singleton(pred), Collections.singleton(succ));
+		}
+	}
+	
+}
