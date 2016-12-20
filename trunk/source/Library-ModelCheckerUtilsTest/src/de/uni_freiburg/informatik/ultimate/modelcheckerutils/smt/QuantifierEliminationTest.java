e751a1bae11a41b6e653e8c28b044e30d764aa46
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2016-12-20 12:33:26 +0100

    JUnit Test

---
+++ trunk/source/Library-ModelCheckerUtilsTest/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/QuantifierEliminationTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2016 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * Copyright (C) 2016 University of Freiburg
+ * 
+ * This file is part of the ULTIMATE ModelCheckerUtilsTest Library.
+ * 
+ * The ULTIMATE ModelCheckerUtilsTest Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The ULTIMATE ModelCheckerUtilsTest Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE ModelCheckerUtilsTest Library. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE ModelCheckerUtilsTest Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
+ * containing parts covered by the terms of the Eclipse Public License, the 
+ * licensors of the ULTIMATE ModelCheckerUtilsTest Library grant you additional permission 
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uni_freiburg.informatik.ultimate.core.coreplugin.services.ToolchainStorage;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.logic.Logics;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Sort;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
+import de.uni_freiburg.informatik.ultimate.smtsolver.external.Scriptor;
+import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateServiceProviderMock;
+
+
+/**
+ * 
+ * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ *
+ */
+public class QuantifierEliminationTest {
+	
+	private IUltimateServiceProvider mServices;
+	private Script mScript;
+	private ManagedScript mMgdScript;
+	private ILogger mLogger;
+	
+	@Before
+	public void setUp(){
+		mServices = new UltimateServiceProviderMock();
+		mLogger = mServices.getLoggingService().getLogger("lol");
+		try {
+			mScript = new Scriptor("z3 SMTLIB2_COMPLIANT=true -memory:2024 -smt2 -in", mLogger, mServices, new ToolchainStorage(), "z3");
+		} catch (final IOException e) {
+			throw new AssertionError(e);
+		}
+//		script = new SMTInterpol();
+		mMgdScript = new ManagedScript(mServices, mScript);
+
+		mScript.setLogic(Logics.ALL);
+	}
+	
+	@Test
+	public void varStilThereBug(){
+		
+		
+		final Sort sort_Bool = mScript.sort("Bool");
+		final Sort sort_Array = mScript.sort("Array");
+		final Sort sort_Int = mScript.sort("Int");
+
+		//Constants
+		final Term con_0 = mScript.numeral("0");
+		final Term con_1 = mScript.numeral("1");
+
+		//Vars
+		final TermVariable oldValid33 = mScript.variable("v_old(#valid)_33", sort_Array);
+		final TermVariable valid = mScript.variable("#valid", sort_Array);
+		final TermVariable varValid = mScript.variable("old(#valid)", sort_Array);
+		final TermVariable base = mScript.variable("v_entry_point_~array~7.base_21", sort_Int);
+
+		//Functions
+
+		//term
+		final Term term = mScript.term("or", mScript.term("=", valid, oldValid33), mScript.quantifier(0, new TermVariable[]{base, base}, mScript.term("and", mScript.term("not", mScript.term("=", mScript.term("store", mScript.term("store", varValid, base, con_1), base, con_0), oldValid33)), mScript.term("=", mScript.term("select", varValid, base), con_0), mScript.term("not", mScript.term("=", base, con_0)))));
+		
+		
+		
+		
+		
+		
+		
+		
+//		//Sorts
+//		final Sort sort_Bool = script.sort("Bool");
+//		final Sort sort_Int = script.sort("Int");
+//		final Sort sort_Array = script.sort("Array", sort_Int, sort_Int);
+//
+//		//Constants
+//		final Term con_0 = script.numeral("0");
+//		final Term con_1 = script.numeral("1");
+//
+//		//Vars
+//		final TermVariable oldValid33 = script.variable("v_old(#valid)_33", sort_Array);
+//		final TermVariable valid = script.variable("#valid", sort_Array);
+//		final TermVariable oldValid = script.variable("old(#valid)", sort_Array);
+//		final TermVariable var_v_entry_point_array7base_21 = script.variable("v_entry_point_~array~7.base_21", sort_Int);
+//
+//		//Functions
+//
+//		//term
+//		final Term term = script.term("or", script.quantifier(0, new TermVariable[]{var_v_entry_point_array7base_21, var_v_entry_point_array7base_21}, script.term("and", script.term("not", script.term("=", script.term("store", script.term("store", oldValid, var_v_entry_point_array7base_21, con_1), var_v_entry_point_array7base_21, con_0), oldValid33)), script.term("=", script.term("select", oldValid, var_v_entry_point_array7base_21), con_0), script.term("not", script.term("=", var_v_entry_point_array7base_21, con_0)))), script.term("=", valid, oldValid33));
+//		PartialQuantifierElimination.tryToEliminate(services, services.getLoggingService().getLogger("lol"), mgdScript, term, SimplificationTechnique.SIMPLIFY_DDA, XnfConversionTechnique.BOTTOM_UP_WITH_LOCAL_SIMPLIFICATION);
+	}
+	
+}
