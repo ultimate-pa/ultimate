ffe2ddbb4a46a0db807ce3ecdcce5fda76d3db9f
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2016-12-23 17:54:52 +0100

    forgot to add..

---
+++ trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/transformula/vp/VPSFO.java
@@ -0,0 +1,12 @@
+package de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationv2.domain.transformula.vp;
+
+public class VPSFO {
+	
+	public static final String vpStateEqualsClock = "vpStateEqualsClock";
+	public static final String propagateDisEqualitiesClock = "propagateDisEqualitiesClock";
+	public static final String addEqualityClock = "addEqualityClock";
+	public static final String applyClock = "applyClock";
+	public static final String conjoinOverallClock = "conjoin (including consecutive addEquality)";
+	public static final String overallFromPreAnalysis = "overallFromPreAnalysis";
+
+}
