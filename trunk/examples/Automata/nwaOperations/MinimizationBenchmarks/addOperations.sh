4e3273c708fbf1104dcac4436269ca441c3f871d
schillic <schillic@informatik.uni-freiburg.de>
2017-01-08 13:15:29 +0100

    examples

--- trunk/examples/Automata/nwaOperations/MinimizationBenchmarks/addOperations.sh
+++ trunk/examples/Automata/nwaOperations/MinimizationBenchmarks/addOperations.sh
@@ -3,7 +3,10 @@
 for i in *.ats; do 
 	echo "minimizeSevpa(removeUnreachable(nwa));" >> "$i"
 	echo "shrinkNwa(removeUnreachable(nwa));" >> "$i"
-	echo "minimizeNwaMaxSat2(removeUnreachable(nwa));" >> "$i"
+	echo "minimizeNwaPmaxSat(removeUnreachable(nwa));" >> "$i"
+	echo "minimizeNwaPmaxSatAsymmetric(removeUnreachable(nwa));" >> "$i"
 	echo "ReduceNwaDirectSimulation(removeUnreachable(nwa));" >> "$i"
 	echo "ReduceNwaDelayedSimulation(removeUnreachable(nwa));" >> "$i"
+	echo "ReduceNwaDirectSimulationB(removeUnreachable(nwa));" >> "$i"
+	echo "ReduceNwaDelayedSimulationB(removeUnreachable(nwa));" >> "$i"
 done
