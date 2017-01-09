e2dcd06df884d109806a45a2537aeb79314c5e59
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-01-09 17:56:47 +0100

    cc fixes

--- trunk/source/JungVisualization/src/de/uni_freiburg/informatik/ultimate/plugins/output/jungvisualization/editor/JungEditor.java
+++ trunk/source/JungVisualization/src/de/uni_freiburg/informatik/ultimate/plugins/output/jungvisualization/editor/JungEditor.java
@@ -3,27 +3,27 @@
  * Copyright (C) 2015 Jelena Barth
  * Copyright (C) 2015 University of Freiburg
  * Copyright (C) 2009-2015 pashko
- * 
+ *
  * This file is part of the ULTIMATE JungVisualization plug-in.
- * 
+ *
  * The ULTIMATE JungVisualization plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE JungVisualization plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE JungVisualization plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE JungVisualization plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE JungVisualization plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE JungVisualization plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.plugins.output.jungvisualization.editor;
@@ -55,28 +55,29 @@ import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
 
 /**
  * Defines an editor for the JungVisualization plug-in.
- * 
+ *
  * @see {@link EditorPart}
  * @see {@link IPartListener}
  * @author lena
  */
 public class JungEditor extends EditorPart implements IPartListener {
 
-	public static final String ID = "de.uni_freiburg.informatik.ultimate.plugins.output.jungvisualization.editor.JungEditor";
+	public static final String ID =
+			"de.uni_freiburg.informatik.ultimate.plugins.output.jungvisualization.editor.JungEditor";
 	public static final String VV_ID_EDITOR_PROPERTY_KEY = "VisualizationViewerID";
 
 	@Override
-	public void doSave(IProgressMonitor monitor) {
-
+	public void doSave(final IProgressMonitor monitor) {
+		// not needed
 	}
 
 	@Override
 	public void doSaveAs() {
-
+		// not needed
 	}
 
 	@Override
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+	public void init(final IEditorSite site, final IEditorInput input) throws PartInitException {
 		setSite(site);
 		setInput(input);
 		setPartName(((JungEditorInput) input).getName());
@@ -99,7 +100,7 @@ public class JungEditor extends EditorPart implements IPartListener {
 	}
 
 	@Override
-	public void createPartControl(Composite parent) {
+	public void createPartControl(final Composite parent) {
 		final JungEditorInput ei = (JungEditorInput) getEditorInput();
 		final String currentVVID = ei.getId();
 		setPartProperty(VV_ID_EDITOR_PROPERTY_KEY, currentVVID);
@@ -115,14 +116,14 @@ public class JungEditor extends EditorPart implements IPartListener {
 		final JungSelectionProvider jsp = new JungSelectionProvider();
 		getSite().setSelectionProvider(jsp);
 
-		final GraphListener gl = new GraphListener(jsp,ei);
+		final GraphListener gl = new GraphListener(jsp, ei);
 
-		final DefaultModalGraphMouse<VisualizationNode, VisualizationEdge> graphMouse = new DefaultModalGraphMouse<VisualizationNode, VisualizationEdge>();
+		final DefaultModalGraphMouse<VisualizationNode, VisualizationEdge> graphMouse = new DefaultModalGraphMouse<>();
 		graphMouse.setZoomAtMouse(true);
 
 		ei.setMode(Mode.PICKING);
 		graphMouse.setMode(ModalGraphMouse.Mode.PICKING);
-		
+
 		graphMouse.add(gl);
 
 		vv.setGraphMouse(graphMouse);
@@ -131,43 +132,39 @@ public class JungEditor extends EditorPart implements IPartListener {
 
 		panel.setVisible(true);
 		awt.add(panel);
-		
 
 	}
 
 	@Override
 	public void setFocus() {
+		// not needed
 	}
 
 	@Override
-	public void partActivated(IWorkbenchPart part) {
-		// TODO Auto-generated method stub
-
+	public void partActivated(final IWorkbenchPart part) {
+		// not needed
 	}
 
 	@Override
-	public void partBroughtToTop(IWorkbenchPart part) {
-		// TODO Auto-generated method stub
-
+	public void partBroughtToTop(final IWorkbenchPart part) {
+		// not needed
 	}
 
 	@Override
-	public void partClosed(IWorkbenchPart part) {
+	public void partClosed(final IWorkbenchPart part) {
 		if (part == this) {
 			part.getSite().getWorkbenchWindow().getPartService().removePartListener(this);
 		}
 	}
 
 	@Override
-	public void partDeactivated(IWorkbenchPart part) {
-		// TODO Auto-generated method stub
-
+	public void partDeactivated(final IWorkbenchPart part) {
+		// not needed
 	}
 
 	@Override
-	public void partOpened(IWorkbenchPart part) {
-		// TODO Auto-generated method stub
-
+	public void partOpened(final IWorkbenchPart part) {
+		// not needed
 	}
 
 }
--- trunk/source/JungVisualization/src/de/uni_freiburg/informatik/ultimate/plugins/output/jungvisualization/editor/JungEditorInput.java
+++ trunk/source/JungVisualization/src/de/uni_freiburg/informatik/ultimate/plugins/output/jungvisualization/editor/JungEditorInput.java
@@ -3,27 +3,27 @@
  * Copyright (C) 2015 Jelena Barth
  * Copyright (C) 2015 University of Freiburg
  * Copyright (C) 2009-2015 pashko
- * 
+ *
  * This file is part of the ULTIMATE JungVisualization plug-in.
- * 
+ *
  * The ULTIMATE JungVisualization plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE JungVisualization plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE JungVisualization plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE JungVisualization plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE JungVisualization plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE JungVisualization plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.plugins.output.jungvisualization.editor;
@@ -41,10 +41,10 @@ import edu.uci.ics.jung.visualization.VisualizationViewer;
 
 /**
  * EditorInput for the {@link JungEditor}.
- * 
+ *
  * @see {@link IEditorInput}
  * @author lena
- * 
+ *
  */
 public class JungEditorInput implements IEditorInput {
 
@@ -59,7 +59,8 @@ public class JungEditorInput implements IEditorInput {
 	 *            The name of the EditorInput.
 	 * @param visualizationViewer
 	 */
-	public JungEditorInput(String name, VisualizationViewer<VisualizationNode, VisualizationEdge> visualizationViewer) {
+	public JungEditorInput(final String name,
+			final VisualizationViewer<VisualizationNode, VisualizationEdge> visualizationViewer) {
 		assert name != null;
 		mName = name;
 		mVisualizationViewer = visualizationViewer;
@@ -70,16 +71,16 @@ public class JungEditorInput implements IEditorInput {
 	public VisualizationViewer<VisualizationNode, VisualizationEdge> getViewer() {
 		return mVisualizationViewer;
 	}
-	
-	public Mode getMode(){
+
+	public Mode getMode() {
 		return mCurrentMode;
 	}
-	
-	public void setMode(Mode mode){
+
+	public void setMode(final Mode mode) {
 		mCurrentMode = mode;
 	}
-	
-	public String getId(){
+
+	public String getId() {
 		return mId;
 	}
 
@@ -110,15 +111,15 @@ public class JungEditorInput implements IEditorInput {
 
 	@SuppressWarnings("rawtypes")
 	@Override
-	public Object getAdapter(Class adapter) {
+	public Object getAdapter(final Class adapter) {
 		return null;
 	}
-	
+
 	/**
-	 * Hacky shit (based on Lenas code; didnt want to learn how to do that properly) 
+	 * Hacky shit (based on Lenas code; didnt want to learn how to do that properly)
 	 */
-	public void displayContextMenu(){
-		
+	public void displayContextMenu() {
+		// not needed
 	}
 
 }
