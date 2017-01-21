04ba9455e6f8f64b03e76faf5efd6b739b786219
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2017-01-21 10:27:38 +0100

    added license headers in TreeAutomizer

--- trunk/source/TreeAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/treeautomizer/graph/HCPredicate.java
+++ trunk/source/TreeAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/treeautomizer/graph/HCPredicate.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (C) 2016 Alexander Nutz (nutz@informatik.uni-freiburg.de)
+ * Copyright (C) 2016 Mostafa M.A. (mostafa.amin93@gmail.com)
+ * Copyright (C) 2016 University of Freiburg
+ *
+ * This file is part of the ULTIMATE TreeAutomizer Plugin.
+ *
+ * The ULTIMATE TreeAutomizer Plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE TreeAutomizer Plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE TreeAutomizer Plugin. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE TreeAutomizer Plugin, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE TreeAutomizer Plugin grant you additional permission
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.plugins.generator.treeautomizer.graph;
 
 import java.util.HashSet;
@@ -29,13 +56,12 @@ public class HCPredicate extends BasicPredicate implements IPredicate {
 	/**
 	 * The published attributes. Update this and getFieldValue() if you add new attributes.
 	 */
-	private final static String[] s_AttribFields = { "ProgramPoint", "Formula", "Vars" };
+	private static final String[] s_AttribFields = { "ProgramPoint", "Formula", "Vars" };
 
 	protected HCPredicate(final HornClausePredicateSymbol programPoint, int serialNumber, final Term term,
 			final Set<IProgramVar> vars, final Map<Term, HCVar> varsMap) {
 		super(serialNumber, new String[]{}, term, vars, null);
 		mProgramPoint = programPoint;
-		
 		mProgramVars = varsMap;
 	}
 
