27a30b0dfb9175e4c609c130d1d662694ada60fb
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-02-03 10:18:09 +0100

    fix DummyLocation in spaceexparser (good intention, wrong values)

--- trunk/source/SpaceExParser/src/de/uni_freiburg/informatik/ultimate/plugins/spaceex/icfg/DummyLocation.java
+++ trunk/source/SpaceExParser/src/de/uni_freiburg/informatik/ultimate/plugins/spaceex/icfg/DummyLocation.java
@@ -4,59 +4,51 @@ import de.uni_freiburg.informatik.ultimate.core.model.models.ILocation;
 import de.uni_freiburg.informatik.ultimate.core.model.models.annotation.IAnnotations;
 
 public class DummyLocation implements ILocation {
-	
+
 	private final String mFilename;
-	
-	public DummyLocation(String filename) {
+
+	public DummyLocation(final String filename) {
 		mFilename = filename;
 	}
-	
+
 	@Override
 	public String getFileName() {
-		// TODO Auto-generated method stub
 		return mFilename;
 	}
-	
+
 	@Override
 	public int getStartLine() {
-		// TODO Auto-generated method stub
-		return 0;
+		return -1;
 	}
-	
+
 	@Override
 	public int getEndLine() {
-		// TODO Auto-generated method stub
-		return 1;
+		return -1;
 	}
-	
+
 	@Override
 	public int getStartColumn() {
-		// TODO Auto-generated method stub
-		return 0;
+		return -1;
 	}
-	
+
 	@Override
 	public int getEndColumn() {
-		// TODO Auto-generated method stub
-		return 10;
+		return -1;
 	}
-	
+
 	@Override
 	public ILocation getOrigin() {
-		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public IAnnotations getCheck() {
-		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public boolean isLoop() {
-		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
 }
