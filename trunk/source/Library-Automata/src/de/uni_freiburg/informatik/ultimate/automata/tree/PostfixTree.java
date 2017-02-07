40ebf20eef5efc3aa0f04a754e30b4dfcf69c70b
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2017-02-07 17:23:51 +0100

    added license information, headers

--- trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/tree/PostfixTree.java
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/tree/PostfixTree.java
@@ -1,9 +1,43 @@
+/*
+ * Copyright (C) 2016 Mostafa M.A. (mostafa.amin93@gmail.com)
+ * Copyright (C) 2014-2016 University of Freiburg
+ * 
+ * This file is part of the ULTIMATE Automata Library.
+ * 
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.automata.tree;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
+/**
+ * 
+ * @author Mostafa M.A. (mostafa.amin93@gmail.com)
+ *
+ * @param <LETTER>
+ * @param <STATE>
+ */
 public class PostfixTree<LETTER, STATE> {
 	
 	private final ArrayList<LETTER> postFix;
@@ -15,15 +49,15 @@ public class PostfixTree<LETTER, STATE> {
 	private final HashMap<Integer, Integer> mBeg;
 	
 	
-	public ArrayList<LETTER> getPostFix() {
+	public List<LETTER> getPostFix() {
 		return postFix;
 	}
 
-	public ArrayList<Integer> getStartIdx() {
+	public List<Integer> getStartIdx() {
 		return startIdx;
 	}
 	
-	public ArrayList<STATE> getPostFixStates() {
+	public List<STATE> getPostFixStates() {
 		return postFixStates;
 	}
 
