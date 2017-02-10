5d53e3d727f9f4a798e1044e69db582834eb5ff1
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-02-10 17:34:32 +0100

    cc fixes

--- trunk/source/Library-UltimateModel/src/de/uni_freiburg/informatik/ultimate/core/model/models/ILocation.java
+++ trunk/source/Library-UltimateModel/src/de/uni_freiburg/informatik/ultimate/core/model/models/ILocation.java
@@ -20,9 +20,9 @@
  * 
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Core grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Core grant you additional permission
  * to convey the resulting work.
  */
 /**
@@ -34,68 +34,60 @@ package de.uni_freiburg.informatik.ultimate.core.model.models;
 import de.uni_freiburg.informatik.ultimate.core.model.models.annotation.IAnnotations;
 
 /**
- * Defines an area in a text file.
- * Used to specify where an BoogieASTNode is defined.
+ * Defines an area in a text file. Used to specify where an BoogieASTNode is defined.
  * 
  * @author heizmann@informatik.uni-freiburg.de
  * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  *
  */
 public interface ILocation {
-	
+
 	/**
 	 * @return Name of this {@code Location}s file.
 	 */
-	public String getFileName();
-	
+	String getFileName();
+
 	/**
 	 * @return Number of line where this {@code Location} begins. -1 if unknown.
 	 */
-	public int getStartLine();
-	
+	int getStartLine();
+
 	/**
 	 * @return Number of line where this {@code Location} ends. -1 if unknown.
 	 */
-	public int getEndLine();
-	
+	int getEndLine();
+
 	/**
 	 * @return Number of column where this {@code Location} begins. -1 if unknown.
 	 */
-	public int getStartColumn();
-	
+	int getStartColumn();
+
 	/**
 	 * @return Number of column where this {@code Location} ends. -1 if unknown.
 	 */
-	public int getEndColumn();
+	int getEndColumn();
 
 	/**
-	 * This {@code Location} can be an auxiliary {@code Location} constructed
-	 * with respect to some <i>origin</i> {@code Location}. E.g.,
-	 * if this is an auxiliary {@code Location} for the else-branch the
-	 * <i>origin</i> {@code Location} can be the {@code Location} of an 
-	 * if-then-else statement of a program.
+	 * This {@code Location} can be an auxiliary {@code Location} constructed with respect to some <i>origin</i>
+	 * {@code Location}. E.g., if this is an auxiliary {@code Location} for the else-branch the <i>origin</i>
+	 * {@code Location} can be the {@code Location} of an if-then-else statement of a program.
 	 * 
-	 * If this {@code Location} is no auxiliary location the <i>origin</i> is
-	 * the location itself.
+	 * If this {@code Location} is no auxiliary location the <i>origin</i> is the location itself.
 	 */
 	@Deprecated
-	public ILocation getOrigin();
-	
-	
-	
+	ILocation getOrigin();
+
 	/**
-	 * Textual description of the type of specification which is checked here.
-	 * E.g., "NullPointerException", "AssertStatement" etc.
+	 * Textual description of the type of specification which is checked here. E.g., "NullPointerException",
+	 * "AssertStatement" etc.
 	 */
 	@Deprecated
-	public IAnnotations getCheck();
-	
-	
-	
+	IAnnotations getCheck();
+
 	/**
 	 * 
 	 * @return true iff this Location represents a loop.
 	 */
 	@Deprecated
-	public boolean isLoop();
+	boolean isLoop();
 }
