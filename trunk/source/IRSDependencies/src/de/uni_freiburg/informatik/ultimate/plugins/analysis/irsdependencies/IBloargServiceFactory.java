e7cb277c003f61ef0fe3efe115aa2a4e8c884ddb
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-03-05 09:06:19 +0100

    service usage in ultimate: an example for julian

---
+++ trunk/source/IRSDependencies/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/irsdependencies/IBloargServiceFactory.java
@@ -0,0 +1,8 @@
+package de.uni_freiburg.informatik.ultimate.plugins.analysis.irsdependencies;
+
+import de.uni_freiburg.informatik.ultimate.core.model.IServiceFactory;
+import de.uni_freiburg.informatik.ultimate.plugins.analysis.irsdependencies.BloargServiceFactory.BloargService;
+
+public interface IBloargServiceFactory extends IServiceFactory<BloargService> {
+
+}
