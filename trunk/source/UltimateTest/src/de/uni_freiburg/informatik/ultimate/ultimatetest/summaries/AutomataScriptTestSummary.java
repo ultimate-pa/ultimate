53e5a0cb605178966f4e160ed96bda6050971c0e
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-03-05 07:18:12 +0100

    cc fixes

--- trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimate/ultimatetest/summaries/AutomataScriptTestSummary.java
+++ trunk/source/UltimateTest/src/de/uni_freiburg/informatik/ultimate/ultimatetest/summaries/AutomataScriptTestSummary.java
@@ -19,9 +19,9 @@
  * 
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Test Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Test Library grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Test Library grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.ultimatetest.summaries;
@@ -41,25 +41,25 @@ import de.uni_freiburg.informatik.ultimate.test.reporting.ITestSummary;
 import de.uni_freiburg.informatik.ultimate.util.statistics.Benchmark;
 
 public class AutomataScriptTestSummary implements ITestSummary {
-	
+
 	private final Class<? extends UltimateTestSuite> mUltimateTestSuite;
 	private final List<SummaryEntry> mResults;
 
-	public AutomataScriptTestSummary(Class<? extends UltimateTestSuite> ultimateTestSuite) {
+	public AutomataScriptTestSummary(final Class<? extends UltimateTestSuite> ultimateTestSuite) {
 		mUltimateTestSuite = ultimateTestSuite;
-		mResults = new ArrayList<SummaryEntry>();
+		mResults = new ArrayList<>();
 	}
-	
+
 	@Override
 	public Class<? extends UltimateTestSuite> getUltimateTestSuiteClass() {
 		return mUltimateTestSuite;
 	}
-	
+
 	@Override
 	public String getDescriptiveLogName() {
 		return this.getClass().getSimpleName();
 	}
-	
+
 	@Override
 	public String getFilenameExtension() {
 		return ".log";
@@ -72,10 +72,10 @@ public class AutomataScriptTestSummary implements ITestSummary {
 		sb.append(mUltimateTestSuite);
 		sb.append(" #################");
 		sb.append("\n");
-		for (final SummaryEntry summaryEntry  : mResults) {
+		for (final SummaryEntry summaryEntry : mResults) {
 			sb.append(summaryEntry.getTestResult().toString());
 			sb.append("\t");
-			sb.append(String.format( "%.2f", summaryEntry.getTime()) + "s");
+			sb.append(String.format("%.2f", summaryEntry.getTime()) + "s");
 			sb.append("\t");
 			sb.append(summaryEntry.getMessage());
 			sb.append("\t");
@@ -86,42 +86,47 @@ public class AutomataScriptTestSummary implements ITestSummary {
 	}
 
 	@Override
-	public void addResult(UltimateRunDefinition ultimateRunDefinition, TestResult threeValuedResult,
-			String category, String message, String testname, IResultService resultService) {
-		final Collection<Benchmark> benchmarkSingleton = ResultUtil.getCsvProviderProviderFromUltimateResults(resultService.getResults(), Benchmark.class);
+	public void addResult(final UltimateRunDefinition ultimateRunDefinition, final TestResult threeValuedResult,
+			final String category, final String message, final String testname, final IResultService resultService) {
+		final Collection<Benchmark> benchmarkSingleton =
+				ResultUtil.getCsvProviderProviderFromUltimateResults(resultService.getResults(), Benchmark.class);
 		if (benchmarkSingleton.size() != 1) {
 			throw new AssertionError("expected single benchmark result");
-		} else {
-			final Benchmark benchmark = benchmarkSingleton.iterator().next();
-			final double time = benchmark.getElapsedTime(de.uni_freiburg.informatik.ultimate.plugins.generator.automatascriptinterpreter.Activator.PLUGIN_NAME, TimeUnit.SECONDS);
-			mResults.add(new SummaryEntry(threeValuedResult, message, time, ultimateRunDefinition.getInput()[0]));
 		}
-		
+		final Benchmark benchmark = benchmarkSingleton.iterator().next();
+		final double time = benchmark.getElapsedTime(
+				de.uni_freiburg.informatik.ultimate.plugins.generator.automatascriptinterpreter.Activator.PLUGIN_NAME,
+				TimeUnit.SECONDS);
+		mResults.add(new SummaryEntry(threeValuedResult, message, time, ultimateRunDefinition.getInput()[0]));
 
 	}
-	
+
 	private class SummaryEntry {
 		private final TestResult mTestResult;
 		private final String mMessage;
 		private final double mTime;
 		private final File mAtsFile;
-		public SummaryEntry(TestResult testResult, String message, double time,
-				File atsFile) {
+
+		public SummaryEntry(final TestResult testResult, final String message, final double time, final File atsFile) {
 			super();
 			mTestResult = testResult;
 			mMessage = message;
 			mTime = time;
 			mAtsFile = atsFile;
 		}
+
 		public TestResult getTestResult() {
 			return mTestResult;
 		}
+
 		public String getMessage() {
 			return mMessage;
 		}
+
 		public double getTime() {
 			return mTime;
 		}
+
 		public File getAtsFile() {
 			return mAtsFile;
 		}
