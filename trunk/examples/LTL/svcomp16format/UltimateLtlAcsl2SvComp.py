fa9764f18d27dac75d0e89cb127baa71c4cfe724
Langenfeld <langenfv@informatik.uni-freiburg.de>
2017-03-09 18:16:08 +0100

    - svcomp ltl examples (see #119) - old bug_something.c files moved to regressions

--- trunk/examples/LTL/svcomp16format/UltimateLtlAcsl2SvComp.py
+++ trunk/examples/LTL/svcomp16format/UltimateLtlAcsl2SvComp.py
@@ -26,10 +26,12 @@ def splitFile(file, fileName, targetDir):
     propertyFileContent = ""
     cFileContent = ""
     ltlInvarFound = False
+    resultInfoFound = False
     
     for line in file:
         if (line.startswith("//#Unsafe") or line.startswith("//#Safe")):
             resultSafe = parseResultInfo(line)
+            resultInfoFound = True
             if INCLUDE_RESULT_ULTIMATE_ANNOTATION:
                  propertyFileContent += line 
         elif line.strip().startswith("//@ ltl invariant"):
@@ -44,8 +46,15 @@ def splitFile(file, fileName, targetDir):
 
     if (not ltlInvarFound):
         print("No LTL-invariant in file %s"%(fileName))
-    newCFile = open(os.path.join(targetDir, fileName ),"w+")
-    newLTLFile = open(getPathForPropertyFile(targetDir, fileName, resultSafe),"w+")
+        return
+    if (not resultInfoFound and (INCLUDE_RESULT_ULTIMATE_ANNOTATION or MAKE_SVCOMP_FILE_FORMAT)):
+        print("No result annotation found in file: %s"%(fileName))
+        return
+    fileNameWithNoExt = os.path.splitext(fileName)[0]
+    cFilePath = os.path.join(targetDir, fileNameWithNoExt + getResultSuffix(resultSafe) + ".c")
+    newCFile = open(cFilePath,"w+")
+    ltlFilePath = os.path.join(targetDir, fileNameWithNoExt + getResultSuffix(resultSafe) + getPropFileExt())
+    newLTLFile = open(ltlFilePath,"w+")
     newCFile.write(cFileContent)
     newLTLFile.write(propertyFileContent)
     newCFile.close()
@@ -80,14 +89,18 @@ def parseResultInfo(resultInfo):
     else:
         return False
     
-def getPathForPropertyFile(targetDir, fileName, resultSafe):
+def getResultSuffix(resultSafe):
     if MAKE_SVCOMP_FILE_FORMAT:
         if resultSafe:
-            return os.path.join(targetDir, fileName[0:-2]+"_true-valid-ltl.prp")
+            return "_true-valid-ltl"
         else:
-            return os.path.join(targetDir, fileName[0:-2]+"_false-valid-ltl.prp")
+            return "_false-valid-ltl"
+
+def getPropFileExt():
+    if MAKE_SVCOMP_FILE_FORMAT:
+        return ".prp"
     else:
-        return os.path.join(targetDir, fileName[0:-2]+".ltl")
+        return ".ltl"
         
 def getSvcompCheckLine(ltlFormula):
     return "CHECK( init(main()), LTL( %s ) )"%ltlFormula
@@ -96,12 +109,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert c file with ACSL-LTL annotations to C file and LTL file')
     parser.add_argument("source", help="directory with .c files containing ltl-acsl",type=str)
     parser.add_argument("target", help="target dir for .c and .ltl files",type=str)
-    parser.add_argument("-r", "--resultInfo", help="include //#Safe and //#Unsafe annotations in LTL files", action="store_true")
-    parser.add_argument("-s", "--svCompFormat", help="append SVComp file endings \"-(false|true)-valid-ltl.prp\" and CHECK annotation (use this to prepare svcomp ltl benchmarks)", action="store_true")
+    parser.add_argument("-r", "--resultInfo", help="include //#Safe and //#Unsafe annotations in LTL files", action="store_true", default= False)
+    parser.add_argument("-s", "--svCompFormat", help="append SVComp file endings \"-(false|true)-valid-ltl.prp\" and CHECK annotation (use this to prepare svcomp ltl benchmarks)", action="store_true",default= False)
     args = parser.parse_args()
-    if args.resultInfo:
-        INCLUDE_RESULT_ULTIMATE_ANNOTATION = True
-    if args.svCompFormat:
-        MAKE_SVCOMP_FILE_FORMAT = True
+    INCLUDE_RESULT_ULTIMATE_ANNOTATION = args.resultInfo
+    MAKE_SVCOMP_FILE_FORMAT = args.svCompFormat
     
     convertFilesInDirectory(args.source, args.target)
---
+++ trunk/examples/LTL/svcomp17format/coolant/coolant_basis_4_2_unsafe_sftyliveness_false-valid-ltl.c
@@ -0,0 +1,78 @@
+
+#include <stdio.h> 
+
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+extern void __VERIFIER_assume() __attribute__ ((__noreturn__));
+extern int __VERIFIER_nondet_int() __attribute__ ((__noreturn__));
+
+int error, tempDisplay, warnLED, tempIn, chainBroken, temp, otime = 0, time = 0, limit, init;
+
+
+void display(int tempdiff, int warning)
+{
+	tempDisplay = tempdiff;
+	warnLED = warning;
+	if (warning != 0)
+	{
+		//No BUG (but not reachable)
+		chainBroken  = 0;
+	}
+}
+
+int vinToCels(int kelvin)
+{
+	if (temp < 0) 
+	{
+		error = 1;
+		display(kelvin - 273, error);
+	}
+	return kelvin -273;
+}
+
+void coolantControl()
+{
+	while(1)
+	{
+		int warning = 0;
+		otime = time;
+		time = otime +1;
+		tempIn = __VERIFIER_nondet_int();
+		temp = vinToCels(tempIn);
+		if(temp > limit) 
+		{
+			chainBroken = 1;
+			warning = 1;
+		}
+		display(temp, warning);
+	}
+}
+
+int main()
+{
+    init = 0;
+    tempDisplay = 0;
+    warnLED = 1;
+    tempIn = 0;
+    error = 0;
+    chainBroken = 0;
+    temp = 0;
+    limit = 8;
+    init = 1;
+	
+	while(1)
+	{
+		int limit = __VERIFIER_nondet_int();
+		if(limit < 10 && limit > -273)
+		{
+			error = 0;
+			display(0, error);
+			break;	
+		} else {
+			error = 1;
+			display(0, error);
+		}	
+	}
+	
+	init = 3;
+	coolantControl();	
+}
