8d6069b5b607324bc91053699fc6235ebf7344b9
betim1988 <musab@informatik.uni-freiburg.de>
2017-03-09 16:36:36 +0100

    parametrize increasing strategy

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/pathinvariants/internal/TemplateDimensionsStrategy.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/pathinvariants/internal/TemplateDimensionsStrategy.java
@@ -28,17 +28,17 @@ public class TemplateDimensionsStrategy {
 
 	public int[] getDimensions(IcfgLocation location, int round) {
 		if (round == 1) {
-			return new int[] {1, 1};
+			return new int[] {mInitialDisjuncts, mInitialConjuncts};
 		} else if (round == 2) {
-			return new int[] {1, 2};
+			return new int[] {mInitialDisjuncts, mInitialConjuncts + mConjunctsPerRound};
 		} else if (round == 3) {
-			return new int[] {2, 2};
+			return new int[] {mInitialDisjuncts + mDisjunctsPerRound, mInitialConjuncts + mConjunctsPerRound};
 		} else if (round == 4) {
-			return new int[] {2, 3};
+			return new int[] {mInitialDisjuncts + mDisjunctsPerRound, mInitialConjuncts + 2 * mConjunctsPerRound};
 		} else if (round == 5) {
-			return new int[] {3, 2};
+			return new int[] {mInitialDisjuncts + 2 * mDisjunctsPerRound, mInitialConjuncts + mConjunctsPerRound};
 		} else if (round == 6) {
-			return new int[] {3, 3};
+			return new int[] {mInitialDisjuncts + 2 * mDisjunctsPerRound, mInitialConjuncts + 2 * mConjunctsPerRound};
 		} else {
 			return new int[] { mInitialDisjuncts + round * mDisjunctsPerRound,
 					mInitialConjuncts + round * mConjunctsPerRound };
