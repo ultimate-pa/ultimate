6b8613a645d5fcdb71cea7ea788dbd1ad3868d36
Matthias <heizmann@informatik.uni-freiburg.de>
2017-03-14 17:43:54 +0100

    cleanup Hoare annotaiton

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/HoareAnnotationWriter.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/HoareAnnotationWriter.java
@@ -30,20 +30,13 @@ package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction;
 import java.util.Map.Entry;
 
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
-import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.CfgSmtToolkit;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfg;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfgCallTransition;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgEdge;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgLocation;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramVar;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils.SimplificationTechnique;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils.XnfConversionTechnique;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.PredicateTransformer;
-import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.ISLPredicate;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.PredicateFactory;
-import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
 
 /**
  * Write a Hoare annotation provided by HoareAnnotationFragments to the CFG.
@@ -59,11 +52,6 @@ public class HoareAnnotationWriter {
 	private final PredicateFactory mPredicateFactory;
 	private final HoareAnnotationComposer mCegarLoopHoareAnnotation;
 
-	/**
-	 * What is the precondition for a context? Strongest postcondition or entry given by automaton?
-	 */
-	private final boolean mUseEntry;
-	private final PredicateTransformer mPredicateTransformer;
 
 	public HoareAnnotationWriter(final IIcfg<?> icfg, final CfgSmtToolkit csToolkit,
 			final PredicateFactory predicateFactory, final HoareAnnotationComposer cegarLoopHoareAnnotation,
@@ -74,9 +62,6 @@ public class HoareAnnotationWriter {
 		mCsToolkit = csToolkit;
 		mPredicateFactory = predicateFactory;
 		mCegarLoopHoareAnnotation = cegarLoopHoareAnnotation;
-		mUseEntry = true;
-		mPredicateTransformer = new PredicateTransformer(services, csToolkit.getManagedScript(), simplicationTechnique,
-				xnfConversionTechnique);
 	}
 
 	public void addHoareAnnotationToCFG() {
@@ -92,69 +77,5 @@ public class HoareAnnotationWriter {
 			}
 			hoareAnnot.addInvariant(entry.getValue());
 		}
-		// for (final Triple<IcfgLocation, IPredicate, IPredicate> locPrecondInvariant :
-		// mCegarLoopHoareAnnotation.getLoc2precond2invariant().entrySet()) {
-		// addFormulasToLocNodes(locPrecondInvariant.getFirst(), locPrecondInvariant.getSecond(),
-		// locPrecondInvariant.getThird());
-		// }
 	}
-
-	/**
-	 * @param csToolkit
-	 * @param precondForContext
-	 * @param pp2preds
-	 */
-	private void addHoareAnnotationForContext(final CfgSmtToolkit csToolkit, final IPredicate precondForContext,
-			final HashRelation<IcfgLocation, IPredicate> pp2preds) {
-		for (final IcfgLocation pp : pp2preds.getDomain()) {
-			final IPredicate[] preds = pp2preds.getImage(pp).toArray(new IPredicate[0]);
-			final Term tvp = mPredicateFactory.or(false, preds);
-			final IPredicate formulaForPP = mPredicateFactory.newPredicate(tvp);
-			addFormulasToLocNodes(pp, precondForContext, formulaForPP);
-		}
-	}
-
-	private void addFormulasToLocNodes(final IcfgLocation pp, final IPredicate context, final IPredicate current) {
-		final String procName = pp.getProcedure();
-		final String locName = pp.getDebugIdentifier();
-		final IcfgLocation locNode = mIcfg.getProgramPoints().get(procName).get(locName);
-		HoareAnnotation hoareAnnot = null;
-
-		final HoareAnnotation taAnnot = HoareAnnotation.getAnnotation(locNode);
-		if (taAnnot == null) {
-			hoareAnnot = mPredicateFactory.getNewHoareAnnotation(pp, mCsToolkit.getModifiableGlobalsTable());
-			hoareAnnot.annotate(locNode);
-		} else {
-			hoareAnnot = taAnnot;
-		}
-		hoareAnnot.addInvariant(context, current);
-	}
-
-	private static IIcfgCallTransition<?> getCall(final ISLPredicate pred) {
-		final IcfgLocation pp = pred.getProgramPoint();
-		IIcfgCallTransition<?> result = null;
-		for (final IcfgEdge edge : pp.getOutgoingEdges()) {
-			if (edge instanceof IIcfgCallTransition<?>) {
-				if (result == null) {
-					result = (IIcfgCallTransition<?>) edge;
-				} else {
-					throw new UnsupportedOperationException("several outgoing calls");
-				}
-			}
-		}
-		if (result == null) {
-			throw new AssertionError("no outgoing call");
-		}
-		return result;
-	}
-
-	private static boolean containsAnOldVar(final IPredicate p) {
-		for (final IProgramVar bv : p.getVars()) {
-			if (bv.isOldvar()) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 }
