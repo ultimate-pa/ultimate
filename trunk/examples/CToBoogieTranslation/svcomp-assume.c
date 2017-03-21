98541603781ef48a5571fce4cdc15dc059f3db88
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-03-21 11:37:33 +0100

    new regression test for unary minus vs &minus;

---
+++ trunk/examples/CToBoogieTranslation/svcomp-assume.c
@@ -0,0 +1,13 @@
+//#Safe
+// Typing "-" on my machine produces &#45;
+// Check your machine's "-" with http://unicodelookup.com/ 
+// "−" (&minus; or 0x2212) vs "-" (&#45; or 0x2D)
+// CDT does not recognize "−" (&minus;) as minus, because only "-" (&#45;) is minus 
+void main(){
+
+	char x 	= __VERIFIER_nondet_char();
+	__VERIFIER_assume(x > −1);
+	if(x == 0){
+		__VERIFIER_error();		
+	}
+}
