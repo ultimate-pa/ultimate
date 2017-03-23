89932517a7e486cf8b4f21b74e8c003212d4fbf9
DESKTOP-Win10\IARI <julian.jarecki@gmail.com>
2017-03-23 15:57:57 +0100

    unused import

--- trunk/source/Library-InteractiveTA/src/de/uni_freiburg/informatik/ultimate/interactive/traceabstraction/TAConverter.java
+++ trunk/source/Library-InteractiveTA/src/de/uni_freiburg/informatik/ultimate/interactive/traceabstraction/TAConverter.java
@@ -1,6 +1,5 @@
 package de.uni_freiburg.informatik.ultimate.interactive.traceabstraction;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
