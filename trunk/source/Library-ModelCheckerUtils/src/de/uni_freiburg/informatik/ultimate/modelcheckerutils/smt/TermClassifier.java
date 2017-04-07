537cdda6b8f1aea62695fcd2a58a5459f398f10c
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-04-07 16:12:06 +0200

    add hasArraySort to TermClassifier

--- trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/TermClassifier.java
+++ trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/TermClassifier.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2017 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2017 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE ModelCheckerUtils Library.
- * 
+ *
  * The ULTIMATE ModelCheckerUtils Library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE ModelCheckerUtils Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE ModelCheckerUtils Library. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE ModelCheckerUtils Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE ModelCheckerUtils Library grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE ModelCheckerUtils Library grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt;
@@ -41,74 +41,27 @@ import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
 
 /**
  * Check for a bunch of criteria if they are satisfied by a given {@link Term}.
+ *
  * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  *
  */
 public class TermClassifier extends NonRecursive {
-	
+
 	private Set<Term> mTermsInWhichWeAlreadyDescended;
-	
-	class MyWalker extends TermWalker {
-		MyWalker(final Term term) { super(term); }
-		
-		@Override
-		public void walk(final NonRecursive walker) {
-			if (mTermsInWhichWeAlreadyDescended.contains(getTerm())) {
-				// do nothing
-			} else {
-				final Sort currentSort = getTerm().getSort();
-				mOccuringSortNames.add(currentSort.getName());
-				super.walk(walker);
-			}
-		}
 
-
-
-
-		@Override
-		public void walk(final NonRecursive walker, final ConstantTerm term) {
-			// cannot descend
-		}
-		@Override
-		public void walk(final NonRecursive walker, final AnnotatedTerm term) {
-			mTermsInWhichWeAlreadyDescended.add(term);
-			walker.enqueueWalker(new MyWalker(term.getSubterm()));
-		}
-		@Override
-		public void walk(final NonRecursive walker, final ApplicationTerm term) {
-			mOccuringFunctionNames.add(term.getFunction().getName());
-			mTermsInWhichWeAlreadyDescended.add(term);
-			for (final Term t : term.getParameters()) {
-				walker.enqueueWalker(new MyWalker(t));
-			}
-		}
-		@Override
-		public void walk(final NonRecursive walker, final LetTerm term) {
-			throw new UnsupportedOperationException();
-		}
-		@Override
-		public void walk(final NonRecursive walker, final QuantifiedFormula term) {
-			mOccuringQuantifiers.add(term.getQuantifier());
-			walker.enqueueWalker(new MyWalker(term.getSubformula()));
-		}
-		@Override
-		public void walk(final NonRecursive walker, final TermVariable term) {
-			// cannot descend
-		}
-	}
-	
 	private final Set<String> mOccuringSortNames;
 	private final Set<String> mOccuringFunctionNames;
 	private final Set<Integer> mOccuringQuantifiers;
-	
-	
+	private boolean mHasArrays;
+
 	public TermClassifier() {
 		super();
 		mOccuringSortNames = new HashSet<>();
 		mOccuringFunctionNames = new HashSet<>();
 		mOccuringQuantifiers = new HashSet<>();
+		mHasArrays = false;
 	}
-	
+
 	public Set<String> getOccuringSortNames() {
 		return mOccuringSortNames;
 	}
@@ -121,13 +74,72 @@ public class TermClassifier extends NonRecursive {
 		return mOccuringQuantifiers;
 	}
 
+	public boolean hasArrays() {
+		return mHasArrays;
+	}
+
 	/**
-	 * Check a/another Term and and the result to the existing classification
-	 * results. 
+	 * Check a/another Term and and the result to the existing classification results.
 	 */
 	public void checkTerm(final Term term) {
 		mTermsInWhichWeAlreadyDescended = new HashSet<>();
 		run(new MyWalker(term));
 		mTermsInWhichWeAlreadyDescended = null;
+	}
+
+	private class MyWalker extends TermWalker {
+		MyWalker(final Term term) {
+			super(term);
+		}
+
+		@Override
+		public void walk(final NonRecursive walker) {
+			if (mTermsInWhichWeAlreadyDescended.contains(getTerm())) {
+				// do nothing
+			} else {
+				final Sort currentSort = getTerm().getSort();
+				mOccuringSortNames.add(currentSort.getName());
+				if (currentSort.isArraySort()) {
+					mHasArrays = true;
+				}
+				super.walk(walker);
+			}
+		}
+
+		@Override
+		public void walk(final NonRecursive walker, final ConstantTerm term) {
+			// cannot descend
+		}
+
+		@Override
+		public void walk(final NonRecursive walker, final AnnotatedTerm term) {
+			mTermsInWhichWeAlreadyDescended.add(term);
+			walker.enqueueWalker(new MyWalker(term.getSubterm()));
+		}
+
+		@Override
+		public void walk(final NonRecursive walker, final ApplicationTerm term) {
+			mOccuringFunctionNames.add(term.getFunction().getName());
+			mTermsInWhichWeAlreadyDescended.add(term);
+			for (final Term t : term.getParameters()) {
+				walker.enqueueWalker(new MyWalker(t));
+			}
+		}
+
+		@Override
+		public void walk(final NonRecursive walker, final LetTerm term) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void walk(final NonRecursive walker, final QuantifiedFormula term) {
+			mOccuringQuantifiers.add(term.getQuantifier());
+			walker.enqueueWalker(new MyWalker(term.getSubformula()));
+		}
+
+		@Override
+		public void walk(final NonRecursive walker, final TermVariable term) {
+			// cannot descend
+		}
 	}
 }
