f0e3c5b5f085b21a55e46d220aa6bfc9aa266780
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-05-02 16:06:32 +0200

    provide z3 scriptor and mocks directly from UltimateMocks (fixing dependency problem in Icfgtransformertest)

--- trunk/source/Library-ModelCheckerUtilsTest/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/bdd/BddBuilderTest.java
+++ trunk/source/Library-ModelCheckerUtilsTest/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/bdd/BddBuilderTest.java
@@ -15,31 +15,30 @@ import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
 import de.uni_freiburg.informatik.ultimate.smtinterpol.smtlib2.SMTInterpol;
-import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateServiceProviderMock;
+import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
 import net.sf.javabdd.BDD;
 import net.sf.javabdd.BDDFactory;
 
-
 public class BddBuilderTest {
-	
+
 	IUltimateServiceProvider services;
 	Script script;
 	SimplifyBdd simplifyBdd;
 	Sort bool;
-	
+
 	@Before
-	public void setUp(){
-		services = new UltimateServiceProviderMock();
+	public void setUp() {
+		services = UltimateMocks.createUltimateServiceProviderMock();
 		script = new SMTInterpol();
 		final ManagedScript mgdScript = new ManagedScript(services, script);
 		simplifyBdd = new SimplifyBdd(services, mgdScript);
 
 		script.setLogic(Logics.CORE);
-		bool = ((SMTInterpol)script).getTheory().getBooleanSort();
+		bool = ((SMTInterpol) script).getTheory().getBooleanSort();
 	}
-	
+
 	@Test
-	public void testAnd(){
+	public void testAnd() {
 		final Term x1 = script.variable("x1", bool);
 		final Term x2 = script.variable("x2", bool);
 		final Term t = SmtUtils.and(script, Arrays.asList(x1, x2));
@@ -47,14 +46,14 @@ public class BddBuilderTest {
 		final List<Term> atoms = ca.getTerms(t);
 		final BddBuilder bb = new BddBuilder();
 		final BDD d = bb.buildBDD(t, atoms);
-		
+
 		final BDDFactory factory = BDDFactory.init("java", atoms.size(), atoms.size(), false);
 		factory.setVarNum(atoms.size());
 		Assert.assertEquals(factory.ithVar(0).and(factory.ithVar(1)), d);
 	}
-	
+
 	@Test
-	public void testOR(){
+	public void testOR() {
 		final Term x1 = script.variable("x1", bool);
 		final Term x2 = script.variable("x2", bool);
 		final Term t = SmtUtils.or(script, Arrays.asList(x1, x2));
@@ -62,28 +61,28 @@ public class BddBuilderTest {
 		final List<Term> atoms = ca.getTerms(t);
 		final BddBuilder bb = new BddBuilder();
 		final BDD d = bb.buildBDD(t, atoms);
-		
+
 		final BDDFactory factory = BDDFactory.init("java", atoms.size(), atoms.size(), false);
 		factory.setVarNum(atoms.size());
 		Assert.assertEquals(factory.ithVar(0).or(factory.ithVar(1)), d);
 	}
-	
+
 	@Test
-	public void testNot(){
+	public void testNot() {
 		final Term x1 = script.variable("x1", bool);
 		final Term t = SmtUtils.not(script, x1);
 		final CollectAtoms ca = new CollectAtoms();
 		final List<Term> atoms = ca.getTerms(t);
 		final BddBuilder bb = new BddBuilder();
 		final BDD d = bb.buildBDD(t, atoms);
-		
-		final BDDFactory factory = BDDFactory.init("java", atoms.size()+2, atoms.size()+2, false);
+
+		final BDDFactory factory = BDDFactory.init("java", atoms.size() + 2, atoms.size() + 2, false);
 		factory.setVarNum(atoms.size());
 		Assert.assertEquals(factory.ithVar(0).not(), d);
 	}
-	
+
 	@Test
-	public void testImp(){
+	public void testImp() {
 		final Term x1 = script.variable("x1", bool);
 		final Term x2 = script.variable("x2", bool);
 		final Term t = script.term("=>", x1, x2);
@@ -91,10 +90,10 @@ public class BddBuilderTest {
 		final List<Term> atoms = ca.getTerms(t);
 		final BddBuilder bb = new BddBuilder();
 		final BDD d = bb.buildBDD(t, atoms);
-		
+
 		final BDDFactory factory = BDDFactory.init("java", atoms.size(), atoms.size(), false);
 		factory.setVarNum(atoms.size());
 		Assert.assertEquals(factory.ithVar(0).imp(factory.ithVar(1)), d);
 	}
-	
+
 }
