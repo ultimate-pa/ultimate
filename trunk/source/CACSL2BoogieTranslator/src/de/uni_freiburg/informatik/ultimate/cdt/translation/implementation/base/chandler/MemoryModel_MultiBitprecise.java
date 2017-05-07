416e640803e389d841fd8b303af7ca54a57a466b
Matthias <heizmann@informatik.uni-freiburg.de>
2017-05-07 20:47:27 +0200

    Support HoenickeLindenmannOriginal also for floats

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/MemoryModel_MultiBitprecise.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/MemoryModel_MultiBitprecise.java
@@ -86,10 +86,6 @@ public class MemoryModel_MultiBitprecise extends AMemoryModel {
 
 	@Override
 	public String getProcedureSuffix(final CPrimitives primitive) {
-		if (primitive.isFloatingtype()) {
-			throw new UnsupportedOperationException("Floating types are not yet supported in "
-					+ this.getClass().getSimpleName());
-		}
 		return getDataHeapArray(primitive).getName();
 	}
 
