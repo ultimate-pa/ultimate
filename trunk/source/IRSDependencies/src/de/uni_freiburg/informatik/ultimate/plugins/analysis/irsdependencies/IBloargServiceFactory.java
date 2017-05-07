75364f4ccdb9552629f62ea849fb2f95ff3cf8a7
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-05-07 11:42:25 +0200

    remove example for julian

--- trunk/source/IRSDependencies/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/irsdependencies/IBloargServiceFactory.java
+++
@@ -1,8 +0,0 @@
-package de.uni_freiburg.informatik.ultimate.plugins.analysis.irsdependencies;
-
-import de.uni_freiburg.informatik.ultimate.core.model.IServiceFactory;
-import de.uni_freiburg.informatik.ultimate.plugins.analysis.irsdependencies.BloargServiceFactory.BloargService;
-
-public interface IBloargServiceFactory extends IServiceFactory<BloargService> {
-
-}
