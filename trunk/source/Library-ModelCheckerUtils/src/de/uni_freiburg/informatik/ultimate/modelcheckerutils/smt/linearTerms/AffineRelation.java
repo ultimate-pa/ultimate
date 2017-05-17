ultimate-pa/ultimate

commit 32d85a3c4cf615b0c3d5072eb941f50877fb247f

close #152 -- use SmtSortUtils throughout Ultimate

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/linearTerms/AffineRelation.java
@@ -37,6 +37,7 @@ import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.Util;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtSortUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.linearTerms.BinaryRelation.NoRelationOfThisKindException;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.linearTerms.BinaryRelation.RelationSymbol;
@@ -284,7 +285,7 @@ public class AffineRelation {
 				// do nothing
 			} else {
 				final Rational newCoeff = entry.getValue().div(termsCoeff);
-				if (newCoeff.isIntegral() || "Real".equals(mAffineTerm.getSort().getName())) {
+				if (newCoeff.isIntegral() || SmtSortUtils.isIntSort(mAffineTerm.getSort())) {
 					final Rational negated = newCoeff.negate();
 					rhsSummands.add(product(script, negated, entry.getKey()));
 				} else {
@@ -301,7 +302,7 @@ public class AffineRelation {
 			}
 			final Rational newConstant = mAffineTerm.getConstant().div(termsCoeff);
 			if (newConstant.isIntegral() && newConstant.isRational()
-					|| "Real".equals(mAffineTerm.getSort().getName())) {
+					|| SmtSortUtils.isRealSort(mAffineTerm.getSort())) {
 				final Rational negated = newConstant.negate();
 				rhsSummands.add(SmtUtils.rational2Term(script, negated, mAffineTerm.getSort()));
 			} else {

commit 01488a3b92a3aa9e8b45137c7c51882fd40b5ad6

fix wrong conversion introduced by 32d85a3c4cf615b0c3d5072eb941f50877fb247f

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/linearTerms/AffineRelation.java
@@ -97,8 +97,8 @@ public class AffineRelation {
 
 		final Term lhs = bnr.getLhs();
 		final Term rhs = bnr.getRhs();
-		final AffineTerm affineLhs = (AffineTerm) (new AffineTermTransformer(script)).transform(lhs);
-		final AffineTerm affineRhs = (AffineTerm) (new AffineTermTransformer(script)).transform(rhs);
+		final AffineTerm affineLhs = (AffineTerm) new AffineTermTransformer(script).transform(lhs);
+		final AffineTerm affineRhs = (AffineTerm) new AffineTermTransformer(script).transform(rhs);
 		if (affineLhs.isErrorTerm() || affineRhs.isErrorTerm()) {
 			throw new NotAffineException("Relation is not affine");
 		}
@@ -285,7 +285,7 @@ public class AffineRelation {
 				// do nothing
 			} else {
 				final Rational newCoeff = entry.getValue().div(termsCoeff);
-				if (newCoeff.isIntegral() || SmtSortUtils.isIntSort(mAffineTerm.getSort())) {
+				if (newCoeff.isIntegral() || SmtSortUtils.isRealSort(mAffineTerm.getSort())) {
 					final Rational negated = newCoeff.negate();
 					rhsSummands.add(product(script, negated, entry.getKey()));
 				} else {