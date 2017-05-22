c1089d0ca4f92bb10048d0871656d3e3380c9c71
schillic <schillic@informatik.uni-freiburg.de>
2017-05-22 16:53:25 +0200

    #184 - added question as TODO

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/predicates/IterativePredicateTransformer.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/predicates/IterativePredicateTransformer.java
@@ -61,8 +61,10 @@ import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPre
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.PredicateTransformer;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.TermDomainOperationProvider;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.Activator;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.FlowSensitiveFaultLocalizer;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.IterativePredicateTransformer.TraceInterpolationException.Reason;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.NestedFormulas;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.TraceCheckerSpWp;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.TracePredicates;
 
 /**
@@ -85,6 +87,12 @@ public class IterativePredicateTransformer {
 	private final IPredicate mPrecondition;
 	private final IPredicate mPostcondition;
 	protected final SortedMap<Integer, IPredicate> mPendingContexts;
+	/**
+	 * TODO 2017-05-22 Christian&Matthias: Not sure about this field. There are two users, {@link TraceCheckerSpWp} and
+	 *      {@link FlowSensitiveFaultLocalizer}. The further passe {@code null}, which should, if this field is read,
+	 *      result in a {@code NullPointerException}. The latter passes a predicate for {@code true}, which is counter-
+	 *      intuitive.
+	 */
 	private final IPredicate mFalsePredicate;
 
 	private final IIcfgSymbolTable mSymbolTable;
@@ -104,7 +112,7 @@ public class IterativePredicateTransformer {
 		mXnfConversionTechnique = xnfConversionTechnique;
 		mMgdScript = mgdScript;
 		mModifiedGlobals = modifiableGlobalsTable;
-		mPredicateTransformer = new PredicateTransformer<Term, IPredicate, TransFormula>(mgdScript, new TermDomainOperationProvider(mServices, mMgdScript));
+		mPredicateTransformer = new PredicateTransformer<>(mgdScript, new TermDomainOperationProvider(mServices, mMgdScript));
 		mPredicateFactory = predicateFactory;
 		mTrace = trace;
 		mPrecondition = precondition;
