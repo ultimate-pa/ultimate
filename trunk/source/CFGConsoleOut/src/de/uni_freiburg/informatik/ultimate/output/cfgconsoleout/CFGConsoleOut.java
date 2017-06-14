0a5b705bbb7cf9a757c848cadc1b84b9ffb8c3f4
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-06-14 14:34:57 +0200

    reappropriate CFGConsoleOut to print IIcfg to console

--- trunk/source/CFGConsoleOut/src/de/uni_freiburg/informatik/ultimate/output/cfgconsoleout/CFGConsoleOut.java
+++ trunk/source/CFGConsoleOut/src/de/uni_freiburg/informatik/ultimate/output/cfgconsoleout/CFGConsoleOut.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE CFGConsoleOut plug-in.
- * 
+ *
  * The ULTIMATE CFGConsoleOut plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE CFGConsoleOut plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE CFGConsoleOut plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE CFGConsoleOut plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE CFGConsoleOut plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE CFGConsoleOut plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.output.cfgconsoleout;
@@ -36,6 +36,11 @@ import de.uni_freiburg.informatik.ultimate.core.model.preferences.IPreferenceIni
 import de.uni_freiburg.informatik.ultimate.core.model.services.IToolchainStorage;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
 
+/**
+ *
+ * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ *
+ */
 public class CFGConsoleOut implements IOutput {
 
 	private IObserver mObserver;
@@ -43,7 +48,7 @@ public class CFGConsoleOut implements IOutput {
 
 	@Override
 	public List<String> getDesiredToolIds() {
-		return null;
+		return Collections.emptyList();
 	}
 
 	@Override
@@ -62,17 +67,17 @@ public class CFGConsoleOut implements IOutput {
 	}
 
 	@Override
-	public void setInputDefinition(ModelType graphType) {
-
+	public void setInputDefinition(final ModelType graphType) {
+		// not needed
 	}
 
 	@Override
-	public void setToolchainStorage(IToolchainStorage storage) {
-
+	public void setToolchainStorage(final IToolchainStorage storage) {
+		// not needed
 	}
 
 	@Override
-	public void setServices(IUltimateServiceProvider services) {
+	public void setServices(final IUltimateServiceProvider services) {
 		mServices = services;
 	}
 
@@ -83,7 +88,7 @@ public class CFGConsoleOut implements IOutput {
 
 	@Override
 	public void finish() {
-
+		// not needed
 	}
 
 	@Override
