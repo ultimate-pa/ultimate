b0d1ba4b1a75ba505f4c83084c64bac383cbfd78
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-06-21 07:52:40 +0200

    fix Linbrary-srParse and Linbrary-srParseTest errors after merge

--- trunk/source/Library-srParseTest/src/de/uni_freiburg/informatik/ultimate/srParse/test/BoogieRequirementsParserTest.java
+++ trunk/source/Library-srParseTest/src/de/uni_freiburg/informatik/ultimate/srParse/test/BoogieRequirementsParserTest.java
@@ -6,142 +6,116 @@ import java.util.Vector;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java_cup.runtime.Symbol;
-import pea.BooleanDecision;
-import pea.CDD;
-import srParse.ReqLexer;
-import srParse.ReqParser;
-import srParse.pattern.PatternType;
+import com.github.jhoenicke.javacup.runtime.Symbol;
+
+import de.uni_freiburg.informatik.ultimate.lib.pea.BooleanDecision;
+import de.uni_freiburg.informatik.ultimate.lib.pea.CDD;
+import de.uni_freiburg.informatik.ultimate.lib.srparse.ReqLexer;
+import de.uni_freiburg.informatik.ultimate.lib.srparse.ReqParser;
+import de.uni_freiburg.informatik.ultimate.lib.srparse.pattern.PatternType;
 
 public class BoogieRequirementsParserTest {
-	
+
 	/**
 	 * Use to supply a string (instead of file) to parser.
+	 * 
 	 * @param reqFileName
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 * 
-	*/
-    private PatternType[] GenPatterns(String testInput) throws Exception {
-		StringReader sr = new StringReader(testInput);
-		ReqLexer lexer = new ReqLexer(sr);
-		ReqParser parser = new ReqParser(lexer);
-		
-		Symbol goal = parser.parse();
-		PatternType[] patterns = (PatternType[]) goal.value;
-		
+	 */
+	private PatternType[] GenPatterns(final String testInput) throws Exception {
+		final StringReader sr = new StringReader(testInput);
+		final ReqLexer lexer = new ReqLexer(sr);
+		final ReqParser parser = new ReqParser(lexer);
+
+		final Symbol goal = parser.parse();
+		final PatternType[] patterns = (PatternType[]) goal.value;
+
 		return patterns;
-    }
-	
-    
-    /**
-     * Test if single Requirement with comparison and . operator is korrectly parsed
-     * @throws Exception
-     */
+	}
+
+	/**
+	 * Test if single Requirement with comparison and . operator is korrectly parsed
+	 * 
+	 * @throws Exception
+	 */
 	@Test
-	public void TestGlobalInvariantBoogie() throws Exception{
-		String testString = "Globally, it is always the case that \"ABC_curr.I >=  BCD_MAX\" holds.";
-		PatternType[] parsedPatterns = GenPatterns(testString);
-		
+	public void TestGlobalInvariantBoogie() throws Exception {
+		final String testString = "Globally, it is always the case that \"ABC_curr.I >=  BCD_MAX\" holds.";
+		final PatternType[] parsedPatterns = GenPatterns(testString);
+
 		System.out.println(parsedPatterns[0].toString());
-		Assert.assertNotNull("Parser did not return anything!", 
-				parsedPatterns);
-		Assert.assertNotNull("Parser did not recognize Global.universal pattern", 
-				parsedPatterns[0]);
-		Vector<CDD> ccds = parsedPatterns[0].getCdds();
+		Assert.assertNotNull("Parser did not return anything!", parsedPatterns);
+		Assert.assertNotNull("Parser did not recognize Global.universal pattern", parsedPatterns[0]);
+		final Vector<CDD> ccds = parsedPatterns[0].getCdds();
 		Assert.assertEquals("ABC_curr.I >= BCD_MAX", ccds.get(0).toString());
 	}
-	
-    /**
-     * Test if single Requirement with comparison and . operator is korrectly parsed
-     * @throws Exception
-     */
+
+	/**
+	 * Test if single Requirement with comparison and . operator is korrectly parsed
+	 * 
+	 * @throws Exception
+	 */
 	@Test
-	public void TestBooleanLiterals() throws Exception{
-		String testString = "Globally, it is always the case that \"true == false\" holds.";
-		PatternType[] parsedPatterns = GenPatterns(testString);
-		
+	public void TestBooleanLiterals() throws Exception {
+		final String testString = "Globally, it is always the case that \"true == false\" holds.";
+		final PatternType[] parsedPatterns = GenPatterns(testString);
+
 		System.out.println(parsedPatterns[0].toString());
-		Assert.assertNotNull("Parser did not return anything!", 
-				parsedPatterns);
-		Assert.assertNotNull("Parser did not recognize Global.universal pattern", 
-				parsedPatterns[0]);
-		Vector<CDD> ccds = parsedPatterns[0].getCdds();
+		Assert.assertNotNull("Parser did not return anything!", parsedPatterns);
+		Assert.assertNotNull("Parser did not recognize Global.universal pattern", parsedPatterns[0]);
+		final Vector<CDD> ccds = parsedPatterns[0].getCdds();
 		Assert.assertEquals("true == false", ccds.get(0).toString());
 	}
-	
+
 	/**
-	 * Test if single requirement with comparison, logical operator and calculation is parsed correctly 
+	 * Test if single requirement with comparison, logical operator and calculation is parsed correctly
+	 * 
 	 * @throws Exception
 	 */
 	@Test
-	public void TestGlobalInvariantBoogieComplexExpression() throws Exception{
-		String testString = "Globally, it is always the case that \"(ABC_curr >=  BCD_MAX &&"
+	public void TestGlobalInvariantBoogieComplexExpression() throws Exception {
+		final String testString = "Globally, it is always the case that \"(ABC_curr >=  BCD_MAX &&"
 				+ " diddlidu + 3 == A_bli -3) || a \" holds";
-		PatternType[] parsedPatterns = GenPatterns(testString);
-		
+		final PatternType[] parsedPatterns = GenPatterns(testString);
+
 		System.out.println(parsedPatterns[0].toString());
-		Assert.assertNotNull("Parser did not return anything!", 
-				parsedPatterns);
-		Assert.assertNotNull("Parser did not recognize Global.universal pattern", 
-				parsedPatterns[0]);
-		Vector<CDD> ccds = parsedPatterns[0].getCdds();
-		Assert.assertEquals("(a ∨ ABC_curr >= BCD_MAX ∧ diddlidu + 3 == A_bli - 3)"
-				, ccds.get(0).toString(true));
+		Assert.assertNotNull("Parser did not return anything!", parsedPatterns);
+		Assert.assertNotNull("Parser did not recognize Global.universal pattern", parsedPatterns[0]);
+		final Vector<CDD> ccds = parsedPatterns[0].getCdds();
+		Assert.assertEquals("(a ∨ ABC_curr >= BCD_MAX ∧ diddlidu + 3 == A_bli - 3)", ccds.get(0).toString(true));
 	}
-	
+
 	@Test
-	public void TestListOfRequirements() throws Exception{
-		String testString = 
-				"Globally, it is always the case that \"ABC_curr.I >=  BCD_MAX\" holds.\n"+
-				"Globally, it is always the case that \"EFG_min >=  X + 3\" holds.\n";
-		PatternType[] parsedPatterns = GenPatterns(testString);
-		
+	public void TestListOfRequirements() throws Exception {
+		final String testString = "Globally, it is always the case that \"ABC_curr.I >=  BCD_MAX\" holds.\n"
+				+ "Globally, it is always the case that \"EFG_min >=  X + 3\" holds.\n";
+		final PatternType[] parsedPatterns = GenPatterns(testString);
+
 		System.out.println(parsedPatterns[0].toString());
-		Assert.assertNotNull("Parser did not return anything!", 
-				parsedPatterns);
-		Assert.assertTrue("Parser did not recognize all Pattern  (2)", 
-				parsedPatterns.length == 2);
-		Vector<CDD> ccds = parsedPatterns[0].getCdds();
+		Assert.assertNotNull("Parser did not return anything!", parsedPatterns);
+		Assert.assertTrue("Parser did not recognize all Pattern  (2)", parsedPatterns.length == 2);
+		final Vector<CDD> ccds = parsedPatterns[0].getCdds();
 	}
-	
+
 	/**
 	 * Test if old behaviour is still correctly working
+	 * 
 	 * @throws Exception
 	 */
-	//@Test
-	public void TestOldBehaviour() throws Exception{
-		String testString = "Globally, it is always the case that \"a\" holds";
-		PatternType[] parsedPatterns = GenPatterns(testString);
-		
+	// @Test
+	public void TestOldBehaviour() throws Exception {
+		final String testString = "Globally, it is always the case that \"a\" holds";
+		final PatternType[] parsedPatterns = GenPatterns(testString);
+
 		System.out.println(parsedPatterns[0].toString());
-		Assert.assertNotNull("Parser did not return anything!", 
-				parsedPatterns);
-		Assert.assertNotNull("Parser did not recognize Global.universal pattern", 
-				parsedPatterns[0]);
-		Vector<CDD> ccds = parsedPatterns[0].getCdds();
+		Assert.assertNotNull("Parser did not return anything!", parsedPatterns);
+		Assert.assertNotNull("Parser did not recognize Global.universal pattern", parsedPatterns[0]);
+		final Vector<CDD> ccds = parsedPatterns[0].getCdds();
 		Assert.assertTrue("Parser did not work on old Requirements with only ap-names",
 				ccds.get(0).decision instanceof BooleanDecision);
 		System.out.println(ccds.get(0).toString());
 	}
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
