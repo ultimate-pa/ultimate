4306e7f5c8ea06f13f07a7e0707caaf9f22362e9
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-06-28 15:02:23 +0200

    remove duplicate test suite

--- trunk/source/UltimateRegressionTest/src/de/uni_freiburg/informatik/ultimate/regressiontest/generic/AbsIntRegressionTestSuite.java
+++ trunk/source/UltimateRegressionTest/src/de/uni_freiburg/informatik/ultimate/regressiontest/generic/AbsIntRegressionTestSuite.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2017 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2017 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Regression Test Library.
- * 
+ *
  * The ULTIMATE Regression Test Library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Regression Test Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Regression Test Library. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Regression Test Library, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
@@ -33,11 +33,10 @@ import de.uni_freiburg.informatik.ultimate.test.decider.OverapproximatingSafetyC
 import de.uni_freiburg.informatik.ultimate.test.util.TestUtil;
 
 /**
- * 
  * This test suite tests the AbstractInterpretation plugin of Ultimate.
- * 
+ *
  * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
- * 
+ *
  */
 public class AbsIntRegressionTestSuite extends AbstractRegressionTestSuite {
 
@@ -47,6 +46,9 @@ public class AbsIntRegressionTestSuite extends AbstractRegressionTestSuite {
 		super();
 		mTimeout = DEFAULT_TIMEOUT;
 		mRootFolder = TestUtil.getPathFromTrunk("examples/programs/abstractInterpretation/");
+
+		// exclude paths that match the following regex
+		mExcludeFilterRegex = ".*(CToBoogieTranslation|Backtranslation|lassos|termination|SignedIntegerOverflow).*";
 	}
 
 	@Override
