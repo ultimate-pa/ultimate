81cd3bd22233a59f78e4bdb0412e573a3e9b8350
Daniel Dietsch <daniel.dietsch@gmail.con>
2017-07-12 16:00:02 +0200

    close #189 -- fix Overapprox annotation being unmergeable

--- trunk/source/Library-UltimateCore/src/de/uni_freiburg/informatik/ultimate/core/lib/models/annotation/Overapprox.java
+++ trunk/source/Library-UltimateCore/src/de/uni_freiburg/informatik/ultimate/core/lib/models/annotation/Overapprox.java
@@ -2,22 +2,22 @@
  * Copyright (C) 2013-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Core.
- * 
+ *
  * The ULTIMATE Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Core. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
@@ -28,7 +28,9 @@
 package de.uni_freiburg.informatik.ultimate.core.lib.models.annotation;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.core.model.models.IElement;
@@ -41,7 +43,7 @@ import de.uni_freiburg.informatik.ultimate.core.model.models.annotation.Visualiz
  * Annotation for transition (e.g., CodeBlock) that indicates that it was not build by a semantics preserving
  * translation but by an overapproximation. This allows model checkers to report <i>unknown</i> instead of <i>unsafe</i>
  * for traces that contain elements with this annotation.
- * 
+ *
  * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  */
@@ -82,5 +84,24 @@ public class Overapprox extends ModernAnnotations {
 
 	public static Overapprox getAnnotation(final IElement node) {
 		return ModelUtils.getAnnotation(node, Overapprox.class.getName(), a -> (Overapprox) a);
+	}
+
+	@Override
+	public IAnnotations merge(final IAnnotations other) {
+		if (other == this || other == null) {
+			return this;
+		}
+		if (!(other instanceof Overapprox)) {
+			return super.merge(other);
+		}
+		final Overapprox otheroverapprox = (Overapprox) other;
+		final Map<String, ILocation> newReasons = new HashMap<>(mReason2Loc);
+		for (final Entry<String, ILocation> entry : otheroverapprox.mReason2Loc.entrySet()) {
+			final ILocation res = newReasons.putIfAbsent(entry.getKey(), entry.getValue());
+			if (res != null) {
+				// TODO: This means we "forget" some location
+			}
+		}
+		return new Overapprox(newReasons);
 	}
 }
