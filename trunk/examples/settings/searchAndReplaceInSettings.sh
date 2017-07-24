688fac6ceedba19ceaabe132146f22bf63438352
Matthias <heizmann@informatik.uni-freiburg.de>
2017-07-24 15:34:44 +0200

    more documentation for settings adaption script

--- trunk/examples/settings/searchAndReplaceInSettings.sh
+++ trunk/examples/settings/searchAndReplaceInSettings.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 # Script for replacing strings in all our settings files.
 # You might want so use this after you updated preferences.
-# This script might have some problems with special characters.
+#
+# Warning: This script might have some problems with special characters.
+#
+# Usage:
+# Go to the trunk/examples/settings folder
+# Call the following command. Don't forget the quote symbols.
+# Use the whole settings string that usually starts with /instance.
+# ./searchAndReplaceInSettings.sh "OLDSTRING" "NEWSTRING"
+#
+# The replacement will be done in all subfolders of trunk/examples/settings
+# and in /trunk/examples/source/WebUltimateBridge/src/de/uni_freiburg/informatik/ultimate/webbridge/resources/settings/
+#
+# Check if your replacement was succesfull using
+# grep -ir SOME_KEYWORD .
+#
 # Author: Matthias Heizmann
 # Date: 2015-02-11
 
