d68c459599a8336e5326a9c7b8b6d7b95a26f046
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2017-08-11 13:34:36 +0200

    added licence & header

--- trunk/source/Library-UltimateUtilTest/src/de/uni_freiburg/informatik/ultimate/util/ThreeValuedEquivalenceRelationTest.java
+++ trunk/source/Library-UltimateUtilTest/src/de/uni_freiburg/informatik/ultimate/util/ThreeValuedEquivalenceRelationTest.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (C) 2017 Alexander Nutz (nutz@informatik.uni-freiburg.de)
+ * Copyright (C) 2017 University of Freiburg
+ * 
+ * This file is part of the ULTIMATE Util Library.
+ * 
+ * The ULTIMATE Util Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The ULTIMATE Util Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Util Library. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Util Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
+ * containing parts covered by the terms of the Eclipse Public License, the 
+ * licensors of the ULTIMATE Util Library grant you additional permission 
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.util;
 
 import static org.junit.Assert.assertFalse;
@@ -8,6 +34,12 @@ import org.junit.Test;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.Equality;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.ThreeValuedEquivalenceRelation;
 
+
+/**
+ * 
+ * @author Alexander Nutz (nutz@informatik.uni-freiburg.de)
+ *
+ */
 public class ThreeValuedEquivalenceRelationTest {
 
 	@Test
@@ -31,8 +63,6 @@ public class ThreeValuedEquivalenceRelationTest {
 		assertTrue(tver1.getEquality("z", "x") == Equality.NOT_EQUAL);
 		assertTrue(tver1.getEquality("y", "z") == Equality.NOT_EQUAL);
 		assertTrue(tver1.getEquality("z", "y") == Equality.NOT_EQUAL);
-		
-		
 	
 		tver1.reportEquality("a", "b");
 
