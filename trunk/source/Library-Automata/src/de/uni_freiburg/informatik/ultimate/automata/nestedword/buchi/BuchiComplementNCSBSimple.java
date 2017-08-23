de2ce8c934a9e7c9817d90273d4374c612e8c47c
LiYong <liyong@ios.ac.cn>
2017-08-23 10:15:03 +0800

    add author name

--- trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/nestedword/buchi/BuchiComplementNCSBSimple.java
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/nestedword/buchi/BuchiComplementNCSBSimple.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2017 Yong Li (liyong@ios.ac.cn)
  * Copyright (C) 2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2009-2015 University of Freiburg
  * 
