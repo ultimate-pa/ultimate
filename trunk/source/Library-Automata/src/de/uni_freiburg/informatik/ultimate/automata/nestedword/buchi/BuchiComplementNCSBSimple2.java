7e5acd925f0a29e9e65c4baeda0534b0d85a5ad0
LiYong <liyong@ios.ac.cn>
2017-08-29 14:11:25 +0800

    add support for nested word in INTSET implementation

---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/nestedword/buchi/BuchiComplementNCSBSimple2.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2017 Yong Li (liyong@ios.ac.cn)
+ * Copyright (C) 2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * Copyright (C) 2009-2015 University of Freiburg
+ * 
+ * This file is part of the ULTIMATE Automata Library.
+ * 
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.nestedword.buchi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryException;
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
+import de.uni_freiburg.informatik.ultimate.automata.AutomataOperationCanceledException;
+import de.uni_freiburg.informatik.ultimate.automata.AutomataOperationStatistics;
+import de.uni_freiburg.informatik.ultimate.automata.AutomatonDefinitionPrinter;
+import de.uni_freiburg.informatik.ultimate.automata.StatisticsType;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.INwaOutgoingLetterAndTransitionProvider;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomataUtils;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NwaOutgoingLetterAndTransitionAdapter;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.UnaryNwaOperation;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.reachablestates.NestedWordAutomatonReachableStates;
+import de.uni_freiburg.informatik.ultimate.automata.statefactory.IBuchiComplementNcsbSimpleStateFactory;
+import de.uni_freiburg.informatik.ultimate.automata.statefactory.IBuchiComplementNcsbStateFactory;
+import de.uni_freiburg.informatik.ultimate.automata.statefactory.IStateFactory;
+
+/**
+ * Buchi Complementation based on the algorithm proposed by Frantisek Blahoudek and Jan Stejcek. This complementation is
+ * only sound for a special class of automata whose working title is TABA (termination analysis Büchi automata).
+ * But with simpler implementation in package optncsb
+ * 
+ * @author Yong Li (liyong@ios.ac.cn)
+ * @param <LETTER>
+ *            letter type
+ * @param <STATE>
+ *            state type
+ */
+public final class BuchiComplementNCSBSimple2<LETTER, STATE> extends UnaryNwaOperation<LETTER, STATE, IStateFactory<STATE>> {
+	private final INwaOutgoingLetterAndTransitionProvider<LETTER, STATE> mOperand;
+	private final NestedWordAutomatonReachableStates<LETTER, STATE> mResult;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param services
+	 *            Ultimate services
+	 * @param stateFactory
+	 *            state factory
+	 * @param operand
+	 *            operand
+	 * @throws AutomataOperationCanceledException
+	 *             if operation was canceled
+	 */
+	public BuchiComplementNCSBSimple2(final AutomataLibraryServices services,
+			final IBuchiComplementNcsbStateFactory<STATE> stateFactory,
+			final INwaOutgoingLetterAndTransitionProvider<LETTER, STATE> operand) throws AutomataOperationCanceledException {
+		super(services);
+		mOperand = operand;
+
+		if (mLogger.isInfoEnabled()) {
+			mLogger.info(startMessage());
+		}
+		final BuchiComplementNCSBSimpleNwa2<LETTER, STATE> onDemandComplement =
+				new BuchiComplementNCSBSimpleNwa2<>(mServices, stateFactory, operand);
+		final NwaOutgoingLetterAndTransitionAdapter<LETTER, STATE> complemented =
+				new NwaOutgoingLetterAndTransitionAdapter<>(onDemandComplement);
+		mResult = new NestedWordAutomatonReachableStates<>(mServices, complemented);
+		if (mLogger.isInfoEnabled()) {
+			mLogger.info(exitMessage());
+		}
+	}
+
+	@Override
+	public String exitMessage() {
+		return "Finished " + getOperationName() + ". Operand " + mOperand.sizeInformation() + " Result "
+				+ mResult.sizeInformation();
+	}
+
+	@Override
+	public boolean checkResult(final IStateFactory<STATE> stateFactory) throws AutomataLibraryException {
+		if (mLogger.isInfoEnabled()) {
+			mLogger.info("Start testing correctness of " + getOperationName());
+		}
+
+		final boolean underApproximationOfComplement = false;
+		final List<NestedLassoWord<LETTER>> lassoWords = new ArrayList<>();
+		final BuchiIsEmpty<LETTER, STATE> operandEmptiness = new BuchiIsEmpty<>(mServices, mOperand);
+		final boolean operandEmpty = operandEmptiness.getResult();
+		if (!operandEmpty) {
+			lassoWords.add(operandEmptiness.getAcceptingNestedLassoRun().getNestedLassoWord());
+		}
+		final BuchiIsEmpty<LETTER, STATE> resultEmptiness = new BuchiIsEmpty<>(mServices, mResult);
+		final boolean resultEmpty = resultEmptiness.getResult();
+		if (!resultEmpty) {
+			lassoWords.add(resultEmptiness.getAcceptingNestedLassoRun().getNestedLassoWord());
+		}
+		boolean correct = true;
+		correct &= !(operandEmpty && resultEmpty);
+		assert correct;
+		/*
+		lassoWords.add(ResultChecker.getRandomNestedLassoWord(mResult, mResult.size()));
+		lassoWords.add(ResultChecker.getRandomNestedLassoWord(mResult, mResult.size()));
+		*/
+		for (int i = 0; i < 11; ++i) {
+			lassoWords.add(NestedWordAutomataUtils.getRandomNestedLassoWord(mResult, 1, i));
+			lassoWords.add(NestedWordAutomataUtils.getRandomNestedLassoWord(mResult, 2, i));
+		}
+		lassoWords.addAll((new LassoExtractor<>(mServices, mOperand)).getResult());
+		lassoWords.addAll((new LassoExtractor<>(mServices, mResult)).getResult());
+
+		for (final NestedLassoWord<LETTER> nlw : lassoWords) {
+			boolean thistime = checkAcceptance(nlw, mOperand, underApproximationOfComplement);
+			if (!thistime) {
+				thistime = checkAcceptance(nlw, mOperand, underApproximationOfComplement);
+			}
+			correct &= thistime;
+			assert correct;
+		}
+
+		if (!correct) {
+			AutomatonDefinitionPrinter.writeToFileIfPreferred(mServices, getOperationName() + "Failed",
+					"language is different", mOperand, mResult);
+		}
+		if (mLogger.isInfoEnabled()) {
+			mLogger.info("Finished testing correctness of " + getOperationName());
+		}
+		return correct;
+	}
+
+	private boolean checkAcceptance(final NestedLassoWord<LETTER> nlw,
+			final INwaOutgoingLetterAndTransitionProvider<LETTER, STATE> operand, final boolean underApproximationOfComplement)
+			throws AutomataLibraryException {
+		final boolean op = (new BuchiAccepts<>(mServices, operand, nlw)).getResult();
+		final boolean res = (new BuchiAccepts<>(mServices, mResult, nlw)).getResult();
+		boolean correct;
+		if (underApproximationOfComplement) {
+			correct = !res || op;
+		} else {
+			correct = op ^ res;
+		}
+		return correct;
+	}
+
+	@Override
+	protected INwaOutgoingLetterAndTransitionProvider<LETTER, STATE> getOperand() {
+		return mOperand;
+	}
+
+	@Override
+	public NestedWordAutomatonReachableStates<LETTER, STATE> getResult() {
+		return mResult;
+	}
+	
+	@Override
+	public AutomataOperationStatistics getAutomataOperationStatistics() {
+		final AutomataOperationStatistics result = new AutomataOperationStatistics();
+
+		final int inputSize = getOperand().size();
+		final int outputSize = getResult().size();
+
+		result.addKeyValuePair(StatisticsType.STATES_INPUT, inputSize);
+		result.addKeyValuePair(StatisticsType.STATES_OUTPUT, outputSize);
+
+		return result;
+	}
+}
