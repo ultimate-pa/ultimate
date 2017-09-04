23eb6edc2bc4904cb56d332c68ed3a8f1fc6a8b1
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2017-09-04 10:16:50 +0200

    termcomp2017 release

--- releaseScripts/2017termcomp/createZip.sh
+++ releaseScripts/2017termcomp/createZip.sh
@@ -6,8 +6,7 @@ cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E4/linux/gtk/x86_
 cp LICENSE* StarExecArchive/Ultimate/
 cp starexec_description.txt StarExecArchive/
 cp ../../trunk/examples/toolchains/AutomizerAndBuchiAutomizerCInlineWithBlockEncoding.xml StarExecArchive/
-cp starexec_run_default StarExecArchive/bin/
-cp starexec_run_NontrivialArrayWrites StarExecArchive/bin/
+cp starexec_run_* StarExecArchive/bin/
 cp Ultimate.ini StarExecArchive/Ultimate/
 cp ../../trunk/examples/settings/buchiAutomizer/termcomp2017.epf StarExecArchive/
 cp ../../trunk/examples/settings/buchiAutomizer/termcomp2017-NontrivialArrayWrites.epf StarExecArchive/
