94c3e1a100729c5995304dac5f1a68c6f48e0b33
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-09-26 13:10:05 +0200

    additional script for svcomp18 deployment

--- releaseScripts/default/makeFresh.sh
+++ releaseScripts/default/makeFresh.sh
@@ -10,41 +10,13 @@ function exitOnFail {
     return $status
 }
 
-
-DEPLOY_SERVER=sotec.informatik.uni-freiburg.de
-DEPLOY_DIR=/export/server/httpd/ultimate/downloads/svcomp2017
-TESTFILE=caniwrite
-FILE="$DEPLOY_DIR"/"$TESTFILE"
-
-if [ -z "$1" ]
-then
-	CURRENTUSER=$(whoami)
-else
-	CURRENTUSER="$1"
-fi
-
-#echo "Enter password for user ${CURRENTUSER} on ${DEPLOY_SERVER}:" 
-#read -s -p "Password: " SSHPASS
-#echo 
-#export SSHPASS
-#sshpass -e ssh -oHostKeyAlgorithms=+ssh-dss $CURRENTUSER@$DEPLOY_SERVER "( test -e ${FILE} || touch ${FILE} ) && test -w ${FILE} && /usr/bin/rm ${FILE}"
-#if [ ! $? -eq 0 ]; then 
-#	echo "Something is wrong with your write permissions to ${DEPLOY_DIR}, fix it before continuing"
-#	exit 1
-#fi
-
 pushd ../../trunk/source/BA_MavenParentUltimate/ > /dev/null
 exitOnFail mvn clean install -Pmaterialize
 popd > /dev/null
 
 # createZip <toolname> <targetarch> <reachtc> <termtc> <witnessvaltc> <memsafetytc> <ltlc>
-./createZip.sh Taipan linux AutomizerCInline_WitnessPrinter.xml NONE AutomizerCInline.xml AutomizerCInline_WitnessPrinter.xml NONE
-./createZip.sh Automizer linux AutomizerC_WitnessPrinter.xml BuchiAutomizerCInline.xml AutomizerC.xml AutomizerC_WitnessPrinter.xml LTLAutomizerC.xml
-./createZip.sh Kojak linux KojakC_WitnessPrinter.xml NONE NONE KojakC_WitnessPrinter.xml NONE
+exitOnFail ./createZip.sh Taipan linux AutomizerCInline_WitnessPrinter.xml NONE AutomizerCInline.xml AutomizerCInline_WitnessPrinter.xml NONE
+exitOnFail ./createZip.sh Automizer linux AutomizerC_WitnessPrinter.xml BuchiAutomizerCInline.xml AutomizerC.xml AutomizerC_WitnessPrinter.xml LTLAutomizerC.xml
+exitOnFail ./createZip.sh Kojak linux KojakC_WitnessPrinter.xml NONE NONE KojakC_WitnessPrinter.xml NONE
 
-# uncomment this after the final release 
-#for z in *.zip; do mv "$z" "${z%.zip}-post-final.zip"; done
-
-#rsync -P --rsh="sshpass -e ssh -l me8 -oHostKeyAlgorithms=+ssh-dss" *.zip $CURRENTUSER@${DEPLOY_SERVER}:${DEPLOY_DIR}/. 
-#rm *.zip 
-
+return 0
