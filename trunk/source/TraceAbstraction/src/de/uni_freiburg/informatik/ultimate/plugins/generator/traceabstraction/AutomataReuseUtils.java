5b73cdeaa872e322aa4b05af882b78996724b57b
Bat-Chen <batcheni89@gmail.com>
2017-10-15 09:28:16 +0300

    Reading FH automata from ats files: Add parsing of states as predicates
    *parsePredicate now used from within interpretAutomata: if parsing is unsuccessful a debug predicate is used as before.
    *A map from predicate to string was added (mapStateToString) to avoid a bug caused by using the toString method on predicates (the result is not always a state of the old automaton, due to the serial number).
    *removeSerialNumber was implemented (prefix before first "#" symbol is removed)
    
    Signed-off-by: Bat-Chen <batcheni89@gmail.com>

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/AutomataReuseUtils.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/AutomataReuseUtils.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.INestedWordAutomaton;
@@ -42,16 +43,11 @@ public class AutomataReuseUtils {
 			final PredicateFactoryForInterpolantAutomata predicateFactoryInterpolantAutomata,
 			final IUltimateServiceProvider services, final PredicateFactory predicateFactory, final ILogger logger,
 			final CfgSmtToolkit csToolkit) {
-		
-		final PredicateParsingWrapperScript ppws = new PredicateParsingWrapperScript(csToolkit);
-
+	
 		final Boolean debugOn = true;
 		final List<INestedWordAutomaton<LETTER, IPredicate>> res = new ArrayList<INestedWordAutomaton<LETTER, IPredicate>>();
 
 		for (final NestedWordAutomaton<String, String> rawAutomatonFromFile : rawFloydHoareAutomataFromFile) {
-			final PredicateUnifier pu = new PredicateUnifier(services, csToolkit.getManagedScript(), predicateFactory,
-					csToolkit.getSymbolTable(), SimplificationTechnique.SIMPLIFY_DDA,
-					XnfConversionTechnique.BOTTOM_UP_WITH_LOCAL_SIMPLIFICATION);
 			
 			// Create map from strings to all equivalent "new" letters (abstraction letters)
 			final HashMap<String, Set<LETTER>> mapStringToLetter = new HashMap<String, Set<LETTER>>();
@@ -69,17 +65,31 @@ public class AutomataReuseUtils {
 					new AutomataLibraryServices(services), abstractionAlphabet, predicateFactoryInterpolantAutomata);
 			// Add states
 			final Set<String> statesOfRawAutomaton = rawAutomatonFromFile.getStates();
-			final HashMap<String, IPredicate> mapStringToFreshState = new HashMap<>();
+			final HashMap<String, IPredicate> mapStringToState = new HashMap<>();
+			final HashMap<IPredicate, String> mapStateToString = new HashMap<>();
+			int reusedStates = 0;
+			int removedStates = 0;
 			for (final String stringState : statesOfRawAutomaton) {
-				final IPredicate predicateState = getPredicateFromString(predicateFactory, stringState);
-				mapStringToFreshState.put(stringState, predicateState);
+				AtomicBoolean parsingResult = new AtomicBoolean(false);
+				final IPredicate predicateState = getPredicateFromString(predicateFactory, stringState, csToolkit, services, parsingResult, logger);
+				if (parsingResult.get()) {
+					reusedStates++;
+				} else {
+					removedStates++;
+				}
+				mapStringToState.put(stringState, predicateState);
+				mapStateToString.put(predicateState, stringState);
 				final boolean isInitial = rawAutomatonFromFile.isInitial(stringState);
 				final boolean isFinal = rawAutomatonFromFile.isFinal(stringState);
 				resAutomaton.addState(isInitial, isFinal, predicateState);
 			}
+			int totalStates = removedStates + reusedStates;
+			assert(totalStates==resAutomaton.size());
+			logger.info(
+					"Reusing " + reusedStates + "/" + totalStates + " states when constructing automaton from file.");
 			// Add transitions
-			addTransitionsFromRawAutomaton(resAutomaton, rawAutomatonFromFile, mapStringToLetter, mapStringToFreshState, 
-					debugOn, logger);
+			addTransitionsFromRawAutomaton(resAutomaton, rawAutomatonFromFile, mapStringToLetter, mapStringToState, 
+					mapStateToString, debugOn, logger);
 			// Add new automaton to list
 			res.add(resAutomaton);
 		}
@@ -87,8 +97,23 @@ public class AutomataReuseUtils {
 		return res;
 	}
 
-	private static final IPredicate getPredicateFromString(final PredicateFactory predicateFactory, final String str) {
-		return predicateFactory.newDebugPredicate(str);
+	private static final IPredicate getPredicateFromString(final PredicateFactory predicateFactory, final String str,
+			final CfgSmtToolkit csToolkit, final IUltimateServiceProvider services, AtomicBoolean parsingSuccesful,
+			ILogger logger) {
+		final PredicateParsingWrapperScript ppws = new PredicateParsingWrapperScript(csToolkit);
+		final PredicateUnifier pu = new PredicateUnifier(services, csToolkit.getManagedScript(), predicateFactory,
+				csToolkit.getSymbolTable(), SimplificationTechnique.SIMPLIFY_DDA,
+				XnfConversionTechnique.BOTTOM_UP_WITH_LOCAL_SIMPLIFICATION);
+		IPredicate res = null;
+		try {
+			res = parsePredicate(ppws, pu, str, logger);
+			parsingSuccesful.set(true);
+		} catch (final UnsupportedOperationException ex) {
+			res = predicateFactory.newDebugPredicate(str);
+			parsingSuccesful.set(false);
+		}
+		return res;
+		//return predicateFactory.newDebugPredicate(str);
 	}
 
 	private static final <LETTER> void addLettersToStringMap(HashMap<String, Set<LETTER>> map,
@@ -135,13 +160,14 @@ public class AutomataReuseUtils {
 	private static final <LETTER> void addTransitionsFromRawAutomaton(NestedWordAutomaton<LETTER, IPredicate> resAutomaton,
 			final NestedWordAutomaton<String, String> rawAutomatonFromFile, 
 			final HashMap<String, Set<LETTER>> mapStringToLetter,
-			final HashMap<String, IPredicate> mapStringToFreshState, final Boolean debugOn, final ILogger logger) {
+			final HashMap<String, IPredicate> mapStringToState, final HashMap<IPredicate, String> mapStateToString,
+			final Boolean debugOn, final ILogger logger) {
 		int[] reusedAndRemoved = {0,0}; //Index 0 is for Reused, index 1 is for removed
 		for (final IPredicate predicateState : resAutomaton.getStates()) {
-			String stringState = predicateState.toString();
-			addTransitionsFromState(rawAutomatonFromFile.callSuccessors(stringState), mapStringToLetter, mapStringToFreshState, resAutomaton, predicateState, reusedAndRemoved);
-			addTransitionsFromState(rawAutomatonFromFile.internalSuccessors(stringState), mapStringToLetter, mapStringToFreshState, resAutomaton, predicateState, reusedAndRemoved);
-			addTransitionsFromState(rawAutomatonFromFile.returnSuccessors(stringState), mapStringToLetter, mapStringToFreshState, resAutomaton, predicateState, reusedAndRemoved);
+			String stringState = mapStateToString.get(predicateState);
+			addTransitionsFromState(rawAutomatonFromFile.callSuccessors(stringState), mapStringToLetter, mapStringToState, resAutomaton, predicateState, reusedAndRemoved);
+			addTransitionsFromState(rawAutomatonFromFile.internalSuccessors(stringState), mapStringToLetter, mapStringToState, resAutomaton, predicateState, reusedAndRemoved);
+			addTransitionsFromState(rawAutomatonFromFile.returnSuccessors(stringState), mapStringToLetter, mapStringToState, resAutomaton, predicateState, reusedAndRemoved);
 		}
 		int totalTransitions = reusedAndRemoved[0]+reusedAndRemoved[1];
 		if (debugOn) {
@@ -151,9 +177,9 @@ public class AutomataReuseUtils {
 	}
 	
 	private static final <LETTER, E extends IOutgoingTransitionlet<String, String>> void addTransitionsFromState(
-			Iterable<E> transitionsIterator,HashMap<String, Set<LETTER>> mapStringToLetter,
-			HashMap<String, IPredicate> mapStringToFreshState, NestedWordAutomaton<LETTER, IPredicate> resAutomaton, 
-			IPredicate predicateState, int[] reusedAndRemovedTransitions) {
+			final Iterable<E> transitionsIterator, final HashMap<String, Set<LETTER>> mapStringToLetter,
+			final HashMap<String, IPredicate> mapStringToFreshState, NestedWordAutomaton<LETTER, IPredicate> resAutomaton, 
+			final IPredicate predicateState, int[] reusedAndRemovedTransitions) {
 		for (E transition : transitionsIterator) {
 			String transitionLetter = transition.getLetter();
 			String transitionSuccString = transition.getSucc();
@@ -183,11 +209,12 @@ public class AutomataReuseUtils {
 		}
 	}
 
-	private IPredicate parsePredicate(final PredicateParsingWrapperScript ppws, final PredicateUnifier pu, final String rawString) {
-		final Term termString = removeSerialNumber(rawString);
+	private static IPredicate parsePredicate(final PredicateParsingWrapperScript ppws, final PredicateUnifier pu, 
+			final String rawString, final ILogger logger) {
+		final String termString = removeSerialNumber(rawString,logger);
 		final Term term;
 		try {
-			term = TermParseUtils.parseTerm(ppws, rawString);
+			term = TermParseUtils.parseTerm(ppws, termString);
 		} catch (final SMTLIBException ex) {
 			if (ex.getMessage().startsWith("Undeclared function symbol (")) {
 				throw new UnsupportedOperationException("Automaton probably uses unknown variables. We should think how we can continue in this case.");
@@ -197,9 +224,17 @@ public class AutomataReuseUtils {
 		return pu.getOrConstructPredicate(term);
 	}
 
-	private Term removeSerialNumber(final String rawString) {
-		// TODO implement this
-		return null;
+	private static String removeSerialNumber(final String rawString, final ILogger logger) {
+		String[] res = rawString.split("#",2);
+		if (res.length == 1){
+			logger.warn("String "+rawString+" doesn't have a # symbol in it. Kepping entire string.");
+			return res[0];
+		} else if (res.length == 2) { //res[0] is the serial number, res[1] is the string
+			return res[1];
+		} else {
+			logger.warn("Unexpected result from String's split function. String parsing failed.");
+			return null;
+		}
 	}
 	
 }
