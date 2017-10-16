3b2fcbbacd1046cf826eb3e8d79ee84ae81b566a
Hamiz Ahmed <hamizahmed93@gmail.com>
2017-10-16 17:52:24 +0500

    Revert "Moved decorator package to CDT Parser"
    This reverts commit 3b24a986a3cb55b59726a8c43dc329f6689e6c8f.

--- trunk/source/CDTParser/src/de/uni_freiburg/informatik/ultimate/cdt/parser/CDTParser.java
+++ trunk/source/CDTParser/src/de/uni_freiburg/informatik/ultimate/cdt/parser/CDTParser.java
@@ -178,7 +178,7 @@ public class CDTParser implements ISource {
 	// created by Hamiz for entering multiple files
 	private ICProject createCDTProjectFromFiles(final File[] files)
 			throws OperationCanceledException, CoreException, FileNotFoundException {
-		final IProject proj = createCDTProject("Test Project 8");
+		final IProject proj = createCDTProject("Test Project 6");
 		mCdtProject = proj;
 		mLogger.info("Created temporary CDT project at " + getFullPath(mCdtProject));
 
