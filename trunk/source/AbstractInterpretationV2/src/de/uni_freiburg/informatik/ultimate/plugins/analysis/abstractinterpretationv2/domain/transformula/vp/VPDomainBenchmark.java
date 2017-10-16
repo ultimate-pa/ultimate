81979d0d3de24f1c2128bb69d8f29e37e52f024a
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2017-10-16 16:58:24 +0200

    #197, nicer HeapSeparatorBenchmark.toString

--- trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/transformula/vp/VPDomainBenchmark.java
+++ trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/domain/transformula/vp/VPDomainBenchmark.java
@@ -45,7 +45,7 @@ public class VPDomainBenchmark implements ICsvProviderProvider<Integer> {
 	private int mSupportingDisequalitiesCounter;
 
 
-	private boolean alreadyGeneratedColumnTitlesAndResults = false;
+	private boolean mAlreadyGeneratedColumnTitlesAndResults = false;
 	final private List<String> mColumnTitles = new ArrayList<>();
 	final private List<Integer> mResults = new ArrayList<>();
 
@@ -62,7 +62,7 @@ public class VPDomainBenchmark implements ICsvProviderProvider<Integer> {
 	}
 
 	protected void generateColumnTitlesAndResults() {
-		if (alreadyGeneratedColumnTitlesAndResults) {
+		if (mAlreadyGeneratedColumnTitlesAndResults) {
 			return;
 		}
 
@@ -88,7 +88,7 @@ public class VPDomainBenchmark implements ICsvProviderProvider<Integer> {
 			mResults.add(mAggregateStatistics.get(stat));
 		}
 
-		alreadyGeneratedColumnTitlesAndResults = true;
+		mAlreadyGeneratedColumnTitlesAndResults = true;
 	}
 
 	public void setSupportingEqualitiesCounter(final int supportingEqualitiesCounter) {
@@ -113,11 +113,6 @@ public class VPDomainBenchmark implements ICsvProviderProvider<Integer> {
 
 		for (int i = 0; i < mColumnTitles.size(); i++) {
 			sb.append(String.format("%-40s : %7d %n", mColumnTitles.get(i), mResults.get(i)));
-//			sb.append("\t");
-//			sb.append(mColumnTitles.get(i));
-//			sb.append(":\t\t");
-//			sb.append(mResults.get(i));
-//			sb.append("\n");
 		}
 		return sb.toString();
 	}
