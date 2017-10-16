857a9973f89fe4fcf4866278157b791030834ab1
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2017-10-16 20:14:54 +0200

    #197, more heapseparator tests (same pattern, increasing size, for benchmarking)

---
+++ trunk/examples/programs/heapseparator/mem-separable-02.c
@@ -0,0 +1,17 @@
+
+int main() {
+  int *p = malloc(4);
+  int *q = malloc(4);
+  int *r = malloc(4);
+  int *s = malloc(4);
+
+  *p = 0;
+  *q = 0;
+  *r = 0;
+  *s = 0;
+
+  free(p);
+  free(q);
+  free(r);
+  free(s);
+}
