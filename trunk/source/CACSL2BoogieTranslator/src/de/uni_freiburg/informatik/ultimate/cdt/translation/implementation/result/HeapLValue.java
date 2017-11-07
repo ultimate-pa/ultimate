1bcf7c7aa8335ae9cbd3f763d9a235beb2783af5
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2017-11-07 20:20:11 +0100

    towards #59, reworking InitializationHandler, intermediate commit

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/result/HeapLValue.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/result/HeapLValue.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2013-2015 Alexander Nutz (nutz@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE CACSL2BoogieTranslator plug-in.
- * 
+ *
  * The ULTIMATE CACSL2BoogieTranslator plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE CACSL2BoogieTranslator plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE CACSL2BoogieTranslator plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE CACSL2BoogieTranslator plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE CACSL2BoogieTranslator plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE CACSL2BoogieTranslator plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.result;
@@ -32,27 +32,31 @@ import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.contai
 public class HeapLValue extends LRValue {
 
 	/**
-	 * A Value inside a ResultExpression that represents a position on the
-	 * heap. Its value may be either the address or the contents of the heap
-	 * position.
-	 * @param address
-	 * @param addressIsValue determines whether the value of this is currently an address
-	 * or a Expression
+	 * LRValue that stores a memory address.
+	 * Two use cases:
+	 *  <li> HeapLValue is read: then we will call "switchToRValue" on the ExpressionResult containing this HeapLValue
+	 *    first. This will generate  code that reads the memory at the address of this HeapLValue and store it inside a
+	 *    temporary variable, which will be the new LRValue of that ExpressionResult.
+	 *  <li> HeapLValue is written: then we use the address of this HeapLValue to generate the code that writes to the
+	 *    according memory location.
+	 *
+	 * @param address the memory address
+	 * @param cType the type (in terms of C, as opposed to boogie) of the information that is stored at address
 	 */
-	public HeapLValue(Expression address, CType cType) {
+	public HeapLValue(final Expression address, final CType cType) {
 		this(address, cType, false);
 	}
 
-	public HeapLValue(Expression address, CType cType, boolean isIntFromPtr) {
+	public HeapLValue(final Expression address, final CType cType, final boolean isIntFromPtr) {
 		super(cType, false, isIntFromPtr);
 		this.address = address;
 	}
 	Expression address;
-	
+
 	public Expression getAddress() {
 		return address;
 	}
-	
+
 	@Override
 	public Expression getValue() {
 		throw new AssertionError("HeapLValues must be converted to RValue before their value can be queried.");
