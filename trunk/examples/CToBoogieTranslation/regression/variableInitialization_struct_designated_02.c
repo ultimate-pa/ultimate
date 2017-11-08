5ccd028bb0edfb5483ab3c4a8d5d52f1aef17b79
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2017-11-08 22:19:12 +0100

    #261, add example programs

---
+++ trunk/examples/CToBoogieTranslation/regression/variableInitialization_struct_designated_02.c
@@ -0,0 +1,16 @@
+//@Unsafe
+#include<stdio.h>
+int main() {
+  struct structtype {
+    int i1;
+    int i2;
+  };
+
+  struct structtype s = { .i2 = 3, .i1 = 7 };
+
+  printf("i1: %i\n", s.i1);
+  printf("i2: %i\n", s.i2);
+
+  //@ assert s.i1 == 3;
+  //@ assert s.i2 == 7;
+}
