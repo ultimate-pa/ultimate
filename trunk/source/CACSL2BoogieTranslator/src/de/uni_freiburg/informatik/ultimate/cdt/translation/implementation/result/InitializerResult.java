2e49e9be376033ac7773267f75bdec7beeba6d5b
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2017-11-11 14:47:51 +0100

    #259, #261, rework InitializationHandler, finished InitializerResult and its builder (not much changed from ExpressionListRecResult, the work will be done in InitializationHandler.InitializerInfo)

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/result/InitializerResult.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/result/InitializerResult.java
@@ -25,28 +25,12 @@
  * licensors of the ULTIMATE CACSL2BoogieTranslator plug-in grant you additional permission
  * to convey the resulting work.
  */
-/**
- * Result for initializer lists. E.g. for arrays and structs.
- */
 package de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.result;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import de.uni_freiburg.informatik.ultimate.boogie.ast.BoogieASTNode;
-import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.expressiontranslation.AExpressionTranslation;
-import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.container.c.CArray;
-import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.container.c.CEnum;
-import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.container.c.CPointer;
-import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.container.c.CPrimitive;
-import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.container.c.CStruct;
-import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.container.c.CType;
-import de.uni_freiburg.informatik.ultimate.cdt.translation.interfaces.Dispatcher;
-import de.uni_freiburg.informatik.ultimate.core.model.models.ILocation;
 
 /**
  * A Result for the C to Boogie translation.
@@ -65,15 +49,6 @@ import de.uni_freiburg.informatik.ultimate.core.model.models.ILocation;
  * @author Alexander Nutz (nutz@informatik.uni-freiburg.de)
  */
 public class InitializerResult extends Result {
-
-
-	/**
-	 * Indicates if this initializer has already been converted for a target type.
-	 * Used for sanity checks.
-	 */
-	boolean mIsConvertedToTargetType;
-
-
 	/**
 	 * Stores all the code that is needed for evaluating the initializer.
 	 *
@@ -87,30 +62,15 @@ public class InitializerResult extends Result {
 	 * (We can/could also use this class for
 	 *  initializers of non-aggregate type)
 	 */
-	private final ExpressionResult mExpressionResult;
+	private final ExpressionResult mRootExpressionResult;
 
 	/**
 	 * The RValue in mExpressionResult may have a designator. This field stores it.
 	 * (Can happen only if we are inside a nested initializer.)
 	 */
-	private String mRootDesignator;
+	private final String mRootDesignator;
 
-	/**
-	 * We store this as a list to keep the ordering.
-	 * Otherwise we could use mTreeNodeIdToValue.keySet() instead.
-	 * (maybe we switch to a more elegant solution some time..)
-	 */
-	private final List<List<Integer>> mTreeNodeIds;
-
-	private final Map<List<Integer>, ExpressionResult> mTreeNodeIdToValue;
-
-
-	/**
-	 * It is possible to give a struct initializer that lists the fields out of order by giving designators for each
-	 * initialization value.
-	 */
-	private final Map<List<Integer>, String> mTreeNodeIdToDesignatorName;
-
+	private final List<InitializerResult> mChildren;
 
 	/**
 	 *
@@ -120,131 +80,27 @@ public class InitializerResult extends Result {
 	 * @param treeNodeIdToRValue
 	 * @param treeNodeIdToDesignatorName
 	 */
-	InitializerResult(final BoogieASTNode node, final ExpressionResult expressionResult,
-			final List<List<Integer>> treeNodeIds, final Map<List<Integer>, ExpressionResult> treeNodeIdToRValue,
-			final Map<List<Integer>, String> treeNodeIdToDesignatorName) {
+	public InitializerResult(final BoogieASTNode node, final String rootDesignator,
+			final ExpressionResult expressionResult, final List<InitializerResult> children) {
 		super(node);
-		mExpressionResult = expressionResult;
-		mTreeNodeIds = treeNodeIds;
-		mTreeNodeIdToValue = treeNodeIdToRValue;
-		mTreeNodeIdToDesignatorName = treeNodeIdToDesignatorName;
+		mRootDesignator = rootDesignator;
+		mRootExpressionResult = expressionResult;
+		mChildren = Collections.unmodifiableList(children);
 	}
 
-	public Collection<List<Integer>> getTreeNodeIds() {
-		assert assertNoDuplicateTreeNodeIds();
-		return mTreeNodeIds;
+	public String getRootDesignator() {
+		return mRootDesignator;
 	}
 
-	public ExpressionResult getTreeNode(final List<Integer> nodeId) {
-		return mTreeNodeIdToValue.get(nodeId);
+	public ExpressionResult getRootExpressionResult() {
+		return mRootExpressionResult;
 	}
 
-	public ExpressionResult getExpressionResult() {
-		return mExpressionResult;
-	}
-
-	/**
-	 * Return the direct children of this node
-	 * @return
-	 */
-	public List<InitializerResult> getTopLevelChildren() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<InitializerResult> getChildren() {
+		return mChildren;
 	}
 
 	public boolean hasRootDesignator() {
 		return mRootDesignator != null;
 	}
-
-	public InitializerResult rexBoolToIntIfNecessary(final ILocation loc, final AExpressionTranslation expressionTranslation) {
-		mExpressionResult.rexBoolToIntIfNecessary(loc, expressionTranslation);
-
-		for (final Entry<List<Integer>, ExpressionResult> en : new HashMap<>(mTreeNodeIdToValue).entrySet()) {
-			en.getValue().rexBoolToIntIfNecessary(loc, expressionTranslation);
-//			final RValue rVal = en.getValue();
-//
-//			if (rVal.isBoogieBool()) {
-//				final RValue newRVal = RValue.boolToInt(loc, rVal, expressionTranslation);
-//				mTreeNodeIdToValue.remove(en.getKey());
-//				mTreeNodeIdToValue.put(en.getKey(), newRVal);
-//			} else {
-//				// do nothing
-//			}
-		}
-
-		throw new AssertionError("TODO: immutable-stlye?..");
-	}
-
-	/**
-	 * Should only happen when this is a non-aggregate initializer.
-	 *
-	 * @param loc
-	 * @param main
-	 * @param targetCType
-	 */
-	public InitializerResult convert(final ILocation loc, final Dispatcher main, final CType targetCType) {
-
-
-		/*
-		 * TODO: this conversion should also do the reordering due to designated intitializers (as the targetCType has
-		 *  the information necessary for the reordering)
-		 */
-
-		if (targetCType instanceof CPrimitive || targetCType instanceof CEnum || targetCType instanceof CPointer) {
-			assert mRootDesignator == null;
-			assert mTreeNodeIds.isEmpty();
-
-			main.mCHandler.convert(loc, mExpressionResult, targetCType);
-			throw new AssertionError("TODO"); //TODO
-		} else if (targetCType instanceof CStruct) {
-			throw new AssertionError("TODO"); //TODO
-		} else if (targetCType instanceof CArray) {
-			throw new AssertionError("TODO"); //TODO
-		} else {
-			throw new UnsupportedOperationException("missing case?");
-		}
-
-
-		// TODO assert that all Boogie code from the ExpressionResults in the returned InitializerResult has been moved
-		// to the root ExpressionResult.
-	}
-
-	private boolean assertNoDuplicateTreeNodeIds() {
-		assert new HashSet<>(mTreeNodeIds).size() == mTreeNodeIds.size();
-		return true;
-	}
-
-	public boolean hasBeenConvertedToTargetType() {
-		return mIsConvertedToTargetType;
-	}
-
-	public boolean hasTreeNode(final List<Integer> arrayIndex) {
-		return mTreeNodeIdToValue.containsKey(arrayIndex);
-	}
-
-
-//	public LRValue getLrVal() {
-//		return mExpressionResult.getLrValue();
-//	}
-//
-//	public Collection<? extends Declaration> getDeclarations() {
-//		return mExpressionResult.getDeclarations();
-//	}
-//
-//	public Collection<? extends Statement> getStatements() {
-//		return mExpressionResult.getStatements();
-//	}
-//
-//	public Map<? extends VariableDeclaration, ? extends ILocation> getAuxVars() {
-//		return mExpressionResult.getAuxVars();
-//	}
-//
-//	public Collection<? extends Overapprox> getOverapprs() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-
-//	public Map<List<Integer>, ExpressionResult> getTree
-
-//	public List<List<Integer>, ExpressionResult> get
 }
