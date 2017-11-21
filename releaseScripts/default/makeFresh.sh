cf83f96a50683ca5c4d6a45c38252ae4a518d7ad
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-11-21 08:20:13 +0100

    add conjunctive block encoding settings and toolchain to automizer and buchi automizer

--- releaseScripts/default/makeFresh.sh
+++ releaseScripts/default/makeFresh.sh
@@ -15,9 +15,19 @@ exitOnFail mvn clean install -Pmaterialize
 popd > /dev/null
 
 # createZip <toolname> <targetarch> <reachtc> <termtc> <witnessvaltc> <memsafetytc> <ltlc> <termwitnessvaltc>
+# Taipan 
 exitOnFail bash createZip.sh Taipan linux AutomizerCInline_WitnessPrinter.xml NONE AutomizerCInline.xml AutomizerCInline_WitnessPrinter.xml NONE NONE
-exitOnFail bash createZip.sh Automizer linux AutomizerC_WitnessPrinter.xml BuchiAutomizerCInline_WitnessPrinter.xml AutomizerC.xml AutomizerC_WitnessPrinter.xml LTLAutomizerC.xml BuchiAutomizerCInline.xml
+
+# Automizer without separate blockencoding plugin 
+#exitOnFail bash createZip.sh Automizer linux AutomizerC_WitnessPrinter.xml BuchiAutomizerCInline_WitnessPrinter.xml AutomizerC.xml AutomizerC_WitnessPrinter.xml LTLAutomizerC.xml BuchiAutomizerCInline.xml
+
+# Automizer with separate blockencoding plugin 
+exitOnFail bash createZip.sh Automizer linux AutomizerC_BE_WitnessPrinter.xml BuchiAutomizerCInline_BE_WitnessPrinter.xml AutomizerC.xml AutomizerC_BE_WitnessPrinter.xml LTLAutomizerC.xml BuchiAutomizerCInline.xml
+
+# Kojak 
 exitOnFail bash createZip.sh Kojak linux KojakC_WitnessPrinter.xml NONE NONE KojakC_WitnessPrinter.xml NONE NONE
+
+# DeltaDebugger
 exitOnFail bash createDeltaDebuggerDir.sh linux 
 
 exit 0
