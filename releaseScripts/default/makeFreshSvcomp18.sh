d02b40ec0dd66c325c3bba720663a16172ab273e
Daniel Dietsch <daniel.dietsch@gmail.com>
2017-11-22 18:46:12 +0100

    also copy to gitlab repo (still manual pull request)

--- releaseScripts/default/makeFreshSvcomp18.sh
+++ releaseScripts/default/makeFreshSvcomp18.sh
@@ -15,6 +15,8 @@ DEPLOY_SERVER=sotec.informatik.uni-freiburg.de
 DEPLOY_DIR=/export/server/httpd/ultimate/downloads/svcomp2018
 TESTFILE=caniwrite
 FILE="$DEPLOY_DIR"/"$TESTFILE"
+SVCOMP_GITLAB_DIR=/storage/svcomp-archives/2018
+
 
 if [ -z "$1" ]
 then
@@ -37,6 +39,12 @@ exitOnFail bash makeFresh.sh
 
 # uncomment this after the final release 
 #for z in *.zip; do mv "$z" "${z%.zip}-post-final.zip"; done
+
+# copy/rename to svcomp gitlab folder 
+for z in *.zip; do
+        f=$(echo $z | sed 's/Ultimate\(.*\)-linux\.zip/u\1\.zip/g' | tr '[:upper:]' '[:lower:]')
+        cp "$z" "$SVCOMP_GITLAB_DIR""/""$f"
+done
 
 rsync -P --rsh="sshpass -e ssh -l me8 -oHostKeyAlgorithms=+ssh-dss" *.zip $CURRENTUSER@${DEPLOY_SERVER}:${DEPLOY_DIR}/. 
 rm *.zip 
