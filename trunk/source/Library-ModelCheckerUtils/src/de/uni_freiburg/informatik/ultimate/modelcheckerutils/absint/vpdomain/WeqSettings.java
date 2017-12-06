a0a0a708e7cdc5cb0bf198dd8f85f400c3410851
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2017-12-06 11:26:45 +0100

    #239, add some settings flags

--- trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/absint/vpdomain/WeqSettings.java
+++ trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/absint/vpdomain/WeqSettings.java
@@ -5,5 +5,7 @@ public class WeqSettings {
 	static final boolean FLATTEN_WEQ_EDGES_BEFORE_JOIN = false;
 
 	static final boolean REPORT_EQ_DEQ_INPLACE = false;
+	public static final boolean REMOVE_ELEMENT_INPLACE = false;
+	public static final boolean ADD_NODE_INPLACE = false;
 
 }
---
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/congruenceclosure/CcSettings.java
@@ -0,0 +1,8 @@
+package de.uni_freiburg.informatik.ultimate.util.datastructures.congruenceclosure;
+
+public class CcSettings {
+	public static final boolean REPORT_EQ_DEQ_INPLACE = false;
+	public static final boolean REMOVE_ELEMENT_INPLACE = false;
+	public static final boolean ADD_NODE_INPLACE = false;
+
+}
