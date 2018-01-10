f411e1e003ca5998f1bd6b5c1715193fe98adcbc
Daniel Dietsch <daniel.dietsch@gmail.com>
2018-01-10 10:48:39 +0100

    add windows binaries to makeFresh.sh

--- releaseScripts/default/makeFresh.sh
+++ releaseScripts/default/makeFresh.sh
@@ -14,20 +14,22 @@ pushd ../../trunk/source/BA_MavenParentUltimate/ > /dev/null
 exitOnFail mvn clean install -Pmaterialize
 popd > /dev/null
 
-# createZip <toolname> <targetarch> <reachtc> <termtc> <witnessvaltc> <memsafetytc> <ltlc> <termwitnessvaltc>
-# Taipan 
-exitOnFail bash createZip.sh Taipan linux AutomizerCInline_WitnessPrinter.xml NONE AutomizerCInline.xml AutomizerCInline_WitnessPrinter.xml NONE NONE
+for platform in {linux,win32}; do
+    # createZip <toolname> <targetarch> <reachtc> <termtc> <witnessvaltc> <memsafetytc> <ltlc> <termwitnessvaltc>
+    # Taipan 
+    exitOnFail bash createZip.sh Taipan $platform AutomizerCInline_WitnessPrinter.xml NONE AutomizerCInline.xml AutomizerCInline_WitnessPrinter.xml NONE NONE
 
-# Automizer without separate blockencoding plugin 
-exitOnFail bash createZip.sh Automizer linux AutomizerC_WitnessPrinter.xml BuchiAutomizerCInline_WitnessPrinter.xml AutomizerC.xml AutomizerC_WitnessPrinter.xml LTLAutomizerC.xml BuchiAutomizerCInline.xml
+    # Automizer without separate blockencoding plugin 
+    exitOnFail bash createZip.sh Automizer $platform AutomizerC_WitnessPrinter.xml BuchiAutomizerCInline_WitnessPrinter.xml AutomizerC.xml AutomizerC_WitnessPrinter.xml LTLAutomizerC.xml BuchiAutomizerCInline.xml
 
-# Automizer with separate blockencoding plugin 
-#exitOnFail bash createZip.sh Automizer linux AutomizerC_BE_WitnessPrinter.xml BuchiAutomizerCInline_BE_WitnessPrinter.xml AutomizerC.xml AutomizerC_BE_WitnessPrinter.xml LTLAutomizerC.xml BuchiAutomizerCInline.xml
+    # Automizer with separate blockencoding plugin 
+    #exitOnFail bash createZip.sh Automizer linux AutomizerC_BE_WitnessPrinter.xml BuchiAutomizerCInline_BE_WitnessPrinter.xml AutomizerC.xml AutomizerC_BE_WitnessPrinter.xml LTLAutomizerC.xml BuchiAutomizerCInline.xml
 
-# Kojak 
-exitOnFail bash createZip.sh Kojak linux KojakC_WitnessPrinter.xml NONE NONE KojakC_WitnessPrinter.xml NONE NONE
+    # Kojak 
+    exitOnFail bash createZip.sh Kojak $platform KojakC_WitnessPrinter.xml NONE NONE KojakC_WitnessPrinter.xml NONE NONE
 
-# DeltaDebugger
-exitOnFail bash createDeltaDebuggerDir.sh linux 
+    # DeltaDebugger
+    exitOnFail bash createDeltaDebuggerDir.sh $platform 
+done 
 
 exit 0
