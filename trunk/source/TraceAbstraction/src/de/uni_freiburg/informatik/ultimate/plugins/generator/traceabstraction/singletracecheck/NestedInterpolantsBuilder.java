55a8838f82e29f161fb3bb8e46def7230b9b6059
danieldietsch <daniel.dietsch@gmail.com>
2018-01-25 18:04:16 +0100

    normalize constants in interpolants before trying to eliminate quantifiers

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/singletracecheck/NestedInterpolantsBuilder.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/singletracecheck/NestedInterpolantsBuilder.java
@@ -73,6 +73,7 @@ import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.Ac
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.PredicateFactory;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.SPredicate;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.singletracecheck.TraceCheck.TraceCheckLock;
+import de.uni_freiburg.informatik.ultimate.smtinterpol.model.ConstantTermNormalizer;
 import de.uni_freiburg.informatik.ultimate.util.DebugMessage;
 
 public class NestedInterpolantsBuilder {
@@ -128,7 +129,7 @@ public class NestedInterpolantsBuilder {
 	private final boolean mInstantiateArrayExt;
 
 	public NestedInterpolantsBuilder(final ManagedScript mgdScriptTc, final TraceCheckLock scriptLockOwner,
-			final NestedFormulas<Term, Term> annotatdSsa, final Map<Term, IProgramVar> mconstants2BoogieVar,
+			final NestedFormulas<Term, Term> annotatdSsa, final Map<Term, IProgramVar> constants2BoogieVar,
 			final IPredicateUnifier predicateBuilder, final PredicateFactory predicateFactory,
 			final Set<Integer> interpolatedPositions, final boolean treeInterpolation,
 			final IUltimateServiceProvider services, final TraceCheck traceCheck, final ManagedScript mgdScriptCfg,
@@ -150,7 +151,7 @@ public class NestedInterpolantsBuilder {
 		mTrace = annotatdSsa.getTrace();
 		mInstantiateArrayExt = instantiateArrayExt;
 		final HashMap<Term, Term> const2RepTv = new HashMap<>();
-		for (final Entry<Term, IProgramVar> entry : mconstants2BoogieVar.entrySet()) {
+		for (final Entry<Term, IProgramVar> entry : constants2BoogieVar.entrySet()) {
 			const2RepTv.put(entry.getKey(), entry.getValue().getTermVariable());
 		}
 		if (mMgdScriptTc != mgdScriptCfg) {
@@ -244,8 +245,8 @@ public class NestedInterpolantsBuilder {
 					throw new AssertionError();
 				}
 			}
-			startNewFormula = isInterpolatedPositio(i);
-			if (isInterpolatedPositio(i)) {
+			startNewFormula = isInterpolatedPosition(i);
+			if (isInterpolatedPosition(i)) {
 				mStackHeightAtLastInterpolatedPosition = mStartOfSubtreeStack.size();
 				mCraigInt2interpolantIndex.add(i);
 			}
@@ -296,11 +297,11 @@ public class NestedInterpolantsBuilder {
 			// everything ok
 		} else {
 			if (mStackHeightAtLastInterpolatedPosition + 1 == mStartOfSubtreeStack.size() && mTrace.isCallPosition(i)
-					&& (i == 0 || isInterpolatedPositio(i - 1))) {
+					&& (i == 0 || isInterpolatedPosition(i - 1))) {
 				// everything ok
 			} else {
 				if (mStackHeightAtLastInterpolatedPosition - 1 == mStartOfSubtreeStack.size()
-						&& mTrace.isReturnPosition(i) && isInterpolatedPositio(i - 1)) {
+						&& mTrace.isReturnPosition(i) && isInterpolatedPosition(i - 1)) {
 					// everything ok
 				} else {
 					throw new IllegalArgumentException(
@@ -329,7 +330,7 @@ public class NestedInterpolantsBuilder {
 		mInterpolInput.set(lastPosition, newFormula);
 	}
 
-	public boolean isInterpolatedPositio(final int i) {
+	public boolean isInterpolatedPosition(final int i) {
 		assert i >= 0;
 		assert i < mTrace.length();
 		if (i == mTrace.length() - 1) {
@@ -555,7 +556,7 @@ public class NestedInterpolantsBuilder {
 				positionOfThisCraigInterpolant = mCraigInt2interpolantIndex.get(craigInterpolPos);
 			}
 			assert positionOfThisCraigInterpolant >= resultPos;
-			if (isInterpolatedPositio(resultPos)) {
+			if (isInterpolatedPosition(resultPos)) {
 				Term withIndices = mCraigInterpolants[craigInterpolPos];
 				assert resultPos == mCraigInt2interpolantIndex.get(craigInterpolPos);
 				craigInterpolPos++;
@@ -574,8 +575,9 @@ public class NestedInterpolantsBuilder {
 							&& new SubtermPropertyChecker(x -> isAtDiffTerm(x)).isPropertySatisfied(withoutIndices)) {
 						throw new UnsupportedOperationException(DIFF_IS_UNSUPPORTED);
 					}
+					final Term withoutIndicesNormalized = new ConstantTermNormalizer().transform(withoutIndices);
 					final Term lessQuantifiers = PartialQuantifierElimination.tryToEliminate(mServices, mLogger,
-							mMgdScriptCfg, withoutIndices, mSimplificationTechnique, mXnfConversionTechnique);
+							mMgdScriptCfg, withoutIndicesNormalized, mSimplificationTechnique, mXnfConversionTechnique);
 					result[resultPos] = mPredicateUnifier.getOrConstructPredicate(lessQuantifiers);
 					withIndices2Predicate.put(withIndices, result[resultPos]);
 				}
