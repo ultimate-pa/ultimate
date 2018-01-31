aa7c7e890642aa171ef52c53166436afa870203c
buehlery <33325812+buehlery@users.noreply.github.com>
2018-01-31 18:38:27 +0100

    added function pointers to simpleinclude example

--- trunk/examples/CToBoogieTranslation/multifiles/simpleinclude/bar.c
+++ trunk/examples/CToBoogieTranslation/multifiles/simpleinclude/bar.c
@@ -1,8 +1,13 @@
 #include "foo.h"
 
+int (*src)(int) = randomNumber;
+
 int main() {
     // Use a function from the header
     int pick = randomNumber(3);
+    
+    // Test function pointers to check their syntax.
+    int otherPick = src(4);
 
     // Use a constant from the header
     if (pick == VERIFIED_RANDOM) {
