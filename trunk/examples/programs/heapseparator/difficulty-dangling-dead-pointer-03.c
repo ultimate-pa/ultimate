b22515da27da5853590e76b5302434dec9691baf
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-02-14 15:28:25 +0100

    misc test programs

---
+++ trunk/examples/programs/heapseparator/difficulty-dangling-dead-pointer-03.c
@@ -0,0 +1,28 @@
+/**
+ * Currently (09.01.2018), the heap separator cannot separate the memory array 
+ * in this example, although there is a semantics-preserving separation.
+ *  (partition would be {{p}, {q,r}})
+ * Combination of triggers for this problem:
+ *  - r is used to access the memory array, thus it is taken into account for 
+ *   partitioning.
+ *  - r has a nondeterministic value at the positions where p and q are 
+ *   dereferenced, it dangles between the potential partition blocks so to say, 
+ *   and thus triggers their unification.
+ */
+int * nondet() {
+  int *i;
+  return i;
+}
+
+int main() {
+
+  int *p = malloc(sizeof(int));
+  int *q = malloc(sizeof(int));
+
+  int *r = q;
+  int z = *r;
+  r = nondet();
+
+  int x = *p;
+  int y = *q;
+}
