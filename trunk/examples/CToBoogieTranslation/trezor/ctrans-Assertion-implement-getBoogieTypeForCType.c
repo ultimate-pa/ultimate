a71d5b662f1c9aaeb4253898bc402abb836021a0
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-02-27 18:18:56 +0100

    #311: fix example so it compiles

--- trunk/examples/CToBoogieTranslation/trezor/ctrans-Assertion-implement-getBoogieTypeForCType.c
+++ trunk/examples/CToBoogieTranslation/trezor/ctrans-Assertion-implement-getBoogieTypeForCType.c
@@ -2,7 +2,8 @@ enum rcc_periph_clken {
  RCC_OTGFS = (((0x34) << 5) + (7)),
 };
 
-void rcc_periph_clock_enable(enum rcc_periph_clken clken);
+void rcc_periph_clock_enable(enum rcc_periph_clken clken) {
+}
 
 int main(void)
 {
