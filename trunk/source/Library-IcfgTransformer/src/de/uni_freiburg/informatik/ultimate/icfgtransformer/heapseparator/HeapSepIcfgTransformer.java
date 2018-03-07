35aef075c2332b8308903c7a48541fdce3e02fc0
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-03-07 17:54:51 +0100

    #295, had to revise understanding of what we call an "array write" or "array update" (use case: handle assumes that work as an array update, like in the inlining of the write-int methods)  - implemented computation of array groups and StoreIndexInfos before all IIcfg preprocessings  - StoreIndexInfo now refers to an ArrayGroup instead of a single array (of type IProgramVarOrConst)

--- trunk/source/Library-IcfgTransformer/src/de/uni_freiburg/informatik/ultimate/icfgtransformer/heapseparator/HeapSepIcfgTransformer.java
+++ trunk/source/Library-IcfgTransformer/src/de/uni_freiburg/informatik/ultimate/icfgtransformer/heapseparator/HeapSepIcfgTransformer.java
@@ -1,10 +1,12 @@
 package de.uni_freiburg.informatik.ultimate.icfgtransformer.heapseparator;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -21,20 +23,27 @@ import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.absint.vpdomain.CongruenceClosureSmtUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.absint.vpdomain.HeapSepProgramConst;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfg;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgEdge;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgEdgeIterator;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgLocation;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transformations.ReplacementVarFactory;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transitions.TransFormula;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transitions.TransFormulaUtils;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transitions.UnmodifiableTransFormula;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramConst;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramNonOldVar;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramVarOrConst;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.ProgramVarUtils;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.ApplicationTermFinder;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtSortUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.arrays.MultiDimensionalSort;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.equalityanalysis.IEqualityAnalysisResultProvider;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.equalityanalysis.IEqualityProvidingIntermediateState;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.cfg.BoogieIcfgLocation;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.DataStructureUtils;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.UnionFind;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.NestedMap2;
 
 public class HeapSepIcfgTransformer<INLOC extends IcfgLocation, OUTLOC extends IcfgLocation>
@@ -142,11 +151,20 @@ public class HeapSepIcfgTransformer<INLOC extends IcfgLocation, OUTLOC extends I
 		final ILocationFactory<OUTLOC, OUTLOC> outToOutLocFac =
 				(ILocationFactory<OUTLOC, OUTLOC>) createIcfgLocationToIcfgLocationFactory();
 
+		final NestedMap2<EdgeInfo, Term, StoreIndexInfo> edgeToIndexToStoreIndexInfo;
+		final Map<IProgramVarOrConst, ArrayGroup> arrayToArrayGroup;
+		{
+			final ComputeStoreIndexInfosAndArrayGroups<INLOC> csiiaag =
+					new ComputeStoreIndexInfosAndArrayGroups<>(originalIcfg, mHeapArrays);
+			edgeToIndexToStoreIndexInfo =
+					csiiaag.getEdgeToIndexToStoreIndexInfo();
+			arrayToArrayGroup = csiiaag.getArrayToArrayGroup();
+		}
+
 		/*
 		 * 1. Execute the preprocessing
 		 */
 		final IIcfg<OUTLOC> preprocessedIcfg;
-		final NestedMap2<EdgeInfo, Term, StoreIndexInfo> edgeToIndexToStoreIndexInfo;
 		final Map<StoreIndexInfo, IProgramNonOldVar> storeIndexInfoToFreezeVar;
 		final Map<StoreIndexInfo, IProgramConst> storeIndexInfoToLocLiteral;
 		final IProgramNonOldVar memlocArrayInt;
@@ -158,11 +176,12 @@ public class HeapSepIcfgTransformer<INLOC extends IcfgLocation, OUTLOC extends I
 			 */
 			final StoreIndexFreezerIcfgTransformer<INLOC, OUTLOC> sifit =
 					new StoreIndexFreezerIcfgTransformer<>(mLogger, "icfg_with_uninitialized_freeze_vars",
-							outLocationClass, originalIcfg, funLocFac, backtranslationTracker, mHeapArrays);
+							outLocationClass, originalIcfg, funLocFac, backtranslationTracker, mHeapArrays,
+							edgeToIndexToStoreIndexInfo);
 			IIcfg<OUTLOC> icfgWFreezeVarsUninitialized = sifit.getResult();
 
 			storeIndexInfoToFreezeVar = sifit.getArrayAccessInfoToFreezeVar();
-			edgeToIndexToStoreIndexInfo = sifit.getEdgeToIndexToStoreIndexInfo();
+//			edgeToIndexToStoreIndexInfo = sifit.getEdgeToIndexToStoreIndexInfo();
 
 			mLogger.info("finished StoreIndexFreezer, created " + storeIndexInfoToFreezeVar.size() + " freeze vars and "
 					+ "freeze var literals (each corresponds to one heap write)");
@@ -257,10 +276,10 @@ public class HeapSepIcfgTransformer<INLOC extends IcfgLocation, OUTLOC extends I
 			final MemlocArrayUpdaterIcfgTransformer<INLOC, OUTLOC> mauit =
 					new MemlocArrayUpdaterIcfgTransformer<>(mLogger, "icfg_with_memloc_updates",
 							outLocationClass, originalIcfg, funLocFac, backtranslationTracker, memlocArrayInt,
-							memLocSort, mHeapArrays);
+							memLocSort, mHeapArrays, edgeToIndexToStoreIndexInfo);
 			IIcfg<OUTLOC> icfgWithMemlocUpdates = mauit.getResult();
 
-			edgeToIndexToStoreIndexInfo = mauit.getEdgeToIndexToStoreIndexInfo();
+//			edgeToIndexToStoreIndexInfo = mauit.getEdgeToIndexToStoreIndexInfo();
 			storeIndexInfoToLocLiteral = mauit.getStoreIndexInfoToLocLiteral();
 
 			mLogger.info("finished MemlocArrayUpdater, created " + mauit.getLocationLiterals().size() +
@@ -321,7 +340,7 @@ public class HeapSepIcfgTransformer<INLOC extends IcfgLocation, OUTLOC extends I
 				new HashSet<>(heapSepPreanalysis.getArrayToArrayGroup().values())));
 		mLogger.info("  select infos: " + DataStructureUtils.prettyPrint(heapSepPreanalysis.getSelectInfos()));
 
-		final Map<IProgramVarOrConst, ArrayGroup> arrayToArrayGroup = heapSepPreanalysis.getArrayToArrayGroup();
+//		final Map<IProgramVarOrConst, ArrayGroup> arrayToArrayGroup = heapSepPreanalysis.getArrayToArrayGroup();
 
 		final HeapPartitionManager partitionManager;
 		if (mSettings.getPreprocessing() == Preprocessing.FREEZE_VARIABLES) {
@@ -342,7 +361,6 @@ public class HeapSepIcfgTransformer<INLOC extends IcfgLocation, OUTLOC extends I
 		}
 		partitionManager.finish();
 
-
 		/*
 		 * 4. Execute the transformer that splits up the arrays according to the result from the equality analysis.
 		 *  Note that this transformation is done on the original input Icfg, not on the output of the
@@ -358,6 +376,8 @@ public class HeapSepIcfgTransformer<INLOC extends IcfgLocation, OUTLOC extends I
 						mStatistics);
 		mResultIcfg = heapSeparatingTransformer.getResult();
 	}
+
+
 
 	public void assertLiteralDisequalitiesIntoScript(final Set<Term> literalTerms) {
 		mMgdScript.lock(this);
@@ -445,5 +465,293 @@ class HeapSepSettings {
 
 	public Preprocessing getPreprocessing() {
 		return mPreprocessing;
+	}
+}
+
+/**
+ * A Note on the notion of array writes in our setting:
+ * <li> array writes are to an array group
+ * <li> a write to an array group is given by a store term in the program whose base array is an array of the group
+ * <li> as the base arrays on both sides of an equation are always in the same array group, the write is also to the
+ *   array on the side of the equation other from where the store term is (so the standard notion of array updates is
+ *    covered, but also for example assume statements in Boogie can constitute an array write)
+ *
+ * We compute array groups per TransFormula and globally, where the per transformula partitions form the constraints
+ * for the global partition.
+ * Aux vars may also belong to an array group, because they are equated to some term that belongs to a pvoc in their
+ * TransFormula.
+ *
+ * @author Alexander Nutz (nutz@informatik.uni-freiburg.de)
+ *
+ * @param <LOC>
+ */
+class ComputeStoreIndexInfosAndArrayGroups<LOC extends IcfgLocation> {
+
+	final NestedMap2<EdgeInfo, Term, StoreIndexInfo> mEdgeToIndexToStoreIndexInfo = new NestedMap2<>();
+	final Map<IProgramVarOrConst, ArrayGroup> mArrayToArrayGroup = new HashMap<>();
+
+	private int mStoreIndexInfoCounter;
+
+	public ComputeStoreIndexInfosAndArrayGroups(final IIcfg<LOC> icfg, final List<IProgramVarOrConst> heapArrays) {
+
+		final UnionFind<IProgramVarOrConst> globalArrayPartition = new UnionFind<>();
+		// base line for the array groups: the heap arrays
+		heapArrays.forEach(globalArrayPartition::findAndConstructEquivalenceClassIfNeeded);
+
+		final Map<EdgeInfo, UnionFind<Term>> edgeToPerEdgeArrayPartition = new HashMap<>();
+		{
+			final IcfgEdgeIterator edgeIt = new IcfgEdgeIterator(icfg);
+			while (edgeIt.hasNext()) {
+				final IcfgEdge edge = edgeIt.next();
+				final UnmodifiableTransFormula tf = edge.getTransformula();
+				final EdgeInfo edgeInfo = new EdgeInfo(edge);
+
+
+				/*
+				 * construct the per-edge (or per-transformula, the difference does not matter here) array partition
+				 */
+				final UnionFind<Term> perTfArrayPartition = new UnionFind<>();
+
+				final List<ArrayEqualityAllowStores> aeass =
+						ArrayEqualityAllowStores.extractArrayEqualityAllowStores(tf.getFormula());
+				for (final ArrayEqualityAllowStores aeas : aeass) {
+					final Term lhsArrayTerm = getArrayId(aeas.getLhsArray());
+					final Term rhsArrayTerm = getArrayId(aeas.getRhsArray());
+					perTfArrayPartition.findAndConstructEquivalenceClassIfNeeded(lhsArrayTerm);
+					perTfArrayPartition.findAndConstructEquivalenceClassIfNeeded(rhsArrayTerm);
+					perTfArrayPartition.union(lhsArrayTerm, rhsArrayTerm);
+				}
+
+				edgeToPerEdgeArrayPartition.put(edgeInfo, perTfArrayPartition);
+
+				/*
+				 * update the global array partition
+				 */
+				for (final Set<Term> eqc : perTfArrayPartition.getAllEquivalenceClasses()) {
+					// pick some element that has a pvoc from the group of array terms
+
+					final Set<IProgramVarOrConst> eqcPvocs = eqc.stream()
+							.map(term -> TransFormulaUtils.getProgramVarOrConstForTerm(tf, term))
+							.filter(pvoc -> pvoc != null)
+							.collect(Collectors.toSet());
+					globalArrayPartition.union(eqcPvocs);
+				}
+			}
+		}
+
+		/*
+		 * Construct the array groups and their map.
+		 */
+		{
+			final Set<ArrayGroup> arrayGroups = new HashSet<>();
+			for (final Set<IProgramVarOrConst> block : globalArrayPartition.getAllEquivalenceClasses()) {
+				arrayGroups.add(new ArrayGroup(block));
+			}
+
+			for (final ArrayGroup ag : arrayGroups) {
+				for (final IProgramVarOrConst a : ag.getArrays()) {
+					mArrayToArrayGroup.put(a, ag);
+				}
+			}
+		}
+
+		// set up the mapping of terms to ArrayGroups for each edge/TransFormula
+		final NestedMap2<EdgeInfo, Term, ArrayGroup> edgeToTermToArrayGroup = new NestedMap2<>();
+		{
+			for (final Entry<EdgeInfo, UnionFind<Term>> en : edgeToPerEdgeArrayPartition.entrySet()) {
+				for (final Term arrayTerm : en.getValue().getAllElements()) {
+
+					/*
+					 * does the current arrayTerm's partition block contain a term that belongs to a pvoc?
+					 *  if yes: map it to that pvoc's array group
+					 *  if no: map it to the "NoArrayGroup" dummy array group
+					 */
+					final IProgramVarOrConst pvocInSameBlock =
+							findPvoc(en.getKey().getEdge().getTransformula(),
+									en.getValue().getEquivalenceClassMembers(arrayTerm));
+					if (pvocInSameBlock == null) {
+						edgeToTermToArrayGroup.put(en.getKey(), arrayTerm, ArrayGroup.getNoArrayGroup());
+					} else {
+						edgeToTermToArrayGroup.put(en.getKey(), arrayTerm, mArrayToArrayGroup.get(pvocInSameBlock));
+					}
+				}
+			}
+		}
+
+		{
+			final IcfgEdgeIterator it = new IcfgEdgeIterator(icfg);
+			while (it.hasNext()) {
+				final IcfgEdge edge = it.next();
+				final UnmodifiableTransFormula tf = edge.getTransformula();
+				final EdgeInfo edgeInfo = new EdgeInfo(edge);
+
+				final Map<Term, ArrayGroup> arrayTermToArrayGroup = edgeToTermToArrayGroup.get(edgeInfo);
+
+				/*
+				 * construct the StoreIndexInfos
+				 */
+				for (final StoreInfo store : StoreInfo.extractStores(tf.getFormula(), arrayTermToArrayGroup)) {
+					final StoreIndexInfo storeIndexInfo = getOrConstructStoreIndexInfo(edgeInfo, store.getWriteIndex());
+					storeIndexInfo.addArrayAccessDimension(store.getWrittenArray(), store.getWrittenDimension());
+				}
+			}
+		}
+	}
+
+	/**
+	 * Search through the set of terms for a term that belongs to a pvoc according to the given TransFormula.
+	 *
+	 * @return
+	 */
+	private IProgramVarOrConst findPvoc(final TransFormula edge, final Set<Term> terms) {
+		for (final Term term : terms) {
+			final IProgramVarOrConst pvoc = TransFormulaUtils.getProgramVarOrConstForTerm(edge, term);
+			if (pvoc != null) {
+				return pvoc;
+			}
+		}
+		return null;
+	}
+
+	public NestedMap2<EdgeInfo, Term, StoreIndexInfo> getEdgeToIndexToStoreIndexInfo() {
+		return mEdgeToIndexToStoreIndexInfo;
+	}
+
+	public Map<IProgramVarOrConst, ArrayGroup> getArrayToArrayGroup() {
+		return Collections.unmodifiableMap(mArrayToArrayGroup);
+	}
+
+	private Term getArrayId(final Term lhs) {
+		assert lhs.getSort().isArraySort();
+		Term result = lhs;
+		while (SmtUtils.isFunctionApplication(result, "store")) {
+			result = ((ApplicationTerm) result).getParameters()[0];
+		}
+		return result;
+	}
+
+	/**
+	 * updates mEdgeToIndexToStoreIndexInfo
+	 *
+	 * @param tfInfo
+	 * @param indexTerm
+	 * @return
+	 */
+	private StoreIndexInfo getOrConstructStoreIndexInfo(final EdgeInfo tfInfo, final Term indexTerm) {
+		StoreIndexInfo sii = mEdgeToIndexToStoreIndexInfo.get(tfInfo, indexTerm);
+		if (sii == null) {
+			sii = new StoreIndexInfo(tfInfo, indexTerm, mStoreIndexInfoCounter++);
+			mEdgeToIndexToStoreIndexInfo.put(tfInfo, indexTerm, sii);
+		}
+		return sii;
+	}
+
+
+	static class StoreInfo {
+
+		//	private final IProgramVarOrConst mWrittenArray;
+		private final ArrayGroup mWrittenArray;
+		private final int mWrittenDimension;
+		private final Term writeIndex;
+
+		//	public StoreInfo(final IProgramVarOrConst writtenArray, final int writtenDimension, final Term writeIndex) {
+		public StoreInfo(final ArrayGroup writtenArray, final int writtenDimension, final Term writeIndex) {
+			super();
+			mWrittenArray = writtenArray;
+			mWrittenDimension = writtenDimension;
+			this.writeIndex = writeIndex;
+		}
+
+
+		//	public static Set<StoreInfo> extractStores(final Term inputTerm, final TransFormula tf) {
+		public static Set<StoreInfo> extractStores(final Term inputTerm, final Map<Term, ArrayGroup> termToArrayGroup) {
+			final Set<StoreInfo> result = new HashSet<>();
+
+			final Set<ApplicationTerm> allStores = new ApplicationTermFinder("store", false)
+					.findMatchingSubterms(inputTerm);
+
+			for (final ApplicationTerm storeTerm : allStores) {
+
+				final Term arrayTerm = storeTerm.getParameters()[0];
+				final Term index = storeTerm.getParameters()[1];
+
+				Term arrayId = arrayTerm;
+				int dimensionOffset = 0;
+				while (SmtUtils.isFunctionApplication(arrayId, "store")) {
+					dimensionOffset ++;
+					arrayId = ((ApplicationTerm) arrayId).getParameters()[0];
+				}
+
+				final int writtenDimension = new MultiDimensionalSort(arrayId.getSort()).getDimension() - dimensionOffset;
+
+				//			final IProgramVarOrConst arrayPvoc = TransFormulaUtils.getProgramVarOrConstForTerm(tf, arrayId);
+				final ArrayGroup arrayPvoc = termToArrayGroup.get(arrayId);
+				assert arrayPvoc != null;
+
+				result.add(new StoreInfo(arrayPvoc, writtenDimension, index));
+			}
+
+			return result;
+		}
+
+		//	public IProgramVarOrConst getWrittenArray() {
+		public ArrayGroup getWrittenArray() {
+			return mWrittenArray;
+		}
+
+
+		public int getWrittenDimension() {
+			return mWrittenDimension;
+		}
+
+
+		public Term getWriteIndex() {
+			return writeIndex;
+		}
+
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((mWrittenArray == null) ? 0 : mWrittenArray.hashCode());
+			result = prime * result + mWrittenDimension;
+			result = prime * result + ((writeIndex == null) ? 0 : writeIndex.hashCode());
+			return result;
+		}
+
+
+		@Override
+		public boolean equals(final Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			final StoreInfo other = (StoreInfo) obj;
+			if (mWrittenArray == null) {
+				if (other.mWrittenArray != null) {
+					return false;
+				}
+			} else if (!mWrittenArray.equals(other.mWrittenArray)) {
+				return false;
+			}
+			if (mWrittenDimension != other.mWrittenDimension) {
+				return false;
+			}
+			if (writeIndex == null) {
+				if (other.writeIndex != null) {
+					return false;
+				}
+			} else if (!writeIndex.equals(other.writeIndex)) {
+				return false;
+			}
+			return true;
+		}
+
 	}
 }
--- trunk/source/Library-IcfgTransformer/src/de/uni_freiburg/informatik/ultimate/icfgtransformer/heapseparator/MemlocArrayUpdaterIcfgTransformer.java
+++ trunk/source/Library-IcfgTransformer/src/de/uni_freiburg/informatik/ultimate/icfgtransformer/heapseparator/MemlocArrayUpdaterIcfgTransformer.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -28,21 +29,20 @@ import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProg
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramVarOrConst;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SubTermFinder;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.arrays.ArrayUpdate;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.NestedMap2;
 
 /**
- * Applies the "freeze-variables transformation"
+ * Applies the "memloc-array transformation"
  * Performs the following steps:
- *  <li> for each index term that is used in an array update, this introduces a fresh global ProgramVariable, called
- *   the freeze variable, adds a conjunct to the transformula, that nondeterministically assigns the freeze variable the
- *   index term or not.
- *  <li> adds initialization code for all newly introduced freeze variables to the beginning(s) of the program. The
- *   initialization code ensures that initially, a freeze variable is distinct from all other expressions in the program
- *   .. furthermore the valid-array at each freeze variable is set to "1" (this is somewhat hacky, as it is the only
- *   place where the Hoenicke-Lindenmann memory model comes into play..)
- *
- * @author Alexander Nutz (nutz@informatik.uni-freiburg.de)
+ * <ul>
+ *  <li> introduce a fresh array-type variable, called the memloc-array
+ *  <li> at each write to an array in the program, at location l with index variable i:
+ *   <ul>
+ *    <li> introduce a fresh memloc-literal "(l, i)" that represents this write location
+ *    <li> update the memloc array like this: memloc[i] := (l,i);
+ *   </ul>
+ *  <li> make sure that all memloc-literals are assumed as distinct by the solver (may be done outside this class)
+ * </ul>
  *
  * @param <INLOC>
  * @param <OUTLOC>
@@ -58,7 +58,6 @@ public class MemlocArrayUpdaterIcfgTransformer<INLOC extends IcfgLocation, OUTLO
 	private final static boolean TRACK_CONSTANTS = false;
 	private final Set<ConstantTerm> mAllConstantTerms;
 
-	private int mStoreIndexInfoCounter;
 
 	private final IProgramVar mMemlocArrayInt;
 
@@ -68,16 +67,14 @@ public class MemlocArrayUpdaterIcfgTransformer<INLOC extends IcfgLocation, OUTLO
 
 	private final List<IProgramVarOrConst> mHeapArrays;
 
-	public MemlocArrayUpdaterIcfgTransformer(final ILogger logger,
-			final String resultName,
+	public MemlocArrayUpdaterIcfgTransformer(final ILogger logger, final String resultName,
 			final Class<OUTLOC> outLocClazz, final IIcfg<INLOC> inputCfg,
 			final ILocationFactory<INLOC, OUTLOC> funLocFac, final IBacktranslationTracker backtranslationTracker,
-			final IProgramVar memlocArrayInt,
-			final Sort memLocSort, final List<IProgramVarOrConst> heapArrays) {
+			final IProgramVar memlocArrayInt, final Sort memLocSort, final List<IProgramVarOrConst> heapArrays,
+			final NestedMap2<EdgeInfo, Term, StoreIndexInfo> edgeToIndexToStoreIndexInfo) {
 		super(logger, resultName, outLocClazz, inputCfg, funLocFac, backtranslationTracker);
-		mEdgeToIndexToStoreIndexInfo = new NestedMap2<>();
+		mEdgeToIndexToStoreIndexInfo = edgeToIndexToStoreIndexInfo;
 		mAllConstantTerms = TRACK_CONSTANTS ? new HashSet<>() : null;
-		mStoreIndexInfoCounter = 0;
 		mMemlocArrayInt = memlocArrayInt;
 		mMemLocSort = memLocSort;
 		mStoreIndexInfoToLocLiteral = new HashMap<>();
@@ -90,7 +87,7 @@ public class MemlocArrayUpdaterIcfgTransformer<INLOC extends IcfgLocation, OUTLO
 				new EdgeInfo(oldTransition));
 		return super.transform(oldTransition, newSource, newTarget, newTransformula);
 	}
-//
+
 	public final UnmodifiableTransFormula transformTransformula(final UnmodifiableTransFormula tf,
 			final EdgeInfo edgeInfo) {
 
@@ -112,32 +109,47 @@ public class MemlocArrayUpdaterIcfgTransformer<INLOC extends IcfgLocation, OUTLO
 
 		mMgdScript.lock(this);
 
-		final List<ArrayUpdate> aus = ArrayUpdate.extractArrayUpdates(tf.getFormula(), true);
+//		final List<ArrayUpdate> aus = ArrayUpdate.extractArrayUpdates(tf.getFormula(), true);
+
+//		final List<ArrayEqualityAllowStores> aeass = ArrayEqualityAllowStores.extractArrayEqualityAllowStores(tf.getFormula());
+//
+//		final Set<StoreInfo> stores = StoreInfo.extractStores(tf.getFormula(), tf);
 
 		final List<Term> memlocUpdates = new ArrayList<>();
 
-		for (final ArrayUpdate au : aus) {
+//		for (final ArrayUpdate au : aus) {
+//		for (final StoreInfo si : stores) {
+//		for (final ArrayEqualityAllowStores aeas : aeass) {
+		for (final Entry<Term, StoreIndexInfo> en : mEdgeToIndexToStoreIndexInfo.get(edgeInfo).entrySet()) {
 
-			final IProgramVarOrConst lhsPvoc = edgeInfo.getProgramVarOrConstForTerm(au.getNewArray());
-			final IProgramVarOrConst rhsPvoc = edgeInfo.getProgramVarOrConstForTerm(au.getOldArray());
-			if (!lhsPvoc.equals(rhsPvoc)){
-				/*
-				 *  we are only interested in array updates that update one cell here, i.e., the lhs and rhs array must
-				 *  refer to the same program variable
-				 */
-				continue;
-			}
+			final StoreIndexInfo storeIndexInfo = en.getValue();
 
-			if (!mHeapArrays.contains(lhsPvoc)) {
-				/* we are only interested in writes to heap arrays */
-				continue;
-			}
+//			final IProgramVarOrConst lhsPvoc = edgeInfo.getProgramVarOrConstForTerm(au.getNewArray());
+//			final IProgramVarOrConst rhsPvoc = edgeInfo.getProgramVarOrConstForTerm(au.getOldArray());
+//			if (!lhsPvoc.equals(rhsPvoc)){
+//				/*
+//				 *  we are only interested in array updates that update one cell here, i.e., the lhs and rhs array must
+//				 *  refer to the same program variable
+//				 */
+//				continue;
+//			}
 
-			for (int dim = 0; dim < au.getIndex().size(); dim++) {
-				final Term indexTerm = au.getIndex().get(dim);
-
-				final StoreIndexInfo storeIndexInfo = getStoreIndexInfo(edgeInfo, indexTerm);
-				storeIndexInfo.addArrayAccessDimension(lhsPvoc, dim);
+//			if (!mHeapArrays.contains(lhsPvoc)) {
+//			if (!mHeapArrays.contains(si.getWrittenArray())) {
+//				/* we are only interested in writes to heap arrays */
+//				continue;
+//			}
+//
+////			for (int dim = 0; dim < au.getIndex().size(); dim++) {
+////				final Term indexTerm = au.getIndex().get(dim);
+//			final int dim = si.getWrittenDimension();
+//			final Term indexTerm = si.getWriteIndex();
+			final Term indexTerm = storeIndexInfo.getIndexTerm();
+//
+////				final StoreIndexInfo storeIndexInfo = getOrConstructStoreIndexInfo(edgeInfo, indexTerm);
+//				final StoreIndexInfo storeIndexInfo = mEdgeToIndexToStoreIndexInfo.get(edgeInfo, indexTerm);
+//				storeIndexInfo.addArrayAccessDimension(lhsPvoc, dim);
+//				storeIndexInfo.addArrayAccessDimension(si.getWrittenArray(), dim);
 
 				final IProgramConst locLit = getLocationLiteral(storeIndexInfo);
 				extraConstants.add(locLit);
@@ -165,7 +177,7 @@ public class MemlocArrayUpdaterIcfgTransformer<INLOC extends IcfgLocation, OUTLO
 						SmtUtils.store(mMgdScript.getScript(), memlocIntInVar, indexTerm, locLit.getTerm()));
 
 				memlocUpdates.add(arrayUpdateTerm);
-			}
+//			}
 		}
 
 		mMgdScript.unlock(this);
@@ -212,14 +224,14 @@ public class MemlocArrayUpdaterIcfgTransformer<INLOC extends IcfgLocation, OUTLO
 		return "mll_" + storeIndexInfo.getEdgeInfo().getSourceLocation() + "_" + mMemLocLitCounter ++;
 	}
 
-	private StoreIndexInfo getStoreIndexInfo(final EdgeInfo tfInfo, final Term indexTerm) {
-		StoreIndexInfo sii = mEdgeToIndexToStoreIndexInfo.get(tfInfo, indexTerm);
-		if (sii == null) {
-			sii = new StoreIndexInfo(tfInfo, indexTerm, mStoreIndexInfoCounter++);
-			mEdgeToIndexToStoreIndexInfo.put(tfInfo, indexTerm, sii);
-		}
-		return sii;
-	}
+//	private StoreIndexInfo getOrConstructStoreIndexInfo(final EdgeInfo tfInfo, final Term indexTerm) {
+//		StoreIndexInfo sii = mEdgeToIndexToStoreIndexInfo.get(tfInfo, indexTerm);
+//		if (sii == null) {
+//			sii = new StoreIndexInfo(tfInfo, indexTerm, mStoreIndexInfoCounter++);
+//			mEdgeToIndexToStoreIndexInfo.put(tfInfo, indexTerm, sii);
+//		}
+//		return sii;
+//	}
 
 	public NestedMap2<EdgeInfo, Term, StoreIndexInfo> getEdgeToIndexToStoreIndexInfo() {
 		return mEdgeToIndexToStoreIndexInfo;
@@ -245,4 +257,6 @@ public class MemlocArrayUpdaterIcfgTransformer<INLOC extends IcfgLocation, OUTLO
 	}
 
 }
+
+
 
