04325adc76c33a75d06cedfa600c78dcb1f1302a
danieldietsch <daniel.dietsch@gmail.com>
2018-03-08 10:12:13 +0100

    fix merge in BoogieLocation by not merging if the same location is presented

--- trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/BoogieLocation.java
+++ trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/BoogieLocation.java
@@ -2,22 +2,22 @@
  * Copyright (C) 2013-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Core.
- * 
+ *
  * The ULTIMATE Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Core. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
@@ -40,7 +40,7 @@ import de.uni_freiburg.informatik.ultimate.core.model.models.annotation.IAnnotat
 
 /**
  * Location in a boogie program.
- * 
+ *
  * @author heizmann@informatik.uni-freiburg.de
  */
 public class BoogieLocation extends DefaultLocation {
@@ -87,6 +87,9 @@ public class BoogieLocation extends DefaultLocation {
 	@Override
 	public IAnnotations merge(final IAnnotations other) {
 		if (other == null) {
+			return this;
+		}
+		if (other == this) {
 			return this;
 		}
 		if (other instanceof ILocation) {
