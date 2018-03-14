b3ee2df8cbc3582670b69baf73a94853987ea537
danieldietsch <daniel.dietsch@gmail.com>
2018-03-14 14:40:45 +0100

    more C translation issues

---
+++ trunk/examples/CToBoogieTranslation/regression/ctrans-CCE-cprimitives.c
@@ -0,0 +1,10 @@
+typedef long unsigned int __uint32_t;
+typedef __uint32_t uint32_t ;
+
+uint32_t random_uniform( ){ return 0;}
+
+int main()
+{
+  int j = random_uniform( );
+}
+
---
+++ trunk/examples/CToBoogieTranslation/regression/ctrans-bug-min-TE-strange.c
@@ -0,0 +1,7 @@
+int * const pubkey  = { "" };
+
+void main()
+{
+  pubkey;
+}
+
