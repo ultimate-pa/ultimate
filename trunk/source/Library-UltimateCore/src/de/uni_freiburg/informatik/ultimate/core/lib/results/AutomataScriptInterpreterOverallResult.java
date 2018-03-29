1d4f54988b546f8ba55915ab8215f4150c3e8c40
danieldietsch <daniel.dietsch@gmail.com>
2018-03-29 10:25:28 +0200

    cc fixes

--- trunk/source/Library-UltimateCore/src/de/uni_freiburg/informatik/ultimate/core/lib/results/AutomataScriptInterpreterOverallResult.java
+++ trunk/source/Library-UltimateCore/src/de/uni_freiburg/informatik/ultimate/core/lib/results/AutomataScriptInterpreterOverallResult.java
@@ -2,27 +2,27 @@
  * Copyright (C) 2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2014-2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Core.
- * 
+ *
  * The ULTIMATE Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Core. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Core grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Core grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.core.lib.results;
@@ -30,33 +30,29 @@ package de.uni_freiburg.informatik.ultimate.core.lib.results;
 import de.uni_freiburg.informatik.ultimate.core.model.results.IResultWithSeverity;
 
 /**
- * While interpreting an automata script file such an overall result is
- * constructed.
+ * While interpreting an automata script file such an overall result is constructed.
+ *
  * @author Matthias Heizmann
  *
  */
-public class AutomataScriptInterpreterOverallResult extends AbstractResult
-		implements IResultWithSeverity {
-	
+public class AutomataScriptInterpreterOverallResult extends AbstractResult implements IResultWithSeverity {
+
 	public enum OverallResult {
-		ALL_ASSERTIONS_HOLD, 
-		NO_ASSERTION, 
-		SOME_ASSERTION_FAILED, 
-		EXCEPTION_OR_ERROR, 
-		TIMEOUT,
-		OUT_OF_MEMORY
-//		SYNTAX_ERROR
+		ALL_ASSERTIONS_HOLD, NO_ASSERTION, SOME_ASSERTION_FAILED, EXCEPTION_OR_ERROR, TIMEOUT, OUT_OF_MEMORY
+		// SYNTAX_ERROR
 	}
-	
+
 	private final OverallResult mOverallResult;
 	private final String mErrorMessage;
-	
-	public AutomataScriptInterpreterOverallResult(final String plugin, final OverallResult overallResult, final String errorMessage) {
+
+	public AutomataScriptInterpreterOverallResult(final String plugin, final OverallResult overallResult,
+			final String errorMessage) {
 		super(plugin);
 		if (errorMessage == null && overallResult == OverallResult.EXCEPTION_OR_ERROR) {
 			throw new UnsupportedOperationException("provide error message if there was an error");
 		}
-		if (errorMessage != null && overallResult != OverallResult.EXCEPTION_OR_ERROR && overallResult != OverallResult.TIMEOUT) {
+		if (errorMessage != null && overallResult != OverallResult.EXCEPTION_OR_ERROR
+				&& overallResult != OverallResult.TIMEOUT) {
 			throw new UnsupportedOperationException("provide error message only if there was an error or timeout");
 		}
 		mOverallResult = overallResult;
@@ -74,7 +70,7 @@ public class AutomataScriptInterpreterOverallResult extends AbstractResult
 			return "Finished interpretation of automata script.";
 		case SOME_ASSERTION_FAILED:
 			return "Some assert statements have been evaluated to false.";
-//		case SYNTAX_ERROR:
+		// case SYNTAX_ERROR:
 		case TIMEOUT:
 			return "Timeout during interpretation of automata script.";
 		case OUT_OF_MEMORY:
@@ -108,7 +104,7 @@ public class AutomataScriptInterpreterOverallResult extends AbstractResult
 			return Severity.INFO;
 		case SOME_ASSERTION_FAILED:
 			return Severity.ERROR;
-//		case SYNTAX_ERROR:
+		// case SYNTAX_ERROR:
 		case TIMEOUT:
 			return Severity.WARNING;
 		case OUT_OF_MEMORY:
@@ -125,11 +121,8 @@ public class AutomataScriptInterpreterOverallResult extends AbstractResult
 	public String getErrorMessage() {
 		if (mErrorMessage == null) {
 			throw new UnsupportedOperationException("there is no error message, because there was no error");
-		} else {
-			return mErrorMessage;
 		}
+		return mErrorMessage;
 	}
-	
-	
 
 }
