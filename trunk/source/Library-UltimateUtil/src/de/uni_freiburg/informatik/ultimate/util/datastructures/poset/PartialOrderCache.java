30ee92d1259f33f32b95bf4ec070b008c8eaf31f
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-04-03 17:17:23 +0200

    #290, benchmarking for PartialOrderCache

--- trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/poset/PartialOrderCache.java
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/poset/PartialOrderCache.java
@@ -37,6 +37,7 @@ import java.util.Set;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.UnionFind;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.poset.IPartialComparator.ComparisonResult;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
+import de.uni_freiburg.informatik.ultimate.util.statistics.BenchmarkWithCounters;
 
 public class PartialOrderCache<E> {
 
@@ -50,18 +51,32 @@ public class PartialOrderCache<E> {
 
 	private final Set<E> mMaximalElements;
 
+	private final boolean mBenchmarkMode;
+	private BenchmarkWithCounters mBenchmark;
+
 	public PartialOrderCache(final IPartialComparator<E> comparator) {
 		mComparator = comparator;
 		mStrictlySmaller = new HashRelation<>();
 		mNotStrictlySmaller = new HashRelation<>();
 		mEquivalences = new UnionFind<>();
 		mMaximalElements = new HashSet<>();
+
+		mBenchmarkMode = true;
+		if (mBenchmarkMode) {
+			mBenchmark = new BenchmarkWithCounters();
+			mBenchmark.registerCountersAndWatches(PocBmNames.getNames());
+		} else {
+			mBenchmark = null;
+		}
+
 	}
 
 	public E addElement(final E elemToAdd) {
+		bmStart(PocBmNames.ELEMENTS_ADDED);
 		E rep = mEquivalences.find(elemToAdd);
 		if (rep != null) {
 			// we already know this element, return the representative
+			bmEnd(PocBmNames.ELEMENTS_ADDED);
 			return rep;
 		}
 		// elemToAdd element is new
@@ -74,7 +89,10 @@ public class PartialOrderCache<E> {
 				// nothing to do
 				continue;
 			}
-			switch (mComparator.compare(elemToAdd, otherRep)) {
+			bmStart(PocBmNames.ORDER_REQUESTS_MADE);
+			final ComparisonResult comparisonResult = mComparator.compare(elemToAdd, otherRep);
+			bmEnd(PocBmNames.ORDER_REQUESTS_MADE);
+			switch (comparisonResult) {
 			case EQUAL:
 				mEquivalences.union(rep, otherRep);
 				final E newRep = mEquivalences.find(rep);
@@ -91,6 +109,7 @@ public class PartialOrderCache<E> {
 					mStrictlySmaller.replaceRangeElement(rep, newRep);
 				}
 				assert sanityCheck();
+				bmEnd(PocBmNames.ELEMENTS_ADDED);
 				return newRep;
 			case STRICTLY_SMALLER:
 				addStrictlySmaller(elemToAdd, otherRep);
@@ -105,6 +124,7 @@ public class PartialOrderCache<E> {
 			}
 		}
 
+		bmEnd(PocBmNames.ELEMENTS_ADDED);
 		assert sanityCheck();
 		return rep;
 	}
@@ -126,6 +146,7 @@ public class PartialOrderCache<E> {
 	}
 
 	public boolean lowerEqual(final E elem1, final E elem2) {
+		bmStart(PocBmNames.ORDER_REQUESTS_ANSWERED);
 		if (elem1 == elem2) {
 			return true;
 		}
@@ -134,13 +155,16 @@ public class PartialOrderCache<E> {
 		final E rep2 = addElement(elem2);
 		if (rep1 == rep2) {
 			// elements are equal
+			bmEnd(PocBmNames.ORDER_REQUESTS_ANSWERED);
 			return true;
 		}
 		/*
 		 * elements are not equal
 		 * We need to test if there is a path through the graph mStrictlySmaller from rep1 to rep2.
 		 */
-		return strictlySmaller(rep1, rep2);
+		final boolean result = strictlySmaller(rep1, rep2);
+		bmEnd(PocBmNames.ORDER_REQUESTS_ANSWERED);
+		return result;
 	}
 
 	protected boolean strictlySmaller(final E rep1, final E rep2) {
@@ -173,9 +197,9 @@ public class PartialOrderCache<E> {
 		return false;
 	}
 
-	public boolean greaterEqual(final E elem1, final E elem2) {
-		throw new UnsupportedOperationException("not yet implemented");
-	}
+//	public boolean greaterEqual(final E elem1, final E elem2) {
+//		throw new UnsupportedOperationException("not yet implemented");
+//	}
 
 	/**
 	 * Get the maximal elements from to the given list (or elements equivalent to those)
@@ -254,4 +278,46 @@ public class PartialOrderCache<E> {
 	public boolean knowsElement(final E elem) {
 		return mEquivalences.find(elem) != null;
 	}
+
+	private static enum PocBmNames {
+
+		ORDER_REQUESTS_MADE, ORDER_REQUESTS_ANSWERED, ELEMENTS_ADDED;
+
+		static String[] getNames() {
+			final String[] result = new String[values().length];
+			for (int i = 0; i < values().length; i++) {
+				result[i] = values()[i].name();
+			}
+			return result;
+		}
+	}
+
+	public boolean hasBenchmark() {
+		return mBenchmarkMode;
+	}
+
+	public BenchmarkWithCounters getBenchmark() {
+		if (!hasBenchmark()) {
+			throw new IllegalStateException();
+		}
+		return mBenchmark;
+	}
+
+	private void bmStart(final PocBmNames watch) {
+		if (!mBenchmarkMode) {
+			return;
+		}
+//		bmStartOverall();
+		mBenchmark.incrementCounter(watch.name());
+		mBenchmark.unpauseWatch(watch.name());
+	}
+
+	private void bmEnd(final PocBmNames watch) {
+		if (!mBenchmarkMode) {
+			return;
+		}
+//		bmEndOverall();
+		mBenchmark.pauseWatch(watch.name());
+	}
+
 }
