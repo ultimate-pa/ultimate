428d34d19443c15648ee95c16152fe031004a9b5
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-04-04 10:06:23 +0200

    license header, comment

--- trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/statistics/BenchmarkWithCounters.java
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/statistics/BenchmarkWithCounters.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (C) 2017-2018 Alexander Nutz (nutz@informatik.uni-freiburg.de)
+ * Copyright (C) 2017-2018 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Util Library.
+ *
+ * The ULTIMATE Util Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Util Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Util Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Util Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Util Library grant you additional permission
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.util.statistics;
 
 import java.util.ArrayList;
@@ -12,11 +38,12 @@ import de.uni_freiburg.informatik.ultimate.util.csv.ICsvProviderProvider;
 import de.uni_freiburg.informatik.ultimate.util.csv.SimpleCsvProvider;
 
 /**
+ * Combines an instance of {@link Benchmark} with counters. Whenever a watch is unpaused, the counter of the same name
+ * is incremented.
  *
  * @author Alexander Nutz (nutz@informatik.uni-freiburg.de)
  *
  */
-//public class BenchmarkWithCounters extends Benchmark {
 public class BenchmarkWithCounters implements ICsvProviderProvider<Number> {
 
 	private final Benchmark mWatchBenchmark = new Benchmark();
