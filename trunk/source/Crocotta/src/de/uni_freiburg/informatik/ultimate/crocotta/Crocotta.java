428b910caae00309ad1e52711adb0d38e74448a6
danieldietsch <daniel.dietsch@gmail.com>
2018-04-18 16:26:00 +0200

    new source plugin Crocotta for automata fixpoints

---
+++ trunk/source/Crocotta/src/de/uni_freiburg/informatik/ultimate/crocotta/Crocotta.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2018 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ * Copyright (C) 2018 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Crocotta plug-in.
+ *
+ * The ULTIMATE Crocotta plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Crocotta plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Crocotta plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Crocotta plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Crocotta plug-in grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.crocotta;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import de.uni_freiburg.informatik.ultimate.core.model.ISource;
+import de.uni_freiburg.informatik.ultimate.core.model.models.IElement;
+import de.uni_freiburg.informatik.ultimate.core.model.models.ModelType;
+import de.uni_freiburg.informatik.ultimate.core.model.preferences.IPreferenceInitializer;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IToolchainStorage;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+
+public class Crocotta implements ISource {
+	private ILogger mLogger;
+	private final List<String> mFileNames = new ArrayList<>();
+	private IUltimateServiceProvider mServices;
+
+	@Override
+	public void init() {
+		// not necessary
+	}
+
+	@Override
+	public String getPluginName() {
+		return Activator.PLUGIN_NAME;
+	}
+
+	@Override
+	public String getPluginID() {
+		return Activator.PLUGIN_ID;
+	}
+
+	@Override
+	public File[] parseable(final File[] files) {
+		final List<File> rtrList = Arrays.stream(files).filter(this::parseable).collect(Collectors.toList());
+		return rtrList.toArray(new File[rtrList.size()]);
+	}
+
+	public boolean parseable(final File file) {
+		return Arrays.stream(getFileTypes()).anyMatch(a -> file.getName().endsWith(a));
+	}
+
+	@Override
+	public IElement parseAST(final File[] files) throws Exception {
+		return null;
+	}
+
+	@Override
+	public String[] getFileTypes() {
+		return new String[] { ".croc" };
+	}
+
+	@Override
+	public ModelType getOutputDefinition() {
+		try {
+			return new ModelType(getPluginID(), ModelType.Type.AST, mFileNames);
+		} catch (final Exception ex) {
+			mLogger.fatal("syntax error: " + ex.getMessage());
+			return null;
+		}
+	}
+
+	@Override
+	public IPreferenceInitializer getPreferences() {
+		return null;
+	}
+
+	@Override
+	public void setToolchainStorage(final IToolchainStorage storage) {
+		// not necessary
+	}
+
+	@Override
+	public void setServices(final IUltimateServiceProvider services) {
+		mServices = services;
+		mLogger = services.getLoggingService().getLogger(Activator.PLUGIN_ID);
+	}
+
+	@Override
+	public void finish() {
+		// not necessary
+	}
+}
