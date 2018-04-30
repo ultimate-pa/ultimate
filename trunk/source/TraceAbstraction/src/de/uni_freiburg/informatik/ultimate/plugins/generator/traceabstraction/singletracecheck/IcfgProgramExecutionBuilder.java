4ab2480271b77a92d867a322386d7fa125546f38
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-04-30 16:21:04 +0200

    IcfgProgramExecutionBuilder: if no value is known for an expression (at a certain point in the code), omit translation (before we an assertion that ensured there is a value, and a NPE, when running -da)

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/singletracecheck/IcfgProgramExecutionBuilder.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/singletracecheck/IcfgProgramExecutionBuilder.java
@@ -2,22 +2,22 @@
  * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2013-2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE TraceAbstraction plug-in.
- * 
+ *
  * The ULTIMATE TraceAbstraction plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE TraceAbstraction plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE TraceAbstraction plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE TraceAbstraction plug-in, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
@@ -154,8 +154,9 @@ public class IcfgProgramExecutionBuilder {
 			if (SmtUtils.isSortForWhichWeCanGetValues(bv.getTermVariable().getSort())) {
 				final int assignPos = indexWhereVarWasAssignedTheLastTime(bv, position);
 				final Term value = mvar2pos2value.get(bv).get(assignPos);
-				assert value != null;
-				result.put(bv, value);
+				if (value != null) {
+					result.put(bv, value);
+				}
 			}
 		}
 		return result;
