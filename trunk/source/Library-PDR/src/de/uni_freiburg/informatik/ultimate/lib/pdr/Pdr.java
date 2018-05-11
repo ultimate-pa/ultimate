5bc8bb7db65d22982cc4e48bce94264ccda2d431
JonasWerner <jonaswerner95@gmail.com>
2018-05-11 17:45:50 +0200

    add functionality to PDR

--- trunk/source/Library-PDR/src/de/uni_freiburg/informatik/ultimate/lib/pdr/Pdr.java
+++ trunk/source/Library-PDR/src/de/uni_freiburg/informatik/ultimate/lib/pdr/Pdr.java
@@ -25,13 +25,25 @@
  */
 package de.uni_freiburg.informatik.ultimate.lib.pdr;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.CfgSmtToolkit;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.IcfgUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfg;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgEdge;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgEdgeFactory;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgLocation;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgLocationIterator;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicateFactory;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
@@ -39,6 +51,7 @@ import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPre
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.PredicateTransformer;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.TermDomainOperationProvider;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.rcfgbuilder.util.IcfgProgramExecution;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Triple;
 
 /**
  *
@@ -49,19 +62,128 @@ public class Pdr<LOC extends IcfgLocation> {
 
 	private final ILogger mLogger;
 	private final IUltimateServiceProvider mServices;
+	private final Map<IcfgLocation, ArrayList<Term>> mFrames;
+	private ManagedScript mScript;
 
 	public Pdr(final ILogger logger, final IUltimateServiceProvider services, final Object settings) {
 		mLogger = logger;
 		mServices = services;
+		mFrames = new HashMap<>();
+		mScript = null;
 	}
 
 	PdrResult computePdr(final IIcfg<LOC> icfg, final IPredicateUnifier predicateUnifier) {
 		final CfgSmtToolkit csToolkit = icfg.getCfgSmtToolkit();
-		final ManagedScript mgdScript = csToolkit.getManagedScript();
 		final BasicPredicateFactory predicateFac = predicateUnifier.getPredicateFactory();
-		final PredicateTransformer predTrans =
-				new PredicateTransformer<>(mgdScript, new TermDomainOperationProvider(mServices, mgdScript));
-		return null;
+
+		mScript = csToolkit.getManagedScript();
+
+		final PredicateTransformer predTrans = new PredicateTransformer<>(mScript,
+				new TermDomainOperationProvider(mServices, mScript));
+
+		final Set<LOC> init = icfg.getInitialNodes();
+		final Set<LOC> error = IcfgUtils.getErrorLocations(icfg);
+
+		/**
+		 * Check for 0-Counter-Example
+		 */
+		for (final IcfgLocation initNode : init) {
+			if (error.contains(initNode)) {
+				return null;
+			}
+		}
+
+		/**
+		 * Initialize level 0.
+		 */
+		final IcfgLocationIterator<LOC> iter = new IcfgLocationIterator<>(icfg);
+		while (iter.hasNext()) {
+			IcfgLocation loc = iter.next();
+			mFrames.put(loc, new ArrayList<>());
+			if (init.contains(loc)) {
+				mFrames.get(loc).add(mScript.getScript().term("true"));
+			} else {
+				mFrames.get(loc).add(mScript.getScript().term("false"));
+			}
+		}
+
+		while (true) {
+			/**
+			 * Initialize new level.
+			 */
+			for (Entry<IcfgLocation, ArrayList<Term>> trace : mFrames.entrySet()) {
+				trace.getValue().add(mScript.getScript().term("true"));
+			}
+
+			final ArrayList<Triple<Term, IcfgLocation, Integer>> proofObligations = new ArrayList<>();
+
+			/**
+			 * Error Access violation:
+			 */
+//			for (final IcfgEdge edge : error.iterator().next().getIncomingEdges()) {
+//				Term proofObligationTerm = edge.getTransformula().getFormula();
+//			}
+
+			/**
+			 * Generated proof-obligation on level 0 -> error is reachable
+			 */
+			if (!blockingPhase(proofObligations)) {
+				PdrResult result = new PdrResult();
+				return result;
+			}
+
+			/**
+			 * Found invariant -> error is not reachable
+			 */
+			if (propagationPhase()) {
+				PdrResult result = new PdrResult();
+				return result;
+			}
+		}
+	}
+
+	/**
+	 * Blocking-phase, for blocking proof-obligations.
+	 * 
+	 * @return false, if proof-obligation on level 0 is created true, if there
+	 *         is no proof-obligation left
+	 */
+	private boolean blockingPhase(final ArrayList<Triple<Term, IcfgLocation, Integer>> initialProofObligations) {
+		Deque<Triple<Term, IcfgLocation, Integer>> proofObligations = new ArrayDeque<>(initialProofObligations);
+
+		while (!proofObligations.isEmpty()) {
+			Triple<Term, IcfgLocation, Integer> proofObligation = proofObligations.pop();
+			final Term toBeBlocked = proofObligation.getFirst();
+			final IcfgLocation location = proofObligation.getSecond();
+			final int level = proofObligation.getThird();
+
+			/**
+			 * for (final IcfgLocation predecessor :
+			 * location.getIncomingNodes()) {
+			 * 
+			 * }
+			 */
+
+		}
+
+		return false;
+	}
+
+	/**
+	 * Propagation-Phase, for finding invariants
+	 */
+	private boolean propagationPhase() {
+		for (final Entry<IcfgLocation, ArrayList<Term>> locationTrace : mFrames.entrySet()) {
+			final ArrayList<Term> frames = locationTrace.getValue();
+			for (int i = 0; i < frames.size(); i++) {
+				for (int k = i + 1; k < frames.size(); k++) {
+					if (frames.get(i) == frames.get(k)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
 
 	private final class PdrResult {
