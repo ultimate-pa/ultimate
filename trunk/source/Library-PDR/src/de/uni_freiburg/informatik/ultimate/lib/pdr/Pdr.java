ccc3a180098162ed04c9dfb9692b7699a6356c40
danieldietsch <daniel.dietsch@gmail.com>
2018-05-23 12:45:38 +0200

    comments and updates to Pdr

--- trunk/source/Library-PDR/src/de/uni_freiburg/informatik/ultimate/lib/pdr/Pdr.java
+++ trunk/source/Library-PDR/src/de/uni_freiburg/informatik/ultimate/lib/pdr/Pdr.java
@@ -29,14 +29,13 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
-import de.uni_freiburg.informatik.ultimate.logic.ApplicationTerm;
-import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.CfgSmtToolkit;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.IcfgUtils;
@@ -44,11 +43,14 @@ import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfg
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfgCallTransition;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfgInternalTransition;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IIcfgReturnTransition;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IInternalAction;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgEdge;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgLocation;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.structure.IcfgLocationIterator;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.transitions.TransFormula;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.cfg.variables.IProgramNonOldVar;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.hoaretriple.IHoareTripleChecker;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.hoaretriple.IHoareTripleChecker.Validity;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.BasicPredicate;
@@ -69,33 +71,32 @@ public class Pdr<LOC extends IcfgLocation> {
 
 	private final ILogger mLogger;
 	private final IUltimateServiceProvider mServices;
-	private final Map<IcfgLocation, ArrayList<Term>> mFrames;
-	private ManagedScript mScript;
-	private BasicPredicateFactory mPredicateFac;
-	private PredicateTransformer<Term, IPredicate, TransFormula> mPredTrans;
-	private IIcfg<LOC> mIcfg;
-	private CfgSmtToolkit mCsToolkit;
+	private final Map<IcfgLocation, List<Term>> mFrames;
+	private final ManagedScript mScript;
+	private final PredicateTransformer<Term, IPredicate, TransFormula> mPredTrans;
+	private final IIcfg<LOC> mIcfg;
+	private final CfgSmtToolkit mCsToolkit;
+	private final IHoareTripleChecker mHtc;
+	private final IPredicateUnifier mPredicateUnifier;
 
-	public Pdr(final ILogger logger, final IUltimateServiceProvider services, final Object settings) {
+	public Pdr(final ILogger logger, final IUltimateServiceProvider services, final IIcfg<LOC> icfg,
+			final IPredicateUnifier predicateUnifier, final IHoareTripleChecker htc, final Object settings) {
 		mLogger = logger;
 		mServices = services;
 		mFrames = new HashMap<>();
-		mScript = null;
-		mPredicateFac = null;
-		mPredTrans = null;
-		mIcfg = null;
-		mCsToolkit = null;
-	}
-
-	PdrResult computePdr(final IIcfg<LOC> icfg, final IPredicateUnifier predicateUnifier) {
 		mCsToolkit = icfg.getCfgSmtToolkit();
 		mScript = mCsToolkit.getManagedScript();
-		mPredicateFac = predicateUnifier.getPredicateFactory();
+		mPredicateUnifier = predicateUnifier;
 		mPredTrans = new PredicateTransformer<>(mScript, new TermDomainOperationProvider(mServices, mScript));
 		mIcfg = icfg;
+		mHtc = htc;
+		computePdr();
+	}
 
-		final Set<LOC> init = icfg.getInitialNodes();
-		final Set<LOC> error = IcfgUtils.getErrorLocations(icfg);
+	private PdrResult computePdr() {
+
+		final Set<LOC> init = mIcfg.getInitialNodes();
+		final Set<LOC> error = IcfgUtils.getErrorLocations(mIcfg);
 
 		/**
 		 * Check for 0-Counter-Example
@@ -109,7 +110,7 @@ public class Pdr<LOC extends IcfgLocation> {
 		/**
 		 * Initialize level 0.
 		 */
-		final IcfgLocationIterator<LOC> iter = new IcfgLocationIterator<>(icfg);
+		final IcfgLocationIterator<LOC> iter = new IcfgLocationIterator<>(mIcfg);
 		while (iter.hasNext()) {
 			final IcfgLocation loc = iter.next();
 			mFrames.put(loc, new ArrayList<>());
@@ -126,21 +127,21 @@ public class Pdr<LOC extends IcfgLocation> {
 			/**
 			 * Initialize new level.
 			 */
-			for (final Entry<IcfgLocation, ArrayList<Term>> trace : mFrames.entrySet()) {
+			for (final Entry<IcfgLocation, List<Term>> trace : mFrames.entrySet()) {
 				trace.getValue().add(mScript.getScript().term("true"));
 			}
 
 			level += 1;
 
-			final ArrayList<Triple<Term, IcfgLocation, Integer>> proofObligations = new ArrayList<>();
+			final List<Triple<Term, IcfgLocation, Integer>> proofObligations = new ArrayList<>();
 
 			/**
 			 * Generate the initial proof-obligations
 			 */
 			for (final IcfgEdge edge : error.iterator().next().getIncomingEdges()) {
 				final Term proofObligationTerm = edge.getTransformula().getFormula();
-				final Triple<Term, IcfgLocation, Integer> initialProofObligation = new Triple<>(proofObligationTerm,
-						edge.getSource(), level);
+				final Triple<Term, IcfgLocation, Integer> initialProofObligation =
+						new Triple<>(proofObligationTerm, edge.getSource(), level);
 				proofObligations.add(initialProofObligation);
 			}
 
@@ -165,13 +166,12 @@ public class Pdr<LOC extends IcfgLocation> {
 	/**
 	 * Blocking-phase, for blocking proof-obligations.
 	 *
-	 * @return false, if proof-obligation on level 0 is created true, if there
-	 *         is no proof-obligation left
+	 * @return false, if proof-obligation on level 0 is created true, if there is no proof-obligation left
 	 */
-	private boolean blockingPhase(final ArrayList<Triple<Term, IcfgLocation, Integer>> initialProofObligations) {
+	private boolean blockingPhase(final List<Triple<Term, IcfgLocation, Integer>> initialProofObligations) {
 
 		final Deque<Triple<Term, IcfgLocation, Integer>> proofObligations = new ArrayDeque<>(initialProofObligations);
-
+		final BasicPredicateFactory prefac = mPredicateUnifier.getPredicateFactory();
 		while (!proofObligations.isEmpty()) {
 			final Triple<Term, IcfgLocation, Integer> proofObligation = proofObligations.pop();
 			final Term toBeBlocked = proofObligation.getFirst();
@@ -182,13 +182,20 @@ public class Pdr<LOC extends IcfgLocation> {
 				final IcfgLocation predecessor = predecessorTransition.getSource();
 				final Term predecessorFrame = mFrames.get(predecessor).get(level - 1);
 				final Term transition = predecessorTransition.getTransformula().getFormula();
-				final Term primedtoBeBlocked = getPrimedTerm(toBeBlocked);
-				final LBool result = SmtUtils.checkSatTerm(mScript.getScript(),
-						SmtUtils.and(mScript.getScript(), predecessorFrame, transition, primedtoBeBlocked));
+
+				// convert all terms that should be used in htc to predicates
+				// Note: HTC checks P && S && !Q, so we have to negate the postcondition
+				// negate postcondition with notP=mPredicateUnifier.getPredicateFactory().not(p) and
+				// mPredicateUnifier.getOrConstruct(notP)
+				//
+				final Validity result = mHtc.checkInternal((IPredicate) predecessorFrame,
+						(IInternalAction) predecessorTransition, (IPredicate) toBeBlocked);
+				// final LBool result = SmtUtils.checkSatTerm(mScript.getScript(),
+				// SmtUtils.and(mScript.getScript(), predecessorFrame, transition, primedtoBeBlocked));
 				/**
 				 * If Sat generate new proof-obligation
 				 */
-				if (result == LBool.SAT) {
+				if (result == Validity.INVALID) {
 					/**
 					 * Found Error trace
 					 */
@@ -196,7 +203,7 @@ public class Pdr<LOC extends IcfgLocation> {
 						return false;
 					}
 
-					final BasicPredicate pred = mPredicateFac.newPredicate(primedtoBeBlocked);
+					final BasicPredicate pred = prefac.newPredicate(toBeBlocked);
 
 					final Term preCondition;
 
@@ -221,24 +228,24 @@ public class Pdr<LOC extends IcfgLocation> {
 								"Unknown transition type: " + predecessorTransition.getClass().toString());
 					}
 
-					final Triple<Term, IcfgLocation, Integer> newProofObligation = new Triple<>(preCondition,
-							predecessor, level - 1);
+					final Triple<Term, IcfgLocation, Integer> newProofObligation =
+							new Triple<>(preCondition, predecessor, level - 1);
 					proofObligations.addFirst(newProofObligation);
 
 					/**
 					 * If Unsat strengthen the frames of the location
 					 */
-				} else if (result == LBool.UNSAT) {
+				} else if (result == Validity.VALID) {
 					for (int i = 0; i <= level; i++) {
 						Term fTerm = mFrames.get(location).get(i);
 						final Term negToBeBlocked = SmtUtils.neg(mScript.getScript(), toBeBlocked);
 						fTerm = SmtUtils.and(mScript.getScript(), fTerm, negToBeBlocked);
 						mFrames.get(location).set(i, fTerm);
 					}
-				} else{
+				} else {
 					/**
 					 * What do if unknown?
-					 * 
+					 *
 					 */
 					throw new UnsupportedOperationException("what to do with unknown?");
 				}
@@ -251,8 +258,8 @@ public class Pdr<LOC extends IcfgLocation> {
 	 * Propagation-Phase, for finding invariants
 	 */
 	private boolean propagationPhase() {
-		for (final Entry<IcfgLocation, ArrayList<Term>> locationTrace : mFrames.entrySet()) {
-			final ArrayList<Term> frames = locationTrace.getValue();
+		for (final Entry<IcfgLocation, List<Term>> locationTrace : mFrames.entrySet()) {
+			final List<Term> frames = locationTrace.getValue();
 			for (int i = 0; i < frames.size(); i++) {
 				for (int k = i + 1; k < frames.size(); k++) {
 					if (frames.get(i) == frames.get(k)) {
@@ -262,10 +269,6 @@ public class Pdr<LOC extends IcfgLocation> {
 			}
 		}
 		return false;
-	}
-
-	private Term getPrimedTerm(final Term term) {
-		return term;
 	}
 
 	private final class PdrResult {
