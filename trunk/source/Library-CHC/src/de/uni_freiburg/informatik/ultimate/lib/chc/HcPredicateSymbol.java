f02a7838412de06ca3bfe73627317294188fe1ea
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-06-04 18:05:56 +0200

    clean up some comments

--- trunk/source/Library-CHC/src/de/uni_freiburg/informatik/ultimate/lib/chc/HcPredicateSymbol.java
+++ trunk/source/Library-CHC/src/de/uni_freiburg/informatik/ultimate/lib/chc/HcPredicateSymbol.java
@@ -35,22 +35,14 @@ import de.uni_freiburg.informatik.ultimate.logic.FunctionSymbol;
 import de.uni_freiburg.informatik.ultimate.logic.Sort;
 
 /**
- * Represents an uninterpreted predicate symbol that appears in a set of Horn clauses. This class is the node class for
- * the Horn clause graph.
- *
- *
- * TODO: this effectively is a FunctionSymbol, right?.. (one might think it is theory-independent, but it is not because
- *   it stores the sorts)
+ * Represents an uninterpreted predicate symbol that appears in a set of Horn clauses.
  *
  * @author nutz, mostafa-mahmoud
  *
  */
 public class HcPredicateSymbol {
 
-//	private final int mArity;
-//	private final String mName;
 	private final FunctionSymbol mFunctionSymbol;
-//	private final List<HCVar> mVars;
 	private final String mFunctionName;
 	private final List<Sort> mParameterSorts;
 
@@ -60,12 +52,9 @@ public class HcPredicateSymbol {
 	 * @param fsym  -- must be from solver (not parser) theory!
 	 * @param functionParameters
 	 */
-	public HcPredicateSymbol(final HcSymbolTable symbolTable,
-			final FunctionSymbol fsym) {//, final List<Sort> functionParameters) {
-//			final String functionName, final List<Sort> functionParameters) {
+	public HcPredicateSymbol(final HcSymbolTable symbolTable, final FunctionSymbol fsym) {
 		mFunctionSymbol = fsym;
 		mFunctionName = fsym.getName();
-//		mParameterSorts = Collections.unmodifiableList(functionParameters);
 		mParameterSorts = Arrays.asList(fsym.getParameterSorts());
 	}
 
@@ -102,7 +91,6 @@ public class HcPredicateSymbol {
 	public static class HornClauseFalsePredicateSymbol extends HornClauseConstantPredicateSymbol {
 		public HornClauseFalsePredicateSymbol(final FunctionSymbol fsym) {
 			super(null, fsym, Collections.emptyList());
-//			super(null, "false", Collections.emptyList());
 		}
 
 		@Override
@@ -120,50 +108,4 @@ public class HcPredicateSymbol {
 			return "False";
 		}
 	}
-
-//	public static class HornClauseTruePredicateSymbol extends HornClauseConstantPredicateSymbol {
-//		public HornClauseTruePredicateSymbol() {
-//			super(null, "true", Collections.emptyList());
-//		}
-//		@Override
-//		public String getName() {
-//			return "True";
-//		}
-//
-//		@Override
-//		public int getArity() {
-//			return 0;
-//		}
-//
-//		@Override
-//		public String toString() {
-//			return "True";
-//		}
-//	}
-//
-//	public static class HornClauseDontCareSymbol extends HornClauseConstantPredicateSymbol {
-//
-//		public HornClauseDontCareSymbol nextVersion() {
-//			final HornClauseDontCareSymbol ret = new HornClauseDontCareSymbol();
-//			return ret;
-//		}
-//		public HornClauseDontCareSymbol() {
-//			super(null, "€", Collections.emptyList());
-//		}
-//
-//		@Override
-//		public String getName() {
-//			return "€";
-//		}
-//
-//		@Override
-//		public int getArity() {
-//			return 0;
-//		}
-//
-//		@Override
-//		public String toString() {
-//			return "€";
-//		}
-//	}
 }
