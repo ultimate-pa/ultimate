ca21904a31f148ebf744de5cc76c1e24be0e6e28
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2018-06-08 04:04:07 +0200

    remove invariant checking hack because we how have a separate plugin

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/TraceAbstractionStarter.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/TraceAbstractionStarter.java
@@ -93,7 +93,6 @@ import de.uni_freiburg.informatik.ultimate.witnessparser.graph.WitnessNode;
 public class TraceAbstractionStarter {
 
 	private static final boolean EXTENDED_HOARE_ANNOTATION_LOGGING = true;
-	private static final boolean INVARIANT_CHECKING = false;
 
 	private final ILogger mLogger;
 	private final IUltimateServiceProvider mServices;
@@ -117,10 +116,6 @@ public class TraceAbstractionStarter {
 		mServices = services;
 		mToolchainStorage = storage;
 		mLogger = mServices.getLoggingService().getLogger(Activator.PLUGIN_ID);
-		if (INVARIANT_CHECKING) {
-			new InvariantChecker(mServices, mToolchainStorage, rcfgRootNode);
-			return;
-		}
 		runCegarLoops(rcfgRootNode, witnessAutomaton, rawFloydHoareAutomataFromFile);
 	}
 
