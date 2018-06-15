daeb4e2b70b87702c671a43b1c80084f04c169bd
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-06-15 07:18:14 +0200

    add subfolders for chc 2018 star exec archives, add star exec stuff for TreeAutomizer

--- releaseScripts/2018chccomp/TreeAutomizer/createZip.sh
+++ releaseScripts/2018chccomp/TreeAutomizer/createZip.sh
@@ -2,13 +2,13 @@
 mkdir StarExecArchive
 mkdir StarExecArchive/bin
 mkdir StarExecArchive/Ultimate
-cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E4/linux/gtk/x86_64/* StarExecArchive/Ultimate/
+cp -a ../../../trunk/source/BA_SiteRepository/target/products/CLI-E4/linux/gtk/x86_64/* StarExecArchive/Ultimate/
 cp LICENSE* StarExecArchive/Ultimate/
 cp starexec_description.txt StarExecArchive/
-cp ../../trunk/examples/toolchains/AutomizerCHC.xml StarExecArchive/
+cp ../../../trunk/examples/toolchains/AutomizerCHC.xml StarExecArchive/
 cp starexec_run_* StarExecArchive/bin/
 cp Ultimate.ini StarExecArchive/Ultimate/
-cp ../../trunk/examples/settings/chccomp2018/chcToBoogie_automizer.epf StarExecArchive/
+cp ../../../trunk/examples/settings/chccomp2018/chcToBoogie_automizer.epf StarExecArchive/
 #cp ../../trunk/examples/settings/buchiAutomizer/termcomp2017-NontrivialArrayWrites.epf StarExecArchive/
 #mkdir StarExecArchive/z3
 cp -LR ../default/adds/z3 StarExecArchive/bin/
