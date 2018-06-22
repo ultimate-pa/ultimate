dc7c10ecf49ca05a9b6895a074c48ae523674e73
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2018-06-22 04:00:32 +0200

    add support for ITE terms for case where ITE is quantifier-free

--- trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/linearterms/PrenexNormalForm.java
+++ trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/linearterms/PrenexNormalForm.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE ModelCheckerUtils Library.
- * 
+ *
  * The ULTIMATE ModelCheckerUtils Library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE ModelCheckerUtils Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE ModelCheckerUtils Library. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE ModelCheckerUtils Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE ModelCheckerUtils Library grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE ModelCheckerUtils Library grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.linearterms;
@@ -39,18 +39,19 @@ import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermTransformer;
 import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.ContainsQuantifier;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.NonCoreBooleanSubTermTransformer;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
 
 /**
  * Transform a Term into prenex normal form (quantifiers outside).
- * 
+ *
  * @author Matthias Heizmann
- * 
+ *
  */
 public class PrenexNormalForm extends TermTransformer {
-	
+
 	private final Script mScript;
 	private final ManagedScript mMgdScript;
 
@@ -58,6 +59,28 @@ public class PrenexNormalForm extends TermTransformer {
 		mScript = mgdScript.getScript();
 		mMgdScript = mgdScript;
 	}
+
+
+
+	@Override
+	protected void convert(final Term term) {
+		if (term instanceof ApplicationTerm) {
+			final ApplicationTerm appTerm = (ApplicationTerm) term;
+			final String fun = appTerm.getFunction().getName();
+			if (fun.equals("ite")) {
+				if (new ContainsQuantifier().containsQuantifier(appTerm)) {
+					throw new UnsupportedOperationException("ite Term with quantifier, use IteRemover first");
+				} else {
+					// since the subTerm does not contain quantifier we can immediately use this as a result
+					setResult(appTerm);
+					return;
+				}
+			}
+		}
+		super.convert(term);
+	}
+
+
 
 	@Override
 	public void convertApplicationTerm(final ApplicationTerm appTerm, final Term[] newArgs) {
@@ -100,7 +123,7 @@ public class PrenexNormalForm extends TermTransformer {
 		for (int i = qVarSeq.size()-1; i>=0; i--) {
 			final QuantifierSequence.QuantifiedVariables quantifiedVars = qVarSeq.get(i);
 			final int resultQuantifier = (quantifiedVars.getQuantifier() + 1) % 2;
-			result = SmtUtils.quantifier(mScript, resultQuantifier, 
+			result = SmtUtils.quantifier(mScript, resultQuantifier,
 					quantifiedVars.getVariables(), result);
 		}
 		return result;
@@ -113,8 +136,8 @@ public class PrenexNormalForm extends TermTransformer {
 			freeVariables.addAll(Arrays.asList(newArgs[i].getFreeVars()));
 			quantifierSequences[i] = new QuantifierSequence(mScript, newArgs[i]);
 		}
-		final Term result = QuantifierSequence.mergeQuantifierSequences(mMgdScript, 
-				appTerm.getFunction().getName(), quantifierSequences, 
+		final Term result = QuantifierSequence.mergeQuantifierSequences(mMgdScript,
+				appTerm.getFunction().getName(), quantifierSequences,
 				freeVariables);
 		return result;
 	}
@@ -123,13 +146,13 @@ public class PrenexNormalForm extends TermTransformer {
 	public void postConvertLet(final LetTerm oldLet, final Term[] newValues, final Term newBody) {
 		throw new UnsupportedOperationException("not yet implemented, we need term without let");
 	}
-	
-	
+
+
 
 	@Override
 	public void postConvertQuantifier(final QuantifiedFormula old, final Term newBody) {
 		if (SmtUtils.isQuantifiedFormulaWithSameQuantifier(old.getQuantifier(), newBody) != null) {
-			final Term result = SmtUtils.quantifier(mScript, old.getQuantifier(), 
+			final Term result = SmtUtils.quantifier(mScript, old.getQuantifier(),
 					new HashSet<TermVariable>(Arrays.asList(old.getVariables())), newBody);
 			setResult(result);
 		} else {
