24629f0cf7f16a87472e5e2de97d25ae5ae29577
Claus Schätzle <schaetzc@tf.uni-freiburg.de>
2018-07-06 17:01:43 +0200

    Intersection with more than two sets

--- trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/SetOperations.java
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/SetOperations.java
@@ -26,14 +26,18 @@
  */
 package de.uni_freiburg.informatik.ultimate.util.datastructures;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 
 /**
- * Set operations like union and intersection.
- * Methods ending with "multi" work like multiset operation in case the result collection allows for duplicates.
+ * Set operations, for instance union and intersection.
+ * These methods work with every type of Collection, not only sets.
+ * In case the inputs contain duplicates, the output may or may not contain duplicates too.
  * 
  * @author schaetzc@tf.uni-freiburg.de
  */
@@ -43,56 +47,60 @@ public final class SetOperations {
 		// No need for objects of this class.
 		// Class contains only static methods.
 	}
-	
-	public static <E, C extends Collection<E>> C intersection(final C result,
-			final Collection<? extends E> setA, final Collection<? extends E> setB) {
-		result.addAll(setA);
-		result.retainAll(setB);
-		return result;
+
+	private static <E> Collection<E> smallestCollection(final Collection<E>[] collections) {
+		Collection<E> min = null;
+		for (Collection<E> collection : collections) {
+			if (min == null || collection.size() < min.size()) {
+				min = collection;
+			}
+		}
+		return min;
 	}
 	
-	public static <E> Set<E> intersection(
-			final Collection<? extends E> setA, final Collection<? extends E> setB) {
-		final Set<E> result = new HashSet<>(setA); 
-		result.retainAll(setB);
+	private static <E, RESULT extends Collection<E>> RESULT intersectionIntern(
+			final Function<Collection<E>, RESULT> smallerSetCopier, final Collection<E>[] operands) {
+		Collection<E> min = smallestCollection(operands);
+		RESULT result = smallerSetCopier.apply(min);
+		for (Collection<E> operand : operands) {
+			if (operand != min) {
+				result.retainAll(operand);
+			}
+		}
 		return result;
 	}
-	
-	public static <E, C extends Collection<E>> C unionMulti(final C result,
-			final Collection<? extends E> setA, final Collection<? extends E> setB) {
-		result.addAll(setA);
-		result.addAll(setB);
-		return result;
+
+	@SafeVarargs
+	public static <E> Set<E> intersection(final Collection<E>... operands) {
+		return intersectionIntern(HashSet::new, operands);
 	}
-	
-	public static <E> Set<E> union(
-			final Collection<? extends E> setA, final Collection<? extends E> setB) {
-		final Set<E> result = new HashSet<>(setA); 
-		result.addAll(setB);
-		return result;
+
+	@SafeVarargs
+	public static <E> List<E> intersectionList(final Collection<E>... operands) {
+		return intersectionIntern(ArrayList::new, operands);
 	}
-	
-	public static <E, C extends Collection<E>> C difference(final C result,
-			final Collection<? extends E> minuend, final Collection<? extends E> subtrahend) {
-		result.addAll(minuend);
+
+	private static <E, RESULT extends Collection<E>> RESULT differenceIntern(
+			final Function<Collection<E>, RESULT> minuendCopier,
+			final Collection<E> minuend, final Collection<E> subtrahend) {
+		RESULT result = minuendCopier.apply(minuend);
 		result.removeAll(subtrahend);
 		return result;
 	}
-	
-	public static <E> Set<E> difference(
-			final Collection<? extends E> minuend, final Collection<? extends E> subtrahend) {
-		final Set<E> result = new HashSet<>(minuend);
-		result.removeAll(subtrahend);
-		return result;
+
+	public static <E> Set<E> difference(final Collection<E> minuend, final Collection<E> subtrahend) {
+		return differenceIntern(HashSet::new, minuend, subtrahend);
 	}
-	
-	public static <E> boolean disjoint(
-			final Collection<? extends E> set1, final Collection<? extends E> set2) {
+
+	public static <E> List<E> differenceList(final Collection<E> minuend, final Collection<E> subtrahend) {
+		return differenceIntern(ArrayList::new, minuend, subtrahend);
+	}
+
+	public static <E> boolean disjoint(final Collection<E> set1, final Collection<E> set2) {
 		return Collections.disjoint(set1, set2);
 	}
-	
-	public static <E> boolean intersecting(
-			final Collection<? extends E> set1, final Collection<? extends E> set2) {
+
+	public static <E> boolean intersecting(final Collection<E> set1, final Collection<E> set2) {
 		return !Collections.disjoint(set1, set2);
 	}
 }
