ab6a05b78ef72e57e38bcd35d0465d699a3f0b25
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2018-07-26 02:52:01 +0200

    add example code

--- trunk/source/Library-MSO/src/de/uni_freiburg/informatik/ultimate/mso/MoNatDiffScript.java
+++ trunk/source/Library-MSO/src/de/uni_freiburg/informatik/ultimate/mso/MoNatDiffScript.java
@@ -28,53 +28,78 @@
 /*
  * ApplicationTerm		:= function symbols
  * ConstantTerm			:= literals
- * QuantifiedFormula	:= 
+ * QuantifiedFormula	:=
  * TermVariable			:= quantified variables
  */
 
 package de.uni_freiburg.informatik.ultimate.mso;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryException;
 import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.INestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedRun;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWord;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.VpAlphabet;
-import de.uni_freiburg.informatik.ultimate.automata.statefactory.IEmptyStackStateFactory;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.buchi.BuchiComplementFKV;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.buchi.BuchiIntersect;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.buchi.BuchiIsEmpty;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.buchi.NestedLassoRun;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.buchi.NestedLassoWord;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.operations.Complement;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.operations.Determinize;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.operations.Intersect;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.operations.IsEmpty;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.operations.Union;
 import de.uni_freiburg.informatik.ultimate.automata.statefactory.StringFactory;
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.logic.ApplicationTerm;
 import de.uni_freiburg.informatik.ultimate.logic.Logics;
 import de.uni_freiburg.informatik.ultimate.logic.NoopScript;
+import de.uni_freiburg.informatik.ultimate.logic.QuantifiedFormula;
+import de.uni_freiburg.informatik.ultimate.logic.Rational;
 import de.uni_freiburg.informatik.ultimate.logic.SMTLIBException;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SmtUtils;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.linearterms.AffineRelation;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.linearterms.AffineTerm;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.linearterms.NotAffineException;
 
 public class MoNatDiffScript extends NoopScript {
-	
-	
+
+
 	private final IUltimateServiceProvider mServices;
 	private final AutomataLibraryServices mAutomataLibraryServices;
 	private final ILogger mLogger;
 
-	public MoNatDiffScript(IUltimateServiceProvider services, ILogger logger) {
+	public MoNatDiffScript(final IUltimateServiceProvider services, final ILogger logger) {
 		mServices = services;
 		mAutomataLibraryServices = new AutomataLibraryServices(services);
 		mLogger = logger;
 	}
 
 	@Override
-	public void setLogic(String logic) throws UnsupportedOperationException, SMTLIBException {
+	public void setLogic(final String logic) throws UnsupportedOperationException, SMTLIBException {
 		mLogger.info("hello world, logic set to " + logic);
 		super.setLogic(logic);
 	}
 
 	@Override
-	public void setLogic(Logics logic) throws UnsupportedOperationException, SMTLIBException {
+	public void setLogic(final Logics logic) throws UnsupportedOperationException, SMTLIBException {
 		mLogger.info("hello world, logic set to " + logic);
 		super.setLogic(logic);
 	}
 
 	@Override
-	public LBool assertTerm(Term term) throws SMTLIBException {
+	public LBool assertTerm(final Term term) throws SMTLIBException {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -84,22 +109,90 @@ public class MoNatDiffScript extends NoopScript {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
-	private void constructAutomaton() {
-		Set<Integer> alphabet = null;
-		VpAlphabet<Integer> vpAlphabet = new VpAlphabet<Integer>(alphabet);
-		IEmptyStackStateFactory stateFactory = new StringFactory();
-		NestedWordAutomaton<Integer, String> automaton = new NestedWordAutomaton<Integer, String>(
+
+
+	private void constructAutomaton() throws AutomataLibraryException {
+		final Set<Integer> alphabet = null;
+		final VpAlphabet<Integer> vpAlphabet = new VpAlphabet<Integer>(alphabet);
+		final StringFactory stateFactory = new StringFactory();
+		final NestedWordAutomaton<Integer, String> automaton = new NestedWordAutomaton<Integer, String>(
 				mAutomataLibraryServices, vpAlphabet, stateFactory);
-		
+
 		// add some initial state
 		automaton.addState(true, false, "q_0");
 		// add some accepting state
 		automaton.addState(false, true, "q_1");
 		// connect both states via transition that is labeled by letter 23
 		automaton.addInternalTransition("q_0", 23, "q_1");
-		
-		
+
+
+		final INestedWordAutomaton<Integer, String> intersection = new Intersect<Integer, String>(
+				mAutomataLibraryServices, stateFactory, automaton, automaton).getResult();
+		final INestedWordAutomaton<Integer, String> buchiIntersection = new BuchiIntersect<Integer, String>(
+				mAutomataLibraryServices, stateFactory, automaton, automaton).getResult();
+		final INestedWordAutomaton<Integer, String> union = new Union<Integer, String>(mAutomataLibraryServices,
+				stateFactory, automaton, automaton).getResult();
+		final INestedWordAutomaton<Integer, String> determinize = new Determinize<Integer, String>(
+				mAutomataLibraryServices, stateFactory, automaton).getResult();
+		final INestedWordAutomaton<Integer, String> complement = new Complement<Integer, String>(
+				mAutomataLibraryServices, stateFactory, automaton).getResult();
+		final INestedWordAutomaton<Integer, String> buchiComplement = new BuchiComplementFKV<Integer, String>(
+				mAutomataLibraryServices, stateFactory, automaton).getResult();
+		final IsEmpty<Integer, String> emptinessCheck = new IsEmpty<Integer, String>(mAutomataLibraryServices, union);
+		if (emptinessCheck.getResult() == false) {
+			final NestedRun<Integer, String> run = emptinessCheck.getNestedRun();
+			final NestedWord<Integer> word = run.getWord();
+		}
+		final BuchiIsEmpty<Integer, String> buchiEmptinessCheck = new BuchiIsEmpty<Integer, String>(
+				mAutomataLibraryServices, buchiComplement);
+		if (emptinessCheck.getResult() == false) {
+			final NestedLassoRun<Integer, String> lassorun = buchiEmptinessCheck.getAcceptingNestedLassoRun();
+			final NestedLassoWord<Integer> lassoword = lassorun.getNestedLassoWord();
+		}
+
+
+
 	}
+
+
+	private void someAuxiliaryMethodsThatMightBeHelpfulForWorkingWithFormulas() {
+		final Term term = null;
+		final Term term2 = null;
+		SmtUtils.isAtomicFormula(term);
+		SmtUtils.and(this, term, term2);
+		final QuantifiedFormula qf = (QuantifiedFormula) term;
+		SmtUtils.quantifier(this, QuantifiedFormula.EXISTS, new HashSet<TermVariable>(Arrays.asList(qf.getFreeVars())), term);
+		SmtUtils.not(this, term2);
+		final ApplicationTerm appTerm = (ApplicationTerm) term2;
+		if (appTerm.getFunction().getName().equals("and")) {
+			// this is an and term
+		}
+
+		// data structure that might help you for working with atoms
+		AffineRelation ar;
+		try {
+			 ar = new AffineRelation(this, appTerm);
+		} catch (final NotAffineException e) {
+			// not an affine relation, maybe we have to descend one level in the tree!?!
+			ar = null;
+		}
+		final AffineTerm at = ar.getAffineTerm();
+		final Map<Term, Rational> var2coeff = at.getVariable2Coefficient();
+		if (var2coeff.size() > 2) {
+			throw new IllegalArgumentException("more than two variables! this is not difference logic");
+		}
+		final Rational literal = at.getConstant();
+		if (!literal.isIntegral()) {
+			throw new IllegalArgumentException("not a integer");
+		}
+		final BigInteger integer = literal.numerator();
+
+
+
+
+
+
+
+	}
+
 }
