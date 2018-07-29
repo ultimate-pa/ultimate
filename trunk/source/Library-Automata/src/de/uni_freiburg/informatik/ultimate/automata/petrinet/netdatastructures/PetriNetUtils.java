55efb354b48189982624491f33896eae4e6d9a7f
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2018-07-29 17:59:19 +0200

    add utils class

---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/petrinet/netdatastructures/PetriNetUtils.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2018 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * Copyright (C) 2018 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.petrinet.netdatastructures;
+
+import java.util.Collection;
+import java.util.Set;
+
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.IPetriNet;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.ITransition;
+
+/**
+ * Provides static methods for Petri nets.
+ *
+ * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ */
+public class PetriNetUtils {
+
+	public static <LETTER, PLACE> boolean similarPredecessorPlaces(final Collection<ITransition<LETTER, PLACE>> transitions,
+			final IPetriNet<LETTER, PLACE> net) {
+		if (transitions.isEmpty()) {
+			return true;
+		} else {
+			final Set<PLACE> predecessorPlaces = net.getPredecessors(transitions.iterator().next());
+			for (final ITransition<LETTER, PLACE> transition : transitions) {
+				final Set<PLACE> transPredPlaces = net.getPredecessors(transition);
+				if (!predecessorPlaces.equals(transPredPlaces)) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+}
