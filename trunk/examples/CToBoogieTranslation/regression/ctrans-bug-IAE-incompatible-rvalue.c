e137c0991609a38dbae1487be59f2add0531dc7a
Daniel Dietsch <daniel.dietsch@gmail.com>
2018-07-30 14:12:18 +0200

    see #359: minimal example for this issue

---
+++ trunk/examples/CToBoogieTranslation/regression/ctrans-bug-IAE-incompatible-rvalue.c
@@ -0,0 +1,14 @@
+/*
+2017-07-30: Example for java.lang.IllegalArgumentException: The value of the constructed RValue has a BoogieType ([int]int) that is incompatible with its CType ($Pointer$).
+-tc ../../../trunk/examples/toolchains/CheckedCTranslation.xml
+-s ../../../trunk/examples/settings/default/automizer/svcomp-DerefFreeMemtrack-32bit-Automizer_Default.epf
+
+Author: Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+*/
+
+int _oledbuffer[1];
+void  oledGetBuffer()
+{
+ return _oledbuffer;
+}
+
