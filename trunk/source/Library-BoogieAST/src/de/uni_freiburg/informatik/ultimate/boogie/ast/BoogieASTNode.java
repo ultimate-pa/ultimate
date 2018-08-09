857df8025e7ba1f314cfcc85b959cbb7b586873e
danieldietsch <daniel.dietsch@gmail.com>
2018-08-09 10:44:34 +0200

    add two new boogie node validators that prevents some blocks with null statements

--- trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/ast/BoogieASTNode.java
+++ trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/ast/BoogieASTNode.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Core.
- * 
+ *
  * The ULTIMATE Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Core. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
@@ -27,6 +27,7 @@
 package de.uni_freiburg.informatik.ultimate.boogie.ast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,6 +43,11 @@ import de.uni_freiburg.informatik.ultimate.core.model.models.ILocation;
 import de.uni_freiburg.informatik.ultimate.core.model.models.ISimpleAST;
 import de.uni_freiburg.informatik.ultimate.core.model.models.IWalkable;
 
+/**
+ *
+ * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ *
+ */
 public class BoogieASTNode extends BasePayloadContainer implements ISimpleAST<BoogieASTNode, VisualizationNode> {
 
 	private static final long serialVersionUID = 5856434889026482850L;
@@ -56,17 +62,26 @@ public class BoogieASTNode extends BasePayloadContainer implements ISimpleAST<Bo
 			final IdentifierExpression iexpr = (IdentifierExpression) instance;
 			return Pattern.matches(IDENTIFIER_REGEX, iexpr.identifier);
 		};
-		VALIDATORS.put(IdentifierExpression.class, iexprValidator);
 		final Predicate<BoogieASTNode> procIdValidator = instance -> {
 			final Procedure proc = (Procedure) instance;
 			return Pattern.matches(IDENTIFIER_REGEX, proc.identifier);
 		};
-		VALIDATORS.put(Procedure.class, procIdValidator);
 		final Predicate<BoogieASTNode> funDeclValidator = instance -> {
 			final FunctionDeclaration funDecl = (FunctionDeclaration) instance;
 			return Pattern.matches(IDENTIFIER_REGEX, funDecl.identifier);
 		};
+		final Predicate<BoogieASTNode> funArrayAllNonNullElementsValidator = instance -> {
+			final HasNullElementVisitor visitor = new HasNullElementVisitor();
+			instance.accept(visitor);
+			return !visitor.getResult();
+		};
+
+		VALIDATORS.put(IdentifierExpression.class, iexprValidator);
+		VALIDATORS.put(Procedure.class, procIdValidator);
 		VALIDATORS.put(FunctionDeclaration.class, funDeclValidator);
+
+		VALIDATORS.put(Body.class, funArrayAllNonNullElementsValidator);
+		VALIDATORS.put(WhileStatement.class, funArrayAllNonNullElementsValidator);
 	}
 
 	public BoogieASTNode(final ILocation location) {
@@ -118,6 +133,49 @@ public class BoogieASTNode extends BasePayloadContainer implements ISimpleAST<Bo
 	@Override
 	public List<BoogieASTNode> getOutgoingNodes() {
 		return new ArrayList<>();
+	}
+
+	public void accept(final GeneratedBoogieAstVisitor visitor) {
+		throw new UnsupportedOperationException("The base class does not accept visitors");
+	}
+
+	/**
+	 * Checks if a {@link BoogieASTNode} type has a array-type field with null elements.
+	 *
+	 * This class is not complete (it does not check all subtypes of {@link BoogieASTNode} that need checking).
+	 *
+	 * Note to implementers: Do not descend into children (this class should be used during creation of the AST, and
+	 * this would lead to rather long runtimes).
+	 *
+	 * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+	 *
+	 */
+	private static final class HasNullElementVisitor extends GeneratedBoogieAstVisitor {
+
+		private boolean mResult = false;
+
+		@Override
+		public boolean visit(final Body node) {
+			mResult = hasNullElement(node.getBlock()) || hasNullElement(node.getLocalVars());
+			return false;
+		}
+
+		@Override
+		public boolean visit(final WhileStatement node) {
+			mResult = hasNullElement(node.getBody()) || hasNullElement(node.getInvariants());
+			return false;
+		}
+
+		public boolean getResult() {
+			return mResult;
+		}
+
+		private static final <T> boolean hasNullElement(final T[] array) {
+			if (array == null || array.length == 0) {
+				return false;
+			}
+			return Arrays.stream(array).anyMatch(a -> a == null);
+		}
 	}
 
 	private class BoogieASTWrapper extends BoogieASTNode {
