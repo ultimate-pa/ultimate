1b588c3068491af7ef3f6295ffa9f75eb4c59279
danieldietsch <daniel.dietsch@gmail.com>
2018-08-09 10:31:14 +0200

    fix log bug

--- trunk/source/PEAtoBoogie/src/de/uni_freiburg/informatik/ultimate/pea2boogie/translator/Req2BoogieTranslator.java
+++ trunk/source/PEAtoBoogie/src/de/uni_freiburg/informatik/ultimate/pea2boogie/translator/Req2BoogieTranslator.java
@@ -759,16 +759,18 @@ public class Req2BoogieTranslator {
 		// get all automata for which conditions should be generated
 		final List<Entry<PatternType, PhaseEventAutomata>> consideredAutomata;
 		if (mSeparateInvariantHandling) {
+			// we only consider automata that do not represent invariants
 			consideredAutomata = mReq2Automata.entrySet().stream().filter(a -> a.getValue().getPhases().length != 1)
 					.collect(Collectors.toList());
 		} else {
 			consideredAutomata = mReq2Automata.entrySet().stream().collect(Collectors.toList());
 		}
 
+		final int total = mReq2Automata.size();
 		final int count = consideredAutomata.size();
-		final int invariant = mReq2Automata.size() - count;
+		final int invariant = total - count;
 		if (mSeparateInvariantHandling) {
-			mLogger.info(String.format("%s of %s requirements are invariant", invariant, count));
+			mLogger.info(String.format("%s of %s requirements are invariant", invariant, total));
 		}
 
 		final int actualCombinationNum = mCombinationNum <= count ? mCombinationNum : count;
