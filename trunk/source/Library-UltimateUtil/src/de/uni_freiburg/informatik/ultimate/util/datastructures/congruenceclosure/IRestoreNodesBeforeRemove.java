ee5830d26778c74716abc21ac2609fc3bb59548f
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-08-27 16:17:20 +0200

    #288, cleanup

--- trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/congruenceclosure/IRestoreNodesBeforeRemove.java
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/congruenceclosure/IRestoreNodesBeforeRemove.java
@@ -2,32 +2,21 @@ package de.uni_freiburg.informatik.ultimate.util.datastructures.congruenceclosur
 
 import java.util.Set;
 
+/**
+ * Introduced to have only one version of the method {@link RemoveCcElement#addNodesToKeepInformation}.
+ *
+ * @author Alexander Nutz (nutz@informatik.uni-freiburg.de)
+ *
+ * @param <ELEM>
+ */
 public interface IRestoreNodesBeforeRemove<ELEM extends ICongruenceClosureElement<ELEM>> {
 
 	boolean isIntroduceNewNodes();
 
 	ELEM getElem();
 
-//	IRestoreNodesBeforeRemove<ELEM> getElementContainer();
 	ICongruenceClosure<ELEM> getElementContainer();
 
-//	boolean hasElement(ELEM n);
-
-//	boolean sanityCheck();
-
-//	boolean isDebugMode();
-
-//	ILogger getLogger();
-
-//	void addElement(ELEM proxyElem, boolean b);
-
-//	boolean isInconsistent();
-
 	void registerAddedNodes(Set<ELEM> nodesToAdd);
-
-//	boolean isConstrained(ELEM elemToRemove);
-
-//	Set<ELEM> getNodesToIntroduceBeforeRemoval(ELEM elemToRemove, Set<ELEM> elementsToRemove,
-//			Map<ELEM, ELEM> nodeToReplacementNode);
 
 }
