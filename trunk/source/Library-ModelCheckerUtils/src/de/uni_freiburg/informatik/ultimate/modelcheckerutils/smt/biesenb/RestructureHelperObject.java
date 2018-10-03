1bd2b58eb69660085e1ded68a838741695a176b1
danieldietsch <daniel.dietsch@gmail.com>
2018-10-03 16:28:59 +0200

    fix typo in RestructureHelperObject

--- trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/biesenb/RestructureHelperObject.java
+++ trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/biesenb/RestructureHelperObject.java
@@ -1,32 +1,32 @@
 package de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.biesenb;
 
 import java.util.Map;
-import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
 
 public class RestructureHelperObject {
-	
+
 	final private int mSerialNumber;
 	final private Map<Term, Term> mWitness;
 	final private IPredicate mPredicate;
 
-	public RestructureHelperObject(int serialNumber, Map<Term, Term> witnesses, IPredicate predicate) {
+	public RestructureHelperObject(final int serialNumber, final Map<Term, Term> witnesses,
+			final IPredicate predicate) {
 		mSerialNumber = serialNumber;
 		mWitness = witnesses;
 		mPredicate = predicate;
 	}
-	
-	public int getSerialNumber(){
+
+	public int getSerialNumber() {
 		return mSerialNumber;
 	}
-	
-	public Map<Term, Term> getWitness(){
+
+	public Map<Term, Term> getWitness() {
 		return mWitness;
 	}
-	
-	public IPredicate getPredicatet(){
+
+	public IPredicate getPredicate() {
 		return mPredicate;
 	}
 }
