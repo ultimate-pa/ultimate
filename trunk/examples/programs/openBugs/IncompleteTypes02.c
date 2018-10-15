b145e4630462c11bbabdf4688149732b6b902b4c
Matthias <heizmann@informatik.uni-freiburg.de>
2018-10-15 22:05:35 +0200

    example

--- trunk/examples/programs/openBugs/IncompleteTypes02.c
+++ trunk/examples/programs/openBugs/IncompleteTypes02.c
@@ -4,21 +4,46 @@
  * Date: 2018-10-11
  */
 
-
+typedef enum myEnum myNewNameForEnum;
 typedef struct myStruct myNewNameForStruct;
+typedef union myUnion myNewNameForUnion;
+
+struct myPointerStruct {
+	myNewNameForEnum *p1;
+	myNewNameForStruct *p2;
+	myNewNameForUnion *p3;
+};
+
+enum myEnum {
+	foo,
+	bar,
+};
 
 struct myStruct {
 	int myFirstField;
-	int mySecondField;
+	myNewNameForEnum mySecondField;
 };
 
-struct mySecondStruct {
-	myNewNameForStruct myField;
+union myUnion {
+	int myFirstMember;
+	myNewNameForStruct mySecondMember ;
 };
 
-myNewNameForStruct myArray[1U];
+
+struct myNonPointerStruct {
+	myNewNameForEnum myField1;
+	myNewNameForStruct myField2;
+	myNewNameForUnion myField3;
+};
+
+myNewNameForEnum myEnumArray[1];
+myNewNameForStruct myStructArray[1];
+myNewNameForUnion myUnionArray[1];
 
 
 int main(void) {
+	myNewNameForEnum e = foo;
+	struct myPointerStruct x;
+	x.p1 = &e;
 	return 0;
 }
