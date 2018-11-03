8d4e1cb3575f010cc924f4222557d1e6b45b4912
Victor Mocanu <mocanuv@tf.uni-freiburg.ce>
2018-11-03 10:13:41 +0100

    Expanded the ReqToTestPowerset class, addeed inport/export rules in Manifests

--- trunk/source/ReqToTestPowerset/src/de/uni_freiburg/informatik/ultimate/reqtotestpowerset/ReqToTestPowersetObserver.java
+++ trunk/source/ReqToTestPowerset/src/de/uni_freiburg/informatik/ultimate/reqtotestpowerset/ReqToTestPowersetObserver.java
@@ -1,20 +1,56 @@
 package de.uni_freiburg.informatik.ultimate.reqtotestpowerset;
 
+import java.util.List;
+
+import de.uni_freiburg.informatik.ultimate.core.lib.models.ObjectContainer;
 import de.uni_freiburg.informatik.ultimate.core.lib.observers.BaseObserver;
 import de.uni_freiburg.informatik.ultimate.core.model.models.IElement;
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IToolchainStorage;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.lib.srparse.pattern.PatternType;
+import de.uni_freiburg.informatik.ultimate.logic.Logics;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SolverBuilder;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SolverBuilder.SolverMode;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.SolverBuilder.SolverSettings;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
+import de.uni_freiburg.informatik.ultimate.reqtotest.req.ReqSymbolTable;
+import de.uni_freiburg.informatik.ultimate.reqtotest.req.ReqToDeclarations;
 
 public class ReqToTestPowersetObserver extends BaseObserver{
 
-	public ReqToTestPowersetObserver(ILogger mLogger, IUltimateServiceProvider mServices, IToolchainStorage mStorage) {
-		// TODO Auto-generated constructor stub
+	private final ILogger mLogger;
+	private final IUltimateServiceProvider mServices;
+	private final IToolchainStorage mStorage;
+	
+	private IElement mBoogieAst;
+	private final ManagedScript mManagedScript;
+	private final Script mScript;
+	
+	public ReqToTestPowersetObserver(final ILogger logger, final IUltimateServiceProvider services,
+			final IToolchainStorage storage) {
+		mLogger = logger;
+		mServices = services;
+		mStorage = storage;
+		
+		final SolverSettings settings = SolverBuilder.constructSolverSettings("", SolverMode.External_DefaultMode,
+				false, SolverBuilder.COMMAND_Z3_NO_TIMEOUT, false, null);
+		mScript = SolverBuilder.buildAndInitializeSolver(services, storage, SolverMode.External_DefaultMode, settings,
+				false, false, Logics.ALL.toString(), "RtInconsistencySolver");
+
+		mManagedScript = new ManagedScript(services, mScript);
 	}
 
 	@Override
 	public boolean process(IElement root) throws Throwable {
-		// TODO Auto-generated method stub
+		if (!(root instanceof ObjectContainer)) {
+		return false;
+		}
+		
+		final List<PatternType> rawPatterns = ((ObjectContainer<List<PatternType>>) root).getValue();
+		final ReqSymbolTable symbolTable =  new ReqToDeclarations(mLogger).initPatternToSymbolTable(rawPatterns);
+
 		return false;
 	}
 
