f126646099cde83e94f6018f7445042cc77b6a18
Matthias <heizmann@informatik.uni-freiburg.de>
2018-11-05 18:35:25 +0100

    add operation for obtaining accepted words from Petri nets

---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/petrinet/unfolding/GetAcceptedWord.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2018 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * Copyright (C) 2018 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.petrinet.unfolding;
+
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryException;
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
+import de.uni_freiburg.informatik.ultimate.automata.AutomataOperationCanceledException;
+import de.uni_freiburg.informatik.ultimate.automata.Word;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.IPetriNet;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.PetriNetRun;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.UnaryNetOperation;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.netdatastructures.BoundedPetriNet;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.operations.Accepts;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.unfolding.PetriNetUnfolder.UnfoldingOrder;
+import de.uni_freiburg.informatik.ultimate.automata.statefactory.IPetriNet2FiniteAutomatonStateFactory;
+
+/**
+ * Obtain a word that is accpeted by a given Petri net.
+ * <p>
+ * NOTE: If the language of the input  is empty, an exception is thrown.
+ *
+ * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ */
+public final class GetAcceptedWord<LETTER, PLACE>
+		extends UnaryNetOperation<LETTER, PLACE, IPetriNet2FiniteAutomatonStateFactory<PLACE>> {
+	private final IPetriNet<LETTER, PLACE> mOperand;
+	private final Word<LETTER> mAcceptedWord;
+
+
+	public GetAcceptedWord(final AutomataLibraryServices services, final BoundedPetriNet<LETTER, PLACE> operand)
+			throws AutomataOperationCanceledException {
+		super(services);
+		mOperand = operand;
+		mLogger.info(startMessage());
+		final PetriNetUnfolder<LETTER, PLACE> unf =
+				new PetriNetUnfolder<>(mServices, operand, UnfoldingOrder.ERV, false, true);
+		final PetriNetRun<LETTER, PLACE> run = unf.getAcceptingRun();
+		if (run == null) {
+			throw new IllegalArgumentException("unable to get word from empty language");
+		}
+		mAcceptedWord = run.getWord();
+		mLogger.info(exitMessage());
+	}
+
+	@Override
+	public String exitMessage() {
+		return "Finished " + getOperationName() + ". Found word of length " + mAcceptedWord.length();
+	}
+
+	@Override
+	protected IPetriNet<LETTER, PLACE> getOperand() {
+		return mOperand;
+	}
+
+	@Override
+	public Word<LETTER> getResult() {
+		return mAcceptedWord;
+	}
+
+	@Override
+	public boolean checkResult(final IPetriNet2FiniteAutomatonStateFactory<PLACE> stateFactory)
+			throws AutomataLibraryException {
+		final boolean isAccepted = new Accepts<LETTER, PLACE>(mServices, getOperand(), getResult()).getResult();
+		return isAccepted;
+	}
+}
