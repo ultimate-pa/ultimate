e55a09f9bd43f7c1592ec9afb19d74caf7407e8f
danieldietsch <daniel.dietsch@gmail.com>
2018-11-07 10:11:21 +0100

    cc fixes

--- trunk/source/Library-IcfgTransformer/src/de/uni_freiburg/informatik/ultimate/icfgtransformer/AxiomsAdderIcfgTransformer.java
+++ trunk/source/Library-IcfgTransformer/src/de/uni_freiburg/informatik/ultimate/icfgtransformer/AxiomsAdderIcfgTransformer.java
@@ -16,32 +16,26 @@ import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.Basi
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.predicates.IPredicate;
 
 public class AxiomsAdderIcfgTransformer<INLOC extends IcfgLocation, OUTLOC extends IcfgLocation>
-	implements IIcfgTransformer<OUTLOC> {
+		implements IIcfgTransformer<OUTLOC> {
 
 	private final BasicIcfg<OUTLOC> mResult;
 
-	public AxiomsAdderIcfgTransformer(final ILogger logger, final String resultName,
-			final Class<OUTLOC> outLocClazz, final IIcfg<INLOC> inputCfg,
-			final ILocationFactory<INLOC, OUTLOC> funLocFac,
-			final IBacktranslationTracker backtranslationTracker,
-			final Term additionalAxioms) {
-//			final Collection<Term> locationLiterals) {
-//			final Set<IProgramConst> locationLiterals) {
+	public AxiomsAdderIcfgTransformer(final ILogger logger, final String resultName, final Class<OUTLOC> outLocClazz,
+			final IIcfg<INLOC> inputCfg, final ILocationFactory<INLOC, OUTLOC> funLocFac,
+			final IBacktranslationTracker backtranslationTracker, final Term additionalAxioms) {
 		assert logger != null;
 
 		final CfgSmtToolkit inputCfgCsToolkit = inputCfg.getCfgSmtToolkit();
-//
 		final ManagedScript mgdScript = inputCfgCsToolkit.getManagedScript();
 
 		assert inputCfgCsToolkit.getAxioms().getClosedFormula() == inputCfgCsToolkit.getAxioms().getFormula();
-		final Term newAxioms = SmtUtils.and(mgdScript.getScript(), inputCfgCsToolkit.getAxioms().getClosedFormula(),
-				additionalAxioms);
+		final Term newAxioms =
+				SmtUtils.and(mgdScript.getScript(), inputCfgCsToolkit.getAxioms().getClosedFormula(), additionalAxioms);
 
 		assert additionalAxioms.getFreeVars().length == 0 : "axioms must be closed";
 
 		final IPredicate newAxiomsPred = new BasicPredicate(Boogie2SMT.HARDCODED_SERIALNUMBER_FOR_AXIOMS, new String[0],
 				additionalAxioms, Collections.emptySet(), newAxioms);
-
 
 		final CfgSmtToolkit newToolkit = new CfgSmtToolkit(inputCfgCsToolkit.getModifiableGlobalsTable(), mgdScript,
 				inputCfgCsToolkit.getSymbolTable(), newAxiomsPred, inputCfgCsToolkit.getProcedures(),
@@ -49,8 +43,7 @@ public class AxiomsAdderIcfgTransformer<INLOC extends IcfgLocation, OUTLOC exten
 				inputCfgCsToolkit.getIcfgEdgeFactory(), inputCfgCsToolkit.getConcurrencyInformation());
 
 		// make a copy of the input Icfg
-		final ITransformulaTransformer noopTransformulaTransformer =
-				new IdentityTransformer(inputCfgCsToolkit);
+		final ITransformulaTransformer noopTransformulaTransformer = new IdentityTransformer(inputCfgCsToolkit);
 		final IcfgTransformer<INLOC, OUTLOC> noopIcfgTransformer = new IcfgTransformer<>(logger, inputCfg, funLocFac,
 				backtranslationTracker, outLocClazz, resultName, noopTransformulaTransformer);
 		final BasicIcfg<OUTLOC> copiedIcfg = (BasicIcfg<OUTLOC>) noopIcfgTransformer.getResult();
