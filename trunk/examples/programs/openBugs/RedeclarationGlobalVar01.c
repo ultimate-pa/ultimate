3b85d1d1c2efca2122706da0b7ec8b523d2d3cae
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2018-11-11 19:55:31 +0100

    example

---
+++ trunk/examples/programs/openBugs/RedeclarationGlobalVar01.c
@@ -0,0 +1,28 @@
+/* #Safe
+ * 
+ * Example demonstrates the following problem:
+ * When we dispatch teh first declaration of g, we construct the 
+ * Boogie declaration of an on-heap variable ~#g~0.
+ * In the main function, we use the variable ~#g~0.
+ * When we dispatch the second declaration of g, we remove the
+ * Boogie declaration of ~#g~0 and declare a new
+ * off-heap variable ~g~0. (Note that this is a slightly different identifier.)
+ * 
+ * Author: heizmann@informatik.uni-freiburg.de
+ * Date: 2018-11-11
+ */
+
+
+struct myStruct {
+  int member;
+};
+
+static struct myStruct const g;
+
+int main(void)
+{
+  int *p = & g;
+  return 0;
+}
+
+static struct myStruct const g = {0}; 
