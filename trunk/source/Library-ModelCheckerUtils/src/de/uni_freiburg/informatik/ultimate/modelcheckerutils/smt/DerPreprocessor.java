ultimate-pa/ultimate

commit 59cddf6a00ea20594c6f732c3c944c57b7738b27

bugfix in PQE for arrays
* add support for the case where a self-update of a quantified variable
is done via a sequence of nested store terms
* fixes at least some instances of the "var is still there" bug

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/DerPreprocessor.java
@@ -28,7 +28,6 @@ package de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,7 +40,7 @@ import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermTransformer;
 import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.arrays.ArraySelect;
-import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.arrays.ArrayStore;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.arrays.NestedArrayStore;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.normalforms.NnfTransformer;
 import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.normalforms.NnfTransformer.QuantifierHandling;
@@ -214,10 +213,12 @@ public class DerPreprocessor extends TermTransformer {
 	}
 
 	private Term constructReplacement(final Term otherSide) {
-		final ArrayStore arrayStore = ArrayStore.convert(otherSide);
-		if (arrayStore != null) {
-			return constructReplacementForStoreCase(arrayStore.getArray(), arrayStore.getIndex(),
-					arrayStore.getValue());
+		if (!Arrays.asList(otherSide.getFreeVars()).contains(mEliminatee)) {
+			throw new AssertionError("This case should habe been handled by DER");
+		}
+		final NestedArrayStore nas = NestedArrayStore.convert(otherSide);
+		if (nas != null) {
+			return constructReplacementForStoreCase(nas);
 		}
 		final ArraySelect arraySelect = ArraySelect.convert(otherSide);
 		if (arraySelect != null) {
@@ -226,37 +227,55 @@ public class DerPreprocessor extends TermTransformer {
 		throw new UnsupportedOperationException("DerPreprocessor supports only store and select, but not " + otherSide);
 	}
 
-	private Term constructReplacementForStoreCase(final Term array, final Term index, final Term value) {
-		final Term newIndex;
-		if (Arrays.asList(index.getFreeVars()).contains(mEliminatee)) {
-			newIndex = mAuxVarCc.getOrConstruct(index);
-		} else {
-			newIndex = index;
+	private Term constructReplacementForStoreCase(final NestedArrayStore nas) {
+		final List<Term> newIndices = constructReplacementsIfNeeded(nas.getIndices());
+		final List<Term> newValues = constructReplacementsIfNeeded(nas.getValues());
+		if (newIndices != nas.getIndices() || newValues != nas.getValues()) {
+			mIntroducedDerPossibility = true;
 		}
-		final Term newValue;
-		if (Arrays.asList(value.getFreeVars()).contains(mEliminatee)) {
-			newValue = mAuxVarCc.getOrConstruct(value);
-		} else {
-			newValue = value;
-		}
-		Term result;
-		if (array.equals(mEliminatee)) {
-			// self-update
-			final Term select = mScript.term("select", array, newIndex);
-			result = QuantifierUtils.applyDerOperator(mScript, mQuantifier, select, newValue);
-		} else if (Arrays.asList(array.getFreeVars()).contains(mEliminatee)) {
-			throw new UnsupportedOperationException("nested self-update not yet implemented: " + array);
-		} else {
-			if (newValue != value || newIndex != index) {
-				mIntroducedDerPossibility = true;
+		final Term result;
+		if (nas.getArray().equals(mEliminatee)) {
+			// is (possibly nested) self-update
+			final LinkedList<Term> indices = new LinkedList<>(newIndices);
+			final LinkedList<Term> values = new LinkedList<>(newValues);
+			final Term[] resultDualFiniteJuncts = new Term[indices.size()];
+			for (int i = 0; i < newIndices.size(); i++) {
+				final Term innermostIndex = indices.removeFirst();
+				final Term innermostValue = values.removeFirst();
+				resultDualFiniteJuncts[i] = constructDisjointIndexImplication(innermostIndex, indices, innermostValue,
+						mEliminatee);
 			}
-			final Term store = mScript.term("store", array, newIndex, newValue);
-			result = QuantifierUtils.applyDerOperator(mScript, mQuantifier, mEliminatee, store);
+			assert indices.isEmpty();
+			values.isEmpty();
+			result = QuantifierUtils.applyDualFiniteConnective(mScript, mQuantifier, resultDualFiniteJuncts);
+		} else {
+			if (Arrays.asList(nas.getArray().getFreeVars()).contains(mEliminatee)) {
+				throw new UnsupportedOperationException(
+						"We have to descend beyond store chains. Introduce auxiliary variables only for arrays of lower dimension to avoid non-termination.");
+			}
+			result = new NestedArrayStore(nas.getArray(), newIndices, newValues).toTerm(mScript);
 		}
-		// TODO: let Prenex transformer deal with non-NNF terms and remove the
-		// following line
-		result = new NnfTransformer(mMgdScript, mServices, QuantifierHandling.CRASH).transform(result);
 		return result;
+	}
+
+	private List<Term> constructReplacementsIfNeeded(final List<Term> indices) {
+		final List<Term> newIndices = new ArrayList<>();
+		boolean replacementMade = false;
+		for (final Term index : indices) {
+			Term newIndex;
+			if (Arrays.asList(index.getFreeVars()).contains(mEliminatee)) {
+				newIndex = mAuxVarCc.getOrConstruct(index);
+				replacementMade = true;
+			} else {
+				newIndex = index;
+			}
+			newIndices.add(newIndex);
+		}
+		if (replacementMade) {
+			return newIndices;
+		} else {
+			return indices;
+		}
 	}
 
 	private Term constructReplacementForSelectCase(final Term array, final Term index) {
@@ -282,15 +301,21 @@ public class DerPreprocessor extends TermTransformer {
 	/**
 	 * Let oi_1,...,oi_n be the terms in otherIndices, construct the formula
 	 * ((idx != oi_1) /\ ... /\ (idx != oi_n)) ==> ((select arr idx) = value)
+	 * for existential quantification and the formula
+	 * (not ((idx == oi_1) \/ ... \/ (idx == oi_n))) /\ ((select arr idx) != value)
+	 * for universal quantification.
 	 */
 	private Term constructDisjointIndexImplication(final Term idx, final List<Term> otherIndices, final Term value,
 			final Term arr) {
 		final Term select = SmtUtils.select(mScript, arr, idx);
-		final Term selectEqualsValue = SmtUtils.binaryEquality(mScript, select, value);
-		final List<Term> conjuncts = otherIndices.stream().map(x -> SmtUtils.distinct(mScript, idx, x))
+		final Term selectEqualsValue = QuantifierUtils.applyDerOperator(mScript, mQuantifier, select, value);
+		final List<Term> dualFiniteJuncts = otherIndices.stream()
+				.map(x -> QuantifierUtils.applyAntiDerOperator(mScript, mQuantifier, idx, x))
 				.collect(Collectors.toList());
-		final Term conjunction = SmtUtils.and(mScript, conjuncts);
-		final Term result = SmtUtils.implies(mScript, conjunction, selectEqualsValue);
+		final Term dualFiniteJunction = QuantifierUtils.applyDualFiniteConnective(mScript, mQuantifier,
+				dualFiniteJuncts);
+		final Term result = QuantifierUtils.applyCorrespondingFiniteConnective(mScript, mQuantifier,
+				SmtUtils.not(mScript, dualFiniteJunction), selectEqualsValue);
 		return result;
 	}
 

trunk/source/Library-ModelCheckerUtilsTest/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/NestedStoreSequenceTest.java
@@ -54,7 +54,7 @@ import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
  * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  *
  */
-public class StoreChainTest {
+public class NestedStoreSequenceTest {
 
 	private IUltimateServiceProvider mServices;
 	private Script mScript;
@@ -77,19 +77,16 @@ public class StoreChainTest {
 		mScript.setLogic(Logics.ALL);
 	}
 
-
 	@Test
 	public void test0() {
 
 		final Sort bv8 = SmtSortUtils.getBitvectorSort(mScript, new BigInteger[] { BigInteger.valueOf(8) });
 		final Sort bv32 = SmtSortUtils.getBitvectorSort(mScript, new BigInteger[] { BigInteger.valueOf(32) });
-		final Sort array = SmtSortUtils.getArraySort(mScript, bv32, bv8);
 
 		mScript.declareFun("idx1", new Sort[0], bv32);
 		mScript.declareFun("val", new Sort[0], bv8);
 
-		final String formulaAsString =
-				"(exists ((a (Array (_ BitVec 32) (_ BitVec 8)))) (and (= (store a idx1 (_ bv5 8)) a) (= (select a idx1) val)))";
+		final String formulaAsString = "(exists ((a (Array (_ BitVec 32) (_ BitVec 8)))) (and (= (store a idx1 (_ bv5 8)) a) (= (select a idx1) val)))";
 
 		final Term formulaAsTerm = TermParseUtils.parseTerm(mScript, formulaAsString);
 		mLogger.info(formulaAsTerm);
@@ -103,29 +100,87 @@ public class StoreChainTest {
 
 	}
 
-
-
 	@Test
 	public void test1() {
 
 		final Sort bv8 = SmtSortUtils.getBitvectorSort(mScript, new BigInteger[] { BigInteger.valueOf(8) });
 		final Sort bv32 = SmtSortUtils.getBitvectorSort(mScript, new BigInteger[] { BigInteger.valueOf(32) });
-		final Sort array = SmtSortUtils.getArraySort(mScript, bv32, bv8);
 
 		mScript.declareFun("idx1", new Sort[0], bv32);
 		mScript.declareFun("idx2", new Sort[0], bv32);
 		mScript.declareFun("val", new Sort[0], bv8);
 
-		final String formulaAsString =
-				"(exists ((a (Array (_ BitVec 32) (_ BitVec 8)))) (and (= (store (store a idx1 (_ bv5 8)) idx2 (_ bv0 8)) a) (= (select a idx1) val)))";
+		final String formulaAsString = "(exists ((a (Array (_ BitVec 32) (_ BitVec 8)))) (and (= (store (store a idx1 (_ bv5 8)) idx2 (_ bv0 8)) a) (= (select a idx1) val)))";
 
 		final Term formulaAsTerm = TermParseUtils.parseTerm(mScript, formulaAsString);
 		mLogger.info(formulaAsTerm);
 		final Term result = PartialQuantifierElimination.tryToEliminate(mServices, mLogger, mMgdScript, formulaAsTerm,
 				SimplificationTechnique.SIMPLIFY_DDA, XnfConversionTechnique.BOTTOM_UP_WITH_LOCAL_SIMPLIFICATION);
+
+		final String expectedResultAsString = "(and (=> (= idx1 idx2) (= (_ bv0 8) val)) (=> (distinct idx1 idx2) (= (_ bv5 8) val)))";
+		final Term expectedResult = TermParseUtils.parseTerm(mScript, expectedResultAsString);
+
+		final boolean resultIsQuantifierFree = QuantifierUtils.isQuantifierFree(result);
+		final boolean resultIsEquivalentToExpectedResult = SmtTestUtils.areEquivalent(mScript, result, expectedResult);
+		Assert.assertTrue(resultIsQuantifierFree && resultIsEquivalentToExpectedResult);
+
 		mLogger.info(result);
 	}
 
+	@Test
+	public void test2() {
+
+		final Sort bv8 = SmtSortUtils.getBitvectorSort(mScript, new BigInteger[] { BigInteger.valueOf(8) });
+		final Sort bv32 = SmtSortUtils.getBitvectorSort(mScript, new BigInteger[] { BigInteger.valueOf(32) });
+
+		mScript.declareFun("idx1", new Sort[0], bv32);
+		mScript.declareFun("idx2", new Sort[0], bv32);
+		mScript.declareFun("idx3", new Sort[0], bv32);
+		mScript.declareFun("val", new Sort[0], bv8);
+
+		final String formulaAsString = "(exists ((a (Array (_ BitVec 32) (_ BitVec 8)))) (and (= (store (store (store a idx1 (_ bv5 8)) idx2 (_ bv0 8)) idx3 (_ bv23 8)) a) (= (select a idx1) val)))";
+
+		final Term formulaAsTerm = TermParseUtils.parseTerm(mScript, formulaAsString);
+		mLogger.info(formulaAsTerm);
+		final Term result = PartialQuantifierElimination.tryToEliminate(mServices, mLogger, mMgdScript, formulaAsTerm,
+				SimplificationTechnique.SIMPLIFY_DDA, XnfConversionTechnique.BOTTOM_UP_WITH_LOCAL_SIMPLIFICATION);
+
+		final String expectedResultAsString = "(and (=> (and (= idx1 idx2) (distinct idx1 idx3)) (= (_ bv0 8) val)) (=> (and (distinct idx1 idx2) (distinct idx1 idx3)) (= (_ bv5 8) val)) (=> (= idx1 idx3) (= (_ bv23 8) val)))";
+		final Term expectedResult = TermParseUtils.parseTerm(mScript, expectedResultAsString);
+
+		final boolean resultIsQuantifierFree = QuantifierUtils.isQuantifierFree(result);
+		final boolean resultIsEquivalentToExpectedResult = SmtTestUtils.areEquivalent(mScript, result, expectedResult);
+		Assert.assertTrue(resultIsQuantifierFree && resultIsEquivalentToExpectedResult);
+
+		mLogger.info(result);
+	}
+
+	@Test
+	public void test3() {
+
+		final Sort bv8 = SmtSortUtils.getBitvectorSort(mScript, new BigInteger[] { BigInteger.valueOf(8) });
+		final Sort bv32 = SmtSortUtils.getBitvectorSort(mScript, new BigInteger[] { BigInteger.valueOf(32) });
+
+		mScript.declareFun("idx1", new Sort[0], bv32);
+		mScript.declareFun("idx2", new Sort[0], bv32);
+		mScript.declareFun("idx3", new Sort[0], bv32);
+		mScript.declareFun("val", new Sort[0], bv8);
+
+		final String formulaAsString = "(not (exists ((a (Array (_ BitVec 32) (_ BitVec 8)))) (and (= (store (store (store a idx1 (_ bv5 8)) idx2 (_ bv0 8)) idx3 (_ bv23 8)) a) (= (select a idx1) val))))";
+
+		final Term formulaAsTerm = TermParseUtils.parseTerm(mScript, formulaAsString);
+		mLogger.info(formulaAsTerm);
+		final Term result = PartialQuantifierElimination.tryToEliminate(mServices, mLogger, mMgdScript, formulaAsTerm,
+				SimplificationTechnique.SIMPLIFY_DDA, XnfConversionTechnique.BOTTOM_UP_WITH_LOCAL_SIMPLIFICATION);
+
+		final String expectedResultAsString = "(not (and (=> (and (= idx1 idx2) (distinct idx1 idx3)) (= (_ bv0 8) val)) (=> (and (distinct idx1 idx2) (distinct idx1 idx3)) (= (_ bv5 8) val)) (=> (= idx1 idx3) (= (_ bv23 8) val))))";
+		final Term expectedResult = TermParseUtils.parseTerm(mScript, expectedResultAsString);
+
+		final boolean resultIsQuantifierFree = QuantifierUtils.isQuantifierFree(result);
+		final boolean resultIsEquivalentToExpectedResult = SmtTestUtils.areEquivalent(mScript, result, expectedResult);
+		Assert.assertTrue(resultIsQuantifierFree && resultIsEquivalentToExpectedResult);
+
+		mLogger.info(result);
+	}
 
 }
-

commit b7e7bfc40ed52c7c2f56177ff60df6642cd8c288

bugfix: some terms constructed by array PQE did not typecheck, the bug was introduced in https://github.com/ultimate-pa/ultimate/commit/59cddf6a00ea20594c6f732c3c944c57b7738b27

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/DerPreprocessor.java
@@ -253,7 +253,8 @@ public class DerPreprocessor extends TermTransformer {
 				throw new UnsupportedOperationException(
 						"We have to descend beyond store chains. Introduce auxiliary variables only for arrays of lower dimension to avoid non-termination.");
 			}
-			result = new NestedArrayStore(nas.getArray(), newIndices, newValues).toTerm(mScript);
+			result = QuantifierUtils.applyDerOperator(mScript, mQuantifier,
+					new NestedArrayStore(nas.getArray(), newIndices, newValues).toTerm(mScript), mEliminatee);
 		}
 		return result;
 	}