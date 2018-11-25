ultimate-pa/ultimate

commit a8b2a85e9715c50626ccd4b97551d1546189e469

optimization: try to immediately eliminate auxvars via DER

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/boogie/Statements2TransFormula.java
@@ -193,7 +193,6 @@ public class Statements2TransFormula {
 			formula = mAssumes;
 		}
 		formula = mBoogie2SMT.getSmtSymbols().inline(mScript, formula);
-		formula = eliminateAuxVars(formula, mAuxVars);
 
 		Infeasibility infeasibility = null;
 		if (simplify) {
@@ -293,6 +292,9 @@ public class Statements2TransFormula {
 			final Term eq = mScript.term("=", tv, rhsTerm);
 
 			mAssumes = SmtUtils.and(mScript, eq, mAssumes);
+			if (!mAuxVars.isEmpty()) {
+				mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+			}
 			if (s_ComputeAsserts) {
 				mAsserts = Util.implies(mScript, eq, mAsserts);
 			}
@@ -322,6 +324,9 @@ public class Statements2TransFormula {
 		final Term f = tlres.getTerm();
 
 		mAssumes = SmtUtils.and(mScript, f, mAssumes);
+		if (!mAuxVars.isEmpty()) {
+			mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+		}
 		if (s_ComputeAsserts) {
 			mAsserts = Util.implies(mScript, f, mAsserts);
 		}
@@ -336,6 +341,10 @@ public class Statements2TransFormula {
 			final Term f = tlres.getTerm();
 
 			mAssumes = SmtUtils.and(mScript, f, mAssumes);
+			if (!mAuxVars.isEmpty()) {
+				mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+			}
+
 			mAsserts = SmtUtils.and(mScript, f, mAsserts);
 			assert assertTermContainsNoNull(mAssumes);
 		} else {
@@ -433,6 +442,9 @@ public class Statements2TransFormula {
 				mOverapproximations.putAll(tlres.getOverappoximations());
 				final Term f = tlres.getTerm();
 				mAssumes = SmtUtils.and(mScript, f, mAssumes);
+				if (!mAuxVars.isEmpty()) {
+					mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+				}
 				if (s_ComputeAsserts) {
 					if (spec.isFree()) {
 						mAsserts = Util.implies(mScript, f, mAsserts);
@@ -455,6 +467,9 @@ public class Statements2TransFormula {
 				mOverapproximations.putAll(tlres.getOverappoximations());
 				final Term f = tlres.getTerm();
 				mAssumes = SmtUtils.and(mScript, f, mAssumes);
+				if (!mAuxVars.isEmpty()) {
+					mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+				}
 				if (s_ComputeAsserts) {
 					if (spec.isFree()) {
 						mAsserts = Util.implies(mScript, f, mAsserts);
@@ -659,7 +674,7 @@ public class Statements2TransFormula {
 	 *            set of free variables occurring in input
 	 * @return
 	 */
-	private Term eliminateAuxVars(final Term input, final Set<TermVariable> auxVars) {
+	private Term eliminateAuxVarsViaDer(final Term input, final Set<TermVariable> auxVars) {
 		final XnfDer xnfDer = new XnfDer(mMgdScript, mServices);
 		final Term result = SmtUtils.and(mScript,
 				xnfDer.tryToEliminate(QuantifiedFormula.EXISTS, SmtUtils.getConjuncts(input), auxVars));
@@ -780,6 +795,9 @@ public class Statements2TransFormula {
 		}
 		assert arguments.length == offset;
 		mAssumes = SmtUtils.and(mScript, assignments);
+		if (!mAuxVars.isEmpty()) {
+			mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+		}
 		return getTransFormula(false, true, simplificationTechnique);
 	}
 
@@ -810,6 +828,9 @@ public class Statements2TransFormula {
 			offset++;
 		}
 		mAssumes = SmtUtils.and(mScript, assignments);
+		if (!mAuxVars.isEmpty()) {
+			mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+		}
 		return getTransFormula(false, true, simplificationTechnique);
 	}
 
@@ -838,6 +859,9 @@ public class Statements2TransFormula {
 			offset++;
 		}
 		mAssumes = SmtUtils.and(mScript, assignments);
+		if (!mAuxVars.isEmpty()) {
+			mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+		}
 		return getTransFormula(false, true, simplificationTechnique);
 	}
 
@@ -874,6 +898,9 @@ public class Statements2TransFormula {
 		}
 		assert st.getLhs().length == offset;
 		mAssumes = SmtUtils.and(mScript, assignments);
+		if (!mAuxVars.isEmpty()) {
+			mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+		}
 		return getTransFormula(false, true, simplicationTechnique);
 	}
 
@@ -904,6 +931,9 @@ public class Statements2TransFormula {
 		}
 		assert st.getLhs().length == offset;
 		mAssumes = SmtUtils.and(mScript, assignments);
+		if (!mAuxVars.isEmpty()) {
+			mAssumes = eliminateAuxVarsViaDer(mAssumes, mAuxVars);
+		}
 		return getTransFormula(false, true, simplificationTechnique);
 	}
 

commit c8fab0646e033a253a37a93efc55feb1a7f1ddb1

bugfix Statements2TransFormula
* fix bugs that were introduced in
a8b2a85e9715c50626ccd4b97551d1546189e469 and
86758bec36043d39c6ca2bb43141cc54987720ef
* update documentation to the changes of
86758bec36043d39c6ca2bb43141cc54987720ef

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/boogie/Statements2TransFormula.java
@@ -292,11 +292,11 @@ public class Statements2TransFormula {
 			final Term eq = mScript.term("=", tv, rhsTerm);
 
 			mAssumes = SmtUtils.and(mScript, eq, mAssumes);
-			eliminateAuxVarsViaDer();
 			if (s_ComputeAsserts) {
 				mAsserts = Util.implies(mScript, eq, mAsserts);
 			}
 		}
+		eliminateAuxVarsViaDer();
 	}
 
 	private void addHavoc(final HavocStatement havoc) {
@@ -653,13 +653,10 @@ public class Statements2TransFormula {
 	}
 
 	/**
-	 * Eliminate auxVars from input if possible. Let {x_1,...,x_n} be a subset of auxVars. Returns a term that is
-	 * equivalent to ∃x_1,...,∃x_n input and remove {x_1,...,x_n} from auxVars. The set {x_1,...,x_n} is determined by
-	 * NaiveDestructiveEqualityResolution.
-	 *
-	 * Returns term that is equisatisfiable to input. If a x is free variable
-	 *
-	 * @return
+	 * Eliminate auxVars from input if possible. Let {x_1,...,x_n} be a subset of
+	 * auxVars. Returns a term that is equivalent to ∃x_1,...,∃x_n input and remove
+	 * {x_1,...,x_n} from auxVars. The set {x_1,...,x_n} is determined by
+	 * Destructive Equality Resolution {@link XnfDer}.
 	 */
 	private void eliminateAuxVarsViaDer() {
 		if (mAuxVars.isEmpty()) {
@@ -668,7 +665,6 @@ public class Statements2TransFormula {
 		final XnfDer xnfDer = new XnfDer(mMgdScript, mServices);
 		mAssumes = SmtUtils.and(mScript,
 				xnfDer.tryToEliminate(QuantifiedFormula.EXISTS, SmtUtils.getConjuncts(mAssumes), mAuxVars));
-		mAuxVars.clear();
 	}
 
 	/**