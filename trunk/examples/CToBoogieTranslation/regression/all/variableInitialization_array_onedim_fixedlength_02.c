11a32314e567a6972e13cbc92123703b10837d49
Alexander Nutz <nutz@informatik.uni-freiburg.de>
2018-11-26 21:48:19 +0100

    modify test so it does not trigger the acsl on heap/off heap bug anymore

--- trunk/examples/CToBoogieTranslation/regression/all/variableInitialization_array_onedim_fixedlength_02.c
+++ trunk/examples/CToBoogieTranslation/regression/all/variableInitialization_array_onedim_fixedlength_02.c
@@ -1,7 +1,18 @@
 //#Safe
 int main() {
   int a[3] = { 1, 2 };
-  //@ assert a[0] == 1;
-  //@ assert a[1] == 2;
-  //@ assert a[2] == 0;
+
+  if (a[0] != 1) {
+    //@ assert \false;
+  }
+  if (a[1] != 2) {
+    //@ assert \false;
+  }
+  if (a[2] != 0) {
+    //@ assert \false;
+  }
+
+//  //@ assert a[0] == 1;
+//  //@ assert a[1] == 2;
+//  //@ assert a[2] == 0;
 }
