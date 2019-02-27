6ba9dd8f75d7a884c7fd9650ea47551b31935975
Matthias <heizmann@informatik.uni-freiburg.de>
2019-02-27 16:25:10 +0100

    add test for polynomial terms

---
+++ trunk/source/Library-ModelCheckerUtilsTest/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/PolynomialTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2019 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * Copyright (C) 2019 University of Freiburg
+ *
+ * This file is part of the ULTIMATE ModelCheckerUtilsTest Library.
+ *
+ * The ULTIMATE ModelCheckerUtilsTest Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE ModelCheckerUtilsTest Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE ModelCheckerUtilsTest Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE ModelCheckerUtilsTest Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE ModelCheckerUtilsTest Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger.LogLevel;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.logic.Logics;
+import de.uni_freiburg.informatik.ultimate.logic.Script;
+import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
+import de.uni_freiburg.informatik.ultimate.logic.Sort;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.linearterms.AffineTerm;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.linearterms.AffineTermTransformer;
+import de.uni_freiburg.informatik.ultimate.modelcheckerutils.smt.managedscript.ManagedScript;
+import de.uni_freiburg.informatik.ultimate.smtsolver.external.TermParseUtils;
+import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
+
+/**
+ *
+ * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ *
+ */
+public class PolynomialTest {
+
+	private IUltimateServiceProvider mServices;
+	private Script mScript;
+	private ManagedScript mMgdScript;
+	private ILogger mLogger;
+	private Sort mRealSort;
+	private Sort mBoolSort;
+	private Sort mIntSort;
+	private Sort[] mEmptySort;
+	private Term mTrueTerm;
+
+	@Before
+	public void setUp() {
+		mServices = UltimateMocks.createUltimateServiceProviderMock();
+		mLogger = mServices.getLoggingService().getLogger("lol");
+		mScript = UltimateMocks.createZ3Script(LogLevel.INFO);
+		mMgdScript = new ManagedScript(mServices, mScript);
+		mScript.setLogic(Logics.ALL);
+		mRealSort = SmtSortUtils.getRealSort(mMgdScript);
+		mBoolSort = SmtSortUtils.getBoolSort(mMgdScript);
+		mIntSort = SmtSortUtils.getIntSort(mMgdScript);
+		mEmptySort = new Sort[0];
+		mTrueTerm = mScript.term("true");
+	}
+
+	@After
+	public void tearDown() {
+		mScript.exit();
+	}
+
+	@Test
+	public void affineTermTest01() {
+		final Sort intSort = SmtSortUtils.getIntSort(mMgdScript);
+		mScript.declareFun("x", new Sort[0], intSort);
+		mScript.declareFun("y", new Sort[0], intSort);
+		final String formulaAsString = "(+ (* 2 x) y)";
+		final Term formulaAsTerm = TermParseUtils.parseTerm(mScript, formulaAsString);
+		mLogger.info("Input: " + formulaAsTerm);
+		final AffineTerm result = (AffineTerm) new AffineTermTransformer(mScript).transform(formulaAsTerm);
+		final Term resultAsTerm = result.toTerm(mScript);
+		mLogger.info("Output: " + resultAsTerm);
+		final boolean resultIsCorrect = areEquivalent(mScript, formulaAsTerm, resultAsTerm);
+		Assert.assertTrue(resultIsCorrect);
+	}
+
+	private static boolean areEquivalent(final Script script, final Term formulaAsTerm, final Term resultAsTerm) {
+		final Term equality = SmtUtils.binaryEquality(script, formulaAsTerm, resultAsTerm);
+		final Term negatedEquality = SmtUtils.not(script, equality);
+		script.push(1);
+		script.assertTerm(negatedEquality);
+		final LBool lbool = script.checkSat();
+		script.pop(1);
+		return lbool == LBool.UNSAT;
+	}
+
+}
