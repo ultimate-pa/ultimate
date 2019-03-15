a1b68bf6ffc4386f4c413b78698d1aa949ec07b3
Claus Schätzle <schaetzc@tf.uni-freiburg.de>
2019-03-15 10:23:24 +0100

    re-licenced copied code from EPL 2.0 to LGPL 3.0 courtesy of its author Johannes Späth

--- trunk/source/Library-PathExpressionsTest/src/de/uni_freiburg/informatik/ultimate/lib/pathexpressions/test/IntGraph.java
+++ trunk/source/Library-PathExpressionsTest/src/de/uni_freiburg/informatik/ultimate/lib/pathexpressions/test/IntGraph.java
@@ -1,14 +1,33 @@
-/*******************************************************************************
- * Copyright (c) 2018 Fraunhofer IEM, Paderborn, Germany.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
- *  
- * SPDX-License-Identifier: EPL-2.0
+/*
+ * Code taken from https://github.com/johspaeth/PathExpression
+ * Copyright (C) 2018 Johannes Spaeth
+ * Copyright (C) 2018 Fraunhofer IEM, Paderborn, Germany
+ * 
+ * Copyright (C) 2019 Claus Schätzle (schaetzc@tf.uni-freiburg.de)
+ * Copyright (C) 2019 University of Freiburg
  *
- * Contributors:
- *     Johannes Spaeth - initial API and implementation
- *******************************************************************************/
+ * This file is part of the ULTIMATE Library-PathExpressions plug-in.
+ *
+ * The ULTIMATE Library-PathExpressions plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Library-PathExpressions plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Library-PathExpressions plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Library-PathExpressions plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Library-PathExpressions plug-in grant you additional permission
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.lib.pathexpressions.test;
 
 import java.util.HashSet;
