ee5a72057900e874abb685ebfbcdd9377bc73121
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2019-04-16 03:32:46 +0200

    example

--- trunk/examples/programs/toy/tooDifficultLoopInvariant/Krtek01.c
+++ trunk/examples/programs/toy/tooDifficultLoopInvariant/Krtek01.c
@@ -11,9 +11,11 @@ extern void __VERIFIER_error() __attribute__ ((__noreturn__));
 int main(void) {
 	unsigned int x = 1;
 	unsigned int y = 1;
-	// loop until x's value is among the largest 256*256 numbers
-	// that can be stored in an unsigned int
-	while(x + 256*256 < 256*256) {
+	// loop until x's value is in the first half of the numbers that
+	// an unsigned int can have on an x86 computer
+	//@ loop invariant (x % 2 == 1 && y % 2 == 1);
+	while(x < 256*256*256*254U) {
+		// choose nondeterministically between if-branch and else-branch
 		if (__VERIFIER_nondet_int()) {
 			// note that in C11 unsigned ints cannot overflow,
 			// because the result is always the remainder of
@@ -28,8 +30,9 @@ int main(void) {
 			y = tmp;
 		}
 	}
-	// the program is erroneous
-	if (y == 0) {
+	// the program is correct if y is never four at the end
+	// of the loop
+	if (y == 4) {
 		__VERIFIER_error();
 	}
 	return 0;
