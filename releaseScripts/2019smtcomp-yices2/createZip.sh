1c4541d60c2b3033349ce96a13c503d63d635ae7
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2019-05-21 14:32:06 +0200

    UltimateEliminator release scripts

---
+++ releaseScripts/2019smtcomp-yices2/createZip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir StarExecArchive
+mkdir StarExecArchive/bin
+mkdir StarExecArchive/Ultimate
+cp -a ../../trunk/source/BA_SiteRepository/target/products/CLI-E4/linux/gtk/x86_64/* StarExecArchive/Ultimate/
+cp *LICENSE* StarExecArchive/Ultimate/
+cp starexec_description.txt StarExecArchive/
+cp ../../trunk/examples/toolchains/Empty.xml StarExecArchive/
+cp starexec_run_* StarExecArchive/bin/
+cp Ultimate.ini StarExecArchive/Ultimate/
+cp ../../trunk/examples/settings/UltimateEliminator/* StarExecArchive/
+cp -LR /opt/bin/yices-smt2 StarExecArchive/bin/
+#cp -LR /opt/bin/mathsat StarExecArchive/bin/
+
+
+cd StarExecArchive
+zip ../UltimateCommandline.zip -r *
+
