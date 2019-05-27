ca5e7c62fcbdf6b1391b8f298514505255773ffb
Daniel Dietsch <daniel.dietsch@gmail.com>
2019-05-27 18:05:03 +0200

    remove spaces

--- releaseScripts/default/makeFresh.sh
+++ releaseScripts/default/makeFresh.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
-# This script builds Ultimate with maven and then calls makeZip.sh for all tools that can be deployed. 
+# This script builds Ultimate with maven and then calls makeZip.sh for all tools that can be deployed.
 
-## include the makeSettings shared functions 
+## include the makeSettings shared functions
 DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/makeSettings.sh"
 
 
-## start the actual script 
+## start the actual script
 pushd ../../trunk/source/BA_MavenParentUltimate/ > /dev/null
 test mvn -T 1C clean install -Pmaterialize
 popd > /dev/null
 
 for platform in {linux,win32}; do
     # createZip <toolname> <targetarch> <reachtc> <termtc> <witnessvaltc> <memsafetytc> <ltlc> <termwitnessvaltc>
-    # Taipan 
+    # Taipan
     exitOnFail bash createZip.sh Taipan $platform AutomizerCInline_WitnessPrinter.xml NONE AutomizerCInline.xml AutomizerCInline_WitnessPrinter.xml NONE NONE
 
-    # Automizer without separate blockencoding plugin 
+    # Automizer without separate blockencoding plugin
     exitOnFail bash createZip.sh Automizer $platform AutomizerCInline_WitnessPrinter.xml BuchiAutomizerCInline_WitnessPrinter.xml AutomizerC.xml AutomizerC_WitnessPrinter.xml LTLAutomizerC.xml BuchiAutomizerCInline.xml
 
-    # Automizer with separate blockencoding plugin 
+    # Automizer with separate blockencoding plugin
     #exitOnFail bash createZip.sh Automizer linux AutomizerC_BE_WitnessPrinter.xml BuchiAutomizerCInline_BE_WitnessPrinter.xml AutomizerC.xml AutomizerC_BE_WitnessPrinter.xml LTLAutomizerC.xml BuchiAutomizerCInline.xml
 
-    # Kojak 
+    # Kojak
     exitOnFail bash createZip.sh Kojak $platform KojakC_WitnessPrinter.xml NONE NONE KojakC_WitnessPrinter.xml NONE NONE
 
     # DeltaDebugger
-    exitOnFail bash createDeltaDebuggerDir.sh $platform 
-done 
+    exitOnFail bash createDeltaDebuggerDir.sh $platform
+done
 
 exit 0
