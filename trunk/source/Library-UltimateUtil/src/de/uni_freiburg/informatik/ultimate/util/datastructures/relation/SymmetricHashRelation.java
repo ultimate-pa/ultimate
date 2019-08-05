7f27f6edf0ffe40e573b34db339ff76e9e78736e
danieldietsch <daniel.dietsch@gmail.com>
2019-08-05 17:37:08 +0200

    cc fixes

--- trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/relation/SymmetricHashRelation.java
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/relation/SymmetricHashRelation.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2017 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2017 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Util Library.
- * 
+ *
  * The ULTIMATE Util Library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Util Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Util Library. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Util Library, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
@@ -34,7 +34,7 @@ import de.uni_freiburg.informatik.ultimate.util.datastructures.Doubleton;
 
 /**
  * Implementation of the AbstractRelation that uses HashMap and HashSet.
- * 
+ *
  * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * @author Daniel Tischner {@literal <zabuza.dev@gmail.com>}
  */
@@ -57,15 +57,15 @@ public class SymmetricHashRelation<E> extends HashRelation<E, E> {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see de.uni_freiburg.informatik.ultimate.util.datastructures.relation.
 	 * AbstractRelation#removePair(java.lang.Object, java.lang.Object)
 	 */
 	@Override
 	public boolean removePair(final E domainElem, final E rangeElem) {
 		// Remove pair in both directions as it is considered to be symmetric
-		boolean containedPairFirstDirection = super.removePair(domainElem, rangeElem);
-		boolean containedPairSecondDirection = super.removePair(rangeElem, domainElem);
+		final boolean containedPairFirstDirection = super.removePair(domainElem, rangeElem);
+		final boolean containedPairSecondDirection = super.removePair(rangeElem, domainElem);
 
 		// A valid symmetric relation always needs to hold both directions if any
 		assert containedPairFirstDirection == containedPairSecondDirection;
@@ -76,7 +76,7 @@ public class SymmetricHashRelation<E> extends HashRelation<E, E> {
 	public Set<Doubleton<E>> buildSetOfDoubletons() {
 		final Set<Doubleton<E>> result = new HashSet<>();
 		for (final Entry<E, E> entry : entrySet()) {
-			result.add(new Doubleton<E>(entry.getKey(), entry.getValue()));
+			result.add(new Doubleton<>(entry.getKey(), entry.getValue()));
 		}
 		return result;
 	}
@@ -85,30 +85,30 @@ public class SymmetricHashRelation<E> extends HashRelation<E, E> {
 		final Set<Doubleton<E>> result = new HashSet<>();
 		for (final Entry<E, E> entry : entrySet()) {
 			if (!entry.getKey().equals(entry.getValue())) {
-				result.add(new Doubleton<E>(entry.getKey(), entry.getValue()));
+				result.add(new Doubleton<>(entry.getKey(), entry.getValue()));
 			}
 		}
 		return result;
 	}
 
 	/**
-	 * Transform this relation into its transitive closure and return all the
-	 * difference of the input and the transitive closure as set of doubletons.
+	 * Transform this relation into its transitive closure and return all the difference of the input and the transitive
+	 * closure as set of doubletons.
 	 */
 	public Set<Doubleton<E>> makeTransitive() {
-		final Set<Doubleton<E>> allAddedDoubletons = new HashSet<Doubleton<E>>();
+		final Set<Doubleton<E>> allAddedDoubletons = new HashSet<>();
 		Set<Doubleton<E>> recentlyAddedDoubletons = this.buildSetOfDoubletons();
 		while (!recentlyAddedDoubletons.isEmpty()) {
-			final Set<Doubleton<E>> newDoubletons = new HashSet<Doubleton<E>>();
+			final Set<Doubleton<E>> newDoubletons = new HashSet<>();
 			for (final Doubleton<E> doubleton : recentlyAddedDoubletons) {
-				for (final E third : this.getImage(doubleton.getOneElement())) {
-					if (!this.containsPair(doubleton.getOtherElement(), third)) {
-						newDoubletons.add(new Doubleton<E>(doubleton.getOtherElement(), third));
+				for (final E third : getImage(doubleton.getOneElement())) {
+					if (!containsPair(doubleton.getOtherElement(), third)) {
+						newDoubletons.add(new Doubleton<>(doubleton.getOtherElement(), third));
 					}
 				}
-				for (final E third : this.getImage(doubleton.getOtherElement())) {
-					if (!this.containsPair(doubleton.getOneElement(), third)) {
-						newDoubletons.add(new Doubleton<E>(doubleton.getOneElement(), third));
+				for (final E third : getImage(doubleton.getOtherElement())) {
+					if (!containsPair(doubleton.getOneElement(), third)) {
+						newDoubletons.add(new Doubleton<>(doubleton.getOneElement(), third));
 					}
 				}
 			}
