1dce23c378f1e607c2a6875d2459a348139ed306
bree@titan <loeffler.uni@gmail.com>
2019-09-16 14:10:27 +0200

    don't use getAssertions()

--- trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/smt/SMTFeatureExtractorScript.java
+++ trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/smt/SMTFeatureExtractorScript.java
@@ -1,6 +1,9 @@
 package de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
@@ -12,6 +15,7 @@ import de.uni_freiburg.informatik.ultimate.logic.WrapperScript;
 public class SMTFeatureExtractorScript extends WrapperScript {
 
 	private final SMTFeatureExtractor mFeatureExtractor;
+	private List<Term> mCurrentAssertionStack;
 	private final ILogger mLogger;
 	private final IUltimateServiceProvider mServices;
 
@@ -27,34 +31,40 @@ public class SMTFeatureExtractorScript extends WrapperScript {
 		mLogger = logger;
 		mServices = services;
 		mFeatureExtractor = new SMTFeatureExtractor(logger, services, dump_path);
+		mCurrentAssertionStack = new ArrayList<Term>();
 	}
 
 	@Override
 	public LBool assertTerm(final Term term) throws SMTLIBException {
 		// TODO Auto-generated method stub
+		mLogger.warn("ASSERT: " + term.toStringDirect());
+		mCurrentAssertionStack.add(term);
 		return super.assertTerm(term);
 	}
 
 	@Override
 	public void push(final int levels) throws SMTLIBException {
 		// TODO Auto-generated method stub
+		mLogger.warn("PUSH " + levels);
 		super.push(levels);
 	}
 
 	@Override
 	public void pop(final int levels) throws SMTLIBException {
 		// TODO Auto-generated method stub
+		mLogger.warn("POP " + levels);
 		super.pop(levels);
 	}
 
 	@Override
 	public LBool checkSat() throws SMTLIBException {
+		mLogger.warn("CHECK SAT");
 		final double start = System.nanoTime();
 		final LBool sat = super.mScript.checkSat();
 		final double analysisTime = (System.nanoTime() - start) / 1000;
-		final Term[] assertions = super.mScript.getAssertions();
 		try {
-			mFeatureExtractor.extractFeature(assertions, analysisTime, sat.toString());
+			mFeatureExtractor.extractFeature(mCurrentAssertionStack, analysisTime, sat.toString());
+			mCurrentAssertionStack = new ArrayList<Term>();
 		} catch (IllegalAccessException | IOException e) {
 			mLogger.error(e);
 		}
