d482a106392e4cc314104c5558a9a03796af0f62
Frank Schüssele <fr.schuessele@gmx.de>
2019-09-17 15:52:35 +0200

    Fix MCR

---
+++ trunk/source/Library-MCR/src/de/uni_freiburg/informatik/ultimate/lib/mcr/MCR.java
@@ -0,0 +1,136 @@
+package de.uni_freiburg.informatik.ultimate.lib.mcr;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import de.uni_freiburg.informatik.ultimate.core.lib.exceptions.ToolchainCanceledException;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.core.model.translation.IProgramExecution;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IIcfgTransition;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgLocation;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.hoaretriple.IHoareTripleChecker;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.interpolant.IInterpolantGenerator;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.interpolant.InterpolantComputationStatus;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.managedscript.ManagedScript;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.predicates.IPredicate;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.predicates.IPredicateUnifier;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.tracecheck.ITraceCheck;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.tracecheck.ITraceCheckPreferences;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.tracecheck.TraceCheckReasonUnknown;
+import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.util.statistics.IStatisticsDataProvider;
+
+/**
+ *
+ * @author Frank Schüssele (schuessf@informatik.uni-freiburg.de)
+ *
+ */
+public class MCR<LETTER extends IIcfgTransition<?>> implements ITraceCheck, IInterpolantGenerator<LETTER> {
+	private final ILogger mLogger;
+	private final IPredicateUnifier mPredicateUnifier;
+	private final IHoareTripleChecker mHtc;
+	private final List<LETTER> mTrace;
+	private final IUltimateServiceProvider mServices;
+	private final ManagedScript mScript;
+	private final IPredicate mTruePred;
+	private final IPredicate mFalsePred;
+
+	public MCR(final ILogger logger, final ITraceCheckPreferences prefs, final IPredicateUnifier predicateUnifier,
+			final IHoareTripleChecker htc, final List<LETTER> counterexample) {
+		mLogger = logger;
+		mPredicateUnifier = predicateUnifier;
+		mHtc = htc;
+		mTrace = counterexample;
+		mServices = prefs.getUltimateServices();
+		mScript = prefs.getCfgSmtToolkit().getManagedScript();
+		mTruePred = mPredicateUnifier.getOrConstructPredicate(mScript.getScript().term("true"));
+		mFalsePred = mPredicateUnifier.getOrConstructPredicate(mScript.getScript().term("false"));
+	}
+
+	@Override
+	public List<LETTER> getTrace() {
+		return mTrace;
+	}
+
+	@Override
+	public IPredicate[] getInterpolants() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPredicateUnifier getPredicateUnifier() {
+		return mPredicateUnifier;
+	}
+
+	@Override
+	public boolean isPerfectSequence() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public InterpolantComputationStatus getInterpolantComputationStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LBool isCorrect() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IPredicate getPrecondition() {
+		return mTruePred;
+	}
+
+	@Override
+	public IPredicate getPostcondition() {
+		return mFalsePred;
+	}
+
+	@Override
+	public Map<Integer, IPredicate> getPendingContexts() {
+		return Collections.emptyMap();
+	}
+
+	@Override
+	public boolean providesRcfgProgramExecution() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public IProgramExecution<IIcfgTransition<IcfgLocation>, Term> getRcfgProgramExecution() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStatisticsDataProvider getTraceCheckBenchmark() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ToolchainCanceledException getToolchainCanceledExpection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TraceCheckReasonUnknown getTraceCheckReasonUnknown() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean wasTracecheckFinishedNormally() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
