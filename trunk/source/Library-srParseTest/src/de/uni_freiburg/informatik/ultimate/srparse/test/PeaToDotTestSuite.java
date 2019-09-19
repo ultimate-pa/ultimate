6e1dacc8f08625fa2c4e0a105208dbb9c7cee2d1
hauff <hauffn@informatik.uni-freiburg.de>
2019-09-19 11:35:05 +0200

    dot

--- trunk/source/Library-srParseTest/src/de/uni_freiburg/informatik/ultimate/srparse/test/PeaToDotTestSuite.java
+++ trunk/source/Library-srParseTest/src/de/uni_freiburg/informatik/ultimate/srparse/test/PeaToDotTestSuite.java
@@ -1,10 +1,13 @@
 package de.uni_freiburg.informatik.ultimate.srparse.test;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -22,11 +25,14 @@ import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
 @RunWith(Parameterized.class)
 public class PeaToDotTestSuite {
 
+	private static String SCOPE_PREFIX = "SrParseScope";
+	private static String PATH = "/home/ubuntu/Desktop/Patterns/";
+
 	private final IUltimateServiceProvider mServiceProvider;
 	private final ILogger mLogger;
 	private final PatternType mPattern;
-	Map<String, Integer> mDuration2Bounds;
-	String mName;
+	private final Map<String, Integer> mDuration2Bounds;
+	private final String mName;
 
 	public PeaToDotTestSuite(final PatternType pattern, final Map<String, Integer> duration2Bounds, final String name) {
 		mServiceProvider = UltimateMocks.createUltimateServiceProviderMock(LogLevel.DEBUG);
@@ -36,18 +42,21 @@ public class PeaToDotTestSuite {
 		mName = name;
 	}
 
-	// @Test
-	public void testDot() throws Exception {
-		final String name = mPattern.getClass().getSimpleName();
-		final String scope = mPattern.getScope().getClass().getSimpleName().replace("SrParseScope", "");
-		final String filename = "/home/ubuntu/Desktop/Patterns/" + name + "_" + scope + ".dot";
+	@Test
+	public void testDot() throws IOException {
+		final String scope = mPattern.getScope().getClass().getSimpleName().replace(SCOPE_PREFIX, "");
+		final String filename = PATH + mName + "_" + scope + ".dot";
 
 		try {
 			final PhaseEventAutomata pea = mPattern.transformToPea(mLogger, mDuration2Bounds);
 			DotWriterNew.write(filename, true, pea, mLogger);
 		} catch (final PatternScopeNotImplemented e) {
-
+			// Ooops, somebody forgot to implement that sh.. ;-)
 		}
+
+		final FileWriter writer = new FileWriter(PATH + mName + "_" + scope + ".md");
+		writer.write(mPattern.toString().replace(mPattern.getId() + ": ", ""));
+		writer.close();
 	}
 
 	@Parameters(name = "{2}")
