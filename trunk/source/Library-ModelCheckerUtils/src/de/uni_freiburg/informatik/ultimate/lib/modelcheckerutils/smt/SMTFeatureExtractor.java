fea91168887c504ea4d7bfb735d5715e8ae6a2f8
bree@titan <loeffler.uni@gmail.com>
2019-09-20 11:30:41 +0200

    set dependencyScore in SMTfeature

--- trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/smt/SMTFeatureExtractor.java
+++ trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/smt/SMTFeatureExtractor.java
@@ -2,11 +2,9 @@ package de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -15,7 +13,6 @@ import java.util.List;
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
-import de.uni_freiburg.informatik.ultimate.smtinterpol.util.DAGSize;
 
 public class SMTFeatureExtractor {
 
@@ -26,42 +23,42 @@ public class SMTFeatureExtractor {
 	private final String mDumpPath;
 	private final String mFilename;
 
-	public SMTFeatureExtractor(ILogger logger, IUltimateServiceProvider services, String dump_path) {
+	public SMTFeatureExtractor(final ILogger logger, final IUltimateServiceProvider services, final String dump_path) {
 		mLogger = logger;
 		mServices = services;
 		mFeatures = new ArrayList<>();
 		mDumpPath = dump_path;
-		String timestamp = ZonedDateTime.now().format(DateTimeFormatter.ofPattern( "uuuu-MM-dd" ));
-		mFilename = mDumpPath + timestamp + "-smtfeatures.csv"; 
-		File f = new File(mFilename);
+		final String timestamp = ZonedDateTime.now().format(DateTimeFormatter.ofPattern( "uuuu-MM-dd" ));
+		mFilename = mDumpPath + timestamp + "-smtfeatures.csv";
+		final File f = new File(mFilename);
 		if(!f.exists()){
-		  try {
-			f.createNewFile();
-			try(FileWriter fw = new FileWriter(mFilename, true);
-				    BufferedWriter bw = new BufferedWriter(fw);
-				    PrintWriter out = new PrintWriter(bw))
+			try {
+				f.createNewFile();
+				try(FileWriter fw = new FileWriter(mFilename, true);
+						BufferedWriter bw = new BufferedWriter(fw);
+						PrintWriter out = new PrintWriter(bw))
 				{
-				    String header = SMTFeature.getCsvHeader(";");
-				    out.println(header);
+					final String header = SMTFeature.getCsvHeader(";");
+					out.println(header);
 				} catch (IOException | IllegalAccessException e) {
 					mLogger.error(e);
 				}
-			
-		} catch (IOException e) {
-			mLogger.error(e);
-		}
+
+			} catch (final IOException e) {
+				mLogger.error(e);
+			}
 		}else{
-		  mLogger.info("SMT feature dump-file already exists.");
+			mLogger.info("SMT feature dump-file already exists.");
 		}
 	}
 
-	public void extractFeature(List<Term> assertions, double time, String result) throws IllegalAccessException, IOException {
+	public void extractFeature(final List<Term> assertions, final double time, final String result) throws IllegalAccessException, IOException {
 		mLogger.warn("Extracting feature..");
-		SMTFeatureExtractionTermClassifier tc = new SMTFeatureExtractionTermClassifier(mLogger);
-		for (Term term : assertions) {
+		final SMTFeatureExtractionTermClassifier tc = new SMTFeatureExtractionTermClassifier(mLogger);
+		for (final Term term : assertions) {
 			tc.checkTerm(term);
 		}
-		SMTFeature feature = new SMTFeature();
+		final SMTFeature feature = new SMTFeature();
 		//feature.assertionStack = tc.getTerm();
 		feature.containsArrays = tc.hasArrays();
 		feature.occuringFunctions = tc.getOccuringFunctionNames();
@@ -72,25 +69,26 @@ public class SMTFeatureExtractor {
 		feature.numberOfVariables = tc.getNumberOfVariables();
 		feature.dagsize = tc.getDAGSize();
 		feature.treesize = tc.getTreeSize();
+		feature.dependencyScore = tc.getDependencyScore();
 		feature.solverresult = result;
 		feature.solvertime = time;
 		mFeatures.add(feature);
 		mLogger.warn("FEATURE: " + feature);
 		dumpFeature(feature);
-		
+
 	}
-	
-	public void dumpFeature(SMTFeature feature) throws IllegalAccessException, IOException {
+
+	public void dumpFeature(final SMTFeature feature) throws IllegalAccessException, IOException {
 		mLogger.warn("Writing to file:" + mFilename);
 		try(FileWriter fw = new FileWriter(mFilename, true);
-			    BufferedWriter bw = new BufferedWriter(fw);
-			    PrintWriter out = new PrintWriter(bw))
-			{
-			    mLogger.warn(SMTFeature.getCsvHeader(";"));
-			    out.println(feature.toCsv(";"));
-			} catch (IOException e) {
-				throw new IOException(e);
-			}
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter out = new PrintWriter(bw))
+		{
+			mLogger.warn(SMTFeature.getCsvHeader(";"));
+			out.println(feature.toCsv(";"));
+		} catch (final IOException e) {
+			throw new IOException(e);
+		}
 	}
 
 }
