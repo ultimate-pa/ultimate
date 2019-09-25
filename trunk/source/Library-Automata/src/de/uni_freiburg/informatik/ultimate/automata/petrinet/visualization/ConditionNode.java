417ec1c7aff74132b250c9c270c85c78dd93621f
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2019-09-25 22:56:08 +0200

    remove method that became obsolote

--- trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/petrinet/visualization/ConditionNode.java
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/petrinet/visualization/ConditionNode.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2011-2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2009-2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Automata Library.
- * 
+ *
  * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Automata Library, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
@@ -26,8 +26,8 @@
  */
 package de.uni_freiburg.informatik.ultimate.automata.petrinet.visualization;
 
-import java.util.ArrayList;
 import java.util.Map;
+import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.automata.LibraryIdentifiers;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.unfolding.BranchingProcess;
@@ -37,7 +37,7 @@ import de.uni_freiburg.informatik.ultimate.core.model.models.annotation.IAnnotat
 
 /**
  * Ultimate model of a OccurenceNet condition.
- * 
+ *
  * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * @param <LETTER>
  *            symbol type
@@ -53,33 +53,24 @@ public final class ConditionNode<LETTER, PLACE> extends PetriNetVisualizationNod
 	 * @param branchingProcess
 	 *            branching process
 	 */
-	public ConditionNode(final Condition<LETTER, PLACE> condition, final BranchingProcess<LETTER, PLACE> branchingProcess) {
+	public ConditionNode(final Condition<LETTER, PLACE> condition,
+			final BranchingProcess<LETTER, PLACE> branchingProcess) {
 		super(condition.toString());
 
 		final DefaultAnnotations annot = new DefaultAnnotations();
 		annot.put("Condition", mName);
 		annot.put("CorrespondingPlace", condition.getPlace());
 		annot.put("NumberSuccesorEvents", condition.getSuccessorEvents().size());
-		annot.put("AllConditionsInCoRelation", allConditionsInCoRelation(condition, branchingProcess));
+		final Set<Condition<LETTER, PLACE>> allCoRelatedConditions = branchingProcess.getCoRelation()
+				.computeCoRelatatedConditions(condition);
+		annot.put("AllConditionsInCoRelation", allCoRelatedConditions);
 		final Map<String, IAnnotations> annotations = getPayload().getAnnotations();
 		annotations.put(LibraryIdentifiers.PLUGIN_ID, annot);
 
 		final PLACE content = condition.getPlace();
 		if (content instanceof IAnnotations) {
 			annot.put("Content", content);
-
 		}
 		// super.setPayload(payload);
-	}
-
-	private ArrayList<Condition<LETTER, PLACE>> allConditionsInCoRelation(final Condition<LETTER, PLACE> condition,
-			final BranchingProcess<LETTER, PLACE> bc) {
-		final ArrayList<Condition<LETTER, PLACE>> conditionsInCoRelation = new ArrayList<>();
-		for (final Condition<LETTER, PLACE> c : bc.getConditions()) {
-			if (bc.getCoRelation().isInCoRelation(condition, c)) {
-				conditionsInCoRelation.add(c);
-			}
-		}
-		return conditionsInCoRelation;
 	}
 }
