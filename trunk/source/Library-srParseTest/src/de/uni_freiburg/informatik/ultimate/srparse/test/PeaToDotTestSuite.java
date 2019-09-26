d3e6dc750c8e00a5b2ce1d57dce81324c2550d0b
hauff <hauffn@informatik.uni-freiburg.de>
2019-09-26 22:08:34 +0200

    dot

--- trunk/source/Library-srParseTest/src/de/uni_freiburg/informatik/ultimate/srparse/test/PeaToDotTestSuite.java
+++ trunk/source/Library-srParseTest/src/de/uni_freiburg/informatik/ultimate/srparse/test/PeaToDotTestSuite.java
@@ -6,6 +6,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -23,6 +25,11 @@ import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger.LogLevel;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
 import de.uni_freiburg.informatik.ultimate.lib.pea.PhaseEventAutomata;
 import de.uni_freiburg.informatik.ultimate.lib.pea.modelchecking.DotWriterNew;
+import de.uni_freiburg.informatik.ultimate.lib.srparse.SrParseScopeAfter;
+import de.uni_freiburg.informatik.ultimate.lib.srparse.SrParseScopeAfterUntil;
+import de.uni_freiburg.informatik.ultimate.lib.srparse.SrParseScopeBefore;
+import de.uni_freiburg.informatik.ultimate.lib.srparse.SrParseScopeBetween;
+import de.uni_freiburg.informatik.ultimate.lib.srparse.SrParseScopeGlobally;
 import de.uni_freiburg.informatik.ultimate.lib.srparse.pattern.PatternScopeNotImplemented;
 import de.uni_freiburg.informatik.ultimate.lib.srparse.pattern.PatternType;
 import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
@@ -43,7 +50,7 @@ public class PeaToDotTestSuite {
 	private final String mScope;
 
 	public PeaToDotTestSuite(final PatternType pattern, final Map<String, Integer> duration2Bounds, final String name) {
-		mServiceProvider = UltimateMocks.createUltimateServiceProviderMock(LogLevel.DEBUG);
+		mServiceProvider = UltimateMocks.createUltimateServiceProviderMock(LogLevel.INFO);
 		mLogger = mServiceProvider.getLoggingService().getLogger("");
 
 		mName = name;
@@ -83,16 +90,24 @@ public class PeaToDotTestSuite {
 		final String formula = mPattern.toString().replace(mPattern.getId() + ": ", "");
 
 		final StringBuilder markdown = new StringBuilder();
-		if (mScope.equals("Glob")) {
-			markdown.append("## " + mName + "\n\n");
-		}
-		markdown.append("### " + mScope + "\n\n");
+		markdown.append("### " + mName + " " + mScope + "\n\n");
 		markdown.append(formula + "\n\n");
-		markdown.append("![](/img/patterns/" + mName + "_" + mScope + ".svg)\n");
+		markdown.append("![](/" + IMAGE_DIR + mName + "_" + mScope + ".svg)\n");
 
-		final File file = new File(ROOT_DIR + MARKDOWN_DIR + mName + "_" + mScope + ".md");
-		final BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+		File file = new File(ROOT_DIR + MARKDOWN_DIR + mName + "_" + mScope + ".md");
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
 		writer.write(markdown.toString());
+		writer.close();
+
+		file = new File(ROOT_DIR + MARKDOWN_DIR + "includes.md");
+		writer = new BufferedWriter(new FileWriter(file, true));
+
+		if (mScope.equals("Globally")) {
+			writer.write("## " + mName + "\n");
+		}
+
+		writer.write("{!" + MARKDOWN_DIR + mName + "_" + mScope + ".md!}");
+		writer.newLine();
 		writer.close();
 	}
 
@@ -118,7 +133,33 @@ public class PeaToDotTestSuite {
 	@Parameters(name = "{2}")
 	public static Collection<Object[]> data() {
 		final Pair<List<PatternType>, Map<String, Integer>> pair = PatternUtil.createAllPatterns();
-		return pair.getFirst().stream().map(a -> new Object[] { a, pair.getSecond(), a.getClass().getSimpleName() })
+
+		return pair.getFirst().stream().sorted(new PatternComparator())
+				.map(a -> new Object[] { a, pair.getSecond(), a.getClass().getSimpleName() })
 				.collect(Collectors.toList());
+	}
+
+	private static final class PatternComparator implements Comparator<PatternType> {
+
+		private static final Map<Class<?>, Integer> scopeOrder = new HashMap<Class<?>, Integer>() {
+			{
+				put(SrParseScopeGlobally.class, 1);
+				put(SrParseScopeBefore.class, 2);
+				put(SrParseScopeAfter.class, 3);
+				put(SrParseScopeBetween.class, 4);
+				put(SrParseScopeAfterUntil.class, 5);
+			}
+		};
+
+		@Override
+		public int compare(final PatternType p1, final PatternType p2) {
+			int result = p1.getClass().getSimpleName().compareTo(p2.getClass().getSimpleName());
+
+			if (result == 0) {
+				result = scopeOrder.get(p1.getScope().getClass()) - scopeOrder.get(p2.getScope().getClass());
+			}
+
+			return result;
+		}
 	}
 }
