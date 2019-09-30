bc57729b1145b0f97d7f1d0595882525371adade
bree@titan <loeffler.uni@gmail.com>
2019-09-30 16:38:33 +0200

    moved IHeuristic into its own file

---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/nestedword/operations/IHeuristic.java
@@ -0,0 +1,32 @@
+package de.uni_freiburg.informatik.ultimate.automata.nestedword.operations;
+
+/**
+ *
+ * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ *
+ * @param <STATE>
+ *            Type of states
+ * @param <LETTER>
+ *            Type of transitions
+ */
+public interface IHeuristic<STATE, LETTER> {
+
+	int getHeuristicValue(STATE state, STATE stateK, LETTER trans);
+
+	int getConcreteCost(LETTER trans);
+
+	static <STATE, LETTER> IHeuristic<STATE, LETTER> getZeroHeuristic() {
+		return new IHeuristic<STATE, LETTER>() {
+			@Override
+			public final int getHeuristicValue(final STATE state, final STATE stateK, final LETTER trans) {
+				return 0;
+			}
+
+			@Override
+			public final int getConcreteCost(final LETTER e) {
+				return 1;
+			}
+		};
+	}
+
+}
