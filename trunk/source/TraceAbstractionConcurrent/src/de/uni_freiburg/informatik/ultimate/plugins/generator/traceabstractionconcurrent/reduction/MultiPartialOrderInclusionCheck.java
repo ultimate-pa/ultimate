a0de1176f5d4950ed0cc818f453a709026ab68e3
Dominik Klumpp <maul.esel@posteo.de>
2019-10-01 11:34:52 +0200

    continue work on MultiPartialOrderInclusionCheck:
    - clean up code
    - decouple from state types
    - compute counterexample run

--- trunk/source/TraceAbstractionConcurrent/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstractionconcurrent/reduction/MultiPartialOrderInclusionCheck.java
+++ trunk/source/TraceAbstractionConcurrent/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstractionconcurrent/reduction/MultiPartialOrderInclusionCheck.java
@@ -1,5 +1,6 @@
 package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstractionconcurrent.reduction;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -9,9 +10,11 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import de.uni_freiburg.informatik.ultimate.automata.Word;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.INestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedRun;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWord;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingInternalTransition;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.predicates.IPredicate;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
 
 /**
@@ -26,17 +29,18 @@ import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
  *
  * @author Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
  *
+ * @param <STATE1>
+ * @param <STATE2>
  * @param <LETTER>
  */
-public class MultiPartialOrderInclusionCheck<LETTER> {
-	private final IIndependenceRelation<Set<IPredicate>, LETTER>[] mRelations;
-	private final INestedWordAutomaton<LETTER, IPredicate> mProgram;
-	private final INestedWordAutomaton<LETTER, IPredicate> mProof;
-	private final List<LETTER> mCounterexample;
+public class MultiPartialOrderInclusionCheck<STATE1, STATE2, LETTER> {
+	private final IIndependenceRelation<STATE2, LETTER>[] mRelations;
+	private final INestedWordAutomaton<LETTER, STATE1> mProgram;
+	private final INestedWordAutomaton<LETTER, STATE2> mProof;
+	private final NestedRun<LETTER, STATE1> mCounterexample;
 
-	public MultiPartialOrderInclusionCheck(final IIndependenceRelation<Set<IPredicate>, LETTER>[] relations,
-			final INestedWordAutomaton<LETTER, IPredicate> program,
-			final INestedWordAutomaton<LETTER, IPredicate> proof) {
+	public MultiPartialOrderInclusionCheck(final IIndependenceRelation<STATE2, LETTER>[] relations,
+			final INestedWordAutomaton<LETTER, STATE1> program, final INestedWordAutomaton<LETTER, STATE2> proof) {
 		mRelations = relations;
 		mProgram = program;
 		mProof = proof;
@@ -49,24 +53,48 @@ public class MultiPartialOrderInclusionCheck<LETTER> {
 		mCounterexample = performCheck();
 	}
 
-	private final List<LETTER> performCheck() {
-		final Pair<List<LETTER>, Integer> result = search(mProgram.getInitialStates(), mProof.getInitialStates(),
-				Collections.emptyMap());
-		return result.getFirst();
+	/**
+	 * @return The result of the inclusion check: {@code true} if the program is
+	 *         guaranteed to be covered by the proof, {@code false} otherwise.
+	 */
+	public boolean getResult() {
+		return mCounterexample == null;
 	}
 
-	private final Pair<List<LETTER>, Integer> search(final Set<IPredicate> location, final Set<IPredicate> predicate,
+	/**
+	 * Retrieves the counterexample found during the search, if any. This is an
+	 * automaton run leading from the program's initial location to an error
+	 * location. The corresponding word is guaranteed not to be accepted by the
+	 * proof automaton.
+	 */
+	public NestedRun<LETTER, STATE1> getCounterexample() {
+		return mCounterexample;
+	}
+
+	private final NestedRun<LETTER, STATE1> performCheck() {
+		final Pair<List<LETTER>, Integer> result = search(getInitial(mProgram), getInitial(mProof),
+				Collections.emptyMap());
+		final List<LETTER> symbols = result.getFirst();
+		if (symbols != null) {
+			return createRun(symbols);
+		}
+		return null;
+	}
+
+	private final Pair<List<LETTER>, Integer> search(final STATE1 location, final STATE2 predicate,
 			final Map<LETTER, Integer> sleepSet) {
 		final int maxLevel = mRelations.length - 1;
 
-		if (predicate.stream().anyMatch(mProof::isFinal)) {
+		if (mProof.isFinal(predicate)) {
+			// Assumes the final state of mProof is a sink state.
+			// Hence we can abort the search here.
 			return new Pair<>(null, maxLevel);
-		} else if (location.stream().anyMatch(mProgram::isFinal)) {
+		} else if (mProgram.isFinal(location)) {
+			// A counterexample has been found.
 			return new Pair<>(Arrays.asList(), maxLevel);
 		}
 
-		final Set<LETTER> enabledActions = location.stream().flatMap(l -> mProgram.lettersInternal(l).stream())
-				.collect(Collectors.toSet());
+		final Set<LETTER> enabledActions = mProgram.lettersInternal(location);
 		int level = maxLevel;
 
 		for (LETTER a : enabledActions) {
@@ -74,15 +102,9 @@ public class MultiPartialOrderInclusionCheck<LETTER> {
 				final Integer sleepLevel = sleepSet.get(a);
 				assert sleepLevel != null;
 				level = Math.min(level, sleepLevel);
-
 			} else {
-				// TODO: there must be a much better way than this, this is horrible
-				final Set<IPredicate> nextLocation = location.stream()
-						.flatMap(l -> StreamSupport.stream(mProgram.internalSuccessors(l, a).spliterator(), false))
-						.map(OutgoingInternalTransition::getSucc).collect(Collectors.toSet());
-				final Set<IPredicate> nextPredicate = predicate.stream()
-						.flatMap(phi -> StreamSupport.stream(mProof.internalSuccessors(phi, a).spliterator(), false))
-						.map(OutgoingInternalTransition::getSucc).collect(Collectors.toSet());
+				final STATE1 nextLocation = getSuccessor(mProgram, location, a);
+				final STATE2 nextPredicate = getSuccessor(mProof, predicate, a);
 
 				final Map<LETTER, Integer> nextSleep = recomputeSleep(sleepSet, predicate, a);
 				final Pair<List<LETTER>, Integer> result = search(nextLocation, nextPredicate, nextSleep);
@@ -102,8 +124,8 @@ public class MultiPartialOrderInclusionCheck<LETTER> {
 		return new Pair<>(null, level);
 	}
 
-	private final Map<LETTER, Integer> recomputeSleep(final Map<LETTER, Integer> oldSleepSet,
-			final Set<IPredicate> context, final LETTER action) {
+	private final Map<LETTER, Integer> recomputeSleep(final Map<LETTER, Integer> oldSleepSet, final STATE2 context,
+			final LETTER action) {
 		final Map<LETTER, Integer> newSleepSet = new HashMap<>(oldSleepSet.size());
 
 		for (Map.Entry<LETTER, Integer> entry : oldSleepSet.entrySet()) {
@@ -119,21 +141,38 @@ public class MultiPartialOrderInclusionCheck<LETTER> {
 		return newSleepSet;
 	}
 
-	/**
-	 * @return The result of the inclusion check: {@code true} if the program is
-	 *         guaranteed to be covered by the proof, {@code false} otherwise.
-	 */
-	public boolean isIncluded() {
-		return mCounterexample == null;
+	private NestedRun<LETTER, STATE1> createRun(final List<LETTER> symbols) {
+		// The search returns the word in reverse order.
+		Collections.reverse(symbols);
+
+		final Word<LETTER> word = new Word<>((LETTER[]) symbols.toArray());
+		final NestedWord<LETTER> nestedWord = NestedWord.nestedWord(word);
+
+		final ArrayList<STATE1> stateSequence = new ArrayList<>(word.length() + 1);
+		STATE1 current = getInitial(mProgram);
+		stateSequence.add(current);
+		for (LETTER a : word) {
+			current = getSuccessor(mProgram, current, a);
+			stateSequence.add(current);
+		}
+
+		return new NestedRun<>(nestedWord, stateSequence);
 	}
 
-	/**
-	 * Retrieves the counterexample found during the search, if any. This is a
-	 * sequence of letters leading from the program's initial location to an error
-	 * location. The sequence is guaranteed not to be accepted by the proof
-	 * automaton.
-	 */
-	public List<LETTER> getCounterexample() {
-		return mCounterexample;
+	// TODO: determinize automata, use built-in product automata
+	private <STATE> STATE getInitial(final INestedWordAutomaton<?, STATE> automaton) {
+		final Set<STATE> initial = automaton.getInitialStates();
+		assert initial.size() == 1 : "Automaton must be deterministic";
+		return initial.iterator().next();
+	}
+
+	private <STATE> STATE getSuccessor(final INestedWordAutomaton<LETTER, STATE> automaton, final STATE state,
+			final LETTER letter) {
+		// TODO: there must be a much better way than this, this is horrible
+		final Set<STATE> successors = StreamSupport
+				.stream(automaton.internalSuccessors(state, letter).spliterator(), false)
+				.map(OutgoingInternalTransition::getSucc).collect(Collectors.toSet());
+		assert successors.size() == 1 : "Automaton must be deterministic";
+		return successors.iterator().next();
 	}
 }
