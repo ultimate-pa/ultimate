5373afd3d7038fbf2ee6da58a461a689145932df
Dominik Klumpp <maul.esel@posteo.de>
2019-10-01 11:35:30 +0200

    begin CEGAR loop for POR

---
+++ trunk/source/TraceAbstractionConcurrent/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstractionconcurrent/CegarLoopPartialOrderReduction.java
@@ -0,0 +1,43 @@
+package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstractionconcurrent;
+
+import java.util.Collection;
+
+import de.uni_freiburg.informatik.ultimate.automata.AutomataOperationCanceledException;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.INestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.CfgSmtToolkit;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IIcfg;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IIcfgTransition;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgLocation;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.debugidentifiers.DebugIdentifier;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.predicates.IPredicate;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.BasicCegarLoop;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.predicates.PredicateFactory;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.preferences.TAPreferences;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.preferences.TraceAbstractionPreferenceInitializer.InterpolationTechnique;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstractionconcurrent.reduction.IIndependenceRelation;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstractionconcurrent.reduction.MultiPartialOrderInclusionCheck;
+
+public class CegarLoopPartialOrderReduction<LETTER extends IIcfgTransition<?>> extends BasicCegarLoop<LETTER> {
+
+	private final IIndependenceRelation<IPredicate, LETTER>[] mRelations;
+
+	public CegarLoopPartialOrderReduction(final DebugIdentifier name, final IIcfg<?> rootNode,
+			final CfgSmtToolkit csToolkit, final PredicateFactory predicateFactory, final TAPreferences taPrefs,
+			final Collection<? extends IcfgLocation> errorLocs, final IUltimateServiceProvider services) {
+		super(name, rootNode, csToolkit, predicateFactory, taPrefs, errorLocs,
+				InterpolationTechnique.Craig_TreeInterpolation, false, services);
+
+		mRelations = null; // TODO
+	}
+
+	@Override
+	protected boolean isAbstractionEmpty() throws AutomataOperationCanceledException {
+		final MultiPartialOrderInclusionCheck<IPredicate, IPredicate, LETTER> check = new MultiPartialOrderInclusionCheck<>(
+				mRelations, (INestedWordAutomaton<LETTER, IPredicate>) mAbstraction, mInterpolAutomaton);
+		if (!check.getResult()) {
+			mCounterexample = check.getCounterexample();
+		}
+		return check.getResult();
+	}
+}
