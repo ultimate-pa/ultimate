db4278ae40ba7850061ffc457ac97cae5c5c122b
Dominik Klumpp <maul.esel@posteo.de>
2019-11-20 09:57:54 +0100

    add missing license header, some documentation

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/petrinetlbe/CoenabledRelation.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/petrinetlbe/CoenabledRelation.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (C) 2019 Elisabeth Schanno
+ * Copyright (C) 2019 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2019 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * Copyright (C) 2019 University of Freiburg
+ *
+ * This file is part of the ULTIMATE TraceAbstractionConcurrent plug-in.
+ *
+ * The ULTIMATE TraceAbstractionConcurrent plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE TraceAbstractionConcurrent plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE TraceAbstractionConcurrent plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE TraceAbstractionConcurrent plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE TraceAbstractionConcurrent plug-in grant you additional permission
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.petrinetlbe;
 
 import java.util.Collection;
@@ -13,6 +41,16 @@ import de.uni_freiburg.informatik.ultimate.automata.petrinet.unfolding.FinitePre
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.unfolding.ICoRelation;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
 
+/**
+ * Relates letters labeling transitions in a Petri net. Two letters are
+ * coenabled if there exists a reachable marking where transitions labelled with
+ * these letters can fire independently (i.e., without one disabling the other).
+ *
+ * @author Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ *
+ * @param <LETTER>
+ *            The type of letters labeling Petri net transitions.
+ */
 public class CoenabledRelation<LETTER> {
 
 	private final HashRelation<LETTER, LETTER> mRelation;
@@ -21,6 +59,9 @@ public class CoenabledRelation<LETTER> {
 		mRelation = relation;
 	}
 
+	/**
+	 * Creates a new instance by computing the relation from the given Petri net.
+	 */
 	public static <PLACE, LETTER> CoenabledRelation<LETTER> fromPetriNet(final AutomataLibraryServices services,
 			IPetriNet<LETTER, PLACE> petriNet) throws AutomataOperationCanceledException, PetriNetNot1SafeException {
 		final BranchingProcess<LETTER, PLACE> bp = new FinitePrefix<>(services, petriNet).getResult();
@@ -44,11 +85,16 @@ public class CoenabledRelation<LETTER> {
 	public int size() {
 		return mRelation.size();
 	}
-	
+
 	public Set<LETTER> getImage(LETTER element) {
 		return mRelation.getImage(element);
 	}
 
+	/**
+	 * For each pair in the relation involving a given letter, creates a new
+	 * corresponding pair involving the other letter. The original pairs are not
+	 * removed, they remain in the relation.
+	 */
 	public void copyRelationships(LETTER from, LETTER to) {
 		for (LETTER t3 : mRelation.getImage(from)) {
 			mRelation.addPair(to, t3);
@@ -60,6 +106,9 @@ public class CoenabledRelation<LETTER> {
 		}
 	}
 
+	/**
+	 * Removes all pairs involving the given letter from the relation.
+	 */
 	public void deleteElement(LETTER letter) {
 		mRelation.removeDomainElement(letter);
 		mRelation.removeRangeElement(letter);
