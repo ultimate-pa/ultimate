930cf3fb6b04239f18d97cc99683e5b78140d877
danieldietsch <daniel.dietsch@gmail.com>
2019-12-10 11:36:17 +0100

    remove useless declarations in mini example

--- trunk/examples/CToBoogieTranslation/regression/only-bitprecise/bitvecfloat-fix.c
+++ trunk/examples/CToBoogieTranslation/regression/only-bitprecise/bitvecfloat-fix.c
@@ -1,12 +1,7 @@
 //#Safe
 /*  
-    https://en.cppreference.com/w/c/numeric/math/copysign
+
 */
-
-#include <math.h>
-
-extern void __VERIFIER_error() __attribute__ ((__noreturn__));
-void __VERIFIER_assert(int cond) { if (!(cond)) { ERROR: __VERIFIER_error(); } return; }
 
 int main(void)
 {
