922ada37d169fec4c6ea128a8027ed1c62b608af
Langenfeld <langenfv@tf.uni-freiburg.de>
2020-01-08 11:03:11 +0100

    cc

--- trunk/source/PEAtoBoogie/src/de/uni_frieburg/informatik/ultimate/pea2boogie/testgen/ReqTestAnnotator.java
+++ trunk/source/PEAtoBoogie/src/de/uni_frieburg/informatik/ultimate/pea2boogie/testgen/ReqTestAnnotator.java
@@ -28,89 +28,78 @@ import de.uni_freiburg.informatik.ultimate.pea2boogie.req2pea.IReq2PeaAnnotator;
 import de.uni_freiburg.informatik.ultimate.util.simplifier.NormalFormTransformer;
 
 public class ReqTestAnnotator implements IReq2PeaAnnotator {
-	
+
 	private final ILogger mLogger;
 	private final IUltimateServiceProvider mServices;
-	private final PeaResultUtil mPeaResultUtil;
 	private final BoogieLocation mLocation;
 	private final IReqSymbolTable mSymbolTable;
 	private final Map<PatternType, PhaseEventAutomata> mReq2Automata;
 	private final Req2CauseTrackingPea mReq2Pea;
-	private NormalFormTransformer<Expression> mNormalFormTransformer;
-	
-	public ReqTestAnnotator(final IUltimateServiceProvider services, final ILogger logger, Req2CauseTrackingPea req2Pea) {
+	private final NormalFormTransformer<Expression> mNormalFormTransformer;
+
+	public ReqTestAnnotator(final IUltimateServiceProvider services, final ILogger logger, final Req2CauseTrackingPea req2Pea) {
 		mLogger = logger;
-		mServices = services;	
+		mServices = services;
 		mSymbolTable = req2Pea.getSymboltable();
 		mReq2Automata = req2Pea.getPattern2Peas();
 		mReq2Pea = req2Pea;
-		mPeaResultUtil = new PeaResultUtil(mLogger, mServices);
 		// TODO: Add locations to pattern type to generate meaningful boogie locations
 		mLocation = new BoogieLocation("", -1, -1, -1, -1);
 		mNormalFormTransformer = new NormalFormTransformer<>(new BoogieExpressionTransformer());
 	}
-	
-	
-	
+
 	@Override
 	public List<Statement> getStateChecks() {
-		List<Statement> statements = new ArrayList<Statement>();
-		Map<PhaseEventAutomata, Set<Integer>> pea2EffectPhase = mReq2Pea.getEffectPhase();
+		final List<Statement> statements = new ArrayList<Statement>();
 		//check that each u_v -> pc_xx == effect(A_r) for every v \in E
-		for(String var: mSymbolTable.getStateVars()) {
-			if(mSymbolTable.getInputVars().contains(var)) {
-				continue;
-			}
-			if(!mSymbolTable.getStateVars().contains("u_"+var)) {
-				//tracking vairable is not used anywhere so do not generate it
-				continue;
-			}
-			for(Map.Entry<PhaseEventAutomata, Set<String>> entry: mReq2Pea.getPea2EffectVars().entrySet()) {
-				List<Expression> disjuncts = new ArrayList<>();
-				if(entry.getValue().contains(var)) {
-					for(Integer phase: pea2EffectPhase.get(entry.getKey())) {
-					Expression expr = new BinaryExpression(
-							mLocation,
-							BinaryExpression.Operator.COMPEQ,
-							mSymbolTable.getIdentifierExpression(mSymbolTable.getPcName(entry.getKey())),
-							new IntegerLiteral(mLocation, phase.toString()));
-					disjuncts.add(expr);
-					//TODO: remove but then the genDisjunction explodes O.o
-					//break;
-					}
-				}
-				if(disjuncts.size() > 0) {
-					Expression expr = new BinaryExpression(
-							mLocation,
-							BinaryExpression.Operator.LOGICIMPLIES,
-							mSymbolTable.getIdentifierExpression("u_"+var),
-							genDisjunction(disjuncts, mLocation));
-					statements.add(new AssumeStatement(mLocation, expr));
-				}
-			}	
-		}	
+		statements.addAll(genEffectInvariants());
 		//generate asserts assert(!(pc_xx == i)) for every i \in effect(A_r)
-		Map<PhaseEventAutomata, Set<Integer>> pea2OutputEffectPhase = mReq2Pea.getOutputEffectPhase();
-		for(Map.Entry<PhaseEventAutomata, Set<Integer>> entry: pea2OutputEffectPhase.entrySet()) {
-			PhaseEventAutomata pea = entry.getKey();
-			for (Integer phaseNr: entry.getValue()) {
-				Expression expr = new BinaryExpression(
+		statements.addAll(genTestAssertions());
+		return statements;
+	}
+
+	private List<Statement> genEffectInvariants(){
+		final List<Statement> statements = new ArrayList<Statement>();
+		for(final String effect_var: mSymbolTable.getStateVars()) {
+			final List<Expression> disjuncts = new ArrayList<>();
+			if(mSymbolTable.getInputVars().contains(effect_var)) {
+				continue;
+			}
+			if(!mSymbolTable.getStateVars().contains("u_"+effect_var)) {
+				//the tracking variable was never used and thus there is no need generating the invariant
+				continue;
+			}
+			for(final Map.Entry<PhaseEventAutomata, Set<String>> entry: mReq2Pea.getPea2EffectVars().entrySet()) {
+				disjuncts.addAll(genEffectPhaseDisjuncts(entry.getKey(), entry.getValue(), effect_var));
+			}
+			if(disjuncts.size() > 0) {
+				final Expression expr = new BinaryExpression(
 						mLocation,
-						BinaryExpression.Operator.COMPEQ,
-						mSymbolTable.getIdentifierExpression(mSymbolTable.getPcName(pea)),
-						new IntegerLiteral(mLocation, phaseNr.toString()));
-				final NamedAttribute[] attr =
-						new NamedAttribute[] { new NamedAttribute(mLocation, "TEST_" + pea.getName(), new Expression[] {}) };
-				final AssertStatement assrt = 
-						new AssertStatement(mLocation, attr, new UnaryExpression(mLocation, Operator.LOGICNEG, expr));
-				statements.add(assrt);
+						BinaryExpression.Operator.LOGICIMPLIES,
+						mSymbolTable.getIdentifierExpression("u_"+effect_var),
+						genDisjunction(disjuncts, mLocation));
+				statements.add(new AssumeStatement(mLocation, expr));
 			}
 		}
 		return statements;
 	}
-	
-	
-	
+
+	private List<Expression> genEffectPhaseDisjuncts(final PhaseEventAutomata pea, final Set<String> vars, final String var) {
+		final List<Expression> disjuncts = new ArrayList<>();
+		final Map<PhaseEventAutomata, Set<Integer>> pea2EffectPhase = mReq2Pea.getEffectPhase();
+		if(vars.contains(var)) {
+			for(final Integer phase: pea2EffectPhase.get(pea)) {
+				final Expression expr = new BinaryExpression(
+						mLocation,
+						BinaryExpression.Operator.COMPEQ,
+						mSymbolTable.getIdentifierExpression(mSymbolTable.getPcName(pea)),
+						new IntegerLiteral(mLocation, phase.toString()));
+				disjuncts.add(expr);
+			}
+		}
+		return disjuncts;
+	}
+
 	private Expression genDisjunction(final List<Expression> exprs, final BoogieLocation bl) {
 		final Iterator<Expression> it = exprs.iterator();
 		if (!it.hasNext()) {
@@ -122,6 +111,32 @@ public class ReqTestAnnotator implements IReq2PeaAnnotator {
 			//cnf = ExpressionFactory.newBinaryExpression(bl, BinaryExpression.Operator.LOGICOR, cnf, it.next());
 		}
 		return mNormalFormTransformer.toNnf(cnf);
+	}
+
+	private  List<Statement> genTestAssertions(){
+		final List<Statement> statements = new ArrayList<Statement>();
+		final Map<PhaseEventAutomata, Set<Integer>> pea2OutputEffectPhase = mReq2Pea.getOutputEffectPhase();
+		for(final Map.Entry<PhaseEventAutomata, Set<Integer>> entry: pea2OutputEffectPhase.entrySet()) {
+			statements.addAll(genTestAssertion(entry.getKey(), entry.getValue()));
+		}
+		return statements;
+	}
+
+	private List<Statement> genTestAssertion(final PhaseEventAutomata pea, final Set<Integer> effectPhases){
+		final List<Statement> statements = new ArrayList<Statement>();
+		for (final Integer phaseNr: effectPhases) {
+			final Expression expr = new BinaryExpression(
+					mLocation,
+					BinaryExpression.Operator.COMPEQ,
+					mSymbolTable.getIdentifierExpression(mSymbolTable.getPcName(pea)),
+					new IntegerLiteral(mLocation, phaseNr.toString()));
+			final NamedAttribute[] attr =
+					new NamedAttribute[] { new NamedAttribute(mLocation, "TEST_" + pea.getName(), new Expression[] {}) };
+			final AssertStatement assrt =
+					new AssertStatement(mLocation, attr, new UnaryExpression(mLocation, Operator.LOGICNEG, expr));
+			statements.add(assrt);
+		}
+		return statements;
 	}
 
 	@Override
