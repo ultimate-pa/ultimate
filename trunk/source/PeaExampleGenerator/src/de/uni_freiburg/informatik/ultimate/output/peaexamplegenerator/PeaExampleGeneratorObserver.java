49628439f34365a9db5597b907c6bd7d95d9cdbf
danieldietsch <daniel.dietsch@gmail.com>
2020-01-21 16:33:12 +0100

    add new plugin de.uni_freiburg.informatik.ultimate.output.peaexamplegenerator

---
+++ trunk/source/PeaExampleGenerator/src/de/uni_freiburg/informatik/ultimate/output/peaexamplegenerator/PeaExampleGeneratorObserver.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2020 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ * Copyright (C) 2020 University of Freiburg
+ *
+ * This file is part of the ULTIMATE PeaExampleGenerator plug-in.
+ *
+ * The ULTIMATE PeaExampleGenerator plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE PeaExampleGenerator plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE PeaExampleGenerator plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE PeaExampleGenerator plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE PeaExampleGenerator plug-in grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.output.peaexamplegenerator;
+
+import java.util.List;
+import java.util.Map;
+
+import de.uni_freiburg.informatik.ultimate.core.lib.observers.BaseObserver;
+import de.uni_freiburg.informatik.ultimate.core.model.models.IElement;
+import de.uni_freiburg.informatik.ultimate.core.model.results.IResult;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+
+/**
+ * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ */
+public class PeaExampleGeneratorObserver extends BaseObserver {
+
+	private final ILogger mLogger;
+	private final IUltimateServiceProvider mServices;
+
+	public PeaExampleGeneratorObserver(final IUltimateServiceProvider services) {
+		mServices = services;
+		mLogger = services.getLoggingService().getLogger(Activator.PLUGIN_ID);
+	}
+
+	@Override
+	public boolean process(final IElement root) {
+		// we only operate on IResults, so we do not need a model
+		return false;
+	}
+
+	@Override
+	public void finish() {
+		// call python script (or perhaps translate it to java)
+		final Map<String, List<IResult>> results = mServices.getResultService().getResults();
+
+	}
+}
