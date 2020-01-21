4944d7e2d1ba12bef077a25017a158b133105e4a
danieldietsch <daniel.dietsch@gmail.com>
2020-01-21 17:14:47 +0100

    cc fixes

--- trunk/source/PEAtoBoogie/src/de/uni_frieburg/informatik/ultimate/pea2boogie/testgen/ReqTestResultTest.java
+++ trunk/source/PEAtoBoogie/src/de/uni_frieburg/informatik/ultimate/pea2boogie/testgen/ReqTestResultTest.java
@@ -3,21 +3,21 @@ package de.uni_frieburg.informatik.ultimate.pea2boogie.testgen;
 import java.util.List;
 
 import de.uni_freiburg.informatik.ultimate.core.model.results.IResult;
+import de.uni_freiburg.informatik.ultimate.pea2boogie.Activator;
 
-public final class ReqTestResultTest implements IResult{
+public final class ReqTestResultTest implements IResult {
 
 	final List<TestStep> mTestSteps;
 	final String mName;
 
-	public ReqTestResultTest(List<TestStep> testSteps, final String name) {
+	public ReqTestResultTest(final List<TestStep> testSteps, final String name) {
 		mTestSteps = testSteps;
 		mName = name;
 	}
 
 	@Override
 	public String getPlugin() {
-		// TODO Auto-generated method stub
-		return null;
+		return Activator.PLUGIN_ID;
 	}
 
 	@Override
@@ -28,7 +28,7 @@ public final class ReqTestResultTest implements IResult{
 	@Override
 	public String getLongDescription() {
 		final StringBuilder resultString = new StringBuilder();
-		for(final TestStep step: mTestSteps) {
+		for (final TestStep step : mTestSteps) {
 			resultString.append(step.toString());
 		}
 		return resultString.toString();
