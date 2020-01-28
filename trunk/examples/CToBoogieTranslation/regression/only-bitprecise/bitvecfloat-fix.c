2e25e9a15cf6f61b9f9eb4e5c9a4400948ca3919
Jill Enke <jill.enke@gmail.com>
2020-01-28 16:00:59 +0100

    add example of basic FLOAT program

--- trunk/examples/CToBoogieTranslation/regression/only-bitprecise/bitvecfloat-fix.c
+++ trunk/examples/CToBoogieTranslation/regression/only-bitprecise/bitvecfloat-fix.c
@@ -1,7 +1,12 @@
 //#Safe
 /*  
-
+    https://en.cppreference.com/w/c/numeric/math/copysign
 */
+
+#include <math.h>
+
+extern void __VERIFIER_error() __attribute__ ((__noreturn__));
+void __VERIFIER_assert(int cond) { if (!(cond)) { ERROR: __VERIFIER_error(); } return; }
 
 int main(void)
 {
