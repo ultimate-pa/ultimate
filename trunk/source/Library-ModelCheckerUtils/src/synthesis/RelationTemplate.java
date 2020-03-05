453396c025d948ee8a5bc24c9397a92bebd544a3
fertmand <daniel.fertmann@web.de>
2020-03-05 17:45:11 +0100

    add template classes for invariant syntesis

---
+++ trunk/source/Library-ModelCheckerUtils/src/synthesis/RelationTemplate.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2020 Daniel Fertmann (fertmand@cs.uni-freiburg.de)
+ * Copyright (C) 2020 Jan Oreans (oreansj@cs.uni-freiburg.de)
+ * Copyright (C) 2020 University of Freiburg
+ */
+
+package synthesis;
+
+public class RelationTemplate extends Template {
+
+}
