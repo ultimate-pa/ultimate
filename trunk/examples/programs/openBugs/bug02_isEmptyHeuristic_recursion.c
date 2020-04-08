f03605c774f5fe35c9fae110022084919a603c85
Breee <loeffler.uni@gmail.com>
2020-04-08 11:50:07 +0200

    add testsuite for IsEmptyHeuristic, add two new regression tests from delta debugger, add one manual example

---
+++ trunk/examples/programs/openBugs/bug02_isEmptyHeuristic_recursion.c
@@ -0,0 +1,21 @@
+//#Safe
+
+/* i for outer loop, j for inner loop, both going 0 to n-1 */
+void recursion(int n, int i, int j)
+{
+  if (i < n) {
+    if (j < n) {
+      // inner loop when j < n
+      recursion(n, i, j+1); // increment inner counter only!
+    } else { // when j has reached n...
+      // outer loop, which restarts inner loop
+      recursion(n, i+1, 0); // increment outer counter, reset inner
+                            // since we're starting a new inner loop
+    }
+  }
+}
+
+int main(){
+    int n,i,j;
+    recursion(n,i,j);
+}
