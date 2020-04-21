2cd117c6b01e2672368728296071cb0b623b804a
Daniel Dietsch <daniel.dietsch@gmail.com>
2020-04-21 11:59:53 +0200

    split copysign tests into three

--- trunk/examples/programs/FloatingPoint/regression/c/ctrans-float-copysign-NAN-2.c
+++ trunk/examples/programs/FloatingPoint/regression/c/ctrans-float-copysign-NAN-2.c
@@ -1,4 +1,4 @@
-//#Safe
+//#Unsafe
 /*  
     https://en.cppreference.com/w/c/numeric/math/copysign
 */
@@ -10,24 +10,6 @@ void __VERIFIER_assert(int cond) { if (!(cond)) { ERROR: __VERIFIER_error(); } r
 
 int main(void)
 {
-
-    
-    double x = NAN;
-    double y = -NAN;
-    double csy = copysign(NAN,-1.0);
-    
-    //long long bv_y;
-    //long long bv_csy;
-    
-    //__VERIFIER_assert(sizeof(long long) == sizeof(double));
-    
-    __VERIFIER_assert(isnan(x));
-    __VERIFIER_assert(isnan(y));
-    __VERIFIER_assert(isnan(csy));
-    __VERIFIER_assert(x != x);
-
-    //memcpy(&bv_y, &y, sizeof y);
-    //memcpy(&bv_csy, &csy, sizeof csy);
-    //__VERIFIER_assert(bv_y == bv_csy);
+    __VERIFIER_assert(-NAN == copysign(NAN,-1.0));
     return 0;
 }
