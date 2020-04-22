32791b97e148220f04a95b206052eff3c0180274
Daniel Dietsch <daniel.dietsch@gmail.com>
2020-04-22 14:57:15 +0200

    better NAN-2 regression test

--- trunk/examples/programs/FloatingPoint/regression/c/ctrans-float-copysign-NAN-2.c
+++ trunk/examples/programs/FloatingPoint/regression/c/ctrans-float-copysign-NAN-2.c
@@ -1,4 +1,4 @@
-//#Unsafe
+//#Safe
 /*  
     https://en.cppreference.com/w/c/numeric/math/copysign
 */
@@ -10,6 +10,24 @@ void __VERIFIER_assert(int cond) { if (!(cond)) { ERROR: __VERIFIER_error(); } r
 
 int main(void)
 {
-    __VERIFIER_assert(-NAN == copysign(NAN,-1.0));
+
+    
+    double x = NAN;
+    double y = -NAN;
+    double csy = copysign(NAN,-1.0);
+    
+    //long long bv_y;
+    //long long bv_csy;
+    
+    //__VERIFIER_assert(sizeof(long long) == sizeof(double));
+    
+    __VERIFIER_assert(isnan(x));
+    __VERIFIER_assert(isnan(y));
+    __VERIFIER_assert(isnan(csy));
+    __VERIFIER_assert(x != x);
+
+    //memcpy(&bv_y, &y, sizeof y);
+    //memcpy(&bv_csy, &csy, sizeof csy);
+    //__VERIFIER_assert(bv_y == bv_csy);
     return 0;
 }
