97094d9af2c376ce68089aeea22e4be14b9e7672
fertmand <daniel.fertmann@web.de>
2020-06-10 14:50:54 +0200

    minor changes

--- trunk/source/Library-ModelCheckerUtils/src/synthesis/Starter.java
+++ trunk/source/Library-ModelCheckerUtils/src/synthesis/Starter.java
@@ -15,18 +15,18 @@ public class Starter {
 
 	public Starter(IIcfg<IcfgLocation> icfg) {
 		System.out.println("Start Starter");
-		IIcfg<IcfgLocation> mIcfg = icfg;
+		// IIcfg<IcfgLocation> mIcfg = icfg;
 		// int numOfLocations = 0;
 		// for (HashMap<String,IcfgLocation> i : icfg.mLocNodes.values()) {
 		// 	numOfLocations = numOfLocations + i.size();
 		// }
 		
-		Map<String, Map<DebugIdentifier, IcfgLocation>> locations = mIcfg.getProgramPoints();
+		Map<String, Map<DebugIdentifier, IcfgLocation>> locations = icfg.getProgramPoints();
 		int numberOfLocations = locations.size();
 		
 		
 		List<IcfgEdge> edges = new ArrayList<>();
-		for (final Entry<String, Map<DebugIdentifier, IcfgLocation>> entry : mIcfg.getProgramPoints().entrySet()) {
+		for (final Entry<String, Map<DebugIdentifier, IcfgLocation>> entry : icfg.getProgramPoints().entrySet()) {
 			for (final Entry<DebugIdentifier, IcfgLocation> innerEntry : entry.getValue().entrySet()) {
 				final IcfgLocation loc = innerEntry.getValue();
 				for (final IcfgEdge edge : loc.getOutgoingEdges()) {
