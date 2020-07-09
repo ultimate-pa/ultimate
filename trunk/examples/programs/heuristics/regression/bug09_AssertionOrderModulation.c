1c73543f452b7536646a80b84773da536c6204f9
Daniel Dietsch <daniel.dietsch@gmail.com>
2020-07-09 14:29:32 +0200

    minimal example for AOM bug

---
+++ trunk/examples/programs/heuristics/regression/bug09_AssertionOrderModulation.c
@@ -0,0 +1,28 @@
+int gl_list = {& gl_list   };
+
+void list_add(int  new , int *head )
+{ int  __cil_tmp3 ;
+
+  __cil_tmp3 = *    head ;
+
+}
+
+void gl_insert(int value )
+{
+
+  list_add(0, & gl_list);
+
+}
+void gl_read( )
+{
+
+    gl_insert(0);
+
+}
+
+void main( )
+{
+
+  gl_read();
+
+}
