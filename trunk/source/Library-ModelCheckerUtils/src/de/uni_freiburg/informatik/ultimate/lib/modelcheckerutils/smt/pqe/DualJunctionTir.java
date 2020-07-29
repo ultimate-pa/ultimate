7d39141b832ef944c5cace42cc52951d0a8c9719
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2020-07-30 00:22:31 +0200

    add TIR for new API

---
+++ trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/smt/pqe/DualJunctionTir.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2020 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * Copyright (C) 2020 University of Freiburg
+ *
+ * This file is part of the ULTIMATE ModelCheckerUtils Library.
+ *
+ * The ULTIMATE ModelCheckerUtils Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE ModelCheckerUtils Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE ModelCheckerUtils Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE ModelCheckerUtils Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE ModelCheckerUtils Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.pqe;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.EliminationTask;
+import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.ManagedScript;
+import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.QuantifierUtils;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
+
+/**
+ * Transitive inequality resolution (TIR).
+ *
+ * @author Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * @author Max Barth
+ */
+public class DualJunctionTir extends DualJunctionQuantifierElimination {
+
+	/**
+	 * @see constructor
+	 */
+	private final boolean mExpensiveEliminations;
+
+	/**
+	 * @param expensiveEliminations
+	 *            If set to true we do expensive eliminations where auxiliary
+	 *            variables and case distinctions are allowed. If set to false we do
+	 *            only inexpensive eliminations where non of the above is allowed.
+	 *            Note that in the first case we will not do all simple
+	 *            eliminations. If you want the full elimination power you should
+	 *            two instances of this class.
+	 */
+	public DualJunctionTir(final ManagedScript script, final IUltimateServiceProvider services,
+			final boolean expensiveEliminations) {
+		super(script, services);
+		mExpensiveEliminations = expensiveEliminations;
+	}
+
+	@Override
+	public String getName() {
+		return "transitive inequality resolution";
+	}
+
+	@Override
+	public String getAcronym() {
+		return "TIR";
+	}
+
+	@Override
+	public EliminationResult tryToEliminate(final EliminationTask inputEt) {
+		final EliminationResult er = tryExhaustivelyToEliminate(inputEt);
+		return er;
+	}
+
+
+	/**
+	 * Try to iteratively eliminate as many eliminatees as possible using the given
+	 * "derHelper". Return null if did not make progress for any eliminatee.
+	 */
+	public EliminationResult tryExhaustivelyToEliminate(final EliminationTask inputEt) {
+		EliminationTask currentEt = inputEt;
+		final LinkedHashSet<TermVariable> aquiredEliminatees = new LinkedHashSet<>();
+		while (true) {
+			final EliminationResult er = tryToEliminateOne(currentEt);
+			if (er == null) {
+				// no success, no further iterations
+				break;
+			}
+			aquiredEliminatees.addAll(er.getNewEliminatees());
+			currentEt = er.getEliminationTask();
+			if (QuantifierUtils.isCorrespondingFiniteJunction(currentEt.getQuantifier(),
+					er.getEliminationTask().getTerm())) {
+				// we can push the quantifier, no further iterations
+				break;
+			}
+		}
+		if (currentEt == inputEt) {
+			// only one non-successful iteration
+			return null;
+		} else {
+			return new EliminationResult(currentEt, aquiredEliminatees);
+		}
+	}
+
+	/**
+	 * Try to eliminate some eliminatee using the given "derHelper". Return
+	 * immediately after the first successful step (note that a step can be
+	 * successful if a case distinction was made and the variable was only
+	 * eliminated in for some cases). Return null if did not make progress
+	 * for any eliminatee.
+	 */
+	private EliminationResult tryToEliminateOne(final EliminationTask inputEt) {
+		for (final TermVariable eliminatee : inputEt.getEliminatees()) {
+			final Term resultTerm = XnfTir.tryToEliminateConjuncts(mServices, mScript, inputEt.getQuantifier(), inputEt.getTerm(), eliminatee, inputEt.getEliminatees());
+			if (resultTerm != null) {
+//				final ExtendedSimplificationResult esr = SmtUtils.simplifyWithStatistics(mMgdScript, resultTerm, null, mServices, SimplificationTechnique.SIMPLIFY_DDA);
+//				final String sizeMessage = String.format("treesize reduction %d, result has %2.1f percent of original size",
+//						esr.getReductionOfTreeSize(), esr.getReductionRatioInPercent());
+//				mLogger.info(sizeMessage);
+				return new EliminationResult(
+						new EliminationTask(inputEt.getQuantifier(), inputEt.getEliminatees(), resultTerm),
+						Collections.emptySet());
+			}
+		}
+		return null;
+	}
+
+}
