3007fc15efbafc749ddf645bc83c6d1262256742
Jochen Hoenicke <hoenicke@gmail.com>
2020-07-31 19:14:24 +0200

    Updated HashUtils from SMTInterpol repo
    - For rotate use >>> instead of >>.  The latter is arithmetic shift and
      will destroy entropy for negative numbers.
    - Add a function to hash byte arrays.

--- trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/HashUtils.java
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/HashUtils.java
@@ -1,71 +1,193 @@
 /*
- * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
- * Copyright (C) 2014-2015 Jochen Hoenicke (hoenicke@informatik.uni-freiburg.de)
- * Copyright (C) 2012-2015 University of Freiburg
- * 
- * This file is part of the ULTIMATE Util Library.
- * 
- * The ULTIMATE Util Library is free software: you can redistribute it and/or modify
+ * Copyright (C) 2012-2013 University of Freiburg
+ *
+ * This file is part of SMTInterpol.
+ *
+ * SMTInterpol is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * The ULTIMATE Util Library is distributed in the hope that it will be useful,
+ *
+ * SMTInterpol is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
- * along with the ULTIMATE Util Library. If not, see <http://www.gnu.org/licenses/>.
- * 
- * Additional permission under GNU GPL version 3 section 7:
- * If you modify the ULTIMATE Util Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE Util Library grant you additional permission 
- * to convey the resulting work.
+ * along with SMTInterpol.  If not, see <http://www.gnu.org/licenses/>.
  */
 package de.uni_freiburg.informatik.ultimate.util;
 
+/**
+ * This implements Jenkins und Hsieh hash functions. It is based on the code at
+ * <a href="http://www.burtleburtle.net/bob/c/lookup3.c">http://www.burtleburtle.net/bob/c/lookup3.c</a>.
+ *
+ *
+ * @author Jürgen Christ, Jochen Hoenicke
+ *
+ */
 public final class HashUtils {
-	
-	// deadbeaf leads to hash conflicts on f...
-	private static final int BASE = 0xcafebabe;
-	
+	private final static int BASE = 0xdeadbeef;
+
 	private HashUtils() {
 		// Hide constructor
 	}
-	
-	public static int hashJenkins(int init, Object... vals) {
+
+	public static int hashJenkins(final int init, final byte[] vals) {
 		if (vals == null || vals.length == 0) {
 			return init;
 		}
 		int a,b,c;
 		a = b = c = BASE + (vals.length << 2) + init;
 		int pos = 0;
-		while (vals.length - pos > 3) { //NOCHECKSTYLE
+		while (vals.length - pos > 12) {
+			a += (vals[pos] & 0xff) + ((vals[pos + 1] & 0xff) << 8) + ((vals[pos + 2] & 0xff) << 16)
+					+ ((vals[pos + 3] & 0xff) << 24);
+			b += (vals[pos + 4] & 0xff) + ((vals[pos + 5] & 0xff) << 8) + ((vals[pos + 6] & 0xff) << 16)
+					+ ((vals[pos + 7] & 0xff) << 24);
+			c += (vals[pos + 8] & 0xff) + ((vals[pos + 9] & 0xff) << 8) + ((vals[pos + 10] & 0xff) << 16)
+					+ ((vals[pos + 11] & 0xff) << 24);
+			// This is the mix function of Jenkins hash.
+			// Note that we need >>> for unsigned shift.
+			// rot(c,4) is ((c << 4) | (c >>> 28)).
+			a -= c;
+			a ^= ((c << 4) | (c >>> 28));
+			c += b;
+			b -= a;
+			b ^= ((a << 6) | (a >>> 26));
+			a += c;
+			c -= b;
+			c ^= ((b << 8) | (b >>> 24));
+			b += a;
+			a -= c;
+			a ^= ((c << 16) | (c >>> 16));
+			c += b;
+			b -= a;
+			b ^= ((a << 19) | (a >>> 13));
+			a += c;
+			c -= b;
+			c ^= ((b << 4) | (b >>> 28));
+			b += a;
+			pos += 3;
+		}
+		switch (vals.length - pos) {
+		case 12:
+			c += (vals[pos + 11] & 0xff) << 24;
+			//$FALL-THROUGH$
+		case 11:
+			c += (vals[pos + 10] & 0xff) << 16;
+			//$FALL-THROUGH$
+		case 10:
+			c += (vals[pos + 9] & 0xff) << 8;
+			//$FALL-THROUGH$
+		case 9:
+			c += (vals[pos + 8] & 0xff);
+			//$FALL-THROUGH$
+		case 8:
+			b += (vals[pos + 7] & 0xff) << 24;
+			//$FALL-THROUGH$
+		case 7:
+			b += (vals[pos + 6] & 0xff) << 16;
+			//$FALL-THROUGH$
+		case 6:
+			b += (vals[pos + 5] & 0xff) << 8;
+			//$FALL-THROUGH$
+		case 5:
+			b += (vals[pos + 4] & 0xff);
+			//$FALL-THROUGH$
+		case 4:
+			a += (vals[pos + 3] & 0xff) << 24;
+			//$FALL-THROUGH$
+		case 3:
+			a += (vals[pos + 2] & 0xff) << 16;
+			//$FALL-THROUGH$
+		case 2:
+			a += (vals[pos + 1] & 0xff) << 8;
+			//$FALL-THROUGH$
+		case 1:
+			a += (vals[pos] & 0xff);
+			// This is the final mix function of Jenkins hash.
+			c ^= b;
+			c -= ((b << 14) | (b >>> 18));
+			a ^= c;
+			a -= ((c << 11) | (c >>> 21));
+			b ^= a;
+			b -= ((a << 25) | (a >>> 7));
+			c ^= b;
+			c -= ((b << 16) | (b >>> 16));
+			a ^= c;
+			a -= ((c << 4) | (c >>> 28));
+			b ^= a;
+			b -= ((a << 14) | (a >>> 18));
+			c ^= b;
+			c -= ((b << 24) | (b >>> 8));
+			//$FALL-THROUGH$
+		case 0:
+			//$FALL-THROUGH$
+		default:
+			break;
+		}
+		return c;
+	}
+
+	public static int hashJenkins(final int init, final Object... vals) {
+		if (vals == null || vals.length == 0) {
+			return init;
+		}
+		int a,b,c;
+		a = b = c = BASE + (vals.length << 2) + init;
+		int pos = 0;
+		while (vals.length - pos > 3) {
 			a += vals[pos].hashCode();
 			b += vals[pos + 1].hashCode();
 			c += vals[pos + 2].hashCode();
-			a -= c; a ^= ((c << 4) | (c >> 28)); c += b; //NOCHECKSTYLE
-			b -= a; b ^= ((a << 6) | (c >> 26)); a += c; //NOCHECKSTYLE
-			c -= b; c ^= ((b << 8) | (b >> 24)); b += a; //NOCHECKSTYLE
-			a -= c; a ^= ((c << 16) | (c >> 16)); c += b; //NOCHECKSTYLE
-			b -= a; b ^= ((a << 19) | (c >> 13)); a += c; //NOCHECKSTYLE
-			c -= b; c ^= ((b << 4) | (b >> 28)); b += a; //NOCHECKSTYLE
-			pos += 3; //NOCHECKSTYLE
+			// This is the mix function of Jenkins hash.
+			// Note that we need >>> for unsigned shift.
+			// rot(c,4) is ((c << 4) | (c >>> 28)).
+			a -= c;
+			a ^= ((c << 4) | (c >>> 28));
+			c += b;
+			b -= a;
+			b ^= ((a << 6) | (a >>> 26));
+			a += c;
+			c -= b;
+			c ^= ((b << 8) | (b >>> 24));
+			b += a;
+			a -= c;
+			a ^= ((c << 16) | (c >>> 16));
+			c += b;
+			b -= a;
+			b ^= ((a << 19) | (a >>> 13));
+			a += c;
+			c -= b;
+			c ^= ((b << 4) | (b >>> 28));
+			b += a;
+			pos += 3;
 		}
 		switch (vals.length - pos) {
-		case 3: c += vals[pos++].hashCode(); //NOCHECKSTYLE
-		case 2: b += vals[pos++].hashCode(); //NOCHECKSTYLE
-		case 1: a += vals[pos].hashCode(); //NOCHECKSTYLE
-			c ^= b; c -= ((b << 14) | (b >> 18)); //NOCHECKSTYLE
-			a ^= c; a -= ((c << 11) | (c >> 21)); //NOCHECKSTYLE
-			b ^= a; b -= ((a << 25) | (a >> 7)); //NOCHECKSTYLE
-			c ^= b; c -= ((b << 16) | (b >> 16)); //NOCHECKSTYLE
-			a ^= c; a -= ((c << 4) | (c >> 28)); //NOCHECKSTYLE
-			b ^= a; b -= ((a << 14) | (a >> 18)); //NOCHECKSTYLE
-			c ^= b; c -= ((b << 24) | (b >> 8)); //NOCHECKSTYLE
+		case 3:
+			c += vals[pos++].hashCode();
+			// fallthrough
+		case 2:
+			b += vals[pos++].hashCode();
+			// fallthrough
+		case 1:
+			a += vals[pos].hashCode();
+			// This is the final mix function of Jenkins hash.
+			c ^= b;
+			c -= ((b << 14) | (b >>> 18));
+			a ^= c;
+			a -= ((c << 11) | (c >>> 21));
+			b ^= a;
+			b -= ((a << 25) | (a >>> 7));
+			c ^= b;
+			c -= ((b << 16) | (b >>> 16));
+			a ^= c;
+			a -= ((c << 4) | (c >>> 28));
+			b ^= a;
+			b -= ((a << 14) | (a >>> 18));
+			c ^= b;
+			c -= ((b << 24) | (b >>> 8));
 			// fallthrough
 		case 0:
 			// fallthrough
@@ -74,22 +196,29 @@ public final class HashUtils {
 		}
 		return c;
 	}
-	
-	public static int hashJenkins(int init, Object val) {
+
+	public static int hashJenkins(final int init, final Object val) {
 		int a,b,c;
-		a = b = BASE + 4 + init; //NOCHECKSTYLE
+		a = b = BASE + 4 + init;
+		// slightly optimized version of hashJenkins(init, new Object[] {val})
 		a += val.hashCode();
-		c = -((b << 14) | (b >> 18)); //NOCHECKSTYLE
-		a ^= c; a -= ((c << 11) | (c >> 21)); //NOCHECKSTYLE
-		b ^= a; b -= ((a << 25) | (a >> 7)); //NOCHECKSTYLE
-		c ^= b; c -= ((b << 16) | (b >> 16)); //NOCHECKSTYLE
-		a ^= c; a -= ((c << 4) | (c >> 28)); //NOCHECKSTYLE
-		b ^= a; b -= ((a << 14) | (a >> 18)); //NOCHECKSTYLE
-		c ^= b; c -= ((b << 24) | (b >> 8)); //NOCHECKSTYLE
+		c = -((b << 14) | (b >>> 18));
+		a ^= c;
+		a -= ((c << 11) | (c >>> 21));
+		b ^= a;
+		b -= ((a << 25) | (a >>> 7));
+		c ^= b;
+		c -= ((b << 16) | (b >>> 16));
+		a ^= c;
+		a -= ((c << 4) | (c >>> 28));
+		b ^= a;
+		b -= ((a << 14) | (a >>> 18));
+		c ^= b;
+		c -= ((b << 24) | (b >>> 8));
 		return c;
 	}
-	
-	public static int hashHsieh(int init, Object... vals) {
+
+	public static int hashHsieh(final int init, final Object... vals) {
 		if (vals == null || vals.length == 0) {
 			return init;
 		}
@@ -97,37 +226,37 @@ public final class HashUtils {
 		/* Main loop */
 		for (final Object o : vals) {
 			final int thingHash = o.hashCode();
-			hash += (thingHash >>> 16); //NOCHECKSTYLE
-			final int tmp = ((thingHash & 0xffff) << 11) ^ hash; //NOCHECKSTYLE
-			hash = (hash << 16) ^ tmp; //NOCHECKSTYLE
-			hash += (hash >> 11); //NOCHECKSTYLE
+			hash += (thingHash >>> 16);
+			final int tmp = ((thingHash & 0xffff) << 11) ^ hash;
+			hash = (hash << 16) ^ tmp;
+			hash += (hash >>> 11);
 		}
 
 		/* Force "avalanching" of final 127 bits */
-		hash ^= hash << 3; //NOCHECKSTYLE
-		hash += hash >> 5; //NOCHECKSTYLE
-		hash ^= hash << 4; //NOCHECKSTYLE
-		hash += hash >> 17; //NOCHECKSTYLE
-		hash ^= hash << 25; //NOCHECKSTYLE
-		hash += hash >> 6; //NOCHECKSTYLE
+		hash ^= hash << 3;
+		hash += hash >>> 5;
+		hash ^= hash << 4;
+		hash += hash >>> 17;
+		hash ^= hash << 25;
+		hash += hash >>> 6;
 		return hash;
 	}
-	
-	public static int hashHsieh(int init, Object val) {
+
+	public static int hashHsieh(final int init, final Object val) {
 		int hash = init;
 		final int thingHash = val.hashCode();
-		hash += (thingHash >>> 16); //NOCHECKSTYLE
-		final int tmp = ((thingHash & 0xffff) << 11) ^ hash; //NOCHECKSTYLE
-		hash = (hash << 16) ^ tmp; //NOCHECKSTYLE
-		hash += (hash >> 11); //NOCHECKSTYLE
+		hash += (thingHash >>> 16);
+		final int tmp = ((thingHash & 0xffff) << 11) ^ hash;
+		hash = (hash << 16) ^ tmp;
+		hash += (hash >>> 11);
 		/* Force "avalanching" of final 127 bits */
-		hash ^= hash << 3; //NOCHECKSTYLE
-		hash += hash >> 5; //NOCHECKSTYLE
-		hash ^= hash << 4; //NOCHECKSTYLE
-		hash += hash >> 17; //NOCHECKSTYLE
-		hash ^= hash << 25; //NOCHECKSTYLE
-		hash += hash >> 6; //NOCHECKSTYLE
+		hash ^= hash << 3;
+		hash += hash >>> 5;
+		hash ^= hash << 4;
+		hash += hash >>> 17;
+		hash ^= hash << 25;
+		hash += hash >>> 6;
 		return hash;
 	}
-	
+
 }
