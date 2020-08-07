3a934511cef874d17921222242d716da7f2be750
danieldietsch <daniel.dietsch@gmail.com>
2020-08-07 11:42:14 +0200

    close #480: add all attributes of AssertStatement to positive and negative message of check

--- trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/output/BoogiePrettyPrinter.java
+++ trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/output/BoogiePrettyPrinter.java
@@ -2,22 +2,22 @@
  * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2010-2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE Core.
- * 
+ *
  * The ULTIMATE Core is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE Core is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE Core. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE Core, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
@@ -29,6 +29,7 @@
 package de.uni_freiburg.informatik.ultimate.boogie.output;
 
 import de.uni_freiburg.informatik.ultimate.boogie.ast.ASTType;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.Attribute;
 import de.uni_freiburg.informatik.ultimate.boogie.ast.BoogieASTNode;
 import de.uni_freiburg.informatik.ultimate.boogie.ast.Expression;
 import de.uni_freiburg.informatik.ultimate.boogie.ast.Procedure;
@@ -40,10 +41,10 @@ import de.uni_freiburg.informatik.ultimate.core.model.translation.IToString;
 
 /**
  * Provides a static method to get a prettyprinted String representation of a (Boogie) Statement.
- * 
+ *
  * @author heizmann@informatik.uni-freiburg.de
  * @author dietsch@informatik.uni-freiburg.de
- * 
+ *
  */
 
 public final class BoogiePrettyPrinter {
@@ -118,12 +119,20 @@ public final class BoogiePrettyPrinter {
 		removeLastLinebreak(sb);
 		return sb.toString();
 	}
-	
+
 	public static String print(final ASTType astType) {
 		final BoogieOutput output = new BoogieOutput(null);
 		final StringBuilder sb = new StringBuilder();
 		output.appendType(sb, astType, 0);
 		removeLastLinebreak(sb);
+		return sb.toString();
+	}
+
+	public static String print(final Attribute[] attrs) {
+		final BoogieOutput output = new BoogieOutput(null);
+		final StringBuilder sb = new StringBuilder();
+		output.appendAttributes(sb, attrs);
+		removeLastSpace(sb);
 		return sb.toString();
 	}
 
@@ -140,6 +149,18 @@ public final class BoogiePrettyPrinter {
 
 		if (sb.substring(length - linebreakLength, length).equals(LINEBREAK)) {
 			sb.replace(length - linebreakLength, length, "");
+		}
+	}
+
+	private static void removeLastSpace(final StringBuilder sb) {
+		final int length = sb.length();
+		final int spaceLength = " ".length();
+		if (length < spaceLength) {
+			return;
+		}
+
+		if (sb.substring(length - spaceLength, length).equals(" ")) {
+			sb.replace(length - spaceLength, length, "");
 		}
 	}
 
