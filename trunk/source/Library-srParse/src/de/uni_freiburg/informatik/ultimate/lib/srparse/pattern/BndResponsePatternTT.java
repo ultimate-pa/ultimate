63acf59653bd27b6a6290e8b9e105161e36b82a0
hauff <hauffn@informatik.uni-freiburg.de>
2020-09-01 20:01:10 +0200

    Minor

--- trunk/source/Library-srParse/src/de/uni_freiburg/informatik/ultimate/lib/srparse/pattern/BndResponsePatternTT.java
+++ trunk/source/Library-srParse/src/de/uni_freiburg/informatik/ultimate/lib/srparse/pattern/BndResponsePatternTT.java
@@ -51,8 +51,11 @@ public class BndResponsePatternTT extends PatternType {
 
 	@Override
 	public List<CounterTrace> transform(final CDD[] cdds, final int[] durations) {
+		assert cdds.length == 2 && durations.length == 2;
+
+		// P and Q are reserved for scope.
+		// R, S, ... are reserved for CDDs, but they are parsed in reverse order.
 		final SrParseScope scope = getScope();
-		// note: P and Q are reserved for scope, cdds are parsed in reverse order
 		final CDD R = cdds[1];
 		final CDD S = cdds[0];
 		final int c1 = durations[0];
