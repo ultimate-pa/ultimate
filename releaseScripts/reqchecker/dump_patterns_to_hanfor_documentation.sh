2f949988690b7625ee4e68049b956a9ca557c689
hauff <hauffn@informatik.uni-freiburg.de>
2020-09-16 22:30:20 +0200

    Added script that dumps patterns to hanfor.

---
+++ releaseScripts/reqchecker/dump_patterns_to_hanfor_documentation.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# 
+# Author: Nico Hauff (hauffn@informatik.uni-freiburg.de)
+
+ultimate_dir="/media/Daten/Projekte/ultimate"
+ultimate_maven_dir="${ultimate_dir}/trunk/source/BA_MavenParentUltimate"
+ultimate_failure_paths_image_dir="${ultimate_dir}/trunk/examples/Requirements/failure-paths"
+hanfor_dir="/media/Daten/Projekte/hanfor"
+hanfor_pattern_dir="${hanfor_dir}/documentation/docs/references/patterns"
+
+run_req_checker_failure_path_generation=false
+run_pea_to_dot=false
+
+while getopts rp opt
+do
+   case $opt in
+       r)  run_req_checker_failure_path_generation=true ;;
+       p)  run_pea_to_dot=true ;;
+       \?) echo "Invalid option: -$OPTARG" >&2 exit 1 ;;
+       :)  echo "Option -$OPTARG requires an argument." >&2 exit 1 ;;
+   esac
+done
+
+
+# Check if branch is dev and is not dirty.
+cd $ultimate_dir
+
+current_branch=$(git branch --show-current)
+if [ "$current_branch" != "dev" ]; then
+	echo "Current branch must be 'dev', but it is: '$current_branch'"
+	exit 1
+fi
+
+if ! git diff-index --quiet HEAD; then
+	echo "Current branch is dirty."
+	exit 1
+fi
+
+
+# Dump revision number to file.
+git rev-parse HEAD > "${hanfor_pattern_dir}/ultimate_revision.txt"
+
+
+# Uninstall Windows or format some hard drive. 
+if $run_req_checker_failure_path_generation
+then
+	cd $maven_dir
+	echo "Current working directory:" $PWD
+	mvn clean integration-test -Pmanualtest -Dtest=ReqCheckerFailurePathGenerationTestSuite
+fi
+
+if $run_pea_to_dot
+then
+  	cd $maven_dir
+	echo "Current working directory:" $PWD
+	mvn clean integration-test -Pmanualtest -Dtest=PeaToDotTestSuite
+fi
+
+exit 0
+
