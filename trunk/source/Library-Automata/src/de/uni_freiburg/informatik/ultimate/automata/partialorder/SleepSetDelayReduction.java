ce4043fccde3e9a019bfc2c1a38ca379b6c41114
ebbima <ebbima98@gmail.com>
2020-10-27 13:36:34 +0100

    SleepSetDelayReduction Run Construction
    added the reconstruction for the accepting run

--- trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/SleepSetDelayReduction.java
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/SleepSetDelayReduction.java
@@ -8,12 +8,15 @@ import java.util.Set;
 import java.util.Stack;
 
 import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
+import de.uni_freiburg.informatik.ultimate.automata.Word;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.INwaOutgoingLetterAndTransitionProvider;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedRun;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWord;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.UnaryNwaOperation;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingInternalTransition;
 import de.uni_freiburg.informatik.ultimate.automata.statefactory.IStateFactory;
 import de.uni_freiburg.informatik.ultimate.util.CoreUtil;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
 
 public class SleepSetDelayReduction<L, S> extends UnaryNwaOperation<L, S, IStateFactory<S>>{
 	
@@ -22,10 +25,15 @@ public class SleepSetDelayReduction<L, S> extends UnaryNwaOperation<L, S, IState
 	private final HashMap<S, Set<L>> mHashMap;
 	private final HashMap<S, Set<L>> mSleepSetMap;
 	private final HashMap<S, Set<Set<L>>> mDelaySetMap;
+	private final HashMap<S, ArrayList<Pair<S, L>>> mPreMap;
 	private final Stack<S> mStack;
 	private final ISleepSetOrder<S, L> mOrder;
 	private final IIndependenceRelation<S, L> mIndependenceRelation;
-	private final NestedRun<L, S> mAcceptingRun;
+	private NestedRun<L, S> mAcceptingRun;
+	private ArrayList<L> mAcceptingTransitionSequence;
+	private Word<L> mAcceptingWord;
+	private NestedWord<L> mAcceptingNestedWord;
+	private ArrayList<S> mAcceptingStateSequence;
 	
 	public SleepSetDelayReduction(final INwaOutgoingLetterAndTransitionProvider<L, S> operand,
 			final IIndependenceRelation<S, L> independenceRelation,
@@ -40,6 +48,7 @@ public class SleepSetDelayReduction<L, S> extends UnaryNwaOperation<L, S, IState
 		mHashMap = new HashMap<S, Set<L>>();
 		mSleepSetMap = new HashMap<S, Set<L>>();
 		mDelaySetMap = new HashMap<S, Set<Set<L>>>();
+		mPreMap = new HashMap<S, ArrayList<Pair<S, L>>>();
 		mStack = new Stack<S>();
 		for (S startState : mStartStateSet) {
 			mSleepSetMap.put(startState, Collections.<L>emptySet());
@@ -48,6 +57,9 @@ public class SleepSetDelayReduction<L, S> extends UnaryNwaOperation<L, S, IState
 		}
 		mOrder = sleepSetOrder;
 		mIndependenceRelation = independenceRelation;
+		mAcceptingTransitionSequence = new ArrayList<L>();
+		mAcceptingStateSequence = new ArrayList<S>();
+		mAcceptingWord = new Word<L>();
 		
 		mAcceptingRun = getAcceptingRun();
 		
@@ -57,10 +69,15 @@ public class SleepSetDelayReduction<L, S> extends UnaryNwaOperation<L, S, IState
 		
 		// TODO Insert pseudo code here
 		S currentState = mStack.firstElement();
+		//accepting run reconstruction
+		if (isGoalState(currentState)) {
+			return constructRun(currentState);
+		}
 		ArrayList<L> successorTransitionList = new ArrayList<L>();
 		Set<L> currentSleepSet = mSleepSetMap.get(currentState);
 		Set<Set<L>> currentDelaySet = mDelaySetMap.get(currentState);
 		
+		//state not visited yet
 		if (mHashMap.get(currentState) == null){
 			mHashMap.put(currentState, mSleepSetMap.get(currentState));
 			for (OutgoingInternalTransition<L, S> transition : mOperand.internalSuccessors(currentState)) {
@@ -69,7 +86,7 @@ public class SleepSetDelayReduction<L, S> extends UnaryNwaOperation<L, S, IState
 				}	
 			}
 		}
-		
+		//state already visited
 		else {
 			Set<L> currentHash = mHashMap.get(currentState);
 			for (L letter : currentHash) {
@@ -103,6 +120,10 @@ public class SleepSetDelayReduction<L, S> extends UnaryNwaOperation<L, S, IState
 				}
 				Set<Set<L>> succDelaySet = Collections.<Set<L>>emptySet();
 				if(mStack.contains(succState)) {
+					//mapping for accepting run reconstruction
+					ArrayList<Pair<S, L>> preList = mPreMap.get(succState);
+					preList.add(0, new Pair<S, L>(currentState, letterTransition));
+					mPreMap.put(succState, preList);
 					if (mDelaySetMap.get(succState) != null) {
 						succDelaySet.addAll(mDelaySetMap.get(succState));
 					}	
@@ -110,6 +131,11 @@ public class SleepSetDelayReduction<L, S> extends UnaryNwaOperation<L, S, IState
 					mDelaySetMap.put(succState, succDelaySet);
 				}
 				else {
+					//mapping for accepting run reconstruction
+					//mPreMap.put(succState, new Pair<S, L>(currentState, letterTransition));
+					ArrayList<Pair<S, L>> preList = new ArrayList<Pair<S, L>>();
+					preList.add(new Pair<S, L>(currentState, letterTransition));
+					mPreMap.put(succState, preList);
 					mDelaySetMap.put(succState, succDelaySet);
 					mStack.push(succState);
 					getAcceptingRun();
@@ -130,6 +156,39 @@ public class SleepSetDelayReduction<L, S> extends UnaryNwaOperation<L, S, IState
 			getAcceptingRun();
 		}
 		return null;
+	}
+	
+	private Boolean isGoalState(S state) {
+		return mOperand.isFinal(state);
+	}
+	
+	private NestedRun<L, S> constructRun(S state){
+		if (mOperand.isInitial(state)) {
+			return null;
+		}
+		S currentState = state;
+		S preState = mPreMap.get(currentState).get(0).getFirst();
+		L currentTransition = mPreMap.get(currentState).get(0).getSecond();
+		while (currentState != null) {
+			//do stuff
+			mAcceptingStateSequence.add(currentState);
+			if (currentTransition != null) {
+				mAcceptingTransitionSequence.add(currentTransition);
+			}
+			mPreMap.get(currentState).remove(0);
+			currentState = preState;
+			currentTransition = mPreMap.get(currentState).get(0).getSecond();
+			preState = mPreMap.get(currentState).get(0).getFirst();
+		}
+		Collections.reverse(mAcceptingStateSequence);
+		Collections.reverse(mAcceptingTransitionSequence);
+		for (L letter : mAcceptingTransitionSequence) {
+			Word<L> tempWord = new Word<L>(letter);
+			mAcceptingWord.concatenate(tempWord);
+		}
+		mAcceptingNestedWord = NestedWord.nestedWord(mAcceptingWord);
+		mAcceptingRun = new NestedRun<L, S>(mAcceptingNestedWord, mAcceptingStateSequence);
+		return mAcceptingRun;
 	}
 	
 	@Override
