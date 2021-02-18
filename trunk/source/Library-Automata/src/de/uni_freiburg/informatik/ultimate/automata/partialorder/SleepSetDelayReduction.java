ultimate-pa/ultimate

commit c163b945137d767cf185bd825903dd955dc5cb52

reductions: simplify stack management slightly

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/SleepSetDelayReduction.java
@@ -29,7 +29,6 @@ package de.uni_freiburg.informatik.ultimate.automata.partialorder;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
@@ -166,7 +165,7 @@ public class SleepSetDelayReduction<L, S> {
 			final Comparator<L> order = mOrder.getOrder(currentState);
 			successorTransitionList.sort(order);
 			final Set<L> explored = new HashSet<>();
-			final ArrayList<S> successorStateList = new ArrayList<>();
+			final Deque<S> successorStateList = new ArrayDeque<>(successorTransitionList.size());
 
 			for (final L currentLetter : successorTransitionList) {
 				final var currentTransition = getOnly(mOperand.internalSuccessors(currentState, currentLetter),
@@ -196,14 +195,11 @@ public class SleepSetDelayReduction<L, S> {
 					}
 				} else {
 					mSleepSetMap.put(succState, succSleepSet);
-					successorStateList.add(succState);
+					successorStateList.addFirst(succState);
 				}
 				explored.add(currentLetter);
 			}
-			Collections.reverse(successorStateList);
-			for (final S succState : successorStateList) {
-				mStateStack.push(succState);
-			}
+			mStateStack.addAll(successorStateList);
 
 		}
 

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/SleepSetNewStateReduction.java
@@ -150,7 +150,7 @@ public class SleepSetNewStateReduction<L, S, R> {
 			// sort successorTransitionList according to the given order
 			successorTransitionList.sort(mOrder.getOrder(currentState));
 			final Set<L> explored = new HashSet<>();
-			final ArrayList<R> successorStateList = new ArrayList<>();
+			final ArrayDeque<R> successorStateList = new ArrayDeque<>(successorTransitionList.size());
 
 			// TODO (Dominik 2021-01-24) Consider pre-computing independence between different outgoing transitions,
 			// and between outgoing transitions and sleep set members, at an earlier point. The background is that
@@ -177,14 +177,11 @@ public class SleepSetNewStateReduction<L, S, R> {
 					return;
 				}
 				if (!prune) {
-					successorStateList.add(succSleepSetState);
+					successorStateList.addFirst(succSleepSetState);
 				}
 				explored.add(currentLetter);
 			}
-			Collections.reverse(successorStateList);
-			for (final R succSleepSetState : successorStateList) {
-				mStateStack.push(succSleepSetState);
-			}
+			mStateStack.addAll(successorStateList);
 		}
 	}
 

commit 1bf70b317570052342848c703494dd2fb79bb794

fix broken stack handling II
Was broken in c163b9451, and fixed in 1c9e0bedc but only for new-states variant.

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/SleepSetDelayReduction.java
@@ -200,8 +200,9 @@ public class SleepSetDelayReduction<L, S> {
 				}
 				explored.add(currentLetter);
 			}
-			mStateStack.addAll(successorStateList);
-
+			for (final S succState : successorStateList) {
+				mStateStack.push(succState);
+			}
 		}
 
 	}