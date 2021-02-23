4aeb6efd28e848cb15b6e3b5710ccf4ac50ac081
maul.esel <maul.esel@posteo.de>
2021-02-23 18:06:07 +0100

    exclude redundant and non-concurrent benchmarks

--- trunk/examples/concurrent/bpl/weaver-benchmarks/get-benchmarks.sh
+++ trunk/examples/concurrent/bpl/weaver-benchmarks/get-benchmarks.sh
@@ -5,19 +5,40 @@ set -e
 DIR=$(realpath $(dirname "$0"))
 cd "$DIR"
 
+# Read blacklisted benchmarks from file
+if test -f blacklist.txt
+then
+  echo "Reading blacklist entries..."
+  readarray -t BLACKLIST < blacklist.txt
+  echo ""
+else
+  BLACKLIST=()
+fi
+
 # Clone repo with original benchmarks
 git clone https://github.com/weaver-verifier/weaver.git weaver
+echo ""
 
 echo -n "Translating benchmarks"
 for file in $(find "weaver/examples" -type f -name '*.wvr')
 do
-  outfile="generated/${file#"weaver/examples/"}.bpl"
-  outdir=$(dirname "$outfile")
-  mkdir -p "$outdir"
+  if [[ " ${BLACKLIST[*]} " == *"$file"* ]]
+  then
+    # Do not translate blacklisted benchmark
+    echo -n "-"
+  else
+    # Translate benchmark
+    outfile="generated/${file#"weaver/examples/"}.bpl"
+    outdir=$(dirname "$outfile")
+    mkdir -p "$outdir"
 
-  python3 "translate-weaver-benchmark.py3" "$file" "$outfile"
-  echo -n "."
+    python3 "translate-weaver-benchmark.py3" "$file" "$outfile"
+    echo -n "."
+  fi
 done
-echo -e "\nDONE."
+echo ""
 
+# Cleanup
 rm -rf "weaver"
+
+echo "DONE."
