28d18dad2ed892416a4bc70f209f01e2c515d27e
danieldietsch <daniel.dietsch@gmail.com>
2021-03-18 10:13:50 +0100

    fix Library-PEA unit tests

--- trunk/source/Library-PEATest/src/de/uni_freiburg/informatik/ultimate/lib/pea/test/CddTest.java
+++ trunk/source/Library-PEATest/src/de/uni_freiburg/informatik/ultimate/lib/pea/test/CddTest.java
@@ -1,74 +1,103 @@
 package de.uni_freiburg.informatik.ultimate.lib.pea.test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
 import de.uni_freiburg.informatik.ultimate.lib.pea.BooleanDecision;
 import de.uni_freiburg.informatik.ultimate.lib.pea.CDD;
+import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
 
 /**
- * TODO: Moved the main method from CDD to this class; make unit tests from it.
- *
  * @author Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  *
  */
+@RunWith(Parameterized.class)
 public class CddTest {
 
-	// XXX: Testing
-	public static void main(final String[] args) {
+	private final TestData mData;
+	private final ILogger mLogger;
+
+	public CddTest(final TestData data) {
+		mData = data;
+		final IUltimateServiceProvider services = UltimateMocks.createUltimateServiceProviderMock();
+		mLogger = services.getLoggingService().getLogger(getClass());
+	}
+
+	@Test
+	public void testToString() {
+		mLogger.info("********************************* CDD ********************************* ");
+		mLogger.info(mData.mCdd.toString());
+		mLogger.info(mData.mCdd.toTexString());
+	}
+
+	@Test
+	public void testIsAtomic() {
+		MatcherAssert.assertThat(mData.mCdd.toString(), mData.mCdd.isAtomic(), Is.is(mData.mIsAtomic));
+	}
+
+	@Test
+	public void testToDnf() {
+		final CDD[] dnf = mData.mCdd.toDNF();
+		mLogger.info("********************************* DNF ********************************* ");
+		for (int i = 0; i < dnf.length; i++) {
+			mLogger.info("Conjunct %d : %s", i, dnf[i]);
+		}
+	}
+
+	@Test
+	public void testToCnf() {
+		final CDD[] cnf = mData.mCdd.toCNF();
+		mLogger.info("********************************* CNF ********************************* ");
+		for (int i = 0; i < cnf.length; i++) {
+			mLogger.info("Disjunct %d : %s", i, cnf[i]);
+		}
+	}
+
+	@Parameters(name = "{index}: {0}")
+	public static Collection<Object[]> data() {
 		final CDD a = BooleanDecision.create("a");
 		final CDD b = BooleanDecision.create("b");
 		final CDD c = BooleanDecision.create("c");
 		final CDD d = BooleanDecision.create("d");
 
-		// CDD main = ((a.and(b)).and(c.or(d))).or(e).or(f.negate());
-		// CDD main2 = ((a.and(b)).or(a.negate().and(b.negate())));
-		// CDD main = main2.or(c.and(d));
-		// CDD main = c.or(main2);
-
-		// CDD teil1 = a.negate().and(b).and(c);
-		// CDD teil2 = a.and(b);
-		// CDD main = teil1.or(teil2);
-
-		// CDD links = a.negate().or(b.or(c));
-		// CDD rechts = a.or(b);
-		// CDD main = links.and(rechts);
-		// CDD links = a.negate().and(b.and(c));
-		// CDD rechts = a.and(d);
-		// CDD main = links.or(rechts);
-		final CDD links = a.negate().and(b);
-		final CDD rechts = a.and(b.or(c)).and(d);
-		final CDD main = links.or(rechts);
-
-		final CDD test = a.negate();
-		final CDD test2 = a.or(b);
-
-		System.out.println("********************************* CDD ********************************* ");
-		System.out.println(main.toString());
-		System.out.println(main.toTexString());
-		testIsAtomic(test);
-		testIsAtomic(test2);
-		testIsAtomic(links);
-		testIsAtomic(main);
-		testIsAtomic(a);
-
-		final CDD[] dnf = main.toDNF();
-		System.out.println("********************************* DNF ********************************* ");
-
-		for (int i = 0; i < dnf.length; i++) {
-			System.out.println("*** Conjunctive term " + i + ": ");
-			System.out.println(dnf[i].toString());
-		}
-
-		final CDD[] cnf = main.toCNF();
-		System.out.println("********************************* CNF ********************************* ");
-
-		for (int i = 0; i < cnf.length; i++) {
-			System.out.println("*** Disjunctive term " + i + ": ");
-			System.out.println(cnf[i].toString());
-		}
-
-		System.out.println("*********************************************************************** ");
+		final List<TestData> data = new ArrayList<>();
+		final CDD left = a.negate().and(b);
+		final CDD right = a.and(b.or(c)).and(d);
+		data.add(new TestData(left, false));
+		data.add(new TestData(right, false));
+		data.add(new TestData(left.or(right), false));
+		data.add(new TestData(a.negate(), true));
+		data.add(new TestData(a.or(b), false));
+		return data.stream().map(TestData::toData).collect(Collectors.toList());
 	}
 
-	private static void testIsAtomic(final CDD cdd) {
-		System.out.println("The formula " + cdd.toString() + " is atomic: " + cdd.isAtomic());
+	private static final class TestData {
+		private final CDD mCdd;
+		private final boolean mIsAtomic;
+
+		public TestData(final CDD cdd, final boolean isAtomic) {
+			mCdd = cdd;
+			mIsAtomic = isAtomic;
+		}
+
+		private Object[] toData() {
+			return new Object[] { this };
+		}
+
+		@Override
+		public String toString() {
+			return mCdd.toString();
+		}
 	}
 }
