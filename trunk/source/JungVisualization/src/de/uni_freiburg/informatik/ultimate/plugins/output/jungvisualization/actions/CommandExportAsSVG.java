db63bac29496dbfd37970f0f588b89dbeb7ae39c
danieldietsch <daniel.dietsch@gmail.com>
2021-03-18 17:26:37 +0100

    remove Library-Jung

--- trunk/source/JungVisualization/src/de/uni_freiburg/informatik/ultimate/plugins/output/jungvisualization/actions/CommandExportAsSVG.java
+++ trunk/source/JungVisualization/src/de/uni_freiburg/informatik/ultimate/plugins/output/jungvisualization/actions/CommandExportAsSVG.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE JungVisualization plug-in.
- * 
+ *
  * The ULTIMATE JungVisualization plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE JungVisualization plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE JungVisualization plug-in. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE JungVisualization plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE JungVisualization plug-in grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE JungVisualization plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.plugins.output.jungvisualization.actions;
@@ -49,7 +49,7 @@ import de.uni_freiburg.informatik.ultimate.plugins.output.jungvisualization.pref
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 
 public class CommandExportAsSVG extends AbstractHandler {
-	
+
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 
@@ -65,8 +65,8 @@ public class CommandExportAsSVG extends AbstractHandler {
 	 */
 	public void exportAsSVG(JungEditorInput editorInput) {
 		assert editorInput != null;
-		final String svgFilePath = new RcpPreferenceProvider(Activator.PLUGIN_ID)
-				.getString(JungPreferenceValues.LABEL_PATH);
+		final String svgFilePath =
+				new RcpPreferenceProvider(Activator.PLUGIN_ID).getString(JungPreferenceValues.LABEL_PATH);
 
 		final JFileChooser chooser = new JFileChooser();
 		chooser.setSelectedFile(new File(svgFilePath + "/default.svg"));
