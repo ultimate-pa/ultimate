4b90c22d34e9bc8efa2092e071f4d36de441ba0b
maul.esel <maul.esel@posteo.de>
2021-04-07 17:08:16 +0200

    add caching for persistent sets

---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/CachedPersistentSetChoice.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2021 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.partialorder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class CachedPersistentSetChoice<L, S> implements IPersistentSetChoice<L, S> {
+	private final Map<S, Set<L>> mCache = new HashMap<>();
+	private final IPersistentSetChoice<L, S> mUnderlying;
+
+	public CachedPersistentSetChoice(final IPersistentSetChoice<L, S> underlying) {
+		mUnderlying = underlying;
+	}
+
+	@Override
+	public Set<L> persistentSet(final S state) {
+		return mCache.computeIfAbsent(state, mUnderlying::persistentSet);
+	}
+}
