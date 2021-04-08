effe8b41fb245965d51bea8df2f9d27ba634f380
maul.esel <maul.esel@posteo.de>
2021-04-08 10:54:15 +0200

    implement pure DFS traversal of deterministic finite automata

---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/DepthFirstTraversal.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2021 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2021 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.partialorder;
+
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.StreamSupport;
+
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.INwaOutgoingLetterAndTransitionProvider;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomataUtils;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingInternalTransition;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.DataStructureUtils;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
+
+/**
+ * Implements a depth-first traversal of deterministic finite automata.
+ *
+ * @author Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ *
+ * @param <L>
+ *            The type of letters in the traversed automaton
+ * @param <S>
+ *            The type of states in the traversed automaton
+ */
+public class DepthFirstTraversal<L, S> {
+	private final INwaOutgoingLetterAndTransitionProvider<L, S> mOperand;
+	private final IDfsOrder<L, S> mOrder;
+	private final IDfsVisitor<L, S> mVisitor;
+
+	private final Deque<S> mStateStack = new ArrayDeque<>();
+	private final Deque<Pair<S, OutgoingInternalTransition<L, S>>> mWorklist = new ArrayDeque<>();
+	private final Set<S> mVisited = new HashSet<>();
+
+	/**
+	 * Performs a depth-first traversal. This constructor is called purely for its side-effects.
+	 *
+	 * @param operand
+	 *            The automaton to be traversed
+	 * @param order
+	 *            The order in which transitions for each state should be explored
+	 * @param visitor
+	 *            A visitor to traverse the automaton
+	 */
+	public DepthFirstTraversal(final INwaOutgoingLetterAndTransitionProvider<L, S> operand, final IDfsOrder<L, S> order,
+			final IDfsVisitor<L, S> visitor) {
+		assert NestedWordAutomataUtils.isFiniteAutomaton(operand) : "DFS supports only finite automata";
+
+		mOperand = operand;
+		mOrder = order;
+		mVisitor = visitor;
+
+		traverse();
+	}
+
+	private void traverse() {
+		final S initial = DataStructureUtils.getOneAndOnly(mOperand.getInitialStates(), "initial state");
+		visitState(initial);
+
+		while (!mWorklist.isEmpty()) {
+			final var current = mWorklist.pop();
+			final S currentState = current.getFirst();
+			final OutgoingInternalTransition<L, S> currentTransition = current.getSecond();
+
+			// Backtrack states still on the stack whose exploration has finished.
+			backtrackUntil(currentState);
+
+			final S nextState = currentTransition.getSucc();
+			mVisitor.discoverTransition(currentState, currentTransition.getLetter(), nextState);
+			if (!mVisited.contains(nextState)) {
+				visitState(nextState);
+			}
+		}
+
+		backtrackUntil(initial);
+		mStateStack.pop();
+		mVisitor.backtrackState(initial);
+	}
+
+	private void backtrackUntil(final S state) {
+		while (!mStateStack.peek().equals(state)) {
+			final S oldState = mStateStack.pop();
+			mVisitor.backtrackState(oldState);
+		}
+	}
+
+	private void visitState(final S state) {
+		assert !mVisited.contains(state) : "must never re-visit state";
+
+		if (mOperand.isInitial(state)) {
+			assert mVisited.isEmpty() : "initial state should be first visited state";
+			mVisitor.addStartState(state);
+		} else {
+			mVisitor.discoverState(state);
+		}
+		mVisited.add(state);
+
+		assert !mStateStack.contains(state) : "must not infinitely unroll loop";
+		mStateStack.push(state);
+
+		final Comparator<OutgoingInternalTransition<L, S>> comp =
+				Comparator.<OutgoingInternalTransition<L, S>, L> comparing(OutgoingInternalTransition::getLetter,
+						mOrder.getOrder(state)).reversed();
+		StreamSupport.stream(mOperand.internalSuccessors(state).spliterator(), false).sorted(comp)
+				.forEachOrdered(out -> mWorklist.push(new Pair<>(state, out)));
+	}
+}
