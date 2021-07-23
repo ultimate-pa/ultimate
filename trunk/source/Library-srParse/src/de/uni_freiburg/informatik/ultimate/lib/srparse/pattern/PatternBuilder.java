25f36bcd79771fe366c1815b374d2e93fc4e5c3f
henkele <henkele@informatik.uni-freiburg.de>
2021-07-23 12:48:05 +0200

    add BndEdgeResponsePatternTU

--- trunk/source/Library-srParse/src/de/uni_freiburg/informatik/ultimate/lib/srparse/pattern/PatternBuilder.java
+++ trunk/source/Library-srParse/src/de/uni_freiburg/informatik/ultimate/lib/srparse/pattern/PatternBuilder.java
@@ -59,14 +59,14 @@ public class PatternBuilder {
 
 	private static final Class<?>[] PATTERNS = new Class<?>[] { BndDelayedResponsePatternUT.class,
 			BndDelayedResponsePatternTU.class, BndEdgeResponsePattern.class, BndEdgeResponsePatternDelayed.class,
-			BndEntryConditionPattern.class, BndExistencePattern.class, BndInvariancePattern.class,
-			BndPossResponsePattern.class, BndRecurrencePattern.class, BndResponsePatternTT.class,
-			BndResponsePatternTU.class, BndResponsePatternUT.class, BndTriggeredEntryConditionPattern.class,
-			BndTriggeredEntryConditionPatternDelayed.class, ConstrainedChainPattern.class,
-			EdgeResponsePatternDelayed.class, InitializationPattern.class, InstAbsPattern.class, InvariantPattern.class,
-			MaxDurationPattern.class, MinDurationPattern.class, PossibilityPattern.class,
-			PrecedenceChain12Pattern.class, PrecedenceChain21Pattern.class, PrecedencePattern.class,
-			ResponseChain12Pattern.class, ResponsePattern.class, UniversalityPattern.class,
+			BndEdgeResponsePatternTU.class, BndEntryConditionPattern.class, BndExistencePattern.class,
+			BndInvariancePattern.class, BndPossResponsePattern.class, BndRecurrencePattern.class,
+			BndResponsePatternTT.class, BndResponsePatternTU.class, BndResponsePatternUT.class,
+			BndTriggeredEntryConditionPattern.class, BndTriggeredEntryConditionPatternDelayed.class,
+			ConstrainedChainPattern.class, EdgeResponsePatternDelayed.class, InitializationPattern.class,
+			InstAbsPattern.class, InvariantPattern.class, MaxDurationPattern.class, MinDurationPattern.class,
+			PossibilityPattern.class, PrecedenceChain12Pattern.class, PrecedenceChain21Pattern.class,
+			PrecedencePattern.class, ResponseChain12Pattern.class, ResponsePattern.class, UniversalityPattern.class,
 			UniversalityPatternDelayed.class };
 
 	private static final Map<Class<? extends PatternType<?>>, PatternTypeConstructor> CONSTRUCTORS = new HashMap<>();
