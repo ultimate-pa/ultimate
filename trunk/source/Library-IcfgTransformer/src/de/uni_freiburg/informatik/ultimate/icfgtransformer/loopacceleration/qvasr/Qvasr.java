39ad8fedaa6c7e0c0c862f237eba7df7610b6c26
JonasWerner <wernerj@informatik.uni-freiburg.de>
2021-10-10 19:17:01 +0200

    Added QVasr-library; changes to accelerated Interpolation to make use of QVasr

---
+++ trunk/source/Library-IcfgTransformer/src/de/uni_freiburg/informatik/ultimate/icfgtransformer/loopacceleration/qvasr/Qvasr.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2021 Jonas Werner (wernerj@informatik.uni-freiburg.de)
+ * Copyright (C) 2021 University of Freiburg
+ *
+ * This file is part of the ULTIMATE IcfgTransformer library.
+ *
+ * The ULTIMATE IcfgTransformer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE IcfgTransformer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE IcfgTransformer library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE IcfgTransformer library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE IcfgTransformer grant you additional permission
+ * to convey the resulting work.
+ */
+
+package de.uni_freiburg.informatik.ultimate.icfgtransformer.loopacceleration.qvasr;
+
+import java.util.Set;
+
+import de.uni_freiburg.informatik.ultimate.logic.Rational;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
+
+/**
+ * @author Jonas Werner (wernerj@informatik.uni-freiburg.de) This class represents a rational vector addition system
+ *         with resets that model the relations of input and output variables of transition formulas. They consist of a
+ *         set of tuples of vectors called transformer. The first vector is a binary reset vector indicating a reset to
+ *         a variable, while the second vector represents an addition to a variable.
+ *
+ */
+public class Qvasr {
+	private final Set<Pair<Rational[], Rational[]>> mTransformer;
+
+	/**
+	 * Construct a new Q-Vasr using a single transformer
+	 *
+	 * @param initialTransformer
+	 */
+	public Qvasr(final Set<Pair<Rational[], Rational[]>> initialTransformer) {
+		mTransformer = initialTransformer;
+	}
+
+	public Set<Pair<Rational[], Rational[]>> getQvasrTransformer() {
+		return mTransformer;
+	}
+
+	public void addTransformer(final Pair<Rational[], Rational[]> transformer) {
+		mTransformer.add(transformer);
+	}
+
+	@Override
+	public String toString() {
+		return mTransformer.toString();
+	}
+}
---
+++ trunk/source/Library-IcfgTransformer/src/de/uni_freiburg/informatik/ultimate/icfgtransformer/loopacceleration/qvasr/QvasrUtils.java
@@ -0,0 +1,35 @@
+package de.uni_freiburg.informatik.ultimate.icfgtransformer.loopacceleration.qvasr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.logic.ApplicationTerm;
+import de.uni_freiburg.informatik.ultimate.logic.Term;
+
+/**
+ *
+ * @author Jonas Werner (wernerj@informatik.uni-freiburg.de) A collection of useful functions needed in
+ *         Q-Vasr-abstraction
+ *
+ */
+public class QvasrUtils {
+	/**
+	 * Split a {@link Term} in DNF into its conjuncts.
+	 *
+	 * @param loopRelation
+	 * @return
+	 */
+	public static List<Term> splitDisjunction(final Term loopRelation, final IUltimateServiceProvider services) {
+		final List<Term> result = new ArrayList<>();
+		final ApplicationTerm dnfAppTerm = (ApplicationTerm) loopRelation;
+		if (!dnfAppTerm.getFunction().getName().equals("or")) {
+			result.add(loopRelation);
+		} else {
+			for (final Term disjunct : dnfAppTerm.getParameters()) {
+				result.add(disjunct);
+			}
+		}
+		return result;
+	}
+}
