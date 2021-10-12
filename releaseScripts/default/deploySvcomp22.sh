c1d8e8c4e94870ac9b2d0906992da2ee3a1a8bb1
Daniel Dietsch <daniel.dietsch@gmail.com>
2021-10-12 10:24:50 +0200

    begin deploying for svcomp 22

--- releaseScripts/default/deploySvcomp22.sh
+++ releaseScripts/default/deploySvcomp22.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Script that deploys a new version to SVCOMP 21. You have to call makeFresh.sh before.
+# Script that deploys a new version to SVCOMP 22. You have to call makeFresh.sh before.
 
-SVCOMP_GITLAB_DIR="/storage/repos/svcomp-archives-2021/2021"
+SVCOMP_GITLAB_DIR="/storage/repos/svcomp-archives-2022/2022"
 POST_FINAL=false
 
 EXPECTED_FILES=(
@@ -18,25 +18,19 @@ function git_is_clean {
 }
 
 if [ ! -d "$SVCOMP_GITLAB_DIR" ]; then
-	echo "Directory $SVCOMP_GITLAB_DIR does not exist"
-	exit 1
+  echo "Directory $SVCOMP_GITLAB_DIR does not exist"
+  exit 1
 fi
 
 pushd "$SVCOMP_GITLAB_DIR" > /dev/null || { echo "Could not switch to directory $SVCOMP_GITLAB_DIR" ; exit 1 ; }
 if ! git_is_clean ; then
-	echo "Repo is dirty, did you do things manually?"
-	exit 1
-fi
-
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-if [ "ultimate" != "$current_branch" ]; then
-	echo "Repo is in branch \"$current_branch\" but should be in branch \"ultimate\""
-	exit 1
+  echo "Repo is dirty, did you do things manually?"
+  exit 1
 fi
 
 echo "Updating..."
 git fetch --all
-git reset --hard origin/ultimate
+git reset --hard origin/main
 popd > /dev/null || { echo "Could not exit directory $SVCOMP_GITLAB_DIR" ; exit 1 ; }
 
 
@@ -46,21 +40,21 @@ VERSION="${VERSION}-"$(git rev-parse HEAD | cut -c1-8)
 echo "Copying .zip files for version $VERSION to SVCOMP GitLab repo in $SVCOMP_GITLAB_DIR"
 for z in "${EXPECTED_FILES[@]}"; do
     if [ ! -f "$z" ]; then
-		echo "$z does not exist"
-		exit 1
-	fi
+    echo "$z does not exist"
+    exit 1
+  fi
     f=$(echo "$z" | sed 's/Ultimate\(.*\)-linux\.zip/u\1\.zip/g' | tr '[:upper:]' '[:lower:]')
-	if $POST_FINAL ; then
-		f="${f%.zip}-post-final.zip"
-	fi
+  if $POST_FINAL ; then
+    f="${f%.zip}-post-final.zip"
+  fi
     echo "Copying $z to ${SVCOMP_GITLAB_DIR}/${f}"
-	cp "$z" "${SVCOMP_GITLAB_DIR}/${f}"
+  cp "$z" "${SVCOMP_GITLAB_DIR}/${f}"
 done
 
 pushd "$SVCOMP_GITLAB_DIR" > /dev/null || { echo "Could not switch to directory $SVCOMP_GITLAB_DIR" ; exit 1 ; }
 
 if [ ! -L "${SVCOMP_GITLAB_DIR}/${VALIDATOR_SYMLINK}" ]; then
-	ln -s "$VALIDATOR" "$VALIDATOR_SYMLINK"
+  ln -s "$VALIDATOR" "$VALIDATOR_SYMLINK"
 fi
 
 echo "Pushing to remote "
