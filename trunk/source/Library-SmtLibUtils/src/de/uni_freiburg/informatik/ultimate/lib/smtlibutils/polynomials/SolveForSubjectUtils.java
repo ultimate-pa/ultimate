ba0915c9e7bc90c675c3a6c0cace43723de74838
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2021-11-02 18:03:56 +0100

    modify auxiliary method

--- trunk/source/Library-SmtLibUtils/src/de/uni_freiburg/informatik/ultimate/lib/smtlibutils/polynomials/SolveForSubjectUtils.java
+++ trunk/source/Library-SmtLibUtils/src/de/uni_freiburg/informatik/ultimate/lib/smtlibutils/polynomials/SolveForSubjectUtils.java
@@ -598,7 +598,7 @@ public class SolveForSubjectUtils {
 
 	@Deprecated
 	private static boolean someGivenTermVariableOccursInTerm(final Term term, final Set<TermVariable> termVariables) {
-		final Set<Term> divSubterms = SmtUtils.extractApplicationTerms("div", term);
+		final Set<Term> divSubterms = SmtUtils.extractApplicationTerms("div", term, false);
 		return divSubterms.stream().anyMatch(x -> Arrays.stream(x.getFreeVars()).anyMatch(termVariables::contains));
 	}
 
