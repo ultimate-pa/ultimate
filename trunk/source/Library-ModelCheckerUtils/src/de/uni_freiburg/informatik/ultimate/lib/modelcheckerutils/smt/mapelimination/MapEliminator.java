ultimate-pa/ultimate

commit 04e4febf48f82fec7bd6198a541b6a173472a596

try to use Substitution instead of PureSubstitution

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/smt/mapelimination/MapEliminator.java
@@ -61,7 +61,7 @@ import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.NonTheorySymbol;
 import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.NonTheorySymbolFinder;
 import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.QuantifierUtils;
 import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.SmtUtils;
-import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.PureSubstitution;
+import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.Substitution;
 import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.arrays.ArrayIndex;
 import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.arrays.MultiDimensionalSelect;
 import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.quantifier.PartialQuantifierElimination;
@@ -515,7 +515,7 @@ public class MapEliminator {
 						getReplacementVar(functionCall, transformula, mScript, mReplacementVarFactory, mAuxVars));
 			}
 		}
-		return new PureSubstitution(mManagedScript, substitution).transform(term);
+		return Substitution.apply(mManagedScript, substitution, term);
 	}
 
 	/**
@@ -551,8 +551,7 @@ public class MapEliminator {
 		final List<Term> conjuncts = new ArrayList<>();
 		conjuncts.addAll(Arrays.asList(SmtUtils.getConjuncts(newTerm)));
 		conjuncts.addAll(auxVarEqualities);
-		final PureSubstitution substitution = new PureSubstitution(mManagedScript, substitutionMap);
-		return substitution.transform(substitution.transform(SmtUtils.and(mScript, conjuncts)));
+		return Substitution.apply(mManagedScript, substitutionMap, SmtUtils.and(mScript, conjuncts));
 	}
 
 	/**
@@ -570,7 +569,7 @@ public class MapEliminator {
 				}
 			}
 		}
-		return new PureSubstitution(mManagedScript, substitutionMap).transform(term);
+		return Substitution.apply(mManagedScript, substitutionMap, term);
 	}
 
 	/**

commit 67fb61137e21b605df398bf3e930742c66a01b01

fix bug that was introduced in 04e4febf48f82fec7bd6198a541b6a173472a596

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/smt/mapelimination/MapEliminator.java
@@ -551,7 +551,11 @@ public class MapEliminator {
 		final List<Term> conjuncts = new ArrayList<>();
 		conjuncts.addAll(Arrays.asList(SmtUtils.getConjuncts(newTerm)));
 		conjuncts.addAll(auxVarEqualities);
-		return Substitution.apply(mManagedScript, substitutionMap, SmtUtils.and(mScript, conjuncts));
+		// 20211226 Matthias: I am wondering why the substitution is applied twice.
+		// Because we often have two-dimensional arrays? Shouldn't we apply the
+		// substitution until a fixpoint is reached?
+		return Substitution.apply(mManagedScript, substitutionMap,
+				Substitution.apply(mManagedScript, substitutionMap, SmtUtils.and(mScript, conjuncts)));
 	}
 
 	/**