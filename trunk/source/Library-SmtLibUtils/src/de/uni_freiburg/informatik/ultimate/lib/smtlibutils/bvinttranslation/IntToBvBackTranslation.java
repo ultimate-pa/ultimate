a1ae0662505b68835e1457157f81fea3114ab1de
Max.Barth95@gmx.de <max.barth95@gmx.de>
2022-01-07 00:34:07 +0100

    Added Copyrights and Bugfix for intand back-translation

--- trunk/source/Library-SmtLibUtils/src/de/uni_freiburg/informatik/ultimate/lib/smtlibutils/bvinttranslation/IntToBvBackTranslation.java
+++ trunk/source/Library-SmtLibUtils/src/de/uni_freiburg/informatik/ultimate/lib/smtlibutils/bvinttranslation/IntToBvBackTranslation.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (C) 2021-2022 Max Barth (Max.Barth95@gmx.de)
+ * Copyright (C) 2021-2022 University of Freiburg
+ *
+ * This file is part of the ULTIMATE ModelCheckerUtils Library.
+ *
+ * The ULTIMATE ModelCheckerUtils Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE ModelCheckerUtils Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE ModelCheckerUtils Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE ModelCheckerUtils Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE ModelCheckerUtils Library grant you additional permission
+ * to convey the resulting work.
+ */
 package de.uni_freiburg.informatik.ultimate.lib.smtlibutils.bvinttranslation;
 
 import java.math.BigInteger;
@@ -438,7 +464,7 @@ public class IntToBvBackTranslation extends TermTransformer {
 		}
 
 		final FunctionSymbol fsym = appTerm.getFunction();
-		if (fsym.equals(mIntand)) {
+		if (fsym.equals(mIntand) || fsym.getName().equals("intand")) {
 			for (int i = 0; i < args.length; i++) {
 				newargs[i] = bringTermToWidth(args[i], getWidth(appTerm), isSigned(oldargs[i]));
 			}
@@ -783,7 +809,6 @@ public class IntToBvBackTranslation extends TermTransformer {
 
 			}
 		}
-
 		super.convertApplicationTerm(appTerm, newargs);
 	}
 
