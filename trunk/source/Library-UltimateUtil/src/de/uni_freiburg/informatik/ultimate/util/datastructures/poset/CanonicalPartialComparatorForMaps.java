68931339763f4e347f44ad745c9fe061c69b93b6
maul.esel <maul.esel@posteo.de>
2022-02-25 16:53:06 +0100

    fix ComparisonResult::aggregate and expand documentation

--- trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/poset/CanonicalPartialComparatorForMaps.java
+++ trunk/source/Library-UltimateUtil/src/de/uni_freiburg/informatik/ultimate/util/datastructures/poset/CanonicalPartialComparatorForMaps.java
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2016 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
- * Copyright (C) 2016 University of Freiburg
+ * Copyright (C) 2016-2022 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
+ * Copyright (C) 2022 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2016-2022 University of Freiburg
  *
  * This file is part of the ULTIMATE Util Library.
  *
@@ -33,7 +34,15 @@ import java.util.Map.Entry;
 /**
  * Comparator for maps with values from a partially ordered set.
  *
+ * Given two maps {@code m1} and {@code m2} we say that {@code m1} is less or equal {@code m2} if the following
+ * conditions hold:
+ * <ul>
+ * <li>{@code m1.keySet()} is a subset of {@code m2.keySet()}</li>
+ * <li>For all keys {@code k} of {@code m1}, the value {@code m1.get(k)} is less or equal {@code m2.get(k)}</li>
+ * </ul>
+ *
  * @author Matthias Heizmann
+ * @author Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
  *
  * @param <K>
  *            The type of keys (no ordering considered)
@@ -43,14 +52,38 @@ import java.util.Map.Entry;
 public class CanonicalPartialComparatorForMaps<K, V> implements IPartialComparator<Map<K, V>> {
 	private final IPartialComparator<V> mComparator;
 
+	/**
+	 * Create a new map comparator from a partial comparator for values.
+	 *
+	 * @param comparator
+	 *            The comparator used to compare values in maps
+	 */
 	public CanonicalPartialComparatorForMaps(final IPartialComparator<V> comparator) {
 		mComparator = comparator;
 	}
 
+	/**
+	 * Create a new map comparator from a Java {@link Comparator} for values.
+	 *
+	 * @param comparator
+	 *            the comparator used to compare values in maps
+	 * @param isConsistent
+	 *            whether or not the comparator is "consistent with equals". See
+	 *            {@link IPartialComparator#fromNonPartialComparator(Comparator, boolean)} for details.
+	 */
 	public CanonicalPartialComparatorForMaps(final Comparator<V> comparator, final boolean isConsistent) {
 		this(IPartialComparator.fromNonPartialComparator(comparator, isConsistent));
 	}
 
+	/**
+	 * Create a new map comparator from a Java {@link Comparator} for values.
+	 *
+	 * For compatibility reasons, this assumes that the given comparator is "consistent with equals" (see
+	 * {@link IPartialComparator#fromNonPartialComparator(Comparator, boolean)} for details.
+	 *
+	 * @param comparator
+	 *            the comparator used to compare values in maps
+	 */
 	public CanonicalPartialComparatorForMaps(final Comparator<V> comparator) {
 		this(comparator, true);
 	}
