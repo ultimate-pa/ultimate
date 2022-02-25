d2d6eedc0d71f9f0490496d3325fa27fedf83be3
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2022-02-25 21:15:00 +0100

    fix and add examples

--- trunk/examples/programs/quantifier/regression/c/BugArrayOfFunctionPointersSize.c
+++ trunk/examples/programs/quantifier/regression/c/BugArrayOfFunctionPointersSize.c
@@ -6,7 +6,7 @@
  * Date: 2016-12-30
  * 
  */
-
+#include <stdio.h>
 
 int foo() {
     return 0;
@@ -14,8 +14,10 @@ int foo() {
 
 
 int main(void) {
-	int (*arrayOfFunctionPointers[])() = { foo };
-//    arrayOfFunctionPointers[0];
-    *arrayOfFunctionPointers;
+    int (*arrayOfFunctionPointers[1])() = { foo };
+    int size1 = sizeof(arrayOfFunctionPointers);
+    int *p;
+    int size2 = sizeof(p);
+    //@ assert size1 == size2;
     return 0;
 }
