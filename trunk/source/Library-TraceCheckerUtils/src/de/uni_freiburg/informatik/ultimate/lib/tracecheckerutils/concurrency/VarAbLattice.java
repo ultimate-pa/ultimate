a555820cc37a9377854fdf5e9cf1dbfb8ad6dd64
Marcel Rogg <marcel.rogg@posteo.de>
2022-03-01 14:30:39 +0100

    rewritten Lattice

--- trunk/source/Library-TraceCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/concurrency/VarAbLattice.java
+++ trunk/source/Library-TraceCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/concurrency/VarAbLattice.java
@@ -26,47 +26,83 @@
  */
 package de.uni_freiburg.informatik.ultimate.lib.tracecheckerutils.concurrency;
 
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IAction;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.variables.IProgramVar;
-import de.uni_freiburg.informatik.ultimate.util.datastructures.poset.CanonicalLatticeForMaps;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.poset.ILattice;
 
 public class VarAbLattice<L extends IAction> implements ILattice<VarAbsConstraints<L>> {
-	CanonicalLatticeForMaps<L, Set<IProgramVar>> mMapLattice;
 	ILattice<Set<IProgramVar>> mLattice;
 
 	// This lattice Should be an UpsideDownLattice of a Powersetlattice
 	public VarAbLattice(final ILattice<Set<IProgramVar>> lattice) {
 		mLattice = lattice;
-		mMapLattice = new CanonicalLatticeForMaps<>(mLattice);
 
 	}
 
 	@Override
 	public ComparisonResult compare(final VarAbsConstraints<L> o1, final VarAbsConstraints<L> o2) {
-		final ComparisonResult inResult =
-				mMapLattice.compare(o1.getCopyOfInContraintsMap(), o2.getCopyOfInContraintsMap());
-		final ComparisonResult outResult =
-				mMapLattice.compare(o1.getCopyOfOutContraintsMap(), o2.getCopyOfOutContraintsMap());
-		if (inResult == ComparisonResult.INCOMPARABLE || outResult == ComparisonResult.INCOMPARABLE) {
+
+		return ComparisonResult.aggregate(compareMaps(o1.getCopyOfInContraintsMap(), o2.getCopyOfInContraintsMap()),
+				compareMaps(o1.getCopyOfOutContraintsMap(), o2.getCopyOfOutContraintsMap()));
+	}
+
+	public ComparisonResult compareMapsSameLetters(final Map<L, Set<IProgramVar>> m1,
+			final Map<L, Set<IProgramVar>> m2) {
+		boolean m2subsetm1 = true;
+		boolean m1subsetm2 = true;
+		for (final Entry<L, Set<IProgramVar>> en : m1.entrySet()) {
+			if (en.getValue().containsAll(m2.get(en.getKey()))) {
+				m2subsetm1 = false;
+			}
+			if (m2.get(en.getKey()).containsAll(en.getValue())) {
+				m1subsetm2 = false;
+			}
+		}
+		if (!m1subsetm2 && !m2subsetm1) {
 			return ComparisonResult.INCOMPARABLE;
-		} else if (inResult == ComparisonResult.EQUAL) {
-			if (outResult == ComparisonResult.STRICTLY_GREATER) {
-				return ComparisonResult.STRICTLY_GREATER;
-			}
-			if (outResult == ComparisonResult.STRICTLY_SMALLER) {
-				return ComparisonResult.STRICTLY_SMALLER;
-			}
-			if (outResult == ComparisonResult.EQUAL) {
+		} else if (m1subsetm2) {
+			if (m2subsetm1) {
 				return ComparisonResult.EQUAL;
 			}
-
-		} else if (inResult == ComparisonResult.STRICTLY_GREATER && outResult == ComparisonResult.STRICTLY_GREATER) {
 			return ComparisonResult.STRICTLY_GREATER;
-		} else if (inResult == ComparisonResult.STRICTLY_SMALLER && outResult == ComparisonResult.STRICTLY_SMALLER) {
-			return ComparisonResult.STRICTLY_SMALLER;
+		}
+		// See reasoning; m1: L-> {x,y}, m2: L -> {x,y}; m1subsetm2 =false, m2subsetm1 = true; m1 strictly smaller
+		// m2, since the more constraints the smaller
+		return ComparisonResult.STRICTLY_SMALLER;
+	}
+
+	public boolean allSameSetsContainMoreElements(final Map<L, Set<IProgramVar>> m1,
+			final Map<L, Set<IProgramVar>> m2) {
+		for (final Entry<L, Set<IProgramVar>> en : m2.entrySet()) {
+			if (!m1.get(en.getKey()).containsAll(en.getValue())) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public ComparisonResult compareMaps(final Map<L, Set<IProgramVar>> m1, final Map<L, Set<IProgramVar>> m2) {
+		// If m1 contains the same letters <L> as m1, only the set differs
+		if (m1.keySet().equals(m2.keySet())) {
+			return compareMapsSameLetters(m1, m2);
+		}
+		// If m1 contains more letters <L> that are constrained by the same Sets of ProgramVars -> m1 is strictly
+		// smaller (if the sets of m1 contain same or more constraining vars)
+		if (!m1.keySet().containsAll(m2.keySet()) && !m2.keySet().containsAll(m1.keySet())) {
+			return ComparisonResult.INCOMPARABLE;
+		}
+		if (m1.keySet().containsAll(m2.keySet())) {
+			return allSameSetsContainMoreElements(m1, m2) ? ComparisonResult.STRICTLY_SMALLER
+					: ComparisonResult.INCOMPARABLE;
+
+		}
+		if (m2.keySet().containsAll(m1.keySet())) {
+			return allSameSetsContainMoreElements(m2, m1) ? ComparisonResult.STRICTLY_GREATER
+					: ComparisonResult.INCOMPARABLE;
 		}
 		return ComparisonResult.INCOMPARABLE;
 	}
