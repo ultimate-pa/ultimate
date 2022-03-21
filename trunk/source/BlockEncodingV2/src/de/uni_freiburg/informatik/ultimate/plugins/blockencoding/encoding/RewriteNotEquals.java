ultimate-pa/ultimate

commit 7a9d4c5290fedaa59fe48f2ba5474a4391740e34

fix copying of IcfgEdges (may be forks)

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/cfg/structure/IcfgEdgeBuilder.java
@@ -283,11 +283,31 @@ public class IcfgEdgeBuilder {
 
 	public IcfgEdge constructInternalTransition(final IcfgEdge oldTransition, final IcfgLocation source,
 			final IcfgLocation target, final UnmodifiableTransFormula tf) {
+		return constructInternalTransition(oldTransition, source, target, tf, false);
+	}
+
+	public IcfgEdge constructInternalTransition(final IcfgEdge oldTransition, final IcfgLocation source,
+			final IcfgLocation target, final UnmodifiableTransFormula tf, final boolean connect) {
 		assert onlyInternal(oldTransition) : "You cannot have calls or returns in normal sequential compositions";
 		final IcfgInternalTransition rtr = mEdgeFactory.createInternalTransition(source, target, null, tf);
-		source.addOutgoing(rtr);
-		target.addIncoming(rtr);
+		if (connect) {
+			source.addOutgoing(rtr);
+			target.addIncoming(rtr);
+		}
 		ModelUtils.copyAnnotations(oldTransition, rtr);
+		return rtr;
+	}
+
+	public IcfgEdge constructForkCurrentTransition(final IcfgForkThreadCurrentTransition oldTransition,
+			final UnmodifiableTransFormula tf, final boolean connect) {
+		final IcfgForkThreadCurrentTransition rtr = mEdgeFactory.createForkThreadCurrentTransition(
+				oldTransition.getSource(), oldTransition.getTarget(), oldTransition.getPayload(), tf,
+				oldTransition.getForkSmtArguments(), oldTransition.getNameOfForkedProcedure());
+		ModelUtils.copyAnnotations(oldTransition, rtr);
+		if (connect) {
+			oldTransition.getSource().addOutgoing(rtr);
+			oldTransition.getTarget().addIncoming(rtr);
+		}
 		return rtr;
 	}
 

trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/concurrency/IcfgCopyFactory.java
@@ -31,6 +31,7 @@ import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.CfgSmtToolk
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IActionWithBranchEncoders;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgEdge;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgEdgeBuilder;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgForkThreadCurrentTransition;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.transitions.UnmodifiableTransFormula;
 import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.SmtUtils.SimplificationTechnique;
 import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.SmtUtils.XnfConversionTechnique;
@@ -61,8 +62,12 @@ public class IcfgCopyFactory implements ICopyActionFactory<IcfgEdge> {
 				throw new IllegalArgumentException(
 						"Action with branch encoders given, but no TF with branch encoders: " + original);
 			}
+			if (original instanceof IcfgForkThreadCurrentTransition) {
+				return mEdgeBuilder.constructForkCurrentTransition((IcfgForkThreadCurrentTransition) original,
+						newTransformula, false);
+			}
 			return mEdgeBuilder.constructInternalTransition(original, original.getSource(), original.getTarget(),
-					newTransformula);
+					newTransformula, false);
 		}
 
 		if (!(original instanceof IActionWithBranchEncoders)) {

commit 371ca762dfa9c9229a491be1ccc88b01d8258af8

fix block encoding regression introduced in 7a9d4c5

trunk/source/BlockEncodingV2/src/de/uni_freiburg/informatik/ultimate/plugins/blockencoding/encoding/RewriteNotEquals.java
@@ -106,7 +106,7 @@ public final class RewriteNotEquals extends BaseBlockEncoder<IcfgLocation> {
 			if (!toRemove.add(edge)) {
 				continue;
 			}
-			final IcfgEdge newEdge = mEdgeBuilder.constructInternalTransition(edge, edge.getSource(), edge.getTarget(),
+			final IcfgEdge newEdge = mEdgeBuilder.constructAndConnectInternalTransition(edge, edge.getSource(), edge.getTarget(),
 					TransFormulaBuilder.constructCopy(mgScript, rewrittenMtf, Collections.emptySet(),
 							Collections.emptySet(), Collections.emptyMap()));
 			rememberEdgeMapping(newEdge, edge);

trunk/source/BlockEncodingV2/src/de/uni_freiburg/informatik/ultimate/plugins/blockencoding/encoding/SmallBlockEncoder.java
@@ -205,7 +205,7 @@ public class SmallBlockEncoder extends BaseBlockEncoder<IcfgLocation> {
 				newSource = createNewLocation(icfg, edge.getTarget());
 				final Term prefixTerm = SmtUtils.and(mScript, prefix);
 				final IcfgEdge newEdge =
-						mEdgeBuilder.constructInternalTransition(edge, edge.getSource(), newSource, prefixTerm);
+						mEdgeBuilder.constructAndConnectInternalTransition(edge, edge.getSource(), newSource, prefixTerm);
 				rememberEdgeMapping(newEdge, edge);
 			}
 
@@ -217,13 +217,13 @@ public class SmallBlockEncoder extends BaseBlockEncoder<IcfgLocation> {
 				newTarget = createNewLocation(icfg, edge.getTarget());
 				final Term suffixTerm = SmtUtils.and(mScript, suffix);
 				final IcfgEdge newEdge =
-						mEdgeBuilder.constructInternalTransition(edge, newTarget, edge.getTarget(), suffixTerm);
+						mEdgeBuilder.constructAndConnectInternalTransition(edge, newTarget, edge.getTarget(), suffixTerm);
 				rememberEdgeMapping(newEdge, edge);
 			}
 
 			// finally, we add the disjunctions between newsource and newtarget
 			for (final Term disjunct : disjunction) {
-				final IcfgEdge newEdge = mEdgeBuilder.constructInternalTransition(edge, newSource, newTarget, disjunct);
+				final IcfgEdge newEdge = mEdgeBuilder.constructAndConnectInternalTransition(edge, newSource, newTarget, disjunct);
 				rememberEdgeMapping(newEdge, edge);
 			}
 		}
@@ -285,7 +285,7 @@ public class SmallBlockEncoder extends BaseBlockEncoder<IcfgLocation> {
 		final IcfgLocation source = oldEdge.getSource();
 		final IcfgLocation target = oldEdge.getTarget();
 		for (final Term disjunct : disjuncts) {
-			final IcfgEdge newEdge = mEdgeBuilder.constructInternalTransition(oldEdge, source, target, disjunct);
+			final IcfgEdge newEdge = mEdgeBuilder.constructAndConnectInternalTransition(oldEdge, source, target, disjunct);
 			rememberEdgeMapping(newEdge, oldEdge);
 		}
 	}

trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/cfg/structure/IcfgEdgeBuilder.java
@@ -252,7 +252,7 @@ public class IcfgEdgeBuilder {
 		return transition instanceof IIcfgInternalTransition<?> && !(transition instanceof IIcfgSummaryTransition<?>);
 	}
 
-	public IcfgEdge constructInternalTransition(final IcfgEdge oldTransition, final IcfgLocation source,
+	public IcfgEdge constructAndConnectInternalTransition(final IcfgEdge oldTransition, final IcfgLocation source,
 			final IcfgLocation target, final Term term) {
 		assert onlyInternal(oldTransition) : "You cannot have calls or returns in normal sequential compositions";
 		final UnmodifiableTransFormula oldTf = IcfgUtils.getTransformula(oldTransition);
@@ -278,22 +278,21 @@ public class IcfgEdgeBuilder {
 		tfb.setInfeasibility(Infeasibility.NOT_DETERMINED);
 
 		final UnmodifiableTransFormula tf = tfb.finishConstruction(mManagedScript);
-		return constructInternalTransition(oldTransition, source, target, tf);
+		return constructAndConnectInternalTransition(oldTransition, source, target, tf);
+	}
+
+	public IcfgEdge constructAndConnectInternalTransition(final IcfgEdge oldTransition, final IcfgLocation source,
+			final IcfgLocation target, final UnmodifiableTransFormula tf) {
+		final IcfgEdge edge = constructInternalTransition(oldTransition, source, target, tf);
+		source.addOutgoing(edge);
+		target.addIncoming(edge);
+		return edge;
 	}
 
 	public IcfgEdge constructInternalTransition(final IcfgEdge oldTransition, final IcfgLocation source,
 			final IcfgLocation target, final UnmodifiableTransFormula tf) {
-		return constructInternalTransition(oldTransition, source, target, tf, false);
-	}
-
-	public IcfgEdge constructInternalTransition(final IcfgEdge oldTransition, final IcfgLocation source,
-			final IcfgLocation target, final UnmodifiableTransFormula tf, final boolean connect) {
 		assert onlyInternal(oldTransition) : "You cannot have calls or returns in normal sequential compositions";
 		final IcfgInternalTransition rtr = mEdgeFactory.createInternalTransition(source, target, null, tf);
-		if (connect) {
-			source.addOutgoing(rtr);
-			target.addIncoming(rtr);
-		}
 		ModelUtils.copyAnnotations(oldTransition, rtr);
 		return rtr;
 	}

trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/concurrency/IcfgCopyFactory.java
@@ -82,7 +82,7 @@ public class IcfgCopyFactory implements ICopyActionFactory<IcfgEdge> {
 						newTransformula, false);
 			}
 			return mEdgeBuilder.constructInternalTransition(original, original.getSource(), original.getTarget(),
-					newTransformula, false);
+					newTransformula);
 		}
 
 		if (!(original instanceof IActionWithBranchEncoders)) {