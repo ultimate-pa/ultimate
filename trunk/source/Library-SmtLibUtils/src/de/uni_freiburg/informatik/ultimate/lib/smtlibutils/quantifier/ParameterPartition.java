6b02e16157ebfd9eb26f26bb99b034482cf57931
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2022-04-14 22:03:06 +0200

    Use LinkedHashRelations in ParameterPartition to improve reproducibility

--- trunk/source/Library-SmtLibUtils/src/de/uni_freiburg/informatik/ultimate/lib/smtlibutils/quantifier/ParameterPartition.java
+++ trunk/source/Library-SmtLibUtils/src/de/uni_freiburg/informatik/ultimate/lib/smtlibutils/quantifier/ParameterPartition.java
@@ -1,27 +1,27 @@
 /*
  * Copyright (C) 2016 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2016 University of Freiburg
- * 
+ *
  * This file is part of the ULTIMATE ModelCheckerUtils Library.
- * 
+ *
  * The ULTIMATE ModelCheckerUtils Library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * The ULTIMATE ModelCheckerUtils Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the ULTIMATE ModelCheckerUtils Library. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Additional permission under GNU GPL version 3 section 7:
  * If you modify the ULTIMATE ModelCheckerUtils Library, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP), 
- * containing parts covered by the terms of the Eclipse Public License, the 
- * licensors of the ULTIMATE ModelCheckerUtils Library grant you additional permission 
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE ModelCheckerUtils Library grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.lib.smtlibutils.quantifier;
@@ -41,10 +41,10 @@ import de.uni_freiburg.informatik.ultimate.logic.Script;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
 import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.UnionFind;
-import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.LinkedHashRelation;
 
 /**
- * Assume we have a quantified term in which we cannot trivially push 
+ * Assume we have a quantified term in which we cannot trivially push
  * the quantifier further because we have
  * - an existentially quantified conjunction, or
  * - an universally quantified disjunction.
@@ -59,7 +59,7 @@ import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRela
  *
  */
 public class ParameterPartition {
-	
+
 	private final boolean mIsPartitionTrivial;
 	private Term mTermWithPushedQuantifier;
 
@@ -76,9 +76,9 @@ public class ParameterPartition {
 		}
 		final Term[] params = QuantifierUtils.getXjunctsInner(quantifier, appTerm);
 		final Set<TermVariable> quantifiedVars = new HashSet<>(Arrays.asList(quantifiedFormula.getVariables()));
-		
-		final HashRelation<TermVariable, Term> quantifiedVars2param = new HashRelation<>();
-		final HashRelation<Term, TermVariable> param2quantifiedVars = new HashRelation<>();
+
+		final LinkedHashRelation<TermVariable, Term> quantifiedVars2param = new LinkedHashRelation<>();
+		final LinkedHashRelation<Term, TermVariable> param2quantifiedVars = new LinkedHashRelation<>();
 		final UnionFind<Term> uf = new UnionFind<>();
 		for (final Term xjunct : params) {
 			for (final TermVariable tv : xjunct.getFreeVars()) {
@@ -89,11 +89,11 @@ public class ParameterPartition {
 			}
 			uf.makeEquivalenceClass(xjunct);
 		}
-		
+
 		for (final TermVariable tv : quantifiedVars2param.getDomain()) {
 			uf.union(quantifiedVars2param.getImage(tv));
 		}
-		
+
 		final Collection<Set<Term>> equivalenceClasses = uf.getAllEquivalenceClasses();
 		mIsPartitionTrivial = (equivalenceClasses.size() == 1);
 		if (!mIsPartitionTrivial) {
@@ -110,8 +110,8 @@ public class ParameterPartition {
 			}
 			mTermWithPushedQuantifier = QuantifierUtils.applyDualFiniteConnective(script, quantifier, resultParams);
 		}
-		
-		
+
+
 	}
 
 	public boolean isIsPartitionTrivial() {
