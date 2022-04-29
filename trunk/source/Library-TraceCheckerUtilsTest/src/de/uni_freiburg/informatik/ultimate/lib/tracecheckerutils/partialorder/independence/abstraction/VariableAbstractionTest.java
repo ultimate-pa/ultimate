901f6df7f3fce7baf17f6e022cf062ba25b9ce3a
maul.esel <maul.esel@posteo.de>
2022-04-29 17:35:04 +0200

    reduce code duplication among abstraction tests
    The common base class should also make it easier to add new tests,
    particularly tests that apply to all abstraction implementations.

--- trunk/source/Library-TraceCheckerUtilsTest/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/partialorder/independence/abstraction/VariableAbstractionTest.java
+++ trunk/source/Library-TraceCheckerUtilsTest/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/partialorder/independence/abstraction/VariableAbstractionTest.java
@@ -27,267 +27,64 @@
  */
 package de.uni_freiburg.informatik.ultimate.lib.tracecheckerutils.partialorder.independence.abstraction;
 
-import java.math.BigInteger;
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
-import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger.LogLevel;
-import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.CfgSmtToolkit;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.DefaultIcfgSymbolTable;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.abstraction.IAbstraction;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.BasicInternalAction;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.transitions.TransFormulaBuilder;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.transitions.TransFormulaUtils;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.transitions.UnmodifiableTransFormula;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.transitions.UnmodifiableTransFormula.Infeasibility;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.variables.IProgramNonOldVar;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.variables.IProgramVar;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.variables.ProgramVarUtils;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.scripttransfer.HistoryRecordingScript;
-import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.CommuhashNormalForm;
-import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.ManagedScript;
-import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.SmtSortUtils;
-import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.SmtUtils;
-import de.uni_freiburg.informatik.ultimate.logic.Logics;
-import de.uni_freiburg.informatik.ultimate.logic.QuantifiedFormula;
-import de.uni_freiburg.informatik.ultimate.logic.Script;
-import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
-import de.uni_freiburg.informatik.ultimate.logic.Sort;
-import de.uni_freiburg.informatik.ultimate.logic.Term;
-import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
-import de.uni_freiburg.informatik.ultimate.smtsolver.external.TermParseUtils;
-import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
 
-public class VariableAbstractionTest {
+public class VariableAbstractionTest extends AbstractAbstractionTestSuite<Set<IProgramVar>> {
 
-	private static final long TEST_TIMEOUT_MILLISECONDS = 10000000000000L;
-	private static final LogLevel LOG_LEVEL = LogLevel.INFO;
-	private static final String SOLVER_COMMAND = "z3 SMTLIB2_COMPLIANT=true -t:1000 -memory:2024 -smt2 -in";
-
-	private static final String PROCEDURE = "VariableAbstractionTest";
-
-	private IUltimateServiceProvider mServices;
-	private ILogger mLogger;
-	private Script mScript;
-	private ManagedScript mMgdScript;
-	private final DefaultIcfgSymbolTable mSymbolTable = new DefaultIcfgSymbolTable();
-	CfgSmtToolkit mToolkit;
-	VariableAbstraction<BasicInternalAction> mVaAbs;
-
-	private Sort mIntSort;
-
-	// variables for SimpleSet example
-	private IProgramVar x, y, a, b, sz, r1, r2, s1, s2;
-
-	// variables for ArrayStack example
-	private IProgramVar arr, max, top, e1, e2;
-
-	@Before
-	public void setUp() {
-		mServices = UltimateMocks.createUltimateServiceProviderMock(LOG_LEVEL);
-		mServices.getProgressMonitorService().setDeadline(System.currentTimeMillis() + TEST_TIMEOUT_MILLISECONDS);
-		mLogger = mServices.getLoggingService().getLogger(VariableAbstractionTest.class);
-
-		mScript = new HistoryRecordingScript(UltimateMocks.createSolver(SOLVER_COMMAND, LOG_LEVEL));
-		mMgdScript = new ManagedScript(mServices, mScript);
-		mScript.setLogic(Logics.ALL);
-		mIntSort = SmtSortUtils.getIntSort(mScript);
-
-		setupSimpleSet();
-		setupArrayStack();
-
-		mToolkit = new CfgSmtToolkit(null, mMgdScript, mSymbolTable, null, null, null, null, null, null);
-		final Set<IProgramVar> mAllVariables = new HashSet<>();
-		for (final IProgramNonOldVar nOV : mSymbolTable.getGlobals()) {
-			mAllVariables.add(nOV);
-		}
-		mVaAbs = new VariableAbstraction<>(VariableAbstractionTest::copyAction, mMgdScript, null, mAllVariables);
-
+	@Override
+	protected IAbstraction<Set<IProgramVar>, BasicInternalAction> createAbstraction() {
+		final Set<IProgramVar> allVariables = new HashSet<>(mSymbolTable.getGlobals());
+		return new VariableAbstraction<>(this::copyAction, mMgdScript, null, allVariables);
 	}
 
-	private static BasicInternalAction copyAction(final BasicInternalAction old,
-			final UnmodifiableTransFormula newTransformula, final UnmodifiableTransFormula newTransformulaWithBE) {
-		assert newTransformulaWithBE == null : "TF with branch encoders should be null";
-		return createAction(newTransformula);
-	}
-
-	private static BasicInternalAction createAction(final UnmodifiableTransFormula newTransformula) {
-		return new BasicInternalAction(PROCEDURE, PROCEDURE, newTransformula);
-	}
-
-	@After
-	public void tearDown() {
-		mScript.exit();
-	}
-
-	public UnmodifiableTransFormula yIsXPlusY() {
-		return TransFormulaBuilder.constructAssignment(List.of(y),
-				List.of(SmtUtils.sum(mScript, mIntSort, x.getTerm(), y.getTerm())), mSymbolTable, mMgdScript);
-	}
-
-	public UnmodifiableTransFormula yIsXTimesTwo() {
-		return TransFormulaBuilder.constructAssignment(List.of(y), List
-				.of(SmtUtils.mul(mScript, mIntSort, x.getTerm(), SmtUtils.constructIntValue(mScript, BigInteger.TWO))),
-				mSymbolTable, mMgdScript);
-	}
-
-	public UnmodifiableTransFormula xIsXPlusOne() {
-		return TransFormulaBuilder.constructAssignment(List.of(x), List
-				.of(SmtUtils.sum(mScript, mIntSort, x.getTerm(), SmtUtils.constructIntValue(mScript, BigInteger.ONE))),
-				mSymbolTable, mMgdScript);
-	}
-
-	public UnmodifiableTransFormula jointHavocXandY() {
-		final TermVariable aux = mMgdScript.variable("aux", mIntSort);
-		final TermVariable xOut = mMgdScript.variable("x_out", mIntSort);
-		final TermVariable yOut = mMgdScript.variable("y_out", mIntSort);
-
-		final Term formula = parseWithVariables("(and (= x_out aux) (= y_out aux))");
-		final TransFormulaBuilder tfb = new TransFormulaBuilder(null, null, true, null, true, null, false);
-		tfb.addOutVar(x, xOut);
-		tfb.addOutVar(y, yOut);
-		tfb.addAuxVar(aux);
-		tfb.setFormula(formula);
-		tfb.setInfeasibility(Infeasibility.NOT_DETERMINED);
-		return tfb.finishConstruction(mMgdScript);
-	}
+	/*
+	 * ****************************************************************************************************************
+	 * Actual test cases
+	 * ****************************************************************************************************************
+	 */
 
 	@Test
 	public void sharedInOutVar() {
-		runTestAbstraction(yIsXPlusY(), Set.of(y));
+		testAbstraction(yIsXPlusY(), Set.of(y));
 	}
 
 	@Test
 	public void rightSideAbstracted() {
 		// abstract variable on right side, but not left side
-		final Set<IProgramVar> constrVars = new HashSet<>();
-		constrVars.add(y);
-		runTestAbstraction(yIsXTimesTwo(), constrVars);
+		testAbstraction(yIsXTimesTwo(), Set.of(y));
 	}
 
 	@Test
-	public void leftSideAbstracton() {
-		final Set<IProgramVar> constrVars = new HashSet<>();
-		constrVars.add(x);
-		runTestAbstraction(yIsXTimesTwo(), constrVars);
+	public void leftSideAbstraction() {
+		testAbstraction(yIsXTimesTwo(), Set.of(x));
 	}
 
 	@Test
 	public void bothSidesDifferentVariablesEmptyConstrVars() {
-		final Set<IProgramVar> constrVars = new HashSet<>();
-		runTestAbstraction(yIsXTimesTwo(), constrVars);
+		testAbstraction(yIsXTimesTwo(), Collections.emptySet());
 	}
 
 	@Test
 	public void doNothingFullConstrVars() {
-		final Set<IProgramVar> constrVars = new HashSet<>();
-		constrVars.add(x);
-		constrVars.add(y);
-		runTestAbstractionDoesNothing(yIsXTimesTwo(), constrVars);
-		runTestAbstractionDoesNothing(xIsXPlusOne(), constrVars);
+		testAbstractionDoesNothing(yIsXTimesTwo(), Set.of(x, y));
+		testAbstractionDoesNothing(xIsXPlusOne(), Set.of(x, y));
 	}
 
 	@Test
 	public void bothSidesSameVariable() {
-		final Set<IProgramVar> constrVars = new HashSet<>();
-		runTestAbstraction(xIsXPlusOne(), constrVars);
+		testAbstraction(xIsXPlusOne(), Collections.emptySet());
 	}
 
 	@Test
 	public void withAuxVar() {
-		runTestAbstraction(jointHavocXandY(), Set.of(x));
+		testAbstraction(jointHavocXandY(), Set.of(x));
 	}
-
-	private void runTestAbstraction(final UnmodifiableTransFormula utf, final Set<IProgramVar> constrainingVars) {
-		final UnmodifiableTransFormula abstractedTF =
-				mVaAbs.abstractLetter(createAction(utf), constrainingVars).getTransformula();
-
-		for (final IProgramVar iv : abstractedTF.getInVars().keySet()) {
-			assert !abstractedTF.getAuxVars().contains(iv.getTermVariable()) : "auxVar in InVar ";
-		}
-
-		for (final IProgramVar iv : abstractedTF.getOutVars().keySet()) {
-			assert !abstractedTF.getAuxVars().contains(abstractedTF.getOutVars().get(iv)) : "auxVar in OutVar "
-					+ iv.getTermVariable().toString();
-		}
-		LBool working = TransFormulaUtils.checkImplication(utf, abstractedTF, mMgdScript);
-		assert working != LBool.SAT : "IS SAT";
-		working = TransFormulaUtils.checkImplication(abstractedTF, utf, mMgdScript);
-		assert working != LBool.UNSAT : "IS UNSAT";
-
-	}
-
-	private void runTestAbstractionDoesNothing(final UnmodifiableTransFormula utf,
-			final Set<IProgramVar> constrainingVars) {
-		final UnmodifiableTransFormula abstractedTF =
-				mVaAbs.abstractLetter(createAction(utf), constrainingVars).getTransformula();
-
-		for (final IProgramVar iv : abstractedTF.getInVars().keySet()) {
-			assert !abstractedTF.getAuxVars().contains(iv.getTermVariable()) : "auxVar in InVar ";
-		}
-
-		for (final IProgramVar iv : abstractedTF.getOutVars().keySet()) {
-			assert !abstractedTF.getAuxVars().contains(abstractedTF.getOutVars().get(iv)) : "auxVar in OutVar "
-					+ iv.getTermVariable().toString();
-		}
-		final LBool working = TransFormulaUtils.checkImplication(utf, abstractedTF, mMgdScript);
-		assert working != LBool.SAT : "IS SAT";
-
-	}
-
-	private void setupSimpleSet() {
-		// generic inputs
-		x = constructVar("x", "Int");
-		y = constructVar("y", "Int");
-
-		// data structure
-		a = constructVar("a", "Int");
-		b = constructVar("b", "Int");
-		sz = constructVar("sz", "Int");
-
-		// generic return values
-		r1 = constructVar("r1", "Bool");
-		r2 = constructVar("r2", "Bool");
-		s1 = constructVar("s1", "Int");
-		s2 = constructVar("s2", "Int");
-
-		// UINT values
-	}
-
-	private void setupArrayStack() {
-		arr = ProgramVarUtils.constructGlobalProgramVarPair("arr",
-				mScript.sort("Array", mScript.sort("Int"), mScript.sort("Int")), mMgdScript, null);
-		mSymbolTable.add(arr);
-
-		max = constructVar("max", "Int");
-		top = constructVar("top", "Int");
-		e1 = constructVar("e1", "Int");
-		e2 = constructVar("e2", "Int");
-
-	}
-
-	private IProgramVar constructVar(final String name, final String sort) {
-		final IProgramVar variable =
-				ProgramVarUtils.constructGlobalProgramVarPair(name, mScript.sort(sort), mMgdScript, null);
-		mSymbolTable.add(variable);
-		return variable;
-	}
-
-	private Term parseWithVariables(final String syntax) {
-		final String declarations = mSymbolTable.getGlobals().stream()
-				.map(pv -> "(" + pv.getTermVariable().getName() + "_in " + pv.getSort() + ") ("
-						+ pv.getTermVariable().getName() + "_out " + pv.getSort() + ") ")
-				.collect(Collectors.joining(" ")) + " (aux Int)";
-		final String fullSyntax = "(forall (" + declarations + ") " + syntax + ")";
-		final QuantifiedFormula quant = (QuantifiedFormula) TermParseUtils.parseTerm(mScript, fullSyntax);
-		return new CommuhashNormalForm(mServices, mScript).transform(quant.getSubformula());
-	}
-
 }
