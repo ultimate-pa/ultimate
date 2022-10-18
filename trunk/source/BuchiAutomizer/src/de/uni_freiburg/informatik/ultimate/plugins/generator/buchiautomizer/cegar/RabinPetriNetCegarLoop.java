293f7e309b94a2a74fd86f1fc741545495fafbd6
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2022-10-18 17:52:46 +0200

    Readd RabinPetriNetCegarLoop

---
+++ trunk/source/BuchiAutomizer/src/de/uni_freiburg/informatik/ultimate/plugins/generator/buchiautomizer/cegar/RabinPetriNetCegarLoop.java
@@ -0,0 +1,91 @@
+package de.uni_freiburg.informatik.ultimate.plugins.generator.buchiautomizer.cegar;
+
+import java.util.stream.Collectors;
+
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryException;
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
+import de.uni_freiburg.informatik.ultimate.automata.AutomataOperationCanceledException;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.INwaOutgoingLetterAndTransitionProvider;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedRun;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWord;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.buchi.NestedLassoRun;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.reachablestates.NestedWordAutomatonReachableStates;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.IRabinPetriNet;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.PetriNetLassoRun;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.PetriNetRun;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.operations.DifferenceRabin;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.unfolding.IsEmptyRabin;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IIcfg;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IIcfgTransition;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.predicates.IPredicate;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.predicates.PredicateFactory;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.buchiautomizer.BuchiCegarLoopBenchmarkGenerator;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.buchiautomizer.RankVarConstructor;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.preferences.TAPreferences;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.preferences.TraceAbstractionPreferenceInitializer.Minimization;
+
+/**
+ *
+ * @author Frank Schüssele (schuessf@informatik.uni-freiburg.de)
+ *
+ */
+public class RabinPetriNetCegarLoop<L extends IIcfgTransition<?>>
+		extends AbstractBuchiCegarLoop<L, IRabinPetriNet<L, IPredicate>> {
+	private final Marking2MLPredicate mMarking2MLPredicate;
+
+	public RabinPetriNetCegarLoop(final IIcfg<?> icfg, final RankVarConstructor rankVarConstructor,
+			final PredicateFactory predicateFactory, final TAPreferences taPrefs,
+			final IUltimateServiceProvider services, final Class<L> transitionClazz,
+			final IRabinPetriNet<L, IPredicate> initialAbstraction,
+			final BuchiCegarLoopBenchmarkGenerator benchmarkGenerator) {
+		super(icfg, rankVarConstructor, predicateFactory, taPrefs, services, transitionClazz, initialAbstraction,
+				benchmarkGenerator);
+		mMarking2MLPredicate = new Marking2MLPredicate(predicateFactory);
+	}
+
+	@Override
+	protected boolean isAbstractionEmpty(final IRabinPetriNet<L, IPredicate> abstraction)
+			throws AutomataLibraryException {
+		final var isempty = new IsEmptyRabin<>(new AutomataLibraryServices(mServices), abstraction, mPref.eventOrder(),
+				mPref.cutOffRequiresSameTransition(), true);
+		final PetriNetLassoRun<L, IPredicate> run = isempty.getRun();
+		if (run == null) {
+			return true;
+		}
+		mCounterexample =
+				new NestedLassoRun<>(constructNestedLassoRun(run.getStem()), constructNestedLassoRun(run.getLoop()));
+		return false;
+	}
+
+	private NestedRun<L, IPredicate> constructNestedLassoRun(final PetriNetRun<L, IPredicate> run) {
+		return new NestedRun<>(NestedWord.nestedWord(run.getWord()), run.getStateSequence().stream()
+				.map(mMarking2MLPredicate::markingToPredicate).collect(Collectors.toList()));
+	}
+
+	@Override
+	protected IRabinPetriNet<L, IPredicate> refineFinite(final IRabinPetriNet<L, IPredicate> abstraction,
+			final INwaOutgoingLetterAndTransitionProvider<L, IPredicate> interpolantAutomaton)
+			throws AutomataLibraryException {
+		// TODO: This is sound, but more complicated than necessary!
+		// We need something like DifferencePairwiseOnDemand for IRabinPetriNet.
+		return refineBuchi(abstraction, interpolantAutomaton);
+	}
+
+	@Override
+	protected IRabinPetriNet<L, IPredicate> refineBuchi(final IRabinPetriNet<L, IPredicate> abstraction,
+			final INwaOutgoingLetterAndTransitionProvider<L, IPredicate> interpolantAutomaton)
+			throws AutomataLibraryException {
+		final DifferenceRabin<L, IPredicate> difference = new DifferenceRabin<>(abstraction,
+				new NestedWordAutomatonReachableStates<>(new AutomataLibraryServices(mServices), interpolantAutomaton),
+				new AutomataLibraryServices(mServices));
+		return difference.getResult();
+	}
+
+	@Override
+	protected IRabinPetriNet<L, IPredicate> reduceAbstractionSize(final IRabinPetriNet<L, IPredicate> abstraction,
+			final Minimization automataMinimization) throws AutomataOperationCanceledException {
+		return abstraction;
+	}
+
+}
