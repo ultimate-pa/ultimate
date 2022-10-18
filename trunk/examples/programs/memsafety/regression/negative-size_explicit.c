8fe4475245cee4aca29f082196d8ff095ced782b
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2022-10-18 10:28:27 +0200

    Another test

---
+++ trunk/examples/programs/memsafety/regression/negative-size_explicit.c
@@ -0,0 +1,19 @@
+//# Safe
+/*
+  Author: Frank Schüssele (schuessf@informatik.uni-freiburg.de)
+  Date: 2022-10-18
+*/
+
+typedef long unsigned int size_t;
+
+void * __attribute__((__cdecl__)) malloc (size_t __size) ;
+
+extern void abort(void);
+void assume_abort_if_not(int cond) {
+  if(!cond) {abort();}
+}
+
+int main() {
+  int *a = malloc(-1);
+  int x = a[0];
+}
