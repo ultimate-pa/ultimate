ultimate-pa/ultimate

commit dcf21767ec88494db73e0e82cec01c9e2f1e340c

IIndependenceRelation::isIndependent: use enum as return value
This allows relations to explicitly return UNKNOWN.

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/independence/CachedIndependenceRelation.java
@@ -27,7 +27,6 @@
 package de.uni_freiburg.informatik.ultimate.automata.partialorder.independence;
 
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IndependenceResultAggregator.Counter;
-import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
 import de.uni_freiburg.informatik.ultimate.util.statistics.AbstractStatisticsDataProvider;
 import de.uni_freiburg.informatik.ultimate.util.statistics.IStatisticsDataProvider;
 
@@ -94,30 +93,24 @@ public class CachedIndependenceRelation<S, L> implements IIndependenceRelation<S
 	}
 
 	@Override
-	public boolean isIndependent(final S state, final L a, final L b) {
+	public Dependence isIndependent(final S state, final L a, final L b) {
 		final S condition = isConditional() ? state : null;
 
-		final LBool cached = mCache.contains(condition, a, b);
-		if (cached == LBool.SAT) {
-			mStatistics.reportPositiveCachedQuery(condition != null);
-			return true;
-		} else if (cached == LBool.UNSAT) {
-			mStatistics.reportNegativeCachedQuery(condition != null);
-			return false;
+		final Dependence cached = mCache.contains(condition, a, b);
+		if (cached != null) {
+			mStatistics.reportCachedQuery(cached, condition != null);
+			return cached;
 		}
 
 		if (isSymmetric()) {
-			final LBool symCached = mCache.contains(condition, b, a);
-			if (symCached == LBool.SAT) {
-				mStatistics.reportPositiveCachedQuery(condition != null);
-				return true;
-			} else if (symCached == LBool.UNSAT) {
-				mStatistics.reportNegativeCachedQuery(condition != null);
-				return false;
+			final Dependence symCached = mCache.contains(condition, b, a);
+			if (symCached != null) {
+				mStatistics.reportCachedQuery(symCached, condition != null);
+				return cached;
 			}
 		}
 
-		final boolean result = mUnderlying.isIndependent(condition, a, b);
+		final Dependence result = mUnderlying.isIndependent(condition, a, b);
 		mCache.cacheResult(condition, a, b, result);
 		mStatistics.reportUncachedQuery(result, condition != null);
 		return result;
@@ -150,17 +143,12 @@ public class CachedIndependenceRelation<S, L> implements IIndependenceRelation<S
 			forward(CACHE_STATISTICS, mCache::getStatistics);
 		}
 
-		private void reportPositiveCachedQuery(final boolean conditional) {
-			reportPositiveQuery(conditional);
-			mCacheQueries.increment(true, conditional);
+		private void reportCachedQuery(final Dependence result, final boolean conditional) {
+			reportQuery(result, conditional);
+			mCacheQueries.increment(result, conditional);
 		}
 
-		private void reportNegativeCachedQuery(final boolean conditional) {
-			reportNegativeQuery(conditional);
-			mCacheQueries.increment(false, conditional);
-		}
-
-		private void reportUncachedQuery(final boolean result, final boolean conditional) {
+		private void reportUncachedQuery(final Dependence result, final boolean conditional) {
 			reportQuery(result, conditional);
 			mCacheQueries.incrementUnknown(conditional);
 		}
@@ -186,10 +174,9 @@ public class CachedIndependenceRelation<S, L> implements IIndependenceRelation<S
 		 *            The first letter
 		 * @param b
 		 *            The second letter
-		 * @return SAT if the letters are known to be independent (possibly under the given condition), UNSAT if they
-		 *         are known to be dependent, and UNKNOWN otherwise.
+		 * @return The cached result of an independence check, or {@code null} if no result has been cached.
 		 */
-		LBool contains(S condition, L a, L b);
+		Dependence contains(S condition, L a, L b);
 
 		/**
 		 * Remove all independence and dependence information involving a given letter.
@@ -208,10 +195,10 @@ public class CachedIndependenceRelation<S, L> implements IIndependenceRelation<S
 		 *            The first letter
 		 * @param b
 		 *            The second letter
-		 * @param independent
-		 *            Whether or not the given letters are independent
+		 * @param result
+		 *            The result of an independence check
 		 */
-		void cacheResult(S condition, L a, L b, boolean independent);
+		void cacheResult(S condition, L a, L b, Dependence result);
 
 		/**
 		 * Merges cached independencies for two letters into a combined letter. If both are independent from some third

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/LiptonReduction.java
@@ -42,6 +42,7 @@ import de.uni_freiburg.informatik.ultimate.automata.AutomataOperationCanceledExc
 import de.uni_freiburg.informatik.ultimate.automata.LibraryIdentifiers;
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.CachedIndependenceRelation;
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation.Dependence;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.PetriNetNot1SafeException;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.netdatastructures.BoundedPetriNet;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.netdatastructures.Transition;
@@ -449,7 +450,8 @@ public class LiptonReduction<L, P> {
 	private boolean isLeftMover(final Transition<L, P> t1) {
 		final Set<L> coEnabledTransitions = mCoEnabledRelation.getImage(t1.getSymbol());
 		mStatistics.reportMoverChecks(coEnabledTransitions.size());
-		return coEnabledTransitions.stream().allMatch(t2 -> mMoverCheck.isIndependent(null, t2, t1.getSymbol()));
+		return coEnabledTransitions.stream()
+				.allMatch(t2 -> mMoverCheck.isIndependent(null, t2, t1.getSymbol()) == Dependence.INDEPENDENT);
 	}
 
 	/**
@@ -462,7 +464,8 @@ public class LiptonReduction<L, P> {
 	private boolean isRightMover(final Transition<L, P> t1) {
 		final Set<L> coEnabledTransitions = mCoEnabledRelation.getImage(t1.getSymbol());
 		mStatistics.reportMoverChecks(coEnabledTransitions.size());
-		return coEnabledTransitions.stream().allMatch(t2 -> mMoverCheck.isIndependent(null, t1.getSymbol(), t2));
+		return coEnabledTransitions.stream()
+				.allMatch(t2 -> mMoverCheck.isIndependent(null, t1.getSymbol(), t2) == Dependence.INDEPENDENT);
 	}
 
 	public BoundedPetriNet<L, P> getResult() {

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/MinimalSleepSetReduction.java
@@ -39,6 +39,7 @@ import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.Outgo
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingInternalTransition;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingReturnTransition;
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation.Dependence;
 import de.uni_freiburg.informatik.ultimate.automata.statefactory.IStateFactory;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.DataStructureUtils;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.ImmutableSet;
@@ -163,7 +164,8 @@ public class MinimalSleepSetReduction<L, S, R> implements INwaOutgoingLetterAndT
 		// TODO factor out sleep set successor computation
 		final ImmutableSet<L> succSleepSet =
 				ImmutableSet.of((Set<L>) Set.of(Stream.concat(currentSleepSet.stream(), explored)
-						.filter(l -> mIndependence.isIndependent(currentState, letter, l)).toArray()));
+						.filter(l -> mIndependence.isIndependent(currentState, letter, l) == Dependence.INDEPENDENT)
+						.toArray()));
 
 		final R succSleepSetState =
 				mStateFactory.createSleepSetState(currentTransitionOpt.get().getSucc(), succSleepSet);

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/SleepSetDelayReduction.java
@@ -42,6 +42,7 @@ import de.uni_freiburg.informatik.ultimate.automata.AutomataOperationCanceledExc
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.INwaOutgoingLetterAndTransitionProvider;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomataUtils;
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation.Dependence;
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.visitors.IDfsVisitor;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.DataStructureUtils;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.ImmutableSet;
@@ -218,8 +219,9 @@ public class SleepSetDelayReduction<L, S, R> {
 
 				final S succState = currentTransition.getSucc();
 				// TODO factor out sleep set successor computation
-				final ImmutableSet<L> succSleepSet = Stream.concat(currentSleepSet.stream(), explored.stream())
-						.filter(l -> mIndependenceRelation.isIndependent(currentRedState, currentLetter, l))
+				final ImmutableSet<L> succSleepSet = Stream
+						.concat(currentSleepSet.stream(), explored.stream()).filter(l -> mIndependenceRelation
+								.isIndependent(currentRedState, currentLetter, l) == Dependence.INDEPENDENT)
 						.collect(ImmutableSet.collector());
 				final R successor = getReductionState(succState, succSleepSet);
 

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/SleepSetNewStateReduction.java
@@ -40,6 +40,7 @@ import de.uni_freiburg.informatik.ultimate.automata.AutomataOperationCanceledExc
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.INwaOutgoingLetterAndTransitionProvider;
 import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomataUtils;
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation.Dependence;
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.visitors.IDfsVisitor;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.DataStructureUtils;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.ImmutableSet;
@@ -159,8 +160,9 @@ public class SleepSetNewStateReduction<L, S, R> {
 
 				final S succState = currentTransition.getSucc();
 				// TODO factor out sleep set successor computation
-				final ImmutableSet<L> succSleepSet = Stream.concat(currentSleepSet.stream(), explored.stream())
-						.filter(l -> mIndependenceRelation.isIndependent(currentState, currentLetter, l))
+				final ImmutableSet<L> succSleepSet = Stream
+						.concat(currentSleepSet.stream(), explored.stream()).filter(l -> mIndependenceRelation
+								.isIndependent(currentState, currentLetter, l) == Dependence.INDEPENDENT)
 						.collect(ImmutableSet.collector());
 				final R succSleepSetState = getSleepSetState(succState, succSleepSet);
 

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/independence/ConditionTransformingIndependenceRelation.java
@@ -82,9 +82,9 @@ public class ConditionTransformingIndependenceRelation<S, T, L> implements IInde
 	}
 
 	@Override
-	public boolean isIndependent(final S state, final L a, final L b) {
+	public Dependence isIndependent(final S state, final L a, final L b) {
 		final T condition = state == null ? null : mTransformer.apply(state);
-		final boolean result = mUnderlying.isIndependent(condition, a, b);
+		final Dependence result = mUnderlying.isIndependent(condition, a, b);
 		mStatistics.reportQuery(result, condition != null);
 		return result;
 	}

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/independence/DefaultIndependenceCache.java
@@ -31,7 +31,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.CachedIndependenceRelation.IIndependenceCache;
-import de.uni_freiburg.informatik.ultimate.logic.Script.LBool;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation.Dependence;
 import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
 import de.uni_freiburg.informatik.ultimate.util.statistics.AbstractStatisticsDataProvider;
 import de.uni_freiburg.informatik.ultimate.util.statistics.IStatisticsDataProvider;
@@ -52,28 +52,34 @@ public class DefaultIndependenceCache<S, L> implements IIndependenceCache<S, L> 
 	private final CacheStatistics mStatistics = new CacheStatistics();
 	private final Map<S, HashRelation<L, L>> mPositiveCache = new HashMap<>();
 	private final Map<S, HashRelation<L, L>> mNegativeCache = new HashMap<>();
+	private final Map<S, HashRelation<L, L>> mUnknownCache = new HashMap<>();
 
 	@Override
-	public LBool contains(final S condition, final L a, final L b) {
+	public Dependence contains(final S condition, final L a, final L b) {
 		if (condition != null) {
 			// For conditional queries, check unconditional independence first.
 			final HashRelation<L, L> globalPositive = mPositiveCache.get(null);
 			if (globalPositive != null && globalPositive.containsPair(a, b)) {
-				return LBool.SAT;
+				return Dependence.INDEPENDENT;
 			}
 		}
 
 		final HashRelation<L, L> positive = mPositiveCache.get(condition);
 		if (positive != null && positive.containsPair(a, b)) {
-			return LBool.SAT;
+			return Dependence.INDEPENDENT;
 		}
 
 		final HashRelation<L, L> negative = mNegativeCache.get(condition);
 		if (negative != null && negative.containsPair(a, b)) {
-			return LBool.UNSAT;
+			return Dependence.DEPENDENT;
 		}
 
-		return LBool.UNKNOWN;
+		final HashRelation<L, L> unknown = mUnknownCache.get(condition);
+		if (unknown != null && unknown.containsPair(a, b)) {
+			return Dependence.UNKNOWN;
+		}
+
+		return null;
 	}
 
 	@Override
@@ -89,10 +95,22 @@ public class DefaultIndependenceCache<S, L> implements IIndependenceCache<S, L> 
 	}
 
 	@Override
-	public void cacheResult(final S condition, final L a, final L b, final boolean independent) {
-		final Map<S, HashRelation<L, L>> cache = independent ? mPositiveCache : mNegativeCache;
+	public void cacheResult(final S condition, final L a, final L b, final Dependence result) {
+		final Map<S, HashRelation<L, L>> cache = getCache(result);
 		final HashRelation<L, L> row = cache.computeIfAbsent(condition, x -> new HashRelation<>());
 		row.addPair(a, b);
+	}
+
+	private Map<S, HashRelation<L, L>> getCache(final Dependence result) {
+		switch (result) {
+		case DEPENDENT:
+			return mNegativeCache;
+		case INDEPENDENT:
+			return mPositiveCache;
+		case UNKNOWN:
+			return mUnknownCache;
+		}
+		throw new IllegalArgumentException();
 	}
 
 	@Override
@@ -126,6 +144,9 @@ public class DefaultIndependenceCache<S, L> implements IIndependenceCache<S, L> 
 		public static final String NEGATIVE_CACHE_SIZE = "Negative cache size";
 		public static final String NEGATIVE_CONDITIONAL_CACHE_SIZE = "Negative conditional cache size";
 		public static final String NEGATIVE_UNCONDITIONAL_CACHE_SIZE = "Negative unconditional cache size";
+		public static final String UNKNOWN_CACHE_SIZE = "Unknown cache size";
+		public static final String UNKNOWN_CONDITIONAL_CACHE_SIZE = "Unknown conditional cache size";
+		public static final String UNKNOWN_UNCONDITIONAL_CACHE_SIZE = "Unknown unconditional cache size";
 
 		private CacheStatistics() {
 			declare(TOTAL_CACHE_SIZE, this::getTotalSize, KeyType.COUNTER);
@@ -135,6 +156,9 @@ public class DefaultIndependenceCache<S, L> implements IIndependenceCache<S, L> 
 			declare(NEGATIVE_CACHE_SIZE, this::getNegativeCacheSize, KeyType.COUNTER);
 			declare(NEGATIVE_CONDITIONAL_CACHE_SIZE, this::getNegativeConditionalCacheSize, KeyType.COUNTER);
 			declare(NEGATIVE_UNCONDITIONAL_CACHE_SIZE, this::getNegativeUnconditionalCacheSize, KeyType.COUNTER);
+			declare(UNKNOWN_CACHE_SIZE, this::getUnknownCacheSize, KeyType.COUNTER);
+			declare(UNKNOWN_CONDITIONAL_CACHE_SIZE, this::getUnknownConditionalCacheSize, KeyType.COUNTER);
+			declare(UNKNOWN_UNCONDITIONAL_CACHE_SIZE, this::getUnknownUnconditionalCacheSize, KeyType.COUNTER);
 		}
 
 		public int getTotalSize() {
@@ -163,6 +187,18 @@ public class DefaultIndependenceCache<S, L> implements IIndependenceCache<S, L> 
 
 		public int getNegativeUnconditionalCacheSize() {
 			return getUnconditionalCacheSize(mNegativeCache);
+		}
+
+		public int getUnknownCacheSize() {
+			return getCacheSize(mUnknownCache);
+		}
+
+		public int getUnknownConditionalCacheSize() {
+			return getUnknownCacheSize() - getUnknownUnconditionalCacheSize();
+		}
+
+		public int getUnknownUnconditionalCacheSize() {
+			return getUnconditionalCacheSize(mUnknownCache);
 		}
 
 		private int getCacheSize(final Map<S, HashRelation<L, L>> cache) {

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/independence/DisjunctiveConditionalIndependenceRelation.java
@@ -73,9 +73,9 @@ public class DisjunctiveConditionalIndependenceRelation<L, S, C extends Collecti
 	}
 
 	@Override
-	public boolean isIndependent(final C state, final L a, final L b) {
+	public Dependence isIndependent(final C state, final L a, final L b) {
 		if (state == null || state.isEmpty()) {
-			final boolean result = mUnderlying.isIndependent(null, a, b);
+			final Dependence result = mUnderlying.isIndependent(null, a, b);
 			mStatistics.reportQuery(result, false);
 			return result;
 		}
@@ -83,15 +83,15 @@ public class DisjunctiveConditionalIndependenceRelation<L, S, C extends Collecti
 		int i = 0;
 		for (final S condition : state) {
 			mStatistics.reportQueriedIndex(i);
-			if (mUnderlying.isIndependent(condition, a, b)) {
+			if (mUnderlying.isIndependent(condition, a, b) == Dependence.INDEPENDENT) {
 				mStatistics.reportPositiveQuery(true);
-				return true;
+				return Dependence.INDEPENDENT;
 			}
 			i++;
 		}
 
 		mStatistics.reportNegativeQuery(true);
-		return false;
+		return Dependence.INDEPENDENT;
 	}
 
 	@Override

commit db84740eb58c1691af00bd41710b81a79cfbc74c

fix bug (copy-paste error) introduced in dcf21767

trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/independence/CachedIndependenceRelation.java
@@ -106,7 +106,7 @@ public class CachedIndependenceRelation<S, L> implements IIndependenceRelation<S
 			final Dependence symCached = mCache.contains(condition, b, a);
 			if (symCached != null) {
 				mStatistics.reportCachedQuery(symCached, condition != null);
-				return cached;
+				return symCached;
 			}
 		}
 