73cf9756a681e6c98f331275ec72ac0453d05c67
maul.esel <maul.esel@posteo.de>
2023-02-15 22:42:57 +0100

    setup tests for DPOR

---
+++ trunk/source/Library-AutomataTest/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/DynamicPORTests.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2021 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2021 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.partialorder;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.nio.file.Path;
+import java.util.Comparator;
+
+import org.junit.runner.RunWith;
+
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryException;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomataUtils;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.visitors.AutomatonConstructingVisitor;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.visitors.IDfsVisitor;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AST.AutomataTestFileAST;
+import de.uni_freiburg.informatik.ultimate.test.junitextension.testfactory.FactoryTestRunner;
+
+@RunWith(FactoryTestRunner.class)
+public class DynamicPORTests extends DynamicPORTestsBase {
+
+	@Override
+	protected void runTest(final Path path, final AutomataTestFileAST ast,
+			final NestedWordAutomaton<String, String> input, final NestedWordAutomaton<String, String> expected,
+			final IIndependenceRelation<?, String> independence) throws AutomataLibraryException {
+		final var constructor = new AutomatonConstructingVisitor<>(input, mAutomataServices, () -> "###empty###");
+
+		// TODO wrap constructor in DynamicPORVisitor, or otherwise modify the code to apply DPOR.
+		final IDfsVisitor<String, String> visitor = constructor;
+
+		DepthFirstTraversal.traverse(mAutomataServices, input, new AlphabeticOrder<>(), visitor);
+		final NestedWordAutomaton<String, String> actual = constructor.getReductionAutomaton();
+
+		assertThat("Automata differ, expected " + expected + " but actual was " + actual, areEqual(expected, actual));
+	}
+
+	private boolean areEqual(final NestedWordAutomaton<String, String> first,
+			final NestedWordAutomaton<String, String> second) {
+		if ((first == null) != (second == null)) {
+			mLogger.error("only one automata is null");
+			return false;
+		}
+
+		assert NestedWordAutomataUtils.isFiniteAutomaton(first);
+		assert NestedWordAutomataUtils.isFiniteAutomaton(second);
+
+		if (!first.getAlphabet().equals(second.getAlphabet())) {
+			mLogger.error("alphabet differs: %s != %s", first.getAlphabet(), second.getAlphabet());
+			return false;
+		}
+
+		if (!first.getStates().equals(second.getStates())) {
+			mLogger.error("states differ");
+			return false;
+		}
+
+		if (!first.getInitialStates().equals(second.getInitialStates())) {
+			mLogger.error("initial states differ");
+			return false;
+		}
+
+		if (!first.getFinalStates().equals(second.getFinalStates())) {
+			mLogger.error("accepting states differ");
+			return false;
+		}
+
+		for (final var state : first.getStates()) {
+			if (!first.lettersInternal(state).equals(second.lettersInternal(state))) {
+				mLogger.error("outgoing letters for state %s differ", state);
+				return false;
+			}
+
+			for (final var letter : first.lettersInternal(state)) {
+				if (!first.succInternal(state, letter).equals(second.succInternal(state, letter))) {
+					mLogger.error("successors for state %s under letter %s differ", state, letter);
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	private class AlphabeticOrder<S> implements IDfsOrder<String, S> {
+		@Override
+		public Comparator<String> getOrder(final S state) {
+			return Comparator.naturalOrder();
+		}
+
+		@Override
+		public boolean isPositional() {
+			return false;
+		}
+	}
+}
---
+++ trunk/source/Library-AutomataTest/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/DynamicPORTestsBase.java
@@ -0,0 +1,221 @@
+/*
+ * Copyright (C) 2023 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2023 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.partialorder;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryException;
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.netdatastructures.BoundedPetriNet;
+import de.uni_freiburg.informatik.ultimate.core.model.results.IResultWithSeverity.Severity;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.automatascriptinterpreter.AutomataDefinitionInterpreter;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.automatascriptinterpreter.IMessagePrinter;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.automatascriptinterpreter.TestFileInterpreter.LoggerSeverity;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AtsASTNode;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AutomataScriptParserRun;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AST.AutomataTestFileAST;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AST.NestedwordAutomatonAST;
+import de.uni_freiburg.informatik.ultimate.test.junitextension.testfactory.FactoryTestMethod;
+import de.uni_freiburg.informatik.ultimate.test.junitextension.testfactory.TestFactory;
+import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
+import de.uni_freiburg.informatik.ultimate.test.util.TestUtil;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
+
+/**
+ * Base class for Dynamic POR tests. Callers must only implemented the method
+ * {@link #runTest(Path, AutomataTestFileAST, BoundedPetriNet, BoundedPetriNet, IIndependenceRelation)}.
+ *
+ * @author Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ *
+ */
+public abstract class DynamicPORTestsBase implements IMessagePrinter {
+	protected IUltimateServiceProvider mServices;
+	protected AutomataLibraryServices mAutomataServices;
+	protected ILogger mLogger;
+	protected AutomataDefinitionInterpreter mInterpreter;
+
+	protected abstract void runTest(final Path path, AutomataTestFileAST ast, NestedWordAutomaton<String, String> input,
+			NestedWordAutomaton<String, String> expected, IIndependenceRelation<?, String> independence)
+			throws AutomataLibraryException;
+
+	@Before
+	public void setUp() {
+		mServices = UltimateMocks.createUltimateServiceProviderMock();
+		mAutomataServices = new AutomataLibraryServices(mServices);
+		mLogger = mServices.getLoggingService().getLogger(getClass());
+		mInterpreter = new AutomataDefinitionInterpreter(this, mLogger, mServices);
+	}
+
+	@TestFactory
+	public Iterable<TestCase> createTests() throws IOException {
+		final Path dir = Path.of(TestUtil.getPathFromTrunk("examples/Automata/dpor/"));
+		try (var files = Files.list(dir)) {
+			return files.map(TestCase::new).sorted().collect(Collectors.toList());
+		}
+	}
+
+	private final class TestCase implements Comparable<TestCase> {
+		private final Path mPath;
+
+		public TestCase(final Path path) {
+			mPath = path;
+		}
+
+		@FactoryTestMethod
+		public void run() throws AutomataLibraryException, IOException {
+			runTestInternal(mPath);
+		}
+
+		@Override
+		public String toString() {
+			return mPath.getFileName().toString();
+		}
+
+		@Override
+		public int compareTo(final TestCase other) {
+			return mPath.compareTo(other.mPath);
+		}
+	}
+
+	private void runTestInternal(final Path path) throws AutomataLibraryException, IOException {
+		final AutomataTestFileAST parsed = parse(path);
+
+		NestedwordAutomatonAST inputAST = null;
+		NestedwordAutomatonAST expectedAST = null;
+		for (final var def : parsed.getAutomataDefinitions().getListOfAutomataDefinitions()) {
+			if ("input".equals(def.getName())) {
+				assert inputAST == null : "multiple inputs specified";
+				inputAST = (NestedwordAutomatonAST) def;
+			} else if ("expected".equals(def.getName())) {
+				assert expectedAST == null : "multiple expected outputs specified";
+				expectedAST = (NestedwordAutomatonAST) def;
+			}
+		}
+		assert inputAST != null && expectedAST != null : "either input or expected is missing";
+
+		mInterpreter.interpret(inputAST);
+		mInterpreter.interpret(expectedAST);
+
+		final var input = (NestedWordAutomaton<String, String>) mInterpreter.getAutomata().get("input");
+		final var expected = (NestedWordAutomaton<String, String>) mInterpreter.getAutomata().get("expected");
+		assert input != null && expected != null : "either input or expected is missing";
+
+		final HashIndependence indep = new HashIndependence(extractCommutativity(path));
+		runTest(path, parsed, input, expected, indep);
+	}
+
+	@Override
+	public void printMessage(final Severity severityForResult, final LoggerSeverity severityForLogger,
+			final String longDescr, final String shortDescr, final AtsASTNode node) {
+		// TODO
+		mLogger.warn(longDescr);
+	}
+
+	private AutomataTestFileAST parse(final Path path) throws FileNotFoundException {
+		final String filename = path.getFileName().toString();
+		final Reader reader = new BufferedReader(new FileReader(path.toFile()));
+		return new AutomataScriptParserRun(mServices, mLogger, reader, filename, path.toString()).getResult();
+	}
+
+	private HashRelation<String, String> extractCommutativity(final Path path) throws IOException {
+		final String prefix = "//@ commutativity ";
+
+		final Optional<String> commLine;
+		try (final var lines = Files.lines(path)) {
+			commLine = lines.filter(l -> l.startsWith(prefix)).findFirst();
+		}
+
+		final HashRelation<String, String> result = new HashRelation<>();
+		if (!commLine.isPresent()) {
+			mLogger.info("no commutativity specification found");
+			return result;
+		}
+
+		final String relDescr = commLine.get().substring(prefix.length());
+		final Pattern pairPattern = Pattern.compile("\\s*\\(([^,]+),([^\\)]+)\\)");
+		final Matcher matcher = pairPattern.matcher(relDescr);
+		while (matcher.find()) {
+			final String left = matcher.group(1).strip();
+			final String right = matcher.group(2).strip();
+			result.addPair(left, right);
+		}
+
+		mLogger.info("commutativity: " + result.getSetOfPairs());
+		return result;
+	}
+
+	private static final class HashIndependence implements IIndependenceRelation<Set<String>, String> {
+		private final HashRelation<String, String> mRelation;
+		private final boolean mSymmetric;
+
+		public HashIndependence(final HashRelation<String, String> relation) {
+			mRelation = relation;
+			mSymmetric = isSymmetric(relation);
+		}
+
+		private static boolean isSymmetric(final HashRelation<String, String> relation) {
+			for (final var pair : relation) {
+				if (!relation.containsPair(pair.getValue(), pair.getKey())) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public boolean isSymmetric() {
+			return mSymmetric;
+		}
+
+		@Override
+		public boolean isConditional() {
+			return false;
+		}
+
+		@Override
+		public Dependence isIndependent(final Set<String> state, final String a, final String b) {
+			return mRelation.containsPair(a, b) ? Dependence.INDEPENDENT : Dependence.DEPENDENT;
+		}
+	}
+}
