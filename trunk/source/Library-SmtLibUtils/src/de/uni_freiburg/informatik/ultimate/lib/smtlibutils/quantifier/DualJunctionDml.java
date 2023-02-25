6bc870cfafdd8ffd500c26a9d42f3de7b4f45bb6
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2023-02-25 23:36:58 +0100

    Apply abs() immediately

--- trunk/source/Library-SmtLibUtils/src/de/uni_freiburg/informatik/ultimate/lib/smtlibutils/quantifier/DualJunctionDml.java
+++ trunk/source/Library-SmtLibUtils/src/de/uni_freiburg/informatik/ultimate/lib/smtlibutils/quantifier/DualJunctionDml.java
@@ -145,7 +145,7 @@ public class DualJunctionDml extends DualJunctionQuantifierElimination {
 						final Term subtermWithModDiv = subterm;
 						final TermVariable eliminate = eliminatee;
 						if ((divisorAsBigInteger.gcd(aAsBigInteger)).equals(BigInteger.valueOf(1))) {
-							inverse = ArithmeticUtils.multiplicativeInverse(aAsBigInteger, divisorAsBigInteger);
+							inverse = ArithmeticUtils.multiplicativeInverse(aAsBigInteger, divisorAsBigInteger.abs());
 							final DmlPossibility dmlPossibility = new DmlPossibility(
 									appTerm.getFunction().getApplicationString(), aAsBigInteger, bAsTerm,
 									divisorAsBigInteger, modDivJunct, inverse, subtermWithModDiv, eliminate);
