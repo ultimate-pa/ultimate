87f76329fc2fa7f5cf31e07e78219cde8976a4de
Lena Funk <yungfunk@gmx.de>
2023-03-06 11:53:26 +0100

    Added test RangeDecisionTest to test my implementation of filterCdd() and strict()

---
+++ trunk/source/Library-PEATest/src/de/uni_freiburg/informatik/ultimate/lib/pea/test/RangeDecisionTest.java
@@ -0,0 +1,58 @@
+package de.uni_freiburg.informatik.ultimate.lib.pea.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Test class for the functions filterCDD() and strict() in the class RangeDecision 
+ * implemented by me as part of my bachelors Thesis.
+ *
+ * @author Lena Funk
+ */
+
+@RunWith(JUnit4.class)
+public class RangeDecisionTest {
+	
+	public RangeDecisionTest() {
+		
+	}
+	
+	public void createTestCDDs() {
+		
+	}
+	
+	
+	/**
+	 * Test extreme case 1: 
+	 */
+	@Test
+	public void removeConstraintTest1() {
+		
+	}
+	
+	/**
+	 * Test extreme case 1: 
+	 */
+	@Test
+	public void removeConstraintTest2() {
+		
+	}
+	
+	/**
+	 * Test "normal" case: 
+	 */
+	@Test
+	public void removeConstraintTest3() {
+		
+	}
+	
+	/**
+	 * Test "crazy" case: 
+	 */
+	@Test
+	public void removeConstraintTest4() {
+		
+	}
+
+}
