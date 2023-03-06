dd99d38b60fbf5b8f8945b800612889a091e6db8
Lena Funk <yungfunk@gmx.de>
2023-03-06 17:48:24 +0100

    implemented getDecisions() in Class CDD, implemented Test cases, fixed bugs

--- trunk/source/Library-PEATest/src/de/uni_freiburg/informatik/ultimate/lib/pea/test/RangeDecisionTest.java
+++ trunk/source/Library-PEATest/src/de/uni_freiburg/informatik/ultimate/lib/pea/test/RangeDecisionTest.java
@@ -1,8 +1,14 @@
 package de.uni_freiburg.informatik.ultimate.lib.pea.test;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import de.uni_freiburg.informatik.ultimate.lib.pea.CDD;
+import de.uni_freiburg.informatik.ultimate.lib.pea.Decision;
+import de.uni_freiburg.informatik.ultimate.lib.pea.RangeDecision;
 
 /**
  * Test class for the functions filterCDD() and strict() in the class RangeDecision 
@@ -13,29 +19,40 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class RangeDecisionTest {
+	ArrayList<CDD> mTestCdds;
 	
 	public RangeDecisionTest() {
+		createTestCDDs();
 		
 	}
 	
 	public void createTestCDDs() {
+		// c1 <= 5
+		CDD c1 = RangeDecision.create("c1", RangeDecision.OP_LTEQ, 5);
+		mTestCdds.add(c1);
+		RangeDecision decision = (RangeDecision) c1.getDecision();
 		
-	}
+		
+		// c2 >= 5
+		CDD c2 = RangeDecision.create("c2", RangeDecision.OP_GTEQ, 5);
+		mTestCdds.add(c2);
+	}	
 	
+	
+	/**
+	 * Test case 1: LTEQ
+	 */
+	@Test
+	public void getDecisionsTest1() {
+		CDD c1 = mTestCdds.get(0);
+		Decision<?> expected = c1.getDecision();
+	}
 	
 	/**
 	 * Test extreme case 1: 
 	 */
 	@Test
-	public void removeConstraintTest1() {
-		
-	}
-	
-	/**
-	 * Test extreme case 1: 
-	 */
-	@Test
-	public void removeConstraintTest2() {
+	public void getDecisionsTest2() {
 		
 	}
 	
@@ -43,7 +60,7 @@ public class RangeDecisionTest {
 	 * Test "normal" case: 
 	 */
 	@Test
-	public void removeConstraintTest3() {
+	public void getDecisionsTest3() {
 		
 	}
 	
