25ee9822d835b0ba0610481259e162918e0c56c0
Lena Funk <yungfunk@gmx.de>
2023-04-12 20:29:45 +0200

    implemented ComplementTransformer and ComplementTransformerReq2Pea

--- trunk/source/PEAtoBoogie/src/de/uni_freiburg/informatik/ultimate/pea2boogie/translator/ComplementTransformer.java
+++ trunk/source/PEAtoBoogie/src/de/uni_freiburg/informatik/ultimate/pea2boogie/translator/ComplementTransformer.java
@@ -5,14 +5,11 @@ import java.util.Map;
 
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
-import de.uni_freiburg.informatik.ultimate.lib.pea.ComplementPEA;
 import de.uni_freiburg.informatik.ultimate.lib.pea.PhaseEventAutomata;
 import de.uni_freiburg.informatik.ultimate.lib.srparse.pattern.DeclarationPattern;
 import de.uni_freiburg.informatik.ultimate.lib.srparse.pattern.PatternType;
-import de.uni_freiburg.informatik.ultimate.lib.srparse.pattern.PatternType.ReqPeas;
 import de.uni_freiburg.informatik.ultimate.pea2boogie.req2pea.IReq2Pea;
 import de.uni_freiburg.informatik.ultimate.pea2boogie.req2pea.IReq2PeaTransformer;
-import de.uni_freiburg.informatik.ultimate.pea2boogie.testgen.Req2CauseTrackingPea;
 import de.uni_freiburg.informatik.ultimate.pea2boogie.testgen.ReqEffectStore;
 
 public class ComplementTransformer implements IReq2PeaTransformer {
@@ -33,16 +30,9 @@ public class ComplementTransformer implements IReq2PeaTransformer {
 	@Override
 	public IReq2Pea transform(final IReq2Pea req2pea, final List<DeclarationPattern> init,
 			final List<PatternType<?>> requirements) {
-		
-		List<ReqPeas> peas = req2pea.getReqPeas();
-		PhaseEventAutomata peaToComplement = peas.get(0).getCounterTrace2Pea().get(0).getValue();
-		ComplementPEA complementPEA = new ComplementPEA(peaToComplement);
-		PhaseEventAutomata complementAutomaton = complementPEA.complement();
-		
-		return req2pea;
+		ComplementTransformerReq2Pea complementTransformerReq2Pea = new ComplementTransformerReq2Pea(mServices, mLogger, init);
+		complementTransformerReq2Pea.transform(req2pea);
+		return complementTransformerReq2Pea;
 	}
-		
-	
-
 }
 
