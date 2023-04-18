be4ceecebf7d9d91c11d0e99c08fbb0c769a914f
philippm <pm251@venus.uni-freiburg.de>
2023-04-18 21:27:49 +0200

    stylefix and rabin test with large component

--- trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/rabin/RabinSccComputation.java
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/rabin/RabinSccComputation.java
@@ -1,35 +1,30 @@
 package de.uni_freiburg.informatik.ultimate.automata.rabin;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.util.scc.DefaultSccComputation;
 import de.uni_freiburg.informatik.ultimate.util.scc.StronglyConnectedComponent;
 
+/*
+ * RabinSccs are only that, which loop back to their beginning
+ */
 public class RabinSccComputation<NODE> extends DefaultSccComputation<NODE> {
 
 	public RabinSccComputation(final ILogger logger, final ISuccessorProvider<NODE> successorProvider,
 			final int numberOfAllNodes, final Set<NODE> startNodes) {
 		super(logger, successorProvider, numberOfAllNodes, startNodes);
 
-		final boolean[] acceptingBalls = new boolean[mBalls.size()];
-
+		final HashSet<StronglyConnectedComponent<NODE>> temp = new HashSet<>();
 		for (int i = 0; i < mBalls.size(); i++) {
 			for (final NODE node : startNodes) {
 				if (mBalls.get(i).getNodes().contains(node)) {
-					acceptingBalls[i] = true;
+					temp.add(mBalls.get(i));
 				}
 			}
 		}
 
-		final ArrayList<StronglyConnectedComponent<NODE>> temp = new ArrayList<>();
-		for (int i = 0; i < acceptingBalls.length; i++) {
-			if (acceptingBalls[i]) {
-				temp.add(mBalls.get(i));
-			}
-
-		}
 		mBalls.clear();
 		mBalls.addAll(temp);
 	}
