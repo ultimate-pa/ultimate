4384703a97c745c0369365ee7e24857d3c9d32d6
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2023-05-10 10:26:01 +0200

    Remove Rabin Petri-Nets work in progress, will be continued on other branch

--- trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/petrinet/IRabinPetriNet.java
+++
@@ -1,9 +0,0 @@
-package de.uni_freiburg.informatik.ultimate.automata.petrinet;
-
-import java.util.Set;
-
-public interface IRabinPetriNet<LETTER, PLACE> extends IPetriNet<LETTER, PLACE> {
-	Set<PLACE> getFinitePlaces();
-
-	boolean isFinite(PLACE place);
-}
