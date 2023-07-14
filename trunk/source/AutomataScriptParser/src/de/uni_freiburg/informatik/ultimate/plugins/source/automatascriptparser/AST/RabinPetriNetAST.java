94e862cf7cdd2de93ea5dc16b41912bb2d6f1a1a
philippm <pm251@venus.uni-freiburg.de>
2023-07-14 17:43:02 +0200

    try to fix PetriNet being parsed before rpn

--- trunk/source/AutomataScriptParser/src/de/uni_freiburg/informatik/ultimate/plugins/source/automatascriptparser/AST/RabinPetriNetAST.java
+++ trunk/source/AutomataScriptParser/src/de/uni_freiburg/informatik/ultimate/plugins/source/automatascriptparser/AST/RabinPetriNetAST.java
@@ -1,30 +1,3 @@
-/*
- * Copyright (C) 2023 Frank Schüssele (schuessf@informatik.uni-freiburg.de)
- * Copyright (C) 2023 University of Freiburg
- *
- * This file is part of the ULTIMATE AutomataScriptParser plug-in.
- *
- * The ULTIMATE AutomataScriptParser plug-in is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The ULTIMATE AutomataScriptParser plug-in is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with the ULTIMATE AutomataScriptParser plug-in. If not, see <http://www.gnu.org/licenses/>.
- *
- * Additional permission under GNU GPL version 3 section 7:
- * If you modify the ULTIMATE AutomataScriptParser plug-in, or any covered work, by linking
- * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
- * containing parts covered by the terms of the Eclipse Public License, the
- * licensors of the ULTIMATE AutomataScriptParser plug-in grant you additional permission
- * to convey the resulting work.
- */
-
 package de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AST;
 
 import java.util.List;
@@ -32,21 +5,68 @@ import java.util.List;
 import de.uni_freiburg.informatik.ultimate.core.model.models.ILocation;
 
 /**
- * A extension of a acceptor Petri net with finite states; Intended for use ´with Rabin automata
+ * A adaptation of acceptor Petri net with finite states; Intended for use with Rabin automata
  *
  * @author Philipp Müller (pm251@venus.uni-freiburg.de)
  *
  */
-public class RabinPetriNetAST extends PetriNetAutomatonAST {
+public class RabinPetriNetAST extends AutomatonAST {
 
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = -6021451678169305249L;
+	// private static final long serialVersionUID = -6021451678169305249L;
+	private List<String> malphabet;
+	private List<String> mplaces;
+
+	private List<PetriNetTransitionAST> mtransitions;
+	private PetriNetMarkingListAST minitialMarkings;
+	private List<String> macceptingPlaces;
+
 	private List<String> mFinitePlaces;
 
 	public RabinPetriNetAST(final ILocation loc, final String name) {
 		super(loc, name);
+	}
+
+	public List<String> getAlphabet() {
+		return malphabet;
+	}
+
+	public void setAlphabet(final List<String> malphabet) {
+		this.malphabet = malphabet;
+	}
+
+	public List<String> getPlaces() {
+		return mplaces;
+	}
+
+	public void setPlaces(final List<String> mplaces) {
+		this.mplaces = mplaces;
+	}
+
+	public List<PetriNetTransitionAST> getTransitions() {
+		return mtransitions;
+	}
+
+	public void setTransitions(final List<PetriNetTransitionAST> mtransitions) {
+		this.mtransitions = mtransitions;
+	}
+
+	public PetriNetMarkingListAST getInitialMarkings() {
+		return minitialMarkings;
+	}
+
+	public void setInitialMarkings(final PetriNetMarkingListAST minitialMarkings) {
+		this.minitialMarkings = minitialMarkings;
+	}
+
+	public List<String> getAcceptingPlaces() {
+		return macceptingPlaces;
+	}
+
+	public void setAcceptingPlaces(final List<String> macceptingPlaces) {
+		this.macceptingPlaces = macceptingPlaces;
 	}
 
 	public List<String> getFinitePlaces() {
@@ -61,15 +81,15 @@ public class RabinPetriNetAST extends PetriNetAutomatonAST {
 	public String toString() {
 		final StringBuilder builder = new StringBuilder();
 		builder.append("RabinPetriNet(" + mName + "): " + "[#alphabet: ");
-		builder.append(super.getAlphabet().size());
+		builder.append(getAlphabet().size());
 		builder.append(" #places: ");
-		builder.append(super.getPlaces().size());
+		builder.append(getPlaces().size());
 		builder.append(" #acceptingPlaces: ");
-		builder.append(super.getAcceptingPlaces().size());
+		builder.append(getAcceptingPlaces().size());
 		builder.append(" #finitePlaces: ");
 		builder.append(getFinitePlaces().size());
 		builder.append(" #transitions: ");
-		builder.append(super.getTransitions().size());
+		builder.append(getTransitions().size());
 		builder.append("]");
 		return builder.toString();
 	}
