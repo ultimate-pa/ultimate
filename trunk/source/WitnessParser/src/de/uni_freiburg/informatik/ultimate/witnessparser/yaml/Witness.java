9dcea7406f6e3bbef4d2710626db2a4e1fb3526c
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2023-07-20 11:34:52 +0200

    Fix Witness, make it immutable

--- trunk/source/WitnessParser/src/de/uni_freiburg/informatik/ultimate/witnessparser/yaml/Witness.java
+++ trunk/source/WitnessParser/src/de/uni_freiburg/informatik/ultimate/witnessparser/yaml/Witness.java
@@ -1,17 +1,19 @@
 package de.uni_freiburg.informatik.ultimate.witnessparser.yaml;
 
 import java.util.List;
-import java.util.ArrayList;
 
-public class Witness {
+import de.uni_freiburg.informatik.ultimate.core.lib.models.BasePayloadContainer;
 
-	private final List<WitnessEntry> mEntries = new ArrayList<WitnessEntry>();
+public class Witness extends BasePayloadContainer {
+	private static final long serialVersionUID = 2111530908758373549L;
+
+	private final List<WitnessEntry> mEntries;
+
+	public Witness(final List<WitnessEntry> entries) {
+		mEntries = entries;
+	}
 
 	public List<WitnessEntry> getEntries() {
 		return mEntries;
-	}
-
-	public void add(final WitnessEntry newEntry) {
-		// TODO
 	}
 }
