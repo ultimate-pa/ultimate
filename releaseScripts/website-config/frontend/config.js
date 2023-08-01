456d12c889755d759859257d0a184c255e8b339f
Daniel Dietsch <daniel.dietsch@gmail.com>
2023-08-01 10:36:25 +0200

    fix webbridge url

--- releaseScripts/website-config/frontend/config.js
+++ releaseScripts/website-config/frontend/config.js
@@ -5,7 +5,7 @@ const _CONFIG = {
 	},
 	backend: {
 		// web_bridge_url: URL to the WebBackend API.
-		web_bridge_url: 'http://127.0.0.1:8080/api'
+		web_bridge_url: 'https://ultimate-pa.org/api'
 	},
 	editor: {
 		// Default content of the editor.
