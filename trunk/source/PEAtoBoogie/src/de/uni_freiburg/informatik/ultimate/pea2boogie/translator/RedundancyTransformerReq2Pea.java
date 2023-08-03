9c46412ed695c59131d01cb1d717df6a72778994
Lena <lenaf@tf.uni.freiburg.de>
2023-08-03 13:52:54 +0200

    implemented RedundancyTransformerReq2Pea

--- trunk/source/PEAtoBoogie/src/de/uni_freiburg/informatik/ultimate/pea2boogie/translator/RedundancyTransformerReq2Pea.java
+++ trunk/source/PEAtoBoogie/src/de/uni_freiburg/informatik/ultimate/pea2boogie/translator/RedundancyTransformerReq2Pea.java
@@ -3,9 +3,12 @@ package de.uni_freiburg.informatik.ultimate.pea2boogie.translator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.lib.pea.ComplementPEA;
+import de.uni_freiburg.informatik.ultimate.lib.pea.CounterTrace;
 import de.uni_freiburg.informatik.ultimate.lib.pea.PhaseEventAutomata;
 import de.uni_freiburg.informatik.ultimate.lib.srparse.Durations;
 import de.uni_freiburg.informatik.ultimate.lib.srparse.pattern.DeclarationPattern;
@@ -15,7 +18,9 @@ import de.uni_freiburg.informatik.ultimate.pea2boogie.IReqSymbolTable;
 import de.uni_freiburg.informatik.ultimate.pea2boogie.req2pea.IReq2Pea;
 import de.uni_freiburg.informatik.ultimate.pea2boogie.req2pea.IReq2PeaAnnotator;
 import de.uni_freiburg.informatik.ultimate.pea2boogie.req2pea.IReq2PeaTransformer;
+import de.uni_freiburg.informatik.ultimate.pea2boogie.req2pea.ReqCheckAnnotator;
 import de.uni_freiburg.informatik.ultimate.pea2boogie.testgen.ReqEffectStore;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Pair;
 
 
 public class RedundancyTransformerReq2Pea implements IReq2Pea {
@@ -37,39 +42,65 @@ public class RedundancyTransformerReq2Pea implements IReq2Pea {
 	}
 	
 	@Override
+	public void transform(IReq2Pea req2pea) {
+		final ReqSymboltableBuilder builder = new ReqSymboltableBuilder(mLogger);
+		final List<ReqPeas> peas = req2pea.getReqPeas();
+		final IReqSymbolTable symbolTable = req2pea.getSymboltable();
+		mSymbolTable = symbolTable;
+		
+		for (final DeclarationPattern p : mInitPattern) {
+			builder.addInitPattern(p);
+			mDurations.addInitPattern(p);
+		}
+		for (ReqPeas reqPeas : peas) {
+			final PatternType<?> pattern = peas.get(0).getPattern();
+			final List<Entry<CounterTrace, PhaseEventAutomata>> ct2pea = reqPeas.getCounterTrace2Pea();
+			
+			final List<Entry<CounterTrace, PhaseEventAutomata>> totalCt2pea = new ArrayList<>();
+			final List<Entry<CounterTrace, PhaseEventAutomata>> complementCt2pea = new ArrayList<>();
+			
+			for (Entry<CounterTrace, PhaseEventAutomata> pea : ct2pea) {
+				PhaseEventAutomata peaToComplement = pea.getValue();
+				ComplementPEA complementPea= new ComplementPEA(peaToComplement);
+				PhaseEventAutomata totalisedPea = complementPea.getTotalisedPEA();
+				PhaseEventAutomata complementedPEA = complementPea.getComplementPEA();
+				totalCt2pea.add(new Pair<>(pea.getKey(), totalisedPea));
+				builder.addPea(pattern, totalisedPea);
+				// The countertrace is wrong for the complemented Pea. I dont know how to negate a DC formula.
+				builder.addPea(pattern, complementedPEA);
+				complementCt2pea.add(new Pair<>(pea.getKey(), complementedPEA));
+				
+			}		
+			mReqPeas.add(new ReqPeas(pattern, totalCt2pea));
+			mReqPeas.add(new ReqPeas(pattern, complementCt2pea));
+			mSymbolTable = builder.constructSymbolTable();
+		}
+	}
+	
+	
+	@Override
 	public List<ReqPeas> getReqPeas() {
-		// TODO Auto-generated method stub
-		return null;
+		return mReqPeas;
 	}
 
 	@Override
 	public IReqSymbolTable getSymboltable() {
-		// TODO Auto-generated method stub
-		return null;
+		return mSymbolTable;
 	}
 
 	@Override
 	public Durations getDurations() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void transform(IReq2Pea req2pea) {
-		// TODO Auto-generated method stub
-		
+		return mDurations;
 	}
 
 	@Override
 	public boolean hasErrors() {
-		// TODO Auto-generated method stub
-		return false;
+		return mHasErrors;
 	}
 
 	@Override
 	public IReq2PeaAnnotator getAnnotator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ReqCheckAnnotator(mServices, mLogger, mReqPeas, mSymbolTable, mDurations);
 	}
 	
 }
