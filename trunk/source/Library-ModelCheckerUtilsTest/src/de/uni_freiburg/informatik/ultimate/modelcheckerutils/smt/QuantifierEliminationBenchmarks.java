fb5851fba5d6dfb5a4651142a5934e3224bb40a5
Matthias Heizmann <heizmann@informatik.uni-freiburg.de>
2023-08-05 00:27:30 +0200

    Change timeout of test

--- trunk/source/Library-ModelCheckerUtilsTest/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/QuantifierEliminationBenchmarks.java
+++ trunk/source/Library-ModelCheckerUtilsTest/src/de/uni_freiburg/informatik/ultimate/modelcheckerutils/smt/QuantifierEliminationBenchmarks.java
@@ -62,7 +62,7 @@ public class QuantifierEliminationBenchmarks {
 	 */
 	private static final boolean WRITE_SMT_SCRIPTS_TO_FILE = false;
 	private static final boolean WRITE_BENCHMARK_RESULTS_TO_WORKING_DIRECTORY = false;
-	private static final long TEST_TIMEOUT_MILLISECONDS = 00_000;
+	private static final long TEST_TIMEOUT_MILLISECONDS = 60_000;
 	private static final LogLevel LOG_LEVEL = LogLevel.INFO;
 	private static final LogLevel LOG_LEVEL_SOLVER = LogLevel.INFO;
 	private static final String SOLVER_COMMAND =
