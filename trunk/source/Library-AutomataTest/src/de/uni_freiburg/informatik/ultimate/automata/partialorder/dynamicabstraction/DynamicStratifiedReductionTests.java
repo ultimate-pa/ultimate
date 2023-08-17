db9d9a79869343f5b5d47914fe89fe9155292151
maul.esel <maul.esel@posteo.de>
2023-08-17 10:22:55 +0200

    add test classes for dynamic stratified reduction

---
+++ trunk/source/Library-AutomataTest/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/dynamicabstraction/DynamicStratifiedReductionTests.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2023 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2023 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.partialorder.dynamicabstraction;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.nio.file.Path;
+
+import org.junit.runner.RunWith;
+
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryException;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomataUtils;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.visitors.AutomatonConstructingVisitor;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AST.AutomataTestFileAST;
+import de.uni_freiburg.informatik.ultimate.test.junitextension.testfactory.FactoryTestRunner;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.DataStructureUtils;
+
+@RunWith(FactoryTestRunner.class)
+public class DynamicStratifiedReductionTests extends DynamicStratifiedReductionTestsBase {
+
+	@Override
+	protected <P> void runTest(final Path path, final AutomataTestFileAST ast,
+			final NestedWordAutomaton<String, String> input, final NestedWordAutomaton<String, String> expected,
+			final IIndependenceInducedByAbstraction<String, String> independence,
+			final IProofManager<String, String, P> proofManager) throws AutomataLibraryException {
+
+		final var stateFactory = new StratifiedStringFactory();
+		final var constructor =
+				new AutomatonConstructingVisitor<String, String>(s -> input.isInitial(stateFactory.getOriginalState(s)),
+						s -> input.isFinal(stateFactory.getOriginalState(s)), input.getVpAlphabet(), mAutomataServices,
+						() -> null);
+
+		final var initial = DataStructureUtils.getOneAndOnly(input.getInitialStates(), "initial state");
+
+		new DynamicStratifiedReduction<>(mAutomataServices, input, new AlphabeticOrder<>(), stateFactory, constructor,
+				initial, independence, proofManager);
+
+		final NestedWordAutomaton<String, String> actual = constructor.getReductionAutomaton();
+
+		assertThat("Automata differ, expected " + expected + " but actual was " + actual, areEqual(expected, actual));
+	}
+
+	private boolean areEqual(final NestedWordAutomaton<String, String> first,
+			final NestedWordAutomaton<String, String> second) {
+		if ((first == null) != (second == null)) {
+			mLogger.error("only one automata is null");
+			return false;
+		}
+
+		assert NestedWordAutomataUtils.isFiniteAutomaton(first);
+		assert NestedWordAutomataUtils.isFiniteAutomaton(second);
+
+		if (!first.getAlphabet().equals(second.getAlphabet())) {
+			mLogger.error("alphabet differs: %s != %s", first.getAlphabet(), second.getAlphabet());
+			return false;
+		}
+
+		if (!first.getStates().equals(second.getStates())) {
+			mLogger.error("states differ");
+			return false;
+		}
+
+		if (!first.getInitialStates().equals(second.getInitialStates())) {
+			mLogger.error("initial states differ");
+			return false;
+		}
+
+		if (!first.getFinalStates().equals(second.getFinalStates())) {
+			mLogger.error("accepting states differ");
+			return false;
+		}
+
+		for (final var state : first.getStates()) {
+			if (!first.lettersInternal(state).equals(second.lettersInternal(state))) {
+				mLogger.error("outgoing letters for state %s differ", state);
+				return false;
+			}
+
+			for (final var letter : first.lettersInternal(state)) {
+				if (!first.succInternal(state, letter).equals(second.succInternal(state, letter))) {
+					mLogger.error("successors for state %s under letter %s differ", state, letter);
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+}
---
+++ trunk/source/Library-AutomataTest/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/dynamicabstraction/DynamicStratifiedReductionTestsBase.java
@@ -0,0 +1,406 @@
+/*
+ * Copyright (C) 2023 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2023 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.partialorder.dynamicabstraction;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import org.junit.Before;
+
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryException;
+import de.uni_freiburg.informatik.ultimate.automata.AutomataLibraryServices;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomaton;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.IDfsOrder;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation;
+import de.uni_freiburg.informatik.ultimate.core.model.results.IResultWithSeverity.Severity;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.automatascriptinterpreter.AutomataDefinitionInterpreter;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.automatascriptinterpreter.IMessagePrinter;
+import de.uni_freiburg.informatik.ultimate.plugins.generator.automatascriptinterpreter.TestFileInterpreter.LoggerSeverity;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AtsASTNode;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AutomataScriptParserRun;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AST.AutomataTestFileAST;
+import de.uni_freiburg.informatik.ultimate.plugins.source.automatascriptparser.AST.NestedwordAutomatonAST;
+import de.uni_freiburg.informatik.ultimate.test.junitextension.testfactory.FactoryTestMethod;
+import de.uni_freiburg.informatik.ultimate.test.junitextension.testfactory.TestFactory;
+import de.uni_freiburg.informatik.ultimate.test.mocks.UltimateMocks;
+import de.uni_freiburg.informatik.ultimate.test.util.TestUtil;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.BidirectionalMap;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.ImmutableSet;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.Quin;
+
+/**
+ * Base class for Dynamic Stratified Reduction tests. Callers must only implemented the method
+ * {@link #runTest(Path, AutomataTestFileAST, NestedWordAutomaton, NestedWordAutomaton, IIndependenceInducedByAbstraction)}.
+ *
+ * @author Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ *
+ */
+public abstract class DynamicStratifiedReductionTestsBase implements IMessagePrinter {
+	protected IUltimateServiceProvider mServices;
+	protected AutomataLibraryServices mAutomataServices;
+	protected ILogger mLogger;
+	protected AutomataDefinitionInterpreter mInterpreter;
+
+	protected abstract <P> void runTest(final Path path, AutomataTestFileAST ast,
+			NestedWordAutomaton<String, String> input, NestedWordAutomaton<String, String> expected,
+			IIndependenceInducedByAbstraction<String, String> independence,
+			IProofManager<String, String, P> proofManager) throws AutomataLibraryException;
+
+	@Before
+	public void setUp() {
+		mServices = UltimateMocks.createUltimateServiceProviderMock();
+		mAutomataServices = new AutomataLibraryServices(mServices);
+		mLogger = mServices.getLoggingService().getLogger(getClass());
+		mInterpreter = new AutomataDefinitionInterpreter(this, mLogger, mServices);
+	}
+
+	@TestFactory
+	public Iterable<TestCase> createTests() throws IOException {
+		final Path dir = Path.of(TestUtil.getPathFromTrunk("examples/Automata/dynamic-abstractions/"));
+		try (var files = Files.list(dir)) {
+			return files.map(TestCase::new).sorted().collect(Collectors.toList());
+		}
+	}
+
+	private final class TestCase implements Comparable<TestCase> {
+		private final Path mPath;
+
+		public TestCase(final Path path) {
+			mPath = path;
+		}
+
+		@FactoryTestMethod
+		public void run() throws AutomataLibraryException, IOException {
+			runTestInternal(mPath);
+		}
+
+		@Override
+		public String toString() {
+			return mPath.getFileName().toString();
+		}
+
+		@Override
+		public int compareTo(final TestCase other) {
+			return mPath.compareTo(other.mPath);
+		}
+	}
+
+	private void runTestInternal(final Path path) throws AutomataLibraryException, IOException {
+		final AutomataTestFileAST parsed = parse(path);
+
+		NestedwordAutomatonAST inputAST = null;
+		NestedwordAutomatonAST expectedAST = null;
+		for (final var def : parsed.getAutomataDefinitions().getListOfAutomataDefinitions()) {
+			if ("input".equals(def.getName())) {
+				assert inputAST == null : "multiple inputs specified";
+				inputAST = (NestedwordAutomatonAST) def;
+			} else if ("expected".equals(def.getName())) {
+				assert expectedAST == null : "multiple expected outputs specified";
+				expectedAST = (NestedwordAutomatonAST) def;
+			}
+		}
+		assert inputAST != null && expectedAST != null : "either input or expected is missing";
+
+		mInterpreter.interpret(inputAST);
+		mInterpreter.interpret(expectedAST);
+
+		final var input = (NestedWordAutomaton<String, String>) mInterpreter.getAutomata().get("input");
+		final var expected = (NestedWordAutomaton<String, String>) mInterpreter.getAutomata().get("expected");
+		assert input != null && expected != null : "either input or expected is missing";
+
+		final IIndependenceInducedByAbstraction<String, String> indep = extractCommutativity(path);
+		final IProofManager<String, String, Integer> proofManager = new StringProofManager(extractProofs(path));
+		runTest(path, parsed, input, expected, indep, proofManager);
+	}
+
+	@Override
+	public void printMessage(final Severity severityForResult, final LoggerSeverity severityForLogger,
+			final String longDescr, final String shortDescr, final AtsASTNode node) {
+		// TODO
+		mLogger.warn(longDescr);
+	}
+
+	private AutomataTestFileAST parse(final Path path) throws FileNotFoundException {
+		final String filename = path.getFileName().toString();
+		final Reader reader = new BufferedReader(new FileReader(path.toFile()));
+		return new AutomataScriptParserRun(mServices, mLogger, reader, filename, path.toString()).getResult();
+	}
+
+	private static List<Set<String>> extractProofs(final Path path) throws IOException {
+		final String prefix = "//@ proof variables ";
+
+		final Optional<String> proofLine;
+		try (final var lines = Files.lines(path)) {
+			proofLine = lines.filter(l -> l.startsWith(prefix)).findFirst();
+		}
+
+		if (!proofLine.isPresent()) {
+			throw new AssertionError("no proof variables specification found");
+		}
+
+		final String relDescr = proofLine.get().substring(prefix.length());
+		final String[] proofDescrs = relDescr.split("|");
+		final var result = new ArrayList<Set<String>>();
+		for (final var proof : proofDescrs) {
+			final var variables = Arrays.stream(proof.split(",")).map(String::strip).collect(Collectors.toSet());
+			result.add(variables);
+		}
+		return result;
+	}
+
+	private <S> IIndependenceInducedByAbstraction<S, String> extractCommutativity(final Path path) throws IOException {
+		final String prefix = "//@ commutativity ";
+
+		final Optional<String> commLine;
+		try (final var lines = Files.lines(path)) {
+			commLine = lines.filter(l -> l.startsWith(prefix)).findFirst();
+		}
+
+		if (!commLine.isPresent()) {
+			mLogger.info("no commutativity specification found");
+			return new HashIndependenceByAbstraction<>(Map.of());
+		}
+
+		final HashMap<Set<String>, HashRelation<String, String>> result = new HashMap<>();
+
+		final String relDescr = commLine.get().substring(prefix.length());
+		final Pattern triplePattern = Pattern.compile("\\s*\\(\\{([^\\}]*)\\},([^,]+),([^\\)]+)\\)");
+		final Matcher matcher = triplePattern.matcher(relDescr);
+		while (matcher.find()) {
+			final Set<String> abstr =
+					Arrays.stream(matcher.group(1).strip().split(" ")).map(String::strip).collect(Collectors.toSet());
+			final String left = matcher.group(2).strip();
+			final String right = matcher.group(3).strip();
+
+			result.computeIfAbsent(abstr, x -> new HashRelation<>()).addPair(left, right);
+		}
+
+		mLogger.info("commutativity:");
+		for (final var entry : result.entrySet()) {
+			mLogger.info("\t%s ::= %s", entry.getKey(), entry.getValue().getSetOfPairs());
+		}
+
+		return new HashIndependenceByAbstraction<>(result.entrySet().stream()
+				.collect(Collectors.toMap(Map.Entry::getKey, e -> new HashIndependence<>(e.getValue()))));
+	}
+
+	private static final class HashIndependenceByAbstraction<S>
+			implements IIndependenceInducedByAbstraction<S, String> {
+
+		private final IIndependenceRelation<S, String> mEmptyIndependence =
+				new HashIndependence<>(new HashRelation<>());
+		private final Map<Set<String>, IIndependenceRelation<S, String>> mMap;
+
+		public HashIndependenceByAbstraction(final Map<Set<String>, IIndependenceRelation<S, String>> map) {
+			mMap = map;
+		}
+
+		@Override
+		public IIndependenceRelation<S, String> getInducedIndependence(final Set<String> freeVariables) {
+			final var result = mMap.get(freeVariables);
+			if (result == null) {
+				// If no independence is given, assume nothing commutes
+				return mEmptyIndependence;
+			}
+			return result;
+		}
+	}
+
+	private static final class HashIndependence<S> implements IIndependenceRelation<S, String> {
+		private final HashRelation<String, String> mRelation;
+		private final boolean mSymmetric;
+
+		public HashIndependence(final HashRelation<String, String> relation) {
+			mRelation = relation;
+			mSymmetric = isSymmetric(relation);
+		}
+
+		private static boolean isSymmetric(final HashRelation<String, String> relation) {
+			for (final var pair : relation) {
+				if (!relation.containsPair(pair.getValue(), pair.getKey())) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public boolean isSymmetric() {
+			return mSymmetric;
+		}
+
+		@Override
+		public boolean isConditional() {
+			return false;
+		}
+
+		@Override
+		public Dependence isIndependent(final S state, final String a, final String b) {
+			return mRelation.containsPair(a, b) ? Dependence.INDEPENDENT : Dependence.DEPENDENT;
+		}
+	}
+
+	private static final class StringProofManager implements IProofManager<String, String, Integer> {
+		private final List<Set<String>> mProofs;
+
+		public StringProofManager(final List<Set<String>> proofs) {
+			mProofs = proofs;
+		}
+
+		private String[] getProofStates(final String state) {
+			final var splitStates = state.split("|");
+			assert splitStates.length == mProofs.size();
+			return splitStates;
+		}
+
+		@Override
+		public boolean isProvenState(final String state) {
+			return Arrays.asList(getProofStates(state)).contains("false");
+		}
+
+		@Override
+		public Integer choseRespProof(final String state) {
+			// always chooses the proof with the minimal index
+			// TODO @Veronika: Customize this if necessary
+			final int result = Arrays.asList(getProofStates(state)).indexOf("false");
+			if (result < 0) {
+				throw new IllegalStateException("No proof can be made responsible in state " + state);
+			}
+			return result;
+		}
+
+		@Override
+		public Set<String> getVariables(final Integer responsible) {
+			return mProofs.get(responsible);
+		}
+	}
+
+	protected class AlphabeticOrder<S> implements IDfsOrder<String, S> {
+		@Override
+		public Comparator<String> getOrder(final S state) {
+			return Comparator.naturalOrder();
+		}
+
+		@Override
+		public boolean isPositional() {
+			return false;
+		}
+	}
+
+	protected class StratifiedStringFactory
+			implements IStratifiedStateFactory<String, String, String, AbstractionLevel<String>> {
+
+		private final BidirectionalMap<String, Quin<String, ImmutableSet<String>, AbstractionLevel<String>, AbstractionLevel<String>, LinkedList<String>>> mMap =
+				new BidirectionalMap<>();
+		private int mCounter;
+
+		@Override
+		public String createEmptyStackState() {
+			return "###empty###";
+		}
+
+		@Override
+		public String createStratifiedState(final String state, final ImmutableSet<String> sleepset,
+				final AbstractionLevel<String> level, final AbstractionLevel<String> limit,
+				final LinkedList<String> loopPredecs) {
+			final var quin = new Quin<>(state, sleepset, level, limit, loopPredecs);
+			final var existing = mMap.inverse().get(quin);
+			if (existing != null) {
+				return existing;
+			}
+
+			// TODO @Veronika It may be nicer to include the parameters in the name.
+			final var name = "s" + mCounter;
+			mCounter++;
+			mMap.put(name, quin);
+			return name;
+		}
+
+		@Override
+		public String getOriginalState(final String state) {
+			return mMap.get(state).getFirst();
+		}
+
+		@Override
+		public ImmutableSet<String> getSleepSet(final String state) {
+			return mMap.get(state).getSecond();
+		}
+
+		@Override
+		public AbstractionLevel<String> getAbstractionLevel(final String state) {
+			return mMap.get(state).getThird();
+		}
+
+		@Override
+		public void addToAbstractionLevel(final String state, final Set<String> variables) {
+			// TODO @Veronika
+			throw new AssertionError("not yet implemented");
+		}
+
+		@Override
+		public AbstractionLevel<String> getAbstractionLimit(final String state) {
+			return mMap.get(state).getFourth();
+		}
+
+		@Override
+		public void addToAbstractionLimit(final String state, final Set<String> variables) {
+			// TODO @Veronika
+			throw new AssertionError("not yet implemented");
+		}
+
+		@Override
+		public LinkedList<String> getLoopablePredecs(final String state) {
+			return mMap.get(state).getFifth();
+		}
+
+		@Override
+		public boolean isLoopCopy(final String state) {
+			// TODO @Veronika
+			throw new AssertionError("not yet implemented");
+		}
+	}
+}
