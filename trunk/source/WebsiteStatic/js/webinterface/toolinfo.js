980a1750edb2bd3bf610158637094669bc205e74
maul.esel <maul.esel@posteo.de>
2023-09-05 10:28:57 +0200

    fix tool links from web interface

--- trunk/source/WebsiteStatic/js/webinterface/toolinfo.js
+++ trunk/source/WebsiteStatic/js/webinterface/toolinfo.js
@@ -6,7 +6,7 @@ _TOOLS = {
   {% for tool in site.tools %}
   "{{ tool.tool_id }}": {
     name: "{{ tool.title }}",
-    url: "{{ tool.url }}",
+    url: "{{ tool.url | relative_url }}",
   },
   {% endfor %}
 };
