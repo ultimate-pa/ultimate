8e295bd6899fb705c62368571b13e84ae87bfbcc
maul.esel <maul.esel@posteo.de>
2023-11-13 11:37:51 +0100

    PlacesCoRelation: use HashRelation; CC fixes

--- trunk/source/Library-TraceCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/owickigries/empire/PlacesCoRelation.java
+++ trunk/source/Library-TraceCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/owickigries/empire/PlacesCoRelation.java
@@ -26,21 +26,20 @@
  */
 package de.uni_freiburg.informatik.ultimate.lib.tracecheckerutils.owickigries.empire;
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Set;
 
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.IPetriNet;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.unfolding.BranchingProcess;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.unfolding.Condition;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.unfolding.ICoRelation;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.relation.HashRelation;
 
 public final class PlacesCoRelation<PLACE, LETTER> {
-	private final HashMap<PLACE, Set<PLACE>> mCoRelatedPlaces;
+	private final HashRelation<PLACE, PLACE> mCoRelatedPlaces;
 	private final ICoRelation<LETTER, PLACE> mCoRelation;
 
 	/**
-	 * 
+	 *
 	 * @param bp
 	 *            Branching process for which the co-relation should be checked.
 	 * @param net
@@ -51,30 +50,29 @@ public final class PlacesCoRelation<PLACE, LETTER> {
 		mCoRelatedPlaces = getAllCorelatedPlaces(net, bp);
 	}
 
-	private final HashMap<PLACE, Set<PLACE>> getAllCorelatedPlaces(final IPetriNet<LETTER, PLACE> net,
-			BranchingProcess<LETTER, PLACE> bp) {
-		Set<PLACE> originalPlaces = net.getPlaces();
-		HashMap<PLACE, Set<PLACE>> coPlacesHashtable = new HashMap<>();
-		for (PLACE place : originalPlaces) {
-			Set<PLACE> coPlacesSet = new HashSet<>();
-			for (PLACE place2 : originalPlaces) {
+	private final HashRelation<PLACE, PLACE> getAllCorelatedPlaces(final IPetriNet<LETTER, PLACE> net,
+			final BranchingProcess<LETTER, PLACE> bp) {
+		final Set<PLACE> originalPlaces = net.getPlaces();
+		final HashRelation<PLACE, PLACE> coPlacesHashtable = new HashRelation<>();
+		for (final PLACE place : originalPlaces) {
+			for (final PLACE place2 : originalPlaces) {
 				if (!place.equals(place2) && placesCoRelation(place, place2, bp)) {
-					coPlacesSet.add(place2);
+					coPlacesHashtable.addPair(place, place2);
 				}
 			}
-			coPlacesHashtable.put(place, coPlacesSet);
 		}
 		return coPlacesHashtable;
 	}
 
-	private final boolean placesCoRelation(PLACE firstPlace, PLACE secondPlace, BranchingProcess<LETTER, PLACE> bp) {
-		if (mCoRelatedPlaces.get(secondPlace).contains(firstPlace)) {
+	private final boolean placesCoRelation(final PLACE firstPlace, final PLACE secondPlace,
+			final BranchingProcess<LETTER, PLACE> bp) {
+		if (mCoRelatedPlaces.containsPair(secondPlace, firstPlace)) {
 			return true;
 		}
-		Set<Condition<LETTER, PLACE>> firstPlaceConditions = bp.getConditions(firstPlace);
-		Set<Condition<LETTER, PLACE>> secondPlaceConditions = bp.getConditions(secondPlace);
-		for (Condition<LETTER, PLACE> condition1 : firstPlaceConditions) {
-			for (Condition<LETTER, PLACE> condition2 : secondPlaceConditions) {
+		final Set<Condition<LETTER, PLACE>> firstPlaceConditions = bp.getConditions(firstPlace);
+		final Set<Condition<LETTER, PLACE>> secondPlaceConditions = bp.getConditions(secondPlace);
+		for (final Condition<LETTER, PLACE> condition1 : firstPlaceConditions) {
+			for (final Condition<LETTER, PLACE> condition2 : secondPlaceConditions) {
 				if (!condition1.equals(condition2) && mCoRelation.isInCoRelation(condition1, condition2)) {
 					return true;
 				}
@@ -85,15 +83,14 @@ public final class PlacesCoRelation<PLACE, LETTER> {
 
 	/**
 	 * Checks co-relation for two places.
-	 * 
+	 *
 	 * @param firstPlace
 	 *            First Place for co-relation check.
 	 * @param secondPlace
 	 *            Second Place for co-relation check.
 	 * @return true if the places are co-related else false.
 	 */
-	public boolean getPlacesCorelation(PLACE firstPlace, PLACE secondPlace) {
-		Set<PLACE> coPlacesSet = mCoRelatedPlaces.get(firstPlace);
-		return coPlacesSet.contains(secondPlace);
+	public boolean getPlacesCorelation(final PLACE firstPlace, final PLACE secondPlace) {
+		return mCoRelatedPlaces.containsPair(firstPlace, secondPlace);
 	}
 }
