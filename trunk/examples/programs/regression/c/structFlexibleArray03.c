ab35561f9b09d4e98ea42898033352535968732b
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2023-11-14 13:53:16 +0100

    Fix and move test

--- trunk/examples/programs/regression/c/structFlexibleArray03.c
+++ trunk/examples/programs/regression/c/structFlexibleArray03.c
@@ -1,7 +1,5 @@
 // #Safe
 /*
- * We currently cannot handle the assignment of structs with flexible arrays.
- * 
  * Date: 2023-11-14
  * Author: schuessf@informatik.uni-freiburg.de
  */
@@ -14,9 +12,9 @@ struct str {
 int main() {
   struct str *s1 = malloc(sizeof (struct str) + 8);
   struct str *s2 = malloc(sizeof (struct str) + 2);
-  int *dp = &(s->d[0]);
+  int *dp = &(s1->d[0]);
   *dp = 42;
   *s1 = *s2;
-  int res = s->d[0];
+  int res = s1->d[0];
   //@ assert res == 42;
 }
