92f54599e0cf5483371354dcc40588c36d014f98
maul.esel <maul.esel@posteo.de>
2023-11-21 01:07:49 +0100

    simplify test

--- trunk/examples/CToBoogieTranslation/regression/all/typedefChainSameName.c
+++ trunk/examples/CToBoogieTranslation/regression/all/typedefChainSameName.c
@@ -6,9 +6,9 @@ typedef struct __mbstate_t __mbstate_t;
 
 typedef __mbstate_t mbstate_t;
 
-struct __mbstate_t { /* sizeof: 8, alignof: 4 */
+struct __mbstate_t {
     int __count;
-    union anonymous_8411 __value;
+    int __value;
 };
 
 int main() {
