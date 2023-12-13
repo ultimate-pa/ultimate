5728a9ae73da044447340a66ddaba8084b03e904
ebbima <ebbima98@gmail.com>
2023-12-13 14:52:20 +0100

    ConditionalCommutativityInterpolantProvider now uses a INwaOutgoingLetterAndTransitionProvider

--- trunk/source/Library-TraceCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/partialorder/independence/ConditionalCommutativityInterpolantProvider.java
+++ trunk/source/Library-TraceCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/partialorder/independence/ConditionalCommutativityInterpolantProvider.java
@@ -1,41 +1,49 @@
 package de.uni_freiburg.informatik.ultimate.lib.tracecheckerutils.partialorder.independence;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.predicates.IPredicate;
 import de.uni_freiburg.informatik.ultimate.lib.tracecheckerutils.ITraceChecker;
-import de.uni_freiburg.informatik.ultimate.automata.IAutomaton;
 import de.uni_freiburg.informatik.ultimate.automata.IRun;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.INwaOutgoingLetterAndTransitionProvider;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingInternalTransition;
 import de.uni_freiburg.informatik.ultimate.automata.partialorder.independence.IIndependenceRelation;
 import de.uni_freiburg.informatik.ultimate.automata.statefactory.IEmptyStackStateFactory;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IIcfgTransition;
-import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgEdge;
 
 public class ConditionalCommutativityInterpolantProvider<L extends IIcfgTransition<?>> {
 
 	private ConditionalCommutativityChecker<L> mChecker;
+	private INwaOutgoingLetterAndTransitionProvider<L, IPredicate> mAbstraction;
 
 	ConditionalCommutativityInterpolantProvider(IConditionalCommutativityCriterion<L> criterion,
 			IIndependenceRelation<IPredicate, L> independenceRelation, SemanticIndependenceConditionGenerator generator,
-			final IAutomaton<L, IPredicate> abstraction, IEmptyStackStateFactory<IPredicate> emptyStackStateFactory,
-			ITraceChecker<L> traceChecker) {
+			final INwaOutgoingLetterAndTransitionProvider<L, IPredicate> abstraction,
+			IEmptyStackStateFactory<IPredicate> emptyStackStateFactory, ITraceChecker<L> traceChecker) {
+		mAbstraction = abstraction;
 		mChecker = new ConditionalCommutativityChecker<>(criterion, independenceRelation, generator, abstraction,
 				emptyStackStateFactory, traceChecker);
 	}
 
 	List<IPredicate> getInterpolants(IRun<L, IPredicate> run, List<IPredicate> predicates) {
 
-		List<IPredicate> stateSequence = run.getStateSequence();
-		for (int i = 0; i < (run.getLength() - 1); i++) {
-			IPredicate state = stateSequence.get(i);
-			List<IcfgEdge> edges = run.getSymbol(i).getSource().getOutgoingEdges();
+		for (IPredicate state : run.getStateSequence()) {
+			Iterator<OutgoingInternalTransition<L, IPredicate>> iterator =
+					mAbstraction.internalSuccessors(state).iterator();
 
-			for (int j = 0; j < edges.size(); j++) {
-				IcfgEdge edge1 = edges.get(j);
-				for (int k = j + 1; k < edges.size(); k++) {
-					IcfgEdge edge2 = edges.get(k);
-					List<IPredicate> conPredicates =
-							mChecker.checkConditionalCommutativity(run, state, (L) edge1, (L) edge2);
+			List<OutgoingInternalTransition<L, IPredicate>> transitions = new ArrayList<>();
+			while (iterator.hasNext()) {
+				transitions.add(iterator.next());
+			}
+
+			for (int j = 0; j < transitions.size(); j++) {
+				OutgoingInternalTransition<L, IPredicate> transition1 = transitions.get(j);
+				for (int k = j + 1; k < transitions.size(); k++) {
+					OutgoingInternalTransition<L, IPredicate> transition2 = transitions.get(k);
+					List<IPredicate> conPredicates = mChecker.checkConditionalCommutativity(run, state,
+							transition1.getLetter(), transition2.getLetter());
 					if (conPredicates != null) {
 						predicates.addAll(conPredicates);
 					}
