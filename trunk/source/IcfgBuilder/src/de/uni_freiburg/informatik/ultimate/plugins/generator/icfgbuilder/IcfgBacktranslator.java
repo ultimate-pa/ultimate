7977a977407ad61f68707522549c41444bd5e986
Matthias Heizmann <matthias@heizmann.name>
2024-05-22 16:30:40 +0200

    Rename license header

--- trunk/source/IcfgBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/icfgbuilder/IcfgBacktranslator.java
+++ trunk/source/IcfgBuilder/src/de/uni_freiburg/informatik/ultimate/plugins/generator/icfgbuilder/IcfgBacktranslator.java
@@ -10,7 +10,7 @@
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * The ULTIMATE RCFGBuilder plug-in is distributed in the hope that it will be useful,
+ * The ULTIMATE IcfgBuilder plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
