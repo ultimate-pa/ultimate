9de10a314b32bd16c39c2b43b244ba6e770c506a
NiklasKult <57148310+NiklasKult@users.noreply.github.com>
2024-05-31 13:57:51 +0200

    add author info

--- trunk/source/BoogiePreprocessor/src/de/uni_freiburg/informatik/ultimate/boogie/preprocessor/BoogiePreprocessorBacktranslator.java
+++ trunk/source/BoogiePreprocessor/src/de/uni_freiburg/informatik/ultimate/boogie/preprocessor/BoogiePreprocessorBacktranslator.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014-2015 Daniel Dietsch (dietsch@informatik.uni-freiburg.de)
+ * Copyright (C) 2024 Niklas Kult (niklas111098@gmail.com)
  * Copyright (C) 2015 University of Freiburg
  *
  * This file is part of the ULTIMATE BoogiePreprocessor plug-in.
