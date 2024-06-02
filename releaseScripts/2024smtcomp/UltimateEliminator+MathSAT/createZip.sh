f34de48b2599174df97260ba45f01123940e0f7e
Matthias Heizmann <matthias@heizmann.name>
2024-06-02 18:29:00 +0200

    Ultimate Eliminator 2024

---
+++ releaseScripts/2024smtcomp/UltimateEliminator+MathSAT/createZip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mkdir StarExecArchive
+mkdir StarExecArchive/bin
+mkdir StarExecArchive/Ultimate
+cp -a ../../../trunk/source/BA_SiteRepository/target/products/UltimateEliminator/linux/gtk/x86_64/* StarExecArchive/Ultimate/
+rm StarExecArchive/Ultimate/Ultimate
+rm StarExecArchive/Ultimate/Ultimate.ini
+cp *LICENSE* StarExecArchive/Ultimate/
+cp starexec_description.txt StarExecArchive/
+cp starexec_run_* StarExecArchive/bin/
+cp smtlib2_trace_executor StarExecArchive/bin/
+cp ../../../trunk/examples/settings/UltimateEliminator/mathsat.epf StarExecArchive/
+
+cp -LR mathsat StarExecArchive/Ultimate
+
+cp -a java/jdk-11.0.2 StarExecArchive/Ultimate/
+
+
+cd StarExecArchive
+zip ../UltimateCommandline.zip -r *
+
