519fde466f30ef4e8afb8e704fc66beed1d750bf
Manuel Bentele <development@manuel-bentele.de>
2024-07-24 22:27:17 +0200

    Update XML binding generation and adjust XML bundles & packages

--- trunk/source/UltimateCLI/src/de/uni_freiburg/informatik/ultimate/cli/ToolchainLocator.java
+++ trunk/source/UltimateCLI/src/de/uni_freiburg/informatik/ultimate/cli/ToolchainLocator.java
@@ -34,7 +34,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.xml.bind.JAXBException;
+import jakarta.xml.bind.JAXBException;
 
 import org.xml.sax.SAXException;
 
