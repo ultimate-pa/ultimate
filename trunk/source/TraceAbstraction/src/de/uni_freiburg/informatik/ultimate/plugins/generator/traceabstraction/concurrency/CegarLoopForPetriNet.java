52ed804f195bfd1301236bce760cd975066580bb
maul.esel <maul.esel@posteo.de>
2024-08-21 19:42:40 +0200

    delete TraceAbstractionConcurrent plugin

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/concurrency/CegarLoopForPetriNet.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/concurrency/CegarLoopForPetriNet.java
@@ -3,26 +3,26 @@
  * Copyright (C) 2011-2015 Matthias Heizmann (heizmann@informatik.uni-freiburg.de)
  * Copyright (C) 2015 University of Freiburg
  *
- * This file is part of the ULTIMATE TraceAbstractionConcurrent plug-in.
+ * This file is part of the ULTIMATE TraceAbstraction plug-in.
  *
- * The ULTIMATE TraceAbstractionConcurrent plug-in is free software: you can redistribute it and/or modify
+ * The ULTIMATE TraceAbstraction plug-in is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * The ULTIMATE TraceAbstractionConcurrent plug-in is distributed in the hope that it will be useful,
+ * The ULTIMATE TraceAbstraction plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with the ULTIMATE TraceAbstractionConcurrent plug-in. If not, see <http://www.gnu.org/licenses/>.
+ * along with the ULTIMATE TraceAbstraction plug-in. If not, see <http://www.gnu.org/licenses/>.
  *
  * Additional permission under GNU GPL version 3 section 7:
- * If you modify the ULTIMATE TraceAbstractionConcurrent plug-in, or any covered work, by linking
+ * If you modify the ULTIMATE TraceAbstraction plug-in, or any covered work, by linking
  * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
  * containing parts covered by the terms of the Eclipse Public License, the
- * licensors of the ULTIMATE TraceAbstractionConcurrent plug-in grant you additional permission
+ * licensors of the ULTIMATE TraceAbstraction plug-in grant you additional permission
  * to convey the resulting work.
  */
 package de.uni_freiburg.informatik.ultimate.plugins.generator.traceabstraction.concurrency;
@@ -122,12 +122,10 @@ public class CegarLoopForPetriNet<L extends IIcfgTransition<?>>
 
 	public int mCoRelationQueries = 0;
 	/**
-	 * Alternative measure to
-	 * {@link CegarLoopStatisticsDefinitions#BiggestAbstraction} which currently
-	 * counts the number of places.
-	 * TODO 20220821 Matthias: Find out whether counting transitions instead of
-	 * places is helpful. An alternative might be to count flow. In the long
-	 * run the most suitable measure should be utilized in the statistics.
+	 * Alternative measure to {@link CegarLoopStatisticsDefinitions#BiggestAbstraction} which currently counts the
+	 * number of places. TODO 20220821 Matthias: Find out whether counting transitions instead of places is helpful. An
+	 * alternative might be to count flow. In the long run the most suitable measure should be utilized in the
+	 * statistics.
 	 */
 	public int mBiggestAbstractionTransitions;
 	/**
