992aa12bd2c554e797adb169164d83d610c18e19
Paswalt <pascal.walter2008@web.de>
2024-09-26 16:58:09 +0200

    Added new redundancy result

---
+++ trunk/source/PEAtoBoogie/src/de/uni_freiburg/informatik/ultimate/pea2boogie/results/ReqCheckRedundancyResult.java
@@ -0,0 +1,48 @@
+package de.uni_freiburg.informatik.ultimate.pea2boogie.results;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import de.uni_freiburg.informatik.ultimate.core.model.models.IElement;
+
+public class ReqCheckRedundancyResult<LOC extends IElement> extends ReqCheckFailResult<LOC> {
+
+	private String mReqName;
+	private String mRedundancySet;
+	
+	public ReqCheckRedundancyResult(LOC element, String plugin, String reqName, String redundancySet) {
+		super(element, plugin);
+		mReqName = reqName;
+		mRedundancySet = redundancySet;
+	}
+
+	@Override
+	public String getLongDescription() {
+		return "Extracted redundancy set for " + mReqName + ": " + mRedundancySet;
+	}
+	
+	/*
+	 * Function that parses loop invariants represented by strings. In the terms of the regex,
+	 * we extract every name of the automata appearing inside of the loop invariant.
+	 * TODO: One issue that persist is if a user names the observable of a requirement
+	 * such that it matches the regex (for example: "input FakeReq_ct0_total_pc IS bool").
+	 * For now the method works under the assumption that names created during the formalization process are
+	 * disjoint of the variable names used in the program that simulates the intersection of automata
+	 */
+	public static Set<String> extractRedundancySet(String invariant) {
+		String regex = "[a-zA-Z_]+[a-zA-Z0-9_]*_total";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(invariant);
+		
+		Set<String> redundancySet = new HashSet<>();
+		
+		while (matcher.find()) {
+			redundancySet.add(matcher.group().split("_ct")[0]);
+		}
+		
+		return redundancySet;
+	}
+	
+}
