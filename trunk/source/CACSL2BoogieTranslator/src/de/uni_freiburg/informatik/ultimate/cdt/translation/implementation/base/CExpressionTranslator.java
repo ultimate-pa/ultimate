f06c4c666221bf48fa7fdf369d04aec9bf0b3cc5
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2024-10-25 12:48:41 +0200

    Bugfix: Don't convert the rhs of a shift to the same type as the lhs Only integer promotion should be performed (see 6.5.7.3)

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/CExpressionTranslator.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/CExpressionTranslator.java
@@ -402,15 +402,15 @@ public class CExpressionTranslator {
 			throw new UnsupportedOperationException("operands have to have integer types");
 		}
 		final ExpressionResult leftPromoted = mExprResultTransformer.promoteToIntegerIfNecessary(loc, left);
-		final CPrimitive typeOfResult = (CPrimitive) leftPromoted.getLrValue().getCType().getUnderlyingType();
-		final ExpressionResult rightConverted =
-				mExprResultTransformer.performImplicitConversion(right, typeOfResult, loc);
+		final CPrimitive leftType = (CPrimitive) leftPromoted.getLrValue().getCType().getUnderlyingType();
+		final ExpressionResult rightPromoted = mExprResultTransformer.promoteToIntegerIfNecessary(loc, right);
+		final CPrimitive rightType = (CPrimitive) rightPromoted.getLrValue().getCType().getUnderlyingType();
 
 		final ExpressionResult result =
-				mExpressionTranslation.handleBitshiftExpression(loc, op, leftPromoted.getLrValue().getValue(),
-						typeOfResult, rightConverted.getLrValue().getValue(), typeOfResult, mAuxVarInfoBuilder);
+				mExpressionTranslation.handleBitshiftExpression(loc, op, leftPromoted.getLrValue().getValue(), leftType,
+						rightPromoted.getLrValue().getValue(), rightType, mAuxVarInfoBuilder);
 		final ExpressionResultBuilder builder =
-				new ExpressionResultBuilder().addAllExceptLrValue(leftPromoted, rightConverted);
+				new ExpressionResultBuilder().addAllExceptLrValue(leftPromoted, rightPromoted);
 		return builder.addAllIncludingLrValue(result).build();
 	}
 
