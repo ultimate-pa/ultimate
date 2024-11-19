7ebed1e14198c2a90881a69b8aafa056b74a4877
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2024-11-19 10:03:50 +0100

    Use IcfgBuilder for Referee in validation

--- releaseScripts/default/makeFresh.sh
+++ releaseScripts/default/makeFresh.sh
@@ -51,7 +51,7 @@ create_tool_zips() {
     exit_on_fail bash makeZip.sh GemCutter $platform AutomizerCInline_WitnessPrinter.xml NONE AutomizerCInline.xml AutomizerCInline_WitnessPrinter.xml NONE NONE
 
     # Referee
-    exit_on_fail bash makeZip.sh Referee $platform RefereeCInline.xml NONE RefereeCInline.xml NONE NONE NONE
+    exit_on_fail bash makeZip.sh Referee $platform RefereeCInline.xml NONE RefereeCInline_IcfgBuilder.xml NONE NONE NONE
 
     # DeltaDebugger
     exit_on_fail bash createDeltaDebuggerDir.sh $platform
