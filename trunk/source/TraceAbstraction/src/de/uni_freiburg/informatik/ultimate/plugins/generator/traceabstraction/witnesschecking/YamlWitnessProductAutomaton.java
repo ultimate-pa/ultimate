af15baa993501dd2f42412aecdae8473ca4e0d2c
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2024-11-28 14:59:38 +0100

    Throw exception in witness validation for column matching on conditionals

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/witnesschecking/YamlWitnessProductAutomaton.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/witnesschecking/YamlWitnessProductAutomaton.java
@@ -249,8 +249,17 @@ public class YamlWitnessProductAutomaton<LETTER extends IIcfgTransition<?>>
 			return false;
 		}
 		final Location witnessLoc = waypoint.getLocation();
-		return programLoc.getStartLine() == witnessLoc.getLine()
-				&& (witnessLoc.getColumn() == null || witnessLoc.getColumn() == programLoc.getStartColumn());
+		if (programLoc.getStartLine() == witnessLoc.getLine()) {
+			if (witnessLoc.getColumn() == null) {
+				return true;
+			}
+			if (programLoc.toString().contains("?")) {
+				throw new UnsupportedOperationException(
+						"Location matching for conditional expressions is not supported yet.");
+			}
+			return witnessLoc.getColumn() == programLoc.getStartColumn();
+		}
+		return false;
 	}
 
 	/** Returns true if the end line and column of the statement match the location of the waypoint */
