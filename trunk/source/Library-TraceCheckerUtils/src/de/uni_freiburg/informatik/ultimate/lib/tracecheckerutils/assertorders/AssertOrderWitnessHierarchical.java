0752012d090d49a7728ed9843fb8b4067275969f
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2024-12-02 16:25:43 +0100

    Add AssertOrderWitnessHierarchical

---
+++ trunk/source/Library-TraceCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/assertorders/AssertOrderWitnessHierarchical.java
@@ -0,0 +1,36 @@
+package de.uni_freiburg.informatik.ultimate.lib.tracecheckerutils.assertorders;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWord;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IAction;
+import de.uni_freiburg.informatik.ultimate.lib.tracecheckerutils.Counterexample;
+import de.uni_freiburg.informatik.ultimate.util.datastructures.DataStructureUtils;
+
+public class AssertOrderWitnessHierarchical<L extends IAction> implements IAssertOrder<L> {
+	private final IAssertOrder<L> mUnderlying;
+
+	public AssertOrderWitnessHierarchical(final IAssertOrder<L> underlying) {
+		mUnderlying = underlying;
+	}
+
+	@Override
+	public List<Set<Integer>> partition(final Counterexample<L> counterexample) {
+		final NestedWord<L> trace = counterexample.getWord();
+		final Set<Integer> witnessActions = IntStream.range(0, trace.length())
+				.filter(x -> AssertOrderUtils.isWitnessAction(trace.getSymbol(x))).boxed().collect(Collectors.toSet());
+		final List<Set<Integer>> result = new ArrayList<>();
+		result.add(witnessActions);
+		for (final var underlyingPartition : mUnderlying.partition(counterexample)) {
+			final Set<Integer> partition = DataStructureUtils.difference(underlyingPartition, witnessActions);
+			if (!partition.isEmpty()) {
+				result.add(partition);
+			}
+		}
+		return result;
+	}
+}
