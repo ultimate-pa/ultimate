58e62334af9b128864500a212e727855a7291743
henkele <henkele@informatik.uni-freiburg.de>
2025-01-16 14:12:44 +0100

    cc fixes

--- trunk/source/PEAtoBoogie/src/de/uni_freiburg/informatik/ultimate/pea2boogie/VerificationResultTransformer.java
+++ trunk/source/PEAtoBoogie/src/de/uni_freiburg/informatik/ultimate/pea2boogie/VerificationResultTransformer.java
@@ -208,8 +208,8 @@ public class VerificationResultTransformer {
 				mLogger.debug("PE after Pea2Boogie result transformation");
 				mLogger.debug(newPe);
 			}
-			final List<Entry<Rational, Map<Term, Term>>> delta2var2value = generateTimeSequenceMap(
-					newPe.getProgramStates());
+			final List<Entry<Rational, Map<Term, Term>>> delta2var2value =
+					generateTimeSequenceMap(newPe.getProgramStates());
 			final String failurePath = formatTimeSequenceMap(delta2var2value);
 			return new ReqCheckRtInconsistentResult<>(element, plugin, failurePath);
 		}
@@ -241,8 +241,8 @@ public class VerificationResultTransformer {
 
 	private String formatTimeSequenceMap(final List<Entry<Rational, Map<Term, Term>>> delta2var2value) {
 
-		final int deltaMaxLength = delta2var2value.stream().map(a -> a.getKey().toString().length())
-				.max(Integer::compare).get();
+		final int deltaMaxLength =
+				delta2var2value.stream().map(a -> a.getKey().toString().length()).max(Integer::compare).get();
 		// there might be two numbers of maxlength, we have 3 additional chars "(;]", we
 		// want 2 spaces
 		// if maxLength is smaller than INITIAL (7) + 5 , use 12 instead
@@ -253,8 +253,8 @@ public class VerificationResultTransformer {
 		Rational current = Rational.ZERO;
 		String lastValues = "";
 		for (final Entry<Rational, Map<Term, Term>> entry : delta2var2value) {
-			final String values = entry.getValue().entrySet().stream().map(this::formatVarValue)
-					.collect(Collectors.joining(" "));
+			final String values =
+					entry.getValue().entrySet().stream().map(this::formatVarValue).collect(Collectors.joining(" "));
 			if (lastValues.equals(values)) {
 				// subsume these values in the current step
 				continue;
@@ -285,13 +285,12 @@ public class VerificationResultTransformer {
 	}
 
 	/**
-	 * @return A map from delta value to variable values that are interesting at
-	 *         this point of time
+	 * @return A map from delta value to variable values that are interesting at this point of time
 	 */
-	private List<Entry<Rational, Map<Term, Term>>> generateTimeSequenceMap(
-			final List<ProgramState<Term>> programStates) {
-		final List<ProgramState<Term>> stateSequence = programStates.stream().filter(Objects::nonNull)
-				.collect(Collectors.toList());
+	private List<Entry<Rational, Map<Term, Term>>>
+			generateTimeSequenceMap(final List<ProgramState<Term>> programStates) {
+		final List<ProgramState<Term>> stateSequence =
+				programStates.stream().filter(Objects::nonNull).collect(Collectors.toList());
 
 		final Map<String, Term> vars = new LinkedHashMap<>(stateSequence.stream()
 				.flatMap(a -> a.getVariables().stream()).distinct().collect(Collectors.toMap(Term::toString, a -> a)));
@@ -396,8 +395,8 @@ public class VerificationResultTransformer {
 			final IcfgProgramExecution<IAction> peWithBE;
 			if (hasInvalidBranchEncoders(pe)) {
 				mLogger.info("Computing branch encoders");
-				final TraceCheck<IAction> tcl = TraceCheck.createTraceCheck(mServices, toolkit, mgdScriptTc, truePred,
-						falsePred, trace);
+				final TraceCheck<IAction> tcl =
+						TraceCheck.createTraceCheck(mServices, toolkit, mgdScriptTc, truePred, falsePred, trace);
 				if (!tcl.providesRcfgProgramExecution()) {
 					mLogger.warn("Could not extract reduced program execution from trace: TraceCheck reported "
 							+ tcl.isCorrect());
@@ -413,21 +412,21 @@ public class VerificationResultTransformer {
 			final List<IAction> cleanedTrace = removeUnrelatedVariables(sequentialTrace, reqCheck, mgdScriptTc);
 
 			mLogger.info("Computing reduced program execution");
-			final TraceCheck<IAction> tc = TraceCheck.createTraceCheck(mServices, toolkit, mgdScriptTc, truePred,
-					falsePred, cleanedTrace);
+			final TraceCheck<IAction> tc =
+					TraceCheck.createTraceCheck(mServices, toolkit, mgdScriptTc, truePred, falsePred, cleanedTrace);
 			if (tc.isCorrect() == LBool.SAT) {
 				return tc.getRcfgProgramExecution();
 			}
 
 			// should be unreachable
 			mLogger.fatal("Reduced program execution is not 'sat'");
-			final TraceCheck<IAction> tcOrig = TraceCheck.createTraceCheck(mServices, toolkit, mgdScriptTc, truePred,
-					falsePred, trace);
-			final TraceCheck<IAction> tcSeq = TraceCheck.createTraceCheck(mServices, toolkit, mgdScriptTc, truePred,
-					falsePred, sequentialTrace);
-			final String msg = String.format(
-					"Cleaned trace is not '%s', but '%s', sequentialized is '%s', original is '%s'.", LBool.SAT,
-					tc.isCorrect(), tcSeq.isCorrect(), tcOrig.isCorrect());
+			final TraceCheck<IAction> tcOrig =
+					TraceCheck.createTraceCheck(mServices, toolkit, mgdScriptTc, truePred, falsePred, trace);
+			final TraceCheck<IAction> tcSeq =
+					TraceCheck.createTraceCheck(mServices, toolkit, mgdScriptTc, truePred, falsePred, sequentialTrace);
+			final String msg =
+					String.format("Cleaned trace is not '%s', but '%s', sequentialized is '%s', original is '%s'.",
+							LBool.SAT, tc.isCorrect(), tcSeq.isCorrect(), tcOrig.isCorrect());
 			mLogger.fatal(msg);
 			throw new AssertionError(msg);
 		} catch (final ToolchainCanceledException e) {
@@ -439,8 +438,8 @@ public class VerificationResultTransformer {
 	}
 
 	/**
-	 * True iff an {@link IcfgProgramExecution} does not have branch encoders or all
-	 * branch encoders are true (i.e., are generated by
+	 * True iff an {@link IcfgProgramExecution} does not have branch encoders or all branch encoders are true (i.e., are
+	 * generated by
 	 * {@link TraceCheckUtils#computeSomeIcfgProgramExecutionWithoutValues(de.uni_freiburg.informatik.ultimate.automata.Word)}).
 	 */
 	private boolean hasInvalidBranchEncoders(final IcfgProgramExecution<?> pe) {
@@ -455,8 +454,8 @@ public class VerificationResultTransformer {
 	private List<IAction> removeUnrelatedVariables(final List<IAction> sequentialTrace, final ReqCheck reqCheck,
 			final ManagedScript mgdScript) {
 		final String firstPeaName = ReqSymboltableBuilder.getPcName(reqCheck.getPeaNames().iterator().next());
-		final Set<String> equivClass = new HashSet<>(
-				mReqSymbolTable.getVariableEquivalenceClasses().getContainingSet(firstPeaName));
+		final Set<String> equivClass =
+				new HashSet<>(mReqSymbolTable.getVariableEquivalenceClasses().getContainingSet(firstPeaName));
 		equivClass.add(mReqSymbolTable.getDeltaVarName());
 
 		assert equivClass.containsAll(
@@ -492,8 +491,8 @@ public class VerificationResultTransformer {
 				nonTheoryConsts = oldTf.getNonTheoryConsts();
 			} else {
 				mLogger.info("Removing %s variables", toRemove.size());
-				final Term quantifiedFormula = SmtUtils.quantifier(mgdScript.getScript(), QuantifiedFormula.EXISTS,
-						toRemove, oldFormula);
+				final Term quantifiedFormula =
+						SmtUtils.quantifier(mgdScript.getScript(), QuantifiedFormula.EXISTS, toRemove, oldFormula);
 				newFormula = tryToEliminate(mgdScript, quantifiedFormula);
 				final Set<ApplicationTerm> constantsInFormula = SmtUtils.extractConstants(newFormula, false);
 				nonTheoryConsts = oldTf.getNonTheoryConsts().stream()
@@ -519,15 +518,12 @@ public class VerificationResultTransformer {
 	}
 
 	/**
-	 * Apply the branch encoders to all parallel compositions of the original
-	 * program execution and transfer all resulting transformulas from the initial
-	 * script (aux) to the script we are using (tc) before projection.
+	 * Apply the branch encoders to all parallel compositions of the original program execution and transfer all
+	 * resulting transformulas from the initial script (aux) to the script we are using (tc) before projection.
 	 *
-	 * The sequentialisation is necessary to identify the reason for rt
-	 * inconsistency. The transfer happens here because we want to use
-	 * TransFormulaUtils.sequentialComposition, which might timeout during
-	 * quantifier elimination, which in turn might pollute the solver, so we are
-	 * using a separate solver for this.
+	 * The sequentialisation is necessary to identify the reason for rt inconsistency. The transfer happens here because
+	 * we want to use TransFormulaUtils.sequentialComposition, which might timeout during quantifier elimination, which
+	 * in turn might pollute the solver, so we are using a separate solver for this.
 	 */
 	private List<IAction> sequentialize(final IcfgProgramExecution<?> pe, final ManagedScript mgdScriptTc,
 			final ManagedScript mgdScriptAux) {
@@ -551,8 +547,8 @@ public class VerificationResultTransformer {
 			}
 
 			// IActions of the old script
-			final List<IAction> sequentialActions = extractSequential(
-					Collections.singletonList((CodeBlock) ate.getTraceElement()), branchEncoder);
+			final List<IAction> sequentialActions =
+					extractSequential(Collections.singletonList((CodeBlock) ate.getTraceElement()), branchEncoder);
 
 			// Transfer transformulas to new script
 			final List<UnmodifiableTransFormula> transFormulas = sequentialActions.stream()
@@ -605,14 +601,14 @@ public class VerificationResultTransformer {
 
 	private static void dieIfUnsupported(final Spec spec) {
 		switch (spec) {
-		case CONSISTENCY:
-		case VACUOUS:
-		case RTINCONSISTENT:
-		case COMPLEMENT:
-		case REDUNDANCY:
-			return;
-		default:
-			throw new UnsupportedOperationException("Unknown spec type " + spec);
+			case CONSISTENCY:
+			case VACUOUS:
+			case RTINCONSISTENT:
+			case COMPLEMENT:
+			case REDUNDANCY:
+				return;
+			default:
+				throw new UnsupportedOperationException("Unknown spec type " + spec);
 		}
 	}
 }
