a981755c738e86e0d5e4ac8a96e153bcfe947ecf
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2025-01-27 17:51:55 +0100

    Fix typo

--- releaseScripts/default/adds/Ultimate.py
+++ releaseScripts/default/adds/Ultimate.py
@@ -598,7 +598,7 @@ def check_witness_type(witness, type):
         valid = check_witness_type_graphml(witness, type)
     else:
         print(f'Unexpected witness file ending .{witness.rpartition(".")[2]}. '
-              'The witness has to end with .yml, .yaml, or .graphml.)
+              'The witness has to end with .yml, .yaml, or .graphml.')
     if not valid:
         sys.exit(ExitCode.FAIL_WRONG_WITNESS_TYPE)
 
