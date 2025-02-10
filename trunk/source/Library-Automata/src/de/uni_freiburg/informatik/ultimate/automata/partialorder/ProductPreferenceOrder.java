8daea25bc0f60499b85eb0cc4616aa7b32fc8580
Emma Bach <ebach2718@gmail.com>
2025-02-10 17:17:48 +0100

    Started work on product preference orders to solve thread-based problems

---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/ProductPreferenceOrder.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2024 Emma Bach
+ * Copyright (C) 2024 Marcel Ebbinghaus
+ * Copyright (C) 2024 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2024 University of Freiburg
+ *
+ * This file is part of the ULTIMATE TraceCheckerUtils Library.
+ *
+ * The ULTIMATE TraceCheckerUtils Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE TraceCheckerUtils Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE TraceCheckerUtils Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE TraceCheckerUtils Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE TraceCheckerUtils Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.partialorder;
+
+import java.util.Comparator;
+
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.INwaOutgoingLetterAndTransitionProvider;
+import de.uni_freiburg.informatik.ultimate.automata.partialorder.ProductPreferenceOrderAutomaton.IProductPreferenceOrderStateFactory;
+
+public class ProductPreferenceOrder<L, S0, S1, S2, S> implements IPreferenceOrder<L, S0, S> {
+	private final IPreferenceOrder<L, S0, S1> mFirstOrder;
+	private final IPreferenceOrder<L, S0, S2> mSecondOrder;
+	private final IProductPreferenceOrderStateFactory<S1, S2, S> mStateFactory;
+	private final ProductPreferenceOrderAutomaton<L, S1, S2, S> mAutomaton;
+
+	public ProductPreferenceOrder(final IPreferenceOrder<L, S0, S1> fst, final IPreferenceOrder<L, S0, S2> snd,
+			final IProductPreferenceOrderStateFactory<S1, S2, S> stateFactory) {
+		mFirstOrder = fst;
+		mSecondOrder = snd;
+		mStateFactory = stateFactory;
+		mAutomaton = new ProductPreferenceOrderAutomaton<>(mFirstOrder.getMonitor(), mSecondOrder.getMonitor(),
+				mStateFactory);
+	}
+
+	@Override
+	public boolean isPositional() {
+		return mFirstOrder.isPositional() || mSecondOrder.isPositional();
+	}
+
+	@Override
+	public INwaOutgoingLetterAndTransitionProvider<L, S> getMonitor() {
+		return mAutomaton;
+	}
+
+	@Override
+	public Comparator<L> getOrder(final S0 programState, final S monitorState) {
+		// This is the hard part.
+		// Given Objects a and b that we want to compare, we need to:
+		// 1. Check if elements are comparable based on one of the original orders.
+		// If not:
+		// 1. Find intersection of sets talked about by each order
+		// 2. (Ideally) Throw exception if intersection is not a blob
+		// 3. Indirectly compare a and b by comparing them to the intersection.
+
+		// Alternatively, we could construct the full transitive closure of the union
+		// of mFirstOrder and mSecondOrder, but that seems unnecessarily inefficient.
+		return null;
+	}
+}
---
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/partialorder/ProductPreferenceOrderAutomaton.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2024 Emma Bach
+ * Copyright (C) 2024 Marcel Ebbinghaus
+ * Copyright (C) 2024 Dominik Klumpp (klumpp@informatik.uni-freiburg.de)
+ * Copyright (C) 2024 University of Freiburg
+ *
+ * This file is part of the ULTIMATE Automata Library.
+ *
+ * The ULTIMATE Automata Library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Automata Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Automata Library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Automata Library, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Automata Library grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.automata.partialorder;
+
+import java.util.ArrayList;
+
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.INwaOutgoingLetterAndTransitionProvider;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.NestedWordAutomataUtils;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.VpAlphabet;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingCallTransition;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingInternalTransition;
+import de.uni_freiburg.informatik.ultimate.automata.nestedword.transitions.OutgoingReturnTransition;
+import de.uni_freiburg.informatik.ultimate.automata.statefactory.IStateFactory;
+
+/**
+ * Automaton representing the product of two given monitors mLeftAutomaton and mRightAutomaton. This corresponds to the
+ * Automaton needed to solve thread-based problems.
+ *
+ * @param <L>
+ *            letter type
+ * @param <S1>
+ *            state type of mLeftAutomaton
+ * @param <S2>
+ *            state type of mRightAutomaton
+ * @param <S>
+ *            state type of the product Monitor
+ */
+public class ProductPreferenceOrderAutomaton<L, S1, S2, S> implements INwaOutgoingLetterAndTransitionProvider<L, S> {
+	private final INwaOutgoingLetterAndTransitionProvider<L, S1> mLeftAutomaton;
+	private final INwaOutgoingLetterAndTransitionProvider<L, S2> mRightAutomaton;
+	private final IProductPreferenceOrderStateFactory<S1, S2, S> mStateFactory;
+
+	public ProductPreferenceOrderAutomaton(final INwaOutgoingLetterAndTransitionProvider<L, S1> leftAutomaton,
+			final INwaOutgoingLetterAndTransitionProvider<L, S2> rightAutomaton,
+			final IProductPreferenceOrderStateFactory<S1, S2, S> stateFactory) {
+		mLeftAutomaton = leftAutomaton;
+		mRightAutomaton = rightAutomaton;
+		mStateFactory = stateFactory;
+
+		assert NestedWordAutomataUtils.isFiniteAutomaton(leftAutomaton) : "calls and returns are not supported";
+		assert NestedWordAutomataUtils.isFiniteAutomaton(rightAutomaton) : "calls and returns are not supported";
+		assert leftAutomaton.getAlphabet().equals(rightAutomaton.getAlphabet()) : "Alphabets must be the same";
+	}
+
+	@Override
+	@Deprecated
+	public IStateFactory<S> getStateFactory() {
+		return mStateFactory;
+	}
+
+	@Override
+	public VpAlphabet<L> getVpAlphabet() {
+		// In the constructor, we assert that mLeftAutomaton and mRightAutomaton
+		// must have the same Alphabet, so it doesn't matter which one gets returned.
+		return mLeftAutomaton.getVpAlphabet();
+	}
+
+	@Override
+	public S getEmptyStackState() {
+		return null;
+	}
+
+	@Override
+	public Iterable<S> getInitialStates() {
+		final var initials = new ArrayList<S>();
+		for (final var s1 : mLeftAutomaton.getInitialStates()) {
+			for (final var s2 : mRightAutomaton.getInitialStates()) {
+				initials.add(mStateFactory.createProductState(s1, s2));
+			}
+		}
+		return initials;
+	}
+
+	@Override
+	// State (a,b) is initial if a is initial in mLeftAutomaton and b is initial in mRightAutomaton
+	public boolean isInitial(final S state) {
+		return mLeftAutomaton.isInitial(mStateFactory.getLeftState(state))
+				&& mRightAutomaton.isInitial(mStateFactory.getRightState(state));
+	}
+
+	@Override
+	// State (a,b) is final if a is initial in mLeftAutomaton or b is initial in mRightAutomaton
+	// Worth considering if and would be more sensible
+	public boolean isFinal(final S state) {
+		return mLeftAutomaton.isFinal(mStateFactory.getLeftState(state))
+				|| mRightAutomaton.isFinal(mStateFactory.getRightState(state));
+	}
+
+	@Override
+	public int size() {
+		return mLeftAutomaton.size() * mRightAutomaton.size();
+	}
+
+	@Override
+	public String sizeInformation() {
+		return "The product automaton has " + size() + " states.";
+	}
+
+	@Override
+	public Iterable<OutgoingInternalTransition<L, S>> internalSuccessors(final S state, final L letter) {
+		final Iterable<OutgoingInternalTransition<L, S1>> leftTransitions =
+				mLeftAutomaton.internalSuccessors(mStateFactory.getLeftState(state), letter);
+		final Iterable<OutgoingInternalTransition<L, S2>> rightTransitions =
+				mRightAutomaton.internalSuccessors(mStateFactory.getRightState(state), letter);
+
+		// return iterable (specifically an ArrayList) that contains all transitions ((a,b),letter)
+		// where a is an internal successor of (mStateFactory.getLeftState(state),letter) in mLeftAutomaton
+		// and b is an internal successor of (mStateFactory.getRightState(state),letter) in mRightAutomaton
+
+		final ArrayList<OutgoingInternalTransition<L, S>> successors = new ArrayList<>();
+
+		for (final OutgoingInternalTransition<L, S1> leftTransition : leftTransitions) {
+			for (final OutgoingInternalTransition<L, S2> rightTransition : rightTransitions) {
+				final S productState =
+						mStateFactory.createProductState(leftTransition.getSucc(), rightTransition.getSucc());
+				final OutgoingInternalTransition<L, S> productTransition =
+						new OutgoingInternalTransition<>(letter, productState);
+				successors.add(productTransition);
+			}
+		}
+		return successors;
+	}
+
+	@Override
+	public Iterable<OutgoingCallTransition<L, S>> callSuccessors(final S state, final L letter) {
+		throw new UnsupportedOperationException("calls are not supported");
+	}
+
+	@Override
+	public Iterable<OutgoingReturnTransition<L, S>> returnSuccessors(final S state, final S hier, final L letter) {
+		throw new UnsupportedOperationException("returns are not supported");
+	}
+
+	public interface IProductPreferenceOrderStateFactory<S1, S2, S> extends IStateFactory<S> {
+		S createProductState(S1 leftState, S2 rightState);
+
+		S1 getLeftState(S state);
+
+		S2 getRightState(S state);
+	}
+}
