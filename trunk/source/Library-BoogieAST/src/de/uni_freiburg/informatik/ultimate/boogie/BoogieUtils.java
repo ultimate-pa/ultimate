30580a0b48d2e2d5c18b330f23f0a4448d724c8d
Matthias Heizmann <matthias@heizmann.name>
2025-03-05 02:06:43 +0100

    CC fixes (autosave)

--- trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/BoogieUtils.java
+++ trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/BoogieUtils.java
@@ -55,10 +55,9 @@ public class BoogieUtils {
 	}
 
 	/**
-	 * Is there a break statement insides this code that is not part of a while
-	 * statement? (Motivation: We want to detect break statement that may jump out
-	 * of this code. If the break statement occurs in an inner while statement, it
-	 * jumps only after the while statement.)
+	 * Is there a break statement insides this code that is not part of a while statement? (Motivation: We want to
+	 * detect break statement that may jump out of this code. If the break statement occurs in an inner while statement,
+	 * it jumps only after the while statement.)
 	 */
 	public static boolean containsOuterBreak(final Statement[] statementList) {
 		for (final Statement element : statementList) {
