702cf89691fcbac3263ddfec9d9637de0742eac4
Matthias Heizmann <matthias@heizmann.name>
2025-05-04 14:32:18 +0200

    Use IcfgUtils#getAllLocations

--- trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/cfg/IcfgUtils.java
+++ trunk/source/Library-ModelCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/modelcheckerutils/cfg/IcfgUtils.java
@@ -192,8 +192,7 @@ public class IcfgUtils {
 		final Set<IcfgLocation> reachableProgramPoints =
 				new IcfgEdgeIterator(icfg).asStream().map(IcfgEdge::getTarget).collect(Collectors.toSet());
 		reachableProgramPoints.addAll(icfg.getInitialNodes());
-		final Set<IcfgLocation> registeredProgramPoints =
-				icfg.getProgramPoints().values().stream().flatMap(x -> x.values().stream()).collect(Collectors.toSet());
+		final Set<IcfgLocation> registeredProgramPoints = getAllLocations(icfg).collect(Collectors.toSet());
 		final Set<IcfgLocation> diff = DataStructureUtils.difference(reachableProgramPoints, registeredProgramPoints);
 		if (!diff.isEmpty()) {
 			throw new AssertionError("Program points reachable but not registered: " + diff);
@@ -205,10 +204,8 @@ public class IcfgUtils {
 		final Set<IcfgLocation> reachableProgramPoints =
 				new IcfgEdgeIterator(icfg).asStream().map(IcfgEdge::getTarget).collect(Collectors.toSet());
 		reachableProgramPoints.addAll(icfg.getInitialNodes());
-		final Set<LOC> registeredProgramPoints =
-				icfg.getProgramPoints().values().stream().flatMap(x -> x.values().stream()).collect(Collectors.toSet());
-		final Set<IcfgLocation> diff = new HashSet<>(registeredProgramPoints);
-		diff.removeAll(reachableProgramPoints);
+		final Set<IcfgLocation> registeredProgramPoints = getAllLocations(icfg).collect(Collectors.toSet());
+		final Set<IcfgLocation> diff = DataStructureUtils.difference(registeredProgramPoints, reachableProgramPoints);
 		// ExitNodes are registered even if they are not reachable (the optimization
 		// where we omit ExitNodes would require many case distinctions and would only
 		// save a few nodes).
@@ -221,8 +218,7 @@ public class IcfgUtils {
 	}
 
 	public static <LOC extends IcfgLocation> boolean areLocationsOfInterestRegistered(final IIcfg<LOC> icfg) {
-		final Set<LOC> registeredProgramPoints =
-				icfg.getProgramPoints().values().stream().flatMap(x -> x.values().stream()).collect(Collectors.toSet());
+		final Set<LOC> registeredProgramPoints = getAllLocations(icfg).collect(Collectors.toSet());
 		final Set<LOC> diff = DataStructureUtils.difference(icfg.getLocationsOfInterest(), registeredProgramPoints);
 		if (!diff.isEmpty()) {
 			throw new AssertionError("Unregistered location of interest (LOI): " + diff);
@@ -231,8 +227,7 @@ public class IcfgUtils {
 	}
 
 	public static <LOC extends IcfgLocation> boolean areLoopLocationsRegistered(final IIcfg<LOC> icfg) {
-		final Set<LOC> registeredProgramPoints =
-				icfg.getProgramPoints().values().stream().flatMap(x -> x.values().stream()).collect(Collectors.toSet());
+		final Set<LOC> registeredProgramPoints = getAllLocations(icfg).collect(Collectors.toSet());
 		final Set<LOC> diff = DataStructureUtils.difference(icfg.getLoopLocations(), registeredProgramPoints);
 		if (!diff.isEmpty()) {
 			throw new AssertionError("Unregistered loop location: " + diff);
