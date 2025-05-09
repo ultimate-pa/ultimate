00b6274c823f602a5fa17f13f0381bf7c51c7e0a
Daniel Kuechler <kuechlerdaniel34@gmail.com>
2025-05-09 13:18:19 +0200

    Create dynamic, per-thread, maxparallelstates mapping

--- trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/algorithm/concurrent/AbstractLocationMap.java
+++ trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/algorithm/concurrent/AbstractLocationMap.java
@@ -1,18 +1,30 @@
 package de.uni_freiburg.informatik.ultimate.plugins.analysis.abstractinterpretationv2.algorithm.concurrent;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
-public final class AbstractLocationMap<LOC> {
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgLocation;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgLocationIterator;
+
+public final class AbstractLocationMap<LOC extends IcfgLocation> {
 	private final Map<LOC, Integer> mMap = new ConcurrentHashMap<>();
 	private final Function<LOC, Integer> mMappingFunction;
 	private final Map<String, ? extends LOC> mEntryLocs;
+	// say we are thread 1, how many combinations of lcoations can thread 2 and thread 3 be in ?
+	// assuming both have 2 abstract locations each -> 2*2 combinations
+	final Map<String, Integer> mLocationCountMap = new HashMap<>();
+	private final Map<String, Integer> mMaxParallelLocationStates;
 
 	public AbstractLocationMap(final Function<LOC, Integer> mappingFunction,
 			final Map<String, ? extends LOC> entryLocs) {
 		mMappingFunction = mappingFunction;
 		mEntryLocs = entryLocs;
+		mMaxParallelLocationStates = new HashMap<>();
+		calculateMaxParallelLocationStates();
 	}
 
 	public int getAbstractEntryLoc(final String threadName) {
@@ -25,5 +37,38 @@ public final class AbstractLocationMap<LOC> {
 
 	public int getAbstractLocation(final LOC loc) {
 		return mMap.computeIfAbsent(loc, mMappingFunction);
+	}
+
+	public int abstractLocationCountOf(final String thread) {
+		return mLocationCountMap.get(thread);
+	}
+
+	public int maxParallelOtherLocationsOf(final String thread) {
+		return mMaxParallelLocationStates.get(thread);
+	}
+
+	private void calculateMaxParallelLocationStates() {
+		for (final LOC entryLoc : mEntryLocs.values()) {
+			final String ownerThreadString = entryLoc.getProcedure();
+			final Set<Integer> abstractLocationSet = new HashSet<>();
+			int counter = 1;
+			final IcfgLocationIterator<LOC> iter = new IcfgLocationIterator<>(entryLoc);
+			while (iter.hasNext()) {
+				final LOC loc = iter.next();
+				final int abstraction = getAbstractLocation(loc);
+				if (!abstractLocationSet.contains(abstraction)) {
+					counter++;
+				}
+				abstractLocationSet.add(abstraction);
+			}
+			mLocationCountMap.put(ownerThreadString, counter);
+		}
+		for (final String thread : mEntryLocs.keySet()) {
+			mMaxParallelLocationStates.put(thread, 0);
+			for (final String otherThread : mEntryLocs.keySet()) {
+				final int oldMax = mMaxParallelLocationStates.get(thread);
+				mMaxParallelLocationStates.put(thread, oldMax + mLocationCountMap.get(otherThread));
+			}
+		}
 	}
 }
