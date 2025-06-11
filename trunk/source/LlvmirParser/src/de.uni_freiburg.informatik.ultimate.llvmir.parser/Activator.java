0b22aa37c1716a79b24f107a6c47041854d4c7e5
Peter Ritter <peter.ritter@onlinehome.de>
2025-06-11 16:29:24 +0200

    refactored package structure

---
+++ trunk/source/LlvmirParser/src/de.uni_freiburg.informatik.ultimate.llvmir.parser/Activator.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2025 Peter Ritter
+ *
+ * This file is part of the ULTIMATE LlvmirParser plug-in.
+ *
+ * The ULTIMATE LlvmirParser plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE LlvmirParser plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE LlvmirParser plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE LlvmirParser plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE LlvmirParser plug-in grant you additional permission
+ * to convey the resulting work.
+ */
+/**
+ * The activator class controls the plug-in life cycle
+ */
+
+package de.uni_freiburg.informatik.ultimate.llvmir.parser;
+
+/**
+ * @author Peter Ritter
+ * @date 11.06.2025
+ */
+public class Activator {
+	
+	public static final String PLUGIN_ID = "LlvmirParser";
+
+	public static final String PLUGIN_NAME = "LlvmirParser";
+	
+}
