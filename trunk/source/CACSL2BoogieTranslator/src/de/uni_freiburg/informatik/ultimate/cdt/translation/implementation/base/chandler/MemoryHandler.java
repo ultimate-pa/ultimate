cea363365464cf51e5ebdf11a3442d63336eb9a1
Jan Körner <jankoerner.1998@gmail.com>
2025-07-07 13:23:07 +0200

    CACSL2Boogie: Removed ExpressionTranslation
    The ExpressionTranslation was neither used in the abstract base class nor in the subclasses.
    Also removed unnecessary cast of TypeHandler.

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/MemoryHandler.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/MemoryHandler.java
@@ -1145,14 +1145,14 @@ public class MemoryHandler {
 			case HoenickeLindenmann_4ByteResolution:
 			case HoenickeLindenmann_8ByteResolution:
 				yield new MemoryStructure_SingleBitprecise(memoryStructurePreference.getByteSize(), mTypeSizes,
-						(TypeHandler) mTypeHandler, mExpressionTranslation);
+						mTypeHandler);
 			case HoenickeLindenmann_Original:
-				yield new MemoryStructure_MultiBitprecise(mTypeSizes, mTypeHandler, mExpressionTranslation);
+				yield new MemoryStructure_MultiBitprecise(mTypeSizes, mTypeHandler);
 			};
 		}
 		return switch (memoryStructurePreference) {
 		case HoenickeLindenmann_Original:
-			yield new MemoryStructure_Unbounded(mTypeSizes, mTypeHandler, mExpressionTranslation);
+			yield new MemoryStructure_Unbounded(mTypeSizes, mTypeHandler);
 		case HoenickeLindenmann_1ByteResolution:
 		case HoenickeLindenmann_2ByteResolution:
 		case HoenickeLindenmann_4ByteResolution:
