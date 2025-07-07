ef509148c489206659c27bd9a470d6bc7e2680a0
Jan Körner <jankoerner.1998@gmail.com>
2025-07-07 13:27:50 +0200

    CACSL2Boogie: Added MemoryModelFactory
    This factory is used to create concrete instances implementing
    interfaces that are used in the memory model. All validity checks are
    also part of the factory.

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/IMemoryStructure.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/IMemoryStructure.java
@@ -1,5 +1,8 @@
 package de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.chandler;
 
+/**
+ * The interface defining the functions for the different memory structures.
+ */
 public interface IMemoryStructure {
 
 }
