20905a5757b1600456a4db95b505c74b7206a5dc
Jan Körner <jankoerner.1998@gmail.com>
2025-07-10 12:06:38 +0200

    CACSL2Boogie: Removed unused variables

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/MemoryHandler.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/MemoryHandler.java
@@ -1025,7 +1025,6 @@ public class MemoryHandler {
 	 */
 	public Expression multiplyWithSizeOfAnotherType(final ILocation loc, final ICType valueType,
 			final Expression integerExpression, final CPrimitive integerExpresionType) {
-		final Expression timesSizeOf;
 		return mExpressionTranslation.constructArithmeticExpression(loc, IASTBinaryExpression.op_multiply,
 				integerExpression, integerExpresionType, calculateSizeOf(loc, valueType), integerExpresionType);
 	}
@@ -2878,7 +2877,6 @@ public class MemoryHandler {
 
 	private Attribute[] constructExpandAndSmtDefinedAttributesForSubArrayStore(final HeapDataArray heapDataArray,
 			final String... subArrayToStore) {
-		final Attribute[] attributes;
 		final List<Attribute> attributeList = new ArrayList<>();
 
 		final CACSLLocation ignoreLoc1 = LocationFactory.createIgnoreCLocation();
@@ -2928,7 +2926,6 @@ public class MemoryHandler {
 
 	private Attribute[] constructExpandAndSmtDefinedAttributesForSubArraySelect(final HeapDataArray heapDataArray,
 			final String... indices) {
-		final Attribute[] attributes;
 		final List<Attribute> attributeList = new ArrayList<>();
 
 		final CACSLLocation ignoreLoc1 = LocationFactory.createIgnoreCLocation();
