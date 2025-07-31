174434b1d39ca7b1ad4f06f2164c99aeba96bacc
Daniel Kuechler <kuechlerdaniel34@gmail.com>
2025-07-31 16:46:42 +0200

    Default to 3 statesMax and 2 iterations till Widening

--- trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/preferences/AbsIntPrefInitializer.java
+++ trunk/source/AbstractInterpretationV2/src/de/uni_freiburg/informatik/ultimate/plugins/analysis/abstractinterpretationv2/preferences/AbsIntPrefInitializer.java
@@ -105,8 +105,8 @@ public class AbsIntPrefInitializer extends UltimatePreferenceInitializer {
 	public static final boolean DEF_RUN_AS_PRE_ANALYSIS = false;
 	public static final String DEF_ABSTRACT_DOMAIN = VALUES_ABSTRACT_DOMAIN[0];
 	public static final String DEF_ABSTRACT_DOMAIN_FUTURE = VALUES_ABSTRACT_DOMAIN_FUTURE[0];
-	public static final int DEF_MAXSTATES_CONC = 4;
-	public static final int DEF_INTERFERENCE_ITERATIONS_UNTIL_WIDENING = 4;
+	public static final int DEF_MAXSTATES_CONC = 3;
+	public static final int DEF_INTERFERENCE_ITERATIONS_UNTIL_WIDENING = 2;
 
 	public static final String INDENT = "   ";
 	public static final String DINDENT = INDENT + INDENT;
