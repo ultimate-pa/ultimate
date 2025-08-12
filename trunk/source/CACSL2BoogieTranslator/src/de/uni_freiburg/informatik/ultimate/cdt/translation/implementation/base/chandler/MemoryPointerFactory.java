ca4f85e1fff1d74d90b25b83711197466211c136
Jan Körner <jankoerner.1998@gmail.com>
2025-08-12 13:49:47 +0200

    CACSL2Boogie: Hide memory pointer constructor
    Hide the memory pointer constructor with a static create method in each
    class in order to ensure that the 1D pointer is only created when the
    settings are compatible.

---
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/MemoryPointerFactory.java
@@ -0,0 +1,29 @@
+package de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.chandler;
+
+import de.uni_freiburg.informatik.ultimate.boogie.type.BoogieType;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.TranslationSettings;
+
+/**
+ * The factory used to create an IMemoryPointer instance.
+ */
+public abstract class MemoryPointerFactory {
+	/**
+	 * The factory method used to create an IMemoryPointer instance.
+	 *
+	 * @return A IMemoryPointer instance.
+	 */
+	public static IMemoryPointer create(final TranslationSettings settings, final BoogieType boogieType,
+			final TypeSizes typeSizes) {
+		final var memoryAddressingPreference = settings.memoryAddressingPreference();
+
+		switch (memoryAddressingPreference) {
+		case One_Dimensional:
+			return OneDimensionalPointer.create(settings, boogieType, typeSizes);
+		case Two_Dimensional:
+			return TwoDimensionalPointer.create(settings, boogieType, typeSizes);
+		default:
+			throw new UnsupportedOperationException(
+					"MemoryAddressing: " + memoryAddressingPreference + " not implemented yet.");
+		}
+	}
+}
