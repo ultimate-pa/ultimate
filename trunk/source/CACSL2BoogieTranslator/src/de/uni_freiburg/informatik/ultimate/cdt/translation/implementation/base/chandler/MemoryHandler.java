cb8cdbef62f661454ecb0a054f1e190510d751e7
Jan Körner <jankoerner.1998@gmail.com>
2025-08-23 01:41:12 +0200

    CACSL2Boogie: Fixed structs in arrays
    There was problem that led to a Classcast exception when the program
    contained an array full of structs.

--- trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/MemoryHandler.java
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/chandler/MemoryHandler.java
@@ -2048,7 +2048,8 @@ public class MemoryHandler {
 
 		final Expression arrayAddress = hlv.getAddress();
 		final Expression valueTypeSize = calculateSizeOf(loc, valueType.getValueType());
-		final int typeSize = mTypeSizes.extractIntegerValue(valueTypeSize, arrayValueType).intValue();
+		final int typeSize = mTypeSizes
+				.extractIntegerValue(valueTypeSize, mExpressionTranslation.getCTypeOfPointerComponents()).intValue();
 
 		final int dim = dimBigInteger.intValue();
 
