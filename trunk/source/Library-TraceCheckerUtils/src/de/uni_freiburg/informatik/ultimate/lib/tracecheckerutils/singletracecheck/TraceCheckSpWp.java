d36a0da05e48b021e5c64fcd81b1e7dd2bd6d703
Max Barth <max.barth95@gmx.de>
2025-12-28 13:24:27 +0100

    forgot to remove a comment

--- trunk/source/Library-TraceCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/singletracecheck/TraceCheckSpWp.java
+++ trunk/source/Library-TraceCheckerUtils/src/de/uni_freiburg/informatik/ultimate/lib/tracecheckerutils/singletracecheck/TraceCheckSpWp.java
@@ -172,7 +172,6 @@ public class TraceCheckSpWp<L extends IAction> extends InterpolatingTraceCheck<L
 			mInterpolantComputationStatus =
 					new InterpolantComputationStatus(ItpErrorStatus.SMT_SOLVER_CANNOT_INTERPOLATE_INPUT, null);
 		}
-		mTcSmtManager.getScript().exit(); // TODO anschauen ob wirs noch bruachen
 	}
 
 	@Override
