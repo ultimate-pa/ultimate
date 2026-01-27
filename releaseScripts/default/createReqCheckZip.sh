f7f81e3a8be91d62974669a3b06945f140167222
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2026-01-27 10:48:26 +0100

    Remove old version of CVC4 that is not used anymore

--- releaseScripts/default/createReqCheckZip.sh
+++ releaseScripts/default/createReqCheckZip.sh
@@ -40,12 +40,12 @@ if [ "$2" == "linux" ]; then
     echo "Building .zip for linux..."
 	ARCH="linux"
 	ARCHPATH="products/CLI-E4/linux/gtk/x86_64"
-    ADDS+=("adds/z3" "adds/cvc4nyu" "adds/cvc4" "adds/mathsat")
+    ADDS+=("adds/z3" "adds/cvc4" "adds/mathsat")
 elif [ "$2" == "win32" ]; then
 	echo "Building .zip for win32..."
 	ARCH="win32"
 	ARCHPATH="products/CLI-E4/win32/win32/x86_64"
-    ADDS+=("adds/z3.exe" "adds/cvc4nyu.exe" "adds/cvc4.exe" "adds/mathsat.exe" "adds/gmp.dll" "adds/mathsat.dll")
+    ADDS+=("adds/z3.exe" "adds/cvc4.exe" "adds/mathsat.exe" "adds/gmp.dll" "adds/mathsat.dll")
 else
     echo "Wrong argument: ""$2"" -- use 'linux' or 'win32'"
 	exit 1
