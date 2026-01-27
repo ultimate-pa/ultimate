f853397ce014aeb34c905ea9860b8133104fc539
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2026-01-27 11:57:35 +0100

    Replace CVC4 with bitwuzla / CVC5 in releaseScripts

--- releaseScripts/default/adds/Ultimate.py
+++ releaseScripts/default/adds/Ultimate.py
@@ -686,8 +686,6 @@ def debug_environment():
     solver_versions = [
         ("z3", "-version"),
         ("mathsat", "-version"),
-        ("cvc4", "--version"),
-        ("cvc4nyu", "--version"),
         ("cvc5", "--version"),
         ("bitwuzla", "--version"),
     ]
