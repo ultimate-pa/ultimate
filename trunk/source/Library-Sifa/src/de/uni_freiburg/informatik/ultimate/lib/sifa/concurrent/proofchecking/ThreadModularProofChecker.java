b19cc7ba0ff1e04db35496d4e501ada31754e858
dkuechler <kuechlerdaniel34@gmail.com>
2026-02-03 00:26:03 +0100

    Trim down proofchecking to single class, make interference check non-redundant

--- trunk/source/Library-Sifa/src/de/uni_freiburg/informatik/ultimate/lib/sifa/concurrent/proofchecking/ThreadModularProofChecker.java
+++ trunk/source/Library-Sifa/src/de/uni_freiburg/informatik/ultimate/lib/sifa/concurrent/proofchecking/ThreadModularProofChecker.java
@@ -1,72 +1,109 @@
 package de.uni_freiburg.informatik.ultimate.lib.sifa.concurrent.proofchecking;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.CfgSmtToolkit;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IIcfg;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IIcfgInternalTransition;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IInternalAction;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgEdge;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.structure.IcfgLocation;
+import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.cfg.transitions.TransFormula;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.hoaretriple.MonolithicHoareTripleChecker;
 import de.uni_freiburg.informatik.ultimate.lib.modelcheckerutils.smt.predicates.IPredicate;
-import de.uni_freiburg.informatik.ultimate.lib.sifa.concurrent.interference.IInterferenceAbstraction;
 import de.uni_freiburg.informatik.ultimate.lib.sifa.concurrent.primedFormulas.RelationalPredicatePostcondition;
+import de.uni_freiburg.informatik.ultimate.lib.sifa.concurrent.primedFormulas.TransFormulaToInterferencePredicate;
 import de.uni_freiburg.informatik.ultimate.lib.sifa.domain.IDomain;
+import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.IncrementalPlicationChecker.Validity;
+import de.uni_freiburg.informatik.ultimate.lib.smtlibutils.SmtUtils;
 
 /**
- * Proof checking: (1) initial locations, (2) inductiveness, (3) interference freedom.
+ * Checks: (1) initial locations are true, (2) edges are inductive, (3) predicates are interference-stable.
  */
 public class ThreadModularProofChecker {
 
 	private final ILogger mLogger;
-	private final InductivenessCheck mInductivenessCheck;
-	private final InterferenceFreedomCheck mInterferenceCheck;
+	private final MonolithicHoareTripleChecker mHoareTripleChecker;
+	private final RelationalPredicatePostcondition mPostcondition;
+	private final TransFormulaToInterferencePredicate mTranslator;
+	private final IDomain mDomain;
 
 	public ThreadModularProofChecker(final ILogger logger, final CfgSmtToolkit cfgSmtToolkit,
-			final RelationalPredicatePostcondition postcondition, final IDomain domain) {
+			final RelationalPredicatePostcondition postcondition, final TransFormulaToInterferencePredicate translator,
+			final IDomain domain) {
 		mLogger = logger;
-		mInductivenessCheck = new InductivenessCheck(new MonolithicHoareTripleChecker(cfgSmtToolkit));
-		mInterferenceCheck = new InterferenceFreedomCheck(postcondition, domain);
+		mHoareTripleChecker = new MonolithicHoareTripleChecker(cfgSmtToolkit);
+		mPostcondition = postcondition;
+		mTranslator = translator;
+		mDomain = domain;
 	}
 
-	public ProofCheckResult checkAll(final IIcfg<IcfgLocation> icfg,
-			final Map<IcfgLocation, IPredicate> locationPredicates, final IInterferenceAbstraction interferences,
-			final Map<String, Map<IcfgLocation, IPredicate>> threadPredicates) {
+	/** Returns true if proof is valid, false otherwise. Violations are logged. */
+	public boolean checkAll(final IIcfg<IcfgLocation> icfg, final Map<IcfgLocation, IPredicate> locPreds,
+			final Map<String, Map<IcfgLocation, IPredicate>> threadPreds) {
 
-		final List<String> allViolations = new ArrayList<>();
+		boolean valid = true;
 
-		final ProofCheckResult initialResult = InitialLocationCheck.check(icfg, locationPredicates);
-		if (!initialResult.isValid()) {
-			allViolations.addAll(initialResult.getViolations());
+		// Check 1: Initial location(s) must be true
+		for (final var loc : icfg.getInitialNodes()) {
+			final var pred = locPreds.get(loc);
+			if (pred == null || !SmtUtils.isTrueLiteral(pred.getFormula())) {
+				mLogger.error("Initial location %s is not true", loc);
+				valid = false;
+			}
 		}
 
-		final ProofCheckResult inductiveResult = mInductivenessCheck.check(locationPredicates);
-		if (!inductiveResult.isValid()) {
-			allViolations.addAll(inductiveResult.getViolations());
+		// Check 2: Hoare Triple Check
+		// TODO: do we only need to check internals, since interferences might change even skips()?
+		for (final var entry : locPreds.entrySet()) {
+			final var pre = entry.getValue();
+			for (final IcfgEdge edge : entry.getKey().getOutgoingEdges()) {
+				final var post = locPreds.get(edge.getTarget());
+				if (post != null && edge instanceof IIcfgInternalTransition<?>
+						&& mHoareTripleChecker.checkInternal(pre, (IInternalAction) edge, post) == Validity.INVALID) {
+					mLogger.error("Invalid: {%s} %s->%s {%s}", pre.getFormula(), edge.getSource(), edge.getTarget(),
+							post.getFormula());
+					valid = false;
+				}
+			}
 		}
 
-		final ProofCheckResult interferenceResult = mInterferenceCheck.checkAllThreads(threadPredicates, interferences);
-		if (!interferenceResult.isValid()) {
-			allViolations.addAll(interferenceResult.getViolations());
+		// Check 3: Predicates must be stable under interferences
+		for (final var threadEntry : threadPreds.entrySet()) {
+			final String threadId = threadEntry.getKey();
+
+			for (final var locEntry : threadEntry.getValue().entrySet()) {
+				final var location = locEntry.getKey();
+				final var pred = locEntry.getValue();
+
+				// TODO: must adjust depending on self-interference
+				for (final var otherEntry : threadPreds.entrySet()) {
+					if (otherEntry.getKey().equals(threadId)) {
+						continue;
+					}
+					for (final var otherLoc : otherEntry.getValue().keySet()) {
+						for (final IcfgEdge edge : otherLoc.getOutgoingEdges()) {
+							if (modifiesGlobals(edge.getTransformula())) {
+								final IPredicate itfPred = mTranslator.translate(edge.getTransformula());
+								final IPredicate postState = mPostcondition.strongestPostcondition(pred, itfPred);
+								if (!mDomain.isSubsetEq(postState, pred).isTrueForAbstraction()) {
+									mLogger.error("Unstable %s under edge %s->%s: P=%s", location, edge.getSource(),
+											edge.getTarget(), pred.getFormula());
+									valid = false;
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 
-		if (allViolations.isEmpty()) {
-			mLogger.info("Proof check: PASSED");
-			return ProofCheckResult.valid();
-		}
-		mLogger.warn("Proof check: FAILED (%d violations)", allViolations.size());
-		return ProofCheckResult.invalid(allViolations);
+		mLogger.info("Proof check: %s", valid ? "PASSED" : "FAILED");
+		return valid;
 	}
 
-	public ProofCheckResult checkAll(final IIcfg<IcfgLocation> icfg,
-			final Map<IcfgLocation, IPredicate> locationPredicates, final IInterferenceAbstraction interferences) {
-		final Map<String, Map<IcfgLocation, IPredicate>> threadPredicates = new HashMap<>();
-		for (final Map.Entry<IcfgLocation, IPredicate> entry : locationPredicates.entrySet()) {
-			final String procedure = entry.getKey().getProcedure();
-			threadPredicates.computeIfAbsent(procedure, k -> new HashMap<>()).put(entry.getKey(), entry.getValue());
-		}
-		return checkAll(icfg, locationPredicates, interferences, threadPredicates);
+	private static boolean modifiesGlobals(final TransFormula tf) {
+		return tf != null && tf.getAssignedVars().stream().anyMatch(pv -> pv.isGlobal());
 	}
 }
