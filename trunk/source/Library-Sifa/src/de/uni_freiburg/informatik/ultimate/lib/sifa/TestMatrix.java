799e8c7adafc003b0d9751b384ff76663fb9e05f
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2026-02-23 11:12:14 +0100

    Add ojAlgo to Library-Sifa and add example usage

---
+++ trunk/source/Library-Sifa/src/de/uni_freiburg/informatik/ultimate/lib/sifa/TestMatrix.java
@@ -0,0 +1,10 @@
+package de.uni_freiburg.informatik.ultimate.lib.sifa;
+
+import org.ojalgo.matrix.MatrixQ128;
+
+public class TestMatrix {
+	public TestMatrix() {
+		// See https://www.ojalgo.org/code-examples/ for more examples
+		final var x = MatrixQ128.FACTORY.make(10, 10);
+	}
+}
