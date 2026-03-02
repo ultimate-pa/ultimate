6960b5dd8051dd47d272d98d15f35d934cb420d3
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2026-03-02 17:53:24 +0100

    Remove unused methods

--- trunk/source/IcfgInterpreter/src/de/uni_freiburg/informatik/ultimate/plugins/generator/icfginterpreter/Util.java
+++ trunk/source/IcfgInterpreter/src/de/uni_freiburg/informatik/ultimate/plugins/generator/icfginterpreter/Util.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.uni_freiburg.informatik.ultimate.logic.ApplicationTerm;
-import de.uni_freiburg.informatik.ultimate.logic.Rational;
 import de.uni_freiburg.informatik.ultimate.logic.SMTLIBConstants;
 import de.uni_freiburg.informatik.ultimate.logic.Sort;
 import de.uni_freiburg.informatik.ultimate.logic.Term;
@@ -13,28 +12,6 @@ import de.uni_freiburg.informatik.ultimate.logic.TermVariable;
 import de.uni_freiburg.informatik.ultimate.plugins.generator.icfginterpreter.ProgramExecutions.Pair;
 
 public class Util {
-	public static Long SMTDiv(final long m, final long n) {
-		final Rational div = Rational.valueOf(m, n);
-		if (n > 0) {
-			// n > 0, (div m n) = floor(m/n)
-			final Rational floorExact = div.floor();
-			return floorExact.numerator().longValue() / floorExact.denominator().longValue();
-		}
-		// n < 0, (div m n) = ceil(m/n)
-		final Rational ceilExact = div.ceil();
-		return ceilExact.numerator().longValue() / ceilExact.denominator().longValue();
-	}
-
-	public static Long SMTMod(final Long m, final Long n) {
-		// i == ((i / j) * j) + (i % j)
-		// i % j == i - ((i / j) * j)
-		final Rational div = Rational.valueOf(Util.SMTDiv(m, n), 1L);
-		final Rational mSafe = Rational.valueOf(m, 1L);
-		final Rational nSafe = Rational.valueOf(n, 1L);
-		final Rational result = mSafe.sub(div.mul(nSafe));
-		return result.numerator().longValueExact(); // should be long because all used numbers are long
-	}
-
 	public static int getBitVecLength(final Sort sort) {
 		if (!sort.isBitVecSort()) {
 			return -1;
