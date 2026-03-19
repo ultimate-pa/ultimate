15f368dd1ea7f88e2222d0a84a9293aa6c6f3548
matthiasz <matthiaszumkeller10@gmail.com>
2026-03-19 14:45:49 +0100

    Bugfix: Also replace atomics in nested blocks, after an atomic_end

---
+++ trunk/examples/concurrent/interrupt_driven_programs/atomic-in-else.c
@@ -0,0 +1,40 @@
+//#Safe
+
+#include <pthread.h>
+extern void __VERIFIER_atomic_begin();
+extern void __VERIFIER_atomic_end();
+
+typedef unsigned long int pthread_t;
+int x = 1;
+int y = 1;
+int z = 1;
+
+void *foo(void *arg) {
+  __VERIFIER_atomic_begin();
+  x = x > 0 ? x : -x;
+  y = y > 0 ? y : -y;
+  z = x > y ? x - y : (y > x ? y - x : 0);
+  x = x + 1;
+  if (x < z) {
+    z = 0;
+    __VERIFIER_atomic_end();
+    y = 5;
+  } else {
+    z = 1;
+    __VERIFIER_atomic_end();
+    y = 7;
+    __VERIFIER_atomic_begin();
+    y = 9;
+    __VERIFIER_atomic_end();
+  }
+  return (void*)NULL;
+}
+
+int main() {
+  pthread_t threadId;
+  pthread_create(&threadId, NULL, &foo, NULL);
+
+  y = 0;
+
+  //@ assert z >= 0;
+}
