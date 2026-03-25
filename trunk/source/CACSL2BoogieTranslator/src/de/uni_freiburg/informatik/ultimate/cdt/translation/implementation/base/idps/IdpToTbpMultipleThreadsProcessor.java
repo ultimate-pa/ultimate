c8c9848793d105026e824861d595aaf5ae40ef37
matthiasz <matthiaszumkeller10@gmail.com>
2026-03-25 19:53:35 +0100

    Add IPostProcessor, struct for ISR and functions for the traslation of IDPs to TBP

---
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/idps/IdpToTbpMultipleThreadsProcessor.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (C) 2026 Matthias Zumkeller
+ * Copyright (C) 2026 University of Freiburg
+ *
+ * This file is part of the ULTIMATE CACSL2BoogieTranslator plug-in.
+ *
+ * The ULTIMATE BoogiePreprocessor plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE BoogiePreprocessor plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE BoogiePreprocessor plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE BoogiePreprocessor plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE BoogiePreprocessor plug-in grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.idps;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.cdt.core.dom.ast.IASTNode;
+
+import de.uni_freiburg.informatik.ultimate.boogie.DeclarationInformation;
+import de.uni_freiburg.informatik.ultimate.boogie.ExpressionFactory;
+import de.uni_freiburg.informatik.ultimate.boogie.StatementFactory;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.Attribute;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.Declaration;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.Expression;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.IdentifierExpression;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.LeftHandSide;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.LoopInvariantSpecification;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.Procedure;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.Statement;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.UnaryExpression.Operator;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.VarList;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.VariableDeclaration;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.VariableLHS;
+import de.uni_freiburg.informatik.ultimate.boogie.ast.WhileStatement;
+import de.uni_freiburg.informatik.ultimate.boogie.type.BoogieType;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.FlatSymbolTable;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.LocationFactory;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.CHandler;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.CTranslationResultReporter;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.TranslationSettings;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.chandler.FunctionHandler;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.chandler.IPostProcessor;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.chandler.InitializationHandler;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.chandler.MemoryHandler;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.chandler.ProcedureManager;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.chandler.StaticObjectsHandler;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.chandler.TypeSizes;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.expressiontranslation.ExpressionTranslation;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.container.AuxVarInfoBuilder;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.result.ExpressionResultBuilder;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.util.SFO;
+import de.uni_freiburg.informatik.ultimate.cdt.translation.interfaces.handler.ITypeHandler;
+import de.uni_freiburg.informatik.ultimate.core.model.models.ILocation;
+import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
+
+public class IdpToTbpMultipleThreadsProcessor implements IPostProcessor {
+
+	private final ILogger mLogger;
+
+	private final ExpressionTranslation mExpressionTranslation;
+	private final CTranslationResultReporter mReporter;
+
+	/*
+	 * Decides if the PostProcessor declares the special function that we use for converting Boogie-Real to a
+	 * Boogie-Int. This is needed when we do a cast from float to int in C.
+	 */
+	private final boolean mDeclareToIntFunction = false;
+
+	private final ITypeHandler mTypeHandler;
+
+	private final AuxVarInfoBuilder mAuxVarInfoBuilder;
+
+	private final FlatSymbolTable mSymboltable;
+
+	private final MemoryHandler mMemoryHandler;
+
+	private final ProcedureManager mProcedureManager;
+
+	private final CHandler mCHandler;
+
+	private final TypeSizes mTypeSize;
+
+	private final StaticObjectsHandler mStaticObjectsHandler;
+
+	private final InitializationHandler mInitHandler;
+
+	private final TranslationSettings mSettings;
+
+	private final FunctionHandler mFunctionhandler;
+
+	private final Set<String> mFunctions;
+
+	private final ILocation mIgnoreLoc = LocationFactory.createIgnoreCLocation();
+
+	public IdpToTbpMultipleThreadsProcessor(final ILogger logger, final ExpressionTranslation expressionTranslation,
+			final ITypeHandler typeHandler, final CTranslationResultReporter reporter,
+			final AuxVarInfoBuilder auxVarInfoBuilder, final Set<String> functions, final TypeSizes typeSizes,
+			final FlatSymbolTable symbolTable, final StaticObjectsHandler staticObjectsHandler,
+			final TranslationSettings settings, final ProcedureManager procedureManager,
+			final MemoryHandler memoryHandler, final InitializationHandler initHandler,
+			final FunctionHandler functionhandler, final CHandler chandler) {
+		mLogger = logger;
+		mExpressionTranslation = expressionTranslation;
+		mReporter = reporter;
+		mTypeHandler = typeHandler;
+		mAuxVarInfoBuilder = auxVarInfoBuilder;
+		mFunctions = functions;
+		mTypeSize = typeSizes;
+		mSymboltable = symbolTable;
+		mStaticObjectsHandler = staticObjectsHandler;
+		mSettings = settings;
+		mProcedureManager = procedureManager;
+		mMemoryHandler = memoryHandler;
+		mInitHandler = initHandler;
+		mFunctionhandler = functionhandler;
+		mCHandler = chandler;
+	}
+
+	@Override
+	public List<Declaration> postProcess(final ILocation loc, final IASTNode hook,
+			final List<Statement> additionalInitializations) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	Map<Integer, IdentifierExpression> constructIntEnabledExpressions(final List<Integer> identifiers) {
+		final var idExpressions = new HashMap<Integer, IdentifierExpression>();
+		for (final Integer irq : identifiers) {
+			final var id = "gpio_int" + irq + "_enabled";
+			final var enabledExpr = ExpressionFactory.constructIdentifierExpression(mIgnoreLoc, BoogieType.TYPE_BOOL,
+					id, DeclarationInformation.DECLARATIONINFO_GLOBAL);
+			idExpressions.put(irq, enabledExpr);
+		}
+		return idExpressions;
+	}
+
+	Statement constructIntEnabledInitializations(final List<VariableLHS> leftHandSides) {
+		final Expression assignment = ExpressionFactory.createBooleanLiteral(mIgnoreLoc, false);
+		final Expression[] assignments = new Expression[leftHandSides.size()];
+		Arrays.fill(assignments, assignment);
+		return StatementFactory.constructAssignmentStatement(mIgnoreLoc, leftHandSides.toArray(new LeftHandSide[0]),
+				assignments);
+	}
+
+	void modifyIntEnableProcedure(final Procedure intEnableProcedure, final VariableLHS intEnabledLhs) {
+		// TODO: Modify Specification
+		final var body = intEnableProcedure.getBody();
+		final var block = body.getBlock();
+		final var assignment = StatementFactory.constructSingleAssignmentStatement(mIgnoreLoc, intEnabledLhs,
+				ExpressionFactory.createBooleanLiteral(mIgnoreLoc, true));
+		final var newBlock = Arrays.asList(block);
+		newBlock.add(assignment);
+		final var atomic = StatementFactory.constructAtomicStatement(mIgnoreLoc, newBlock);
+		body.setBlock(new Statement[] { atomic });
+	}
+
+	Declaration constructThreadGpioProc(final String identifier, final IdentifierExpression threadEnabledId,
+			final Integer irq) {
+		final var procName = constructThreadGpioID(irq);
+		final var declaration = new Procedure(mIgnoreLoc, new Attribute[0], procName, new String[0], new VarList[0],
+				new VarList[0], null, null);
+		mProcedureManager.beginCustomProcedure(mCHandler, mIgnoreLoc, SFO.INIT, declaration);
+		final ExpressionResultBuilder builder = new ExpressionResultBuilder();
+		final var whileStmt = constructIsrWhileLoop(identifier, threadEnabledId);
+		builder.addStatement(whileStmt);
+		final var body = mProcedureManager.constructBody(mIgnoreLoc,
+				builder.getDeclarations().toArray(new VariableDeclaration[builder.getDeclarations().size()]),
+				builder.getStatements().toArray(new Statement[builder.getStatements().size()]), procName);
+		mProcedureManager.endCustomProcedure(mCHandler, SFO.INIT);
+		return new Procedure(mIgnoreLoc, new Attribute[0], procName, new String[0], new VarList[0], new VarList[0],
+				null, body);
+	}
+
+	Statement constructIsrWhileLoop(final String identifier, final IdentifierExpression threadEnabledId) {
+		final var then = StatementFactory.constructCallStatement(mIgnoreLoc, false, new VariableLHS[0], identifier,
+				new Expression[0]);
+		final var enabledExpr =
+				ExpressionFactory.constructUnaryExpression(mIgnoreLoc, Operator.LOGICNEG, threadEnabledId);
+		final var ifStmt =
+				StatementFactory.constructIfStatement(mIgnoreLoc, enabledExpr, new Statement[] { then }, null);
+		final var atomic = StatementFactory.constructAtomicStatement(mIgnoreLoc, List.of(ifStmt));
+		final var alwaysTrue = ExpressionFactory.createBooleanLiteral(mIgnoreLoc, true);
+		return new WhileStatement(mIgnoreLoc, alwaysTrue, new LoopInvariantSpecification[0],
+				new Statement[] { atomic });
+	}
+
+	String constructThreadGpioID(final Integer irq) {
+		return "thr_gpio" + irq;
+	}
+}
---
+++ trunk/source/CACSL2BoogieTranslator/src/de/uni_freiburg/informatik/ultimate/cdt/translation/implementation/base/idps/InterruptServiceRoutines.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2026 Matthias Zumkeller
+ * Copyright (C) 2026 University of Freiburg
+ *
+ * This file is part of the ULTIMATE CACSL2BoogieTranslator plug-in.
+ *
+ * The ULTIMATE BoogiePreprocessor plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE BoogiePreprocessor plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE BoogiePreprocessor plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE BoogiePreprocessor plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE BoogiePreprocessor plug-in grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.cdt.translation.implementation.base.idps;
+
+import java.util.Map;
+
+/**
+ * Simple struct that contains information about the interrupt-service-routines in program. Names of the ISR related
+ * functions are stored as Strings.
+ */
+public class InterruptServiceRoutines {
+	// Map IRQ to ISR Name
+	private Map<Integer, String> mISRMap;
+
+	// Names of the corresponding functions
+
+	private String mRequestEnable;
+	private String mRequestDisable;
+	private String mSetPriority;
+	private String mGetPriority;
+
+	public InterruptServiceRoutines(final Map<Integer, String> isrMap, final String reqEnable, final String reqDisable,
+			final String setPrio, final String getPrio) {
+		mISRMap = isrMap;
+		mRequestEnable = reqEnable;
+		mRequestDisable = reqDisable;
+		mSetPriority = setPrio;
+		mGetPriority = getPrio;
+	}
+
+	public Map<Integer, String> getISRMap() {
+		return mISRMap;
+	}
+
+	public String getRequestEnable() {
+		return mRequestEnable;
+	}
+
+	public String getRequestDisable() {
+		return mRequestDisable;
+	}
+
+	public String getSetPriority() {
+		return mSetPriority;
+	}
+
+	public String getGetPriority() {
+		return mGetPriority;
+	}
+
+	public void setISRMap(final Map<Integer, String> isrMap) {
+		mISRMap = isrMap;
+	}
+
+	public void setRequestEnable(final String reqEnable) {
+		mRequestEnable = reqEnable;
+	}
+
+	public void setRequestDisable(final String reqDisable) {
+		mRequestDisable = reqDisable;
+	}
+
+	public void setSetPriority(final String setPrio) {
+		mSetPriority = setPrio;
+	}
+
+	public void setGetPriority(final String getPrio) {
+		mGetPriority = getPrio;
+	}
+}
