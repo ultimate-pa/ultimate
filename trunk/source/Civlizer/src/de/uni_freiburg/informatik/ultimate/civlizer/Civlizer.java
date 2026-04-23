eab818e7c201fd6c9c299f97b33f26dcbdff05d1
maul.esel <maul.esel@posteo.de>
2026-04-23 11:17:41 +0200

    add Civlizer plugin

---
+++ trunk/source/Civlizer/src/de/uni_freiburg/informatik/ultimate/civlizer/Civlizer.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2026 Dominik Klumpp (klumpp@lix.polytechnique.fr)
+ * Copyright (C) 2026 École Polytechnique
+ *
+ * This file is part of the ULTIMATE Civlizer plug-in.
+ *
+ * The ULTIMATE Civlizer plug-in is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ULTIMATE Civlizer plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ULTIMATE Civlizer plug-in. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ * If you modify the ULTIMATE Civlizer plug-in, or any covered work, by linking
+ * or combining it with Eclipse RCP (or a modified version of Eclipse RCP),
+ * containing parts covered by the terms of the Eclipse Public License, the
+ * licensors of the ULTIMATE Civlizer plug-in grant you additional permission
+ * to convey the resulting work.
+ */
+package de.uni_freiburg.informatik.ultimate.civlizer;
+
+import java.util.Collections;
+import java.util.List;
+
+import de.uni_freiburg.informatik.ultimate.boogie.ast.Unit;
+import de.uni_freiburg.informatik.ultimate.core.model.IAnalysis;
+import de.uni_freiburg.informatik.ultimate.core.model.models.IElement;
+import de.uni_freiburg.informatik.ultimate.core.model.models.ModelType;
+import de.uni_freiburg.informatik.ultimate.core.model.observers.IObserver;
+import de.uni_freiburg.informatik.ultimate.core.model.observers.IUnmanagedObserver;
+import de.uni_freiburg.informatik.ultimate.core.model.preferences.IPreferenceInitializer;
+import de.uni_freiburg.informatik.ultimate.core.model.services.IUltimateServiceProvider;
+
+/**
+ * This class initializes the Civlizer.
+ *
+ * @author Dominik Klumpp (klumpp@lix.polytechnique.fr)
+ *
+ */
+public class Civlizer implements IAnalysis, IUnmanagedObserver {
+
+	private IUltimateServiceProvider mServices;
+
+	@Override
+	public String getPluginName() {
+		return Activator.PLUGIN_NAME;
+	}
+
+	@Override
+	public String getPluginID() {
+		return Activator.PLUGIN_ID;
+	}
+
+	@Override
+	public void init() {
+		// not needed
+	}
+
+	/**
+	 * I give you every model.
+	 */
+	@Override
+	public ModelQuery getModelQuery() {
+		return ModelQuery.LAST;
+	}
+
+	/**
+	 * I don't need a special tool
+	 */
+	@Override
+	public List<String> getDesiredToolIds() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public ModelType getOutputDefinition() {
+		/* use old graph type definition */
+		return null;
+	}
+
+	@Override
+	public void setInputDefinition(final ModelType graphType) {
+		// not required.
+	}
+
+	@Override
+	public List<IObserver> getObservers() {
+		return List.of(this);
+	}
+
+	@Override
+	public boolean isGuiRequired() {
+		return false;
+	}
+
+	@Override
+	public IPreferenceInitializer getPreferences() {
+		return null; // new PreferenceInitializer();
+	}
+
+	@Override
+	public void setServices(final IUltimateServiceProvider services) {
+		mServices = services;
+	}
+
+	@Override
+	public void finish() {
+		// not needed
+	}
+
+	@Override
+	public void init(final ModelType modelType, final int currentModelIndex, final int numberOfModels)
+			throws Throwable {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performedChanges() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean process(final IElement root) throws Throwable {
+		if (root instanceof final Unit boogieFile) {
+			System.out.print(boogieFile);
+		}
+		return false;
+	}
+}
