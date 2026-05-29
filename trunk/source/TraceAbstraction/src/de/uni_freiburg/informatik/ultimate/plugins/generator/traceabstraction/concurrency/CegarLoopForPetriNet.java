7354a6de175a459f55b56e84ce7af3f146802425
maul.esel <maul.esel@posteo.de>
2026-05-29 09:19:15 +0200

    fix compile error introduced by eb55be9f

--- trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/concurrency/CegarLoopForPetriNet.java
+++ trunk/source/TraceAbstraction/src/de/uni_freiburg/informatik/ultimate/plugins/generator/traceabstraction/concurrency/CegarLoopForPetriNet.java
@@ -287,7 +287,7 @@ public class CegarLoopForPetriNet<L extends IIcfgTransition<?>>
 				mAbstraction = enhancementResult.getSecond().getResult();
 				final var finPrefix = enhancementResult.getSecond().getFinitePrefixOfDifference();
 				if (mProofProducer != null && finPrefix.getAcceptingRun() == null) {
-					mProofProducer.finalize(mAbstraction, finPrefix.getResult());
+					mProofProducer.finalize(mAbstraction);
 				}
 			} else {
 				final Difference<L, IPredicate, ?> diff = new Difference<>(new AutomataLibraryServices(getServices()),
