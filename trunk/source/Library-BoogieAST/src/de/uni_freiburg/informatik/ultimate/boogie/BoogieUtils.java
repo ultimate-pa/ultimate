60ac6e9e709843d8442295cdc9c8f4228faf1dc6
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2026-07-02 13:12:13 +0200

    Make field private

--- trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/BoogieUtils.java
+++ trunk/source/Library-BoogieAST/src/de/uni_freiburg/informatik/ultimate/boogie/BoogieUtils.java
@@ -53,7 +53,7 @@ import de.uni_freiburg.informatik.ultimate.core.model.models.ILocation;
  * Provides static auxiliary methods for Boogie.
  */
 public class BoogieUtils {
-	public static final String AUXILIARY_LABEL = "auxiliary_label";
+	private static final String AUXILIARY_LABEL = "auxiliary_label";
 
 	public static final String INIT_PROCEDURE = "ULTIMATE.init";
 	public static final String START_PROCEDURE = "ULTIMATE.start";
