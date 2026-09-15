5f8d42a10832b1434e8d3f8f3887bb8bc189f1dc
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2026-09-15 17:40:57 +0200

    Update filename of launcher JAR

--- releaseScripts/default/adds/run-ultimate.sh
+++ releaseScripts/default/adds/run-ultimate.sh
@@ -4,6 +4,6 @@ java \
 -Dosgi.configuration.area=config/ \
 -Xmx10G \
 -Xss4m \
--jar plugins/org.eclipse.equinox.launcher_1.7.0.v20250519-0528.jar \
+-jar plugins/org.eclipse.equinox.launcher_1.8.0.v20260804-1928.jar \
 -data config/data \
 "$@"
--- trunk/source/WebsiteStatic/scripts/webinterface/externals.py
+++ trunk/source/WebsiteStatic/scripts/webinterface/externals.py
@@ -16,7 +16,7 @@ def get_ultimate_cli() -> list[str]:
     jar = (
         ult_bin.parent
         / "plugins"
-        / "org.eclipse.equinox.launcher_1.7.0.v20250519-0528.jar"
+        / "org.eclipse.equinox.launcher_1.8.0.v20260804-1928.jar"
     )
     datadir = ult_bin.parent / "data"
     return [
